45ce4a0040a2da91d40a5f696d124e8312daec74
Darryl McAdams <psygnisfive@yahoo.com>
2016-12-05 18:23:45 -0500

    adds use of Core.Program instead of raw Env's

--- src/Interface/Integration.hs
+++ src/Interface/Integration.hs
@@ -14,6 +14,7 @@ import Utils.ABT
 import Utils.Env
 import Utils.Vars
 import qualified PlutusCore.Term as Core
+import qualified PlutusCore.Program as Core
 import Plutus.Parser
 import Elaboration.Elaboration
 import Elaboration.Elaborator
@@ -21,6 +22,18 @@ import Elaboration.Elaborator
 
 
 
+
+-- | This function converts a program to a declaration environment.
+
+programToDeclEnv :: Core.Program -> Env String Core.Term
+programToDeclEnv (Core.Program decls) =
+  [ (n,m) | Core.TermDeclaration n m <- decls ]
+
+-- | This function convers a declaration environment to a program.
+
+declEnvToProgram :: Env String Core.Term -> Core.Program
+declEnvToProgram env =
+  Core.Program (map (uncurry Core.TermDeclaration) env)
 
 -- | This function parses and elaborates a program.
 
@@ -34,23 +47,23 @@ loadProgram src =
 -- | This function loads a validator program and ensures that it can be used
 -- to validate a transaction.
 
-loadValidator :: String -> Either String (Env String Core.Term)
+loadValidator :: String -> Either String Core.Program
 loadValidator src =
   do env <- loadProgram src
      case lookup "validator" env of
        Nothing -> Left "Validators must declare the term `validator`"
-       Just _ -> return env
+       Just _ -> return $ declEnvToProgram env
 
 
 -- | This function loads a redeemer program and ensures that it can be used to
 -- redeem a transaction.
 
-loadRedeemer :: String -> Either String (Env String Core.Term)
+loadRedeemer :: String -> Either String Core.Program
 loadRedeemer src =
   do env <- loadProgram src
      case lookup "redeemer" env of
        Nothing -> Left "Redeemers must declare the term `redeemer`"
-       Just _ -> return env
+       Just _ -> return $ declEnvToProgram env
 
 
 -- | This function takes validator and redeemer programs, ensures that they
@@ -60,11 +73,13 @@ loadRedeemer src =
 -- transaction.
 
 buildValidationScript
-  :: Env String Core.Term
-  -> Env String Core.Term
+  :: Core.Program
+  -> Core.Program
   -> Either String (Core.Term, Env String Core.Term)
-buildValidationScript valenv redenv =
-  case [ n | (n,_) <- valenv, any (\(n',_) -> n == n') redenv ] of
+buildValidationScript valprog redprog =
+  let valenv = programToDeclEnv valprog
+      redenv = programToDeclEnv redprog
+  in case [ n | (n,_) <- valenv, any (\(n',_) -> n == n') redenv ] of
     [] ->
       case (lookup "validator" valenv, lookup "redeemer" redenv) of
         (Nothing,Nothing) ->
