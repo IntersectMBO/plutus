abd10d709ec0cd7f616e81a431d368e408dd85d0
Darryl McAdams <psygnisfive@yahoo.com>
2016-12-08 09:32:08 -0500

    adds debugging tools

---
+++ src/Debug.hs
@@ -0,0 +1,12 @@
+module Debug where
+
+import System.IO.Unsafe
+
+debug :: Monad m => String -> IO () -> m ()
+debug n d = unsafePerformIO (putStrLn n >> d) `seq` return ()
+
+debug_ :: Monad m => String -> m ()
+debug_ n = debug n (return ())
+
+debugSeq :: String -> a -> a
+debugSeq s x = unsafePerformIO (putStrLn s) `seq` x
