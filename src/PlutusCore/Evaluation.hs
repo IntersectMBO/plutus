1ad065a85bc5d001483701cbebc2d5592aa89993
Artyom <yom@artyom.me>
2017-01-21 00:50:31 +0300

    Fix the same typo in another place :/

--- src/PlutusCore/Evaluation.hs
+++ src/PlutusCore/Evaluation.hs
@@ -282,7 +282,7 @@ instance Eval (Env (Sourced String) Term) Term where
                             (BS.pack
                               (BA.unpack (hash (BS.toStrict x) :: Digest SHA3_256)))))
           _ ->
-            throwError $ "Incorrect arguments for builtin sha2_256: "
+            throwError $ "Incorrect arguments for builtin sha3_256: "
                       ++ intercalate "," (map pretty xs)
       builtin "equalsByteString" xs =
         case xs of
