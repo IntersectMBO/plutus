e2e2711e6978002279b4d7c49cab1aff47a2fd43
Artyom <yom@artyom.me>
2017-03-09 10:48:06 +0100

    exports Plutus prelude as a string

--- src/Interface/Prelude.hs
+++ src/Interface/Prelude.hs
@@ -13,6 +13,7 @@ module Interface.Prelude where
 import qualified PlutusCore.Program as Core
 import Interface.Integration
 
+import System.FilePath
 import Paths_plutus_prototype
 
 
@@ -20,8 +21,11 @@ import Paths_plutus_prototype
 
 prelude :: IO Core.Program
 prelude = do
-  preludePath <- getDataFileName "src/Prelude.pls"
-  mbRes <- runElabInContexts [] . loadProgram <$> readFile preludePath
+  mbRes <- runElabInContexts [] . loadProgram <$> preludeString
   case mbRes of
     Left err -> error ("Error while parsing Plutus prelude: " ++ err)
     Right x  -> return x
+
+-- | Prelude as a string, useful for compiling it into the binary
+preludeString :: IO String
+preludeString = readFile =<< getDataFileName ("src" </> "Prelude.pls")
