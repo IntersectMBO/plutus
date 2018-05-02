2d5bee8c16d66a8856260f4bb9865625178f3822
Vanessa McHale <vamchale@gmail.com>
2018-05-02 08:26:49 -0500

    more minor fixes

--- language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
+++ language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
@@ -1,9 +1,20 @@
-module Language.PlutusNapkin.Type ( PlutusNapkin (..)
+{-# LANGUAGE DeriveFunctor #-}
+
+module Language.PlutusNapkin.Type ( PlutusNapkinF (..)
+                                  , PlutusNapkin
+                                  , Fix (..)
                                   ) where
 
 import qualified Data.ByteString.Lazy as BSL
 
-data PlutusNapkin = PNByteString BSL.ByteString
-                  | PNInt Int
-                  | PNFloat Float
-                  | Let PlutusNapkin PlutusNapkin
+-- | Base functor for Plutus Napkin expressions
+data PlutusNapkinF a x = PNByteString a BSL.ByteString
+                       | PNInt a Int
+                       | PNFloat a Float
+                       | Let a x x
+                       deriving (Functor)
+
+newtype Fix f = Fix { unFix :: f (Fix f) }
+
+-- | Annotated type for Plutus Napkin expressions.
+type PlutusNapkin a = Fix (PlutusNapkinF a)
