865bdcf131d289dab8ea0ed8e2dec2785df3d1c5
Vanessa McHale <vanessa.mchale@iohk.io>
2018-05-04 14:19:36 -0500

    updated AST

--- language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
+++ language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
@@ -1,8 +1,6 @@
 module Language.PlutusNapkin.Type ( Term (..)
                                   , Type (..)
                                   , Name (..)
-                                  , Dec (..)
-                                  , Clause (..)
                                   , KindSig (..)
                                   , Alt (..)
                                   , Builtin (..)
@@ -11,6 +9,7 @@ module Language.PlutusNapkin.Type ( Term (..)
 
 import qualified Data.ByteString.Lazy as BSL
 import           Data.List.NonEmpty
+import           GHC.Natural
 
 data Builtin = AddInteger
              | SubtractInteger
@@ -41,6 +40,7 @@ data Builtin = AddInteger
              | DropByteString
              | SHA2
              | SHA3
+             | VerifySignature
              | EqByteString
              | TxHash
              | BlockNum
@@ -49,20 +49,18 @@ data Builtin = AddInteger
 -- | Annotated type for names
 data Name a = LexVar a BSL.ByteString
             | LexTyVar a BSL.ByteString
-            | LexTmName a BSL.ByteString
-            | LexMod a BSL.ByteString
+            | LexTermVar a BSL.ByteString
             | LexInt a Integer
-            | LexFloat a Float -- TODO watch for silent truncation in the lexer
+            | LexFloat a Float -- TODO check for silent truncation in the lexer
             | LexExp a Integer
             | LexBS a BSL.ByteString
             | LexBuiltin a Builtin
+            | LexSize a Natural
+            | LexSizeTerm a BSL.ByteString
 
--- | Base functor for types.
-data Type a = TyVar a (Name a) -- TODO actual type safety w.r.t lexical parts?
-            | DecTypye a (Name a)
+data Type a = TyVar a (Name a)
             | TyFun a (Type a) (Type a)
-            | TyCon a (Name a) (Type a)
-            | TyComp a (Type a)
+            | TyFix a (Name a) (Kind a) (Type a)
             | TyForall a (Name a) (Kind a) (Type a)
             | TyByteString
             | TyInteger
@@ -70,35 +68,25 @@ data Type a = TyVar a (Name a) -- TODO actual type safety w.r.t lexical parts?
             | TyLam a (Name a) (Kind a) (Type a)
             | TyApp a (Type a) (NonEmpty (Type a))
 
--- | Base functor for terms
 data Term a = Var a BSL.ByteString
-            | DecName a BSL.ByteString
             | TyAnnot a (Type a) (Term a)
-            | LetRec a (Type a) [Dec a] (Term a)
             | TyAbs a (Name a) (Term a)
             | TyInst a (Term a) (Type a)
-            | TyCase a (Term a) (Clause a)
-            | Success a (Term a)
-            | Failure
-            | Bind a (Term a) (Name a) (Term a)
+            | LamAbs a (Name a) (Term a)
+            | Apply a (Term a) (NonEmpty (Term a))
+            | Fix a (Name a) (Term a)
+            | Builtin a Builtin [Term a]
             | PrimInt Integer
             | PrimFloat Float
             | PrimBS BSL.ByteString
-            | Builtin a Builtin [Term a]
-
-data Clause a = Clause (Name a) [Name a] (Term a)
-
--- | Base functor for declarations.
-data Dec a = TypeDec a (Name a) (Type a)
-           | TermDef a (Name a) (Term a)
-           | DataDec a (Name a) [KindSig a] [Alt a]
-           | TermDec a (Name a) (Type a)
+            | PrimSize (Name a)
 
 -- | An alternative; used for sum types.
 data Alt a = Alt (Name a) [Type a]
 
 -- | Base functor for kinds.
-data Kind a = Type (Type a)
-            | KindFun (Kind a) (Kind a)
+data Kind a = Type a
+            | KindArrow (Kind a) (Kind a)
+            | Size a
 
 data KindSig a = KindSig a (Name a) (Kind a)
