34951a204be6cd99defe51a9103454982320cd2c
Vanessa McHale <vanessa.mchale@iohk.io>
2018-05-03 20:59:58 -0500

    don't use base-functor approach

--- language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
+++ language-plutus-napkin/src/Language/PlutusNapkin/Type.hs
@@ -1,25 +1,16 @@
-{-# LANGUAGE DeriveFunctor #-}
-
-module Language.PlutusNapkin.Type ( Fix (..)
-                                  , TermF (..)
-                                  , Term
-                                  , TypeF (..)
-                                  , Type
-                                  , Lex (..)
-                                  , DecF (..)
-                                  , Dec
+module Language.PlutusNapkin.Type ( Term (..)
+                                  , Type (..)
+                                  , Name (..)
+                                  , Dec (..)
                                   , Clause (..)
                                   , KindSig (..)
                                   , Alt (..)
                                   , Builtin (..)
+                                  , Kind (..)
                                   ) where
 
 import qualified Data.ByteString.Lazy as BSL
 import           Data.List.NonEmpty
-
--- FIXME use the actual spec!
---
--- Term, Clause, Declaration, Type, Kind, Data Declaration, etc.
 
 data Builtin = AddInteger
              | SubtractInteger
@@ -55,67 +46,59 @@ data Builtin = AddInteger
              | BlockNum
              | BlockTime
 
--- | Lexical units
-data Lex a = LexVar a BSL.ByteString
-           | LexTyVar a BSL.ByteString
-           | LexTmName a BSL.ByteString
-           | LexMod a BSL.ByteString
-           | LexInt a Integer
-           | LexFloat a Float -- TODO watch for silent truncation in the lexer
-           | LexExp a Integer
-           | LexBS a BSL.ByteString
-           | LexBuiltin a Builtin
+-- | Annotated type for names
+data Name a = LexVar a BSL.ByteString
+            | LexTyVar a BSL.ByteString
+            | LexTmName a BSL.ByteString
+            | LexMod a BSL.ByteString
+            | LexInt a Integer
+            | LexFloat a Float -- TODO watch for silent truncation in the lexer
+            | LexExp a Integer
+            | LexBS a BSL.ByteString
+            | LexBuiltin a Builtin
 
--- TODO builtins
-data TypeF a x = TyVar a (Lex a) -- TODO actual type safety here??
-               | DecTypye a (Lex a)
-               | TyFun a x x
-               | TyCon a (Lex a) x
-               | TyComp a x
-               | TyForall a (Lex a) (Kind a) x
-               | TyByteString
-               | TyInteger
-               | TyFloat
-               | TyLam a (Lex a) (Kind a) x
-               | TyApp a x (NonEmpty x)
-               deriving (Functor)
+-- | Base functor for types.
+data Type a = TyVar a (Name a) -- TODO actual type safety w.r.t lexical parts?
+            | DecTypye a (Name a)
+            | TyFun a (Type a) (Type a)
+            | TyCon a (Name a) (Type a)
+            | TyComp a (Type a)
+            | TyForall a (Name a) (Kind a) (Type a)
+            | TyByteString
+            | TyInteger
+            | TyFloat
+            | TyLam a (Name a) (Kind a) (Type a)
+            | TyApp a (Type a) (NonEmpty (Type a))
 
-data TermF a x = Var a BSL.ByteString
-               | DecName a BSL.ByteString
-               | TyAnnot a (Type a) x
-               | LetRec a (Type a) [Dec a] x
-               | TyAbs a (Lex a) x
-               | TyInst a (Term a) (Type a)
-               | TyCase a x (Clause a)
-               | Success a x
-               | Failure
-               | Bind a x (Lex a) x
-               | PrimInt Integer
-               | PrimFloat Float
-               | PrimBS BSL.ByteString
-               deriving (Functor)
+-- | Base functor for terms
+data Term a = Var a BSL.ByteString
+            | DecName a BSL.ByteString
+            | TyAnnot a (Type a) (Term a)
+            | LetRec a (Type a) [Dec a] (Term a)
+            | TyAbs a (Name a) (Term a)
+            | TyInst a (Term a) (Type a)
+            | TyCase a (Term a) (Clause a)
+            | Success a (Term a)
+            | Failure
+            | Bind a (Term a) (Name a) (Term a)
+            | PrimInt Integer
+            | PrimFloat Float
+            | PrimBS BSL.ByteString
+            | Builtin a Builtin [Term a]
 
-data Clause a = Clause (Lex a) [Lex a] (Term a)
+data Clause a = Clause (Name a) [Name a] (Term a)
 
-data DecF a x = TypeDec a (Lex a) (Type a)
-              | TermDef a (Lex a) (Term a)
-              | DataDec a (Lex a) [KindSig a] [Alt a]
-              | TermDec a (Lex a) (Type a)
-              deriving (Functor)
+-- | Base functor for declarations.
+data Dec a = TypeDec a (Name a) (Type a)
+           | TermDef a (Name a) (Term a)
+           | DataDec a (Name a) [KindSig a] [Alt a]
+           | TermDec a (Name a) (Type a)
 
-data Alt a = Alt (Lex a) [Type a]
+-- | An alternative; used for sum types.
+data Alt a = Alt (Name a) [Type a]
 
 -- | Base functor for kinds.
-data KindF a x = Type (Type a)
-               | KindFun x x
-               deriving (Functor)
+data Kind a = Type (Type a)
+            | KindFun (Kind a) (Kind a)
 
-data KindSig a = KindSig a (Lex a) (Kind a)
-
-newtype Fix f = Fix { unFix :: f (Fix f) }
-
--- | Annotated type for Plutus Napkin expressions.
-type Term a = Fix (TermF a)
-type Type a = Fix (TypeF a)
-type Dec a = Fix (DecF a)
-type Kind a = Fix (KindF a)
+data KindSig a = KindSig a (Name a) (Kind a)
