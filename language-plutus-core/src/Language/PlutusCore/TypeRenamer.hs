48bca6cabd2513769e96f3352a0ef7357f08d5c6
Vanessa McHale <vanessa.mchale@iohk.io>
2018-06-01 08:11:17 -0500

    work on typechecker/kind checker

--- language-plutus-core/src/Language/PlutusCore/TypeRenamer.hs
+++ language-plutus-core/src/Language/PlutusCore/TypeRenamer.hs
@@ -3,10 +3,14 @@
 module Language.PlutusCore.TypeRenamer ( rename
                                        , fill
                                        , kindCheck
+                                       , typeCheck
                                        , TypeAnnot
                                        , CheckM (..)
+                                       , CheckState (..)
+                                       , TypeError (..)
                                        ) where
 
+import           Control.Monad.Except
 import           Control.Monad.State.Lazy
 import           Data.Functor.Foldable
 import qualified Data.IntMap                    as IM
@@ -31,20 +35,51 @@ type TypeContext = IM.IntMap (Type ())
 -- We need a state monad for kind checking...
 
 data CheckState = CheckState { kindContext :: KindContext
-                             , typeContext :: TypeContext }
+                             , typeContext :: TypeContext
+                             }
 
-data TypeError
+data TypeError = KindMismatch (Kind ()) (Kind ())
+               | InternalError
+
+-- all builtin types have the same kind
+builtinKind :: Kind ()
+builtinKind = KindArrow () (Size ()) (Type ())
 
 newtype CheckM a = CheckM { unCheckM :: StateT CheckState (Either TypeError) a }
-    deriving (Functor, Applicative, Monad, MonadState CheckState)
+    deriving (Functor, Applicative, Monad, MonadState CheckState, MonadError TypeError)
 
+extract :: Type a -> a
+extract (TyApp x _ _)      = x
+extract (TyVar x _)        = x
+extract (TyFun x _ _)      = x
+extract (TyFix x _ _ _)    = x
+extract (TyForall x _ _ _) = x
+extract (TyBuiltin x _)    = x
+extract (TyLam x _ _ _)    = x
+
+-- TODO: throw an error at the end if type-checking is ambiguous?
+-- TODO: figure out a way to "assume" things for added context?
 kindCheck :: Type KindAnnot -> CheckM (Type KindAnnot)
 kindCheck (TyVar Nothing n) = do
     kSt <- gets kindContext
     let maybeKind = IM.lookup (unUnique $ nameUnique n) kSt
     pure $ TyVar maybeKind n
+kindCheck (TyBuiltin Nothing x) =
+    pure (TyBuiltin (Just builtinKind) x)
+kindCheck (TyFun Nothing t t') = do
+    t0 <- extract <$> kindCheck t
+    t1 <- extract <$> kindCheck t'
+    if t0 == pure (Type ()) && t1 ==  pure (Type ())
+        then pure (TyFun (Just (Type ())) t t')
+        else throwError InternalError -- TODO do something better here
 kindCheck x = pure x
--- kindCheck (TyForall l n k t) =
+
+typeCheck :: Term TypeAnnot -> CheckM (Term TypeAnnot)
+typeCheck (Var Nothing n) = do
+    tSt <- gets typeContext
+    let maybeType = IM.lookup (unUnique $ nameUnique n) tSt
+    pure $ Var maybeType n
+typeCheck x = pure x
 
 -- TODO: how should we handle this? should it be based on the judgment rules?
 -- If so, it should be relatively easy, however, I need to know it will
