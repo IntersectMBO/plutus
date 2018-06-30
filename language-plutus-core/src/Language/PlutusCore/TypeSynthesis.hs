079546b856aa388dee25278711099bee6645d968
Vanessa McHale <vanessa.mchale@iohk.io>
2018-06-30 15:16:03 -0500

    kindOf updated

--- language-plutus-core/src/Language/PlutusCore/TypeSynthesis.hs
+++ language-plutus-core/src/Language/PlutusCore/TypeSynthesis.hs
@@ -11,12 +11,14 @@ import           Language.PlutusCore.Lexer.Type
 import           Language.PlutusCore.Name
 import           Language.PlutusCore.Type
 import           Language.PlutusCore.TypeRenamer
+import           PlutusPrelude
 
 data BuiltinTable a = BuiltinTable (M.Map TypeBuiltin (Kind a)) (M.Map BuiltinName (Type TyNameWithKind a))
 type TypeCheckM a = ReaderT (BuiltinTable a) (Either (TypeError a))
 
 data TypeError a = NotImplemented
                  | InternalError
+                 | KindMismatch
 
 isType :: Kind a -> Bool
 isType Type{} = True
@@ -28,12 +30,12 @@ kindOf (TyFun _ ty' ty'') = do
     k' <- kindOf ty''
     if isType k && isType k'
         then pure (Type ())
-        else throwError NotImplemented
+        else throwError KindMismatch
 kindOf (TyForall _ _ _ ty) = do
     k <- kindOf ty
     if isType k
         then pure (Type ())
-        else throwError NotImplemented
+        else throwError KindMismatch
 kindOf (TyLam _ _ k ty) =
     [ KindArrow () (void k) k' | k' <- kindOf ty ]
 kindOf (TyVar _ (TyNameWithKind (TyName (Name (_, k) _ _)))) = pure (void k)
@@ -46,8 +48,17 @@ kindOf (TyFix _ _ ty) = do
     k <- kindOf ty
     if isType k
         then pure (Type ())
-        else throwError NotImplemented
-kindOf _ = throwError NotImplemented
+        else throwError KindMismatch
+kindOf (TyApp _ ty (ty' :| [])) = do
+    k <- kindOf ty
+    case k of
+        KindArrow _ k' k'' -> do
+            k''' <- kindOf ty'
+            if k'' == k'''
+                then pure k'
+                else throwError KindMismatch
+        _ -> throwError KindMismatch
+kindOf TyApp{} = throwError NotImplemented
 
 typeOf :: Term TyNameWithKind NameWithType a -> TypeCheckM a (Type TyNameWithKind ())
 typeOf (Var _ (NameWithType (Name (_, ty) _ _))) = pure (void ty)
