a3d4cb4ffaab67607b0d016898b29a33f029f217
effectfully <effectfully@gmail.com>
2018-07-13 16:46:53 +0300

    Implement 'computeConstantApplication' and 'isValue'

--- language-plutus-core/src/Language/PlutusCore/CkMachine.hs
+++ language-plutus-core/src/Language/PlutusCore/CkMachine.hs
@@ -18,13 +18,39 @@ data Frame tyname name a = FrameTyAbs a (tyname a) (Kind a) (Term tyname name a)
 type Context tyname name a = [Frame tyname name a]
 
 isValue :: Term tyname name a -> Bool
-isValue = undefined
+isValue (TyAbs  _ _ _ body) = isValue body
+isValue (Wrap   _ _ _ term) = isValue term
+isValue (LamAbs _ _ _ body) = isValue body
+isValue (Constant _ _)      = True
+isValue _                   = False
 
 subst :: name a -> Term tyname name a -> Term tyname name a -> Term tyname name a
 subst = undefined
 
+viewConstant :: Term tyname name a -> Maybe (Constant a)
+viewConstant (Constant _ constant) = Just constant
+viewConstant _                     = Nothing
+
+reduceConstantApply :: Constant a -> [Constant a] -> Maybe (Constant a)
+reduceConstantApply = undefined
+
+data IteratedApplication tyname name a = IteratedApplication
+    { iteratedApplicationHead  :: Term tyname name a
+    , iteratedApplicationSpine :: [Term tyname name a]
+    }
+
+viewIteratedApplication :: Term tyname name a -> Maybe (IteratedApplication tyname name a)
+viewIteratedApplication term@Apply{} = Just $ go id term where
+  go k (Apply _ fun arg) = go (k . (undefined arg :)) fun
+  go k  fun              = IteratedApplication fun $ k []
+viewIteratedApplication _            = Nothing
+
 computeConstantApplication :: Term tyname name a -> Maybe (Term tyname name a)
-computeConstantApplication = undefined
+computeConstantApplication term = do
+  IteratedApplication termHead termSpine <- viewIteratedApplication term
+  constHead <- viewConstant termHead
+  constSpine <- traverse viewConstant termSpine
+  return . maybe term (Constant undefined {- again -}) $ reduceConstantApply constHead constSpine
 
 -- s ▷ abs α K M  ↦ s ◁ abs α K M
 -- s ▷ {M A}      ↦ s , {_ A} ▷ M
@@ -59,7 +85,7 @@ FrameWrap ann tyn ty : stack <| term             | isValue term = stack <| Wrap 
 FrameUnwrap          : stack <| Wrap _ _ _ term  = stack <| term
 FrameApplyArg arg    : stack <| fun              = FrameApplyFun fun : stack |> arg
 FrameApplyFun fun    : stack <| arg              = case fun of
-  LamAbs _ name _ body -> stack |> subst name arg body
-  _                    -> stack |> Apply (undefined {- what's here? -}) fun (undefined arg)
+    LamAbs _ name _ body -> stack |> subst name arg body
+    _                    -> stack |> Apply (undefined {- what's here? -}) fun (undefined arg)
 _                    : stack <| err@Error{}      = stack <| err
 _                            <| _                = error "Panic: unhandled case in `(|>)`"
