94b3c4a82088cafcb43a19f0eba219d0a812f6f1
effectfully <effectfully@gmail.com>
2018-07-13 15:56:43 +0300

    Add rules in comments and handling of constants

--- language-plutus-core/src/Language/PlutusCore/CkMachine.hs
+++ language-plutus-core/src/Language/PlutusCore/CkMachine.hs
@@ -23,32 +23,43 @@ isValue = undefined
 subst :: name a -> Term tyname name a -> Term tyname name a -> Term tyname name a
 subst = undefined
 
+computeConstantApplication :: Term tyname name a -> Maybe (Term tyname name a)
+computeConstantApplication = undefined
+
+-- s ▷ abs α K M  ↦ s ◁ abs α K M
+-- s ▷ {M A}      ↦ s , {_ A} ▷ M
+-- s ▷ wrap α A M ↦ s , (wrap α S _) ▷ M
+-- s ▷ unwrap M   ↦ s , (unwrap _) ▷ M
+-- s ▷ lam x A M  ↦ s ◁ lam x A M
+-- s ▷ error A    ↦ s ◁ error A
+-- s ▷ [M N]      ↦ s , [_ N] ▷ M
+-- s ▷ [C S]      ↦ s ◁ [C S]    -- partially saturated constant
+-- s ▷ [C S]      ↦ s ◁ V        -- fully saturated constant, [C S] ~> V
 (|>) :: Context tyname name a -> Term tyname name a -> Constant a
-stack |> tyAbs@TyAbs{}         = stack <| tyAbs
-stack |> TyInst _ fun _        = FrameTyInstArg : stack |> fun
-stack |> Wrap ann tyn ty term  = FrameWrap ann tyn ty : stack |> term
-stack |> Unwrap _ term         = FrameUnwrap : stack |> term
-stack |> lamAbs@LamAbs{}       = stack <| lamAbs
-stack |> Apply _ fun arg       = FrameApplyArg (undefined arg) : stack |> fun
-stack |> err@Error{}           = stack <| err
-stack |> Constant ann constant = undefined
-_     |> _                     = error "Panic: unhandled case in `(|>)`"
+stack |> tyAbs@TyAbs{}        = stack <| tyAbs
+stack |> TyInst _ fun _       = FrameTyInstArg : stack |> fun
+stack |> Wrap ann tyn ty term = FrameWrap ann tyn ty : stack |> term
+stack |> Unwrap _ term        = FrameUnwrap : stack |> term
+stack |> lamAbs@LamAbs{}      = stack <| lamAbs
+stack |> err@Error{}          = stack <| err
+stack |> term                 | Just constApp <- computeConstantApplication term = stack <| constApp
+stack |> Apply _ fun arg      = FrameApplyArg (undefined arg) : stack |> fun
+_     |> _                    = error "Panic: unhandled case in `(|>)`"
 
+-- s , {_ S}           ◁ abs α K M  ↦ s ▷ M
+-- s , (wrap α S _)    ◁ V          ↦ s ▷ wrap α S V
+-- s , (unwrap _)      ◁ wrap α A V ↦ s ◁ V
+-- s , [_ N]           ◁ V          ↦ s , [V _] ▷ N
+-- s , [(lam x A M) _] ◁ V          ↦ s ▷ [V/x]M
+-- s , [C _]           ◁ S          ↦ s ▷ [C S]
+-- s , f               ◁ error A    ↦ s ◁ error A
 (<|) :: Context tyname name a -> Term tyname name a -> Constant a
-FrameTyInstArg       : stack <| TyAbs _ _ _ body    = stack |> body
-FrameWrap ann tyn ty : stack <| term | isValue term = stack <| Wrap ann tyn ty term
-FrameUnwrap          : stack <| Wrap _ _ _ term     = stack <| term
-FrameApplyArg arg    : stack <| fun                 = FrameApplyFun fun : stack |> arg
-FrameApplyFun fun    : stack <| arg                 = case fun of
+FrameTyInstArg       : stack <| TyAbs _ _ _ body = stack |> body
+FrameWrap ann tyn ty : stack <| term             | isValue term = stack <| Wrap ann tyn ty term
+FrameUnwrap          : stack <| Wrap _ _ _ term  = stack <| term
+FrameApplyArg arg    : stack <| fun              = FrameApplyFun fun : stack |> arg
+FrameApplyFun fun    : stack <| arg              = case fun of
   LamAbs _ name _ body -> stack |> subst name arg body
-  _                    -> error "Panic: Passed an argument to a non-lambda"
-_                            <| _                   = error "Panic: unhandled case in `(|>)`"
-
-{-
-FrameApplyArg arg1 : FrameApplyArg arg2 : stack <| Constant addInteger =
-  case (evaluateCk arg1, evaluateCk arg2) of
-    (BuiltinInt x1, BuiltinInt x2) -> BuiltinInt (x1 + x2)
--}
-
-evaluateCk :: Term tyname name a -> Constant a
-evaluateCk term = [] |> term
+  _                    -> stack |> Apply (undefined {- what's here? -}) fun (undefined arg)
+_                    : stack <| err@Error{}      = stack <| err
+_                            <| _                = error "Panic: unhandled case in `(|>)`"
