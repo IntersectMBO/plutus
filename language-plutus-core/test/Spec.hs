fa47a129b1d6f199a7534bbb73428aed2c9ebb3d
Vanessa McHale <vanessa.mchale@iohk.io>
2018-08-06 07:07:27 -0500

    test

--- language-plutus-core/test/Spec.hs
+++ language-plutus-core/test/Spec.hs
@@ -6,9 +6,8 @@ module Main ( main
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text            as T
 import           Data.Text.Encoding   (encodeUtf8)
-import           Hedgehog             hiding (Size, Var, annotate)
-import qualified Hedgehog.Gen         as Gen
-import qualified Hedgehog.Range       as Range
+import           Generators
+import           Hedgehog             hiding (Var, annotate)
 import           Language.PlutusCore
 import           PlutusPrelude
 import           Test.Tasty
@@ -54,75 +53,6 @@ compareType _ _                                      = False
 
 compareProgram :: Eq a => Program TyName Name a -> Program TyName Name a -> Bool
 compareProgram (Program _ v t) (Program _ v' t') = v == v' && compareTerm t t'
-
-genVersion :: MonadGen m => m (Version AlexPosn)
-genVersion = Version emptyPosn <$> int' <*> int' <*> int'
-    where int' = Gen.integral_ (Range.linear 0 10)
-
-genTyName :: MonadGen m => m (TyName AlexPosn)
-genTyName = TyName <$> genName
-
--- TODO make this robust against generating identfiers such as "fix"?
-genName :: MonadGen m => m (Name AlexPosn)
-genName = Name emptyPosn <$> name' <*> int'
-    where int' = Unique <$> Gen.int (Range.linear 0 3000)
-          name' = BSL.fromStrict <$> Gen.utf8 (Range.linear 1 20) Gen.lower
-
-simpleRecursive :: MonadGen m => [m a] -> [m a] -> m a
-simpleRecursive = Gen.recursive Gen.choice
-
-genKind :: MonadGen m => m (Kind AlexPosn)
-genKind = simpleRecursive nonRecursive recursive
-    where nonRecursive = pure <$> sequence [Type, Size] emptyPosn
-          recursive = [KindArrow emptyPosn <$> genKind <*> genKind]
-
-genBuiltinName :: MonadGen m => m BuiltinName
-genBuiltinName = Gen.choice $ pure <$>
-    [ AddInteger, SubtractInteger, MultiplyInteger, DivideInteger, RemainderInteger
-    , LessThanInteger, LessThanEqInteger, GreaterThanInteger, GreaterThanEqInteger
-    , EqInteger, ResizeInteger, IntToByteString, Concatenate, TakeByteString
-    , DropByteString, ResizeByteString, SHA2, SHA3, VerifySignature
-    , EqByteString, TxHash, BlockNum, BlockTime
-    ]
-
-genBuiltin :: MonadGen m => m (Constant AlexPosn)
-genBuiltin = Gen.choice [BuiltinName emptyPosn <$> genBuiltinName, genInt, genSize, genBS]
-    where int' = Gen.integral_ (Range.linear (-10000000) 10000000)
-          size' = Gen.integral_ (Range.linear 1 10)
-          string' = BSL.fromStrict <$> Gen.utf8 (Range.linear 0 40) Gen.unicode
-          genInt = BuiltinInt emptyPosn <$> size' <*> int'
-          genSize = BuiltinSize emptyPosn <$> size'
-          genBS = BuiltinBS emptyPosn <$> size' <*> string'
-
-genType :: MonadGen m => m (Type TyName AlexPosn)
-genType = simpleRecursive nonRecursive recursive
-    where varGen = TyVar emptyPosn <$> genTyName
-          funGen = TyFun emptyPosn <$> genType <*> genType
-          lamGen = TyLam emptyPosn <$> genTyName <*> genKind <*> genType
-          forallGen = TyForall emptyPosn <$> genTyName <*> genKind <*> genType
-          applyGen = TyApp emptyPosn <$> genType <*> genType
-          numGen = TyInt emptyPosn <$> Gen.integral (Range.linear 0 256)
-          recursive = [funGen, applyGen]
-          nonRecursive = [varGen, lamGen, forallGen, numGen]
-
-genTerm :: MonadGen m => m (Term TyName Name AlexPosn)
-genTerm = simpleRecursive nonRecursive recursive
-    where varGen = Var emptyPosn <$> genName
-          absGen = TyAbs emptyPosn <$> genTyName <*> genKind <*> genTerm
-          instGen = TyInst emptyPosn <$> genTerm <*> genType
-          lamGen = LamAbs emptyPosn <$> genName <*> genType <*> genTerm
-          applyGen = Apply emptyPosn <$> genTerm <*> genTerm
-          unwrapGen = Unwrap emptyPosn <$> genTerm
-          wrapGen = Wrap emptyPosn <$> genTyName <*> genType <*> genTerm
-          errorGen = Error emptyPosn <$> genType
-          recursive = [absGen, instGen, lamGen, applyGen, unwrapGen, wrapGen]
-          nonRecursive = [varGen, Constant emptyPosn <$> genBuiltin, errorGen]
-
-genProgram :: MonadGen m => m (Program TyName Name AlexPosn)
-genProgram = Program emptyPosn <$> genVersion <*> genTerm
-
-emptyPosn :: AlexPosn
-emptyPosn = AlexPn 0 0 0
 
 -- Generate a random 'Program', pretty-print it, and parse the pretty-printed
 -- text, hopefully returning the same thing.
