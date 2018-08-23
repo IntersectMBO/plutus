e3eba9b422d9ea34679d8f41f3dcfe96152db1fe
effectfully <roman.kireev@iohk.io>
2018-08-23 21:05:26 +0300

    Removed junk

--- language-plutus-core/test/Evaluation/Terms.hs
+++ language-plutus-core/test/Evaluation/Terms.hs
@@ -25,7 +25,6 @@ module Evaluation.Terms
 
 import           PlutusPrelude
 import           Language.PlutusCore
--- import           Language.PlutusCore.Constant
 
 -- | A type with a hole inside. The reason for having such a thing is that 'Wrap'
 -- expects the pattern functor of a recursive type while in type signatures we use
@@ -269,46 +268,6 @@ getBuiltinFoldNat = do
         . Apply () (Var () f)
         $ Var () z
 
--- -- | TODO: FIXME
--- --
--- -- > /\ (s :: size) -> fix {integer s} {nat} \(rec : integer s -> nat) (i : integer s) ->
--- -- >     if i == 0 then zero else succ (rec (i - s!1))
--- --
--- getBuiltinIntegerToNat :: Natural -> Fresh (Term TyName Name ())
--- getBuiltinIntegerToNat s = do
---     RecursiveType _ nat <- holedToRecursive <$> getBuiltinNat
---     ifThenElse <- getBuiltinIf
---     fix        <- getBuiltinFix
---     scottZero  <- getBuiltinZero
---     scottSucc  <- getBuiltinSucc
---     rec <- freshName () "rec"
---     i   <- freshName () "i"
---     let integerToTerm n
---             = foldl (Apply ()) (Constant () $ BuiltinName () ResizeInteger)
---             $ [ Constant () $ BuiltinSize () s
---               , Constant () $ BuiltinInt () 1 n
---               ]
---     return
---         . Apply () (foldl (TyInst ()) fix [TyBuiltin () TyInteger, nat])
---         . LamAbs () rec (TyFun () (TyBuiltin () TyInteger) nat)
---         . LamAbs () i (TyBuiltin () TyInteger)
---         . foldl (Apply ()) ifThenElse
---         $ [     foldl (Apply ()) (Constant () $ BuiltinName () EqInteger)
---               $ [ Var () i
---                 , Constant $ ()
---                 ]
---           , scottZero
---           ,     Apply () scottSucc
---               . Apply () (Var () rec)
---               . foldl (Apply ()) (Constant () $ BuiltinName () SubtractInteger)
---               $ [ Var () i
---                 ,     foldl (Apply ()) (Constant () $ BuiltinName () ResizeInteger)
---                     $ [ Constant () $ BuiltinSize () s
---                       , Constant () $ BuiltinInt () 1 1
---                       ]
---                 ]
---           ]
-
 -- | @List@ as a PLC type.
 --
 -- > \(a :: *). fix \(list :: *) -> all (r :: *). r -> (a -> list -> r) -> r
@@ -454,6 +413,6 @@ getBuiltinSum s = do
     let int = TyBuiltin () TyInteger
     return
         . foldl (Apply ()) (foldl (TyInst ()) foldList [int, int])
-        $ [ TyInst () (Constant () (BuiltinName () AddInteger)) $ TyInt () s  -- @TyVar () s@
-          , Constant () $ BuiltinInt () s 0                                   -- add 'resizeInteger'
+        $ [ TyInst () (Constant () (BuiltinName () AddInteger)) $ TyInt () s
+          , Constant () $ BuiltinInt () s 0
           ]
