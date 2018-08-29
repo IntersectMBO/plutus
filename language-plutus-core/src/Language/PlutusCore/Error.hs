29c47be40fbd556fe25c9a57ff3b92663b098032
Vanessa McHale <vanessa.mchale@iohk.io>
2018-08-29 13:37:33 -0500

    don't add spurious changes

--- language-plutus-core/src/Language/PlutusCore/Error.hs
+++ language-plutus-core/src/Language/PlutusCore/Error.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE ConstrainedClassMethods #-}
 {-# LANGUAGE DeriveAnyClass          #-}
-{-# LANGUAGE FlexibleContexts        #-}
 {-# LANGUAGE FlexibleInstances       #-}
 
 module Language.PlutusCore.Error ( Error (..)
                                  , IsError (..)
                                  ) where
 
-import           Control.Monad.Except              (MonadError (..))
 import           Language.PlutusCore.Lexer
 import           Language.PlutusCore.Normalize
 import           Language.PlutusCore.PrettyCfg
@@ -25,15 +23,15 @@ class IsError a where
 
     asError :: a -> Error
 
-    asExcept :: MonadError Error m => a -> m b
-    asExcept = throwError . asError
+    asLeft :: a -> Either Error b
+    asLeft = Left . asError
 
     convertError :: Either a b -> Either Error b
     convertError = first asError
 
     collectErrors :: (IsError b) => Either a (Either b c) -> Either Error c
-    collectErrors (Left x)          = asExcept x
-    collectErrors (Right (Left x))  = asExcept x
+    collectErrors (Left x)          = asLeft x
+    collectErrors (Right (Left x))  = asLeft x
     collectErrors (Right (Right x)) = Right x
 
 instance IsError Error where
