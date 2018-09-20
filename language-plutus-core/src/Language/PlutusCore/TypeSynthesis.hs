366b2428d4509ab805a9b7b581f5fb6014ccf0e6
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-19 21:48:38 -0500

    warnings

--- language-plutus-core/src/Language/PlutusCore/TypeSynthesis.hs
+++ language-plutus-core/src/Language/PlutusCore/TypeSynthesis.hs
@@ -14,7 +14,6 @@ import           Control.Monad.Except
 import           Control.Monad.Reader
 import           Control.Monad.State.Class
 import           Control.Monad.Trans.State      hiding (get, modify)
-import           Data.Functor.Foldable
 import qualified Data.IntMap                    as IM
 import qualified Data.Map                       as M
 import           Language.PlutusCore.Clone
