ecdb35fd7265074dba0c8d14bd402d0e6bc58442
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-23 10:05:37 -0500

    make cloning sensible

--- language-plutus-core/src/Language/PlutusCore/Clone.hs
+++ language-plutus-core/src/Language/PlutusCore/Clone.hs
@@ -4,13 +4,16 @@ module Language.PlutusCore.Clone ( cloneType
                                  ) where
 
 import           Control.Monad.State.Class
-import           Data.Functor.Foldable
+import           Control.Monad.Trans.State      hiding (get, modify)
 import qualified Data.IntMap               as IM
 import           Language.PlutusCore.Name
 import           Language.PlutusCore.Quote
 import           Language.PlutusCore.Type
+import PlutusPrelude
 
 type CloneSt = IM.IntMap (TyNameWithKind ())
+
+type CloneM = StateT CloneSt
 
 cloneEnvAssign :: MonadState CloneSt m
                => Unique
@@ -18,23 +21,31 @@ cloneEnvAssign :: MonadState CloneSt m
                -> m ()
 cloneEnvAssign (Unique i) ty = modify (IM.insert i ty)
 
-cloneType :: MonadQuote m => Type TyNameWithKind a -> m (Type TyNameWithKind a)
-cloneType (TyFix l (TyNameWithKind (TyName (Name l' n u))) ty) = do
-    u' <- liftQuote freshUnique
-    TyFix l (TyNameWithKind (TyName (Name l' n u'))) <$> cloneType (tyRewrite u u' ty)
-cloneType (TyForall l (TyNameWithKind (TyName (Name l' n u))) k ty) = do
-    u' <- liftQuote freshUnique
-    TyForall l (TyNameWithKind (TyName (Name l' n u'))) k <$> cloneType (tyRewrite u u' ty)
-cloneType (TyLam l (TyNameWithKind (TyName (Name l' n u))) k ty) = do
-    u' <- liftQuote freshUnique
-    TyLam l (TyNameWithKind (TyName (Name l' n u'))) k <$> cloneType (tyRewrite u u' ty)
-cloneType (TyApp l ty ty') = TyApp l <$> cloneType ty <*> cloneType ty'
-cloneType (TyFun l ty ty') = TyFun l <$> cloneType ty <*> cloneType ty'
-cloneType x@TyVar{}        = pure x
-cloneType x@TyBuiltin{}    = pure x
-cloneType x@TyInt{}        = pure x
+cloneType :: MonadQuote m => Type TyNameWithKind a -> m (Type TyNameWithKind ())
+cloneType = flip evalStateT mempty . cloneTypeM
 
-tyRewrite :: Unique -> Unique -> Type TyNameWithKind a -> Type TyNameWithKind a
-tyRewrite u u' = cata a where
-    a (TyVarF l (TyNameWithKind (TyName (Name l' n u'')))) | u == u'' = TyVar l (TyNameWithKind (TyName (Name l' n u')))
-    a x                                                   = embed x
+cloneTypeM :: (MonadQuote m) => Type TyNameWithKind a -> CloneM m (Type TyNameWithKind ())
+cloneTypeM (TyFix _ (TyNameWithKind (TyName (Name l' n u))) ty) = do
+    u' <- liftQuote freshUnique
+    let tn = void $ TyNameWithKind (TyName (Name l' n u')) -- TODO use lenses
+    cloneEnvAssign u tn
+    TyFix () tn <$> cloneTypeM ty
+cloneTypeM (TyForall _ (TyNameWithKind (TyName (Name l' n u))) k ty) = do
+    u' <- liftQuote freshUnique
+    let tn = void $ TyNameWithKind (TyName (Name l' n u'))
+    cloneEnvAssign u tn
+    TyForall () tn (void k) <$> cloneTypeM ty
+cloneTypeM (TyLam _ (TyNameWithKind (TyName (Name l' n u))) k ty) = do
+    u' <- liftQuote freshUnique
+    let tn = void $ TyNameWithKind (TyName (Name l' n u'))
+    cloneEnvAssign u tn
+    TyLam () tn (void k) <$> cloneTypeM ty
+cloneTypeM (TyApp _ ty ty') = TyApp () <$> cloneTypeM ty <*> cloneTypeM ty'
+cloneTypeM (TyFun _ ty ty') = TyFun () <$> cloneTypeM ty <*> cloneTypeM ty'
+cloneTypeM x@(TyVar _ (TyNameWithKind (TyName (Name _ _ u)))) = do
+    cloneSt <- get
+    case IM.lookup (unUnique u) cloneSt of
+        Just n -> pure (TyVar () n)
+        Nothing -> pure (void x)
+cloneTypeM x@TyBuiltin{}    = pure (void x)
+cloneTypeM x@TyInt{}        = pure (void x)
