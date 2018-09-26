8f2836c234b00b501d4b34e1c6651772fd5a831e
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-26 12:58:08 -0500

    minor fixes

--- language-plutus-core/bench/Bench.hs
+++ language-plutus-core/bench/Bench.hs
@@ -15,24 +15,20 @@ main =
                       [ bench "parse (addInteger)" $ nf parse f
                       , bench "parse (stringLiteral)" $ nf parse g
                       ]
-                , env typeFiles $ \ ~(f, g) ->
+                , env largeTypeFile $ \ g ->
                    bgroup "type-check"
-                      [ bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) f
-                      , bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) g
+                      [ bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) g
                       ]
                 , env largeTypeFile $ \ g ->
                    bgroup "normal-form check"
                       [ bench "check" $ nf (fmap check) (parse g)
                       ]
-                , env typeFiles $ \ ~(f, g) ->
+                , env largeTypeFile $ \ g ->
                   bgroup "CBOR"
-                    [ bench "writeProgram" $ nf (fmap writeProgram) (parse f)
-                    , bench "writeProgram" $ nf (fmap writeProgram) (parse g)
+                    [ bench "writeProgram" $ nf (fmap writeProgram) (parse g)
                     ]
                 ]
     where envFile = BSL.readFile "test/data/addInteger.plc"
           stringFile = BSL.readFile "test/data/stringLiteral.plc"
           files = (,) <$> envFile <*> stringFile
-          typeFile = BSL.readFile "test/types/addIntegerCorrect.plc"
           largeTypeFile = BSL.readFile "test/types/negation.plc"
-          typeFiles = (,) <$> typeFile <*> largeTypeFile
