a10a1452870cb94afff7c05dd828002e605e9de3
Vanessa McHale <vanessa.mchale@iohk.io>
2018-10-02 11:51:36 -0500

    fix duplication

--- language-plutus-core/src/Language/PlutusCore/Clone.hs
+++ language-plutus-core/src/Language/PlutusCore/Clone.hs
@@ -22,22 +22,23 @@ cloneEnvAssign (Unique i) ty = modify (IM.insert i ty)
 cloneType :: MonadQuote m => Type TyNameWithKind a -> m (Type TyNameWithKind ())
 cloneType = flip evalStateT mempty . cloneTypeM
 
-cloneTypeM :: (MonadQuote m, MonadState CloneSt m) => Type TyNameWithKind a -> m (Type TyNameWithKind ())
-cloneTypeM (TyFix _ (TyNameWithKind (TyName (Name l' n u))) ty) = do
+replaceTyName :: (MonadQuote m, MonadState CloneSt m) => TyNameWithKind () -> m (TyNameWithKind ())
+replaceTyName (TyNameWithKind (TyName (Name l' n u))) = do
     u' <- liftQuote freshUnique
     let tn = void $ TyNameWithKind (TyName (Name l' n u')) -- TODO use lenses
     cloneEnvAssign u tn
-    TyFix () tn <$> cloneTypeM ty
-cloneTypeM (TyForall _ (TyNameWithKind (TyName (Name l' n u))) k ty) = do
-    u' <- liftQuote freshUnique
-    let tn = void $ TyNameWithKind (TyName (Name l' n u'))
-    cloneEnvAssign u tn
-    TyForall () tn (void k) <$> cloneTypeM ty
-cloneTypeM (TyLam _ (TyNameWithKind (TyName (Name l' n u))) k ty) = do
-    u' <- liftQuote freshUnique
-    let tn = void $ TyNameWithKind (TyName (Name l' n u'))
-    cloneEnvAssign u tn
-    TyLam () tn (void k) <$> cloneTypeM ty
+    pure tn
+
+cloneTypeM :: (MonadQuote m, MonadState CloneSt m) => Type TyNameWithKind a -> m (Type TyNameWithKind ())
+cloneTypeM (TyFix _ tn ty) = do
+    tn' <- replaceTyName (void tn)
+    TyFix () tn' <$> cloneTypeM ty
+cloneTypeM (TyForall _ tn k ty) = do
+    tn' <- replaceTyName (void tn)
+    TyForall () tn' (void k) <$> cloneTypeM ty
+cloneTypeM (TyLam _ tn k ty) = do
+    tn' <- replaceTyName (void tn)
+    TyLam () tn' (void k) <$> cloneTypeM ty
 cloneTypeM (TyApp _ ty ty') = TyApp () <$> cloneTypeM ty <*> cloneTypeM ty'
 cloneTypeM (TyFun _ ty ty') = TyFun () <$> cloneTypeM ty <*> cloneTypeM ty'
 cloneTypeM x@(TyVar _ (TyNameWithKind (TyName (Name _ _ u)))) = do
