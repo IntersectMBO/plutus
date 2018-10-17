06a1c27c37f76b66abc32487b89bc1b94d14a4df
David Smith <shmish111@gmail.com>
2018-10-17 17:24:59 +0100

    HTTP MockChain Part 2
    This commit adds new endpoints to trigger validation
    and notifications.
    
    The next stage is to create a client and try to fit
    it into the Trace api. The only issue I see is assertions.
    Since these are Haskell functions, we cannot serialize
    them. It may be that we can remove them from the Trace API
    and run them separately. We may need more endpoints for
    this in order to see more state within the emulator.

--- wallet-api/emulator/Main.hs
+++ wallet-api/emulator/Main.hs
@@ -1,13 +1,10 @@
 module Main where
 
-import           Control.Concurrent.STM   (atomically, newTVar)
-import qualified Data.HashMap.Strict      as HM
 import           Network.Wai.Handler.Warp (run)
-import           Wallet.Emulator.Http     (State (State), app)
+import           Wallet.Emulator.Http     (app, initialState)
 
 main :: IO ()
 main = do
   putStrLn "start emulator"
-  initialState <- atomically $ newTVar HM.empty
-  let state = State initialState
+  state <- initialState
   run 8080 $ app state
