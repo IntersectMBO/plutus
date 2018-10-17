374feba76b6bede58828df5c918af2c3e9ffb13d
Vanessa McHale <vanessa.mchale@iohk.io>
2018-10-17 10:57:01 -0500

    add haddocks

--- language-plutus-core/src/Language/PlutusCore/MkPlc.hs
+++ language-plutus-core/src/Language/PlutusCore/MkPlc.hs
@@ -25,16 +25,16 @@ mkTypeLet name bind ty body = TyInst () (TyAbs () name ty body) bind
 
 -- | Make an iterated application.
 mkIterApp
-    :: Term tyname name ()
-    -> [Term tyname name ()]
-    -> Term tyname name ()
+    :: Term tyname name () -- ^ @f@
+    -> [Term tyname name ()] -- ^@[ x0 ... xn ]@
+    -> Term tyname name () -- ^ @[f x0 ... xn ]@
 mkIterApp = foldl' (Apply ())
 
 -- | Make an iterated instantiation.
 mkIterInst
-    :: Term tyname name ()
-    -> [Type tyname ()]
-    -> Term tyname name ()
+    :: Term tyname name () -- ^ @a@
+    -> [Type tyname ()] -- ^ @ [ x0 ... xn ] @
+    -> Term tyname name () -- ^ @{ a x0 ... xn }@
 mkIterInst = foldl' (TyInst ())
 
 -- | Lambda abstract a list of names.
@@ -53,9 +53,9 @@ mkIterTyAbs args body = foldr (\(n, ty) acc -> TyAbs () n ty acc) body args
 
 -- | Make an iterated type application.
 mkIterTyApp
-    :: Type tyname ()
-    -> [Type tyname ()]
-    -> Type tyname ()
+    :: Type tyname () -- ^ @f@
+    -> [Type tyname ()] -- ^ @[ x0 ... xn ]@
+    -> Type tyname () -- ^ @[ f x0 ... xn ]@
 mkIterTyApp = foldl' (TyApp ())
 
 -- | Make an iterated function type.
