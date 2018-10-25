b052638594ec99cc7e8d39c59a6facf1d01aad51
David Smith <shmish111@gmail.com>
2018-10-25 11:29:06 +0100

    PR suggestions

--- wallet-api/src/Wallet/Emulator/Client.hs
+++ wallet-api/src/Wallet/Emulator/Client.hs
@@ -1,10 +1,25 @@
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
 
-module Wallet.Emulator.Client where
+module Wallet.Emulator.Client
+  ( Environment(Environment)
+  , WalletClient
+  , runWalletClient
+  , wallets
+  , fetchWallet
+  , createWallet
+  , getTransactions
+  , blockValidated
+  , blockHeight
+  , blockchainActions
+  , setValidationData
+  , assertOwnFundsEq
+  , assertIsValidated
+  ) where
 
-import           Control.Monad.Reader  (ReaderT, asks, lift, runReaderT)
+import           Control.Monad.Reader  (MonadReader, ReaderT, asks, lift, runReaderT)
 import           Data.Proxy            (Proxy (Proxy))
 import           Data.Set              (Set)
 import           Servant.API           ((:<|>) ((:<|>)), NoContent)
@@ -17,41 +32,44 @@ import           Wallet.UTXO           (Block, Height, Tx, TxIn', TxOut', Valida
 api :: Proxy API
 api = Proxy
 
-wallets' :: ClientM [Wallet]
-fetchWallet' :: Wallet -> ClientM Wallet
-createWallet' :: Wallet -> ClientM NoContent
+wallets :: ClientM [Wallet]
+fetchWallet :: Wallet -> ClientM Wallet
+createWallet :: Wallet -> ClientM NoContent
+myKeyPair' :: Wallet -> ClientM KeyPair
 createPaymentWithChange' :: Wallet -> Value -> ClientM (Set TxIn', TxOut')
 payToPublicKey' :: Wallet -> Value -> ClientM TxOut'
 submitTxn' :: Wallet -> Tx -> ClientM ()
-getTransactions' :: ClientM [Tx]
-blockchainActions' :: ClientM [Tx]
-setValidationData' :: ValidationData -> ClientM ()
-blockValidated' :: Wallet -> Block -> ClientM ()
-blockHeight' :: Wallet -> Height -> ClientM ()
-assertOwnFundsEq' :: Wallet -> Value -> ClientM NoContent
-assertIsValidated' :: Tx -> ClientM NoContent
-(wallets' :<|> fetchWallet' :<|> createWallet' :<|> createPaymentWithChange' :<|> payToPublicKey' :<|> submitTxn' :<|> getTransactions') :<|> (blockValidated' :<|> blockHeight') :<|> (blockchainActions' :<|> setValidationData') :<|> (assertOwnFundsEq' :<|> assertIsValidated') =
+getTransactions :: ClientM [Tx]
+blockchainActions :: ClientM [Tx]
+setValidationData :: ValidationData -> ClientM ()
+blockValidated :: Wallet -> Block -> ClientM ()
+blockHeight :: Wallet -> Height -> ClientM ()
+assertOwnFundsEq :: Wallet -> Value -> ClientM NoContent
+assertIsValidated :: Tx -> ClientM NoContent
+(wallets :<|> fetchWallet :<|> createWallet :<|> myKeyPair' :<|> createPaymentWithChange' :<|> payToPublicKey' :<|> submitTxn' :<|> getTransactions) :<|> (blockValidated :<|> blockHeight) :<|> (blockchainActions :<|> setValidationData) :<|> (assertOwnFundsEq :<|> assertIsValidated) =
   client api
 
 data Environment = Environment
   { getWallet    :: Wallet
-  , getKeyPair   :: KeyPair
   , getClientEnv :: ClientEnv
   }
 
-type WalletClient = ReaderT Environment ClientM
+newtype WalletClient a = WalletClient
+  { unWalletClient :: ReaderT Environment ClientM a
+  } deriving (Functor, Applicative, Monad, MonadReader Environment)
 
 runWalletClient :: Environment -> WalletClient a -> IO (Either ServantError a)
-runWalletClient env act = runClientM (runReaderT act env) (getClientEnv env)
+runWalletClient env act =
+  runClientM (runReaderT (unWalletClient act) env) (getClientEnv env)
 
 liftWallet :: (Wallet -> ClientM a) -> WalletClient a
 liftWallet action = do
   wallet <- asks getWallet
-  lift $ action wallet
+  WalletClient $ lift $ action wallet
 
 instance WalletAPI WalletClient where
   submitTxn tx = liftWallet (`submitTxn'` tx)
-  myKeyPair = asks getKeyPair
+  myKeyPair = liftWallet myKeyPair'
   createPaymentWithChange value = liftWallet (`createPaymentWithChange'` value)
   register _ _ = pure () -- TODO: Keep track of triggers in emulated wallet
   payToPublicKey value = liftWallet (`payToPublicKey'` value)
