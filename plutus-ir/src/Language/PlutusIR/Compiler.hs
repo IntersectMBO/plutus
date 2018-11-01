fb2d487244f8bdcd4df11f9710ad54211c79c3c6
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2018-11-01 17:42:35 +0000

    plutus-ir: add a note about uniqueness of names

--- plutus-ir/src/Language/PlutusIR/Compiler.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler.hs
@@ -1,11 +1,9 @@
 module Language.PlutusIR.Compiler (
     compileTerm,
-    compileDatatype,
     Compiling,
     CompError (..),
     runCompiling) where
 
-import           Language.PlutusIR.Compiler.Datatype
 import           Language.PlutusIR.Compiler.Error
 import           Language.PlutusIR.Compiler.Term
 import           Language.PlutusIR.Compiler.Types
