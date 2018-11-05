582d26e0d5d86fd00ac8c10f009ebda3cbae3cb9
kwxm <kenneth.mackenzie@iohk.io>
2018-11-05 11:14:01 +0000

    Added improved size bound check for benchmarking comparisons

--- language-plutus-core/src/Language/PlutusCore/Constant/Make.hs
+++ language-plutus-core/src/Language/PlutusCore/Constant/Make.hs
@@ -30,7 +30,6 @@ module Language.PlutusCore.Constant.Make
 import           Language.PlutusCore.Constant.Dynamic.Pretty
 import           Language.PlutusCore.Constant.Function
 import           Language.PlutusCore.Constant.Typed
-import           Language.PlutusCore.MkPlc
 import           Language.PlutusCore.Name
 import           Language.PlutusCore.Quote
 import           Language.PlutusCore.StdLib.Data.Bool
@@ -122,44 +121,6 @@ makeDynBuiltinIntSizedAs
     -> Term tyname name ()
 makeDynBuiltinIntSizedAs sTy intAs = makeDynBuiltinInt sTy sTerm where
     sTerm = Apply () (TyInst () (builtinNameAsTerm SizeOfInteger) sTy) intAs
-
--- | Compute the size of an 'Integer'. See also 'toBoundsInt'.
-sizeOfInteger :: Integer -> Size
-sizeOfInteger i = fromIntegral $ ilogRound 2 (abs i + d) `div` 8 + 1 where
-    d = if i < 0 then 0 else 1
-
--- | Make a 'Constant' out of an 'Integer'. The size is computed using 'sizeOfInteger'.
-makeAutoSizedBuiltinInt :: Integer -> Constant ()
-makeAutoSizedBuiltinInt i = BuiltinInt () (sizeOfInteger i) i
-
-{- Note [Dynamic sized built-ins]
-How do we increment an integer in PLC? We can't simply write @addInteger {s} i 1@, because @1@
-is not even legal syntax. The legal syntax is @1!1@ where the right @1@ is an integer and the left
-@1@ is its size. In order for two integers to be addable, they have to be of the same type, so
-we can't simply write @addInteger {s} i 1!1@, because @1!1@ is not of type @integer s@
-(unless @s@ is literally @1@). Hence we need to resize @1!1@. The final solution is
-
-> addInteger {s} i (resizeInteger {1} {s} ss 1!1)
-
-Constructing such terms by hand is tedious and error-prone, therefore we define the
-'makeDynamicBuiltinInt' function, which computes the size of an @Integer@, constructs the
-corresponding built-in @integer@ and applies appropriately instantiated 'resizeInteger'
-to the result.
-
-Same considerations apply to bytestrings.
--}
-
--- | Convert a Haskell 'Integer' to the corresponding PLC @integer@.
-makeDynamicBuiltinInt
-    :: Type tyname ()       -- ^ An actual size @s@.
-    -> Term tyname name ()  -- ^ A singleton for @s@.
-    -> Integer              -- ^ An 'Integer' to lift.
-    -> Term tyname name ()
-makeDynamicBuiltinInt sTy sTerm intVal =
-    mkIterApp (mkIterInst resizeInteger [TyInt () sizeOfIntVal, sTy]) [sTerm, intTerm] where
-        sizeOfIntVal = sizeOfInteger intVal
-        resizeInteger = Constant () $ BuiltinName () ResizeInteger
-        intTerm = Constant () $ BuiltinInt () sizeOfIntVal intVal
 
 -- | Check whether an 'Integer' is in bounds (see 'checkBoundsInt') and return it as a 'Constant'.
 makeBuiltinInt :: Size -> Integer -> Maybe (Constant ())
