c447854b67dfbda4ca308e1fad793239b857e13f
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2018-11-05 12:44:39 +0000

    pir: add an isTermValue function

---
+++ plutus-ir/src/Language/PlutusIR/Value.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
+module Language.PlutusIR.Value (isTermValue) where
+
+import           Language.PlutusIR
+
+-- | Whether the given PIR term is (will compile to) a PLC term value.
+isTermValue :: Term tyname name a -> Bool
+isTermValue = \case
+    -- Let is not a value (will compile into applications and/or type instantiations)
+    Let {} -> False
+    -- Lambdas and constants are always values
+    LamAbs {} -> True
+    Constant {} -> True
+    -- Type abstractions and wraps are values if their bodies are
+    TyAbs _ _ _ t -> isTermValue t
+    Wrap _ _ _ t -> isTermValue t
+    -- All other PLC terms are not values
+    Var {} -> False
+    Apply {} -> False
+    TyInst {} -> False
+    Error {} -> False
+    Unwrap {} -> False
