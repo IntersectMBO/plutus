879d02a18b76d8a8a2bc31309aac6a2af676bce4
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2018-11-23 10:43:05 +0000

    plutus-tx: be more careful about constructing the expression so that the plugin recognizes it even without simplifications
    Otherwise it doesn't get found in doctests.

--- plutus-tx/src/Language/PlutusTx/TH.hs
+++ plutus-tx/src/Language/PlutusTx/TH.hs
@@ -17,25 +17,26 @@ import qualified Language.Haskell.TH.Syntax as TH
 
 -- | Covert a quoted Haskell expression into a corresponding Plutus Core program.
 plutus :: TH.Q (TH.TExp a) -> TH.Q (TH.TExp PlcCode)
-plutus e = do
-    TH.addCorePlugin "Language.PlutusTx.Plugin"
-    loc <- TH.location
-    let locStr = TH.pprint loc
-    -- See note [Typed TH]
-    let
-        partial :: TH.Q (TH.TExp (a -> PlcCode))
-        partial = TH.unsafeTExpCoerce [| \a -> plc @($(TH.litT $ TH.strTyLit locStr)) a |]
-    [|| $$(partial) $$(e) ||]
+-- See note [Typed TH]
+plutus e = TH.unsafeTExpCoerce $ plutusUntyped $ TH.unType <$> e
 
 {- Note [Typed TH]
-It's nice to use typed TH! However, we sadly can't *quite* use it thoroughly, because we want to make a type literal,
-and there's no way to do that properly with typed TH.
+It's nice to use typed TH! However, we sadly can't *quite* use it thoroughly, because we
+want to make a type literal, and there's no way to do that properly with typed TH.
 
-However, we can isolate the badness into as small a box as possible, and then use unsafeTExpCoerce.
+Moreover, we really want to create an expression with the precise form that we want,
+so we can't isolate the badness much. So we pretty much just have to use unsafeTExpCoerce
+and assert that we know what we're doing.
+
+This isn't so bad, since our plc function accepts an argument of any type, so that's always
+going to typecheck, and the result is always a PlcCode, so that's also fine.
 -}
 
 -- | Covert a quoted Haskell expression into a corresponding Plutus Core program.
 plutusUntyped :: TH.Q TH.Exp -> TH.Q TH.Exp
--- Just force our typed version to accept it and then turn it back into an untyped one. This means it will
--- get typechecked again later, which is good because we lied about knowing the type of the argument.
-plutusUntyped e = TH.unType <$> plutus (TH.unsafeTExpCoerce e)
+plutusUntyped e = do
+    TH.addCorePlugin "Language.PlutusTx.Plugin"
+    loc <- TH.location
+    let locStr = TH.pprint loc
+    -- See note [Typed TH]
+    [| plc @($(TH.litT $ TH.strTyLit locStr)) $(e) |]
