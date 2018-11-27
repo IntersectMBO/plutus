7db44665988a94a33703ebca7321d86a9a96c3ee
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2018-11-27 15:06:34 +0000

    Simplifiy

--- plutus-tx/src/Language/PlutusTx/Evaluation.hs
+++ plutus-tx/src/Language/PlutusTx/Evaluation.hs
@@ -13,13 +13,13 @@ stringBuiltins :: DynamicBuiltinNameMeanings
 stringBuiltins =
     insertDynamicBuiltinNameDefinition dynamicCharToStringDefinition $ insertDynamicBuiltinNameDefinition dynamicAppendDefinition mempty
 
--- TODO: pretty sure we shouldn't need the unsafePerformIOs here, we should expose a pure interface even if it has IO hacks under the hood
-
 -- | Evaluate a program in the CEK machine with the usual string dynamic builtins.
 evaluateCek
     :: Program TyName Name ()
     -> EvaluationResult
-evaluateCek p = unsafePerformIO $ evaluate $ runCek stringBuiltins p
+evaluateCek = runCek stringBuiltins
+
+-- TODO: pretty sure we shouldn't need the unsafePerformIOs here, we should expose a pure interface even if it has IO hacks under the hood
 
 -- | Evaluate a program in the CEK machine with the usual string dynamic builtins and tracing, additionally
 -- returning the trace output.
