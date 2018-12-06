970ffdf7c916c521923f78e970221f105e4d921a
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2018-12-06 10:11:42 +0000

    Strip suffixes from the Lift class
    It's better not to try and indicate what we're lifting "to" in the
    names, as TH does.

--- plutus-tx-plugin/src/Language/PlutusTx/Lift/Instances.hs
+++ plutus-tx-plugin/src/Language/PlutusTx/Lift/Instances.hs
@@ -7,49 +7,49 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Language.PlutusTx.Lift.Instances () where
 
-import qualified Language.PlutusCore            as PLC
+import qualified Language.PlutusCore          as PLC
 
-import           Language.PlutusTx.Lift.LiftPir
+import           Language.PlutusTx.Lift.Class
 import           Language.PlutusTx.Utils
 
 import           Language.PlutusIR
 
-import qualified Data.ByteString.Lazy           as BSL
+import qualified Data.ByteString.Lazy         as BSL
 import           Data.Proxy
 
 -- Derived instances
 
 -- This instance ensures that we can apply typeable type constructors to typeable arguments and get a typeable
 -- type. We need the kind variable, so that partial application of type constructors works.
-instance (TypeablePir (f :: * -> k), TypeablePir (a :: *)) => TypeablePir (f a) where
+instance (Typeable (f :: * -> k), Typeable (a :: *)) => Typeable (f a) where
     typeRep _ = TyApp () <$> typeRep (undefined :: Proxy f) <*> typeRep (undefined :: Proxy a)
 
-instance (TypeablePir (a :: *), TypeablePir (b :: *)) => TypeablePir (a -> b) where
+instance (Typeable (a :: *), Typeable (b :: *)) => Typeable (a -> b) where
     typeRep _ = TyFun () <$> typeRep (undefined :: Proxy a) <*> typeRep (undefined :: Proxy b)
 
 -- Primitives
 
-instance TypeablePir Int where
+instance Typeable Int where
     typeRep _ = pure $ appSize haskellIntSize (TyBuiltin () PLC.TyInteger)
 
-instance LiftPir Int where
+instance Lift Int where
     lift i = pure $ Constant () $ PLC.BuiltinInt () haskellIntSize $ fromIntegral i
 
-instance TypeablePir BSL.ByteString where
+instance Typeable BSL.ByteString where
     typeRep _ = pure $ appSize haskellBSSize (TyBuiltin () PLC.TyByteString)
 
-instance LiftPir BSL.ByteString where
+instance Lift BSL.ByteString where
     lift bs = pure $ Constant () $ PLC.BuiltinBS () haskellBSSize bs
 
 -- Standard types
 -- These need to be in a separate file for TH staging reasons
 
-makeLiftPir ''Bool
-makeLiftPir ''Maybe
-makeLiftPir ''Either
-makeLiftPir ''[]
+makeLift ''Bool
+makeLift ''Maybe
+makeLift ''Either
+makeLift ''[]
 -- include a few tuple instances for convenience
-makeLiftPir ''(,)
-makeLiftPir ''(,,)
-makeLiftPir ''(,,,)
-makeLiftPir ''(,,,,)
+makeLift ''(,)
+makeLift ''(,,)
+makeLift ''(,,,)
+makeLift ''(,,,,)
