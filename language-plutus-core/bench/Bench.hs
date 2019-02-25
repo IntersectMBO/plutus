4e76318202257fa1fdee02cb7444c1d95b95367e
Vanessa McHale <vanessa.mchale@iohk.io>
2019-02-25 12:24:03 -0600

    Somewhat more sensible approach to benchmarks; more laconic

--- language-plutus-core/bench/Bench.hs
+++ language-plutus-core/bench/Bench.hs
@@ -8,6 +8,15 @@ import qualified Data.Text                  as T
 import           Language.PlutusCore
 import           Language.PlutusCore.Pretty
 
+renameConcrete :: Program TyName Name AlexPosn -> Program TyName Name AlexPosn
+renameConcrete = runQuote . rename
+
+typeCheckConcrete :: Program TyName Name AlexPosn -> Either (Error AlexPosn) (Normalized (Type TyName ()))
+typeCheckConcrete = runQuoteT . inferTypeOfProgram defOffChainConfig
+
+parsed :: BSL.ByteString -> Either (Error AlexPosn) (Program TyName Name AlexPosn)
+parsed = runQuoteT . parseScoped
+
 main :: IO ()
 main =
     defaultMain [ env envFile $ \ f ->
@@ -25,15 +34,15 @@ main =
 
                 , env files $ \ ~(f, g) ->
                     bgroup "parse"
-                      [ bench "parse (addInteger)" $ nf parse f
-                      , bench "parse (stringLiteral)" $ nf parse g
+                      [ bench "addInteger" $ nf parse f
+                      , bench "stringLiteral" $ nf parse g
                       ]
 
                 , env largeTypeFiles $ \ ~(f, g, h) ->
                    bgroup "type-check"
-                      [ bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) f
-                      , bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) g
-                      , bench "printType" $ nf (printType :: BSL.ByteString -> Either (Error AlexPosn) T.Text) h
+                      [ bench "typeCheck" $ nf (typeCheckConcrete =<<) (parsed f)
+                      , bench "typeCheck" $ nf (typeCheckConcrete =<<) (parsed g)
+                      , bench "typeCheck" $ nf (typeCheckConcrete =<<) (parsed h)
                       ]
 
                 , env largeTypeFiles $ \ ~(f, g, h) ->
@@ -51,11 +60,20 @@ main =
                       ]
 
                 , env largeTypeFiles $ \ ~(f, g, h) ->
-                    bgroup "CBOR"
-                      [ bench "writeProgram" $ nf (fmap (serialise . void)) $ parse f
-                      , bench "writeProgram" $ nf (fmap (serialise . void)) $ parse g
-                      , bench "writeProgram" $ nf (fmap (serialise . void)) $ parse h
-                      ]
+                    let mkBench src = bench "serialise" $ nf (fmap (serialise . void)) $ parse src
+                    in
+
+                    bgroup "CBOR" $ mkBench <$> [f, g, h]
+
+                , env largeTypeFiles $ \ ~(f, g, h) ->
+                    let deserialiseProgram :: BSL.ByteString -> Program TyName Name ()
+                        deserialiseProgram = deserialise
+                        parseAndSerialise :: BSL.ByteString -> Either (ParseError AlexPosn) BSL.ByteString
+                        parseAndSerialise = fmap (serialise . void) . parse
+                        mkBench src = bench "deserialise" $ nf (fmap deserialiseProgram) $ parseAndSerialise src
+                    in
+
+                    bgroup "CBOR" $ mkBench <$> [f, g, h]
 
                 ]
 
@@ -70,6 +88,3 @@ main =
           typeCompare0 = BSL.readFile "test/types/example.plc"
           typeCompare1 = BSL.readFile "bench/example-compare.plc"
           typeCompare = (,) <$> typeCompare0 <*> typeCompare1
-
-renameConcrete :: Program TyName Name AlexPosn -> Program TyName Name AlexPosn
-renameConcrete = runQuote . rename
