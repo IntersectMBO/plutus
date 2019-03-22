002999008bdfa679b45d1a49c0d7459ce3eb0df6
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2019-03-22 10:15:11 +0000

    Turn off some things that seem to be broken on darwin

--- release.nix
+++ release.nix
@@ -3,8 +3,10 @@ let
   fixedNixpkgs = localLib.nixpkgs;
   nixpkgs = import fixedNixpkgs { };
   lib = nixpkgs.lib;
+  linux = ["x86_64-linux"];
+  darwin = ["x86_64-darwin"];
 in
-  { supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+  { supportedSystems ? linux ++ darwin
   , scrubJobs ? true
   , fasterBuild ? false
   , skipPackages ? []
@@ -24,7 +26,7 @@ let
 
   # This is a mapping from attribute paths to systems. So it needs to mirror the structure of the
   # attributes in default.nix exactly
-  systemMapping = supportedSystems: {
+  systemMapping = {
     localPackages = 
       # Due to the magical split test machinery, packages that have a 'testdata' attribute should
       # have their tests run via the 'testrun' derivation. I don't know how to *also* have a mapping
@@ -32,17 +34,22 @@ let
       # the tests will depend on the main package so that's okay.
       lib.mapAttrs (n: p: if p ? testdata then { testrun = supportedSystems; } else supportedSystems)
          packageSet.localPackages;
-    plutus-playground = lib.mapAttrs (_: _: supportedSystems) 
+    # At least the client is broken on darwin for some yarn reason
+    plutus-playground = lib.mapAttrs (_: _: linux) 
+        # don't build the docker image on hydra
         (lib.filterAttrs (n: v: n != "docker") packageSet.plutus-playground);  
-    meadow = lib.mapAttrs (_: _: supportedSystems) 
+    # At least the client is broken on darwin for some yarn reason
+    meadow = lib.mapAttrs (_: _: linux) 
+        # don't build the docker image on hydra
         (lib.filterAttrs (n: v: n != "docker") packageSet.meadow);  
-    docs = lib.mapAttrs (_: _: supportedSystems) packageSet.docs;  
+    # texlive is broken on darwin at our nixpkgs pin
+    docs = lib.mapAttrs (_: _: linux) packageSet.docs;  
     tests = lib.mapAttrs (_: _: supportedSystems) packageSet.tests;  
     dev.packages = lib.mapAttrs (_: _: supportedSystems) packageSet.dev.packages;  
     dev.scripts = lib.mapAttrs (_: _: supportedSystems) packageSet.dev.scripts;  
   }; 
   
-  testJobsets = mapTestOn (systemMapping supportedSystems);
+  testJobsets = mapTestOn systemMapping;
 
   # Recursively collect all jobs (derivations) in a jobset 
   allJobs = jobset: lib.collect lib.isDerivation jobset;
