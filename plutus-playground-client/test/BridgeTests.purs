6e22a4e397786baccfcd5b533bd7e6f4325956ad
Kris Jenkins <krisajenkins@gmail.com>
2019-03-25 17:04:43 +0000

    Playground: Extracting some testing helpers.

--- plutus-playground-client/test/BridgeTests.purs
+++ plutus-playground-client/test/BridgeTests.purs
@@ -5,21 +5,17 @@ module BridgeTests
 import Prelude
 
 import AjaxUtils (ourDecodeJson)
-import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (RANDOM)
 import Data.Argonaut.Core as Argonaut
-import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either(..))
-import Data.Generic (class Generic, gShow)
 import Data.List (List)
 import Data.List.Types (NonEmptyList)
 import Language.Haskell.Interpreter (CompilationError)
-import Node.Encoding (Encoding(UTF8))
 import Node.FS (FS)
-import Node.FS.Sync as FS
 import Playground.API (CompilationResult, EvaluationResult, KnownCurrency, TokenId)
-import Test.Unit (TestSuite, Test, failure, success, suite, test)
+import Test.Unit (TestSuite, suite, test)
+import TestUtils (assertDecodesTo, equalGShow)
 import Type.Proxy (Proxy(..))
 
 all :: forall eff. TestSuite (exception :: EXCEPTION, fs :: FS, random :: RANDOM | eff)
@@ -58,27 +54,3 @@ jsonHandling = do
         assertDecodesTo
           (Proxy :: Proxy (Array CompilationError))
           "test/evaluation_error1.json"
-
-equalGShow :: forall eff a. Eq a => Generic a => a -> a -> Test eff
-equalGShow expected actual =
-  if expected == actual then success
-  else failure $ "expected " <> gShow expected <>
-       ", got " <> gShow actual
-
-assertRight :: forall e a. Either String a -> Test e
-assertRight (Left err) = failure err
-assertRight (Right _) = success
-
-assertDecodesTo :: forall eff a. Generic a => Proxy a -> String -> Test (fs :: FS, exception :: EXCEPTION | eff)
-assertDecodesTo proxy filename = do
-  result :: Either String a <- decodeFile filename
-  assertRight result
-
-decodeFile :: forall m a eff.
-  MonadEff (fs :: FS, exception :: EXCEPTION | eff) m
-  => Generic a
-  => String
-  -> m (Either String a)
-decodeFile filename = do
-  contents <- liftEff $ FS.readTextFile UTF8 filename
-  pure (jsonParser contents >>= ourDecodeJson)
--- plutus-playground-client/test/GistsSpec.purs
+++ plutus-playground-client/test/GistsSpec.purs
@@ -7,11 +7,11 @@ import Prelude
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (RANDOM)
 import Data.Either (Either(..))
-import Data.Generic (gShow)
 import Gist (GistId(..))
 import Gists (parseGistUrl)
 import Node.FS (FS)
-import Test.Unit (TestSuite, Test, failure, success, suite, test)
+import Test.Unit (TestSuite, suite, test)
+import TestUtils (equalGShow)
 
 all :: forall eff. TestSuite (exception :: EXCEPTION, fs :: FS, random :: RANDOM | eff)
 all =
@@ -22,15 +22,8 @@ parseGistUrlTests :: forall eff. TestSuite eff
 parseGistUrlTests =
   suite "parseGistUrlTests" do
     let gistId = GistId "9d8feacacd8c4b553f870c4448483938"
-    test "Ref" $ equalWithFormatter gShow (Right gistId) (parseGistUrl "9d8feacacd8c4b553f870c4448483938")
-    test "Direct link" $ equalWithFormatter gShow (Right gistId) (parseGistUrl "https://gist.github.com/9d8feacacd8c4b553f870c4448483938")
-    test "User link" $ equalWithFormatter gShow (Right gistId) (parseGistUrl "https://gist.github.com/krisajenkins/9d8feacacd8c4b553f870c4448483938")
-    test "No ID" $ equalWithFormatter gShow (Left "Could not parse Gist Url") (parseGistUrl "https://gist.github.com/")
-    test "Too long" $ equalWithFormatter gShow (Left  "Could not parse Gist Url") (parseGistUrl "aaaabbbbccccddddeeeeffff000011112")
-
-equalWithFormatter :: forall a e. Eq a => (a -> String) -> a -> a -> Test e
-equalWithFormatter f expected actual =
-  if expected == actual then success
-
-  else failure $ "expected " <> f expected <>
-       ", got " <> f actual
+    test "Ref" $ equalGShow (Right gistId) (parseGistUrl "9d8feacacd8c4b553f870c4448483938")
+    test "Direct link" $ equalGShow (Right gistId) (parseGistUrl "https://gist.github.com/9d8feacacd8c4b553f870c4448483938")
+    test "User link" $ equalGShow (Right gistId) (parseGistUrl "https://gist.github.com/krisajenkins/9d8feacacd8c4b553f870c4448483938")
+    test "No ID" $ equalGShow (Left "Could not parse Gist Url") (parseGistUrl "https://gist.github.com/")
+    test "Too long" $ equalGShow (Left  "Could not parse Gist Url") (parseGistUrl "aaaabbbbccccddddeeeeffff000011112")
