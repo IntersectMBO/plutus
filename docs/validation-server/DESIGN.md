179ddb352689c6210049059d7453e3591db3b493
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-02 12:43:12 -0500

    validator script &c.

--- docs/validation-server/DESIGN.md
+++ docs/validation-server/DESIGN.md
@@ -7,9 +7,21 @@ The Plutus Core validation server will behave as follows:
   - The server should accept `GET` requests that contain the JSON data for
     validation in the request body
 
+    - As an example:
+      ```json
+      {
+          "validator": "ab34ce2d",
+          "redeemer": "5cb418",
+          "data": "958bce"
+      }
+
     - For other methods (e.g. `POST`), it will error (response status 400)
 
   - The server will ignore HTTP headers
 
   - The server will return response status 200 with no headers. The response
     body will contain JSON: either `True` or `False`
+
+    ```json
+    { "isValid": true }
+    ```
--- plutus-validation-server/src/Main.hs
+++ plutus-validation-server/src/Main.hs
@@ -1,4 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import qualified Data.ByteString.Lazy      as BSL
+import           Network.HTTP.Types.Status
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+
 main :: IO ()
-main = print "Hello"
+main = run 3000 app
+
+trueJSON :: BSL.ByteString
+trueJSON = "{\"isValid\":true}"
+
+-- typecheck, run/validate
+
+app :: Application
+app req respond =
+    let bsReq = strictRequestBody
+    in
+    -- TODO: what to do when error is returned?
+    respond $ responseLBS status200 [] trueJSON -- TODO: return actual answer we want instead of always saying it's valid
