018d825344e46ee4b3bdfe465c6d8a143f9d2288
David Smith <shmish111@gmail.com>
2019-04-05 15:38:53 +0100

    add meadow to ssh config

--- deployment/terraform/ssh_config.tf
+++ deployment/terraform/ssh_config.tf
@@ -34,13 +34,38 @@ data "template_file" "ssh_config_section_playground_b" {
   }
 }
 
-data "template_file" "ssh_config" {
-  template = "\n$${nixops_node}\n$${playground_a}\n$${playground_b}"
+data "template_file" "ssh_config_section_meadow_a" {
+  template = "${file("${path.module}/templates/ssh-config")}"
 
   vars {
-    nixops_node     = "${data.template_file.ssh_config_section_nixops.rendered}"
+    full_hostname    = "meadow-a.${aws_route53_zone.plutus_private_zone.name}"
+    short_hostname   = "meadow-a.${var.project}"
+    ip               = "${aws_instance.meadow_a.private_ip}"
+    bastion_hostname = "${aws_instance.bastion.*.public_ip[0]}"
+    user_name        = "plutus"
+  }
+}
+
+data "template_file" "ssh_config_section_meadow_b" {
+  template = "${file("${path.module}/templates/ssh-config")}"
+
+  vars {
+    full_hostname    = "meadow-b.${aws_route53_zone.plutus_private_zone.name}"
+    short_hostname   = "meadow-b.${var.project}"
+    ip               = "${aws_instance.meadow_b.private_ip}"
+    bastion_hostname = "${aws_instance.bastion.*.public_ip[0]}"
+    user_name        = "plutus"
+  }
+}
+data "template_file" "ssh_config" {
+  template = "\n$${nixops_node}\n$${playground_a}\n$${playground_b}\n$${meadow_a}\n$${meadow_b}"
+
+  vars {
+    nixops_node      = "${data.template_file.ssh_config_section_nixops.rendered}"
     playground_a     = "${data.template_file.ssh_config_section_playground_a.rendered}"
     playground_b     = "${data.template_file.ssh_config_section_playground_b.rendered}"
+    meadow_a         = "${data.template_file.ssh_config_section_meadow_a.rendered}"
+    meadow_b         = "${data.template_file.ssh_config_section_meadow_b.rendered}"
   }
 }
 
