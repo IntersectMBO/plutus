9c5d8effa0dcb7c6b830904e2ecf3f426dbd006e
Kris Jenkins <krisajenkins@gmail.com>
2019-05-01 11:35:58 +0100

    Playground: Generalising and extracting a couple of QuickCheck generators.

--- plutus-playground-client/test/TestUtils.purs
+++ plutus-playground-client/test/TestUtils.purs
@@ -1,15 +1,18 @@
 module TestUtils where
 
+import Prelude
+
 import AjaxUtils as AjaxUtils
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Gen (class MonadGen, chooseInt)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either(..))
+import Data.Foldable (class Foldable, length)
 import Data.Generic (class Generic, gShow)
 import Node.Encoding (Encoding(UTF8))
 import Node.FS (FS)
 import Node.FS.Sync as FS
-import Prelude (class Eq, bind, pure, ($), (<$>), (<>), (==), (>>=))
 import Test.Unit (Test, failure, success)
 import Test.Unit.Assert (equal)
 import Type.Proxy (Proxy)
@@ -47,3 +50,14 @@ equalWithFormatter f expected actual =
 
 equalGShow :: forall eff a. Eq a => Generic a => a -> a -> Test eff
 equalGShow = equalWithFormatter gShow
+
+genIndex :: forall f a m. MonadGen m => Foldable f => f a -> m Int
+genIndex xs = chooseInt 0 (length xs - 1)
+
+-- | Generates an index that will either be within the array bounds, or slightly outside of it.
+--
+-- Useful when you want to test valid indexes, plus the edge cases,
+-- but there's no point using truly random Ints and checking -1 AND
+-- -1234 AND -9237581, etc.
+genLooseIndex :: forall m f a. MonadGen m => Foldable f => f a -> m Int
+genLooseIndex xs = chooseInt (-2) (length xs + 2)
