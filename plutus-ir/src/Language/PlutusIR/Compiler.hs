766b5a5744d15bdc3f6f86b1110ef08353a95708
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2019-05-20 18:11:52 +0100

    plutus-ir: allow selective compilation of different kinds of let

--- plutus-ir/src/Language/PlutusIR/Compiler.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler.hs
@@ -23,7 +23,12 @@ import           Control.Monad
 
 -- | Compile a 'Term' into a PLC Term. Note: the result *does* have globally unique names.
 compileTerm :: Compiling m e a => Term TyName Name a -> m (PLCTerm a)
-compileTerm = lowerTerm <=< Let.compileLets <=< thunkRecursionsTerm . original
+compileTerm =
+    lowerTerm
+    <=< Let.compileLets Let.NonRecTerms
+    <=< Let.compileLets Let.RecTerms
+    <=< Let.compileLets Let.Types
+    <=< thunkRecursionsTerm . original
 
 -- | Compile a 'Program' into a PLC Program. Note: the result *does* have globally unique names.
 compileProgram :: Compiling m e a => Program TyName Name a -> m (PLC.Program TyName Name (Provenance a))
--- plutus-ir/src/Language/PlutusIR/Compiler/Let.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler/Let.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 -- | Functions for compiling PIR let terms.
-module Language.PlutusIR.Compiler.Let (compileLets) where
+module Language.PlutusIR.Compiler.Let (compileLets, LetKind(..)) where
 
 import           Language.PlutusIR
 import           Language.PlutusIR.Compiler.Datatype
@@ -18,62 +18,67 @@ import           Control.Monad.Reader
 
 import           Data.List
 
+data LetKind = RecTerms | NonRecTerms | Types
+
 -- | Compile the let terms out of a 'Term'. Note: the result does *not* have globally unique names.
-compileLets :: Compiling m e a => PIRTerm a -> m (PIRTerm a)
-compileLets = \case
+compileLets :: Compiling m e a => LetKind -> PIRTerm a -> m (PIRTerm a)
+compileLets kind = \case
     Let p r bs body -> local (const $ LetBinding r p) $ do
-        body' <- compileLets body
-        bs' <- traverse compileInBinding bs
+        body' <- compileLets kind body
+        bs' <- traverse (compileInBinding kind) bs
         case r of
-            NonRec -> foldM compileNonRecBinding body' bs'
-            Rec    -> compileRecBindings body' bs'
+            NonRec -> foldM (compileNonRecBinding kind) body' bs'
+            Rec    -> compileRecBindings kind body' bs'
     Var x n -> pure $ Var x n
-    TyAbs x n k t -> TyAbs x n k <$> compileLets t
-    LamAbs x n ty t -> LamAbs x n ty <$> compileLets t
-    Apply x t1 t2 -> Apply x <$> compileLets t1 <*> compileLets t2
+    TyAbs x n k t -> TyAbs x n k <$> compileLets kind t
+    LamAbs x n ty t -> LamAbs x n ty <$> compileLets kind t
+    Apply x t1 t2 -> Apply x <$> compileLets kind t1 <*> compileLets kind t2
     Constant x c -> pure $ Constant x c
     Builtin x bi -> pure $ Builtin x bi
-    TyInst x t ty -> TyInst x <$> compileLets t <*> pure ty
+    TyInst x t ty -> TyInst x <$> compileLets kind t <*> pure ty
     Error x ty -> pure $ Error x ty
-    IWrap x tn ty t -> IWrap x tn ty <$> compileLets t
-    Unwrap x t -> Unwrap x <$> compileLets t
+    IWrap x tn ty t -> IWrap x tn ty <$> compileLets kind t
+    Unwrap x t -> Unwrap x <$> compileLets kind t
 
 compileInBinding
     :: Compiling m e a
-    => Binding TyName Name (Provenance a)
+    => LetKind
+    -> Binding TyName Name (Provenance a)
     -> m (Binding TyName Name (Provenance a))
-compileInBinding b = case b of
-    TermBind x d rhs -> TermBind x d <$> compileLets rhs
+compileInBinding kind b = case b of
+    TermBind x d rhs -> TermBind x d <$> compileLets kind rhs
     _ -> pure b
 
-compileRecBindings :: Compiling m e a => PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
-compileRecBindings body bs =
+compileRecBindings :: Compiling m e a => LetKind -> PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
+compileRecBindings kind body bs =
     let
         partitionBindings = partition (\case { TermBind {} -> True ; _ -> False; })
         (termBinds, typeBinds) = partitionBindings bs
-    in if null typeBinds then compileRecTermBindings body termBinds
-       else if null termBinds then compileRecTypeBindings body typeBinds
+    in if null typeBinds then compileRecTermBindings kind body termBinds
+       else if null termBinds then compileRecTypeBindings kind body typeBinds
        else ask >>= \p -> throwing _Error $ CompilationError p "Mixed term and type bindings in recursive let"
 
-compileRecTermBindings :: Compiling m e a => PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
-compileRecTermBindings body bs = do
+compileRecTermBindings :: Compiling m e a => LetKind -> PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
+compileRecTermBindings RecTerms body bs = do
     binds <- forM bs $ \case
         TermBind _ vd rhs -> pure $ PIR.Def vd rhs
         _ -> ask >>= \p -> throwing _Error $ CompilationError p "Internal error: type binding in term binding group"
     compileRecTerms body binds
+compileRecTermBindings _ body bs = ask >>= \p -> pure $ Let p Rec bs body
 
-compileRecTypeBindings :: Compiling m e a => PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
-compileRecTypeBindings body bs = do
+compileRecTypeBindings :: Compiling m e a => LetKind -> PIRTerm a -> [Binding TyName Name (Provenance a)] -> m (PIRTerm a)
+compileRecTypeBindings Types body bs = do
     binds <- forM bs $ \case
         DatatypeBind _ d -> pure d
         _ -> ask >>= \p -> throwing _Error $ CompilationError p "Internal error: term or type binding in datatype binding group"
     compileRecDatatypes body binds
+compileRecTypeBindings _ body bs = ask >>= \p -> pure $ Let p Rec bs body
 
-compileNonRecBinding :: Compiling m e a => PIRTerm a -> Binding TyName Name (Provenance a) ->  m (PIRTerm a)
-compileNonRecBinding body b =  case b of
-    TermBind x d rhs -> local (const x) $ local (TermBinding (varDeclNameString d)) $
-        PIR.mkTermLet <$> ask <*> pure (PIR.Def d rhs) <*> pure body
-    TypeBind x d rhs -> local (const x) $ local (TypeBinding (tyVarDeclNameString d)) $
-        PIR.mkTypeLet <$> ask <*> pure (PIR.Def d rhs) <*> pure body
-    DatatypeBind x d -> local (const x) $ local (TypeBinding (datatypeNameString d)) $
-        compileDatatype NonRec body d
+compileNonRecBinding :: Compiling m e a => LetKind -> PIRTerm a -> Binding TyName Name (Provenance a) -> m (PIRTerm a)
+compileNonRecBinding NonRecTerms body (TermBind x d rhs) = local (const x) $ local (TermBinding (varDeclNameString d)) $
+   PIR.mkTermLet <$> ask <*> pure (PIR.Def d rhs) <*> pure body
+compileNonRecBinding Types body (TypeBind x d rhs) = local (const x) $ local (TypeBinding (tyVarDeclNameString d)) $
+   PIR.mkTypeLet <$> ask <*> pure (PIR.Def d rhs) <*> pure body
+compileNonRecBinding Types body (DatatypeBind x d) = local (const x) $ local (TypeBinding (datatypeNameString d)) $
+   compileDatatype NonRec body d
+compileNonRecBinding _ body b = ask >>= \p -> pure $ Let p NonRec [b] body
