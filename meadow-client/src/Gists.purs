65e4861c95a3e7879645a98928368cbf156b09e9
David Smith <shmish111+github@gmail.com>
2019-06-04 11:37:30 +0100

    basic gist saving for meadow

--- meadow-client/src/Gists.purs
+++ meadow-client/src/Gists.purs
@@ -1,159 +1,186 @@
-module Gists (gistControls, mkNewGist) where
+module Gists
+  ( gistControls
+  , mkNewGist
+  , gistSourceFilename
+  , parseGistUrl
+  , playgroundGistFile
+  ) where
 
-import AjaxUtils (showAjaxError)
-import Auth (AuthRole(..), AuthStatus, authStatusAuthRole)
-import Bootstrap (btn, btnDanger, btnInfo, btnPrimary, nbsp)
+import AjaxUtils (ajaxErrorPane)
+import Auth (AuthRole(..), authStatusAuthRole)
+import Bootstrap (btn, btnBlock, btnDanger, btnInfo, btnPrimary, btnSmall, col12_, col6_, empty, formControl, isInvalid, isValid, nbsp, pullRight, row_)
+import DOM.HTML.Indexed.InputType (InputType(..))
 import Data.Array (catMaybes)
-import Data.Lens (view)
-import Data.Maybe (Maybe(..))
-import Gist (Gist, NewGist(NewGist), NewGistFile(NewGistFile), gistHtmlUrl)
-import Halogen.HTML (ClassName(ClassName), HTML, a, br_, div, div_, text)
-import Halogen.HTML.Events (input_, onClick)
-import Halogen.HTML.Properties (class_, classes, href, target)
-import Icons (Icon(..), icon)
-import Network.RemoteData (RemoteData(NotAsked, Loading, Failure, Success))
-import Prelude (Unit, ($), (<$>))
-import Servant.PureScript.Ajax (AjaxError)
-import Types (Query(..))
 import Data.Array as Array
+import Data.Array.NonEmpty as NonEmptyArray
+import Data.Either (Either(..), isRight, note)
+import Data.Generic.Rep (class Generic)
+import Data.Lens (findOf, traversed, view)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype, unwrap)
+import Data.String.Regex (Regex, match, regex)
+import Data.String.Regex.Flags (ignoreCase)
+import Foreign.Class (class Decode, class Encode)
+import Foreign.Generic (aesonSumEncoding, defaultOptions, encodeJSON, genericDecode, genericEncode)
+import Gist (Gist, GistFile, GistId(GistId), NewGist(NewGist), NewGistFile(NewGistFile), gistFileFilename, gistFiles, gistHtmlUrl)
+import Halogen.HTML (ClassName(ClassName), HTML, IProp, a, button, div, div_, input, text)
+import Halogen.HTML.Events (input_, onClick, onValueInput)
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties (class_, classes, disabled, href, id_, placeholder, target, type_, value)
+import Icons (Icon(..), icon)
+import Language.Haskell.Interpreter (SourceCode)
+import Network.RemoteData (RemoteData(NotAsked, Loading, Failure, Success))
+import Prelude (Unit, bind, ($), (<$>), (<<<), (<>), (=<<), (==))
+import Types (FrontendState, Query(SetGistUrl, LoadGist, PublishGist), MarloweState)
 
-gistControls ::
-  forall p.
-  RemoteData AjaxError AuthStatus ->
-  RemoteData AjaxError Gist ->
-  HTML p (Query Unit)
-gistControls authStatus createGistResult =
-  div_
-    [ a publishAttributes publishContent
-    , br_
-    , div_
-        [ case createGistResult of
-            Success gist -> gistPane gist
-            Failure err -> showAjaxError err
-            Loading -> nbsp
-            NotAsked -> nbsp
-        ]
+idPublishGist :: forall r i. IProp (id :: String | r) i
+idPublishGist = id_ "publish-gist"
+
+idLoadGist :: forall r i. IProp (id :: String | r) i
+idLoadGist = id_ "load-gist"
+
+gistControls :: forall p. FrontendState -> HTML p (Query Unit)
+gistControls {authStatus, createGistResult, gistUrl} =
+  div [classes [ClassName "gist-controls"]]
+    [ authButton
+        $ div_
+            [ row_
+                [ col12_
+                    [ input
+                        [ type_ InputText
+                        , value $ fromMaybe "" $ gistUrl
+                        , id_ "gist-id"
+                        , classes
+                            ( [formControl]
+                              <> case parsedGistId of
+                                  Just (Left err) -> [isInvalid]
+                                  Just (Right err) -> [isValid]
+                                  Nothing -> []
+                            )
+                        , placeholder "Load Gist ID"
+                        , onValueInput $ HE.input SetGistUrl
+                        ]
+                    ]
+                , col6_ [publishButton]
+                , col6_ [loadButton]
+                ]
+            , case createGistResult of
+                Success gist -> gistPane gist
+                Failure err -> ajaxErrorPane err
+                Loading -> empty
+                NotAsked -> empty
+            ]
     ]
   where
-  publishAttributes = case (view authStatusAuthRole <$> authStatus), createGistResult of
-    Failure _, _ ->
-      [ classes
-          [ btn
-          , btnDanger
-          ]
-      ]
-    _, Failure _ ->
-      [ classes
-          [ btn
-          , btnDanger
-          ]
-      ]
-    Success Anonymous, _ ->
-      [ classes
-          [ btn
-          , btnInfo
-          ]
-      , href "/api/oauth/github"
-      ]
-    Success GithubUser, NotAsked ->
-      [ classes
-          [ btn
-          , btnPrimary
-          ]
-      , onClick $ input_ PublishGist
-      ]
-    Success GithubUser, Success _ ->
-      [ classes
-          [ btn
-          , btnPrimary
-          ]
-      , onClick $ input_ PublishGist
-      ]
-    Loading, _ ->
-      [ classes
-          [ btn
-          , btnInfo
-          ]
-      ]
-    _, Loading ->
-      [ classes
-          [ btn
-          , btnInfo
-          ]
-      ]
-    NotAsked, _ ->
-      [ classes
-          [ btn
-          , btnInfo
-          ]
-      ]
+  canTryLoad = isRight $ parseGistUrl =<< note "No gist Url set" gistUrl
 
-  publishContent = case (view authStatusAuthRole <$> authStatus), createGistResult of
-    Failure _, _ ->
-      [ text "Failure"
-      ]
-    _, Failure _ ->
-      [ text "Failure"
-      ]
-    Success Anonymous, _ ->
-      [ icon Github
-      , nbsp
-      , text "Publish"
-      ]
-    Success GithubUser, NotAsked ->
-      [ icon Github
-      , nbsp
-      , text "Publish"
-      ]
-    Success GithubUser, Success _ ->
-      [ icon Github
-      , nbsp
-      , text "Republish"
-      ]
-    Loading, _ ->
-      [ icon Spinner
-      ]
-    _, Loading ->
-      [ icon Spinner
-      ]
-    NotAsked, _ ->
-      [ icon Github
-      , nbsp
-      , text "Publish"
-      ]
+  parsedGistId :: Maybe (Either String GistId)
+  parsedGistId = parseGistUrl <$> gistUrl
 
-gistPane ::
-  forall p i.
-  Gist ->
-  HTML p i
+  authButton authorisedView = case view authStatusAuthRole <$> authStatus of
+    Failure _ ->
+      button
+        [ idPublishGist
+        , classes [btn, btnDanger, pullRight]
+        ] [text "Failure"]
+    Success Anonymous ->
+      a
+        [ idPublishGist
+        , classes [btn, btnInfo, pullRight]
+        , href "/api/oauth/github"
+        ]
+        [ icon Github
+        , nbsp
+        , text "Log In"
+        ]
+    Success GithubUser -> authorisedView
+    Loading ->
+      button
+        [ idPublishGist
+        , classes [btn, btnInfo, pullRight]
+        , disabled true
+        ] [icon Spinner]
+    NotAsked ->
+      button
+        [ idPublishGist
+        , classes [btn, btnInfo, pullRight]
+        , disabled true
+        ] [icon Spinner]
+
+  publishButton = case createGistResult of
+    Failure _ ->
+      button
+        [ idPublishGist
+        , classes [btn, btnBlock, btnSmall, btnDanger]
+        ] [text "Failure"]
+    Success _ ->
+      button
+        [ idPublishGist
+        , classes [btn, btnBlock, btnSmall, btnPrimary]
+        , onClick $ input_ PublishGist
+        ] [icon Github, nbsp, text "Republish"]
+    Loading ->
+      button
+        [ idPublishGist
+        , classes [btn, btnBlock, btnSmall, btnInfo]
+        , disabled true
+        ] [icon Spinner]
+    NotAsked ->
+      button
+        [ idPublishGist
+        , classes [btn, btnBlock, btnSmall, btnPrimary]
+        , onClick $ input_ PublishGist
+        ] [icon Github, nbsp, text "Publish"]
+
+  loadMessage = [icon Github, nbsp, text "Load"]
+
+  loadButton = case createGistResult of
+    Loading -> empty
+    _ ->
+      button
+        [ idLoadGist
+        , classes
+            [ btn
+            , btnBlock
+            , btnSmall
+            , case parsedGistId of
+                Just (Left url) -> btnDanger
+                Just (Right url) -> btnPrimary
+                Nothing -> btnInfo
+            ]
+        , onClick $ input_ LoadGist
+        , disabled
+            $ case parsedGistId of
+                Just (Left url) -> true
+                Just (Right url) -> false
+                Nothing -> true
+        ] loadMessage
+
+gistPane :: forall p i. Gist -> HTML p i
 gistPane gist =
-  div
-    [ class_ $ ClassName "gist-link"
-    ]
+  div [class_ $ ClassName "gist-link"]
     [ a
         [ href $ view gistHtmlUrl gist
         , target "_blank"
-        ]
-        [ text $ "View on Github"
-        ]
+        ] [text $ "View on Github"]
     ]
 
 mkNewGist ::
-  Maybe String ->
+  Maybe SourceCode ->
   Maybe NewGist
-mkNewGist mSource = if Array.null gistFiles
+mkNewGist source = if Array.null gistFiles
   then Nothing
   else
     Just
       $ NewGist
-          { _newGistDescription: "Plutus Playground Smart Contract"
+          { _newGistDescription: "Marlowe Smart Contract"
           , _newGistPublic: true
           , _newGistFiles: gistFiles
           }
   where
   gistFiles =
     catMaybes
-      [ mkNewGistFile "Playground.hs" <$> mSource
+      [ mkNewGistFile gistSourceFilename <<< unwrap <$> source
       ]
 
   mkNewGistFile _newGistFilename _newGistFileContent =
@@ -161,3 +188,24 @@ mkNewGist mSource = if Array.null gistFiles
       { _newGistFilename
       , _newGistFileContent
       }
+
+gistSourceFilename :: String
+gistSourceFilename = "Meadow.hs"
+
+gistIdInLinkRegex :: Either String Regex
+gistIdInLinkRegex = regex "^(.*/)?([0-9a-f]{32})$" ignoreCase
+
+parseGistUrl :: String -> Either String GistId
+parseGistUrl str = do
+  gistIdInLink <- gistIdInLinkRegex
+  note "Could not parse Gist Url"
+    $ do
+        matches <- match gistIdInLink str
+        match <- NonEmptyArray.index matches 2
+        GistId <$> match
+
+firstMatch :: String -> Gist -> Maybe GistFile
+firstMatch filename = findOf (gistFiles <<< traversed) (\gistFile -> view gistFileFilename gistFile == filename)
+
+playgroundGistFile :: Gist -> Maybe GistFile
+playgroundGistFile = firstMatch gistSourceFilename
