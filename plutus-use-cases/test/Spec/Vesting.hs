accd76a8ff6974ae100d346612a675fa061ba436
Jann Müller <jann.mueller@iohk.io>
2019-09-03 12:59:14 +0200

    use-cases: Update vesting contract
    * Use new contracts API
    * Simple implementation so that it can be used as an example in the
      playground
    * Possible improvements:
      * Better error handling & checking of preconditions in 'retrieveC'
      * Use a token to represent the owner (instead of a public key)
      * List of tranches instead of fixed number

--- plutus-use-cases/test/Spec/Vesting.hs
+++ plutus-use-cases/test/Spec/Vesting.hs
@@ -1,174 +1,90 @@
 {-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns -fno-warn-unused-do-bind #-}
 module Spec.Vesting(tests) where
 
-import           Control.Monad                                    (void)
-import           Data.Either                                      (isRight)
-import           Data.Foldable                                    (traverse_)
-import qualified Data.Map                                         as Map
-import           Hedgehog                                         (Property, forAll, property)
-import qualified Hedgehog
 import           Test.Tasty
-import           Test.Tasty.Hedgehog                              (testProperty)
 import qualified Test.Tasty.HUnit                                 as HUnit
 
-import           Spec.Lib                                                      as Lib
+import           Spec.Lib                                         as Lib
 
-import qualified Language.PlutusTx as PlutusTx
-import qualified Language.PlutusTx.Prelude as PlutusTx
-
-import           Language.PlutusTx.Coordination.Contracts.Vesting (Vesting (..), VestingData (..), VestingTranche (..),
-                                                                   retrieveFunds, totalAmount, validatorScript, mkValidator,
-                                                                    vestFunds)
+import qualified Language.PlutusTx                                as PlutusTx
+import           Language.PlutusTx.Lattice
+import qualified Language.PlutusTx.Numeric                        as Numeric
+import           Language.PlutusTx.Coordination.Contracts.Vesting
 import qualified Ledger
 import qualified Ledger.Ada                                       as Ada
-import           Ledger.Value                                     (Value)
-import           Wallet                                           (PubKey (..))
-import           Wallet.Emulator
-import qualified Wallet.Emulator.Generators                       as Gen
-import qualified Wallet.Generators                                as Gen
 
-w1, w2 :: Wallet
-w1 = Wallet 1
-w2 = Wallet 2
+import           Language.Plutus.Contract.Test
+import           Prelude   hiding (not)
+
+wallet1, wallet2 :: Wallet
+wallet1 = Wallet 1
+wallet2 = Wallet 2
 
 tests :: TestTree
-tests = testGroup "vesting" [
-    testProperty "secure some funds with the vesting script" secureFunds,
-    testProperty "retrieve some funds" canRetrieveFunds,
-    testProperty "cannot retrieve more than allowed" cannotRetrieveTooMuch,
-    testProperty "can retrieve everything at end" canRetrieveFundsAtEnd,
-    Lib.goldenPir "test/Spec/vesting.pir" $$(PlutusTx.compile [|| mkValidator ||]),
-    HUnit.testCase "script size is reasonable" (Lib.reasonable (validatorScript $ vsVestingScheme scen1) 30000)
+tests =
+    let con = vestingContract vesting in
+    testGroup "vesting"
+    [ checkPredicate "secure some funds with the vesting script"
+        con
+        (walletFundsChange wallet2 (Numeric.negate $ totalAmount vesting))
+        ( callEndpoint @"vest funds" wallet2 ()
+        >> handleBlockchainEvents wallet2)
+
+    , checkPredicate "retrieve some funds"
+        con
+        (walletFundsChange wallet2 (Numeric.negate $ totalAmount vesting)
+        /\ assertNoFailedTransactions
+        /\ walletFundsChange wallet1 (Ada.lovelaceValueOf 10))
+        (callEndpoint @"vest funds" wallet2 ()
+        >> handleBlockchainEvents wallet2
+        >> addBlocks 10
+        >> callEndpoint @"retrieve funds" wallet1 (Ada.lovelaceValueOf 10)
+        >> notifySlot wallet1
+        >> addBlocks 1
+        >> handleBlockchainEvents wallet1)
+
+    , checkPredicate "cannot retrieve more than allowed"
+        con
+        (walletFundsChange wallet1 mempty
+        /\ not assertNoFailedTransactions)
+        (callEndpoint @"vest funds" wallet2 ()
+        >> handleBlockchainEvents wallet2
+        >> addBlocks 10
+        >> callEndpoint @"retrieve funds" wallet1 (Ada.lovelaceValueOf 30)
+        >> notifySlot wallet1
+        >> addBlocks 1
+        >> handleBlockchainEvents wallet1)
+
+    , checkPredicate "can retrieve everything at the end"
+        con
+        (walletFundsChange wallet1 (totalAmount vesting)
+        /\ assertNoFailedTransactions
+        /\ assertDone wallet1 (const True) "should be done")
+        (callEndpoint @"vest funds" wallet2 ()
+        >> handleBlockchainEvents wallet2
+        >> addBlocks 20
+        >> callEndpoint @"retrieve funds" wallet1 (totalAmount vesting)
+        >> notifySlot wallet1
+        >> addBlocks 1
+        >> handleBlockchainEvents wallet1)
+
+    , Lib.goldenPir "test/Spec/vesting.pir" $$(PlutusTx.compile [|| validate ||])
+    , HUnit.testCase "script size is reasonable" (Lib.reasonable (vestingScript vesting) 33000)
     ]
 
 -- | The scenario used in the property tests. It sets up a vesting scheme for a
---   total of 600 ada over 20 blocks (200 ada can be taken out before that, at
---   10 blocks).
-scen1 :: VestingScenario
-scen1 = VestingScenario{..} where
-    vsVestingScheme = Vesting {
-        vestingTranche1 = VestingTranche (Ledger.Slot 10) (Ada.adaValueOf 200),
-        vestingTranche2 = VestingTranche (Ledger.Slot 20) (Ada.adaValueOf 400),
-        vestingOwner    = walletPubKey w1 }
-    vsInitialBalances = Map.fromList [
-        (walletPubKey w1, startingBalance),
-        (walletPubKey w2, startingBalance)]
-
--- | Commit some funds from a wallet to a vesting scheme. Returns the reference
---   to the transaction output that is locked by the schemes's validator
---   script (and can be collected by the scheme's owner)
-commit :: Wallet -> Vesting -> Value -> Trace MockWallet Ledger.TxOutRef
-commit w vv vl = exScriptOut <$> walletAction w (void $ vestFunds vv vl) where
-    exScriptOut = snd . head . filter (Ledger.isPayToScriptOut . fst) . Ledger.txOutRefs . head . snd
-
-secureFunds :: Property
-secureFunds = checkVestingTrace scen1 $ do
-    let VestingScenario s _ = scen1
-    updateAll
-    _ <- commit w2 s total
-    updateAll
-    traverse_ (uncurry assertOwnFundsEq) [
-        (w2, w2Funds),
-        (w1, startingBalance)]
-
-canRetrieveFunds :: Property
-canRetrieveFunds = checkVestingTrace scen1 $ do
-    let VestingScenario s _ = scen1
-        amt = Ada.adaValueOf 150
-    updateAll
-
-    -- Wallet 2 locks 600 ada under the scheme described in `scen1`
-    ref <- commit w2 s total
-    updateAll
-
-    -- Advance the clock so that the first tranche (200 ada) becomes unlocked.
-    addBlocks' 10
-    let ds = VestingData amt
-
-    -- Take 150 ada out of the scheme
-    walletAction w1 $ void (retrieveFunds s ds ref amt)
-    updateAll
-    traverse_ (uncurry assertOwnFundsEq) [
-        (w2, w2Funds),
-        (w1, 1 `timesFeeAdjustV` (startingBalance PlutusTx.+ amt))]
-
-cannotRetrieveTooMuch :: Property
-cannotRetrieveTooMuch = checkVestingTrace scen1 $ do
-    let VestingScenario s _ = scen1
-    updateAll
-    ref <- commit w2 s total
-    updateAll
-    addBlocks' 10
-
-    -- at slot 11, not more than 200 may be taken out
-    -- so the transaction submitted by `retrieveFunds` below
-    -- is invalid and will be rejected by the mockchain.
-    let ds = VestingData (Ada.adaValueOf 250)
-    walletAction w1 $ void (retrieveFunds s ds ref (Ada.adaValueOf 250))
-    updateAll
-
-    -- The funds of both wallets should be unchanged.
-    traverse_ (uncurry assertOwnFundsEq) [(w2, w2Funds), (w1, startingBalance)]
-
-canRetrieveFundsAtEnd :: Property
-canRetrieveFundsAtEnd = checkVestingTrace scen1 $ do
-    let VestingScenario s _ = scen1
-    updateAll
-    ref <- commit w2 s total
-    updateAll
-    addBlocks' 20
-
-    -- everything can be taken out at h=21
-    let ds = VestingData (Ada.adaValueOf 600)
-    walletAction w1 $ void (retrieveFunds s ds ref (Ada.adaValueOf 600))
-    updateAll
-
-    -- Wallet 1 now has control of all the funds that were locked in the
-    -- vesting scheme.
-    traverse_ (uncurry assertOwnFundsEq) [
-        (w2, w2Funds),
-        (w1, 1 `timesFeeAdjustV` (startingBalance PlutusTx.+ total))]
-
--- | Vesting scenario with test parameters
-data VestingScenario = VestingScenario {
-    vsVestingScheme   :: Vesting,
-    vsInitialBalances :: Map.Map PubKey Ledger.Value
-    }
-
--- | Funds available to each wallet after the initial transaction on the
---   mockchain
-startingBalance :: Ledger.Value
-startingBalance = Ada.adaValueOf 1000
-
--- | Amount of money left in wallet `Wallet 2` after committing funds to the
---   vesting scheme
-w2Funds :: Ledger.Value
-w2Funds = 1 `timesFeeAdjustV` (startingBalance PlutusTx.- total)
-
--- | Total amount of money vested in the scheme `scen1`
-total :: Value
-total = totalAmount $ vsVestingScheme scen1
-
--- | Run a trace with the given scenario and check that the emulator finished
---   successfully with an empty transaction pool.
-checkVestingTrace :: VestingScenario -> Trace MockWallet () -> Property
-checkVestingTrace VestingScenario{vsInitialBalances} t = property $ do
-    let model = Gen.generatorModel { Gen.gmInitialBalance = vsInitialBalances }
-    (result, st) <- forAll $ Gen.runTraceOn model t
-    Hedgehog.assert (isRight result)
-    Hedgehog.assert ([] == _txPool st)
-
--- | Validate all pending transactions and notify the wallets
-updateAll :: Trace MockWallet [Ledger.Tx]
-updateAll =
-    processPending >>= walletsNotifyBlock [w1, w2]
-
--- | Add a number of blocks and notify the wallets
-addBlocks' :: Int -> Trace MockWallet ()
-addBlocks' i = traverse_ (const updateAll) [1..i]
+--   total of 60 lovelace over 20 blocks (20 lovelace can be taken out before 
+--   that, at 10 blocks).
+vesting :: VestingParams
+vesting =
+    VestingParams
+        { vestingTranche1 = VestingTranche (Ledger.Slot 10) (Ada.lovelaceValueOf 20)
+        , vestingTranche2 = VestingTranche (Ledger.Slot 20) (Ada.lovelaceValueOf 40)
+        , vestingOwner    = walletPubKey wallet1 }
