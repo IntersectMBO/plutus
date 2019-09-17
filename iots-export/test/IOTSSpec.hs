01db949c87d1104e218be239915c6948ce6b56e7
Kris Jenkins <krisajenkins@gmail.com>
2019-09-17 14:03:42 +0100

    Whitespace changes.

--- iots-export/test/IOTSSpec.hs
+++ iots-export/test/IOTSSpec.hs
@@ -25,58 +25,60 @@ spec = toIOTSSpec
 
 toIOTSSpec :: Spec
 toIOTSSpec =
-  describe "Export to IOTS format" $ do
-    it "Should export a single, fieldless constructor" $
-      exportsAs (Proxy @SingleFieldless) "test/IOTS/singleFieldless.ts"
-    it "Should export a simple sum type" $
-      exportsAs (Proxy @SimpleSum) "test/IOTS/simpleSum.ts"
-    it "Should export a sample user" $
-      exportsAs (Proxy @User) "test/IOTS/user.ts"
-    it "Should export a more interesting sum type" $
-      exportsAs (Proxy @(Response String)) "test/IOTS/response.ts"
-    it "Should export a function with tuples." $ do
-      let someFunction :: (CurrencySymbol, TokenName) -> String
-          someFunction _ = "Anything."
-      exportsAs
-        (Tagged @"tupleFunction" someFunction)
-        "test/IOTS/functionTuple.ts"
-    it "Should export a function with maybes." $ do
-      let someFunction :: Maybe Slot -> String
-          someFunction _ = "Anything."
-      exportsAs
-        (Tagged @"maybeFunction" someFunction)
-        "test/IOTS/functionMaybe.ts"
-    it "Should export a function with lists." $ do
-      let someFunction :: [User] -> String
-          someFunction _ = "Anything."
-      exportsAs
-        (Tagged @"listFunction" someFunction)
-        "test/IOTS/functionList.ts"
-    let functionA ::
-             (CurrencySymbol, TokenName)
-          -> Maybe Value
-          -> Interval Slot
-          -> [VestingTranche]
-          -> String
-        functionA _ _ _ _ = "Anything."
-        functionB :: Interval Slot -> [CurrencySymbol] -> String
-        functionB _ _ = "Anything."
-    it "Should export a complex example" $ do
-      exportsAs (Tagged @"functionA" functionA) "test/IOTS/vestingTranche.ts"
-      exportsAs
-        (HCons (Tagged @"functionA" functionA) HNil)
-        "test/IOTS/vestingTranche.ts"
-    it "Should export a mixed example" $
-      exportsAs
-        (HCons
-           (Tagged @"functionA" functionA)
-           (HCons (Tagged @"functionB" functionB) HNil))
-        "test/IOTS/fullContract.ts"
+    describe "Export to IOTS format" $ do
+        it "Should export a single, fieldless constructor" $
+            exportsAs (Proxy @SingleFieldless) "test/IOTS/singleFieldless.ts"
+        it "Should export a simple sum type" $
+            exportsAs (Proxy @SimpleSum) "test/IOTS/simpleSum.ts"
+        it "Should export a sample user" $
+            exportsAs (Proxy @User) "test/IOTS/user.ts"
+        it "Should export a more interesting sum type" $
+            exportsAs (Proxy @(Response String)) "test/IOTS/response.ts"
+        it "Should export a function with tuples." $ do
+            let someFunction :: (CurrencySymbol, TokenName) -> String
+                someFunction _ = "Anything."
+            exportsAs
+                (Tagged @"tupleFunction" someFunction)
+                "test/IOTS/functionTuple.ts"
+        it "Should export a function with maybes." $ do
+            let someFunction :: Maybe Slot -> String
+                someFunction _ = "Anything."
+            exportsAs
+                (Tagged @"maybeFunction" someFunction)
+                "test/IOTS/functionMaybe.ts"
+        it "Should export a function with lists." $ do
+            let someFunction :: [User] -> String
+                someFunction _ = "Anything."
+            exportsAs
+                (Tagged @"listFunction" someFunction)
+                "test/IOTS/functionList.ts"
+        let functionA ::
+                   (CurrencySymbol, TokenName)
+                -> Maybe Value
+                -> Interval Slot
+                -> [VestingTranche]
+                -> String
+            functionA _ _ _ _ = "Anything."
+            functionB :: Interval Slot -> [CurrencySymbol] -> String
+            functionB _ _ = "Anything."
+        it "Should export a complex example" $ do
+            exportsAs
+                (Tagged @"functionA" functionA)
+                "test/IOTS/vestingTranche.ts"
+            exportsAs
+                (HCons (Tagged @"functionA" functionA) HNil)
+                "test/IOTS/vestingTranche.ts"
+        it "Should export a mixed example" $
+            exportsAs
+                (HCons
+                     (Tagged @"functionA" functionA)
+                     (HCons (Tagged @"functionB" functionB) HNil))
+                "test/IOTS/fullContract.ts"
 
 exportsAs :: (IotsExportable a) => a -> FilePath -> IO ()
 exportsAs exportable filename = do
-  file <- Text.readFile =<< getDataFileName filename
-  export exportable `shouldBePrettyDiff` file
+    file <- Text.readFile =<< getDataFileName filename
+    export exportable `shouldBePrettyDiff` file
   where
     shouldBePrettyDiff :: Text -> Text -> Assertion
     shouldBePrettyDiff (Text.stripEnd -> a) (Text.stripEnd -> b) =
@@ -86,74 +88,74 @@ exportsAs exportable filename = do
     formatError err = unlines [filename, "Export failed with:", err]
 
 data SingleFieldless =
-  VeryDull
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    VeryDull
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 data SimpleSum
-  = This
-  | That
-  | TheOther
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    = This
+    | That
+    | TheOther
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 data User =
-  User
-    { userId :: Int
-    , name   :: String
-    }
-  deriving (Show, Eq, Generic, IotsType)
+    User
+        { userId :: Int
+        , name   :: String
+        }
+    deriving (Show, Eq, Generic, IotsType)
 
 data Response a
-  = UnknownError Int Text
-  | StatusError
-      { code    :: Int
-      , message :: Text
-      , headers :: Map String Int
-      }
-  | EmptyResponse
-  | Response a
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    = UnknownError Int Text
+    | StatusError
+          { code    :: Int
+          , message :: Text
+          , headers :: Map String Int
+          }
+    | EmptyResponse
+    | Response a
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 newtype Slot =
-  Slot
-    { getSlot :: Integer
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    Slot
+        { getSlot :: Integer
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 newtype CurrencySymbol =
-  CurrencySymbol
-    { unCurrencySymbol :: Text
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    CurrencySymbol
+        { unCurrencySymbol :: Text
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 newtype TokenName =
-  TokenName
-    { unTokenName :: Text
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    TokenName
+        { unTokenName :: Text
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 newtype AssocMap k v =
-  AssocMap
-    { unMap :: [(k, v)]
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    AssocMap
+        { unMap :: [(k, v)]
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 newtype Value =
-  Value
-    { getValue :: AssocMap CurrencySymbol (AssocMap TokenName Integer)
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    Value
+        { getValue :: AssocMap CurrencySymbol (AssocMap TokenName Integer)
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 data Interval a =
-  Interval
-    { ivFrom :: Maybe a
-    , ivTo   :: Maybe a
-    }
-  deriving (Show, Eq, Ord, Generic, IotsType)
+    Interval
+        { ivFrom :: Maybe a
+        , ivTo   :: Maybe a
+        }
+    deriving (Show, Eq, Ord, Generic, IotsType)
 
 data VestingTranche =
-  VestingTranche
-    { vestingTrancheDate   :: Slot
-    , vestingTrancheAmount :: Value
-    , validity             :: Interval Slot
-    }
-  deriving (Show, Eq, Generic, IotsType)
+    VestingTranche
+        { vestingTrancheDate   :: Slot
+        , vestingTrancheAmount :: Value
+        , validity             :: Interval Slot
+        }
+    deriving (Show, Eq, Generic, IotsType)
