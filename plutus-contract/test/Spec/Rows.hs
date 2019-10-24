61c2e325108242ec63adbd8442bb59b2f2ab929b
Jann Müller <jann.mueller@iohk.io>
2019-10-24 17:00:33 +0200

    contracts: Fix a bug in a ToJSON instance

---
+++ plutus-contract/test/Spec/Rows.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
+module Spec.Rows(tests) where
+
+import qualified Data.Aeson                                      as Aeson
+import           Test.Tasty
+import qualified Test.Tasty.HUnit                                as HUnit
+import           Test.Tasty.Providers                            (TestTree)
+
+import           Language.Plutus.Contract
+import           Language.Plutus.Contract.Effects.ExposeEndpoint as Endpoint
+
+type TheSchema = Endpoint "endpoint1" Int .\/ Endpoint "endpoint2" String
+
+tests :: TestTree
+tests = testGroup "JSON instances"
+         [ HUnit.testCase "should round-trip" $ do
+            let e = Endpoint.event @"endpoint1" @_ @TheSchema 10
+                e' = Aeson.eitherDecode $ Aeson.encode e
+            HUnit.assertBool "round-trip failed" $ Right e == e'
+
+         ]
