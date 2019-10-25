b7d2c05f0a0f801549586e663a1d16a438b5344a
James Chapman <james.chapman@iohk.io>
2019-10-25 14:55:44 +0100

    an attempt at calling plc-agda's main from inside Haskell

--- metatheory/Test.hs
+++ metatheory/Test.hs
@@ -3,19 +3,17 @@ module Main where
 import System.Exit (exitFailure)
 import System.Process
 
-main = do
-  callCommand "plc example -s succInteger | plc-agda evaluate --stdin"
-  callCommand "plc example -s unitval | plc-agda evaluate --stdin"
-  callCommand "plc example -s true | plc-agda evaluate --stdin"
-  callCommand "plc example -s false | plc-agda evaluate --stdin"
-  callCommand "plc example -s churchZero | plc-agda evaluate --stdin"
-  callCommand "plc example -s churchSucc | plc-agda evaluate --stdin"
-  callCommand "plc example -s overapplication | plc-agda evaluate --stdin"
-  callCommand "plc example -s factorial | plc-agda evaluate --stdin"
-  callCommand "plc example -s fibonacci | plc-agda evaluate --stdin"
-  callCommand "plc example -s NatRoundTrip | plc-agda evaluate --stdin"
-  callCommand "plc example -s ListSum | plc-agda evaluate --stdin"
-  callCommand "plc example -s IfIntegers | plc-agda evaluate --stdin"
-  callCommand "plc example -s ApplyAdd1 | plc-agda evaluate --stdin"
-  callCommand "plc example -s ApplyAdd2 | plc-agda evaluate --stdin"
-  callCommand "plc example -s DivideByZero | plc-agda evaluate --stdin"
+import qualified MAlonzo.Code.Main as M
+
+import System.Environment
+
+tests = ["succInteger","unitval","true","false","churchZero","churchSucc","overapplication","factorial","fibonacci","NatRoundTrip","ListSum","IfIntegers","ApplyAdd1","ApplyAdd2"]
+
+runTest :: String -> IO [()]
+runTest test = do
+  example <- readProcess "plc" ["example","-s",test] []
+  writeFile "tmp" example
+  withArgs ["evaluate","--file","tmp"]  M.main
+
+
+main = sequence (map runTest tests)
