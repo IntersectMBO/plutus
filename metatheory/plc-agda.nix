115ca81a0a49bc410702b1132707b7687d23bff7
James Chapman <james.chapman@iohk.io>
2019-10-25 12:36:07 +0100

    applying @michaelpj's plc-agda.nix patch

--- metatheory/plc-agda.nix
+++ metatheory/plc-agda.nix
@@ -1,5 +1,6 @@
 { mkDerivation, base, bytestring, cryptonite, ieee754
-, language-plutus-core, memory, stdenv, text
+, language-plutus-core, memory, optparse-applicative, plutus-exe
+, process, stdenv, text
 }:
 mkDerivation {
   pname = "plc-agda";
@@ -8,8 +9,11 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base bytestring cryptonite ieee754 language-plutus-core memory text
+    base bytestring cryptonite ieee754 language-plutus-core memory
+    optparse-applicative text
   ];
+  testHaskellDepends = [ base process ];
+  testToolDepends = [ plutus-exe ];
   homepage = "https://github.com/input-output-hk/plutus";
   description = "Command line tool for running plutus core programs";
   license = stdenv.lib.licenses.asl20;
