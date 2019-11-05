8cbb8069bc3779d2a17ea763d6e52877c0bee615
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2019-11-05 15:26:20 +0000

    Don't build updateClientDeps on darwin

--- default.nix
+++ default.nix
@@ -67,6 +67,15 @@ let
   latex = pkgs.callPackage ./nix/latex.nix {};
   sources = import ./nix/sources.nix;
 
+  # easy-purescript-nix has some kind of wacky internal IFD
+  # usage that breaks the logic that makes source fetchers
+  # use native dependencies. This isn't easy to fix, since
+  # the only places that need to use native dependencies
+  # are deep inside, and we don't want to build the whole 
+  # thing native. Fortunately, we only want to build the
+  # client on Linux, so that's okay. However, it does
+  # mean that e.g. we can't build the client dep updating
+  # script on Darwin.
   easyPS = pkgs.callPackage sources.easy-purescript-nix { }; 
 
   purty = pkgs.callPackage ./purty { };
