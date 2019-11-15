092bcbf4f546ab6c6b225491eca0ac3bcd90cfbd
Kris Jenkins <krisajenkins@gmail.com>
2019-11-15 12:25:42 +0000

    Adding a docstring.

--- plutus-playground-client/src/Array/Extra.purs
+++ plutus-playground-client/src/Array/Extra.purs
@@ -3,6 +3,8 @@ module Array.Extra where
 import Data.FoldableWithIndex (class FoldableWithIndex, foldMapWithIndex)
 import Data.Tuple.Nested (type (/\), (/\))
 
+--| Turn a nested foldable into an array with keys. So, for example, a
+-- `List (Map String Bool)` would become an `Array (Int /\ String /\ Bool)`.
 collapse ::
   forall m n i j a.
   FoldableWithIndex i m =>
