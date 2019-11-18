57256c4332f937b3ad459cc88cd4371cd27d3a12
Kris Jenkins <krisajenkins@gmail.com>
2019-11-18 09:53:11 +0000

    Frontend: Merging Array.Extra and Data.Array.Extra.
    There's no reason to have both.

--- plutus-playground-client/src/Data/Array/Extra.purs
+++ plutus-playground-client/src/Data/Array/Extra.purs
@@ -2,14 +2,16 @@ module Data.Array.Extra
   ( move
   , intersperse
   , lookup
+  , collapse
   ) where
 
 import Prelude
-import Data.Tuple.Nested (type (/\))
 import Data.Array (snoc, foldl)
 import Data.Array as Array
+import Data.FoldableWithIndex (class FoldableWithIndex, foldMapWithIndex)
 import Data.Maybe (Maybe, fromMaybe)
 import Data.Tuple (fst, snd)
+import Data.Tuple.Nested (type (/\), (/\))
 
 move :: forall a. Int -> Int -> Array a -> Array a
 move source destination before
@@ -30,3 +32,13 @@ intersperse sep = foldl reducer []
 
 lookup :: forall k v. Eq k => k -> Array (k /\ v) -> Maybe v
 lookup key = map snd <<< Array.find (fst >>> (==) key)
+
+--| Turn a nested foldable into an array with keys. So, for example, a
+-- `List (Map String Bool)` would become an `Array (Int /\ String /\ Bool)`.
+collapse ::
+  forall m n i j a.
+  FoldableWithIndex i m =>
+  FoldableWithIndex j n =>
+  m (n a) ->
+  Array (i /\ j /\ a)
+collapse = foldMapWithIndex (\i -> foldMapWithIndex (\j a -> [ i /\ j /\ a ]))
