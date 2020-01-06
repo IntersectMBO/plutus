cdbad8531312512c2f74d8cce3f7bfcad73d358c
Kris Jenkins <kris.jenkins@tweag.io>
2020-01-06 10:27:25 +0000

    Frontend: Rearranging the plutus playground client startup a little.
    This moves some of the logic which is specific to our app into
    `MainFrame.purs`, leaving `Main.purs` all about generic Halogen wiring
    and bootstrapping.

--- plutus-playground-client/src/Main.purs
+++ plutus-playground-client/src/Main.purs
@@ -2,31 +2,24 @@ module Main where
 
 import Prelude
 import Control.Coroutine (Consumer, Process, connect, consumer, runProcess)
-import Control.Monad.Reader.Trans (runReaderT)
 import Data.Maybe (Maybe(Nothing))
 import Effect (Effect)
 import Effect.Aff (Aff, forkAff)
 import Effect.Class.Console (log)
 import Effect.Unsafe (unsafePerformEffect)
-import Halogen (hoist)
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.VDom.Driver (runUI)
 import LocalStorage (RawStorageEvent)
 import LocalStorage as LocalStorage
 import MainFrame (mkMainFrame)
-import Playground.Server (SPParams_(..))
-import Servant.PureScript.Settings (SPSettings_, defaultSettings)
 import Types (HAction(..))
-
-ajaxSettings :: SPSettings_ SPParams_
-ajaxSettings = defaultSettings $ SPParams_ { baseURL: "/api/" }
 
 main :: Effect Unit
 main = do
   mainFrame <- mkMainFrame
   runHalogenAff do
     body <- awaitBody
-    driver <- runUI (hoist (flip runReaderT ajaxSettings) mainFrame) Mounted body
+    driver <- runUI mainFrame Mounted body
     forkAff $ runProcess watchLocalStorageProcess
 
 watchLocalStorageProcess :: Process Aff Unit
