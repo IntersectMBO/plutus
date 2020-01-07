090e349da2199d57b40bc2d2a8c87381579a54f4
Kris Jenkins <kris.jenkins@tweag.io>
2020-01-07 17:19:27 +0000

    Playground: Making the documentation links more prominent.
    I think it's too hard to notice them, yet they're one of the first
    things you want to find.

--- web-common/src/Bootstrap.purs
+++ web-common/src/Bootstrap.purs
@@ -112,11 +112,20 @@ colXs6 = ClassName "col-xs-6"
 colXs12 :: ClassName
 colXs12 = ClassName "col-xs-12"
 
+colSm4 :: ClassName
+colSm4 = ClassName "col-sm-4"
+
 colSm5 :: ClassName
 colSm5 = ClassName "col-sm-5"
 
 colSm6 :: ClassName
 colSm6 = ClassName "col-sm-6"
+
+colSm7 :: ClassName
+colSm7 = ClassName "col-sm-7"
+
+colSm8 :: ClassName
+colSm8 = ClassName "col-sm-8"
 
 colSm12 :: ClassName
 colSm12 = ClassName "col-sm-12"
@@ -129,6 +138,18 @@ colMd5 = ClassName "col-md-5"
 
 colMd6 :: ClassName
 colMd6 = ClassName "col-md-6"
+
+colMd7 :: ClassName
+colMd7 = ClassName "col-md-7"
+
+colMd8 :: ClassName
+colMd8 = ClassName "col-md-8"
+
+colLg5 :: ClassName
+colLg5 = ClassName "col-lg-5"
+
+colLg7 :: ClassName
+colLg7 = ClassName "col-lg-7"
 
 colAuto :: ClassName
 colAuto = ClassName "col-auto"
