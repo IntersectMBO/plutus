292692bc0ff11183a7d6d338a95bfe9b1a320270
Kris Jenkins <kris.jenkins@tweag.io>
2020-01-13 09:40:16 +0000

    Bumping our Aeson version.
    We were on 1.4.2.0. The error messages were hugely improved in 1.4.3.0,
    so I want at least that version. (And while I'm here, I might as well go
    to 1.4.6.0., which is the latest minor version.)

--- lib.nix
+++ lib.nix
@@ -44,7 +44,7 @@ let
 
   isPlutus = name: builtins.elem name plutusPkgList;
 
-  regeneratePackages = iohkNix.stack2nix.regeneratePackages { hackageSnapshot = "2019-09-12T00:02:45Z"; };
+  regeneratePackages = iohkNix.stack2nix.regeneratePackages { hackageSnapshot = "2020-01-13T00:00:00Z"; };
 
   unfreePredicate = pkg: 
       if pkg ? name then (builtins.parseDrvName pkg.name).name == "kindlegen" 
