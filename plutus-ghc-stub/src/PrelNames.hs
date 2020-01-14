8f1117f89aa9d438f80f7cb52039338bdce42c29
Moritz Angermann <moritz.angermann@gmail.com>
2020-01-14 10:26:48 +0800

    add more stubs

--- plutus-ghc-stub/src/PrelNames.hs
+++ plutus-ghc-stub/src/PrelNames.hs
@@ -1,1 +1,10 @@
 module PrelNames where
+
+import StubTypes
+
+eqName, eqIntegerPrimName :: Name
+eqName = undefined
+eqIntegerPrimName = undefined
+
+nameOccName :: Name -> OccName
+nameOccName = undefined
