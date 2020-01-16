e4dbb1750aff7c1e7dc2c54ab95721486e7e73fa
James Chapman <james.chapman@iohk.io>
2020-01-16 12:08:34 +0000

    update plc-agda.nix

--- metatheory/plc-agda.nix
+++ metatheory/plc-agda.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, bytestring, Cabal, cryptonite, directory
 , ieee754, language-plutus-core, memory, optparse-applicative
-, plutus-exe, process, stdenv, text
+, plutus-exe, process, stdenv, text, transformers
 }:
 mkDerivation {
   pname = "plc-agda";
@@ -10,15 +10,14 @@ mkDerivation {
   isExecutable = true;
   executableHaskellDepends = [
     base bytestring cryptonite ieee754 language-plutus-core memory
-    optparse-applicative text
+    optparse-applicative text transformers
   ];
   testHaskellDepends = [
     base bytestring Cabal cryptonite directory ieee754
     language-plutus-core memory optparse-applicative process text
+    transformers
   ];
   testToolDepends = [ plutus-exe ];
-  enableLibraryProfiling = false;
-  enableExecutableProfiling = false;
   homepage = "https://github.com/input-output-hk/plutus";
   description = "Command line tool for running plutus core programs";
   license = stdenv.lib.licenses.asl20;
