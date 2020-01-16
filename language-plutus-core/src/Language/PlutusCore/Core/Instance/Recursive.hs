90a64ea94a9b794bb170abbdde1babdb6d2ce32f
Simon Hafner <simon.hafner@iohk.io>
2020-01-16 22:30:05 +0100

    one last stylish change

--- language-plutus-core/src/Language/PlutusCore/Core/Instance/Recursive.hs
+++ language-plutus-core/src/Language/PlutusCore/Core/Instance/Recursive.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
 
 module Language.PlutusCore.Core.Instance.Recursive
     ( -- * Base functors
