52490bb73471dfccf36514828edf1f2a31103021
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2020-01-29 16:28:02 +0100

    Use correct note-comment coding style.

--- plutus-ir/src/Language/PlutusIR/Compiler/Let.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler/Let.hs
@@ -32,6 +32,23 @@ this, but this does the job.
 Also we should pull out more stuff (e.g. see 'NonStrict' which uses unit).
 -}
 
+{- Note [Right-associative compilation of let-bindings for linear scoping]
+
+The 'foldM' function for lists is left-associative, but we need right-associative for our case, i.e.
+every right let must be wrapped/scoped by its left let
+
+An pseudocode PIR example:
+let b1 = rhs1;
+    b2 = rhs2  (b1 is visible in rhs2);
+in ...
+
+must be treated the same as let b1 = rhs in (let b2 = rhs2 in ... )
+
+Since there is no 'foldrM' in the stdlib, so we first reverse the bindings list,
+and then apply the left-associative 'foldM' on them,
+which yields the same results as doing a right-associative fold.
+-}
+
 data LetKind = RecTerms | NonRecTerms | Types
 
 -- | Compile the let terms out of a 'Term'. Note: the result does *not* have globally unique names.
@@ -43,13 +60,7 @@ compileLets kind t = getEnclosing >>= \p ->
 compileLet :: Compiling m e a => LetKind -> PIRTerm a -> DefT SharedName (Provenance a) m (PIRTerm a)
 compileLet kind = \case
     Let p r bs body -> withEnclosing (const $ LetBinding r p) $ case r of
-            -- NOTE: 'foldM' for lists is left associative, but we need right associative for our case (every right let must be wrapped/scoped by its left let), example:
-            -- let b1 = rhs1;
-            --     b2 = rhs2  (b1 is visible in rhs2);
-            -- in ...
-            -- must be same as let b1 = rhs in (let b2 = rhs2 in ... )
-            -- There is no 'foldrM' in the stdlib, so we first reverse the bindings list and then left-associative 'foldM' on them,
-            -- which is the same as doing a right-associative fold.
+            -- See Note [Right-associative compilation of let-bindings for linear scoping]
             NonRec -> lift $ foldM (compileNonRecBinding kind) body (reverse bs)
             Rec    -> compileRecBindings kind body bs
     x -> pure x
