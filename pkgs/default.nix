f12b6d5671bc536c6f473ffcd37be0f32d7cd2b5
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-01-31 12:29:07 +0000

    unix-compat-0.5.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -75725,10 +75725,10 @@ license = stdenv.lib.licenses.bsd3;
 mkDerivation {
 
 pname = "unix-compat";
-version = "0.5.1";
-sha256 = "a39d0c79dd906763770b80ba5b6c5cb710e954f894350e9917de0d73f3a19c52";
+version = "0.5.2";
+sha256 = "659a4e442f71505d45b0d0fb28a347aa1ac5e1f39feaea5d58e674e5d2ce0ba9";
 revision = "1";
-editedCabalFile = "0c4ak3y72nhv3nf025cyfi1dri2wjmsal3icjwj0aczmlb0imwrd";
+editedCabalFile = "1yx38asvjaxxlfs8lpbq0dwd84ynhgi7hw91rn32i1hsmz7yn22m";
 libraryHaskellDepends = [
 base
 unix
