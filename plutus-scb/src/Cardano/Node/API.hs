1637a0e59c70fba4e0f7355211a45e800c0c0fdf
Jann Müller <j-mueller@users.noreply.github.com>
2020-02-13 15:37:07 +0100

    SCB: Work on mock node (#1841)
    * Make compile
    
    * Add Plutus.SCV.Events.Node
    
    * Add a tx to the node's mempool
    
    * Log new transactions
    
    * node server: Make some values configurable
    
    * activitySimulator -> slotCoordinator
    
    * node server: Use chain state instead of emulator state; freer-simple
    
    * mock node: Add a random tx generator
    
    * Warnings
    
    * Use initial distribution
    
    * node server: Periodically discard old blocks

--- plutus-scb/src/Cardano/Node/API.hs
+++ plutus-scb/src/Cardano/Node/API.hs
@@ -5,10 +5,11 @@ module Cardano.Node.API
     ( API
     ) where
 
-import           Ledger      (Slot)
-import           Servant.API ((:<|>), (:>), Get, JSON, NoContent, Put)
+import           Ledger      (Tx, Slot)
+import           Servant.API ((:<|>), (:>), Get, JSON, NoContent, ReqBody, Post)
 
 type API
      = "healthcheck" :> Get '[ JSON] NoContent
-       :<|> "block" :> (Get '[ JSON] Slot
-                        :<|> "add" :> Put '[ JSON] Slot)
+       :<|> "mempool" :> ReqBody '[ JSON] Tx :> Post '[ JSON] NoContent
+       :<|> "slot" :> Get '[ JSON] Slot
+       :<|> "random-tx" :> Get '[ JSON] Tx
