7dfb21b24c17502f3feec0995cb2f789439bcef4
Kris Jenkins <kris.jenkins@tweag.io>
2020-02-14 12:36:59 +0000

    SCB: Implementing the mock wallet's watched address map support.

--- plutus-scb/src/Cardano/Wallet/Client.hs
+++ plutus-scb/src/Cardano/Wallet/Client.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString.Lazy   as BSL
 import           Data.Function          ((&))
 import           Data.Proxy             (Proxy (Proxy))
 import           Ledger                 (PubKey, Signature, Value)
+import           Ledger.AddressMap      (AddressMap)
 import           Network.HTTP.Client    (defaultManagerSettings, newManager)
 import           Servant.Client         (ClientM, client, mkClientEnv, parseBaseUrl, runClientM)
 import           Servant.Extra          (left, right)
@@ -15,18 +16,25 @@ import           Wallet.Emulator.Wallet (Wallet)
 
 selectCoins :: WalletId -> Value -> ClientM ([Value], Value)
 allocateAddress :: WalletId -> ClientM PubKey
+getWatchedAddresses :: ClientM AddressMap
 getWallets :: ClientM [Wallet]
 getOwnPubKey :: ClientM PubKey
 sign :: BSL.ByteString -> ClientM Signature
-(getWallets, getOwnPubKey, sign, selectCoins, allocateAddress) =
-    (getWallets_, getOwnPubKey_, sign_, selectCoins_, allocateAddress_)
+(getWallets, getOwnPubKey, sign, getWatchedAddresses, selectCoins, allocateAddress) =
+    ( getWallets_
+    , getOwnPubKey_
+    , sign_
+    , getWatchedAddresses_
+    , selectCoins_
+    , allocateAddress_)
   where
     api = client (Proxy @API)
-    getWallets_ = left api
-    active_ = right api & left
-    getOwnPubKey_ = left active_
-    sign_ = right active_
-    byWalletId =  right api & right
+    getWallets_ = api & left
+    active_ = api & right & left
+    getOwnPubKey_ = active_ & left
+    sign_ = active_ & right & left
+    getWatchedAddresses_ = active_ & right & right
+    byWalletId = api & right & right
     selectCoins_ walletId = byWalletId walletId & left
     allocateAddress_ walletId = byWalletId walletId & right
 
