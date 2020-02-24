3481240bf4b8958e45df2290e48bffaa21bd4c28
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2020-02-24 12:56:13 +0100

    Fix PIR-Free.hs after pair programming.

--- plutus-ir/src/Language/PlutusIR/Analysis/Free.hs
+++ plutus-ir/src/Language/PlutusIR/Analysis/Free.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase       #-}
-
+{-# LANGUAGE ScopedTypeVariables #-}
+-- | Functions for computing the free (type-)variables of a PIR term or type.
 module Language.PlutusIR.Analysis.Free (fTerm, fType) where
 
 import           Language.PlutusIR
@@ -14,67 +15,111 @@ import           Control.Lens
 
 import qualified Data.Set                  as S
 
+{- Note: [PIR Free variables]
+
+The functions of this module behave similar to those found in 'language-plutus-core/src/Language/PlutusCore/Subst.hs',
+with the difference:
+
+- extended to support the plutus-ir (letrec and letnonrec)
+- place the free variables and free type variables in the same set of uniques.
+
+The functions do not require that the input term be prior 'renamed'/'uniqueified'.
+-}
+
+
+{- Note [Free variables and letnonrec linear scope]
+
+Let (nonrec) is linearly scoped, as shown by the example:
+
+let b1 = rhs1;
+    b2 = rhs2  (b1 is visible in rhs2);
+in ...
+
+is the same as as let b1 = rhs in (let b2 = rhs2 in ... )
+
+And is different than letrec where all identifiers are visible/scoped:
+
+let b1 = rhs1 (b2 is visible in rhs1);
+    b2 = rhs2  (b1 is visible in rhs2);
+in ...
+
+
+In this regard, extra care has been taken when counting the free variables of a letnorec vs a letrec, as
+shown by the example:
+
+(let nonrec { x = y
+              y = 3 }
+) ===> y is free in x
+
+VS
+
+(let rec { x = y
+           y = 3 }
+) ===> y is not free in x
+
+-}
+
+
 -- | Given a term, it returns a set of all the FREE variables inside that term (i.e. not declared/scoped inside the term).
+--
+-- It does not require the input term to be prior 'PLC.rename'd.
 -- TODO: refactor using recursion-schemes (or usingtermSubterms, termSubtypes can be used and lens + fold), see 'language-plutus-core/src/Language/PlutusCore/Subst.hs'
-fTerm ::
+-- See Note: [PIR Free variables]
+fTerm :: forall name tyname a.
   (Ord (tyname a), PLC.HasUnique (tyname a) PLC.TypeUnique, PLC.HasUnique (name a) PLC.TermUnique)
   => Term tyname name a -> S.Set PLC.Unique
 fTerm = \case
   Var _ x -> S.singleton $ x ^. PLC.unique . coerced
 
-  -- linear scoping on the let non-rec
-  -- we don't assume term is renamed, that is why we do this scoping-accumulation for name shadowing
-  Let _ NonRec bs tIn -> snd $ foldl (\(accLinearScope,accFreeSet) -> \case
-                              TermBind _ _ (VarDecl _ n ty) tRhs -> ( S.insert (n ^. PLC.unique . coerced) accLinearScope
-                                                                     , (S.union (fType ty) (fTerm tRhs) S.\\ accLinearScope) `S.union` accFreeSet
-                                                                     )
-                              DatatypeBind _ dt@(Datatype _ _ _ _ vdecls) -> (
-                                -- new linear scope is the introduced identifiers plus the old linear scope
-                                -- add to the next linear scope, all the newly introduced identifiers
-                                datatypeIds dt `S.union` accLinearScope,
-
-                                -- all Ty occurences of this databind - the previous linear scope
-                                (S.unions (fmap (\(VarDecl _ _ ty) -> fType ty) vdecls) S.\\ accLinearScope) `S.union` accFreeSet
-                                )
-                              TypeBind _ (TyVarDecl _ n _k) tyRhs -> ( S.insert (n ^. PLC.unique . coerced) accLinearScope
-                                                                     , (fType tyRhs S.\\ accLinearScope) `S.union` accFreeSet
-                                                                     )
-                                     )
-                             ( S.empty,
-                               fTerm tIn
-                               S.\\   -- removes all the identifiers introduced by this letnonrec
-                               (foldMap (\case
-                                            TermBind _ _ (VarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced)
-                                            DatatypeBind _ dt -> datatypeIds dt
-                                            TypeBind _ (TyVarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced))
-                                bs)
-                             ) bs
-
-  -- all variables in the letrec are scoped
-  Let _ Rec bs tIn -> foldl (\acc -> \case
-                              TermBind _ _ (VarDecl _ _ ty) tRhs -> S.unions [fType ty, fTerm tRhs, acc]
-                              DatatypeBind _ (Datatype _ _ _ _ vdecls) -> S.unions $ fmap (\(VarDecl _ _ ty) -> fType ty) vdecls
-                              TypeBind _ (TyVarDecl _ _ _k) tyRhs -> fType tyRhs `S.union` acc
-                            ) (fTerm tIn) bs
-                      S.\\      -- removes all the variable names introduced by this letrec
-                      -- TODO: duplicate code
-                      (foldMap (\case
-                              TermBind _ _ (VarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced)
-                              DatatypeBind _ dt -> datatypeIds dt
-                              TypeBind _ (TyVarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced))
-                       bs)
+  Let _ NonRec bs tIn -> fLet bs tIn NonRec
+  Let _ Rec bs tIn -> fLet bs tIn Rec
 
   TyAbs _ n _ t -> S.delete (n ^. PLC.unique . coerced) $ fTerm t
   LamAbs _ n ty t -> fType ty
-                     `S.union`
+                     <>
                      S.delete (n ^. PLC.unique . coerced) (fTerm t)
-  Apply _ t1 t2 -> fTerm t1 `S.union` fTerm t2
-  TyInst _ t ty -> fTerm t `S.union` fType ty
-  IWrap _ ty1 ty2 t -> S.unions [fType ty1, fType ty2, fTerm t]
+  Apply _ t1 t2 -> fTerm t1 <> fTerm t2
+  TyInst _ t ty -> fTerm t <> fType ty
+  IWrap _ ty1 ty2 t -> mconcat [fType ty1, fType ty2, fTerm t]
   Unwrap _ t -> fTerm t
   _ -> S.empty
 
+ where
+  fLet :: [Binding tyname name a] -> Term tyname name a -> Recursivity -> S.Set PLC.Unique
+  fLet bs tIn =
+    let fIn = fTerm tIn S.\\ foldMap bindingIds bs -- the free variables of termIn (scoped by all the let id's)
+    in \case
+      Rec -> (foldMap fBinding bs S.\\ foldMap bindingIds bs) <> fIn -- all id's in the letrec can be seen (scoped) by each other
+      NonRec -> let initialScope = S.empty -- starting with an empty linear-scope
+                    initialFreeSet = fIn -- starting from the freeset found in tIn
+                in snd $ foldl nonRecVarAcc (initialScope, initialFreeSet) bs
 
+  -- it threads an ever-increasing linear-scope and an accumulated set of free variables found, i.e. (linearScope, freeSet)
+  -- See Note [Free variables and letnonrec linear scope]
+  -- See Note [Right-associative compilation of let-bindings for linear scoping]
+  nonRecVarAcc :: (S.Set PLC.Unique, S.Set PLC.Unique) -> Binding tyname name a -> (S.Set PLC.Unique, S.Set PLC.Unique)
+  nonRecVarAcc (accLinearScope,accFreeSet) b =
+    let newScope = bindingIds b <> accLinearScope
+        newFreeSet = (fBinding b S.\\ accLinearScope) <> accFreeSet
+    in (newScope, newFreeSet)
+
+  fBinding ::  Binding tyname name a -> S.Set PLC.Unique
+  fBinding = \case
+    TermBind _ _ (VarDecl _ _ ty) tRhs -> fType ty <> fTerm tRhs
+    DatatypeBind _ (Datatype _ _ _ _ constrs) -> foldMap fVarDecl constrs
+    TypeBind _ (TyVarDecl _ _ _k) tyRhs -> fType tyRhs
+
+  fVarDecl :: VarDecl tyname name a -> S.Set PLC.Unique
+  fVarDecl (VarDecl _ _ ty) = fType ty
+
+  bindingIds :: Binding tyname name a -> S.Set PLC.Unique
+  bindingIds = \case
+    TermBind _ _ (VarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced)
+    DatatypeBind _ dt -> datatypeIds dt
+    TypeBind _ (TyVarDecl _ n _) _ -> S.singleton (n^.PLC.unique.coerced)
+
+-- | Given a PIR type, it returns the free type variables of that type.
+-- See Note: [PIR Free variables]
 fType :: (Ord (tyname a), PLC.HasUnique (tyname a) PLC.TypeUnique)
   => Type tyname a -> S.Set PLC.Unique
 fType = S.map (^. PLC.unique . coerced) . ftvTy
