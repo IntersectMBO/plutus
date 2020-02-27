cc27c29aceb038143ed97515b47e6e612363ca79
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2020-02-27 18:25:36 +0100

    LetFloat: Refactor 2nd pass

--- plutus-ir/src/Language/PlutusIR/Transform/LetFloat.hs
+++ plutus-ir/src/Language/PlutusIR/Transform/LetFloat.hs
@@ -91,8 +91,8 @@ removeLets = flip runState M.empty . go
          -- this overrides the 'termSubterms' functionality only for the 'Let' constructor
          Let a r bs tIn -> do
            forM_ bs $ \ b -> do
-                               b' <- bindingSubterms go b
-                               modify (M.insert (b'^.princUnique) (a, r, b'))
+             b' <- bindingSubterms go b
+             modify (M.insert (b'^.princUnique) (a, r, b'))
            go tIn
          t -> termSubterms go t
 
@@ -120,24 +120,18 @@ p1Term pir = runReader (goTerm pir) (M.empty, 0)
       Unwrap _ t -> goTerm t
 
       -- "ground" terms (terms that do not contain other terms)
-      _ -> asks fst -- propagate the ctx's rankdata back upwards as the return value
+      _ -> asks fst -- the ctx's rankdata is propagated back upwards as the return value
 
     goBody :: PLC.HasUnique b b' => b -> Term tyname name a -> Reader Ctx RankData
     goBody n tBody =
       let lamName= n^.PLC.unique.coerced
-      in M.delete lamName -- we don't care about lambdas in RankData
-         <$> (local (\ (rd,depth1) ->
-                        let newDepth = depth1+1
+      in M.delete lamName -- this rick removes any lamrank from the final value, since we don't care about lambdas in the final pass1 result (RankData)
+         <$> (local (\ (scope,oldDepth) ->
+                        let newDepth = oldDepth+1
                             newRank = LamDep newDepth lamName
-                        in (M.insert lamName (newRank,lamName) rd, newDepth)
-                    ) $ goTerm tBody)
-
-    computeMaxRanks freeVars bs = do
-      (scope, _) <- ask
-      let freeRanks = fmap fst $ M.restrictKeys scope freeVars
-          maxRank = fold freeRanks --shared for all bindings of Rec
-          newScope = foldr (\ b -> M.insert (b^.princUnique) (maxRank, b^.princUnique)) scope bs
-      pure newScope
+                            newScope = M.insert lamName (newRank,lamName) scope -- adds a new lam rank to the current scope
+                        in (newScope, newDepth))
+                    $ goTerm tBody)
 
     goRec :: [Binding tyname name a] -> Term tyname name a -> Reader Ctx RankData
     goRec bs tIn = do
@@ -145,7 +139,7 @@ p1Term pir = runReader (goTerm pir) (M.empty, 0)
         -- excluding the newly-introduced binding identifiers of this letrec
         let freeVars = foldMap fBinding bs S.\\ foldMap bindingIds bs
         -- all bindings share their commonly-maximum rank
-        newScope <- computeMaxRanks freeVars bs
+        newScope <- addRanks bs freeVars
         resBs <- mconcat <$> forM (bs^..traverse.bindingSubterms)
                                   (withScope newScope . goTerm)
         resIn <- withScope newScope $ goTerm tIn
@@ -156,58 +150,108 @@ p1Term pir = runReader (goTerm pir) (M.empty, 0)
       foldr (\ b acc -> do
            -- this means that we see each binding individually, not at the whole let level
            let freeVars = fBinding b
-           -- compute a maxrank for this binding and a newscope that includes it
-           newScope <- computeMaxRanks freeVars (S.singleton b)
+           -- compute a maxrank for this binding alone and a newscope that includes it
+           newScope <- addRanks [b] freeVars
            resHead <- mconcat <$> forM (b^..bindingSubterms) goTerm
            resRest <- withScope newScope $ acc
            pure $ resHead <> resRest
        ) (goTerm tIn) bs
 
+
+    -- | Given a set of bindings and the union of their free-variables,
+    -- compute their maximum,common free-variable (Rank),
+    -- and rank these bindings by adding them to the current scope (RankData)
+    addRanks :: [Binding tyname name a] -- ^ bindings
+             -> S.Set PLC.Unique -- ^ their free-vars
+             -> Reader Ctx RankData -- ^ the updated scope that includes the added ranks
+    addRanks bs freeVars = do
+      (scope, _) <- ask
+      let
+          -- the ranks of the free variables
+          freeRanks = fmap fst $ M.restrictKeys scope freeVars
+          -- the max rank from all their free variables
+          maxRank = fold freeRanks
+          -- this computed max rank is linked/associated to all the given bindings and added to the current scope
+          newScope = foldr (\ b -> M.insert (b^.princUnique) (maxRank, b^.princUnique)) scope bs
+      pure newScope
+
+
     withScope :: RankData -> Reader Ctx b -> Reader Ctx b
     withScope s = local (set _1 s) -- changes only the scope (i.e. rankdata in our case)
 
 
+-- OPTIMIZE: We could potentially replace the pure descending of DepthData (Reader DepthData) to a State DepthData. In such a case
+-- this transformation might stop earlier, when the state becomes empty: i.e. no let-rhses left to float.
 floatTerm :: forall name tyname a.
              (Ord (tyname a), PLC.HasUnique (tyname a) PLC.TypeUnique, PLC.HasUnique (name a) PLC.TermUnique, Monoid a)
              => Term tyname name a -> Term tyname name a
-floatTerm pir = floatBody mempty depthData pirClean
+floatTerm pir =
+    let topLoc = mempty  -- starting from the top-level
+    in goBody topLoc depthData pirClean -- start by trying to float some lets at the top-level
  where
-  depthData = IM.toAscList $ toDepthData $ p1Term pir -- run the first pass
-  -- Visiting a term to apply the float transformation
-  visitTerm curRank remInfo = \case
-      LamAbs a n ty tBody -> LamAbs a n ty (floatAbs n tBody)
-      TyAbs a n k tBody  -> TyAbs a n k (floatAbs n tBody)
-      t -> over termSubterms (visitTerm curRank remInfo) t -- descend
-    where
-      floatAbs :: PLC.HasUnique b _c => b -> Term tyname name a -> Term tyname name a
-      floatAbs n tBody =
-        let newRank = LamDep (curRank^.depth+1) (n ^. PLC.unique . coerced)
-        in floatBody newRank remInfo tBody
 
-  -- Special case of 'visitTerm' where we visit a lambda or a Lambda body
-  floatBody _  [] tBody = tBody -- no lets are left to float, do not descend
-  floatBody curRank depthTable@((searchingForDepth, lams_lets):restDepthTable) tBody
-    | curRank^.depth < searchingForDepth = visitTerm curRank depthTable tBody
-    | curRank^.depth == searchingForDepth =
-        let tBody' = visitTerm curRank restDepthTable tBody
-        in case M.lookup (curRank^.name) lams_lets of
-             Just lets -> wrapLets lets curRank restDepthTable tBody'
+  -- | Visiting each term to apply the float transformation
+  goTerm :: Int -- ^ current depth
+         -> [(Int, M.Map PLC.Unique (S.Set PLC.Unique))] -- ^ the lambdas we are searching for (to float some lets inside them)
+         -> Term tyname name a
+         -> Term tyname name a
+  goTerm curDepth searchTable = \case
+      -- we are only interested in lambdas/Lambdas
+      LamAbs a n ty tBody -> LamAbs a n ty $ goAbs curDepth searchTable n tBody
+      TyAbs a n k tBody  -> TyAbs a n k $ goAbs curDepth searchTable n tBody
+      -- descend otherwise to apply the transformations to subterms
+      t -> over termSubterms (goTerm curDepth searchTable) t
+
+  -- | The function just updates the current location (depth+lamname) and visits its body
+  goAbs :: PLC.HasUnique b b'
+        => Int -- ^ current depth
+        -> [(Int, M.Map PLC.Unique (S.Set PLC.Unique))] -- ^ the lambdas we are searching for (to float some lets inside them)
+        -> b                                            -- ^ lambda/Lambda's name
+        -> Term tyname name a                           -- ^ lambda/Lambda's body
+        -> Term tyname name a
+  goAbs oldDepth searchTable n tBody =
+    let newLam = LamDep (oldDepth+1) (n^.PLC.unique.coerced)
+    in goBody newLam searchTable tBody
+
+  -- | We are currently INSIDE (exactly under) a lambda/Lambda body/Top (Top if we are right at the start of the algorithm)
+  -- We try to see if we have some lets to float here based on our 1st-pass-table data (searchTable).
+  goBody :: Rank -- ^ the rank/location of the lambda above that has this body
+         -> [(Int, M.Map PLC.Unique (S.Set PLC.Unique))] -- ^ the lambdas we are searching for (to float some lets inside them)
+         -> Term tyname name a                           -- ^ the body term
+         -> Term tyname name a                           -- ^ the transformed body term
+  goBody _  [] tBody = tBody -- no lets are left to float, do not descend
+  goBody aboveLamLoc searchTable@( (searchingForDepth, searchingForLams_Lets) : restSearchTable) tBody =
+    let curDepth = aboveLamLoc^.depth
+    in case curDepth `compare` searchingForDepth of
+      -- the minimum next depth we are looking for, is not this one, so just descend
+      LT -> goTerm curDepth searchTable tBody
+      -- found depth, see if our lambda above is a lambda we are interested in (to float some lets)
+      EQ ->
+        -- transform the lambody, passing the rest table
+        let tBody' = goTerm curDepth restSearchTable tBody
+        in case M.lookup (aboveLamLoc^.name) searchingForLams_Lets of
+             -- the lambda above-this-body has some let-rhses to insert (float) here.
+             Just lets -> wrapLets lets curDepth restSearchTable tBody'
+             -- nothing to do for the lambda above, i.e. no let-rhses to insert here
              _         -> tBody'
-    | otherwise = error "just for completion"
+      GT -> error "This shouldn't happen, because the algorithm takes care to stop descending when EQ is reached."
 
-  -- TODO: using the above pure/local way has the disadvantage that we visit a bit more than we need. To fix this we can use State DepthDataRemaining instead.
 
-  (pirClean :: Term tyname name a,
-   rhsTable :: RhsTable tyname name a) = removeLets pir
 
-  -- the dependency graph (as the one used by the deadcode elimination)
-  -- but w/ot root node and only uses the name of the var (PLC.Unique) as the node id
+  depthData :: [(IM.Key, M.Map PLC.Unique (S.Set PLC.Unique))]
+  depthData = IM.toAscList $ toDepthData $ p1Term pir -- run the first pass
+
+
+  (pirClean :: Term tyname name a, rhsTable :: RhsTable tyname name a) = removeLets pir
+
+  -- | the dependency graph (as the one used by the deadcode elimination)
+  -- but w/o the root node and only uses the name of the var (PLC.Unique) as the node id
   depGraph :: AM.AdjacencyMap PLC.Unique
   depGraph = AM.gmap (\case Variable u -> u; _ -> error "just for completion")
              $ AM.removeVertex Root -- we remove Root because we do not care about it
              $ runTermDeps pir
 
-  -- the dependency graph as before, but with datatype-related nodes merged/grouped under a single node per datatypebind a, see 'bindingUnique'.
+  -- | the dependency graph as before, but with datatype-related nodes merged/grouped under a single node per datatypebind a, see 'bindingUnique'.
   reducedDepGraph :: AM.AdjacencyMap PLC.Unique
   reducedDepGraph = M.foldr (\ (_,_,b) accGraph ->
                               case b of
@@ -215,38 +259,49 @@ floatTerm pir = floatBody mempty depthData pirClean
                                                      in foldr (\ assocB -> AM.replaceVertex assocB princ) accGraph $ datatypeIds dt
                                 _ -> accGraph) depGraph rhsTable
 
-  -- take the strongly-connected components of the reduced dep graph, because it may contain loops (introduced by the LetRecs)
+  -- |take the strongly-connected components of the reduced dep graph, because it may contain loops (introduced by the LetRecs)
   -- topologically sort these sccs, since we rely on linear (sorted) scoping in our 'wrapLets' code generation
   topSortedSccs :: [AMN.AdjacencyMap PLC.Unique]
   topSortedSccs = fromMaybe (error "Cycle detected in the depgraph. This shouldn't happen in the first place.") $ AM.topSort $ AM.scc reducedDepGraph
 
-  -- | Tries to wrap a given term with newly-generated Let expression(s), essentially floating some Let-Rhses.
-  -- The given set of lets is not sorted w.r.t. linear scoping, so this function uses the 'topSortedSccs' of the dependency graph,
-  -- to figure out the order in which to generate those new Lets.
+  -- | Groups a given set of lets into one or more multilets and wraps these multilets around a given term.
+  -- The grouping is done through the strongly-connected components
+  -- The input lets is not sorted w.r.t. linear scoping, so this function uses the topological-sort of these SCCs
+  -- to figure out the correct (dependend/linear) order in which to generate these new multilets.
   --
-  -- The resulting term is wrapped with linear-scope-sorted LetRecs and LetNonRecs (interspersed between each other because letnonrec and letrec syntax cannot be combined)
+  -- Note that the resulting term is wrapped with linear-scope-sorted LetRecs and LetNonRecs (interspersed between each other because letnonrec and letrec syntax cannot be combined)
   -- Example: `let {i = e, ...} in let rec {j = e, ...} in let rec {...} in let {...} in ..... in originalTerm`
-  wrapLets :: S.Set PLC.Unique -- ^ the lets to wrap around a term
-           -> Rank -- ^ current rank
+  wrapLets :: S.Set PLC.Unique -- ^ all the let identifiers to wrap around this term
+           -> Int -- ^ current depth
            -> [(Int, M.Map PLC.Unique (S.Set PLC.Unique))] -- ^ the table remaining
            -> Term tyname name a                           -- ^ the term to be wrapped
            -> Term tyname name a                           -- ^ the final wrapped term
-  wrapLets lets curRank restDepthTable t =
-    foldl (\ acc dcc ->
-             let vs = AMN.vertexSet dcc
-             in if vs `S.isSubsetOf` lets -- mandatory check to see if this scc belongs to this rank
-                then
-                  let newBindings = fmap (\ v -> over bindingSubterms (visitTerm curRank restDepthTable) ((rhsTable M.! v)^._3)) $ S.toList vs
-                      (mAnn, mRecurs) = foldMap (\ v -> let b = rhsTable M.! v in (b^._1,b^._2)) vs
-                  in case acc of
-                       Let accAnn NonRec accBs accIn | mRecurs == NonRec ->
-                         -- merge current let-group with previous let-group if both groups' recursivity is nonrec
-                         Let (accAnn <> mAnn) NonRec (newBindings ++ accBs) accIn
-                       _ ->
-                         -- never merge if the previous let-group is a Rec or the current let-group is Rec,
-                         -- but nest the current let-group under the previous let-group (above)
-                         Let mAnn mRecurs newBindings acc
-               else acc -- skip
+  wrapLets lets curDepth restDepthTable t =
+    foldl (\ acc scc ->
+             let vs = AMN.vertexSet scc
+             in if vs `S.isSubsetOf` lets -- mandatory check to see if this scc belongs to this rank, because we fold over all sccs
+                then -- Recursively float all terms of RHSes.
+                     -- Group the transformed RHSes into a new let-group (since they depend on each other).
+                     -- The group gets a common annotation, recursion and a GROUPING (i.e. list) of transformed bindings of the RHSes
+                     let (mAnn, mRecurs, newBindings) =
+                           foldMap (\ v ->
+                                       case M.lookup v rhsTable of
+                                         Just rhs ->
+                                           -- lift each resulting binding in a list monoid to accumulate them
+                                           over _3 pure $
+                                           -- visit the generated rhs-term as well for any potential floating
+                                           over (_3.bindingSubterms)
+                                                (goTerm curDepth restDepthTable)
+                                                rhs
+                                         _ -> error "Something went wrong: no rhs was found for this let in the rhstable."
+                                   ) vs
+                     in case acc of
+                       -- merge current let-group with previous let-group iff both groups' recursivity is NonRec
+                       Let accAnn NonRec accBs accIn | mRecurs == NonRec -> Let (mAnn <> accAnn) NonRec (newBindings <> accBs) accIn
+                       -- never merge if the previous let-group is a Rec or the current let-group is Rec,
+                       -- but instead create a nested current let-group under the previous let-group (above)
+                       _ -> Let mAnn mRecurs newBindings acc
+               else acc -- skip this scc, we are not interested in it right now
              ) t topSortedSccs
 
 
@@ -254,7 +309,7 @@ floatTerm pir = floatBody mempty depthData pirClean
 ----------
 
 toDepthData :: RankData -> DepthData
-toDepthData = M.foldr (\ (dep, letNamePrinc) acc -> IM.insertWith (M.unionWith S.union) (dep^.depth) (M.singleton (dep^.name) (S.singleton letNamePrinc)) acc) IM.empty
+toDepthData = foldr (\ (dep, letNamePrinc) acc -> IM.insertWith (M.unionWith (<>)) (dep^.depth) (M.singleton (dep^.name) (S.singleton letNamePrinc)) acc) IM.empty
 
 -- | Returns a single 'Unique' for a particular binding.
 -- We need this because datatypebinds introduce multiple identifiers, but we need only one as a key of our 'RhsTable',etc. See also: 'datatypeIdentifiers'
