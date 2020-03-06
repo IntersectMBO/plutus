cfa2d4f38bf19dff3134b7d16bde51609d8f71cd
Jann Müller <jann.mueller@iohk.io>
2020-03-06 10:57:21 +0100

    scb: Use node follower protocol in chain index

--- plutus-scb/src/Cardano/ChainIndex/Server.hs
+++ plutus-scb/src/Cardano/ChainIndex/Server.hs
@@ -19,14 +19,15 @@ import qualified Control.Monad.Freer.State     as Eff
 import           Control.Monad.Freer.Writer
 import qualified Control.Monad.Freer.Writer    as Eff
 import           Control.Monad.IO.Class        (MonadIO (..))
-import           Control.Monad.Logger          (MonadLogger, logInfoN)
+import           Control.Monad.Logger          (MonadLogger, logDebugN, logInfoN, runStdoutLoggingT)
+import           Data.Foldable                 (traverse_)
 import           Data.Proxy                    (Proxy (Proxy))
 import qualified Data.Sequence                 as Seq
 import           Data.Time.Units               (Second, toMicroseconds)
 import           Network.HTTP.Client           (defaultManagerSettings, newManager)
 import           Network.Wai.Handler.Warp      (run)
 import           Servant                       ((:<|>) ((:<|>)), Application, NoContent (NoContent), hoistServer, serve)
-import           Servant.Client                (BaseUrl (baseUrlPort), ClientEnv, mkClientEnv)
+import           Servant.Client                (BaseUrl (baseUrlPort), ClientEnv, mkClientEnv, runClientM)
 
 import           Ledger.Address                (Address)
 import           Ledger.AddressMap             (AddressMap)
@@ -34,8 +35,10 @@ import           Plutus.SCB.Utils              (tshow)
 
 import           Cardano.ChainIndex.API
 import           Cardano.ChainIndex.Types
+import qualified Cardano.Node.Client           as NodeClient
 import           Wallet.Emulator.ChainIndex    (ChainIndexEffect, ChainIndexEvent, ChainIndexState)
 import qualified Wallet.Emulator.ChainIndex    as ChainIndex
+import           Wallet.Emulator.NodeClient    (Notification (..))
 
 -- $chainIndex
 -- The SCB chain index that keeps track of transaction data (UTXO set enriched
@@ -59,7 +62,7 @@ main ChainIndexConfig{ciBaseUrl} nodeBaseUrl = do
             pure $ mkClientEnv nodeManager nodeBaseUrl
     mVarState <- liftIO $ newMVar initialAppState
     logInfoN "Starting node client thread"
-    void $ liftIO $ forkIO $ updateThread 10 mVarState nodeClientEnv
+    void $ liftIO $ forkIO $ runStdoutLoggingT $ updateThread 10 mVarState nodeClientEnv
     liftIO $ run port $ app mVarState
 
 healthcheck :: Monad m => m NoContent
@@ -73,7 +76,7 @@ watchedAddresses = ChainIndex.watchedAddresses
 
 -- | Get the latest transactions from the node and update the index accordingly
 updateThread ::
-    MonadIO m
+    (MonadIO m, MonadLogger m)
     => Second
     -- ^ Interval between two queries for new blocks
     -> MVar AppState
@@ -81,9 +84,17 @@ updateThread ::
     -> ClientEnv
     -- ^ Servant client for the node API
     -> m ()
-updateThread updateInterval mv _ =
+updateThread updateInterval mv nodeClientEnv = do
+    logInfoN "Obtaining follower ID"
+    followerID <- liftIO $ runClientM NodeClient.newFollower nodeClientEnv >>= either (error . show) pure
+    logInfoN $ "Follower ID: " <> tshow followerID
     forever $ do
-        void $ processIndexEffects mv (pure ())
+        logInfoN "Asking the node for new blocks"
+        newBlocks <- liftIO $ runClientM (NodeClient.getBlocks followerID) nodeClientEnv >>= either (error . show) pure
+        logInfoN $ "Received " <> tshow (length newBlocks) <> " blocks"
+        logDebugN $ tshow newBlocks
+        let notifications = BlockValidated <$> newBlocks
+        traverse_ (processIndexEffects mv . ChainIndex.chainIndexNotify) notifications
         liftIO $ threadDelay $ fromIntegral $ toMicroseconds updateInterval
 
 type ChainIndexEffects m
