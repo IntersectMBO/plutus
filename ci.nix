88a1696d8cb6e5549e8b94e5b765e5f4746eccd8
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-03-16 18:11:11 +0000

    Use dimension in ci.nix

--- ci.nix
+++ ci.nix
@@ -1,19 +1,24 @@
 let
-  # TODO: make the system/attribute mapping nicer
-  systemBuilds = system:
-    let
-      packageSet = import ./default.nix { inherit system; };
-      pkgs = packageSet.pkgs;
-      lib = pkgs.lib;
-      collectComponents' = pkgs.haskell-nix.haskellLib.collectComponents';
-    in pkgs.recurseIntoAttrs {
-      libs = collectComponents' "library" packageSet.haskell.projectPackages;
-      tests = collectComponents' "tests" packageSet.haskell.projectPackages;
-      benchmarks = collectComponents' "benchmarks" packageSet.haskell.projectPackages;
-      exes = collectComponents' "exes" packageSet.haskell.projectPackages;
-      checks = pkgs.recurseIntoAttrs (builtins.mapAttrs (_: p: p.checks) packageSet.haskell.projectPackages);
+  inherit (import ./nix/dimension.nix) dimension;
+  # Darwin builds are not working on Hercules right now for some reason
+  systems = {"x86_64-linux" = {};}; #"x86_64-darwin" = {};};
+in dimension "System" systems (system: _:
+  let
+    packageSet = import ./default.nix { inherit system; };
+    pkgs = packageSet.pkgs;
+    lib = pkgs.lib;
+    collectChecks = _: ps: pkgs.recurseIntoAttrs (builtins.mapAttrs (_: p: p.checks) ps);
+    collectComponents = type: ps: packageSet.pkgs.haskell-nix.haskellLib.collectComponents' type ps;
+  in
+    dimension
+      "Haskell component"
+      {"library" = collectComponents; "tests" = collectComponents; "benchmarks" = collectComponents; "exes" = collectComponents; "checks" = collectChecks;}
+      (type: selector: (selector type) packageSet.haskell.projectPackages)
+    //
+    {
       dev = pkgs.recurseIntoAttrs packageSet.dev.packages;
-    } // (lib.optionalAttrs (system == linux) {
+    }
+    // lib.optionalAttrs (system == "x86_64-linux") {
       # Marlowe lambda builds with musl, and only on linux
       marlowe-symbolic-lambda = packageSet.marlowe-symbolic-lambda;
       # Need to list this manually to work around https://github.com/input-output-hk/haskell.nix/issues/464
@@ -23,9 +28,4 @@ let
         test-plc-agda = packageSet.haskell.packages.plc-agda.components.tests.test-plc-agda;
         test2-plc-agda = packageSet.haskell.packages.plc-agda.components.tests.test2-plc-agda;
       };
-    });
-  linux = "x86_64-linux";
-  darwin = "x86_64-darwin";
-  # Darwin builds are not working on Hercules right now for some reason
-  systems = [linux ];
-in builtins.listToAttrs (builtins.map (system: { name = system; value = systemBuilds system; }) systems)
+    })
