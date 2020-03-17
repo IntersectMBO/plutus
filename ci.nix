1c5682beed381da57c6e95b9803e623ad6371b2b
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-03-17 12:17:00 +0000

    I think the metatheory stuff works on darwin now

--- ci.nix
+++ ci.nix
@@ -16,15 +16,14 @@ in dimension "System" systems (system: _:
     //
     {
       dev = pkgs.recurseIntoAttrs packageSet.dev.packages;
-    }
-    // lib.optionalAttrs (system == "x86_64-linux") {
-      # Marlowe lambda builds with musl, and only on linux
-      marlowe-symbolic-lambda = packageSet.marlowe-symbolic-lambda;
       # Need to list this manually to work around https://github.com/input-output-hk/haskell.nix/issues/464
-      # Doesn't work on darwin
       metatheory = pkgs.recurseIntoAttrs {
         plc-agda = packageSet.haskell.packages.plc-agda.components.exes.plc-agda;
         test-plc-agda = packageSet.haskell.packages.plc-agda.components.tests.test-plc-agda;
         test2-plc-agda = packageSet.haskell.packages.plc-agda.components.tests.test2-plc-agda;
       };
+    }
+    // lib.optionalAttrs (system == "x86_64-linux") {
+      # Marlowe lambda builds with musl, and only on linux
+      marlowe-symbolic-lambda = packageSet.marlowe-symbolic-lambda;
     })
