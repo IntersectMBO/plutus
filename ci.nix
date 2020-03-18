0a6188560e13e7d48183c68d5eba01032f121965
Moritz Angermann <moritz.angermann@gmail.com>
2020-03-18 22:19:05 +0800

    Move Plutus to haskell.nix (#1825)
    * Move to haskell.nix and ci.nix for Hercules
    
    Not everything is in ci.nix yet, but we're moving that way.
    
    * Start tidying things and splitting up default.nix a bit
    
    * Comment about shas
    
    * Try to make things more lorri-friendly
    
    Filter more, explicit references to default.nix files rather than
    folders.
    
    Crucially, we need to avoid `cleanGit`...
    
    * Use dimension in ci.nix
    
    * Partially revert source filtering
    
    * Fix stupid error
    
    * Finally nail source filtering
    
    * Filter out dimension too
    
    * Try re-enabling darwin again
    
    * I think the metatheory stuff works on darwin now
    
    * Simplify workaround for plc-agda
    
    * Improve docs
    
    * Fix (ish) plc-agda test runs
    
    * Improve caching by naming src
    
    * And the other one
    
    Co-authored-by: Michael Peyton Jones <michael.peyton-jones@iohk.io>

--- ci.nix
+++ ci.nix
@@ -1,1 +1,23 @@
-{ }
+let
+  inherit (import ./nix/dimension.nix) dimension;
+  systems = {"x86_64-linux" = {}; "x86_64-darwin" = {};};
+in dimension "System" systems (system: _:
+  let
+    packageSet = import ./default.nix { inherit system; };
+    pkgs = packageSet.pkgs;
+    lib = pkgs.lib;
+    collectChecks = _: ps: pkgs.recurseIntoAttrs (builtins.mapAttrs (_: p: p.checks) ps);
+    collectComponents = type: ps: packageSet.pkgs.haskell-nix.haskellLib.collectComponents' type ps;
+  in
+    dimension
+      "Haskell component"
+      {"library" = collectComponents; "tests" = collectComponents; "benchmarks" = collectComponents; "exes" = collectComponents; "checks" = collectChecks;}
+      (type: selector: (selector type) packageSet.haskell.projectPackages)
+    //
+    {
+      dev = pkgs.recurseIntoAttrs packageSet.dev.packages;
+    }
+    // lib.optionalAttrs (system == "x86_64-linux") {
+      # Marlowe lambda builds with musl, and only on linux
+      marlowe-symbolic-lambda = packageSet.marlowe-symbolic-lambda;
+    })
