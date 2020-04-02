a6c2aa68064d85d061d5da0466d8b7fefa45a42d
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-04-02 10:03:38 +0100

    Handle supportedSystems in ci.nix
    This is useful for instantiating only for your current system so it
    works.

--- release.nix
+++ release.nix
@@ -21,7 +21,7 @@ in
 
 # The revision passed in by Hydra, if there is one
 let rev = if builtins.isNull plutus then null else plutus.rev;
-    ci = import ./ci.nix;
+    ci = import ./ci.nix { inherit supportedSystems; };
 
 in with (import (fixedNixpkgs + "/pkgs/top-level/release-lib.nix") {
   inherit supportedSystems scrubJobs;
