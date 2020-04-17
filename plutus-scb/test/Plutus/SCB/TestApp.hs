7a196d97c7f35c4b209587ccb25f7686ff64ab62
Jann Mueller <jann.mueller@iohk.io>
2020-04-17 15:20:28 +0200

    parent 4fe1da779baeb4c43fba475f7c6b3029a040404e author Jann Mueller <jann.mueller@iohk.io> 1587129628 +0200 committer Jann Mueller <jann.mueller@iohk.io> 1587464780 +0200
    scb: Multi-agent tests
    
    Comments
    
    Stylish
    
    comment

--- plutus-scb/test/Plutus/SCB/TestApp.hs
+++ plutus-scb/test/Plutus/SCB/TestApp.hs
@@ -18,9 +18,10 @@
 module Plutus.SCB.TestApp
     ( runScenario
     , sync
+    , syncAll
     , TestAppEffects
-    , getFollowerID
     , valueAt
+    , defaultWallet
     ) where
 
 import           Cardano.Node.Follower                         (NodeFollowerEffect, handleNodeFollower)
@@ -32,14 +33,13 @@ import           Cardano.Node.Types                            (AppState, Follow
 import qualified Cardano.Node.Types                            as NodeServer
 import qualified Cardano.Wallet.Mock                           as WalletServer
 import           Control.Concurrent.MVar                       (MVar, newMVar)
-import           Control.Lens                                  (below, iso, makeLenses, view, zoom)
+import           Control.Lens                                  hiding (use)
 import           Control.Monad                                 (void)
 import           Control.Monad.Freer
 import           Control.Monad.Freer.Error                     (Error, handleError, runError, throwError)
 import           Control.Monad.Freer.Extra.Log                 (Log, logDebug, logInfo, runStderrLog)
 import           Control.Monad.Freer.Extra.State               (assign, use)
-import           Control.Monad.Freer.Extras                    (errorToMonadError, handleZoomedError, handleZoomedState,
-                                                                handleZoomedWriter, stateToMonadState, writeIntoState)
+import           Control.Monad.Freer.Extras
 import qualified Control.Monad.Freer.Log                       as EmulatorLog
 import           Control.Monad.Freer.State                     (State, runState)
 import           Control.Monad.Freer.Writer                    (Writer, runWriter)
@@ -50,6 +50,8 @@ import           Data.Aeson                                    as JSON
 import           Data.Aeson.Types                              as JSON
 import           Data.Bifunctor                                (first)
 import           Data.Foldable                                 (toList, traverse_)
+import           Data.Map                                      (Map)
+import qualified Data.Map                                      as Map
 import qualified Data.Sequence                                 as Seq
 import           Data.Text                                     (Text)
 import qualified Data.Text                                     as Text
@@ -67,12 +69,14 @@ import qualified Ledger.AddressMap                             as AM
 import           Plutus.SCB.Command                            ()
 import           Plutus.SCB.Core
 import           Plutus.SCB.Effects.Contract                   (ContractEffect (..))
+import           Plutus.SCB.Effects.ContractTest               (handleContractTest)
 import           Plutus.SCB.Effects.EventLog                   (EventLogEffect, handleEventLogSql, handleEventLogState)
+import           Plutus.SCB.Effects.MultiAgent                 (AgentState, MultiAgentSCBEffect)
+import qualified Plutus.SCB.Effects.MultiAgent                 as SCB.MultiAgent
 import           Plutus.SCB.Effects.UUID                       (UUIDEffect, handleUUIDEffect)
 import           Plutus.SCB.Events                             (ChainEvent)
 import           Plutus.SCB.Query                              (pureProjection)
-import           Plutus.SCB.Types                              (SCBError (ContractCommandError, ContractNotFound, OtherError),
-                                                                _WalletError)
+import           Plutus.SCB.Types                              (SCBError (..), _WalletError)
 import           Plutus.SCB.Utils                              (abbreviate, tshow)
 import           Test.QuickCheck.Instances.UUID                ()
 
@@ -102,13 +106,10 @@ import qualified Wallet.Emulator.Wallet
 data TestState =
     TestState
         { _eventStore       :: EventMap ChainEvent
-        , _walletState      :: WalletState
+        , _agentStates      :: Map Wallet AgentState
         , _nodeState        :: NodeServer.AppState
-        , _signingProcess   :: SigningProcess
-        , _nodeClientState  :: NodeClientState
         , _chainEventLog    :: [ChainEvent]
         , _emulatorEventLog :: [EmulatorEvent]
-        , _chainIndex       :: ChainIndex.AppState
         }
 
 makeLenses 'TestState
@@ -120,61 +121,40 @@ initialTestState :: TestState
 initialTestState =
     TestState
         { _eventStore = emptyEventMap
+        , _agentStates = Map.empty
         , _nodeState = NodeServer.initialAppState
-        , _walletState = WalletServer.initialState
-        , _signingProcess = SP.defaultSigningProcess WalletServer.activeWallet
-        , _nodeClientState = Wallet.Emulator.NodeClient.emptyNodeClientState
         , _chainEventLog = []
         , _emulatorEventLog = []
-        , _chainIndex = ChainIndex.initialAppState
         }
 
 type TestAppEffects =
-    '[ GenRandomTx
-     , NodeFollowerEffect
-     , WalletEffect
-     , UUIDEffect
-     , ContractEffect
-     , ChainIndexEffect
-     , ChainIndexControlEffect
-     , NodeClientEffect
-     , NodeControlEffect
-     , SigningProcessEffect
-     , SigningProcessControlEffect
+    '[ MultiAgentSCBEffect
      , ChainEffect
      , EventLogEffect ChainEvent
-     , State NodeFollowerState
-     , State ChainState
-     , State AppState
-     , State WalletState
-     , State SigningProcess
-     , State NodeClientState
-     , State ChainIndex.AppState
-     , State ChainIndexState
-     , State (EventMap ChainEvent)
-     , EmulatorLog.Log
-     , Log
-     , Writer [Wallet.Emulator.Wallet.WalletEvent]
-     , Writer [Wallet.Emulator.NodeClient.NodeClientEvent]
-     , Writer [ChainIndex.ChainIndexEvent]
-     , Writer [Wallet.Emulator.Chain.ChainEvent]
-     , Writer [EmulatorEvent]
-     , Writer [ChainEvent]
-     , Error WalletAPIError
-     , Error SCBError
      , State TestState
+     , Log
      , IO
      ]
 
-valueAt :: Address -> Eff TestAppEffects Ledger.Value
-valueAt = WalletServer.valueAt
+-- | The value at an address, in the UTXO set of the emulated node.
+--   Note that the agents may have a different view of this (use 'syncAll'
+--   to synchronise all agents)
+valueAt :: Member (State TestState) effs => Address -> Eff effs Ledger.Value
+valueAt address =
+    use (nodeState
+        . NodeServer.chainState
+        . Wallet.Emulator.Chain.chainNewestFirst
+        . to (AM.values . AM.fromChain)
+        . at address
+        . non mempty
+        )
 
 runScenario :: Eff TestAppEffects a -> IO ()
 runScenario action = do
     let testState = initialTestState
     (result, finalState) <- runTestApp initialTestState $ do
                 Wallet.Emulator.Chain.processBlock
-                sync
+                syncAll
                 void action
     case result of
         Left err -> do
@@ -195,82 +175,71 @@ runScenario action = do
                 chainEvents <- use emulatorEventLog
                 traverse_ (logDebug . abbreviate 120 . tshow) chainEvents
                 logDebug "--"
-                logDebug "Final chain index events"
+                logDebug "Final chain index events (default wallet)"
                 logDebug "--"
-                chainIndexEvents <- use (chainIndex . ChainIndex.indexEvents)
+                chainIndexEvents <- use (agentStates . at defaultWallet . anon (SCB.MultiAgent.emptyAgentState defaultWallet) (const False) . SCB.MultiAgent.chainIndexState  . ChainIndex.indexEvents)
                 traverse_ (logDebug . abbreviate 120 . tshow) chainIndexEvents
                 logDebug "--"
             error $ show err
         Right _  -> pure ()
 
-handleContractTest ::
-    (Member (Error SCBError) effs)
-    => Eff (ContractEffect ': effs)
-    ~> Eff effs
-handleContractTest = interpret $ \case
-    InvokeContract (InitContract "game") ->
-        either throwError pure $ do
-            value <- fromResumable $ initialResponse Contracts.Game.game
-            fromString $ JSON.eitherDecode (JSON.encode value)
-    InvokeContract (UpdateContract "game" payload) ->
-        either throwError pure $ do
-            request <- fromString $ JSON.parseEither JSON.parseJSON payload
-            value <- fromResumable $ runUpdate Contracts.Game.game request
-            fromString $ JSON.eitherDecode (JSON.encode value)
-    InvokeContract (InitContract contractPath) ->
-        throwError $ ContractNotFound contractPath
-    InvokeContract (UpdateContract contractPath _) ->
-        throwError $ ContractNotFound contractPath
-
 runTestApp :: TestState -> Eff TestAppEffects () -> IO (Either SCBError (), TestState)
-runTestApp state =
+runTestApp state action =
     runM
-    . runState state
-    . runError
-    . interpret (handleZoomedError _WalletError)
-    . interpret (writeIntoState chainEventLog)
-    . interpret (writeIntoState emulatorEventLog)
-    . interpret (writeIntoState (nodeState . NodeServer.eventHistory))
-    . interpret (writeIntoState (chainIndex . ChainIndex.indexEvents . iso toList Seq.fromList))
-    . interpret (handleZoomedWriter (below (walletClientEvent defaultWallet)))
-    . interpret (handleZoomedWriter (below (walletEvent defaultWallet)))
-    . runStderrLog
-    . interpret (handleZoomedWriter (below (walletEvent defaultWallet . Wallet.Emulator.Wallet._WalletMsg)))
-    . interpret (handleZoomedState eventStore)
-    . interpret (handleZoomedState (chainIndex . ChainIndex.indexState))
-    . interpret (handleZoomedState chainIndex)
-    . interpret (handleZoomedState nodeClientState)
-    . interpret (handleZoomedState signingProcess)
-    . interpret (handleZoomedState walletState)
-    . interpret (handleZoomedState nodeState)
-    . interpret (handleZoomedState (nodeState . NodeServer.chainState))
-    . interpret (handleZoomedState (nodeState . NodeServer.followerState))
-    . handleEventLogState
-    . handleChain
-    . handleSigningProcessControl
-    . handleSigningProcess
-    . handleNodeControl
-    . handleNodeClient
-    . handleChainIndexControl
-    . handleChainIndex
-    . handleContractTest
-    . handleUUIDEffect
-    . Wallet.Emulator.Wallet.handleWallet
-    . handleNodeFollower
-    . runGenRandomTx
+    $ handleUUIDEffect
+    $ runState state
 
-sync :: Eff TestAppEffects ()
-sync = ChainIndex.syncState
+    -- error handlers
+    $ runError
+    $ flip handleError (throwError . WalletError)
 
-getFollowerID :: Eff TestAppEffects FollowerID
-getFollowerID = do
-    mID <- use (chainIndex . ChainIndex.indexFollowerID)
-    case mID of
-        Just fID -> pure fID
-        Nothing  -> throwError $ OtherError "TestApp not initialised correctly!"
+    -- state handlers
+    $ interpret (handleZoomedState eventStore)
+    $ interpret (handleZoomedState agentStates)
+    $ interpret (handleZoomedState (nodeState . NodeServer.chainState))
+    $ interpret (handleZoomedState (nodeState . NodeServer.followerState))
+
+    -- writers
+    $ interpret (writeIntoState emulatorEventLog)
+    $ interpret (writeIntoState chainEventLog)
+    $ interpret (handleZoomedWriter @[EmulatorEvent] @_ @[Wallet.Emulator.Chain.ChainEvent] (below chainEvent))
+
+    -- handlers for 'TestAppEffects'
+    $ subsume
+    $ runStderrLog
+    $ subsume
+    $ handleEventLogState
+    $ handleChain
+    $ SCB.MultiAgent.handleMultiAgent
+    $ raiseEnd6
+        -- interpret the 'TestAppEffects' using
+        -- the following list of effects
+        @'[ Writer [Wallet.Emulator.Chain.ChainEvent]
+          , Writer [ChainEvent]
+          , Writer [EmulatorEvent]
+
+          , State _
+          , State _
+          , State (Map Wallet AgentState)
+          , State (EventMap ChainEvent)
+
+          , Error WalletAPIError
+          , Error SCBError
+
+          , State TestState
+
+          , UUIDEffect
+          , IO
+          ]
+        action
+
+-- | Synchronise the agent's view of the blockchain with the node.
+sync :: Member MultiAgentSCBEffect effs => Wallet -> Eff effs ()
+sync wllt = SCB.MultiAgent.agentControlAction wllt ChainIndex.syncState
+
+-- | Run 'sync' for all agents
+syncAll :: Member MultiAgentSCBEffect effs => Eff effs ()
+syncAll = traverse_ sync (Wallet <$> [1..10])
 
 fromString :: Either String a -> Either SCBError a
 fromString = first (ContractCommandError 0 . Text.pack)
-
-fromResumable :: Either (ResumableError Text) a -> Either SCBError a
-fromResumable = first (ContractCommandError 0 . Text.pack . show)
