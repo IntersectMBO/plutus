93375e7fbc3ce6a124bb0dfbeb2b5e789de991c8
David Smith <shmish111+github@gmail.com>
2020-04-23 11:21:20 -0300

    use materialized file if it exists - this means we won't need to be careful about committing this file

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -22,7 +22,9 @@ let
     };
     # This turns the output into a fixed-output derivation, which speeds things
     # up, but means we need to invalidate this hash when we change stack.yaml.
-    stack-sha256 = "1q17rcnlkv898hrpjx384knhg514w13chlbbxb0jahxm3k5zc9mk";
+    #stack-sha256 = "0z94zz98pni4a72mm4446yhh875fn1q4577d5zy3ivr5y7zmcl7i";
+    # See ../CONTRIBUTING.doc for help with materlialization
+    materialized = if builtins.pathExists ./stack.materialized then ./stack.materialized else null;
     inherit checkMaterialization;
     modules = [
         {
