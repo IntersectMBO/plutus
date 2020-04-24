d84659d73f91c4b47b4272daa6e6fbc0a783656f
kwxm <kenneth.mackenzie@iohk.io>
2020-04-24 06:04:51 +0100

    Fix type ambiguity in Compiler/Definitions.hs

--- plutus-ir/src/Language/PlutusIR/Compiler/Definitions.hs
+++ plutus-ir/src/Language/PlutusIR/Compiler/Definitions.hs
@@ -187,10 +187,8 @@ lookupDestructor
     :: forall key uni ann m . (MonadDefs key uni ann m)
     => ann -> key -> m (Maybe (Term TyName Name uni ann))
 lookupDestructor x name =
-    undefined
-{-    do
-    ds <- liftDef @key @uni $ DefT $ use datatypeDefs
+    do
+    ds <- liftDef @key @uni @ann $ DefT $ use datatypeDefs
     pure $ case Map.lookup name ds of
         Just (PLC.Def{PLC.defVal=(Datatype _ _ _ destr _)}, _) -> Just $ Var x destr
         Nothing                                                -> Nothing
--}
