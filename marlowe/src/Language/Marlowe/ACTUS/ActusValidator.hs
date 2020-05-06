a8f011ed0161535a2af985510bfccece4ce6c743
dk14 <dmkndr@gmail.com>
2020-05-06 17:54:15 +0700

    some wiring

--- marlowe/src/Language/Marlowe/ACTUS/ActusValidator.hs
+++ marlowe/src/Language/Marlowe/ACTUS/ActusValidator.hs
@@ -5,17 +5,24 @@ module Language.Marlowe.ACTUS.ActusValidator where
 import Language.Marlowe.ACTUS.ContractTerms
 import Language.Marlowe.ACTUS.Schedule
 import Language.Marlowe.ACTUS.BusinessEvents
+import Language.Marlowe.ACTUS.SCHED.ContractSchedule
+import Language.Marlowe.ACTUS.INIT.StateInitialization
+import Language.Marlowe.ACTUS.POF.Payoff
+import Language.Marlowe.ACTUS.STF.StateTransition
 
 import Language.Marlowe
 import Data.Time
-import Data.Map
 import Data.Maybe
+import Control.Arrow
 
-genShiftedSchedule :: EventType -> ContractTerms -> ShiftedSchedule
-genShiftedSchedule eventType terms = []
+import Data.List
+import qualified Data.List as L
 
-findPaymentDay :: Day -> ShiftedSchedule -> Maybe Day
-findPaymentDay day schedule  = Nothing
+genShiftedSchedule :: EventType -> ContractTerms -> Maybe ShiftedSchedule
+genShiftedSchedule = schedule
+
+isPaymentDay :: Day -> ShiftedSchedule -> Bool
+isPaymentDay day = fmap paymentDay >>> L.elem day
 
 type ValidatedCashFlows = [CashFlow]
 
@@ -23,22 +30,22 @@ checkAllScheduledEventsHappened :: Day -> ShiftedSchedule -> ValidatedCashFlows 
 checkAllScheduledEventsHappened present schedule past = True --todo: minus credit events in past
 
 --will do STF and POF through all validated events
-replayValidatedEvents :: ValidatedCashFlows -> CashFlow -> Double
-replayValidatedEvents past present = 0.0
+replayValidatedEvents :: [ScheduledEvent] -> Double
+replayValidatedEvents events = undefined
 
 -- validated cashflows are part of transaction state, present is proposed cashflow
 validateCashFlow :: ContractTerms -> ValidatedCashFlows -> CashFlow -> Bool
-validateCashFlow terms past present@CashFlow{..} = 
-    let schedule = (genShiftedSchedule (mapEventType cashEvent) terms)
-        noUnreportedOverdue = checkAllScheduledEventsHappened cashPaymentDay schedule past 
-    in case cashEvent of 
+validateCashFlow terms past present = 
+    let schedule = fromJust (genShiftedSchedule (mapEventType (cashEvent present)) terms)
+        noUnreportedOverdue = checkAllScheduledEventsHappened (cashPaymentDay present) schedule past 
+    in case (cashEvent present) of 
         PP_EVENT {..} -> noUnreportedOverdue -- maybe check that outstanding notional is still positive and compare pp_payoff to amount
         CE_EVENT {..} -> not noUnreportedOverdue  
-        otherwise -> 
+        _ -> 
             let 
-                expectedPaymentDay = findPaymentDay cashPaymentDay schedule
-                expectedPayOff = replayValidatedEvents past present
-            in noUnreportedOverdue && fromJust expectedPaymentDay == cashPaymentDay && expectedPayOff == amount 
+                expectedPaymentDayOk = isPaymentDay (cashPaymentDay present) schedule
+                expectedPayOff = replayValidatedEvents (fmap cashEvent $ past ++ [present])
+            in noUnreportedOverdue && expectedPaymentDayOk && expectedPayOff == amount present
     --todo check currency from contract terms
     --todocheck contractId
     --todo check if party is eligble to initate this event
