b88ca924a63fbbcf0df7df8a0add8cdd2648441a
effectfully <effectfully@gmail.com>
2020-05-20 19:43:24 +0300

    Added shrinking for the property tests

--- prettyprinter-configurable/test/Default.hs
+++ prettyprinter-configurable/test/Default.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TypeApplications          #-}
 
 module Default where
 
@@ -13,19 +15,12 @@ import           Text.PrettyBy.Fixity
 import           Text.PrettyBy.Monad
 
 import           Data.Proxy
-import           Data.Text                 (Text)
 import qualified Data.Text                 as Text
+import           Data.Text.Arbitrary
 import           Data.Text.Prettyprint.Doc
 import           Test.QuickCheck
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
-
-data TestCase = TestCase
-    { _testCaseExprType    :: String
-    , _testCaseExpr        :: String
-    , _testCaseViaPretty   :: String
-    , _testCaseViaPrettyBy :: String
-    }
 
 newtype OnlyType = OnlyType RenderContext
 
@@ -33,45 +28,41 @@ instance HasRenderContext OnlyType where
     renderContext f (OnlyType context) = OnlyType <$> f context
 
 instance PrettyBy OnlyType (Proxy a) => PrettyBy OnlyType (Proxy [a]) where
-    prettyBy =
-        viaPrettyM $ \_ ->
-            compoundDocM unitFixity $ \prettyIn ->
-                brackets $ prettyIn Forward botFixity (Proxy @a)
+    prettyBy = viaPrettyM $ \_ ->
+        compoundDocM unitFixity $ \prettyIn ->
+            brackets $ prettyIn Forward botFixity (Proxy @a)
 
 instance PrettyBy OnlyType (Proxy a) => PrettyBy OnlyType (Proxy (Maybe a)) where
-    prettyBy =
-        viaPrettyM $ \_ ->
-            sequenceDocM juxtFixity $ \prettyEl ->
-                "Maybe" <+> prettyEl (Proxy @a)
+    prettyBy = viaPrettyM $ \_ ->
+        sequenceDocM juxtFixity $ \prettyEl ->
+            "Maybe" <+> prettyEl (Proxy @a)
 
 instance (PrettyBy OnlyType (Proxy a), PrettyBy OnlyType (Proxy b)) =>
             PrettyBy OnlyType (Proxy (a, b)) where
-    prettyBy =
-        viaPrettyM $ \_ ->
-            compoundDocM unitFixity $ \prettyIn -> mconcat
-                [ "("
-                , prettyIn Forward botFixity $ Proxy @a
-                , ", "
-                , prettyIn Forward botFixity $ Proxy @b
-                , ")"
-                ]
+    prettyBy = viaPrettyM $ \_ ->
+        compoundDocM unitFixity $ \prettyIn -> mconcat
+            [ "("
+            , prettyIn Forward botFixity $ Proxy @a
+            , ", "
+            , prettyIn Forward botFixity $ Proxy @b
+            , ")"
+            ]
 
 instance PrettyBy OnlyType (Proxy Integer) where
-    prettyBy = viaPrettyM $ \_ -> unitDocM "Integer"
+    prettyBy = viaPrettyM $ \_ ->
+        unitDocM "Integer"
 
 instance PrettyBy OnlyType (Proxy Char) where
-    prettyBy = viaPrettyM $ \_ -> unitDocM "Char"
+    prettyBy = viaPrettyM $ \_ ->
+        unitDocM "Char"
 
 instance PrettyBy OnlyType (Proxy Text) where
-    prettyBy = viaPrettyM $ \_ -> unitDocM "Text"
+    prettyBy = viaPrettyM $ \_ ->
+        unitDocM "Text"
 
-makeTestCase :: (Show a, Pretty a, PrettyBy () a, PrettyBy OnlyType (Proxy a)) => a -> TestCase
-makeTestCase (x :: a) =
-    TestCase
-        (show . prettyBy (OnlyType botRenderContext) $ Proxy @a)
-        (show x)
-        (show $ pretty x)
-        (show $ prettyBy () x) -- ++ show (prettyBy () x))
+type TestCaseConstr a = (Show a, Pretty a, PrettyBy () a, PrettyBy OnlyType (Proxy a), Arbitrary a)
+
+data TestCase = forall a. TestCaseConstr a => TestCase a
 
 maybeOf :: Gen a -> Gen (Maybe a)
 maybeOf genX = frequency
@@ -79,28 +70,38 @@ maybeOf genX = frequency
     , (3, Just <$> genX)
     ]
 
-genPrettyPrintable :: Gen TestCase
-genPrettyPrintable = go 13 $ fmap makeTestCase where
-    go
-        :: Int
-        -> (forall a. (Show a, Pretty a, PrettyBy () a, PrettyBy OnlyType (Proxy a)) => Gen a -> Gen r)
-        -> Gen r
-    go i k = frequency $ filter ((> 0) . fst)
-      [ (i, go (i - 3) $ k . vectorOf (2 * i))
-      , (i, go (i - 3) $ k . maybeOf)
-      , (i, go (i `div` 2) $ \genX -> go (i `div` 2) $ \genY -> k $ (,) <$> genX <*> genY)
-      , (14 - i, k $ arbitrarySizedIntegral @Integer)
-      , (14 - i, k arbitraryPrintableChar)
-      , (14 - i, k $ Text.pack <$> listOf arbitraryPrintableChar)
-      ]
+instance Arbitrary TestCase where
+    arbitrary = go 13 $ fmap TestCase where
+        go
+            :: Int
+            -> (forall a. TestCaseConstr a => Gen a -> Gen TestCase)
+            -> Gen TestCase
+        go i k = frequency $ filter ((> 0) . fst)
+          [ (i, go (i - 3) $ k . vectorOf (2 * i))
+          , (i, go (i - 3) $ k . maybeOf)
+          , (i, go (i `div` 2) $ \genX -> go (i `div` 2) $ \genY -> k $ (,) <$> genX <*> genY)
+          , (14 - i, k $ arbitrarySizedIntegral @Integer)
+          , (14 - i, k arbitraryPrintableChar)
+          , (14 - i, k $ Text.pack <$> listOf arbitraryPrintableChar)
+          ]
+
+    -- We do not attempt to shrink types of generated expressions, only expressions themselves.
+    shrink (TestCase x) = map TestCase $ shrink x
+
+prettyByRespectsDefaults :: Blind TestCase -> Property
+prettyByRespectsDefaults (Blind (TestCase (x :: a))) =
+    label exprType $ counterexample err $ viaPretty == viaPrettyBy
+  where
+    exprType    = show . prettyBy (OnlyType botRenderContext) $ Proxy @a
+    exprRepr    = show x
+    viaPretty   = show $ pretty x
+    viaPrettyBy = show $ prettyBy () x
+
+    err = unlines
+        [ "Mismatch for: " ++ exprRepr
+        , "Via Pretty: " ++ viaPretty
+        , "Via PrettyBy: " ++ viaPrettyBy
+        ]
 
 test_default :: TestTree
-test_default =
-    testProperty "default" . withMaxSuccess 500 $
-        forAllBlind genPrettyPrintable $ \(TestCase exprType expr viaPretty viaPrettyBy) ->
-            let message = unlines
-                    [ "Mismatch for: " ++ expr
-                    , "via Pretty: " ++ viaPretty
-                    , "via PrettyBy: " ++ viaPrettyBy
-                    ]
-            in collect exprType $ counterexample message $ viaPretty == viaPrettyBy
+test_default = testProperty "default" $ withMaxSuccess 500 prettyByRespectsDefaults
