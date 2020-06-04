eb4699eaf4e1b574d5df787350dd7e624cfabead
effectfully <effectfully@gmail.com>
2020-06-04 20:12:41 +0300

    Tweaking 'cabal-doctest' stuff

--- prettyprinter-configurable/Setup.hs
+++ prettyprinter-configurable/Setup.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
+
+-- That's how people do that. I don't know why.
+#ifndef MIN_VERSION_cabal_doctest
+#define MIN_VERSION_cabal_doctest(x,y,z) 0
+#endif
+
+#if MIN_VERSION_cabal_doctest(1,0,0)
 
 import           Distribution.Extra.Doctest (defaultMainWithDoctests)
 
 main :: IO ()
 main = defaultMainWithDoctests "prettyprinter-configurable-doctest"
+
+#else
+
+import           Distribution.Simple
+
+main :: IO ()
+main = defaultMain
+
+#endif
