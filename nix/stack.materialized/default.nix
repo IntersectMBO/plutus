38d61c182217fa167af9fc34b4d02e194bc1c926
dk14 <dmkndr@gmail.com>
2020-07-14 17:19:38 +0700

    nix

--- nix/stack.materialized/default.nix
+++ nix/stack.materialized/default.nix
@@ -41,6 +41,7 @@
         plutus-use-cases = ./plutus-use-cases.nix;
         playground-common = ./playground-common.nix;
         marlowe = ./marlowe.nix;
+        marlowe-actus = ./marlowe-actus.nix;
         marlowe-playground-server = ./marlowe-playground-server.nix;
         plc-agda = ./plc-agda.nix;
         plutus-ledger = ./plutus-ledger.nix;
