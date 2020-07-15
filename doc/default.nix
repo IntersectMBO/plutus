17c8b77e809adccaa274fcd07c047164fcadf3b4
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-07-15 11:01:07 +0100

    Add python deps to Nix build

--- doc/default.nix
+++ doc/default.nix
@@ -1,8 +1,16 @@
-{ stdenv, lib, pythonPackages, sphinxcontrib-domaintools, sphinxcontrib-haddock, combined-haddock, ... }:
+{ stdenv, lib, pythonPackages, sphinxcontrib-domaintools, sphinxcontrib-haddock, sphinx-markdown-tables, sphinxemoji, combined-haddock, ... }:
 stdenv.mkDerivation {
   name = "plutus-docs";
   src = lib.sourceFilesBySuffices ./. [ ".py" ".rst" ".hs" ];
-  buildInputs = [ pythonPackages.sphinx pythonPackages.sphinx_rtd_theme sphinxcontrib-domaintools sphinxcontrib-haddock ];
+  buildInputs = with pythonPackages; [
+    sphinx
+    sphinx_rtd_theme
+    sphinxcontrib-domaintools
+    sphinxcontrib-haddock
+    sphinx-markdown-tables
+    sphinxemoji
+    recommonmark
+  ];
   buildPhase = ''
     cp -aR ${combined-haddock}/share/doc haddock
     # -n gives warnings on missing link targets, -W makes warnings into errors
