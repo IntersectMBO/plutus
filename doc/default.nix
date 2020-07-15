54310ff13e0de102343b8e5b6f45ab3405245b95
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-07-15 11:07:32 +0100

    Include graphics in source

--- doc/default.nix
+++ doc/default.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, pythonPackages, sphinxcontrib-domaintools, sphinxcontrib-haddock, sphinx-markdown-tables, sphinxemoji, combined-haddock, ... }:
 stdenv.mkDerivation {
   name = "plutus-docs";
-  src = lib.sourceFilesBySuffices ./. [ ".py" ".rst" ".hs" ];
+  src = lib.sourceFilesBySuffices ./. [ ".py" ".rst" ".hs" ".png" ];
   buildInputs = with pythonPackages; [
     sphinx
     sphinx_rtd_theme
