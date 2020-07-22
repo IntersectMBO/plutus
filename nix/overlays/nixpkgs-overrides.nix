78c611d515ef000044663051d56e6f2a849ff123
Simon Hafner <simon.hafner@iohk.io>
2020-07-22 16:02:06 +0200

    why use your own machine if you've got hydra

--- nix/overlays/nixpkgs-overrides.nix
+++ nix/overlays/nixpkgs-overrides.nix
@@ -4,4 +4,6 @@ self: super: {
   # that hasn't been merged upstream yet. And you can't override the pieces of a 'bundlerApp'.
   asciidoctor= super.callPackage ../asciidoctor { };
   z3 = super.callPackage ../z3.nix {};
+  # to add debug info for valgrind
+  gmp = super.gmp.overrideAttrs (_: { dontStrip = true; });
 }
