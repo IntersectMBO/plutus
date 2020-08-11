09ed5aa860d36c2914cfe6029475198808cabf08
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-08-11 11:27:36 +0100

    Use my fixed branch of haskell.nix
    Revert back to master once
    https://github.com/input-output-hk/haskell.nix/pull/810
    is in.
    
    Based on newer haskell.nix, so need a few fixes for that.

--- nix/haskell-extra.nix
+++ nix/haskell-extra.nix
@@ -5,12 +5,13 @@
 # These are for e.g. developer usage, or for running formatting tests.
 ############################################################################
 { pkgs, index-state, checkMaterialization }:
-{
+let compiler-nix-name = "ghc883";
+in {
   Agda = pkgs.haskell-nix.hackage-package {
     name = "Agda";
     version = "2.6.1";
-    plan-sha256 = "1l8fviiy9baqys6gq1s1lrpczv355yl0w4p30ma2n9y71xv356pq";
-    inherit index-state checkMaterialization;
+    plan-sha256 = "0gvvhwjcwmq8avcqz8azv8db2171qvigkam3i9bgr6z13xk995ha";
+    inherit compiler-nix-name index-state checkMaterialization;
     modules = [{
       # Agda is a huge pain. They have a special custom setup that compiles the interface files for
       # the Agda that ships with the compiler. These go in the data files for the *library*, but they
@@ -41,23 +42,23 @@
   cabal-install = pkgs.haskell-nix.hackage-package {
     name = "cabal-install";
     version = "3.2.0.0";
-    inherit index-state checkMaterialization;
+    inherit compiler-nix-name index-state checkMaterialization;
     # Invalidate and update if you change the version or index-state
-    plan-sha256 = "0cal7blv3cc354r8carx3h8ghjqdlkcw30vrqqw59dhzi8nsrpsw";
+    plan-sha256 = "06j2m7jvqfw65cn717kgflh49a0n1jkra4iw841z5ryh2y7y48pr";
   };
   stylish-haskell = pkgs.haskell-nix.hackage-package {
     name = "stylish-haskell";
     version = "0.10.0.0";
-    inherit index-state checkMaterialization;
+    inherit compiler-nix-name index-state checkMaterialization;
     # Invalidate and update if you change the version or index-state
-    plan-sha256 = "1lcz59vax5f3xc9m4kiafi40q4z24rjd15dk8nnxcpqk0d2ahjhf";
+    plan-sha256 = "0zaypywwcq8kh1q0flc6azqilvabbzbchi2i155agjsq3b7xs3k0";
   };
   hlint = pkgs.haskell-nix.hackage-package {
     name = "hlint";
     version = "2.2.11";
-    inherit index-state checkMaterialization;
+    inherit compiler-nix-name index-state checkMaterialization;
     # Invalidate and update if you change the version or index-state
-    plan-sha256 = "05abw66612hb9sg3fy2sxnin0dvp4m8x3i6n3xrydvh54g5pp5zn";
+    plan-sha256 = "10zw1zvs2bbdyh2mg01vm8wz6n65m2qkvz1va9hzhj8ch00nvpbh";
   };
   inherit (
     let hspkgs = pkgs.haskell-nix.cabalProject {
@@ -72,10 +73,9 @@
           "https://github.com/wz1000/shake"."fb3859dca2e54d1bbb2c873e68ed225fa179fbef" = "0sa0jiwgyvjsmjwpfcpvzg2p7277aa0dgra1mm6afh2rfnjphz8z";
           "https://github.com/peti/cabal-plan"."894b76c0b6bf8f7d2f881431df1f13959a8fce87" = "06iklj51d9kh9bhc42lrayypcpgkjrjvna59w920ln41rskhjr4y";
           }."${location}"."${tag}";
-        inherit index-state checkMaterialization;
+        inherit compiler-nix-name index-state checkMaterialization;
         # Invalidate and update if you change the version
         plan-sha256 = "0a6c4lhnlm2lkic91ips0gb3hqlp3fk2aa01nsa8dhz9l8zg63da";
-        compiler-nix-name = "ghc883";
         modules = [{
           # Tests don't pass for some reason, but this is a somewhat random revision.
           packages.haskell-language-server.doCheck = false;
@@ -94,6 +94,11 @@
         # Invalidate and update if you change the version
         stack-sha256 = "1r1fyzbl69jir30m0vqkyyf82q2548kdql4m05lss7fdsbdv4bw1";
         inherit checkMaterialization;
+
+        # Force using 8.6.5 to work around https://github.com/input-output-hk/haskell.nix/issues/811
+        ghc = pkgs.buildPackages.haskell-nix.compiler.ghc865;
+        modules = [{ compiler.nix-name = pkgs.lib.mkForce "ghc865"; }];
+
         pkg-def-extras = [
           # Workaround for https://github.com/input-output-hk/haskell.nix/issues/214
           (hackage: {
