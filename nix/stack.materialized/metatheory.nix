ae1bab47683778ce0bdd68211424ace6936e089f
James Chapman <james.chapman@iohk.io>
2020-08-11 17:58:07 +0100

    revert package name

--- nix/stack.materialized/metatheory.nix
+++ nix/stack.materialized/metatheory.nix
@@ -11,7 +11,7 @@
     flags = {};
     package = {
       specVersion = "2.4";
-      identifier = { name = "metatheory"; version = "0.1.0.0"; };
+      identifier = { name = "plc-agda"; version = "0.1.0.0"; };
       license = "Apache-2.0";
       copyright = "";
       maintainer = "james.chapman@iohk.io";
