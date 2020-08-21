e47e92cd6a92f17fa0260ceb87054c4ccae76a28
David Smith <shmish111+github@gmail.com>
2020-08-21 11:35:12 +0100

    ghc as a service (#2253)

--- default.nix
+++ default.nix
@@ -157,6 +157,25 @@ in rec {
     eutxoma = pkgs.callPackage ./papers/eutxoma/default.nix { inherit latex; };
   };
 
+  web-ghc = 
+    let
+      web-ghc-server = set-git-rev haskell.packages.web-ghc.components.exes.web-ghc-server;
+      runtimeGhc = haskell.packages.ghcWithPackages (ps: [
+        ps.playground-common
+        ps.plutus-playground-server
+        ps.plutus-use-cases
+      ]);
+    in pkgs.runCommand "web-ghc" { buildInputs = [pkgs.makeWrapper]; } ''
+      # We need to provide the ghc interpreter with the location of the ghc lib dir and the package db
+      mkdir -p $out/bin
+      ln -s ${web-ghc-server}/bin/web-ghc-server $out/bin/web-ghc-server
+      wrapProgram $out/bin/web-ghc-server \
+        --set GHC_LIB_DIR "${runtimeGhc}/lib/ghc-${runtimeGhc.version}" \
+        --set GHC_BIN_DIR "${runtimeGhc}/bin" \
+        --set GHC_PACKAGE_PATH "${runtimeGhc}/lib/ghc-${runtimeGhc.version}/package.conf.d" \
+        --set GHC_RTS "-M2G"
+    '';
+
   plutus-playground = pkgs.recurseIntoAttrs (rec {
     playground-exe = set-git-rev haskell.packages.plutus-playground-server.components.exes.plutus-playground-server;
     server-invoker = let
---
+++ web-ghc/app/Main.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE ApplicativeDo       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main
+  ( main,
+  )
+where
+
+import           Control.Monad.IO.Class   (MonadIO)
+import           Control.Monad.Logger     (MonadLogger, logInfoN, runStderrLoggingT)
+import qualified Data.Text                as Text
+import           Git                      (gitRev)
+import           Network.Wai.Handler.Warp (HostPreference, defaultSettings, setHost, setPort)
+import           Options.Applicative      (CommandFields, Mod, Parser, auto, command, customExecParser, disambiguate,
+                                           fullDesc, help, helper, idm, info, infoOption, long, option, prefs, short,
+                                           showDefault, showHelpOnEmpty, showHelpOnError, strOption, subparser, value)
+import qualified Webserver
+
+data Command = Webserver
+  { _host :: !HostPreference,
+    _port :: !Int
+  }
+  deriving (Show, Eq)
+
+versionOption :: Parser (a -> a)
+versionOption =
+  infoOption
+    (Text.unpack gitRev)
+    (short 'v' <> long "version" <> help "Show the version")
+
+commandParser :: Parser Command
+commandParser = subparser $ webserverCommandParser
+
+webserverCommandParser :: Mod CommandFields Command
+webserverCommandParser =
+  command "webserver"
+    $ flip info fullDesc
+    $ do
+      _host <-
+        strOption
+          ( short 'b' <> long "bind" <> help "Webserver bind address"
+              <> showDefault
+              <> value "127.0.0.1"
+          )
+      _port <-
+        option
+          auto
+          ( short 'p' <> long "port" <> help "Webserver port number"
+              <> showDefault
+              <> value 8080
+          )
+      pure Webserver {..}
+
+runCommand :: (MonadIO m, MonadLogger m) => Command -> m ()
+runCommand Webserver {..} = Webserver.run settings
+  where
+    settings = setHost _host . setPort _port $ defaultSettings
+
+main :: IO ()
+main = do
+  options <-
+    customExecParser
+      (prefs $ disambiguate <> showHelpOnEmpty <> showHelpOnError)
+      (info (helper <*> versionOption <*> commandParser) idm)
+  runStderrLoggingT $ do
+    logInfoN $ "Running: " <> Text.pack (show options)
+    runCommand options
