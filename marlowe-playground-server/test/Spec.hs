00577f242b1cc1b4d050427b7e45e1f0e6797e54
dk14 <dmkndr@gmail.com>
2020-08-21 07:34:09 +0700

    fix

--- marlowe-playground-server/test/Spec.hs
+++ marlowe-playground-server/test/Spec.hs
@@ -23,133 +23,85 @@ runBasicSpec :: Spec
 runBasicSpec = describe "Basic Contract" $
     it "should compile" $ runHaskell escrow >>= (`shouldBe` escrowResult)
     where
-        escrowResult = Right . InterpreterResult [] . RunResult $ [r|When
-    [Case
-        (Deposit
-            (AccountId
-                0
-                (Role "alice")
-            )
-            (Role "alice")
-            (Token "" "")
-            (Constant 450)
-        )
-        (When
-            [Case
-                (Choice
-                    (ChoiceId
-                        "choice"
-                        (Role "alice")
-                    )
-                    [Bound 0 1]
-                )
-                (When
-                    [Case
-                        (Choice
-                            (ChoiceId
-                                "choice"
-                                (Role "bob")
-                            )
-                            [Bound 0 1]
-                        )
-                        (If
-                            (ValueEQ
-                                (ChoiceValue
-                                    (ChoiceId
-                                        "choice"
-                                        (Role "alice")
-                                    ))
-                                (ChoiceValue
-                                    (ChoiceId
-                                        "choice"
-                                        (Role "bob")
-                                    ))
-                            )
-                            (If
-                                (ValueEQ
-                                    (ChoiceValue
-                                        (ChoiceId
-                                            "choice"
-                                            (Role "alice")
-                                        ))
-                                    (Constant 0)
-                                )
-                                (Pay
-                                    (AccountId
-                                        0
-                                        (Role "alice")
-                                    )
-                                    (Party (Role "bob"))
-                                    (Token "" "")
-                                    (Constant 450)
-                                    Close 
-                                )
-                                Close 
-                            )
-                            (When
-                                [Case
-                                    (Choice
-                                        (ChoiceId
-                                            "choice"
-                                            (Role "carol")
-                                        )
-                                        [Bound 1 1]
-                                    )
-                                    Close , Case
-                                    (Choice
-                                        (ChoiceId
-                                            "choice"
-                                            (Role "carol")
-                                        )
-                                        [Bound 0 0]
-                                    )
-                                    (Pay
-                                        (AccountId
-                                            0
-                                            (Role "alice")
-                                        )
-                                        (Party (Role "bob"))
-                                        (Token "" "")
-                                        (Constant 450)
-                                        Close 
-                                    )]
-                                100 Close 
-                            )
-                        )]
-                    60
-                    (When
-                        [Case
-                            (Choice
-                                (ChoiceId
-                                    "choice"
-                                    (Role "carol")
-                                )
-                                [Bound 1 1]
-                            )
-                            Close , Case
-                            (Choice
-                                (ChoiceId
-                                    "choice"
-                                    (Role "carol")
-                                )
-                                [Bound 0 0]
-                            )
-                            (Pay
-                                (AccountId
-                                    0
-                                    (Role "alice")
-                                )
-                                (Party (Role "bob"))
+        escrowResult = Right . InterpreterResult [] . RunResult $ [r|When [
+  (Case
+     (Deposit
+        (AccountId 0
+           (Role "alice"))
+        (Role "alice")
+        (Token "" "")
+        (Constant 450))
+     (When [
+        (Case
+           (Choice
+              (ChoiceId "choice"
+                 (Role "alice")) [
+              (Bound 0 1)])
+           (When [
+              (Case
+                 (Choice
+                    (ChoiceId "choice"
+                       (Role "bob")) [
+                    (Bound 0 1)])
+                 (If
+                    (ValueEQ
+                       (ChoiceValue
+                          (ChoiceId "choice"
+                             (Role "alice")))
+                       (ChoiceValue
+                          (ChoiceId "choice"
+                             (Role "bob"))))
+                    (If
+                       (ValueEQ
+                          (ChoiceValue
+                             (ChoiceId "choice"
+                                (Role "alice")))
+                          (Constant 0))
+                       (Pay
+                          (AccountId 0
+                             (Role "alice"))
+                          (Party
+                             (Role "bob"))
+                          (Token "" "")
+                          (Constant 450) Close) Close)
+                    (When [
+                          (Case
+                             (Choice
+                                (ChoiceId "choice"
+                                   (Role "carol")) [
+                                (Bound 1 1)]) Close)
+                          ,
+                          (Case
+                             (Choice
+                                (ChoiceId "choice"
+                                   (Role "carol")) [
+                                (Bound 0 0)])
+                             (Pay
+                                (AccountId 0
+                                   (Role "alice"))
+                                (Party
+                                   (Role "bob"))
                                 (Token "" "")
-                                (Constant 450)
-                                Close 
-                            )]
-                        100 Close 
-                    )
-                )]
-            40 Close 
-        )]
-    10 Close 
+                                (Constant 450) Close))] 100 Close)))] 60
+              (When [
+                    (Case
+                       (Choice
+                          (ChoiceId "choice"
+                             (Role "carol")) [
+                          (Bound 1 1)]) Close)
+                    ,
+                    (Case
+                       (Choice
+                          (ChoiceId "choice"
+                             (Role "carol")) [
+                          (Bound 0 0)])
+                       (Pay
+                          (AccountId 0
+                             (Role "alice"))
+                          (Party
+                             (Role "bob"))
+                          (Token "" "")
+                          (Constant 450) Close))] 100 Close)))] 40 Close))] 10 Close
 |]
 
 runHaskell :: ByteString -> IO (Either InterpreterError (InterpreterResult RunResult))
