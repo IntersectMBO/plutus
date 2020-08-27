828c687090ffbe7c6f4529e66783b2e40019f9df
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-08-27 21:22:05 +1200

    Update example/shell.nix to latest commit

--- example/shell.nix
+++ example/shell.nix
@@ -1,6 +1,6 @@
 { plutus ? builtins.fetchTarball {
     name = "plutus";
-    url = https://github.com/input-output-hk/plutus/archive/9e8f3a894558ec212dac3d5286f473a2ebd49481.tar.gz;
-    sha256 = "0jivkkns58akh8ffrlnn4vmdwrpr7jhd37qax0k2s0yg67s0c4i7";
+    url = https://github.com/input-output-hk/plutus/archive/0c29874ebc27d73f39384945bc38d15e5f4ead7d.tar.gz;
+    sha256 = "03d1iqaxvm3jq273bc3kh3c3rv1gj5rpm0plxxj6rrfxzad80vza";
   }
 }: import (plutus + "/use-case-shell.nix") {}
