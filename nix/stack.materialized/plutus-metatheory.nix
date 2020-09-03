0a5d8bf9c96f63687347829fa0e1eee97eed0f05
James Chapman <james.chapman@iohk.io>
2020-09-03 23:00:41 +0100

    updateMaterialised

--- nix/stack.materialized/plutus-metatheory.nix
+++ nix/stack.materialized/plutus-metatheory.nix
@@ -56,6 +56,7 @@
           "Scoped"
           "Opts"
           "MAlonzo/Code/Main"
+          "MAlonzo/Code/Algorithmic/Evaluation"
           "MAlonzo/Code/Agda/Builtin/Bool"
           "MAlonzo/Code/Agda/Builtin/Char"
           "MAlonzo/Code/Agda/Builtin/Equality"
