2a1199d88924fc33c2a7230456e524ad3dde71b9
Pablo Lamela <pablo.lamela@iohk.io>
2020-09-18 13:00:13 +0100

    SCP-1188 - Remove need for bigInt constructor and get TS to compile in JS tab (#2306)
    * Modify JS evaluation process so that it uses Monaco editor's TS -> JS transpilation.
    * Overload API to support (string | number | bignumber.BigNumber), which I have called SomeNumber, and removes the need to use the BigNumber constructor for small numbers (or if using a string).
    * Overload API to support SomeNumber | Value which I have called EValue, and removes the need to use the constant constructor.
    * Add API types to JS import.
    * Add type annotations to JS examples (now TS examples), to illustrate how to take advantage of TypeScript.
    * Fix evaluation process so that it doesn't crash in the absence of the /* === Code above this comment ... comment.
    * Fix some issues with transition from BigInt to BigNumber.

--- marlowe-playground-client/src/Language/Javascript/Interpreter.js
+++ marlowe-playground-client/src/Language/Javascript/Interpreter.js
@@ -2,16 +2,30 @@
 'use strict';
 const safeEval = require('notevil')
 
-exports.eval_ = function (left, right, javascript) {
+exports.eval_ = function (left, right, model) {
   // include any libraries etc we want by providing a context. be careful!
   // here we can pass in our library for constructing contracts
+  var monaco = global.monaco;
   var context = require('src/Language/Javascript/MarloweJS.ts');
-  context['bignumber'] = require('bignumber');
-  try {
-    var justCode = javascript.split(/^.*\/\* === Code above this comment will be removed at compile time === \*\/$/gm).slice(1).join('');
-    let res = safeEval(justCode, context);
-    return right(JSON.stringify(res));
-  } catch (error) {
-    return (left(error.toString()));
-  }
+  context['bignumber'] = require('bignumber.js');
+  return monaco.languages.typescript.getTypeScriptWorker()
+          .then(function(worker) {
+              return (worker(model.uri)
+                  .then(function(proxy) {
+                      return proxy.getEmitOutput(model.uri.toString())
+                                    .then((r) => {
+                        var javascript = r.outputFiles[0].text;
+                        try {
+                          var slices = javascript.split(/^.*\/\* === Code above this comment will be removed at compile time === \*\/$/gm);
+                          var takeSlice = 0;
+                          if (slices.length > 1) { takeSlice = 1 };
+                          var justCode = slices.slice(takeSlice).join('');
+                          let res = safeEval(justCode, context);
+                          return right(JSON.stringify(res));
+                        } catch (error) {
+                          return left(error.toString());
+                        }
+                    });
+                }));
+          })
 }
--- marlowe-playground-client/src/MainFrame.purs
+++ marlowe-playground-client/src/MainFrame.purs
@@ -12,8 +12,6 @@ import Data.List.NonEmpty as NEL
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
-import Data.Time.Duration (Milliseconds(..))
-import Effect.Aff (delay)
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect)
 import Gists (GistAction(..))
@@ -41,7 +39,6 @@ import JSEditor as JSEditor
 import Language.Haskell.Interpreter (_InterpreterResult)
 import Language.Haskell.Monaco as HM
 import Language.Javascript.Interpreter as JSI
-import Language.Javascript.Interpreter as JSInterpreter
 import LocalStorage as LocalStorage
 import Marlowe (SPParams_)
 import Marlowe as Server
@@ -235,16 +232,16 @@ handleAction _ (JSSelectEditorKeyBindings bindings) = do
   void $ query _jsEditorSlot unit (Monaco.SetKeyBindings bindings unit)
 
 handleAction _ CompileJSProgram = do
-  mContents <- query _jsEditorSlot unit (Monaco.GetText identity)
-  case mContents of
+  maybeModel <- query _jsEditorSlot unit (Monaco.GetModel identity)
+  case maybeModel of
     Nothing -> pure unit
-    Just contents -> do
+    Just model -> do
       assign _jsCompilationResult JSCompiling
       void $ subscribe
         $ affEventSource
             ( \emitter -> do
-                delay (Milliseconds 10.0) -- Small pause to allow UI to redraw
-                emit emitter (CompiledJSProgram (JSInterpreter.eval contents))
+                res <- JSI.eval model
+                emit emitter (CompiledJSProgram res)
                 pure mempty
             )
       pure unit
