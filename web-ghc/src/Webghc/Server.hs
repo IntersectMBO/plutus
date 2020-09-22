9ff2a51a96c17adcc19ad2a22477459ecd8dce70
David Smith <shmish111+github@gmail.com>
2020-09-22 17:14:27 +0100

    change runghc to use JSON so that client can be generated (#2318)

--- web-ghc/src/Webghc/Server.hs
+++ web-ghc/src/Webghc/Server.hs
@@ -1,22 +1,34 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TypeOperators      #-}
 module Webghc.Server where
 
 import           Control.Monad.Catch          (MonadMask)
 import           Control.Monad.Except         (runExceptT)
 import           Control.Monad.IO.Class       (MonadIO, liftIO)
+import           Data.Aeson                   (FromJSON, ToJSON)
 import           Data.Text                    (Text)
+import           GHC.Generics                 (Generic)
 import           Git                          (gitRev)
 import           Interpreter                  (compile, maxInterpretationTime)
 import           Language.Haskell.Interpreter (InterpreterError, InterpreterResult)
 import           Language.Haskell.Interpreter (SourceCode (SourceCode))
-import           Servant                      ((:<|>) ((:<|>)), (:>), Get, JSON, PlainText, Post, QueryFlag, ReqBody)
+import           Servant                      ((:<|>) ((:<|>)), (:>), Get, JSON, PlainText, Post, ReqBody)
 import           Servant.Server               (Server)
+
+type FrontendAPI = "runghc" :> ReqBody '[JSON] CompileRequest :> Post '[JSON] (Either InterpreterError (InterpreterResult String))
 
 type API =
   "version" :> Get '[PlainText, JSON] Text
     :<|> "health" :> Get '[JSON] ()
-    :<|> "runghc" :> QueryFlag "prelude" :> ReqBody '[PlainText] Text :> Post '[JSON] (Either InterpreterError (InterpreterResult String))
+    :<|> FrontendAPI
+
+data CompileRequest = CompileRequest { code :: Text, implicitPrelude :: Bool }
+  deriving stock (Generic)
+  deriving anyclass (FromJSON, ToJSON)
 
 server :: Server API
 server =
@@ -31,9 +43,8 @@ health = pure ()
 runghc ::
   MonadMask m =>
   MonadIO m =>
-  Bool ->
-  Text ->
+  CompileRequest ->
   m (Either InterpreterError (InterpreterResult String))
-runghc implicitPrelude code = do
+runghc CompileRequest {..} = do
   liftIO $ print code
   runExceptT $ compile maxInterpretationTime implicitPrelude $ SourceCode code
