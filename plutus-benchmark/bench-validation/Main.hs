a71d9448fea81c7acdc2a0ced0a5e19dc7489dee
kwxm <kenneth.mackenzie@iohk.io>
2020-10-19 15:01:34 +0100

    Relativise data paths

--- plutus-benchmark/bench-validation/Main.hs
+++ plutus-benchmark/bench-validation/Main.hs
@@ -11,13 +11,13 @@ import           Language.PlutusCore.Constant.Dynamic
 import           Language.PlutusCore.Evaluation.Machine.ExBudgetingDefaults
 import qualified Language.PlutusCore.Pretty                                 as PP
 
+import           Criterion.Main
+import           Criterion.Types                                            (Config (..))
 import qualified Language.PlutusCore.Universe                               as PLC
 import qualified Language.UntypedPlutusCore                                 as UPLC
 import qualified Language.UntypedPlutusCore.DeBruijn                        as UPLC
 import qualified Language.UntypedPlutusCore.Evaluation.Machine.Cek          as UPLC
-
-import           Criterion.Main
-import           Criterion.Types                                            (Config (..))
+import           Paths_plutus_benchmark                                     (getDataFileName)
 
 import           Control.Monad
 import           Control.Monad.Trans.Except                                 (runExceptT)
@@ -39,16 +39,6 @@ type Term a    = UPLC.Term PLC.Name PLC.DefaultUni a
 type Program a = UPLC.Program PLC.Name PLC.DefaultUni a
 type PlcParserError = PLC.Error PLC.DefaultUni PLC.AlexPosn
 
-
-{- Generate an HTML report.  If run via stack/cabal this will be written to the
-   `plutus-benchmark` directory by default.  The -o option can be used to change this,
-   but an absolute path will  probably be required (eg, "-o=$PWD/report.html") . -}
-config :: Config
-config = defaultConfig
-  { reportFile = Just "report.html"
-  , template = "./default.tpl"  -- Include total number of iterations in HTML
-  }
-
 loadPlcSource :: FilePath -> IO (Program ())
 loadPlcSource file = do
   contents <- BSL.readFile file
@@ -63,12 +53,6 @@ benchCek program = nf (UPLC.unsafeEvaluateCek getStringBuiltinMeanings defaultCo
 plcSuffix :: String
 plcSuffix = "plc"
 
--- The directory containing the scripts.  This will be relative to the working
--- directory, which will be `plutus-benchmark` if the benchmarks are being run
--- via cabal or stack.
-dataDir :: String
-dataDir = "bench-validation" </> "data"
-
 {- Construct an applied validator.  We assume that relevant validators, datum
    scripts, redeemers and contexts are stored in CBOR format under `<progName>`
    in the `data` directory.  These should have names like "Redeemer01.cbor",
@@ -77,10 +61,10 @@ dataDir = "bench-validation" </> "data"
    benchmark itself. -}
 getAppliedScript :: String -> Int -> Int -> Int -> Int -> IO (Term ())
 getAppliedScript progName validatorNumber datumNumber redeemerNumber contextNumber = do
-  let dataPath = dataDir </> progName
-      loadScript base scriptNumber = do
-          let file = dataPath </> (base ++ printf "%02d" scriptNumber) <.> plcSuffix
-          loadPlcSource file
+  let loadScript base scriptNumber = do
+          let file = "bench-validation" </> "data" </> progName </> (base ++ printf "%02d" scriptNumber) <.> plcSuffix
+          dataPath <- getDataFileName file
+          loadPlcSource dataPath
   validator <- loadScript "Validator" validatorNumber
   datum     <- loadScript "Datum"     datumNumber
   redeemer  <- loadScript "Redeemer"  redeemerNumber
@@ -103,8 +87,17 @@ mkBgroup dirname bms = bgroup dirname (map (mkBM dirname) bms)
 {- See the README files in the data directories for the combinations of scripts.
    you can run specific benchmarks by typing things like
    `stack bench -- plutus-benchmark:validation --ba crowdfunding/2`. -}
-main :: IO ()
-main = defaultMainWith config
+{- The definition of `config` below will cause an HTML report to be generated.  If
+   run via stack/cabal this will be written to the `plutus-benchmark` directory
+   by default.  The -o option can be used to change this, but an absolute path
+   will probably be required (eg, "-o=$PWD/report.html") . -}
+main = do
+  templateDir <- getDataFileName "templates"
+  let template = templateDir </> "with-iterations" <.> "tpl" -- Include number of iterations in HTML report
+      config = defaultConfig {
+                 template = template,
+                 reportFile = Just "report.html" }
+  defaultMainWith config
        [ mkBgroup
          "crowdfunding"
          [ (1, (1,1,1,1))
