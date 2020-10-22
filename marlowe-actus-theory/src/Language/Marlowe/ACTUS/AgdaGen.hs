687c148ab3b8063905d5dcf75cef9fe3340167aa
dk14 <dmkndr@gmail.com>
2020-10-22 12:32:32 +0700

    type signatures and modules

--- marlowe-actus-theory/src/Language/Marlowe/ACTUS/AgdaGen.hs
+++ marlowe-actus-theory/src/Language/Marlowe/ACTUS/AgdaGen.hs
@@ -1,6 +1,6 @@
-module Language.Marlowe.ACTUS.AgdaGen() where
+module Language.Marlowe.ACTUS.AgdaGen(genDefinition, genModule) where
 
-import           Agda.Syntax.Common                                    (NamedArg, MaybePlaceholder, ExpandedEllipsis(..), noPlaceholder, defaultNamedArg)
+import           Agda.Syntax.Common                                    (NamedArg, MaybePlaceholder, ExpandedEllipsis(..), noPlaceholder, defaultNamedArg, defaultArgInfo, defaultArg)
 import           Agda.Syntax.Position                                  (Range'(..))
 import           Agda.Syntax.Literal                                   (Literal(..))
 import           Agda.Syntax.Concrete                                  (Expr(..), OpApp(..), Declaration(..), WhereClause'(..), LHS(..), RHS'(..), Pattern(..))
@@ -8,13 +8,22 @@ import           Agda.Syntax.Concrete.Name                             (Name(..)
 import           Data.List.NonEmpty                                    (NonEmpty(..))
 import           Agda.Utils.List2                                      (List2(..))
 
-genDefinition :: Expr -> String -> String -> [String] -> [String] -> Declaration
-genDefinition expr name param1 params types = 
+genDefinition :: Expr -> String -> String -> [String] -> [String] -> String -> [Declaration]
+genDefinition expr name param1 params inputTypes outputType = 
     let fName = Name NoRange NotInScope $ Id name :| []
         paramName nm = Name NoRange NotInScope $ Id nm :| []
-        toParam param = IdentP $ QName $ paramName param
+        toParamP param = IdentP $ QName $ paramName param
+        toParam param = Ident $ QName $ paramName param
         lhsPattern = 
-            RawAppP NoRange $ List2 (IdentP $ QName fName) (toParam param1) (toParam <$> params)
+            RawAppP NoRange $ List2 (IdentP $ QName fName) (toParamP param1) (toParamP <$> params)
         lhs = LHS lhsPattern [] [] NoEllipsis
         rhs = RHS $ expr
-    in FunClause lhs rhs NoWhere False
+        funsigChunk paramType cont = Fun NoRange (defaultArg (toParam paramType)) cont
+        funsigTerminal = toParam outputType
+        funsig = foldr funsigChunk funsigTerminal inputTypes
+    in [TypeSig defaultArgInfo Nothing fName funsig, FunClause lhs rhs NoWhere False]
+
+genModule :: String -> [Declaration] -> Declaration
+genModule name declarations = 
+    let moduleName = QName (Name NoRange NotInScope $ Id name :| [])
+    in Module NoRange moduleName [] declarations
