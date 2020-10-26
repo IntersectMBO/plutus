4c1ee3164a3c1f2246aac504265334b33c0af347
Alexander Nemish <anemish@gmail.com>
2020-10-26 18:48:18 +0200

    Query cryptowat.ch for Oracle pricesEnlarge Choice input fieldSimplify oracle server-side, add decimal.js for arbitrary decimals. Fix Hs->PS bindings of Scientific.

--- marlowe-playground-client/spago.dhall
+++ marlowe-playground-client/spago.dhall
@@ -12,6 +12,7 @@ You can edit this file as you like.
   , "datetime"
   , "datetime-iso"
   , "debug"
+  , "decimals"
   , "effect"
   , "formatters"
   , "functions"
--- marlowe-playground-server/src/API.hs
+++ marlowe-playground-server/src/API.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE TypeOperators      #-}
 
 module API where
 
 import qualified Auth
-import           Data.Aeson                                       (FromJSON, ToJSON)
+import           Data.Aeson                                       (FromJSON, ToJSON, Value)
 import           Data.Text                                        (Text)
 import           GHC.Generics                                     (Generic)
 import qualified Language.Marlowe.ACTUS.Definitions.ContractTerms as CT
-import           Servant.API                                      ((:<|>), (:>), Get, Header, JSON, NoContent,
+import           Servant.API                                      ((:<|>), (:>), Capture, Get, Header, JSON, NoContent,
                                                                    PlainText, Post, Raw, ReqBody)
 
 type API
-     = "version" :> Get '[ PlainText, JSON] Text
+     = "oracle" :> Capture "exchange" String :> Capture "pair" String :> Get '[JSON] Value
+       :<|> "version" :> Get '[ PlainText, JSON] Text
        :<|> "actus" :> ("generate" :> ReqBody '[ JSON] CT.ContractTerms :> Post '[ JSON] String
                         :<|> "generate-static" :> ReqBody '[ JSON] CT.ContractTerms :> Post '[ JSON] String)
