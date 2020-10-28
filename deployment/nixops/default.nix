9b8abb3e9d7e0066cf10082bd49d59106bc509ca
David Smith <shmish111+github@gmail.com>
2020-10-28 15:30:00 +0000

    remove playground machines

--- deployment/nixops/default.nix
+++ deployment/nixops/default.nix
@@ -1,6 +1,5 @@
 let
   plutus = import ../../. { };
-  serverTemplate = import ./server.nix;
   prometheusTemplate = import ./prometheus.nix;
   webghc = import ./webghc.nix;
   machines = (plutus.pkgs.lib.importJSON ./machines.json);
@@ -9,28 +8,6 @@ let
   enableGithubHooks = plutus.pkgs.lib.hasAttr "githubWebhookKey" secrets;
   deploymentConfigDir = plutus.pkgs.copyPathToStore ../nixops;
   deploymentServer = plutus.haskell.packages.deployment-server.components.exes.deployment-server-exe;
-  plutusUrl = "https://${machines.environment}.${machines.plutusTld}";
-  mkConfig = ghsecrets: redirectUrl: callbackUrl: name: plutus.pkgs.writeTextFile {
-    name = name;
-    text = ''
-      auth:
-        # Maintainers' notes:
-        # 1) Github keys and URL in here *must* match the ones set up for this app on
-        #    github.
-        # 2) If you change the JWT signature, it will break all existing logins.
-        #    Don't change it unless that's something you specifically want!
-        github-client-id: ${ghsecrets.githubClientId}
-        github-client-secret: ${ghsecrets.githubClientSecret}
-        jwt-signature: ${ghsecrets.jwtSignature}
-        redirect-url: ${redirectUrl}
-      marlowe:
-        # The API Gateway url to trigger the marlowe symbolic lambda
-        symbolic-url: "${machines.marloweSymbolicUrl}"
-        api-key: "${secrets.apiGatewayKey}"
-        callback-url: "${callbackUrl}"
-    '';
-  };
-  playgroundConfig = mkConfig secrets.plutus plutusUrl "" "playground.yaml";
   stdOverlays = [ overlays.journalbeat ];
   nixpkgsLocation = https://github.com/NixOS/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz;
   nixosLocation = "/root/.nix-defexpr/channels/nixos";
@@ -39,14 +16,6 @@ let
   deploymentName = "playgrounds";
   options = { inherit stdOverlays machines defaultMachine plutus secrets nixpkgsLocation nixosLocation slackChannel nixopsStateFile deploymentName; };
   defaultMachine = (import ./default-machine.nix) options;
-  playgroundOptions = options // {
-    serviceConfig = playgroundConfig;
-    serviceName = "plutus-playground";
-    server-invoker = plutus.plutus-playground.server-invoker;
-    client = plutus.plutus-playground.client;
-  };
-  playgroundA = serverTemplate.mkInstance playgroundOptions machines.playgroundA;
-  playgroundB = serverTemplate.mkInstance playgroundOptions machines.playgroundB;
   webGhcA = webghc.mkInstance (options // { web-ghc = plutus.web-ghc; }) machines.webGhcA;
   webGhcB = webghc.mkInstance (options // { web-ghc = plutus.web-ghc; }) machines.webGhcA;
   nixops = prometheusTemplate.mkInstance
@@ -57,4 +26,4 @@ let
       name = "nixops";
     };
 in
-{ inherit playgroundA playgroundB nixops webGhcA webGhcB; }
+{ inherit nixops webGhcA webGhcB; }
--- deployment/terraform/ssh_config.tf
+++ deployment/terraform/ssh_config.tf
@@ -10,30 +10,6 @@ data "template_file" "ssh_config_section_nixops" {
   }
 }
 
-data "template_file" "ssh_config_section_playground_a" {
-  template = file("${path.module}/templates/ssh-config")
-
-  vars = {
-    full_hostname    = "playground-a.${aws_route53_zone.plutus_private_zone.name}"
-    short_hostname   = "playground-a.${var.project}"
-    ip               = aws_instance.playground_a.private_ip
-    bastion_hostname = aws_instance.bastion.*.public_ip[0]
-    user_name        = "plutus"
-  }
-}
-
-data "template_file" "ssh_config_section_playground_b" {
-  template = file("${path.module}/templates/ssh-config")
-
-  vars = {
-    full_hostname    = "playground-b.${aws_route53_zone.plutus_private_zone.name}"
-    short_hostname   = "playground-b.${var.project}"
-    ip               = aws_instance.playground_b.private_ip
-    bastion_hostname = aws_instance.bastion.*.public_ip[0]
-    user_name        = "plutus"
-  }
-}
-
 data "template_file" "ssh_config_section_webghc_a" {
   template = file("${path.module}/templates/ssh-config")
 
@@ -59,12 +35,10 @@ data "template_file" "ssh_config_section_webghc_b" {
 }
 
 data "template_file" "ssh_config" {
-  template = "\n$${nixops_node}\n$${playground_a}\n$${playground_b}\n$${webghc_a}\n$${webghc_b}"
+  template = "\n$${nixops_node}\n$${webghc_a}\n$${webghc_b}"
 
   vars = {
     nixops_node      = data.template_file.ssh_config_section_nixops.rendered
-    playground_a     = data.template_file.ssh_config_section_playground_a.rendered
-    playground_b     = data.template_file.ssh_config_section_playground_b.rendered
     webghc_a         = data.template_file.ssh_config_section_webghc_a.rendered
     webghc_b         = data.template_file.ssh_config_section_webghc_b.rendered
   }
