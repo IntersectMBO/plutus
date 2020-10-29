72dffc5616235ef06352871bb04ca8522153b70b
kwxm <kenneth.mackenzie@iohk.io>
2020-10-29 13:56:33 +0000

    updateMaterialized

--- nix/stack.materialized/plutus-benchmark.nix
+++ nix/stack.materialized/plutus-benchmark.nix
@@ -1,152 +1,154 @@
 { system
-  , compiler
-  , flags
-  , pkgs
-  , hsPkgs
-  , pkgconfPkgs
-  , errorHandler
-  , config
-  , ... }:
-  {
-    flags = {};
-    package = {
-      specVersion = "2.2";
-      identifier = { name = "plutus-benchmark"; version = "0.1.0.0"; };
-      license = "Apache-2.0";
-      copyright = "";
-      maintainer = "radu.ometita@iohk.io";
-      author = "Radu Ometita";
-      homepage = "https://github.com/iohk/plutus#readme";
-      url = "";
-      synopsis = "";
-      description = "Please see the README on GitHub at <https://github.com/input-output-hk/plutus#readme>";
-      buildType = "Simple";
-      isLocal = true;
-      detailLevel = "FullDetails";
-      licenseFiles = [ "LICENSE" "NOTICE" ];
-      dataDir = "";
-      dataFiles = [
-        "validation/data/crowdfunding/*.plc"
-        "validation/data/future/*.plc"
-        "validation/data/multisigSM/*.plc"
-        "validation/data/vesting/*.plc"
-        "validation/data/marlowe/trustfund/*.plc"
-        "validation/data/marlowe/zerocoupon/*.plc"
-        "templates/*.tpl"
-        ];
-      extraSrcFiles = [];
-      extraTmpFiles = [];
-      extraDocFiles = [];
-      };
-    components = {
-      "library" = {
+, compiler
+, flags
+, pkgs
+, hsPkgs
+, pkgconfPkgs
+, errorHandler
+, config
+, ...
+}:
+{
+  flags = { };
+  package = {
+    specVersion = "2.2";
+    identifier = { name = "plutus-benchmark"; version = "0.1.0.0"; };
+    license = "Apache-2.0";
+    copyright = "";
+    maintainer = "radu.ometita@iohk.io";
+    author = "Radu Ometita";
+    homepage = "https://github.com/iohk/plutus#readme";
+    url = "";
+    synopsis = "";
+    description = "Please see the README on GitHub at <https://github.com/input-output-hk/plutus#readme>";
+    buildType = "Simple";
+    isLocal = true;
+    detailLevel = "FullDetails";
+    licenseFiles = [ "LICENSE" "NOTICE" ];
+    dataDir = "";
+    dataFiles = [
+      "validation/data/crowdfunding/*.plc"
+      "validation/data/future/*.plc"
+      "validation/data/multisigSM/*.plc"
+      "validation/data/vesting/*.plc"
+      "validation/data/marlowe/trustfund/*.plc"
+      "validation/data/marlowe/zerocoupon/*.plc"
+      "templates/*.tpl"
+    ];
+    extraSrcFiles = [ ];
+    extraTmpFiles = [ ];
+    extraDocFiles = [ ];
+  };
+  components = {
+    "library" = {
+      depends = [
+        (hsPkgs."base" or (errorHandler.buildDepError "base"))
+        (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+        (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
+        (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
+        (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+      ];
+      buildable = true;
+      modules = [
+        "Plutus/Benchmark/Clausify"
+        "Plutus/Benchmark/Queens"
+        "Plutus/Benchmark/Knights"
+        "Plutus/Benchmark/Knights/ChessSetList"
+        "Plutus/Benchmark/Knights/KnightHeuristic"
+        "Plutus/Benchmark/Knights/Queue"
+        "Plutus/Benchmark/Knights/Sort"
+        "Plutus/Benchmark/Knights/Utils"
+        "Plutus/Benchmark/LastPiece"
+        "Plutus/Benchmark/Prime"
+      ];
+      hsSourceDirs = [ "nofib/src" ];
+    };
+    exes = {
+      "nofib-exe" = {
         depends = [
           (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
           (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
           (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
           (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-          ];
+          (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."serialise" or (errorHandler.buildDepError "serialise"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+        ];
         buildable = true;
-        modules = [
-          "Plutus/Benchmark/Clausify"
-          "Plutus/Benchmark/Queens"
-          "Plutus/Benchmark/Knights"
-          "Plutus/Benchmark/Knights/ChessSetList"
-          "Plutus/Benchmark/Knights/KnightHeuristic"
-          "Plutus/Benchmark/Knights/Queue"
-          "Plutus/Benchmark/Knights/Sort"
-          "Plutus/Benchmark/Knights/Utils"
-          "Plutus/Benchmark/LastPiece"
-          "Plutus/Benchmark/Prime"
-          ];
-        hsSourceDirs = [ "nofib/src" ];
-        };
-      exes = {
-        "nofib-exe" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
-            (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
-            (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
-            (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-            (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
-            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
-            (hsPkgs."serialise" or (errorHandler.buildDepError "serialise"))
-            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
-            ];
-          buildable = true;
-          hsSourceDirs = [ "nofib/exe" ];
-          mainPath = [ "Main.hs" ];
-          };
-        };
-      tests = {
-        "plutus-benchmark-nofib-tests" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
-            (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
-            (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
-            (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
-            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
-            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
-            ];
-          buildable = true;
-          hsSourceDirs = [ "nofib/test" ];
-          mainPath = [ "Spec.hs" ];
-          };
-        };
-      benchmarks = {
-        "nofib" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
-            (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
-            (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
-            (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
-            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
-            ];
-          buildable = true;
-          modules = [ "Common" "Paths_plutus_benchmark" ];
-          hsSourceDirs = [ "nofib/bench" ];
-          };
-        "nofib-hs" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
-            (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
-            (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
-            (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
-            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
-            ];
-          buildable = true;
-          modules = [ "Common" "Paths_plutus_benchmark" ];
-          hsSourceDirs = [ "nofib/bench" ];
-          };
-        "validation" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
-            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
-            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
-            (hsPkgs."text" or (errorHandler.buildDepError "text"))
-            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
-            ];
-          buildable = true;
-          modules = [ "Paths_plutus_benchmark" ];
-          hsSourceDirs = [ "validation" ];
-          };
-        };
+        hsSourceDirs = [ "nofib/exe" ];
+        mainPath = [ "Main.hs" ];
       };
-    } // rec {
-    src = (pkgs.lib).mkDefault ./plutus-benchmark;
-    }
+    };
+    tests = {
+      "plutus-benchmark-nofib-tests" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
+          (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+          (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
+          (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+          (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+          (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+        ];
+        buildable = true;
+        hsSourceDirs = [ "nofib/test" ];
+        mainPath = [ "Spec.hs" ];
+      };
+    };
+    benchmarks = {
+      "nofib" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
+          (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+          (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
+          (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+        ];
+        buildable = true;
+        modules = [ "Common" "Paths_plutus_benchmark" ];
+        hsSourceDirs = [ "nofib/bench" ];
+      };
+      "nofib-hs" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."plutus-benchmark" or (errorHandler.buildDepError "plutus-benchmark"))
+          (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+          (hsPkgs."plutus-tx-plugin" or (errorHandler.buildDepError "plutus-tx-plugin"))
+          (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+        ];
+        buildable = true;
+        modules = [ "Common" "Paths_plutus_benchmark" ];
+        hsSourceDirs = [ "nofib/bench" ];
+      };
+      "validation" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."plutus-core" or (errorHandler.buildDepError "plutus-core"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+        ];
+        buildable = true;
+        modules = [ "Paths_plutus_benchmark" ];
+        hsSourceDirs = [ "validation" ];
+      };
+    };
+  };
+} // rec {
+  src = (pkgs.lib).mkDefault ./plutus-benchmark;
+}
