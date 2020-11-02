a5533cd8c3b690b65c917add51fda400d723416d
David Smith <shmish111+github@gmail.com>
2020-11-02 14:30:30 +0000

    new homepage design (#2428)

--- marlowe-playground-client/src/Home.purs
+++ marlowe-playground-client/src/Home.purs
@@ -1,17 +1,17 @@
 module Home where
 
+import Prelude hiding (div)
 import Data.Maybe (Maybe(..))
 import Halogen (ClassName(..), ComponentHTML)
-import Halogen.Classes (blocklyIconColour, flex, fullWidth, haskellIcon, horizontalFlip, javascriptIcon, marloweLogo2, rightArrow, scroll, simulationIcon)
-import Halogen.HTML (button, div, div_, h2_, img, p_, text)
+import Halogen.Classes (flex, option1, option2, option3, vl)
+import Halogen.HTML (a, button, div, h2_, hr_, img, p_, text)
 import Halogen.HTML.Events (onClick)
-import Halogen.HTML.Properties (class_, classes, src)
-import Prelude (const, (<<<))
-import MainFrame.Types (ModalView(..), Action(..), ChildSlots, State)
+import Halogen.HTML.Properties (classes, href, src, target)
+import MainFrame.Types (Action(..), ChildSlots, ModalView(..), State)
 
 render :: forall m. State -> ComponentHTML Action ChildSlots m
 render state =
-  div [ classes [ scroll, ClassName "homepage-container" ] ]
+  div [ classes [ ClassName "homepage-container" ] ]
     [ div [ classes [ ClassName "marlowe-intro-container" ] ]
         [ div [ classes [ ClassName "text-block" ] ]
             [ h2_ [ text "What is Marlowe?" ]
@@ -26,73 +26,19 @@ render state =
             , p_ [ text "Marlowe contracts can be built in different ways. You can write them as Marlowe text, but also use the Blockly visual programming tool to create contracts by fitting together blocks that represent the different components. Marlowe is embedded in JavaScript and Haskell, and so you can use features from them to help you to build Marlowe contracts more readably and succinctly." ]
             ]
         ]
+    , hr_
     , div [ classes [ flex, ClassName "start-with-container" ] ]
-        [ div [ classes [ fullWidth, flex ] ]
-            [ div [ class_ (ClassName "even-item") ] []
-            , startWithHaskell state
-            , div [ class_ (ClassName "even-item") ] []
+        [ div [ classes [ ClassName "rectangle" ] ] [ img [ src option1 ] ]
+        , div [ classes [ ClassName "rectangle" ] ] [ img [ src option2 ] ]
+        , div [ classes [ ClassName "rectangle" ] ] [ img [ src option3 ] ]
+        ]
+    , div [ classes [ ClassName "ready-to-go-buttons" ] ]
+        [ h2_ [ text "Ready to go?" ]
+        , button [ onClick ((const <<< Just <<< OpenModal) NewProject) ] [ text "Start a new project" ]
+        , div [ classes [ ClassName "links" ] ]
+            [ a [ href "./tutorial/index.html", target "_blank" ] [ text """Read our "Getting Started" guide""" ]
+            , vl
+            , a [ onClick ((const <<< Just <<< OpenModal) OpenDemo) ] [ text "Browse the example files" ]
             ]
-        ]
-    , div [ classes [ ClassName "home-buttons" ] ]
-        [ div [ classes [ ClassName "ready-to-go-buttons" ] ]
-            [ button [ onClick ((const <<< Just <<< OpenModal) NewProject) ] [ text "Start coding!" ]
-            ]
-        ]
-    ]
-
-startWithHaskell :: forall m. State -> ComponentHTML Action ChildSlots m
-startWithHaskell state =
-  div [ classes [ ClassName "start-with-haskell", ClassName "even-item" ] ]
-    [ div [ classes [ ClassName "group", ClassName "compilers-group" ] ]
-        [ div [ classes [ ClassName "group", flex, ClassName "compiler-group" ] ]
-            [ div [ classes [ ClassName "icon-group" ] ]
-                [ img [ src haskellIcon, classes [ ClassName "haskell-icon" ] ]
-                , p_ [ text "Haskell" ]
-                ]
-            , div [ classes [ rightArrow ] ] []
-            ]
-        , div [ classes [ ClassName "group", flex, ClassName "compiler-group" ] ]
-            [ div [ classes [ ClassName "icon-group" ] ]
-                [ img [ src javascriptIcon, classes [ ClassName "javascript-icon" ] ]
-                , p_ [ text "Javascript" ]
-                ]
-            , div [ classes [ rightArrow ] ] []
-            ]
-        ]
-    , marloweBlocklyBox state
-    , div [ classes [ ClassName "group", flex, ClassName "simulation-group" ] ]
-        [ div [ classes [ rightArrow ] ] []
-        , div_
-            [ img [ src simulationIcon, classes [ ClassName "sim-icon" ] ]
-            , p_ [ text "Simulator" ]
-            ]
-        ]
-    ]
-
-startWithMarlowe :: forall m. State -> ComponentHTML Action ChildSlots m
-startWithMarlowe state =
-  div [ classes [ ClassName "start-with-marlowe" ] ]
-    [ marloweBlocklyBox state
-    , div [ classes [ rightArrow ] ] []
-    , div_
-        [ img [ src simulationIcon, classes [ ClassName "sim-icon" ] ]
-        , p_ [ text "Simulator" ]
-        ]
-    ]
-
-marloweBlocklyBox :: forall m. State -> ComponentHTML Action ChildSlots m
-marloweBlocklyBox state =
-  div [ classes [ ClassName "marlowe-blockly-box" ] ]
-    [ div [ classes [ ClassName "t-align-center" ] ]
-        [ img [ src marloweLogo2 ]
-        , p_ [ text "Marlowe" ]
-        ]
-    , div [ classes [ ClassName "arrows" ] ]
-        [ div [ classes [ rightArrow ] ] []
-        , div [ classes [ rightArrow, horizontalFlip ] ] []
-        ]
-    , div [ classes [ ClassName "t-align-center" ] ]
-        [ img [ src blocklyIconColour ]
-        , p_ [ text "Blockly" ]
         ]
     ]
