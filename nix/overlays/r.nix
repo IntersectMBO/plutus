7eed054213ffd3a72e5254f99976604761921948
dk14 <dmkndr@gmail.com>
2020-11-03 23:29:11 +0700

    nix fmt

--- nix/overlays/r.nix
+++ nix/overlays/r.nix
@@ -1,10 +1,10 @@
 self: super: {
-    rPackages = super.rPackages.override {
-        overrides = ({
-            hexbin = super.rPackages.hexbin.overrideDerivation(attrs: {
-                nativeBuildInputs = attrs.nativeBuildInputs ++ [ super.libiconv ];
-                buildInputs = attrs.buildInputs ++ [ super.libiconv ];
-            });
-        });
-    }; 
+  rPackages = super.rPackages.override {
+    overrides = ({
+      hexbin = super.rPackages.hexbin.overrideDerivation (attrs: {
+        nativeBuildInputs = attrs.nativeBuildInputs ++ [ super.libiconv ];
+        buildInputs = attrs.buildInputs ++ [ super.libiconv ];
+      });
+    });
+  };
 }
