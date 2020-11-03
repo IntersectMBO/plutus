6113a3572e2fb8ea0efc65fa68ad71dda60e8057
Tobias Pflug <tobias.pflug@gmail.com>
2020-11-03 17:10:17 +0100

    marlowe-playground: extract to local default.nix

--- default.nix
+++ default.nix
@@ -126,49 +126,9 @@ rec {
     inherit set-git-rev haskell docs easyPS nodejs-headers webCommon;
   };
 
-  marlowe-playground = pkgs.recurseIntoAttrs (rec {
-    playground-exe = set-git-rev haskell.packages.marlowe-playground-server.components.exes.marlowe-playground-server;
-    server-invoker =
-      let
-        # the playground uses ghc at runtime so it needs one packaged up with the dependencies it needs in one place
-        runtimeGhc = haskell.packages.ghcWithPackages (ps: [
-          ps.marlowe
-          ps.marlowe-playground-server
-        ]);
-      in
-      pkgs.runCommand "marlowe-server-invoker" { buildInputs = [ pkgs.makeWrapper ]; } ''
-        # We need to provide the ghc interpreter with the location of the ghc lib dir and the package db
-        mkdir -p $out/bin
-        ln -s ${playground-exe}/bin/marlowe-playground-server $out/bin/marlowe-playground
-        wrapProgram $out/bin/marlowe-playground \
-          --set GHC_LIB_DIR "${runtimeGhc}/lib/ghc-${runtimeGhc.version}" \
-          --set GHC_BIN_DIR "${runtimeGhc}/bin" \
-          --set GHC_PACKAGE_PATH "${runtimeGhc}/lib/ghc-${runtimeGhc.version}/package.conf.d" \
-          --set GHC_RTS "-M2G"
-      '';
-
-    generated-purescript = pkgs.runCommand "marlowe-playground-purescript" { } ''
-      mkdir $out
-      ${playground-exe}/bin/marlowe-playground-server psgenerator $out
-    '';
-
-    client =
-      pkgs.callPackage ./nix/purescript.nix rec {
-        inherit nodejs-headers;
-        inherit easyPS webCommon;
-        psSrc = generated-purescript;
-        src = ./marlowe-playground-client;
-        packageJSON = ./marlowe-playground-client/package.json;
-        yarnLock = ./marlowe-playground-client/yarn.lock;
-        yarnNix = ./marlowe-playground-client/yarn.nix;
-        additionalPurescriptSources = [ "../web-common/**/*.purs" ];
-        packages = pkgs.callPackage ./marlowe-playground-client/packages.nix { };
-        spagoPackages = pkgs.callPackage ./marlowe-playground-client/spago-packages.nix { };
-        name = (pkgs.lib.importJSON packageJSON).name;
-      };
-
-    tutorial = docs.marlowe-tutorial;
-  });
+  marlowe-playground = pkgs.callPackage ./marlowe-playground-client {
+    inherit set-git-rev haskell docs easyPS nodejs-headers webCommon;
+  };
 
   marlowe-symbolic-lambda = pkgsMusl.callPackage ./marlowe-symbolic/lambda.nix { haskellPackages = haskell.muslPackages; };
 
---
+++ marlowe-playground-client/default.nix
@@ -0,0 +1,48 @@
+{ pkgs, set-git-rev, haskell, docs, easyPS, webCommon, nodejs-headers }:
+
+let
+  playground-exe = set-git-rev haskell.packages.marlowe-playground-server.components.exes.marlowe-playground-server;
+
+  server-invoker =
+    let
+      # the playground uses ghc at runtime so it needs one packaged up with the dependencies it needs in one place
+      runtimeGhc = haskell.packages.ghcWithPackages (ps: [
+        ps.marlowe
+        ps.marlowe-playground-server
+      ]);
+    in
+    pkgs.runCommand "marlowe-server-invoker" { buildInputs = [ pkgs.makeWrapper ]; } ''
+      # We need to provide the ghc interpreter with the location of the ghc lib dir and the package db
+      mkdir -p $out/bin
+      ln -s ${playground-exe}/bin/marlowe-playground-server $out/bin/marlowe-playground
+      wrapProgram $out/bin/marlowe-playground \
+        --set GHC_LIB_DIR "${runtimeGhc}/lib/ghc-${runtimeGhc.version}" \
+        --set GHC_BIN_DIR "${runtimeGhc}/bin" \
+        --set GHC_PACKAGE_PATH "${runtimeGhc}/lib/ghc-${runtimeGhc.version}/package.conf.d" \
+        --set GHC_RTS "-M2G"
+    '';
+
+  generated-purescript = pkgs.runCommand "marlowe-playground-purescript" { } ''
+    mkdir $out
+    ${playground-exe}/bin/marlowe-playground-server psgenerator $out
+  '';
+in
+{
+
+  tutorial = docs.marlowe-tutorial;
+
+  client = pkgs.callPackage ../nix/purescript.nix rec {
+    inherit nodejs-headers;
+    inherit easyPS webCommon;
+    psSrc = generated-purescript;
+    src = ./.;
+    packageJSON = ./package.json;
+    yarnLock = ./yarn.lock;
+    yarnNix = ./yarn.nix;
+    additionalPurescriptSources = [ "../web-common/**/*.purs" ];
+    packages = pkgs.callPackage ./packages.nix { };
+    spagoPackages = pkgs.callPackage ./spago-packages.nix { };
+    name = (pkgs.lib.importJSON packageJSON).name;
+  };
+
+}
