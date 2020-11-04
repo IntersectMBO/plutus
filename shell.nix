db23280fb8c64188635b31c48d4d07b536220f9f
Tobias Pflug <tobias.pflug@gmail.com>
2020-11-04 19:50:44 +0100

    add files to exclude for nixpkgs-fmt
    This adds the following purescript related files to the excludes in
    .ignore and shell.nix:
    
    - spago-packages.nix
    - yarn.nix
    - packages.nix

--- shell.nix
+++ shell.nix
@@ -32,7 +32,7 @@ let
         # While nixpkgs-fmt does exclude patterns specified in `.ignore` this
         # does not appear to work inside the hook. For now we have to thus
         # maintain excludes here *and* in `./.ignore` and *keep them in sync*.
-        excludes = [ ".*nix/stack.materialized/.*" ".*nix/sources.nix$" ];
+        excludes = [ ".*nix/stack.materialized/.*" ".*nix/sources.nix$" ".*/spago-packages.nix$" ".*/yarn.nix$" ".*/packages.nix$" ];
       };
       shellcheck.enable = true;
     };
