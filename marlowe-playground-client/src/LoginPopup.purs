2fd5a4e3caa494b7813aff3339f9131e280352a8
Hernan Rajchert <hrajchert@gmail.com>
2020-11-10 15:38:39 -0300

    Clean up login popup

--- marlowe-playground-client/src/LoginPopup.purs
+++ marlowe-playground-client/src/LoginPopup.purs
@@ -2,51 +2,43 @@ module LoginPopup where
 
 import Prelude
 
-import Auth (AuthRole(..), AuthStatus(..), authStatusAuthRole)
+import Auth (AuthRole, AuthStatus, authStatusAuthRole)
 import Control.Monad.Except (runExcept)
-import Control.Promise (Promise, toAffE)
 import Data.Either (Either(..), hush)
 import Data.Lens (view)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (for_)
-import Debug.Trace (traceM)
 import Effect (Effect)
-import Effect.Aff (Aff, bracket, makeAff, never, nonCanceler)
+import Effect.Aff (Aff, finally, makeAff, nonCanceler)
 import Effect.Class (liftEffect)
-import Effect.Console (log)
 import Effect.Ref (Ref)
 import Effect.Ref as Ref
-import Effect.Uncurried as FU
-import Foreign (Foreign)
 import Foreign.Generic.Class (encode, decode)
 import Network.RemoteData (RemoteData(..))
 import Types (WebData)
 import Web.Event.Event (EventType(..), Event)
 import Web.Event.EventTarget (EventListener, addEventListener, eventListener, removeEventListener)
 import Web.HTML as Web
-import Web.HTML.Window (open, opener, outerHeight, outerWidth)
-import Web.HTML.Window as WebWindow
+import Web.HTML.Window (outerHeight, outerWidth)
+import Web.HTML.Window as Window
 import Web.HTML.WindowExtra (close, postMessage)
 import Web.Socket.Event.MessageEvent as MessageEvent
 
-foreign import _openLoginPopup :: FU.EffectFn1 Unit (Promise Boolean)
-
--- TODO: the open login is sync, should we use an Effect and then trigger an Action
--- in the MainFrame when the login succeds? or should we use an Aff to expect eventual
--- response (LogedIn | Error | Cancel)
-openLoginPopup' :: Aff Boolean
-openLoginPopup' = toAffE $ FU.runEffectFn1 _openLoginPopup unit
-
+-- This function is intended to be called between a parent window. It creates a popup to the
+-- the Github auth page and waits for the popup to send a message with the current AuthRole
 openLoginPopup :: Aff AuthRole
 openLoginPopup = do
+  window <- liftEffect Web.window
   let
     popupHeight = 620
 
     popupWidth = 600
 
-    features :: Effect String
-    features = do
-      window <- Web.window
+    githubLoginPage = "/api/oauth/github"
+    -- githubLoginPage = "/#/gh-oauth-cb"
+
+    features :: Aff String
+    features = liftEffect do
       top <-
         outerHeight window
           <#> \windowHeight -> windowHeight / 2 - popupHeight / 2
@@ -71,46 +63,39 @@ openLoginPopup = do
     waitForEvent :: Ref (Maybe EventListener) -> Aff AuthRole
     waitForEvent listenerRef = makeAff resolver where
       resolver cb = do
-        window <- Web.window
-        listener <- eventListener \event -> do
-          log $ "waitForEvent event listener "
-          traceM event
-
-          case decodeMessageEvent event  of
+        -- This callback listens for all "message" events, but only succeeds when
+        -- we can decode the event.data as an AuthRole
+        listener <- eventListener \event -> case decodeMessageEvent event  of
             Nothing -> pure unit
             Just role -> cb $ Right role
 
         Ref.write (Just listener) listenerRef
-        addEventListener (EventType "message") listener false $ WebWindow.toEventTarget window
+        addEventListener (EventType "message") listener false $ Window.toEventTarget window
+        -- We can return a nonCanceler because the waitForEvent is called with a finally
         pure nonCanceler
 
-    popup :: Effect Unit
-    popup = do
-      ft <- features
-      window <- Web.window
-      _ <- open "/#/gh-oauth-cb" "_blank" ft window
-      -- _ <- open "/api/oauth/github" "_blank" ft window
-      pure unit
-  liftEffect popup
-  -- never
+    removeWaitForEventListener :: Ref (Maybe EventListener) -> Aff Unit
+    removeWaitForEventListener listenerRef = liftEffect do
+      maybeListener <- Ref.read listenerRef
+      for_ maybeListener \listener ->
+        removeEventListener (EventType "message") listener false (Window.toEventTarget window)
+
+  featureString <- features
+  _ <- liftEffect $ Window.open githubLoginPage "_blank" featureString window
   listenerRef <- liftEffect $ Ref.new Nothing
-  authRole <- waitForEvent listenerRef
-  liftEffect do
-    window <- Web.window
-    maybeListener <- Ref.read listenerRef
-    for_ maybeListener \listener ->
-      removeEventListener (EventType "message") listener false (WebWindow.toEventTarget window)
-  pure authRole
+  -- Make sure that the event listener is removed no matter what
+  finally
+    (removeWaitForEventListener listenerRef)
+    (waitForEvent listenerRef)
 
-
-
+-- This function is intended to be called from the popup window created by openLoginPopup.
 -- TODO: Move the WebData part to the route handling and rename as informParentAndClose
 informParentAndClose :: WebData AuthStatus -> Effect Unit
 informParentAndClose (Success authStatus) = do
   let
     authRole = view authStatusAuthRole authStatus
   window <- Web.window
-  maybeParent <- opener window
+  maybeParent <- Window.opener window
   for_ maybeParent \parent -> do
     postMessage (encode authRole) parent
     close window
