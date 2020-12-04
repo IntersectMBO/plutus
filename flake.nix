d35008cf4c71461bb72f11b8b692826abe3f5480
Michael Fellinger <michael.fellinger@iohk.io>
2020-12-04 06:40:26 +0100

    wip 2

--- flake.nix
+++ flake.nix
@@ -15,17 +15,57 @@
 
       legacyPackages = nixpkgs.legacyPackages.${system};
 
-      packages = let inherit (import ./nix { }) ownOverlays;
-      in import ./nix {
-        inherit system;
-        config = { allowUnfreePredicate = (import ./lib.nix).unfreePredicate; };
-        rev = self.rev;
-        pkgs = { overlays, config, ... }:
-          import nixpkgs {
-            inherit config;
-            overlays = [ haskell-nix.overlay ] ++ ownOverlays;
+      packages = let
+        inherit (import ./nix { }) ownOverlays;
+        sources = let
+          sourcesInfo =
+            builtins.fromJSON (builtins.readFile ./nix/sources.json);
+          fetch = sourceInfo:
+            builtins.fetchTarball { inherit (sourceInfo) url sha256; };
+        in builtins.mapAttrs (_: fetch) sourcesInfo;
+
+        iohkNix = import sources.iohk-nix { };
+        overlays = [
+          haskell-nix.overlay
+        ]
+        # haskell-nix.haskellLib.extra: some useful extra utility functions for haskell.nix
+          ++ iohkNix.overlays.haskell-nix-extra
+          # iohkNix: nix utilities and niv:
+          ++ iohkNix.overlays.iohkNix
+          # our own overlays:
+          ++ [
+            # Modifications to derivations from nixpkgs
+            (import ./nix/overlays/nixpkgs-overrides.nix)
+            # This contains musl-specific stuff, but it's all guarded by appropriate host-platform
+            # checks, so we can include it unconditionally
+            (import ./nix/overlays/musl.nix)
+            # fix r-modules
+            (import ./nix/overlays/r.nix)
+          ];
+        pkgs = import nixpkgs {
+          config = {
+            allowUnfreePredicate = (import ./lib.nix).unfreePredicate;
           };
-      };
+          localSystem = system;
+          overlays = overlays;
+        };
+
+        plutusMusl = import sources.nixpkgs {
+          inherit system;
+          crossSystem = pkgs.lib.systems.examples.musl64;
+          overlays = overlays;
+          config = haskell-nix.config // {
+            allowUnfreePredicate = (import ./lib.nix).unfreePredicate;
+          };
+        };
+
+        rev = null;
+
+        plutus = import ./nix/pkgs {
+          inherit rev pkgs plutusMusl sources;
+          checkMaterialization = false;
+        };
+      in plutus;
 
       apps.web-ghc = flake-utils.lib.mkApp { drv = legacyPackages.web-ghc; };
     }));
