53d6c6198b26cff0ebdcc150bcbcb591270ba438
dk14 <dmkndr@gmail.com>
2020-12-09 15:06:03 +0700

    filled in contract terms

--- marlowe-actus/src/Language/Marlowe/ACTUS/QCGenerator.hs
+++ marlowe-actus/src/Language/Marlowe/ACTUS/QCGenerator.hs
@@ -7,8 +7,6 @@ module Language.Marlowe.ACTUS.QCGenerator where
 import           Language.Marlowe.ACTUS.Definitions.ContractTerms
 import           Test.QuickCheck
 import           Data.Time
-import           Data.Time.Calendar(toGregorian)
-import           Data.Time.Clock(utctDay,UTCTime)
 import           Data.Time.Clock.POSIX(posixSecondsToUTCTime)
 
 epochToDay :: Integer -> Day
@@ -38,51 +36,92 @@ oneOr original = oneof [return 1.0, original]
 date :: Gen Day
 date = epochToDay <$> choose (0, 1000)
 
-cycle :: Gen Cycle
-cycle = Cycle <$> choose (0, 100) <*> elements [P_D, P_W, P_M, P_Q, P_H, P_Y] <*> elements [ShortStub, LongStub]
+datecycle :: Gen Cycle
+datecycle = Cycle <$> choose (0, 100) <*> elements [P_D, P_W, P_M, P_Q, P_H, P_Y] <*> elements [ShortStub, LongStub]
 
 contractTermsGen :: Gen ContractTerms
 contractTermsGen = do 
     contracttype <- elements [PAM]
     sd <- date
     ied <- date
+
     interest <- mightbe percentage
     notional <- mightbe largeamount
     pdied <- zeroOr smallamount
+
     feebasis <- elements [FEB_A, FEB_N]
     feerate <- zeroOr percentage
+
     penaltytype <- elements [PYTP_A , PYTP_N , PYTP_I , PYTP_O]
     penaltyrate <- zeroOr percentage
     cpyrt <- zeroOr percentage
+
     includeendday <- elements [True, False]
     eomc <- elements [EOMC_EOM, EOMC_SD]
     bdc <- elements [BDC_NULL, BDC_SCF, BDC_SCMF, BDC_CSF, BDC_CSMF, BDC_SCP, BDC_SCMP, BDC_CSP, BDC_CSMP]
     dcc <- elements [DCC_A_AISDA, DCC_A_360, DCC_A_365, DCC_E30_360ISDA, DCC_E30_360, DCC_B_252]
     pref <- elements [PREF_Y, PREF_N]
     cntrl <- elements [CR_BUY, CR_SEL]
+
     scef <- elements [SE_000, SE_0N0, SE_00M, SE_0NM, SE_I00, SE_IN0, SE_I0M, SE_INM]
     scixsd <- oneOr scalingFactor
     scied <- oneOr scalingFactor
+
     rrsp <- zeroOr percentage
     rrmlt <- oneOr scalingFactor
     rrpf <- zeroOr percentage
     rrpc <- oneOr percentage
     rrlc <- oneOr percentage
     rrlf <- zeroOr percentage
+
+    nextPrincipalRedemption <- mightbe largeamount
+    purchaseDate <- mightbe date
+    maturityDate <- mightbe date
+    terminationDate <- mightbe date
+    priceAtTerminationDate <- mightbe smallamount
+    priceAtPurchaseDate <- mightbe smallamount
+
+    optionalityCycle <- mightbe datecycle
+    optionalityAnchor <- mightbe date
+
+    scalingCycle <- mightbe datecycle
+    scalingAnchor <- mightbe date
+
+    rateResetCycle <- mightbe datecycle
+    rateResetAnchor <- mightbe date
+    nextRateReset <- mightbe percentage
+
+    interestCapitalisationDate <- mightbe date
+    interestPaymentCycle <- mightbe datecycle
+    interestPaymentAnchor <- mightbe date
+    accruedInterest <- mightbe smallamount
+
+    principalRedemptionCycle <- mightbe datecycle
+    principalRedemptionAnchor <- mightbe date
+
+    interestPaymentCalculationBase <- mightbe $ elements [IPCB_NT, IPCB_NTIED, IPCB_NTL]
+    interestPaymentCalculationBaseAmount <- mightbe largeamount
+    interestPaymentCalculationBaseCycle <- mightbe datecycle
+    interestPaymentCalculationBaseAnchor <- mightbe date
+
+    feeCycle <- mightbe datecycle
+    feeAnchor <- mightbe date
+    feeAccrued <- mightbe smallamount
+
     return ContractTerms {
         contractId     = "0"
         , contractType = Just contracttype
         , ct_IED       = ied
         , ct_SD        = sd
-        , ct_MD        = Nothing
-        , ct_TD        = Nothing
-        , ct_PRNXT     = Nothing
-        , ct_PRD       = Nothing
+        , ct_MD        = maturityDate
+        , ct_TD        = terminationDate
+        , ct_PRNXT     = nextPrincipalRedemption
+        , ct_PRD       = purchaseDate
         , ct_CNTRL     = cntrl
         , ct_PDIED     = pdied
         , ct_NT        = notional
-        , ct_PPRD      = Nothing
-        , ct_PTD       = Nothing
+        , ct_PPRD      = priceAtPurchaseDate
+        , ct_PTD       = priceAtTerminationDate
         , ct_DCC       = dcc
         , ct_PREF      = pref
         , ct_PRF       = CS_PF
@@ -97,18 +136,18 @@ contractTermsGen = do
         , ct_PYTP      = penaltytype
         , ct_cPYRT     = cpyrt
         -- Optionality
-        , ct_OPCL      = Nothing
-        , ct_OPANX     = Nothing
+        , ct_OPCL      = optionalityCycle
+        , ct_OPANX     = optionalityAnchor
         -- Scaling:
         , ct_SCIED     = scied
         , ct_SCEF      = scef
-        , ct_SCCL      = Nothing
-        , ct_SCANX     = Nothing
+        , ct_SCCL      = scalingCycle
+        , ct_SCANX     = scalingAnchor
         , ct_SCIXSD    = scixsd
         -- Rate Reset
-        , ct_RRCL      = Nothing
-        , ct_RRANX     = Nothing
-        , ct_RRNXT     = Nothing
+        , ct_RRCL      = rateResetCycle
+        , ct_RRANX     = rateResetAnchor
+        , ct_RRNXT     = nextRateReset
         , ct_RRSP      = rrsp
         , ct_RRMLT     = rrmlt
         , ct_RRPF      = rrpf
@@ -116,21 +155,21 @@ contractTermsGen = do
         , ct_RRLC      = rrlc
         , ct_RRLF      = rrlf
         -- Interest
-        , ct_IPCED     = Nothing
-        , ct_IPCL      = Nothing
-        , ct_IPANX     = Nothing
+        , ct_IPCED     = interestCapitalisationDate
+        , ct_IPCL      = interestPaymentCycle
+        , ct_IPANX     = interestPaymentAnchor
         , ct_IPNR      = interest
-        , ct_IPAC      = Nothing
-        , ct_PRCL      = Nothing
-        , ct_PRANX     = Nothing
-        , ct_IPCB      = Nothing
-        , ct_IPCBA     = Nothing
-        , ct_IPCBCL    = Nothing
-        , ct_IPCBANX   = Nothing
+        , ct_IPAC      = accruedInterest
+        , ct_PRCL      = principalRedemptionCycle
+        , ct_PRANX     = principalRedemptionAnchor
+        , ct_IPCB      = interestPaymentCalculationBase
+        , ct_IPCBA     = interestPaymentCalculationBaseAmount
+        , ct_IPCBCL    = interestPaymentCalculationBaseCycle
+        , ct_IPCBANX   = interestPaymentCalculationBaseAnchor
         -- Fee
-        , ct_FECL      = Nothing
-        , ct_FEANX     = Nothing
-        , ct_FEAC      = Nothing
+        , ct_FECL      = feeCycle
+        , ct_FEANX     = feeAnchor
+        , ct_FEAC      = feeAccrued
         , ct_FEB       = feebasis
         , ct_FER       = feerate
         -- enable settlement currency
