2c4d263ce13734efafacab57b0fba270e8dcefdf
Tobias Pflug <tobias.pflug@gmail.com>
2020-12-14 16:08:31 +0100

    Drop asciidoctor

--- nix/overlays/nixpkgs-overrides.nix
+++ nix/overlays/nixpkgs-overrides.nix
@@ -1,7 +1,4 @@
 self: super: {
   nix-gitignore = super.callPackage ((import ../sources.nix).nix-gitignore) { };
-  # We can *nearly* replace this with upstream nixpkgs, but unfortunately we also need a patch
-  # that hasn't been merged upstream yet. And you can't override the pieces of a 'bundlerApp'.
-  asciidoctor = super.callPackage ../pkgs/asciidoctor { };
   z3 = super.callPackage ../pkgs/z3 { };
 }
