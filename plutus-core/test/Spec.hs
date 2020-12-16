036d4317242c4136b26f40d8784d2d7fc6bf3e2b
kwxm <kenneth.mackenzie@iohk.io>
2020-12-16 10:49:32 +0000

    Fix stylish haskell

--- plutus-core/test/Spec.hs
+++ plutus-core/test/Spec.hs
@@ -37,8 +37,8 @@ import qualified Hedgehog.Gen                               as Gen
 import qualified Hedgehog.Range                             as Range
 import           Test.Tasty
 import           Test.Tasty.Golden
-import           Test.Tasty.Hedgehog
 import           Test.Tasty.HUnit
+import           Test.Tasty.Hedgehog
 
 main :: IO ()
 main = do
