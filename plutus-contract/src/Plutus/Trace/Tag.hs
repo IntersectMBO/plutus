1c2772fec44b80f7d3926ebc8a24526a539760a6
Kris Jenkins <kris.jenkins@tweag.io>
2021-01-05 16:39:05 +0000

    Correcting the JSON instances for Plutus.Trace.Tag.

--- plutus-contract/src/Plutus/Trace/Tag.hs
+++ plutus-contract/src/Plutus/Trace/Tag.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
 module Plutus.Trace.Tag(Tag(..)) where
@@ -8,11 +9,12 @@ import           Data.Text                 (Text)
 import           Data.Text.Prettyprint.Doc (Pretty (..), braces)
 import           GHC.Generics              (Generic)
 
--- | A human-readably piece of data, used to identify threads and contract
+-- | A human-readable piece of data, used to identify threads and contract
 --   instances. See note [Thread Tag]
 newtype Tag = Tag { unTag :: Text }
     deriving stock (Eq, Ord, Show, Generic)
-    deriving newtype (ToJSON, FromJSON, IsString)
+    deriving newtype (IsString)
+    deriving anyclass (ToJSON, FromJSON)
 
 instance Pretty Tag where
     pretty = braces . pretty . unTag
