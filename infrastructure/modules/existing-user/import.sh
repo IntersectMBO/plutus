1a527ecbaaa3a77ff1f5228290c90ce354295aad
David Smith <shmish111+github@gmail.com>
2021-01-08 10:57:11 +0000

    fix shellcheck

--- infrastructure/modules/existing-user/import.sh
+++ infrastructure/modules/existing-user/import.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 
 # usage: sh import.sh joe.blogs
 set -x
 USERNAME=$1
 RESOURCE=${USERNAME/./_}
-touch ./keys/$USERNAME.base64
-terraform import module.$RESOURCE.aws_iam_user.user $USERNAME
-terraform import module.$RESOURCE.aws_iam_user_policy_attachment.user $USERNAME/arn:aws:iam::aws:policy/AdministratorAccess
-terraform import module.$RESOURCE.aws_iam_user_login_profile.user $USERNAME
+touch ./keys/"$USERNAME".base64
+terraform import module."$RESOURCE".aws_iam_user.user "$USERNAME"
+terraform import module."$RESOURCE".aws_iam_user_policy_attachment.user "$USERNAME"/arn:aws:iam::aws:policy/AdministratorAccess
+terraform import module."$RESOURCE".aws_iam_user_login_profile.user "$USERNAME"
