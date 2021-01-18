60391c0925d89f95c9810887ed91d50aa895e580
Amyas Merivale <merivale@gmail.com>
2021-01-18 23:47:42 +0100

    Adding simulationId to hello world simulation. (#2615)
    * Adding simulationId to hello world simulation.
    
    * Adding simulationId import.

--- plutus-playground-server/usecases/HelloWorldSimulations.hs
+++ plutus-playground-server/usecases/HelloWorldSimulations.hs
@@ -6,7 +6,7 @@ module HelloWorldSimulations where
 
 import           HelloWorld            (registeredKnownCurrencies)
 import           Playground.Types      (ContractCall (AddBlocks), Simulation (Simulation), simulationActions,
-                                        simulationName, simulationWallets)
+                                        simulationId, simulationName, simulationWallets)
 import           SimulationUtils       (simulatorWallet)
 import           Wallet.Emulator.Types (Wallet (Wallet), getWallet)
 
@@ -21,6 +21,7 @@ simulations = [helloWorld]
     helloWorld =
         Simulation
             { simulationName = "Hello, world"
+            , simulationId = 1
             , simulationWallets
             , simulationActions = [ AddBlocks 1 ]
             }
