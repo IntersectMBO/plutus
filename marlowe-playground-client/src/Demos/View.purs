e3c4f52279109431f26b510c1a539a0d442fb731
dk14 <dmkndr@gmail.com>
2021-02-16 08:30:45 +0700

    registered demo view

--- marlowe-playground-client/src/Demos/View.purs
+++ marlowe-playground-client/src/Demos/View.purs
@@ -24,9 +24,11 @@ render state =
     [ modalHeader "Demo Files" (Just Cancel)
     , div [ classes [ modalContent, ClassName "projects-container" ] ]
         [ demoFile (wrap "Escrow") "Escrow" "Escrow is a financial arrangement where a third party holds and regulates payment of the funds required for two parties involved in a given transaction."
+        , demoFile (wrap "EscrowWithCollateral") "Escrow With Collateral" "Variation of Escrow contract that relies on a mutual collateral insteead of trusted third-party"
         , demoFile (wrap "ZeroCouponBond") "Zero Coupon Bond" "A zero-coupon bond is a debt security that does not pay interest but instead trades at a deep discount, rendering a profit at maturity, when the bond is redeemed for its full face value."
         , demoFile (wrap "CouponBondGuaranteed") "Coupon Bond Guaranteed" "A guaranteed bond is a debt security that offers a secondary guarantee that interest and principal payments will be made by a third party, should the issuer default. It can be backed by a bond insurance company, a fund or group entity, a government authority, or the corporate parents of subsidiaries or joint ventures that are issuing bonds."
         , demoFile (wrap "Swap") "Swap" "A swap is a derivative contract through which two parties exchange the cash flows or liabilities from two different financial instruments. Most swaps involve cash flows based on a notional principal amount such as a loan or bond, although the instrument can be almost anything. Usually, the principal does not change hands. Each cash flow comprises one leg of the swap. One cash flow is generally fixed, while the other is variable and based on a benchmark interest rate, floating currency exchange rate or index price. "
+        , demoFile (wrap "CFD") "Contract For Differences" "A contract for differences (CFD) is an arrangement made in financial derivatives trading where the differences in the settlement between the open and closing trade prices are cash-settled"
         ]
     ]
 
