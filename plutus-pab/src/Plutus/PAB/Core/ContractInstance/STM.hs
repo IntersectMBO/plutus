a2747e01343e71ac57a18c211686ceebcf120d24
Jann Mueller <jann.mueller@iohk.io>
2021-02-16 11:57:15 +0100

    STM design

--- plutus-pab/src/Plutus/PAB/Core/ContractInstance/STM.hs
+++ plutus-pab/src/Plutus/PAB/Core/ContractInstance/STM.hs
@@ -13,8 +13,9 @@ module Plutus.PAB.Core.ContractInstance.STM(
 import           Control.Concurrent.STM (TMVar, TVar)
 import           Data.Aeson             (Value)
 import           Data.Map               (Map)
-import           Ledger.Slot            (Slot)
-import           Ledger.Tx              (Tx)
+import           Data.Set               (Set)
+import           Ledger                 (Address, Slot, TxId)
+import           Ledger.AddressMap      (AddressMap)
 
 {- Note [Contract instance thread model]
 
@@ -27,7 +28,8 @@ As a result
 
 Contract instances can make requests to services that are managed by the PAB,
 such as the wallet backend, the chain index and the node. From the contract's
-POV we assume that these requests are responded to instantaneously. To handle these requests the PAB uses the
+POV we assume that these requests are responded to instantaneously. To handle
+these requests the PAB uses the
 'Wallet.Emulator.MultiAgent.EmulatedWalletEffects' list of effects.
 
 In addition to making requests to PAB services, contract instances can wait for
@@ -37,7 +39,7 @@ external clients of the PAB including end-users (calling contract endpoints).
 To handle this kind of waiting the PAB uses STM and the types defined in this
 module.
 
-# QoS degradation
+# QoS
 
 One of the main goals of the queueless STM design is to avoid a degradation
 of the quality of service (QoS) when the system is operating at capacity. This
@@ -47,11 +49,20 @@ which only guarantees to retry at some point (not immediately) after a variable
 has changed. So if the variable changes again before the retry happens, the
 intermediate state is not visible.
 
-What does this imply for the PAB? We could miss out on two things:
-1. Crossing slot boundaries
-2. Transactions modifying an address that the contract is interested in
+# Event types
 
-It does not mean we drop endpoint calls.
+What does this imply for the PAB? Rather than being notified of changes, we want
+to be notified of new states. Therefore we choose the following types for the
+events that we want to know about.
+
+* Time: TVar with current time
+* Modifications to an address: TVar with current UTXO set of that address
+* Transactions & rollbacks: TVar with current status of transactions
+* Endpoints: For each endpoint a TMVar that changes from empty to full if & when
+  the endpoint gets called.
+
+All other requests of the contract are handled using the wallet effects (chain index,
+tx construction and signing).
 
 [1] Keynote by Duncan Coutts at the Haskell Symposium 2020. https://vimeo.com/452222780.
 
@@ -65,19 +76,45 @@ data OpenEndpoint =
             , oepResponse :: TMVar Value -- ^ A place to write the response to.
             }
 
+{- Note [TxStatus state machine]
+
+The status of a transaction is described by the following state machine.
+
+Current state        | New state(s)
+-----------------------------------------------------
+InMemPool            | Invalid, TentativelyConfirmed
+Invalid              | -
+TentativelyConfirmed | InMemPool, DefinitelyConfirmed
+DefinitelyConfirmed  | -
+
+The initial state after submitting the transaction is InMemPool.
+
+-}
+
+-- | Status of a transaction from the perspective of the contract.
+--   See note [TxStatus state machine]
+data TxStatus =
+    InMemPool -- ^ Not on chain yet
+    | Invalid -- ^ Invalid (its inputs were spent or its validation range has passed)
+    | TentativelyConfirmed -- ^ On chain, can still be rolled back
+    | DefinitelyConfirmed -- ^ Cannot be rolled back
+
 -- | Data about the blockchain that contract instances
 --   may be interested in.
 data BlockchainEnv =
     BlockchainEnv
-        { envLastSlot  :: TVar Slot -- ^ The current slot.
-        , envLastBlock :: TVar [Tx] -- ^ Last block that was validated
-        -- TODO: Add rollbacks when we get them from
-        --       the mock node
+        { beCurrentSlot :: TVar Slot
+        , beAddressMap  :: TVar AddressMap
+        , beTxChanges   :: TVar (Map TxId TxStatus)
         }
+
+data Activity = Active | Done
 
 -- | The state of an active contract instance.
 data InstanceState =
     InstanceState
-        { issEndpoints :: TVar (Map String OpenEndpoint) -- ^ Open endpoints that can be responded to.
-        , issState     :: TVar Value -- ^ The state of the instance.
+        { issEndpoints    :: TVar (Map String OpenEndpoint) -- ^ Open endpoints that can be responded to.
+        , issAddresses    :: TVar (Set Address) -- ^ Addresses that the contract wants to watch
+        , issTransactions :: TVar (Set TxId) -- ^ Transactions whose status the contract is interested in
+        , issStatus       :: TVar Activity -- ^ Whether the instance is still running.
         }
