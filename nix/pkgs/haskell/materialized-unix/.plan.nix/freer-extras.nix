5056d905dfc9120f6515ec3c2981d0c00e76201b
Tobias Pflug <tobias.pflug@gmail.com>
2021-02-17 09:33:10 +0100

    haskell.nix: update materialized

---
+++ nix/pkgs/haskell/materialized-unix/.plan.nix/freer-extras.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "freer-extras"; version = "0.1.0.0"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "tobias.pflug@iohk.io";
+      author = "Tobias Pflug";
+      homepage = "";
+      url = "";
+      synopsis = "Useful extensions to simple-freer";
+      description = "freer-extras provides logging and monitoring functions extending simple-freer";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" ];
+      dataDir = "";
+      dataFiles = [];
+      extraSrcFiles = [ "CHANGELOG.md" ];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."freer-simple" or (errorHandler.buildDepError "freer-simple"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+          (hsPkgs."streaming" or (errorHandler.buildDepError "streaming"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        modules = [
+          "Control/Monad/Freer/Extras"
+          "Control/Monad/Freer/Log"
+          "Control/Monad/Freer/Stream"
+          ];
+        hsSourceDirs = [ "src" ];
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../freer-extras; }
