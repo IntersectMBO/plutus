e14bd82fcaf02a92c1b244dc34f463ae073b0707
Jann Mueller <jann.mueller@iohk.io>
2021-03-12 18:04:31 +0100

    Start implementing web server

---
+++ plutus-pab/src/Plutus/PAB/Simulator/Server.hs
@@ -0,0 +1,10 @@
+{-
+
+Experimental webserver implementing the new PAB API.
+-}
+module Plutus.PAB.Simulator.Server(server) where
+
+import           Plutus.PAB.Webserver.API (NewAPI)
+
+server :: IO ()
+server = undefined
