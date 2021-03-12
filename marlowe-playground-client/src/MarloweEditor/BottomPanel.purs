ded609b0772da5ed815586770c57fc5da4754a97
Pablo Lamela <pablo.lamela@iohk.io>
2021-03-12 23:48:02 +0000

    SCP-2025 - Even out the layout of the metadata form

--- marlowe-playground-client/src/MarloweEditor/BottomPanel.purs
+++ marlowe-playground-client/src/MarloweEditor/BottomPanel.purs
@@ -3,7 +3,7 @@ module MarloweEditor.BottomPanel
   ) where
 
 import Prelude hiding (div)
-import Data.Array (drop, head)
+import Data.Array (concat, concatMap, drop, head)
 import Data.Array as Array
 import Data.Lens (to, (^.))
 import Data.List (List)
@@ -15,56 +15,63 @@ import Data.String (take)
 import Data.String.Extra (unlines)
 import Data.Tuple.Nested (type (/\), (/\))
 import Halogen.Classes (flex, flexCol, fontBold, fullWidth, grid, gridColsDescriptionLocation, justifySelfEnd, minW0, minusBtn, overflowXScroll, paddingRight, plusBtn, smallBtn, underline)
-import Halogen.HTML (ClassName(..), HTML, a, button, div, div_, em_, h6_, input, li_, ol_, option, pre_, section, section_, select, span, span_, text)
+import Halogen.HTML (ClassName(..), HTML, a, button, div, div_, em_, h6_, input, option, pre_, section, section_, select, span_, text)
 import Halogen.HTML.Events (onClick, onValueChange)
 import Halogen.HTML.Properties (InputType(..), class_, classes, placeholder, selected, type_, value)
-import Marlowe.Extended.Metadata (MetaData, _choiceDescriptions, _choiceNames, _roleDescriptions, _roles, _slotParameterDescriptions, _slotParameters, _valueParameterDescriptions, _valueParameters)
 import Marlowe.Extended (contractTypeArray, contractTypeName, contractTypeInitials, initialsToContractType)
+import Marlowe.Extended.Metadata (MetaData, _choiceDescriptions, _choiceNames, _roleDescriptions, _roles, _slotParameterDescriptions, _slotParameters, _valueParameterDescriptions, _valueParameters)
 import MarloweEditor.Types (Action(..), BottomPanelView(..), MetadataAction(..), State, _editorErrors, _editorWarnings, _hasHoles, _metadataHintInfo, _showErrorDetail, contractHasErrors)
 import StaticAnalysis.BottomPanel (analysisResultPane, analyzeButton, clearButton)
 import StaticAnalysis.Types (_analysisExecutionState, _analysisState, isCloseAnalysisLoading, isNoneAsked, isReachabilityLoading, isStaticLoading)
 import Text.Parsing.StringParser.Basic (lines)
 
-metadataList :: forall p. Map String String -> Set String -> (String -> String -> MetadataAction) -> (String -> MetadataAction) -> String -> String -> HTML p Action
+metadataList :: forall p. Map String String -> Set String -> (String -> String -> MetadataAction) -> (String -> MetadataAction) -> String -> String -> Array (HTML p Action)
 metadataList metadataMap hintSet setAction deleteAction typeNameTitle typeNameSmall =
-  div [ class_ $ ClassName "metadata-field-group" ]
-    if Map.isEmpty combinedMap then
-      []
-    else
-      [ h6_ [ em_ [ text $ typeNameTitle <> " descriptions" ] ]
-      , ol_
-          $ map
-              ( \(key /\ val) ->
-                  li_
-                    ( case val of
-                        Just (desc /\ needed) ->
-                          [ text $ typeNameTitle <> " " <> show key <> ": "
-                          , input
+  if Map.isEmpty combinedMap then
+    []
+  else
+    [ div [ class_ $ ClassName "metadata-group-title" ]
+        [ h6_ [ em_ [ text $ typeNameTitle <> " descriptions" ] ] ]
+    ]
+      <> ( concatMap
+            ( \(key /\ val) ->
+                ( case val of
+                    Just (desc /\ needed) ->
+                      [ div [ class_ $ ClassName "metadata-prop-label" ]
+                          [ text $ typeNameTitle <> " " <> show key <> ": " ]
+                      , div [ class_ $ ClassName "metadata-prop-edit" ]
+                          [ input
                               [ type_ InputText
                               , placeholder $ "Description for " <> typeNameSmall <> " " <> show key
                               , class_ $ ClassName "metadata-input"
                               , value desc
                               , onValueChange $ Just <<< MetadataAction <<< setAction key
                               ]
-                          , button
+                          ]
+                      , div [ class_ $ ClassName "metadata-prop-delete" ]
+                          [ button
                               [ classes [ if needed then plusBtn else minusBtn, smallBtn, ClassName "align-top" ]
                               , onClick $ const $ Just $ MetadataAction $ deleteAction key
                               ]
                               [ text "-" ]
                           ]
-                            <> if needed then [] else [ span [ classes [ ClassName "metadata-error", ClassName "metadata-not-used" ] ] [ text "Not used" ] ]
-                        Nothing ->
-                          [ span [ class_ (ClassName "metadata-error") ] [ text $ typeNameTitle <> " " <> show key <> " description not defined" ]
-                          , button
-                              [ classes [ plusBtn, smallBtn, ClassName "align-top" ]
+                      ]
+                        <> if needed then [] else [ div [ classes [ ClassName "metadata-error", ClassName "metadata-prop-not-used" ] ] [ text "Not used" ] ]
+                    Nothing ->
+                      [ div [ classes [ ClassName "metadata-error", ClassName "metadata-prop-not-defined" ] ]
+                          [ text $ typeNameTitle <> " " <> show key <> " description not defined" ]
+                      , div [ class_ $ ClassName "metadata-prop-create" ]
+                          [ button
+                              [ classes [ minusBtn, smallBtn, ClassName "align-top" ]
                               , onClick $ const $ Just $ MetadataAction $ setAction key mempty
                               ]
                               [ text "+" ]
                           ]
-                    )
-              )
-          $ Map.toUnfoldable combinedMap
-      ]
+                      ]
+                )
+            )
+            $ Map.toUnfoldable combinedMap
+        )
   where
   mergeMaps :: (Maybe (String /\ Boolean)) -> (Maybe (String /\ Boolean)) -> (Maybe (String /\ Boolean))
   mergeMaps (Just (x /\ _)) _ = Just (x /\ true)
@@ -84,47 +91,55 @@ metadataList metadataMap hintSet setAction deleteAction typeNameTitle typeNameSm
 
 panelContents :: forall p. State -> MetaData -> BottomPanelView -> HTML p Action
 panelContents state metadata MetadataView =
-  div [ classes [ ClassName "padded-explanation" ] ]
-    [ div [ class_ $ ClassName "metadata-field-group" ]
-        [ text "Contract type: "
-        , select
-            [ class_ $ ClassName "metadata-input"
-            , onValueChange $ Just <<< MetadataAction <<< SetContractType <<< initialsToContractType
-            ] do
-            ct <- contractTypeArray
-            pure
-              $ option
-                  [ value $ contractTypeInitials ct
-                  , selected (ct == metadata.contractType)
+  div [ classes [ ClassName "metadata-form" ] ]
+    ( concat
+        [ [ div [ class_ $ ClassName "metadata-mainprop-label" ]
+              [ text "Contract type: " ]
+          , div [ class_ $ ClassName "metadata-mainprop-edit" ]
+              [ select
+                  [ class_ $ ClassName "metadata-input"
+                  , onValueChange $ Just <<< MetadataAction <<< SetContractType <<< initialsToContractType
+                  ] do
+                  ct <- contractTypeArray
+                  pure
+                    $ option
+                        [ value $ contractTypeInitials ct
+                        , selected (ct == metadata.contractType)
+                        ]
+                        [ text $ contractTypeName ct
+                        ]
+              ]
+          ]
+        , [ div [ class_ $ ClassName "metadata-mainprop-label" ]
+              [ text "Contract name: " ]
+          , div [ class_ $ ClassName "metadata-mainprop-edit" ]
+              [ input
+                  [ type_ InputText
+                  , placeholder "Contract name"
+                  , class_ $ ClassName "metadata-input"
+                  , value metadata.contractName
+                  , onValueChange $ Just <<< MetadataAction <<< SetContractName
                   ]
-                  [ text $ contractTypeName ct
+              ]
+          ]
+        , [ div [ class_ $ ClassName "metadata-mainprop-label" ]
+              [ text "Contract description: " ]
+          , div [ class_ $ ClassName "metadata-mainprop-edit" ]
+              [ input
+                  [ type_ InputText
+                  , placeholder "Contract description"
+                  , class_ $ ClassName "metadata-input"
+                  , value metadata.contractDescription
+                  , onValueChange $ Just <<< MetadataAction <<< SetContractDescription
                   ]
+              ]
+          ]
+        , generateMetadataList _roleDescriptions _roles SetRoleDescription DeleteRoleDescription "Role" "role"
+        , generateMetadataList _choiceDescriptions _choiceNames SetChoiceDescription DeleteChoiceDescription "Choice" "choice"
+        , generateMetadataList _slotParameterDescriptions _slotParameters SetSlotParameterDescription DeleteSlotParameterDescription "Slot parameter" "slot parameter"
+        , generateMetadataList _valueParameterDescriptions _valueParameters SetValueParameterDescription DeleteValueParameterDescription "Value parameter" "value parameter"
         ]
-    , div [ class_ $ ClassName "metadata-field-group" ]
-        [ text "Contract name: "
-        , input
-            [ type_ InputText
-            , placeholder "Contract name"
-            , class_ $ ClassName "metadata-input"
-            , value metadata.contractName
-            , onValueChange $ Just <<< MetadataAction <<< SetContractName
-            ]
-        ]
-    , div [ class_ $ ClassName "metadata-field-group" ]
-        [ text "Contract description: "
-        , input
-            [ type_ InputText
-            , placeholder "Contract description"
-            , class_ $ ClassName "metadata-input"
-            , value metadata.contractDescription
-            , onValueChange $ Just <<< MetadataAction <<< SetContractDescription
-            ]
-        ]
-    , generateMetadataList _roleDescriptions _roles SetRoleDescription DeleteRoleDescription "Role" "role"
-    , generateMetadataList _choiceDescriptions _choiceNames SetChoiceDescription DeleteChoiceDescription "Choice" "choice"
-    , generateMetadataList _slotParameterDescriptions _slotParameters SetSlotParameterDescription DeleteSlotParameterDescription "Slot parameter" "slot parameter"
-    , generateMetadataList _valueParameterDescriptions _valueParameters SetValueParameterDescription DeleteValueParameterDescription "Value parameter" "value parameter"
-    ]
+    )
   where
   generateMetadataList mapLens setLens = metadataList (metadata ^. mapLens) (state ^. (_metadataHintInfo <<< setLens))
 
