7c4865165c2ce855a6d8a15bdfd2ab86c707cad0
silky <noonsilk+-noonsilk@gmail.com>
2021-03-15 18:24:54 +0000

    Add docs prologue text and flag to build all docs ...
    for which haddock would otherwise fail because "of the plugin".
    
    We use set `defer-plugin-errors` for those packages.

--- nix/pkgs/plutus-haddock-combined/default.nix
+++ nix/pkgs/plutus-haddock-combined/default.nix
@@ -1,11 +1,25 @@
 { haddock-combine, haskell, haskell-nix, writeTextFile }:
 let
-  toHaddock = haskell-nix.haskellLib.collectComponents' "library" haskell.projectPackages;
+  toHaddock = haskell-nix.haskellLib.collectComponents' "library" haskell.projectPackagesAllHaddock;
 in
 haddock-combine {
   hspkgs = builtins.attrValues toHaddock;
   prologue = writeTextFile {
     name = "prologue";
-    text = "Combined documentation for all the public Plutus libraries.";
+    text = ''
+      = Combined documentation for all the public Plutus libraries
+
+      == Handy module entrypoints
+
+        * "PlutusTx": Compiling Haskell to PLC (Plutus Core; on-chain code).
+        * "PlutusTx.Prelude": Haskell prelude replacement compatible with PLC.
+        * "Plutus.Contract": Writing Plutus apps (off-chain code).
+        * "Ledger.Constraints": Constructing and validating Plutus
+           transactions. Built on "PlutusTx" and 
+           "Plutus.Contract".
+        * "Ledger.Typed.Scripts": A type-safe interface for spending and
+           producing script outputs. Built on "PlutusTx".
+        * "Plutus.Trace.Emulator": Testing Plutus contracts in the emulator.
+    '';
   };
 }
