a6f9042471a9c2cdca1afdeddb479873855ecf30
Tobias Pflug <tobias.pflug@gmail.com>
2021-03-25 18:24:37 +0100

    nixos-tests: only run `all` test
    Somehow the tests are behaving weirdly so let's limit
    this to the `all` test for now.

--- nix/tests/vm.nix
+++ nix/tests/vm.nix
@@ -11,10 +11,10 @@ let
   testing = import (pkgs.path + "/nixos/lib/testing-python.nix") { system = builtins.currentSystem; };
   makeTest = testing.makeTest;
   tests = pkgs.recurseIntoAttrs {
-    plutus-playground-server = pkgs.callPackage ./vm-tests/plutus-playground.nix { inherit makeTest;inherit plutus-playground; };
-    marlowe-playground-server = pkgs.callPackage ./vm-tests/marlowe-playground.nix { inherit makeTest;inherit marlowe-playground; };
-    web-ghc = pkgs.callPackage ./vm-tests/web-ghc.nix { inherit makeTest;inherit web-ghc; };
-    pab = pkgs.callPackage ./vm-tests/pab.nix { inherit makeTest;inherit plutus-pab marlowe-dashboard; };
+    #plutus-playground-server = pkgs.callPackage ./vm-tests/plutus-playground.nix { inherit makeTest;inherit plutus-playground; };
+    #marlowe-playground-server = pkgs.callPackage ./vm-tests/marlowe-playground.nix { inherit makeTest;inherit marlowe-playground; };
+    #web-ghc = pkgs.callPackage ./vm-tests/web-ghc.nix { inherit makeTest;inherit web-ghc; };
+    #pab = pkgs.callPackage ./vm-tests/pab.nix { inherit makeTest;inherit plutus-pab marlowe-dashboard; };
     all = pkgs.callPackage ./vm-tests/all.nix { inherit makeTest;inherit plutus-playground marlowe-playground marlowe-dashboard web-ghc marlowe-app plutus-pab; };
   };
 in
