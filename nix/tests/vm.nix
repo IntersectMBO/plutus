7c628a61ea27d31be934eda4955e4199cad1276b
Tobias Pflug <tobias.pflug@gmail.com>
2021-03-25 16:54:14 +0100

    nixos-tests: Skipping on darwin

--- nix/tests/vm.nix
+++ nix/tests/vm.nix
@@ -7,13 +7,15 @@
 , marlowe-app
 }:
 let
+  inherit (pkgs.stdenv) isDarwin;
   testing = import (pkgs.path + "/nixos/lib/testing-python.nix") { system = builtins.currentSystem; };
   makeTest = testing.makeTest;
+  tests = pkgs.recurseIntoAttrs {
+    plutus-playground-server = pkgs.callPackage ./vm-tests/plutus-playground.nix { inherit makeTest;inherit plutus-playground; };
+    marlowe-playground-server = pkgs.callPackage ./vm-tests/marlowe-playground.nix { inherit makeTest;inherit marlowe-playground; };
+    web-ghc = pkgs.callPackage ./vm-tests/web-ghc.nix { inherit makeTest;inherit web-ghc; };
+    pab = pkgs.callPackage ./vm-tests/pab.nix { inherit makeTest;inherit plutus-pab marlowe-dashboard; };
+    all = pkgs.callPackage ./vm-tests/all.nix { inherit makeTest;inherit plutus-playground marlowe-playground marlowe-dashboard web-ghc marlowe-app plutus-pab; };
+  };
 in
-pkgs.recurseIntoAttrs {
-  plutus-playground-server = pkgs.callPackage ./vm-tests/plutus-playground.nix { inherit makeTest;inherit plutus-playground; };
-  marlowe-playground-server = pkgs.callPackage ./vm-tests/marlowe-playground.nix { inherit makeTest;inherit marlowe-playground; };
-  web-ghc = pkgs.callPackage ./vm-tests/web-ghc.nix { inherit makeTest;inherit web-ghc; };
-  pab = pkgs.callPackage ./vm-tests/pab.nix { inherit makeTest;inherit plutus-pab marlowe-dashboard; };
-  all = pkgs.callPackage ./vm-tests/all.nix { inherit makeTest;inherit plutus-playground marlowe-playground marlowe-dashboard web-ghc marlowe-app plutus-pab; };
-}
+if isDarwin then { } else tests
