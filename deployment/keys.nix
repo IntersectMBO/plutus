f576a5f04ad474fd252155a6eadbf937f4f5cd33
Pablo Lamela <pablo.lamela@iohk.io>
2021-03-31 19:12:50 +0100

    Add "quanterall deployer" key to testing environment (#2934)

--- deployment/keys.nix
+++ deployment/keys.nix
@@ -4,4 +4,5 @@
   hernan = { name = "hernan.rajchert.gpg"; id = "AFF6767D33EFF77D519EE6B8C7BBC002683C8DCB"; };
   tobias = { name = "tobias.pflug.gpg"; id = "46681E3759BA8680B3180AA0EBA3115F56AA2315"; };
   amyas = { name = "amyas.merivale.gpg"; id = "8504C0C97F2164419433AAF63F91FB4358BD1137"; };
+  quanterall = { name = "quanterall.gpg"; id = "9257DDECAC836A431373F32B2FE6B18850D52CA5"; };
 }
