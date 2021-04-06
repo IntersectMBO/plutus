0dc5bf4aa682a2f1bc5a8de9c35e258acea13378
Pablo Lamela <pablo.lamela@iohk.io>
2021-04-06 21:01:58 +0100

    Print time taken for static analysis

--- marlowe-symbolic/src/Marlowe/Symbolic/Server.hs
+++ marlowe-symbolic/src/Marlowe/Symbolic/Server.hs
@@ -11,12 +11,15 @@
 
 module Marlowe.Symbolic.Server where
 
+import           Control.Exception                     (evaluate)
 import           Control.Monad.IO.Class                (MonadIO, liftIO)
 import qualified Data.Aeson                            as JSON
 import           Data.Bifunctor                        (first)
 import           Data.ByteString.Lazy.UTF8             as BSU
 import           Data.Maybe                            (fromMaybe)
 import           Data.Proxy                            (Proxy (Proxy))
+import           Formatting                            (fprintLn, (%))
+import           Formatting.Clock                      (timeSpecs)
 import           Language.Marlowe                      (Contract, Slot (Slot), State, TransactionInput,
                                                         TransactionWarning)
 import           Language.Marlowe.Analysis.FSSemantics (warningsTraceCustom)
@@ -24,6 +27,7 @@ import           Marlowe.Symbolic.Types.Request        (Request (..))
 import           Marlowe.Symbolic.Types.Response       (Result (..))
 import           Servant                               (Application, Handler (Handler), JSON, Post, ReqBody, Server,
                                                         ServerError, hoistServer, serve, (:<|>) ((:<|>)), (:>))
+import           System.Clock                          (Clock (Monotonic), getTime)
 import           System.Process                        (system)
 import           Text.PrettyPrint.Leijen               (displayS, renderCompact)
 
@@ -46,10 +50,15 @@ makeResponse (Right res) =
 handlers :: Server API
 handlers Request {..} =
   liftIO $ do
+    start <- getTime Monotonic
     evRes <- warningsTraceCustom onlyAssertions contract (Just state)
+    evaluate evRes
+    end <- getTime Monotonic
     let resp = makeResponse (first show evRes)
     putStrLn $ BSU.toString $ JSON.encode resp
+    fprintLn ("Static analysis took " % timeSpecs) start end
     pure resp
+
 
 app :: Application
 app = serve (Proxy @API) handlers
