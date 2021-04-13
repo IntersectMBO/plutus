a525e55d21bbafee30161bea07440a546847d22f
Jann Müller <j-mueller@users.noreply.github.com>
2021-04-13 07:56:53 +0200

    SCP-2107: JSON encoding bug (#2981)
    * SCP-2107: JSON encoding bug
    * Add a test sending a response to the contact

--- plutus-pab/src/Plutus/PAB/ContractCLI.hs
+++ plutus-pab/src/Plutus/PAB/ContractCLI.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DataKinds            #-}
 {-# LANGUAGE DerivingStrategies   #-}
 {-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE LambdaCase           #-}
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE RankNTypes           #-}
 {-# LANGUAGE TypeApplications     #-}
@@ -25,29 +27,56 @@ module Plutus.PAB.ContractCLI
     , runCliCommand
     , runUpdate
     , Command(..)
+    -- * Debugging
+    , contractCliApp
+    , runPromptPure
+    , runPromptIO
     ) where
 
-import           Control.Monad.IO.Class   (MonadIO, liftIO)
-import           Data.Aeson               (FromJSON, ToJSON)
-import qualified Data.Aeson               as JSON
-import qualified Data.Aeson.Encode.Pretty as JSON
-import           Data.Bifunctor           (bimap)
-import qualified Data.ByteString          as BS
-import qualified Data.ByteString.Char8    as BS8
-import qualified Data.ByteString.Lazy     as BSL
-import           Data.Proxy               (Proxy (..))
-import           Data.Row                 (AllUniqueLabels, Forall, type (.\\))
-import           Data.Text                (Text)
-import qualified Data.Text                as Text
-import           Options.Applicative      (CommandFields, Mod, Parser, command, customExecParser, disambiguate,
-                                           fullDesc, helper, idm, info, prefs, progDesc, showHelpOnEmpty,
-                                           showHelpOnError, subparser)
-import           Playground.Schema        (EndpointToSchema, endpointsToSchemas)
-import           Plutus.Contract          (BlockchainActions, Contract)
-import           Plutus.Contract.Schema   (Input, Output)
-import qualified Plutus.Contract.State    as ContractState
-import           System.Exit              (ExitCode (ExitFailure), exitSuccess, exitWith)
+import           Control.Monad.Freer               (Eff, LastMember, Member, interpret, run, runM, send, sendM,
+                                                    type (~>))
+import           Control.Monad.Freer.Error         (Error, runError, throwError)
+import qualified Control.Monad.Freer.Extras.Modify as Modify
+import           Control.Monad.IO.Class            (liftIO)
+import           Data.Aeson                        (FromJSON, ToJSON)
+import qualified Data.Aeson                        as JSON
+import qualified Data.Aeson.Encode.Pretty          as JSON
+import           Data.Bifunctor                    (bimap)
+import qualified Data.ByteString                   as BS
+import qualified Data.ByteString.Char8             as BS8
+import qualified Data.ByteString.Lazy              as BSL
+import           Data.Foldable                     (traverse_)
+import           Data.Proxy                        (Proxy (..))
+import           Data.Row                          (AllUniqueLabels, Forall, type (.\\))
+import           Data.Text                         (Text)
+import qualified Data.Text                         as Text
+import           Options.Applicative               (CommandFields, Mod, Parser, ParserResult, command, disambiguate,
+                                                    execParserPure, fullDesc, helper, idm, info, prefs, progDesc,
+                                                    showHelpOnEmpty, showHelpOnError, subparser)
+import qualified Options.Applicative
+import           Playground.Schema                 (EndpointToSchema, endpointsToSchemas)
+import           Plutus.Contract                   (BlockchainActions, Contract)
+import           Plutus.Contract.Schema            (Input, Output)
+import qualified Plutus.Contract.State             as ContractState
+import           Prelude                           hiding (getContents)
+import           System.Environment                (getArgs)
+import           System.Exit                       (ExitCode (ExitFailure), exitSuccess, exitWith)
 import qualified System.IO
+
+-- | Read from stdin
+data CliEff r where
+    GetContents :: CliEff BS.ByteString -- ^ Read from stdin
+    HandleParseResult :: Show a => ParserResult a -> CliEff a -- ^ Deal with a 'ParserResult' from @optparse-applicative@
+
+getContents :: forall effs. Member CliEff effs => Eff effs BS.ByteString
+getContents = send GetContents
+
+handleParseResult :: forall a effs. (Member CliEff effs, Show a) => ParserResult a -> Eff effs a
+handleParseResult = send . HandleParseResult
+
+-- | A program that takes some argument, reads some input
+--   and returns either an error or a response
+type Prompt a = Eff '[CliEff, Error [BS.ByteString]] a
 
 data Command
     = Initialise
@@ -77,27 +106,26 @@ exportSignatureParser =
     command "export-signature" $
     info (pure ExportSignature) (fullDesc <> progDesc "Export the contract's signature.")
 
-runCliCommand :: forall w s s2 m.
+runCliCommand :: forall w s s2.
        ( AllUniqueLabels (Input s)
        , Forall (Input s) FromJSON
        , Forall (Output s) ToJSON
        , Forall (Input s) ToJSON
        , EndpointToSchema (s .\\ s2)
-       , MonadIO m
        , ToJSON w
        , Monoid w
        )
     => Proxy s2
     -> Contract w s Text ()
     -> Command
-    -> m (Either BS8.ByteString BS8.ByteString)
-runCliCommand _ schema Initialise = pure $ Right $ BSL.toStrict $ JSON.encodePretty $ ContractState.initialiseContract schema
+    -> Prompt BS8.ByteString
+runCliCommand _ schema Initialise = pure $ BSL.toStrict $ JSON.encodePretty $ ContractState.initialiseContract schema
 runCliCommand _ schema Update = do
-    arg <- liftIO BS.getContents
-    pure $ runUpdate schema arg
+    arg <- getContents
+    runUpdate schema arg
 runCliCommand _ _ ExportSignature = do
   let r = endpointsToSchemas @(s .\\ s2)
-  pure $ Right $ BSL.toStrict $ JSON.encodePretty r
+  pure $ BSL.toStrict $ JSON.encodePretty r
 
 runUpdate :: forall w s.
     ( AllUniqueLabels (Input s)
@@ -109,8 +137,8 @@ runUpdate :: forall w s.
     )
     => Contract w s Text ()
     -> BS.ByteString
-    -> Either BS8.ByteString BS8.ByteString
-runUpdate contract arg =
+    -> Prompt BS8.ByteString
+runUpdate contract arg = either (throwError @[BS.ByteString] . return) pure $
     bimap
         (BSL.toStrict . JSON.encodePretty . Text.pack)
         (BSL.toStrict . JSON.encodePretty . ContractState.insertAndUpdateContract contract)
@@ -145,17 +173,57 @@ commandLineApp' :: forall w s s2.
     => Proxy s2
     -> Contract w s Text ()
     -> IO ()
-commandLineApp' p schema = do
-    cmd <-
-        customExecParser
-            (prefs $ disambiguate <> showHelpOnEmpty <> showHelpOnError)
-            (info (helper <*> commandLineParser) idm)
-    result <- runCliCommand p schema cmd
+commandLineApp' p schema = runPromptIO (contractCliApp  p schema)
+
+contractCliApp :: forall w s s2.
+       ( AllUniqueLabels (Input s)
+       , Forall (Input s) FromJSON
+       , Forall (Input s) ToJSON
+       , Forall (Output s) ToJSON
+       , EndpointToSchema (s .\\ s2)
+       , ToJSON w
+       , Monoid w
+       )
+    => Proxy s2
+    -> Contract w s Text ()
+    -> [String]
+    -> Prompt BS.ByteString
+contractCliApp p schema args = do
+    cmd <- handleParseResult $ execParserPure (prefs $ disambiguate <> showHelpOnEmpty <> showHelpOnError) (info (helper <*> commandLineParser) idm) args
+    runCliCommand p schema cmd
+
+handleCliEffIO ::
+    forall effs.
+    LastMember IO effs
+    => CliEff
+    ~> Eff effs
+handleCliEffIO = \case
+    GetContents         -> sendM BS.getContents
+    HandleParseResult a -> liftIO (Options.Applicative.handleParseResult a)
+
+handleCliEffPure ::
+    forall effs.
+    Member (Error [BS.ByteString]) effs
+    => BS.ByteString
+    -> CliEff
+    ~> Eff effs
+handleCliEffPure input = \case
+    GetContents -> return input
+    HandleParseResult a -> case Options.Applicative.getParseResult a of
+        Nothing -> throwError @[BS.ByteString] [BS8.pack $ show a]
+        Just a' -> pure a'
+
+runPromptIO :: ([String] -> Prompt BS.ByteString) -> IO ()
+runPromptIO p = do
+    args <- getArgs
+    result <- runM $ runError $ interpret handleCliEffIO $ Modify.raiseEnd $ p args
     case result of
-        Left err -> do
-            BS8.hPut System.IO.stderr "Error "
-            BS8.hPut System.IO.stderr (BSL.toStrict $ JSON.encodePretty err)
+        Left errs -> do
+            traverse_ (BS8.hPut System.IO.stderr) errs
             exitWith $ ExitFailure 1
         Right response -> do
             BS8.putStrLn response
             exitSuccess
+
+runPromptPure :: Prompt a -> BS.ByteString -> Either [BS.ByteString] a
+runPromptPure p input = run $ runError $ interpret (handleCliEffPure input) p
