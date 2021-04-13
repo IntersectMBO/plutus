c03d040c555351e3a94023b1bee8036e79175696
Tobias Pflug <tobias.pflug@gmail.com>
2021-04-13 16:55:02 +0200

    building deployments: build `toplevel` attribute
    Instead of building the whole attrset that `pkgs.nixos` returns just
    pick the `toplevel` derivation. This way we only get a single derivation
    instead of an attribute set which was causing problems when collecting
    derivations for hydra.

--- deployment/morph/default.nix
+++ deployment/morph/default.nix
@@ -29,7 +29,7 @@ let
   # Get a `buildMachine` function that wraps a `mkMachine` call with the fake deployment option
   # in a `pkgs.nixos` call to build the machine outside of morph.
   mkMachine = pkgs.callPackage ./mk-machine.nix { inherit plutus tfinfo; extraImports = [ fakeDeploymentOption ]; };
-  buildMachine = { config, name }: pkgs.nixos (mkMachine { inherit config name; });
+  buildMachine = { config, name }: (pkgs.nixos (mkMachine { inherit config name; })).toplevel;
 in
 import ./machines.nix {
   inherit pkgs tfinfo;
