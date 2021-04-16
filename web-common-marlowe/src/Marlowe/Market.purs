5fe6c90f1a4f024065d7811971609b79538a344a
Pablo Lamela <pablo.lamela@iohk.io>
2021-04-16 14:39:02 +0100

    SCP-2097 - Implement Contract for Differences with Oracle (#2999)
    * Add support for inverse rates in kraken queries
    * Fix conversion of Rational from Haskell and other details
    * Add CFD with Oracle example
    * Address review by @simonjohnthompson

--- web-common-marlowe/src/Marlowe/Market.purs
+++ web-common-marlowe/src/Marlowe/Market.purs
@@ -2,13 +2,14 @@ module Marlowe.Market (contractTemplates) where
 
 -- At some point we will want to keep contract templates in a database. In the
 -- meantime, this is a simple solution to get things up and running.
-import Marlowe.Extended.Template (ContractTemplate)
+import Examples.PureScript.ContractForDifferences as ContractForDifferences
+import Examples.PureScript.ContractForDifferencesWithOracle as ContractForDifferencesWithOracle
+import Examples.PureScript.CouponBondGuaranteed as CouponBondGuaranteed
 import Examples.PureScript.Escrow as Escrow
 import Examples.PureScript.EscrowWithCollateral as EscrowWithCollateral
-import Examples.PureScript.ZeroCouponBond as ZeroCouponBond
-import Examples.PureScript.CouponBondGuaranteed as CouponBondGuaranteed
 import Examples.PureScript.Swap as Swap
-import Examples.PureScript.ContractForDifferences as ContractForDifferences
+import Examples.PureScript.ZeroCouponBond as ZeroCouponBond
+import Marlowe.Extended.Template (ContractTemplate)
 
 contractTemplates :: Array ContractTemplate
 contractTemplates =
@@ -18,4 +19,5 @@ contractTemplates =
   , CouponBondGuaranteed.contractTemplate
   , Swap.contractTemplate
   , ContractForDifferences.contractTemplate
+  , ContractForDifferencesWithOracle.contractTemplate
   ]
