714d19758d974c4a19257d0bc3cb60261c0a91ec
Hernan Rajchert <hrajchert@gmail.com>
2021-04-28 16:48:19 -0300

    Only do css minification on production build

--- marlowe-website/postcss.config.js
+++ marlowe-website/postcss.config.js
@@ -1,18 +1,21 @@
 "use strict";
 
+const extraPlugins =
+  process.env.NODE_ENV === "production"
+    ? [
+        require("cssnano")({
+          preset: [
+            "default",
+            {
+              discardComments: {
+                removeAll: true,
+              },
+            },
+          ],
+        }),
+      ]
+    : [];
+
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("cssnano")({
-      preset: [
-        "default",
-        {
-          discardComments: {
-            removeAll: true,
-          },
-        },
-      ],
-    }),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer"), ...extraPlugins],
 };
