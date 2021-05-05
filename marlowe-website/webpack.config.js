b37a5a96adf8ed2b2b4cae4e36bc5e0ec4f2b419
Hernan Rajchert <hrajchert@gmail.com>
2021-05-05 17:54:09 -0300

    wip2

--- marlowe-website/webpack.config.js
+++ marlowe-website/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     filename: "js/main-[chunkhash].js",
     path: path.resolve(__dirname, "public"),
+    publicPath: "/",
     clean: true,
   },
   devServer: {
