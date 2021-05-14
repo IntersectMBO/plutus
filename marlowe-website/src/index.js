d9bbf18b0b92a76b21f9c92256a15c4e740193ec
Hernan Rajchert <hrajchert@gmail.com>
2021-05-14 09:37:26 -0300

    scp-1865: First version of the marlowe website (#3182)

--- marlowe-website/src/index.js
+++ marlowe-website/src/index.js
@@ -1,5 +1,39 @@
-import "tailwindcss/tailwind.css"
+import "./index.css";
 
-window.onload = function () {
-  console.log('Placeholder');
+function initializeFaqComponent() {
+  const faqElements = document.getElementsByClassName("faq");
+  for (const faqComponent of faqElements) {
+    faqComponent.addEventListener("click", () => faqComponent.classList.toggle("expanded"));
+  }
 }
+
+function initializeHeaderComponent() {
+  const header = document.getElementsByTagName("header")[0];
+  const headerHeight = header.getBoundingClientRect().height;
+  const mainTryButtons = document.getElementById("main-try-buttons");
+
+  window.onscroll = function () {
+    // The header always have fixed position and as soon as the scroll moves we add
+    // a background to it
+    if (window.scrollY > 0) {
+      header.classList.add("bg-blured");
+    } else {
+      header.classList.remove("bg-blured");
+    }
+
+    // The header "try Run/Build" buttons appear when the same buttons in the main section
+    // are no longer visible
+    const mainButtonBoundingBox = mainTryButtons.getBoundingClientRect();
+    if (headerHeight > mainButtonBoundingBox.y + mainButtonBoundingBox.height) {
+      header.classList.add("with-buttons");
+    }
+    // and dissapear once the main buttons are fully visible again
+    if (headerHeight <= mainButtonBoundingBox.y) {
+      header.classList.remove("with-buttons");
+    }
+  };
+}
+window.onload = function () {
+  initializeFaqComponent();
+  initializeHeaderComponent();
+};
