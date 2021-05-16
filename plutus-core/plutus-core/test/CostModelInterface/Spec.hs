23146d4635e931fc5c59d025128b499a0a81e74d
kwxm <kenneth.mackenzie@iohk.io>
2021-05-16 11:06:59 +0100

    Improve cost model update tests

--- plutus-core/plutus-core/test/CostModelInterface/Spec.hs
+++ plutus-core/plutus-core/test/CostModelInterface/Spec.hs
@@ -11,7 +11,8 @@ import           PlutusCore.Evaluation.Machine.CostModelInterface
 import           PlutusCore.Evaluation.Machine.ExBudget
 import           PlutusCore.Evaluation.Machine.MachineParameters
 
--- import           Data.Aeson
+import qualified Data.Map                                         as Map
+import qualified Data.Text                                        as Text
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
@@ -30,57 +31,125 @@ randomCekCosts =
                     , cekBuiltinCost = ExBudget 4 4
                     }
 
+cekVarCostCpuKey :: Text.Text
+cekVarCostCpuKey = "cek_var_cost-_ex_budget_cpu"  -- flatten . camelToSnake
+
 randomCekCostModel :: CekCostModel
 randomCekCostModel = CostModel randomCekCosts defaultBuiltinCostModel
 
-test_costModelInterface :: TestTree
-test_costModelInterface =
-    testGroup "cost model interface tests" [test_extract, test_apply]
-
-test_extract :: TestTree
-test_extract = testGroup "extractCostModelParams works"
-               [ testCase "defaultCekCostModel" $ mkExtractionTest defaultCekCostModel
-               , testCase "randomCekCostModel"    $ mkExtractionTest randomCekCostModel
-               ]
 
 -- | Extract the params from a cost model and return them, failing if it doesn't work
 extractParams :: CekCostModel -> IO CostModelParams
 extractParams model = do
-    case extractCostModelParams model of
-         Nothing -> assertFailure "extractCostModelParams failed"
-         Just p  -> pure p
+  case extractCostModelParams model of
+    Nothing -> assertFailure "extractCostModelParams failed"
+    Just p  -> pure p
 
 -- | Extract some params from a cost model and return the updated version, failing if it doesn't work
 applyParams :: CekCostModel -> CostModelParams -> IO CekCostModel
 applyParams model params = do
-    case applyCostModelParams model params of
-      Nothing     ->  assertFailure "applyCostModelParams failed"
-      Just model' -> pure model'
+  case applyCostModelParams model params of
+    Nothing     -> assertFailure "applyCostModelParams failed"
+    Just model' -> pure model'
 
 -- | Just check that extraction works.
-mkExtractionTest :: CekCostModel -> IO ()
-mkExtractionTest model = do
-  _extracted <- extractParams model
---  putStrLn $ show _extracted
+testExtraction :: CekCostModel -> IO ()
+testExtraction model = do
+  _extracted <- extractParams model  --We're not going to use this, but it may still fail.
   pure ()
 
--- Update model1 with the parameters fom model2 and check that the extracted params from model1 match those from model2
-mkUpdateTest :: CekCostModel -> CekCostModel -> IO ()
-mkUpdateTest model1 model2 = do
-    params1 <- extractParams model1
-    updated <- applyParams model2 params1
-    params2 <- extractParams updated
-    params1 @?= params2
+-- Update a model with its own parameters and check that we get the same model back
+testSelfUpdate :: CekCostModel -> IO ()
+testSelfUpdate model = do
+  params <- extractParams model
+  updated <- applyParams model params
+  updated @?= model
 
--- TODO: we could do with a test to check that if you extract the params from a
--- model M and then apply them to M, nothing changes.  We need an instance of Eq
--- for cost models for this, but that's tricky.  Can we fake one using toJSON
--- for example?
+-- Update a model model1 with the parameters from model2 and check that we get model2
+testOverwrite :: CekCostModel -> CekCostModel -> IO ()
+testOverwrite model1 model2 = do
+  params <- extractParams model2
+  updated <- applyParams model1 params
+  updated @?= model2
 
-test_apply :: TestTree
-test_apply = testGroup "applyCostModelParams works"
-         [ testCase "defaultCekCostModel <- defaultCekCostModel" $ mkUpdateTest defaultCekCostModel defaultCekCostModel
-         , testCase "defaultCekCostModel <- randomCekCostModel"  $ mkUpdateTest defaultCekCostModel randomCekCostModel
-         , testCase "randomCekCostModel  <- randomCekCostModel"  $ mkUpdateTest randomCekCostModel  randomCekCostModel
-         , testCase "randomCekCostModel  <- defaultCekCostModel" $ mkUpdateTest randomCekCostModel  defaultCekCostModel
-         ]
+-- Update a model with its own params with an extra entry: this is apparently OK,
+-- although what happens to that entry when we convert back to a model?
+testSelfUpdateWithExtraEntry :: CekCostModel -> IO ()
+testSelfUpdateWithExtraEntry model =
+    do
+      params <- extractParams model
+      let params' = Map.insert "XYZ" 123 params
+      model' <- applyParams model params'
+      model @?= model'
+
+-- Update a model with its own params with an entry deleted: this should
+-- be OK because the original member of the model will still be there.
+testSelfUpdateWithMissingEntry :: CekCostModel -> IO ()
+testSelfUpdateWithMissingEntry model =
+    do
+      params <- extractParams model
+      assertBool "CekVarCost not found in params" (Map.member cekVarCostCpuKey params)
+      let params' = Map.delete cekVarCostCpuKey params
+      model' <- applyParams model params'
+      model @?= model'
+
+-- Update a model with the params from another model with an entry
+-- deleted.  The result should be different from both of the original models.
+testOtherUpdateWithMissingEntry :: CekCostModel -> CekCostModel -> IO ()
+testOtherUpdateWithMissingEntry model1 model2 =
+    do
+      params2 <- extractParams model2
+      assertBool "CekVarCost not found in params" (Map.member cekVarCostCpuKey params2)
+      let params2' = Map.delete cekVarCostCpuKey params2
+      assertBool "CekVarCost still in params" (not (Map.member cekVarCostCpuKey params2'))
+      assertBool "params are the same" (params2 /= params2')
+      model1' <- applyParams model1 params2'
+      assertBool "The updated model is the same as the other model" (model1' /= model2)
+      assertBool "The updated model is the same as the original"    (model1' /= model1)
+
+-- Update a model with the params from another and then chdck that
+-- extraction returns the same params.
+testExtractAfterUpdate ::  CekCostModel -> CekCostModel -> IO ()
+testExtractAfterUpdate model1 model2 =
+    do
+      params <- extractParams model2
+      updated <- applyParams model1 params
+      params' <- extractParams updated
+      params @?= params'
+
+test_costModelInterface :: TestTree
+test_costModelInterface =
+    testGroup" cost model interface tests"
+       [ testGroup "extractCostModelParams works"
+       [ testCase "defaultCekCostModel" $ testExtraction defaultCekCostModel
+       , testCase "randomCekCostModel"  $ testExtraction randomCekCostModel
+       ]
+     , testGroup "self-update is identity"
+       [ testCase "defaultCekCostModel <- defaultCekCostModel" $ testSelfUpdate defaultCekCostModel
+       , testCase "randomCekCostModel  <- randomCekCostModel"  $ testSelfUpdate randomCekCostModel
+       ]
+     , testGroup "overwriting works"
+       [ testCase "defaultCekCostModel <- randomCekCostModel"  $ testOverwrite defaultCekCostModel randomCekCostModel
+       , testCase "randomCekCostModel  <- defaultCekCostModel" $ testOverwrite randomCekCostModel  defaultCekCostModel
+       ]
+     , testGroup "superfluous entry in params is OK in self-update"
+       [ testCase "defaultCekCostModel" $ testSelfUpdateWithExtraEntry defaultCekCostModel
+       , testCase "randomCekCostModel"  $ testSelfUpdateWithExtraEntry randomCekCostModel
+       ]
+     , testGroup "missing entry in params is OK in self-update"
+       [ testCase "defaultCekCostModel" $ testSelfUpdateWithMissingEntry defaultCekCostModel
+       , testCase "randomCekCostModel"  $ testSelfUpdateWithMissingEntry randomCekCostModel
+       ]
+     , testGroup "missing entry in update of different model"
+       [ testCase "defaultCekCostModel" $ testOtherUpdateWithMissingEntry defaultCekCostModel randomCekCostModel
+       , testCase "randomCekCostModel"  $ testOtherUpdateWithMissingEntry randomCekCostModel defaultCekCostModel
+       ]
+     , testGroup "extract after apply is identity"
+       [ testCase "defaultCekCostModel <- defaultCekCostModel" $ testExtractAfterUpdate defaultCekCostModel defaultCekCostModel
+       , testCase "randomCekCostModel  <- randomCekCostModel"  $ testExtractAfterUpdate randomCekCostModel  randomCekCostModel
+       , testCase "randomCekCostModel  <- defaultCekCostModel" $ testExtractAfterUpdate randomCekCostModel  defaultCekCostModel
+       , testCase "defaultCekCostModel <- randomCekCostModel"  $ testExtractAfterUpdate defaultCekCostModel randomCekCostModel
+       ]
+     ]
+
+
