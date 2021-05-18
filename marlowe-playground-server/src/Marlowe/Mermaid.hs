ea897982c469921697e4729a847c6e33f04613b5
Hernan Rajchert <hrajchert@gmail.com>
2021-05-18 18:19:00 -0300

    Applied feedback

--- marlowe-playground-server/src/Marlowe/Mermaid.hs
+++ marlowe-playground-server/src/Marlowe/Mermaid.hs
@@ -26,8 +26,8 @@ module Marlowe.Mermaid (toMermaid) where
 
 import           Data.Hashable             (hash)
 import           Data.List                 (nub)
+import           GHC.Real                  (denominator, numerator)
 import           Language.Marlowe.Extended
-
 
 -- | Convert the 'Contract' DSL into a flat list (like converting from a graph
 -- representation to a vertex list). The result is a list denoting the
@@ -113,7 +113,7 @@ valueShow (MulValue val1 val2) = "(" ++ valueShow val1 ++ " * " ++ valueShow val
 valueShow (ChoiceValue (ChoiceId id party)) = show party ++ " choice on " ++ show id
 valueShow (Cond obs val1 val2) = "(" ++ observationShow obs ++ " ? " ++ valueShow val1 ++ " : " ++ valueShow val2 ++ ")"
 valueShow (UseValue (ValueId id)) = show id
-valueShow (Scale fraction val) = "(" ++ show fraction ++ " * " ++ valueShow val ++ ")"
+valueShow (Scale fraction val) = "(" ++ show (numerator fraction) ++ " / " ++ show (denominator fraction) ++ " * " ++ valueShow val ++ ")"
 valueShow (AvailableMoney party _) = show party ++ "'s available money"
 valueShow SlotIntervalStart  = "slot interval start"
 valueShow SlotIntervalEnd  = "slot interval end"
