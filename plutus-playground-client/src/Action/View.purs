867551d6191eb12acd27ba1fa40e12b070fca03f
Luigy Leon <luigy@outlook.com>
2021-05-21 06:51:40 -0400

    Use action index to give labels unique name (#3234)
    fixes #3196

--- plutus-playground-client/src/Action/View.purs
+++ plutus-playground-client/src/Action/View.purs
@@ -93,7 +93,7 @@ actionPaneBody index (CallEndpoint { caller, argumentValues }) =
         , text ": "
         , text $ view (_FunctionSchema <<< _endpointDescription <<< _getEndpointDescription) argumentValues
         ]
-    , actionArgumentForm (PopulateAction index) $ view (_FunctionSchema <<< _argument) argumentValues
+    , actionArgumentForm index (PopulateAction index) $ view (_FunctionSchema <<< _argument) argumentValues
     ]
 
 actionPaneBody index (PayToWallet { sender, recipient, amount }) =
