86c1d56ead57e55f681ecd995b8012813396d3ef
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2021-05-25 14:32:25 +0200

    Plutus chain index package skeleton

---
+++ nix/pkgs/haskell/materialized-unix/.plan.nix/plutus-chain-index.nix
@@ -0,0 +1,57 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "plutus-chain-index"; version = "0.1.0.0"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "jann.mueller@iohk.io";
+      author = "Jann Müller";
+      homepage = "https://github.com/iohk/plutus#readme";
+      url = "";
+      synopsis = "";
+      description = "Please see the README on GitHub at <https://github.com/input-output-hk/plutus#readme>";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" "NOTICE" ];
+      dataDir = ".";
+      dataFiles = [];
+      extraSrcFiles = [];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."plutus-ledger" or (errorHandler.buildDepError "plutus-ledger"))
+          (hsPkgs."plutus-ledger-api" or (errorHandler.buildDepError "plutus-ledger-api"))
+          (hsPkgs."plutus-tx" or (errorHandler.buildDepError "plutus-tx"))
+          (hsPkgs."plutus-contract" or (errorHandler.buildDepError "plutus-contract"))
+          (hsPkgs."freer-extras" or (errorHandler.buildDepError "freer-extras"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          ];
+        buildable = true;
+        modules = [ "Plutus/ChainIndex" ];
+        hsSourceDirs = [ "src" ];
+        };
+      tests = {
+        "plutus-chain-index-test" = {
+          depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+          buildable = true;
+          modules = [ "Spec" ];
+          hsSourceDirs = [ "test" ];
+          mainPath = [ "Spec.hs" ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../plutus-chain-index; }
