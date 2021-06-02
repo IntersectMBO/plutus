88fd69b0409c962e394545d82c6efd8a939cf626
Jann Mueller <jann.mueller@iohk.io>
2021-06-02 10:57:40 +0200

    SCP-2317: Integrate cardano-api (WIP)

---
+++ plutus-contract/src/Plutus/Contract/CardanoAPI.hs
@@ -0,0 +1,9 @@
+{-
+
+Interface to the transaction types from 'cardano-api'
+
+-}
+module Plutus.Contract.CardanoAPI() where
+
+-- TODO: Conversion functions between our Tx types and those from Cardano.Api
+-- https://input-output-hk.github.io/cardano-node/cardano-api/Cardano-Api.html
