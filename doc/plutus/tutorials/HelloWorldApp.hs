3aca46a05f64b4ba026dd52ab96e266625e92f00
Jann Mueller <jann.mueller@iohk.io>
2021-06-11 10:37:01 +0200

    Fix doc

--- doc/plutus/tutorials/HelloWorldApp.hs
+++ doc/plutus/tutorials/HelloWorldApp.hs
@@ -1,22 +1,29 @@
+{-# LANGUAGE PolyKinds        #-}
 {-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies     #-}
+{-# LANGUAGE TypeOperators    #-}
 module HelloWorldApp where
 
 import qualified Data.Text           as T
 import           Playground.Contract
-import           Plutus.Contract     hiding (when)
+import           Plutus.Contract
 import           PlutusTx.Prelude
 
 
 -- BLOCK1
+
 -- | A 'Contract' that logs a message.
 hello :: Contract () EmptySchema T.Text ()
 hello = logInfo @String "Hello, world"
+
 -- BLOCK2
 
 endpoints :: Contract () EmptySchema T.Text ()
 endpoints = hello
 
-mkSchemaDefinitions ''EmptySchema
+type DummySchema = Endpoint "dummy" ()
+
+mkSchemaDefinitions ''DummySchema
 
 $(mkKnownCurrencies [])
