4af385909299699061d55f481e61aba8fc14686f
Kenneth MacKenzie <kwxm@inf.ed.ac.uk>
2021-06-15 09:48:27 +0100

    Kwxm/extend validation benchmarks (SCP-2354) (#3346)
    * Update data files for new use-case scripts
    
    * Update validation benchmarking code etc.
    
    * Update validation benchmarking code etc.
    
    * Remove mysteriously missing scripts
    
    * Restore Marlowe benchmarks
    
    * Rename files
    
    * WIP
    
    * Add option to run a small set of benchmarks
    
    * Restore flat benchmarks in cabal file
    
    * Respond to PR comments

--- plutus-benchmark/validation/Main.hs
+++ plutus-benchmark/validation/Main.hs
@@ -1,89 +1,120 @@
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main where
 
-import           Paths_plutus_benchmark                   (getDataFileName)
-
+import           Paths_plutus_benchmark                   (getDataDir, getDataFileName)
 
 import qualified PlutusCore                               as PLC
-import qualified PlutusCore.Pretty                        as PP
 
 import qualified UntypedPlutusCore                        as UPLC
 import qualified UntypedPlutusCore.Evaluation.Machine.Cek as UPLC
 
 import           Criterion.Main
+import           Criterion.Main.Options                   (Mode, parseWith)
 import           Criterion.Types                          (Config (..))
+import           Options.Applicative
 
-import           Control.Monad
+import           Control.DeepSeq                          (force)
 import           Control.Monad.Trans.Except               (runExceptT)
+import qualified Data.ByteString                          as BS
 import qualified Data.ByteString.Lazy                     as BSL
+import           Data.List                                (sort)
+import           Flat
+import           System.Directory                         (listDirectory)
 import           System.FilePath
-import           Text.Printf                              (printf)
 
-{-- | This set of benchmarks is based on validations occurring in the tests in
-  plutus-use-cases.  Those tests are run on the blockchain simulator, and a
-  modified version of Ledger.Scripts was used to extract validator scripts and
-  their arguments.  These are stored in the `data` directory as Plutus Core
-  source code, along with README files explaining which scripts were involved in
-  each validation during the tests.  --}
+{-- | Benchmarks based on validations obtained using
+plutus-use-cases:plutus-use-cases-scripts, which runs various contracts on the
+blockchain simulator and dumps the applied validators as flat-encoded
+scripts. Generating these scripts is a very lengthy process involving building a
+lot of code, so the scripts were generated once and copied to the 'data'
+directory here.
 
-type Term a    = UPLC.Term PLC.Name PLC.DefaultUni PLC.DefaultFun a
-type Program a = UPLC.Program PLC.Name PLC.DefaultUni PLC.DefaultFun a
-type PlcParserError = PLC.Error PLC.DefaultUni PLC.DefaultFun PLC.AlexPosn
+NB. Running these benchmarks with `stack bench` will use copies of the scripts
+in `.stack_work` (and accessed via Paths_plutus_benchmark), and if a file in
+`data` is removed and the benchmarks are re-run, the benchmarking code may still
+be able to access the old copy in stack's files.  --}
 
-loadPlcSource :: FilePath -> IO (Program ())
-loadPlcSource file = do
-  contents <- BSL.readFile file
-  PLC.runQuoteT $ runExceptT (UPLC.parseScoped contents) >>= \case
-     Left (err::PlcParserError) -> error $ "Parser error: " ++ PP.displayPlcDef err
-     Right p                    -> return $ () <$ p
+{- | The name of the directory where the scripts are kept.  This must match the
+   location of the files relative to the directory containing the cabal file.
+   IF THE DIRECTORY IS MOVED, THIS MUST BE UPDATED. -}
+getScriptDirectory :: IO FilePath
+getScriptDirectory = do
+  root <- getDataDir
+  return $ root </> "validation" </> "data"
 
-benchCek :: Term () -> Benchmarkable
-benchCek program = nf (UPLC.unsafeEvaluateCek PLC.defaultCekParameters) program
+{- | Subdirectories for different contracts. The hierarchical arrangement makes
+   it easier to group benchmarks. Any changes here should be reflected in the
+   "data-files" section of the cabal file to ensure that Paths_plutus_benchmark
+   still works. -}
+contractDirs :: [FilePath]
+contractDirs =
+    [ "crowdfunding"
+    , "currency"
+    , "escrow"
+    , "future"
+    , "game-sm"
+    , "multisig-sm"
+    , "ping-pong"
+    , "prism"
+    , "pubkey"
+    , "stablecoin"
+    , "token-account"
+    , "vesting"
+    , "marlowe" </> "trustfund"  -- The Marlowe examples aren't included in plutus-use-cases-scripts
+    , "marlowe" </> "zerocoupon"
+    ]
+
+-- | A small subset of the contracts for quick benchmarking
+contractDirs2 :: [FilePath]
+contractDirs2 =
+    [ "crowdfunding"
+    , "prism"
+    , "token-account"
+    , "marlowe" </> "zerocoupon"
+    ]
 
 
-plcSuffix :: String
-plcSuffix = "plc"
+type Term          = UPLC.Term    PLC.Name      PLC.DefaultUni PLC.DefaultFun ()
+type Program       = UPLC.Program PLC.Name      PLC.DefaultUni PLC.DefaultFun ()
+type DbProgram     = UPLC.Program UPLC.DeBruijn PLC.DefaultUni PLC.DefaultFun ()
 
--- The name of the directory where the scripts are kept.
--- This must match the location of the files relative to the directory containing the cabal file.
--- IF THE DIRECTORY IS MOVED, THIS MUST BE UPDATED.
-scriptDirectory :: String
-scriptDirectory = "validation" </> "data"
+fromDeBruijn :: DbProgram -> IO Program
+fromDeBruijn prog = do
+    let namedProgram = UPLC.programMapNames (\(UPLC.DeBruijn ix) -> UPLC.NamedDeBruijn "v" ix) prog
+    case PLC.runQuote $ runExceptT @UPLC.FreeVariableError $ UPLC.unDeBruijnProgram namedProgram of
+      Left e  -> errorWithoutStackTrace $ show e
+      Right p -> return p
 
-{- Construct an applied validator.  We assume that relevant validators, datum
-   scripts, redeemers and contexts are stored in CBOR format under `<progName>`
-   in the `data` directory.  These should have names like "Redeemer01.cbor",
-   "Context03.cbor", and so on. This function returnes a Criterion environment
-   to be fed to the relevant benchmark, to keep the IO overhead out of the
-   benchmark itself. -}
-getAppliedScript :: String -> Int -> Int -> Int -> Int -> IO (Term ())
-getAppliedScript progName validatorNumber datumNumber redeemerNumber contextNumber = do
-  let loadScript base scriptNumber = do
-          let file = scriptDirectory </> progName </> (base ++ printf "%02d" scriptNumber) <.> plcSuffix
-          dataPath <- getDataFileName file
-          loadPlcSource dataPath
-  validator <- loadScript "Validator" validatorNumber
-  datum     <- loadScript "Datum"     datumNumber
-  redeemer  <- loadScript "Redeemer"  redeemerNumber
-  context   <- loadScript "Context"   contextNumber
-  let appliedValidator = validator `UPLC.applyProgram` datum `UPLC.applyProgram` redeemer `UPLC.applyProgram` context
-  pure $ void . UPLC.toTerm $ appliedValidator
+loadFlat :: FilePath -> IO Term
+loadFlat file = do
+  contents <- BSL.fromStrict <$> BS.readFile file
+  case unflat contents of
+    Left e  -> errorWithoutStackTrace $ "Flat deserialisation failure for " ++ file ++ ": " ++ show e
+    Right r -> do
+        p <- fromDeBruijn r
+        return $! force $ UPLC.toTerm p
+        -- `force` to try to ensure that deserialiation is not included in benchmarking time.
 
-{- Create a benchmark with a name like "crowdfunding/5" by applying validator
-   number v to datum number d, redeemer number r, and context number c in the
-   directory data/<dirname>.  The 'bmId' argument is just to make the names of the
-   indvidual benchmarks more readable and more easily typed. -}
-mkBM :: String -> (Int, (Int, Int, Int, Int)) -> Benchmark
-mkBM dirname (bmId, (v,d,r,c)) =
-    env (getAppliedScript dirname v d r c) $ \script -> bench (show bmId) $ benchCek script
+mkCekBM :: Term -> Benchmarkable
+mkCekBM program = nf (UPLC.unsafeEvaluateCek PLC.defaultCekParameters) program
 
--- Make a `bgroup` collecting together a number of benchmarks for the same contract
-mkBgroup :: String -> [(Int, (Int, Int, Int, Int))] -> Benchmark
-mkBgroup dirname bms = bgroup dirname (map (mkBM dirname) bms)
+mkScriptBM :: FilePath -> FilePath -> Benchmark
+mkScriptBM dir file =
+    env (loadFlat $ dir </> file) $ \script -> bench (dropExtension file) $ mkCekBM script
 
+-- Make a benchmark group including benchmarks for all the files in a given directory.
+mkContractBMs :: FilePath -> IO Benchmark
+mkContractBMs dirName = do
+  scriptDirectory <- getScriptDirectory
+  let dirPath = scriptDirectory </> dirName
+  files <- listDirectory dirPath
+  let files' = sort $ filter (isExtensionOf ".flat") files  -- Just in case there's anything else in the directory.
+  return $ bgroup dirName $ fmap (mkScriptBM dirPath) files'
+
+
+----------------------- Main -----------------------
 
 {- | The Criterion configuration returned by `getConfig` will cause an HTML report
    to be generated.  If run via stack/cabal this will be written to the
@@ -100,58 +131,34 @@ getConfig limit = do
                 timeLimit = limit
               }
 
-{- See the README files in the data directories for the combinations of scripts.
-   you can run specific benchmarks by typing things like
-   `stack bench -- plutus-benchmark:validation --ba crowdfunding/2`. -}
+-- Extend the options to include `--quick`: see eg https://github.com/haskell/criterion/pull/206
+data BenchOptions = BenchOptions
+  { quick        :: Bool
+  , otherOptions :: Mode  -- The standard options
+  }
+
+parseBenchOptions :: Config -> Parser BenchOptions
+parseBenchOptions cfg = BenchOptions
+  <$> switch
+      (  short 'q'
+      <> long "quick"
+      <> help "Run only a small subset of the benchmarks")
+  <*> parseWith cfg
+
+parserInfo :: Config -> ParserInfo BenchOptions
+parserInfo cfg =
+    info (helper <*> parseBenchOptions cfg) $ header "Plutus Core validation benchmark suite"
+
+{- Run the benchmarks.  You can run groups of benchmarks by typing things like
+     `stack bench -- plutus-benchmark:validation --ba crowdfunding`
+   or
+     `cabal bench -- plutus-benchmark:validation --benchmark-options crowdfunding`.
+-}
 main :: IO ()
 main = do
-  config <- getConfig 20.0  -- Run each benchmark for at least 20 seconds.  Change this with -L or --timeout (longer is better).
-  defaultMainWith config
-       [ mkBgroup
-         "crowdfunding"
-         [ (1, (1,1,1,1))
-         , (2, (1,2,1,2))
-         , (3, (1,3,1,3))
-         , (4, (1,3,2,4))
-         , (5, (1,1,2,5))
-         ]
-       , mkBgroup
-         "future"
-         [ (1, (1,1,1,1))
-         , (2, (2,2,1,2))
-         , (3, (2,3,1,3))
-         , (4, (3,4,2,4))
-         , (5, (3,5,3,5))
-         , (6, (3,4,4,6))
-         , (7, (3,4,3,7))
-         ]
-       , mkBgroup
-         "multisigSM"
-         [ (1,  (1,1,1,1))
-         , (2,  (1,2,2,2))
-         , (3,  (1,3,3,3))
-         , (4,  (1,4,4,4))
-         , (5,  (1,5,5,5))
-         , (6,  (1,1,1,6))
-         , (7,  (1,2,2,7))
-         , (8,  (1,3,3,8))
-         , (9,  (1,4,4,9))
-         , (10, (1,5,5,10))
-         ]
-       , mkBgroup
-         "vesting"
-         [ (1,  (1,1,1,1))
-         , (2,  (2,1,1,2))
-         , (3,  (3,1,1,1))
-         ]
-       , mkBgroup
-         "marlowe/trustfund"
-         [ (1,  (1,1,1,1))
-         , (2,  (1,2,2,2))
-         ]
-       , mkBgroup
-         "marlowe/zerocoupon"
-         [ (1,  (1,1,1,1))
-         , (2,  (1,2,2,2))
-         ]
-       ]
+  cfg <- getConfig 20.0  -- Run each benchmark for at least 20 seconds.  Change this with -L or --timeout (longer is better).
+  options <- execParser $ parserInfo cfg
+  benchmarks <- if quick options
+                then mapM mkContractBMs contractDirs2
+                else mapM mkContractBMs contractDirs
+  runMode (otherOptions options) benchmarks
