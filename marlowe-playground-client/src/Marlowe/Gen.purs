3a4755484f1a52c6c17f895819f4af9bbcd1f40b
Hernan Rajchert <hrajchert@gmail.com>
2021-06-16 18:54:20 -0300

    fix purty

--- marlowe-playground-client/src/Marlowe/Gen.purs
+++ marlowe-playground-client/src/Marlowe/Gen.purs
@@ -43,11 +43,11 @@ genRational = do
   d <- genBigInteger
   pure
     -- we need to do this because in tests where we wrap a Rational in a Term or TermWrapper
-
+    
     -- when we have two negative values then the column position of the term will different
-
+    
     -- to if we have two positive values, even though the rationals themselves are equal
-
+    
     $ if d > zero then
         Rational n d
       else
