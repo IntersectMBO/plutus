32c8156459571cefda692705d94c188a82e3d85f
Michael Bishop <cleverca22@gmail.com>
2021-07-21 14:10:57 -0300

    bind to the any ip

--- pkgs/web-ghc-server.nix
+++ pkgs/web-ghc-server.nix
@@ -3,7 +3,7 @@
 let
   entrypoint = writeShellScriptBin "entrypoint" ''
     PATH=${ghcWithPlutus}/bin:$PATH
-    ${web-ghc-server}/bin/web-ghc-server webserver -p 8009
+    ${web-ghc-server}/bin/web-ghc-server webserver -p 8009 --bind 0.0.0.0
   '';
 in symlinkJoin {
   name = "entrypoint";
