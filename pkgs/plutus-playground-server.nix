95684007c17037c6ee139dceab7ee2417effb701
Michael Bishop <cleverca22@gmail.com>
2021-07-26 15:19:42 -0300

    update the ssl path to meet haskells expectation

--- pkgs/plutus-playground-server.nix
+++ pkgs/plutus-playground-server.nix
@@ -4,7 +4,7 @@ let
   deps = [ pkg ] ++ lib.optional (variant == "marlowe") ghcWithPlutus;
   entrypoint = writeShellScriptBin "entrypoint" ''
     export PATH=${lib.makeBinPath deps}
-    export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    export SYSTEM_CERTIFICATE_PATH=${cacert}/etc/ssl/certs/ca-bundle.crt
     ${variant}-playground-server webserver -p 4003
   '';
 in symlinkJoin {
