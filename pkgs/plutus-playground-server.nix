f184236161313a484632d56d008f3182896a9e2e
Michael Bishop <cleverca22@gmail.com>
2021-07-26 14:55:43 -0300

    include cacert in the server env

--- pkgs/plutus-playground-server.nix
+++ pkgs/plutus-playground-server.nix
@@ -1,9 +1,10 @@
-{ writeShellScriptBin, pkg, variant, symlinkJoin, lib, ghcWithPlutus }:
+{ writeShellScriptBin, pkg, variant, symlinkJoin, lib, ghcWithPlutus, cacert }:
 
 let
   deps = [ pkg ] ++ lib.optional (variant == "marlowe") ghcWithPlutus;
   entrypoint = writeShellScriptBin "entrypoint" ''
     export PATH=${lib.makeBinPath deps}
+    export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
     ${variant}-playground-server webserver -p 4003
   '';
 in symlinkJoin {
