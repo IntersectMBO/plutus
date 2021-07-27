c7beb560544e3caeb0db2c6673be453e959488a3
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2021-07-27 10:18:11 -0400

    Moved orphan instances for Digest SHA256 from plutus-ledger to plutus-contract as only it that uses them.

--- plutus-contract/src/Wallet/Rollup/Render.hs
+++ plutus-contract/src/Wallet/Rollup/Render.hs
@@ -8,16 +8,23 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE ViewPatterns          #-}
 
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Wallet.Rollup.Render(
     showBlockchain
     , showBlockchainFold
     ) where
 
+import           Codec.Serialise.Class                 (Serialise, decode, encode)
 import           Control.Lens.Combinators              (itraverse)
 import           Control.Monad.Except                  (MonadError, throwError)
 import           Control.Monad.Reader
-import           Crypto.Hash                           (Digest, SHA256)
+import           Crypto.Hash                           (Digest, SHA256, digestFromByteString)
+import           Data.Aeson                            (FromJSON (parseJSON), ToJSON (toJSON))
+import qualified Data.Aeson                            as JSON
 import qualified Data.Aeson.Extras                     as JSON
+import qualified Data.ByteArray                        as BA
+import qualified Data.ByteString                       as BSS
 import           Data.Foldable                         (fold)
 import           Data.List                             (intersperse)
 import           Data.Map                              (Map)
@@ -29,8 +36,7 @@ import qualified Data.Text                             as Text
 import           Data.Text.Prettyprint.Doc             (Doc, Pretty, defaultLayoutOptions, fill, indent, layoutPretty,
                                                         line, parens, pretty, viaShow, vsep, (<+>))
 import           Data.Text.Prettyprint.Doc.Render.Text (renderStrict)
-import           Ledger                                (Address, Blockchain, PubKey, PubKeyHash, Signature, Tx (Tx),
-                                                        TxId, TxIn (TxIn),
+import           Ledger                                (Address, Blockchain, Tx (Tx), TxId, TxIn (TxIn),
                                                         TxInType (ConsumePublicKeyAddress, ConsumeScriptAddress),
                                                         TxOut (TxOut), TxOutRef (TxOutRef, txOutRefId, txOutRefIdx),
                                                         Value, txFee, txMint, txOutValue, txOutputs, txSignatures)
@@ -40,7 +46,8 @@ import           Ledger.Scripts                        (Datum (getDatum), Script
                                                         ValidatorHash (ValidatorHash), unValidatorScript)
 import           Ledger.Value                          (CurrencySymbol (CurrencySymbol), TokenName (TokenName))
 import qualified Ledger.Value                          as Value
-import qualified PlutusTx                              as PlutusTx
+import           Plutus.V1.Ledger.Crypto               (PubKey, PubKeyHash, Signature)
+import qualified PlutusTx
 import qualified PlutusTx.AssocMap                     as AssocMap
 import qualified PlutusTx.Builtins                     as Builtins
 import           Wallet.Emulator.Folds                 (EmulatorEventFold)
@@ -56,7 +63,7 @@ import           Wallet.Rollup.Types                   (AnnotatedTx (AnnotatedTx
 showBlockchainFold :: [(PubKeyHash, Wallet)] -> EmulatorEventFold (Either Text Text)
 showBlockchainFold walletKeys =
     let r txns =
-            (renderStrict . layoutPretty defaultLayoutOptions)
+            renderStrict . layoutPretty defaultLayoutOptions
             <$> runReaderT (render txns) (Map.fromList walletKeys)
     in fmap r Folds.annotatedBlockchain
 
@@ -316,3 +323,28 @@ abbreviate n t =
      in if prefix == t
             then t
             else prefix <> "..."
+
+{- Note [Serialising Digests from Crypto.Hash]
+This is more complicated than you might expect.  If you say
+`encode = encode . BA.unpack` then the contents of the digest are
+unpacked into a `Word8` list with 32 entries.  However, when cborg
+serialises a list, every element in the output is preceded by a type
+tag (in this case, 24), and this means that the serialised version is
+about 64 bytes long, twice the length of the original data.  Packing
+the `Word8` list into a `ByteString` first fixes this because cborg
+just serialises it as a sequence of contiguous bytes. -}
+
+instance Serialise (Digest SHA256) where
+    encode = encode . BSS.pack . BA.unpack
+    decode = do
+      d :: BSS.ByteString <- decode
+      let bs :: BA.Bytes = BA.pack . BSS.unpack $ d
+      case digestFromByteString bs of
+        Nothing -> fail $ "Couldn't decode SHA256 Digest: " ++ show d
+        Just v  -> pure v
+
+instance ToJSON (Digest SHA256) where
+    toJSON = JSON.String . JSON.encodeSerialise
+
+instance FromJSON (Digest SHA256) where
+    parseJSON = JSON.decodeSerialise
