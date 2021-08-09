986200a559fe3b370d37bc83083ba9e0675918c7
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2021-08-09 10:29:19 +0100

    Partially revert d41aa2593747157aa1b19819903a95eaa30e7b05

--- plutus-core/untyped-plutus-core/test/Evaluation/ApplyBuiltinName.hs
+++ plutus-core/untyped-plutus-core/test/Evaluation/ApplyBuiltinName.hs
@@ -72,6 +72,8 @@ test_applyDefaultBuiltin =
         , test_applyBuiltinFunction RemainderInteger
         , test_applyBuiltinFunction LessThanInteger
         , test_applyBuiltinFunction LessThanEqInteger
+        , test_applyBuiltinFunction GreaterThanInteger
+        , test_applyBuiltinFunction GreaterThanEqInteger
         , test_applyBuiltinFunction EqInteger
         , test_applyBuiltinFunction Concatenate
         , test_applyBuiltinFunction TakeByteString
