414c583c0168dd620cb6388e20d98d117456a853
kwxm <kenneth.mackenzie@iohk.io>
2021-08-11 09:47:21 +0100

    Mysterious revert during 'git pull'

--- plutus-core/cost-model/budgeting-bench/Nops.hs
+++ plutus-core/cost-model/budgeting-bench/Nops.hs
@@ -4,8 +4,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
-module Nops where
+{- | A set of no-op built-in functions used in cost model calibration. Benchmarks
+   based on these are used to estimate the overhead of calling a built-in
+   function.
+-}
+
+module Nops (NopFuns(..), nopCostParameters) where
 
 import           PlutusCore
 import           PlutusCore.Constant
@@ -17,15 +24,15 @@ import           UntypedPlutusCore.Evaluation.Machine.Cek
 import           Control.DeepSeq                                 (NFData)
 import           Data.Char                                       (toLower)
 import           Data.Ix                                         (Ix)
-import           GHC.Generics
+import           GHC.Generics                                    (Generic)
 
-data NopBuiltins
+data NopFuns
     = Nop1
     | Nop2
     | Nop3
     deriving (Show, Eq, Ord, Enum, Bounded, Generic, NFData, Ix, PrettyBy PrettyConfigPlc)
 
-instance Pretty NopBuiltins where
+instance Pretty NopFuns where
     pretty fun = pretty $ case show fun of
         ""    -> ""
         c : s -> toLower c : s
@@ -37,47 +44,53 @@ data NopCostModel =
     , paramNop3 :: CostingFun ModelThreeArguments
     }
 
--- A fake cost model for nops.  This is just to make sure that the overhead of
--- calling the costing function is included, so the precise contents don't
--- matter as long as the basic form is correct (and benchmarks suggest that nops
--- indeed have constant costs).
+{- | A fake cost model for nops.  This is just to make sure that the overhead of
+   calling the costing function is included, so the precise contents don't
+   matter as long as the basic form is correct (and benchmarks suggest that nops
+   indeed have constant costs). -}
 nopCostModel :: NopCostModel
 nopCostModel =
     NopCostModel
     {
       paramNop1 = CostingFun
-                  (ModelOneArgumentConstantCost 120000)
-                  (ModelOneArgumentConstantCost 1000)
+                  (ModelOneArgumentConstantCost 1000000)
+                  (ModelOneArgumentConstantCost 100)
     , paramNop2 = CostingFun
-                  (ModelTwoArgumentsConstantCost 140000)
-                  (ModelTwoArgumentsConstantCost 2000)
+                  (ModelTwoArgumentsConstantCost 1200000)
+                  (ModelTwoArgumentsConstantCost 200)
     , paramNop3 = CostingFun
-                  (ModelThreeArgumentsConstantCost 160000)
-                  (ModelThreeArgumentsConstantCost 3000)
+                  (ModelThreeArgumentsConstantCost 1500000)
+                  (ModelThreeArgumentsConstantCost 300)
     }
 
-nopCekParameters :: MachineParameters CekMachineCosts CekValue DefaultUni NopBuiltins
-nopCekParameters = toMachineParameters $ CostModel defaultCekMachineCosts nopCostModel
+nopCostParameters :: MachineParameters CekMachineCosts CekValue DefaultUni NopFuns
+nopCostParameters = toMachineParameters $ CostModel defaultCekMachineCosts nopCostModel
 
-instance uni ~ DefaultUni
-    => ToBuiltinMeaning uni NopBuiltins where
-        type CostingPart uni NopBuiltins = NopCostModel
-        toBuiltinMeaning
-            :: forall term . HasConstantIn uni term
-               => NopBuiltins -> BuiltinMeaning term NopCostModel
-        toBuiltinMeaning Nop1 =
-            makeBuiltinMeaning
-               @(Integer -> ())
-               (\_ -> ())
-               (runCostingFunOneArgument . paramNop1)
-        toBuiltinMeaning Nop2 =
-            makeBuiltinMeaning
-               @(Integer -> Integer -> ())
-               (\_ _ -> ())
-               (runCostingFunTwoArguments . paramNop2)
-        toBuiltinMeaning Nop3 =
-            makeBuiltinMeaning
-               @(Integer -> Integer -> Integer -> ())
-               (\_ _ _ -> ())
-               (runCostingFunThreeArguments . paramNop3)
+{- | The meanings of the builtins.  Each one takes a number of integer arguments
+   and returns an integer without doing any other work.  We could have used
+   units instead of integers, but using integers makes it possible to check that
+   the cost of calling the functions doesn't depend on the size of the
+   arguments.  We have checked this and there there was no dependence: let's
+   leave open the possibility of doing it again in case anything changes.
+-}
+instance (uni `Contains` Integer, GEq uni, GShow uni) => ToBuiltinMeaning uni NopFuns where
+    type CostingPart uni NopFuns = NopCostModel
+    toBuiltinMeaning
+        :: HasConstantIn uni term
+           => NopFuns -> BuiltinMeaning term NopCostModel
+    toBuiltinMeaning Nop1 =
+        makeBuiltinMeaning
+             @(Integer -> Integer)
+             (\_ -> 11)
+             (runCostingFunOneArgument . paramNop1)
+    toBuiltinMeaning Nop2 =
+        makeBuiltinMeaning
+             @(Integer -> Integer -> Integer)
+             (\_ _ -> 22)
+             (runCostingFunTwoArguments . paramNop2)
+    toBuiltinMeaning Nop3 =
+        makeBuiltinMeaning
+             @(Integer -> Integer -> Integer -> Integer)
+             (\_ _ _ -> 33)
+             (runCostingFunThreeArguments . paramNop3)
 
