c1d0973468f0b8564f651594d1bd698d66c74756
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2021-08-11 18:14:34 +0100

    SCP-2650: chunked encoding of big integers and bytestrings in Data
    This is a tricky issue. The limit is quite annoying, since it creates
    cutoff thresholds that could *potentially* trip up users. Much nicer to
    not have the limits. Fortunately, I came up with a clever encoding
    trick using indefinite-length bytestrings.
    
    However, this is pending some confirmation that the trick is kosher and
    is "obfuscated enough".

--- plutus-core/plutus-core/src/PlutusCore/Data.hs
+++ plutus-core/plutus-core/src/PlutusCore/Data.hs
@@ -10,14 +10,18 @@ module PlutusCore.Data (Data (..)) where
 
 import           Codec.CBOR.Decoding       (Decoder)
 import qualified Codec.CBOR.Decoding       as CBOR
+import qualified Codec.CBOR.Magic          as CBOR
 import qualified Codec.CBOR.Term           as CBOR
 import           Codec.Serialise           (Serialise (decode, encode))
 import           Codec.Serialise.Decoding  (decodeSequenceLenIndef, decodeSequenceLenN)
 import           Control.DeepSeq           (NFData)
 import           Control.Monad.Except
 import           Data.Bifunctor            (bimap)
+import           Data.Bits                 (shiftR)
 import qualified Data.ByteString           as BS
+import qualified Data.ByteString.Lazy      as BSL
 import           Data.Text.Prettyprint.Doc
+import           Data.Word                 (Word64, Word8)
 import           GHC.Generics
 import           Prelude
 
@@ -69,6 +73,38 @@ The scheme is:
 - Any alternatives, including those that don't fit in the above -> tag 102 followed by an integer for the actual alternative.
 -}
 
+{- Note [The 64-byte limit]
+We impose a 64-byte *on-the-wire* limit on the leaves of a serialized 'Data'. This prevents people from inserting
+Mickey Mouse entire.
+
+The simplest way of doing this is to check during deserialization that we never deserialize something that uses
+more than 64-bytes, and this is largely what we do. Then it's the user's problem to not produce something too big.
+
+But this is quite inconvenient, so see Note [Evading the 64-byte limit] for how we get around this.
+-}
+
+{- Note [Evading the 64-byte limit]
+Implementing Note [The 64-byte limit] naively would be quite annoying:
+- Users would be responsible for not creating Data values with leaves that were too big.
+- If a script *required* such a thing (e.g. a counter that somehow got above 64 bytes), then the user is totally
+stuck: the script demands something they cannot represent.
+
+This is unpleasant and introduces limits. Probably limits that nobody will hit, but it's nicer to just not have them.
+And it turns out that we can evade the problem with some clever encoding.
+
+The fundamental argument is that an *indefinite-length* CBOR bytestring is just as obfuscated as a list of bytestrings,
+since it consists of a list of chunks *with metadata*. Since we already allow people to make lists of <64 byte bytestrings,
+we might as well let them make indefinite-length bytestrings too.
+
+So that solves the problem for bytestrings: if they are >64bytes, we encode them as indefinite-length bytestrings
+with 64-byte chunks. We have to write our own encoders/decoders so we can produce chunks of the right size and check
+the sizes when we decode, but that's okay.
+
+For integers, we have two cases. Small integers (<64bits) can be encoded normally. Big integers are already
+encoded *with a byte string*. The spec allows this to be an indefinite-length bytestring (although cborg doesn't
+like it), so we can reuse our trick. Again, we need to write some manual encoders/decoders.
+-}
+
 -- | Turn Data into a CBOR Term.
 toTerm :: Data -> CBOR.Term
 toTerm = \case
@@ -78,27 +114,72 @@ toTerm = \case
     Constr i ds | otherwise         -> CBOR.TTagged 102 (CBOR.TList $ CBOR.TInteger i : fmap toTerm ds)
     Map es                          -> CBOR.TMap (fmap (bimap toTerm toTerm) es)
     List ds                         -> CBOR.TList $ fmap toTerm ds
-    I i                             -> CBOR.TInteger i
-    B b                             -> CBOR.TBytes b
+    I i                             -> integerToTerm i
+    B b                             -> bsToTerm b
+
+-- Logic for choosing encoding borrowed from Codec.CBOR.Write
+-- | Given an integer, create a 'CBOR.Term' that encodes it, following our size restrictions.
+integerToTerm :: Integer -> CBOR.Term
+-- If it fits in a Word64, then it's less than 64 bytes for sure, and we can just send it off
+-- as a normal integer for cborg to deal with
+integerToTerm i | i >= 0 , i <= fromIntegral (maxBound :: Word64) = CBOR.TInteger i
+                | i <  0 , i >= -1 - fromIntegral (maxBound :: Word64) = CBOR.TInteger i
+-- Otherwise, it would be encoded as a bignum anyway, so we manually do the bignum
+-- encoding with a bytestring inside, and since we use bsToTerm, that bytestring will
+-- get chunked up if it's too big.
+-- See Note [Evading the 64-byte limit]
+integerToTerm i | i >= 0 = CBOR.TTagged 2 $ bsToTerm (integerToBytes i)
+integerToTerm i | otherwise = CBOR.TTagged 3 $ bsToTerm (integerToBytes (-1 -i))
+
+-- Taken exactly from Codec.CBOR.Write
+integerToBytes :: Integer -> BS.ByteString
+integerToBytes n0
+  | n0 == 0   = BS.pack [0]
+  | otherwise = BS.pack (reverse (go n0))
+  where
+    go n | n == 0    = []
+         | otherwise = narrow n : go (n `shiftR` 8)
+
+    narrow :: Integer -> Word8
+    narrow = fromIntegral
+
+-- | Given an bytestring, create a 'CBOR.Term' that encodes it, following our size restrictions.
+bsToTerm :: BS.ByteString -> CBOR.Term
+bsToTerm b | BS.length b <= 64 = CBOR.TBytes b
+-- BSL.fromChunks preserves the chunks exactly as they are given (checked the source),
+-- and encoding a TBytesI encodes it with exactly the chunks in the lazy bytestring (checked the source),
+-- so this successfully results in our chunks being used on the wire.
+-- See Note [Evading the 64-byte limit]
+bsToTerm b                     = CBOR.TBytesI (BSL.fromChunks (to64ByteChunks b))
+
+-- | Turns a 'BS.ByteString' into a list of <=64 byte chunks.
+to64ByteChunks :: BS.ByteString -> [BS.ByteString]
+to64ByteChunks b | BS.length b > 64 =
+                   let (chunk, rest) = BS.splitAt 64 b
+                   in chunk:to64ByteChunks rest
+to64ByteChunks b = [b]
 
 {- Note [Definite and indefinite forms of CBOR]
 CBOR is annoying and you can have both definite (with a fixed length) and indefinite lists, maps, etc.
 
-So we have to be careful to handle both cases when decoding. When encoding we simply don't make
-the indefinite kinds.
+So we have to be careful to handle both cases when decoding. When encoding we mostly don't make
+the indefinite kinds, but see Note [Avoiding the 64-byte limit] for some cases where we do.
 -}
 
 -- | Turn a CBOR Term into Data if possible.
 decodeData :: forall s. Decoder s Data
 decodeData = CBOR.peekTokenType >>= \case
+  -- These integers are at most 64 *bits*, so certainly less than 64 *bytes*
   CBOR.TypeUInt         -> I <$> CBOR.decodeInteger
   CBOR.TypeUInt64       -> I <$> CBOR.decodeInteger
   CBOR.TypeNInt         -> I <$> CBOR.decodeInteger
   CBOR.TypeNInt64       -> I <$> CBOR.decodeInteger
-  CBOR.TypeInteger      -> decodeBoundedInteger
+  -- See Note [The 64-byte limit]
+  CBOR.TypeInteger      -> I <$> decodeBoundedBigInteger
 
-  CBOR.TypeBytes        -> decodeBoundedBytes
-  CBOR.TypeBytesIndef   -> decodeBoundedBytes
+  -- See Note [The 64-byte limit]
+  CBOR.TypeBytes        -> B <$> decodeBoundedBytes
+  CBOR.TypeBytesIndef   -> B . BSL.toStrict <$> decodeBoundedBytesIndef
 
   CBOR.TypeListLen      -> decodeList
   CBOR.TypeListLen64    -> decodeList
@@ -113,23 +194,38 @@ decodeData = CBOR.peekTokenType >>= \case
 
   t                     -> fail ("Unrecognized value of type " ++ show t)
 
-decodeBoundedInteger :: Decoder s Data
-decodeBoundedInteger = do
-  i <- CBOR.decodeInteger
-  unless (inBounds i) $ fail "Integer exceeds 64 bytes"
-  pure $ I i
-  where
-  bound :: Integer
-  -- The maximum value of a 64 byte unsigned integer
-  bound = 2 ^ (64 * 8 :: Integer) - 1
-  inBounds x = (x <= bound) && (x >= -1 - bound)
+decodeBoundedBigInteger :: Decoder s Integer
+decodeBoundedBigInteger = do
+    tag <- CBOR.decodeTag
+    -- Bignums contain a bytestring as the payload
+    bs <- CBOR.peekTokenType >>= \case
+        CBOR.TypeBytes      -> decodeBoundedBytes
+        CBOR.TypeBytesIndef -> BSL.toStrict <$> decodeBoundedBytesIndef
+        t                   -> fail ("Bignum must contain a byte string, got: " ++ show t)
+    -- Depending on the tag, the bytestring is either a positive or negative integer
+    case tag of
+        2 -> pure $ CBOR.uintegerFromBytes bs
+        3 -> pure $ CBOR.nintegerFromBytes bs
+        t -> fail ("Bignum tag must be one of 2 or 3, got: " ++ show t)
 
-decodeBoundedBytes :: Decoder s Data
+-- Adapted from Codec.CBOR.Read
+decodeBoundedBytesIndef :: Decoder s BSL.ByteString
+decodeBoundedBytesIndef = CBOR.decodeBytesIndef >> decodeBoundedBytesIndefLen []
+
+-- Adapted from Codec.CBOR.Read, to call the size-checking bytestring decoder
+decodeBoundedBytesIndefLen :: [BS.ByteString] -> Decoder s BSL.ByteString
+decodeBoundedBytesIndefLen acc = do
+    stop <- CBOR.decodeBreakOr
+    if stop then return $! BSL.fromChunks (reverse acc)
+            else do !bs <- decodeBoundedBytes
+                    decodeBoundedBytesIndefLen (bs : acc)
+
+decodeBoundedBytes :: Decoder s BS.ByteString
 decodeBoundedBytes =  do
   b <- CBOR.decodeBytes
-  if BS.length b <= 64
-    then pure $ B b
-    else fail $ "ByteString exceeds 64 bytes"
+  -- See Note [The 64-byte limit]
+  unless (BS.length b <= 64) $ fail "ByteString exceeds 64 bytes"
+  pure b
 
 decodeList :: Decoder s Data
 decodeList = List <$> decodeListOf decodeData
