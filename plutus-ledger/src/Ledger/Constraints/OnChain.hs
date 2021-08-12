0e75f03f21a5ddbe73de3cea9966f9cebeb35b09
Matthias Benkort <5680256+KtorZ@users.noreply.github.com>
2021-08-12 17:57:16 +0200

    Provide 'MustSatisfyAnyOf' constructor for TxContraints. (#3706)
    * Provide 'MustSatisfyAnyOf' constructor for TxContraints.
    
      It is actually useful in some cases to be able to perform a disjunction of constraints, and while it is possible to resort to 'Bool' in some cases, when using internal libraries like e.g. the state-machine, we are stuck with the TxConstraints API and therefore, unable to express some useful conditions.
    
    * re-generate the plutus-use-cases tests as needed.

--- plutus-ledger/src/Ledger/Constraints/OnChain.hs
+++ plutus-ledger/src/Ledger/Constraints/OnChain.hs
@@ -48,7 +48,7 @@ checkOwnOutputConstraint ctx@ScriptContext{scriptContextTxInfo} OutputConstraint
 
 {-# INLINABLE checkTxConstraint #-}
 checkTxConstraint :: ScriptContext -> TxConstraint -> Bool
-checkTxConstraint ScriptContext{scriptContextTxInfo} = \case
+checkTxConstraint ctx@ScriptContext{scriptContextTxInfo} = \case
     MustIncludeDatum dv ->
         traceIfFalse "L2" -- "Missing datum"
         $ dv `elem` fmap snd (txInfoData scriptContextTxInfo)
@@ -92,6 +92,9 @@ checkTxConstraint ScriptContext{scriptContextTxInfo} = \case
     MustHashDatum dvh dv ->
         traceIfFalse "Lc" -- "MustHashDatum"
         $ V.findDatum dvh scriptContextTxInfo == Just dv
+    MustSatisfyAnyOf xs ->
+        traceIfFalse "Ld" -- "MustSatisfyAnyOf"
+        $ any (checkTxConstraint ctx) xs
 
 {-# INLINABLE checkScriptContext #-}
 -- | Does the 'ScriptContext' satisfy the constraints?
