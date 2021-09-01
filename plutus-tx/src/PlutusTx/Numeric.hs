8e001f4671f06bcff9bae3c2517b5bde663a1d80
Anton Myasnikov <awkure@protonmail.ch>
2021-09-01 21:22:59 +0300

    add numeric type classes to unify numeric operations and their corresponding deriving newtypes

--- plutus-tx/src/PlutusTx/Numeric.hs
+++ plutus-tx/src/PlutusTx/Numeric.hs
@@ -2,32 +2,27 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
-module PlutusTx.Numeric (AdditiveSemigroup (..), AdditiveMonoid (..), AdditiveGroup (..), negate, Additive (..), MultiplicativeSemigroup (..), MultiplicativeMonoid (..), Multiplicative (..), Semiring, Ring, Module (..)) where
+module PlutusTx.Numeric
+  ( AdditiveSemigroup (..)
+  , AdditiveMonoid (..)
+  , AdditiveGroup (..)
+  , negate
+  , Additive (..)
+  , MultiplicativeSemigroup (..)
+  , MultiplicativeMonoid (..)
+  , Multiplicative (..)
+  , Semiring
+  , Ring
+  , Module (..)
+  , AdditiveHemigroup (..)
+  , monus
+  ) where
 
-import           Data.Semigroup     (Product (..), Sum (..))
-import           PlutusTx.Builtins
+import           Data.Semigroup         (Product (..), Sum (..))
 import           PlutusTx.Monoid
+import           PlutusTx.Numeric.Class
 import           PlutusTx.Semigroup
-import           Prelude            hiding (Functor (..), Monoid (..), Num (..), Semigroup (..))
-
-infixl 7 *
-infixl 6 +, -
-
--- | A 'Semigroup' that it is sensible to describe using addition.
-class AdditiveSemigroup a where
-    (+) :: a -> a -> a
-
--- | A 'Monoid' that it is sensible to describe using addition and zero.
-class AdditiveSemigroup a => AdditiveMonoid a where
-    zero :: a
-
--- | A 'Group' that it is sensible to describe using addition, zero, and subtraction.
-class AdditiveMonoid a => AdditiveGroup a where
-    (-) :: a -> a -> a
-
-{-# INLINABLE negate #-}
-negate :: AdditiveGroup a => a -> a
-negate x = zero - x
+import           Prelude                hiding (Functor (..), Monoid (..), Num (..), Semigroup (..), divMod)
 
 -- | A newtype wrapper to derive 'Additive' classes via.
 newtype Additive a = Additive a
@@ -44,16 +39,6 @@ instance Group a => AdditiveGroup (Additive a) where
     {-# INLINABLE (-) #-}
     Additive x - Additive y = Additive (x `gsub` y)
 
--- | A 'Semigroup' that it is sensible to describe using multiplication.
-class MultiplicativeSemigroup a where
-    (*) :: a -> a -> a
-
--- | A 'Semigroup' that it is sensible to describe using multiplication and one.
-class MultiplicativeSemigroup a => MultiplicativeMonoid a where
-    one :: a
-
--- TODO: multiplicative group? I haven't added any since for e.g. integers division
--- is not a proper inverse, so it's of limited use.
 
 -- | A newtype wrapper to derive 'Multiplicative' classes via.
 newtype Multiplicative a = Multiplicative a
@@ -66,50 +51,6 @@ instance Monoid a => MultiplicativeMonoid (Multiplicative a) where
     {-# INLINABLE one #-}
     one = Multiplicative mempty
 
--- | A semiring.
-type Semiring a = (AdditiveMonoid a, MultiplicativeMonoid a)
--- | A ring.
-type Ring a = (AdditiveGroup a, MultiplicativeMonoid a)
-
-instance AdditiveSemigroup Integer where
-    {-# INLINABLE (+) #-}
-    (+) = addInteger
-
-instance AdditiveMonoid Integer where
-    {-# INLINABLE zero #-}
-    zero = 0
-
-instance AdditiveGroup Integer where
-    {-# INLINABLE (-) #-}
-    (-) = subtractInteger
-
-instance MultiplicativeSemigroup Integer where
-    {-# INLINABLE (*) #-}
-    (*) = multiplyInteger
-
-instance MultiplicativeMonoid Integer where
-    {-# INLINABLE one #-}
-    one = 1
-
-instance AdditiveSemigroup Bool where
-    {-# INLINABLE (+) #-}
-    (+) = (||)
-
-instance AdditiveMonoid Bool where
-    {-# INLINABLE zero #-}
-    zero = False
-
-instance MultiplicativeSemigroup Bool where
-    {-# INLINABLE (*) #-}
-    (*) = (&&)
-
-instance MultiplicativeMonoid Bool where
-    {-# INLINABLE one #-}
-    one = True
-
--- | A module, with a type of scalars which can be used to scale the values.
-class (Ring s, AdditiveGroup v) => Module s v | v -> s where
-    scale :: s -> v -> v
 
 instance AdditiveSemigroup a => Semigroup (Sum a) where
     {-# INLINABLE (<>) #-}
