a3801a3fa96f2618b47a5ffea7eff43495741735
Anton Myasnikov <awkure@protonmail.ch>
2021-09-01 19:01:59 +0300

    add datumInputsAt and datumOutputsAt helpers

--- plutus-ledger/src/Ledger/Scripts.hs
+++ plutus-ledger/src/Ledger/Scripts.hs
@@ -14,24 +14,33 @@ the Cardano Node CLI expects serialised binary values to be wrapped with a
 module Ledger.Scripts (
     module Export
     , datumHash
+    , datumInputsAt
+    , datumOutputsAt
+    , toDatumHash
+    , toRedeemer
+    , fromRedeemer
     , redeemerHash
     , validatorHash
     , mintingPolicyHash
     , stakeValidatorHash
     ) where
 
-import           Cardano.Api              (AsType, HasTextEnvelope (textEnvelopeType), HasTypeProxy (proxyToAsType),
-                                           SerialiseAsCBOR, TextEnvelopeType (TextEnvelopeType))
-import           Cardano.Binary           (FromCBOR (fromCBOR), ToCBOR (toCBOR))
-import qualified Cardano.Crypto.Hash      as Crypto
-import           Codec.Serialise          (Serialise, decode, encode, serialise)
-import qualified Data.ByteArray           as BA
-import qualified Data.ByteString.Lazy     as BSL
-import qualified Data.Text                as Text
-import           Plutus.V1.Ledger.Api     (plutusDatumEnvelopeType, plutusRedeemerEnvelopeType,
-                                           plutusScriptEnvelopeType)
-import           Plutus.V1.Ledger.Scripts as Export
-import           PlutusTx.Builtins        as Builtins
+import           Cardano.Api               (AsType, HasTextEnvelope (textEnvelopeType), HasTypeProxy (proxyToAsType),
+                                            SerialiseAsCBOR, TextEnvelopeType (TextEnvelopeType))
+import           Cardano.Binary            (FromCBOR (fromCBOR), ToCBOR (toCBOR))
+import qualified Cardano.Crypto.Hash       as Crypto
+import           Codec.Serialise           (Serialise, decode, encode, serialise)
+import qualified Data.ByteArray            as BA
+import qualified Data.ByteString.Lazy      as BSL
+import qualified Data.Text                 as Text
+import           Plutus.V1.Ledger.Api      (FromData, TxInfo, Value, plutusDatumEnvelopeType,
+                                            plutusRedeemerEnvelopeType, plutusScriptEnvelopeType)
+import qualified Plutus.V1.Ledger.Api      as Ledger
+import qualified Plutus.V1.Ledger.Api      as PlutusTx
+import           Plutus.V1.Ledger.Contexts (findDatum, scriptInputsAt, scriptOutputsAt)
+import           Plutus.V1.Ledger.Scripts  as Export
+import           PlutusTx.Builtins         as Builtins
+import qualified PlutusTx.Foldable         as Foldable
 
 instance HasTextEnvelope Script where
     textEnvelopeType _ = TextEnvelopeType $ Text.unpack plutusScriptEnvelopeType
@@ -79,7 +88,59 @@ instance HasTypeProxy Redeemer where
     proxyToAsType _ = AsRedeemer
 
 datumHash :: Datum -> DatumHash
-datumHash = DatumHash . Builtins.sha2_256 . BA.convert
+datumHash = Ledger.DatumHash . Builtins.sha2_256 . BA.convert
+
+-- | Get 'DatumHash' using 'ToData' instance.
+-- **Note**: not intended to be used on-chain.
+toDatumHash
+  :: forall datum.
+     Ledger.ToData datum
+  => datum
+  -> Ledger.DatumHash
+toDatumHash = datumHash . toDatum @datum
+
+
+{-# INLINEABLE datumOutputsAt #-}
+
+-- | Version of scriptOutputsAt which also extracts the associated Datums
+datumOutputsAt
+  :: forall datum.
+     FromData datum
+  => ValidatorHash
+  -> TxInfo
+  -> [(datum, Value)]
+datumOutputsAt script txInfo = do
+  (datumHash, value) <- scriptOutputsAt script txInfo
+  datum <-
+    Foldable.toList $
+      findDatum datumHash txInfo >>= (fromDatum @datum)
+  [(datum, value)] -- PlutusTx doesn't export Applicative List, for some reason
+
+{-# INLINEABLE datumInputsAt #-}
+
+-- | Version scriptInputsAt which also extracts the associated Datums
+datumInputsAt
+  :: forall datum.
+     PlutusTx.FromData datum
+  => ValidatorHash
+  -> TxInfo
+  -> [(datum, Ledger.Value)]
+datumInputsAt script txInfo = do
+  (datumHash, value) <- scriptInputsAt script txInfo
+  datum <-
+    Foldable.toList $
+      findDatum datumHash txInfo >>= fromDatum @datum
+  [(datum, value)]
+
+-- | Construct 'Redeemer' using 'ToData' instance.
+{-# INLINEABLE toRedeemer #-}
+toRedeemer :: Ledger.ToData redeemer => redeemer -> Ledger.Redeemer
+toRedeemer = Ledger.Redeemer . Ledger.toBuiltinData
+
+-- | Parse 'Redeemer' using 'FromData' instance.
+{-# INLINEABLE fromRedeemer #-}
+fromRedeemer :: Ledger.FromData redeemer => Ledger.Redeemer -> Maybe redeemer
+fromRedeemer (Ledger.Redeemer d) = Ledger.fromBuiltinData d
 
 redeemerHash :: Redeemer -> RedeemerHash
 redeemerHash = RedeemerHash . Builtins.sha2_256 . BA.convert
