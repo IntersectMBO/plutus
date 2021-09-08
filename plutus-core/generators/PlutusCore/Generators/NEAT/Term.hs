19839de20bb8416cf1c8fb51400b599c0a912711
James Chapman <james.chapman@iohk.io>
2021-09-08 12:18:42 +0100

    fix warning

--- plutus-core/generators/PlutusCore/Generators/NEAT/Term.hs
+++ plutus-core/generators/PlutusCore/Generators/NEAT/Term.hs
@@ -297,7 +297,7 @@ instance Check (Kind ()) TypeBuiltinG where
   check (Type _) TyIntegerG    = true
   check (Type _) TyBoolG       = true
   check (Type _) TyUnitG       = true
-  check (Type _) (TyListG a)   = false -- check (Type ()) a
+  check (Type _) (TyListG _a)  = false -- check (Type ()) a
   check (Type _) TyStringG     = true
   check (Type _) TyDataG       = true
   check _        _             = false
