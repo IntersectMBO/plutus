10b2558122acdf3c5e76807b5f9be18ef9dd2605
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2021-09-17 11:27:48 +0100

    Init v2

---
+++ plutus-ledger-api/src/Plutus/V2/Ledger/Contexts.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE DerivingVia          #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE NamedFieldPuns       #-}
+{-# LANGUAGE NoImplicitPrelude    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns         #-}
+{-# OPTIONS_GHC -Wno-simplifiable-class-constraints #-}
+{-# OPTIONS_GHC -fno-strictness #-}
+{-# OPTIONS_GHC -fno-specialise #-}
+{-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
+module Plutus.V2.Ledger.Contexts
+    (
+    -- * Pending transactions and related types
+      TxInfo(..)
+    , ScriptContext(..)
+    , ScriptPurpose(..)
+    , TxOut(..)
+    , TxOutRef(..)
+    , TxInInfo(..)
+    , findOwnInput
+    , findDatum
+    , findDatumHash
+    , findTxInByTxOutRef
+    , findContinuingOutputs
+    , getContinuingOutputs
+    -- ** Hashes (see note [Hashes in validator scripts])
+    -- * Validator functions
+    -- ** Signatures
+    , txSignedBy
+    -- ** Transactions
+    , pubKeyOutput
+    , scriptOutputsAt
+    , pubKeyOutputsAt
+    , valueLockedBy
+    , valuePaidTo
+    , adaLockedBy
+    , signsTransaction
+    , spendsOutput
+    , valueSpent
+    , valueProduced
+    , ownCurrencySymbol
+    , ownHashes
+    , ownHash
+    , fromSymbol
+    ) where
+
+import           Data.Text.Prettyprint.Doc   (Pretty (..), nest, vsep, (<+>))
+import           GHC.Generics                (Generic)
+import           PlutusTx.Prelude
+
+import           Plutus.V1.Ledger.Credential (StakingCredential)
+import           Plutus.V1.Ledger.Crypto     (PubKeyHash (..))
+import           Plutus.V1.Ledger.DCert      (DCert (..))
+import           Plutus.V1.Ledger.Scripts
+import           Plutus.V1.Ledger.Time       (POSIXTimeRange)
+import           Plutus.V1.Ledger.TxId
+import           Plutus.V1.Ledger.Value      (Value)
+import qualified Prelude                     as Haskell
+
+import           Plutus.V1.Ledger.Contexts   hiding (ScriptContext (..), TxInfo (..))
+
+-- | A pending transaction. This is the view as seen by validator scripts, so some details are stripped out.
+data TxInfo = TxInfo
+    { txInfoInputs      :: [TxInInfo] -- ^ Transaction inputs
+    , txInfoOutputs     :: [TxOut] -- ^ Transaction outputs
+    , txInfoFee         :: Value -- ^ The fee paid by this transaction.
+    , txInfoMint        :: Value -- ^ The 'Value' minted by this transaction.
+    , txInfoDCert       :: [DCert] -- ^ Digests of certificates included in this transaction
+    , txInfoWdrl        :: [(StakingCredential, Integer)] -- ^ Withdrawals
+    , txInfoValidRange  :: POSIXTimeRange -- ^ The valid range for the transaction.
+    , txInfoSignatories :: [PubKeyHash] -- ^ Signatures provided with the transaction, attested that they all signed the tx
+    , txInfoData        :: [(DatumHash, Datum)]
+    , txInfoId          :: TxId
+    -- ^ Hash of the pending transaction (excluding witnesses)
+    } deriving stock (Generic, Haskell.Show, Haskell.Eq)
+
+instance Eq TxInfo where
+    {-# INLINABLE (==) #-}
+    TxInfo i o f m c w r s d tid == TxInfo i' o' f' m' c' w' r' s' d' tid' =
+        i == i' && o == o' && f == f' && m == m' && c == c' && w == w' && r == r' && s == s' && d == d' && tid == tid'
+
+instance Pretty TxInfo where
+    pretty TxInfo{txInfoInputs, txInfoOutputs, txInfoFee, txInfoMint, txInfoDCert, txInfoWdrl, txInfoValidRange, txInfoSignatories, txInfoData, txInfoId} =
+        vsep
+            [ "TxId:" <+> pretty txInfoId
+            , "Inputs:" <+> pretty txInfoInputs
+            , "Outputs:" <+> pretty txInfoOutputs
+            , "Fee:" <+> pretty txInfoFee
+            , "Value minted:" <+> pretty txInfoMint
+            , "DCerts:" <+> pretty txInfoDCert
+            , "Wdrl:" <+> pretty txInfoWdrl
+            , "Valid range:" <+> pretty txInfoValidRange
+            , "Signatories:" <+> pretty txInfoSignatories
+            , "Datums:" <+> pretty txInfoData
+            ]
+
+data ScriptContext = ScriptContext{scriptContextTxInfo :: TxInfo, scriptContextPurpose :: ScriptPurpose }
+    deriving stock (Generic, Haskell.Eq, Haskell.Show)
+
+instance Eq ScriptContext where
+    {-# INLINABLE (==) #-}
+    ScriptContext info purpose == ScriptContext info' purpose' = info == info' && purpose == purpose'
+
+instance Pretty ScriptContext where
+    pretty ScriptContext{scriptContextTxInfo, scriptContextPurpose} =
+        vsep
+            [ "Purpose:" <+> pretty scriptContextPurpose
+            , nest 2 $ vsep ["TxInfo:", pretty scriptContextTxInfo]
+            ]
