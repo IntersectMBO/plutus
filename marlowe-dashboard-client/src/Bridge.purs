9f3d5f34fbc269084a6e53fc468fa2c85973228f
Hernan Rajchert <hrajchert@gmail.com>
2021-09-20 15:28:26 -0300

    Remove unused references

--- marlowe-dashboard-client/src/Bridge.purs
+++ marlowe-dashboard-client/src/Bridge.purs
@@ -9,14 +9,13 @@ import Prologue
 import Cardano.Wallet.Types (WalletInfo(..)) as Back
 import Data.Bifunctor (bimap)
 import Data.BigInteger (BigInteger)
-import Data.Json.JsonTuple (JsonTuple(..))
 import Data.Json.JsonUUID (JsonUUID(..))
 import Data.Lens (Iso', iso)
 import Data.Map (Map, fromFoldable, toUnfoldable) as Front
 import Data.Tuple.Nested ((/\))
 import Data.Json.JsonNTuple (JsonNTuple(..))
 import Marlowe.PAB (PlutusAppId(..)) as Front
-import Marlowe.Semantics (Assets(..), Slot(..), SlotInterval(..)) as Front
+import Marlowe.Semantics (Assets(..), Slot(..)) as Front
 import Network.RemoteData (RemoteData)
 import Plutus.V1.Ledger.Crypto (PubKey(..), PubKeyHash(..)) as Back
 import Plutus.V1.Ledger.Slot (Slot(..)) as Back
@@ -68,10 +67,6 @@ instance tupleBridge :: (Bridge a c, Bridge b d) => Bridge (Tuple a b) (Tuple c 
   toFront (a /\ b) = toFront a /\ toFront b
   toBack (c /\ d) = toBack c /\ toBack d
 
-instance jsonTupleBridge :: (Bridge a c, Bridge b d) => Bridge (JsonTuple a b) (JsonTuple c d) where
-  toFront (JsonTuple tuple) = JsonTuple $ toFront tuple
-  toBack (JsonTuple tuple) = JsonTuple $ toBack tuple
-
 instance jsonNTupleBridge :: (Bridge a c, Bridge b d) => Bridge (JsonNTuple a b) (JsonNTuple c d) where
   toFront (JsonNTuple a b) = JsonNTuple (toFront a) (toFront b)
   toBack (JsonNTuple a b) = JsonNTuple (toBack a) (toBack b)
@@ -91,11 +86,6 @@ instance mapBridge :: (Ord a, Ord c, Bridge a c, Bridge b d) => Bridge (Back.Map
 instance slotBridge :: Bridge Back.Slot Front.Slot where
   toFront slot@(Back.Slot { getSlot }) = Front.Slot getSlot
   toBack (Front.Slot slot) = Back.Slot { getSlot: slot }
-
--- FIXME remove
-instance slotIntervalBridge :: Bridge (JsonTuple Back.Slot Back.Slot) Front.SlotInterval where
-  toFront (JsonTuple (Tuple slot1 slot2)) = Front.SlotInterval (toFront slot1) (toFront slot2)
-  toBack (Front.SlotInterval slot1 slot2) = JsonTuple $ Tuple (toBack slot1) (toBack slot2)
 
 instance bigIntegerBridge :: Bridge BigInteger BigInteger where
   toFront = identity
