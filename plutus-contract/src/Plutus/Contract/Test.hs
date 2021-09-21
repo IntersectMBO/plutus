c34847c86f12d753239d36f0021fd8d6b61e3ad2
kk-anlm <anlm@kikaitech.io>
2021-09-21 11:21:20 +0700

    resolve comment

--- plutus-contract/src/Plutus/Contract/Test.hs
+++ plutus-contract/src/Plutus/Contract/Test.hs
@@ -331,11 +331,18 @@ dataAtAddress address check =
               <+> foldMap (foldMap pretty . Ledger.txData . Ledger.txOutTxTx) utxo)
       pure result
 
-outputsAtAddress :: forall d. FromData d => Address -> ([(d, Value)] -> Bool) -> TracePredicate
+getTxOuts :: forall d. (FromData d) => Ledger.TxOutTx -> Maybe (Either Ledger.DatumHash d, Value)
+getTxOuts (Ledger.TxOutTx _ (Ledger.TxOut _ _ Nothing)) = Nothing
+getTxOuts (Ledger.TxOutTx tx' (Ledger.TxOut _ vl (Just datumHash))) = Just $
+    case Ledger.lookupDatum tx' datumHash >>= (Ledger.getDatum >>> fromBuiltinData @d) of
+        Just dt -> (Right dt, vl)
+        _       -> (Left datumHash, vl)
+
+outputsAtAddress :: forall d. FromData d => Address -> ([(Either Ledger.DatumHash d, Value)] -> Bool) -> TracePredicate
 outputsAtAddress address check =
   flip postMapM (L.generalize $ Folds.utxoAtAddress address) $ \utxo -> do
-    let datums = mapMaybe (uncurry $ getTxOutDatum @d) $ M.toList utxo
-        result = check $ zip datums (Ledger.txOutValue . Ledger.txOutTxOut . snd <$> M.toList utxo)
+    let outputs = mapMaybe (getTxOuts @d) (snd <$> M.toList utxo)
+        result = check outputs
     unless result $ do
       tell @(Doc Void)
         ( "Outputs at address" <+> pretty address <+> "were"
