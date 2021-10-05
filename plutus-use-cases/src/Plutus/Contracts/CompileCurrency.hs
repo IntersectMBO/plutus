ea2e3e7fa6c76cb112658b1b5260f8439566e54a
hsloan <ishaqsloan@gmail.com>
2021-10-05 18:10:05 +0000

    WIP: add a main module capable of compiling a unique Uniswap token

--- plutus-use-cases/src/Plutus/Contracts/CompileCurrency.hs
+++ plutus-use-cases/src/Plutus/Contracts/CompileCurrency.hs
@@ -6,15 +6,17 @@ import           Ledger
 import           Plutus.Contracts.Currency
 
 txOutRef :: TxOutRef
-txOutRef = TxOutRef "9cbf810d18f1737fdb455ced40cf5f2b6c7c350de89d41a43493297900ca4ce5" 0
+txOutRef = TxOutRef "bd59d826d94d3099ef61362cf974b1ab1226744bb476eceb4a78b8c510fe5843" 0
 
 main :: IO ()
 main = do
+  -- create a unique uniswap token
   let sth = mkCurrency txOutRef [("Uniswap", 1)]
       sthelse = currencySymbol sth
       pol = curPolicy sth
   print sth
   print sthelse
   let currencyPolicyScript = toCardanoApiScript $ getMintingPolicy pol
+  -- write compiled script to file, to be used by cardano-cli to build a script address
   result <- writeFileTextEnvelope "uniswapCurrency.plutus" Nothing currencyPolicyScript
   print result
