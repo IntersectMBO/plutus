cb89273cc8e1a3a47dae49242d80e90178c45fc7
hsloan <ishaqsloan@gmail.com>
2021-10-05 23:44:04 +0000

    refactor script compiler example

--- plutus-use-cases/src/Plutus/Contracts/CompileCurrency.hs
+++ plutus-use-cases/src/Plutus/Contracts/CompileCurrency.hs
@@ -1,22 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Plutus.Contracts.CompileCurrency where
 
 import           Cardano.Api
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Lazy.UTF8  as BLU
 import           Ledger
 import           Plutus.Contracts.Currency
+import qualified PlutusTx.Builtins.Internal as Builtins
+import           Safe
+import           System.Environment
 
-txOutRef :: TxOutRef
-txOutRef = TxOutRef "bd59d826d94d3099ef61362cf974b1ab1226744bb476eceb4a78b8c510fe5843" 0
-
-main :: IO ()
-main = do
-  -- create a unique uniswap token
-  let sth = mkCurrency txOutRef [("Uniswap", 1)]
-      sthelse = currencySymbol sth
-      pol = curPolicy sth
-  print sth
-  print sthelse
-  let currencyPolicyScript = toCardanoApiScript $ getMintingPolicy pol
-  -- write compiled script to file, to be used by cardano-cli to build a script address
-  result <- writeFileTextEnvelope "uniswapCurrency.plutus" Nothing currencyPolicyScript
-  print result
+main :: String -> String -> IO ()
+main hash idx = do
+  let mTxIndex :: Maybe Integer = readMay idx
+  case mTxIndex of
+    Nothing -> print ("The second argument, tx index must be an integer" :: String)
+    Just txIndex -> do
+      -- create a unique uniswap token
+      let txHash = Ledger.TxId $ Builtins.BuiltinByteString $ BSL.toStrict $ BLU.fromString hash
+          txOutRef = TxOutRef txHash txIndex
+          c = mkCurrency txOutRef [("Uniswap", 1)]
+          cSym = currencySymbol c
+          pol = curPolicy c
+      print c
+      print cSym
+      let currencyPolicyScript = toCardanoApiScript $ getMintingPolicy pol
+      -- write compiled script to file, to be used by cardano-cli to build a script address
+      result <- writeFileTextEnvelope "uniswapCurrency.plutus" Nothing currencyPolicyScript
+      print result
