9cb3e2409b626aa738f5853b089b661fc14801ba
kwxm <kenneth.mackenzie@iohk.io>
2021-10-29 19:52:40 +0100

    Cost model tests are working again

--- plutus-core/cost-model/test/TestCostModels.hs
+++ plutus-core/cost-model/test/TestCostModels.hs
@@ -19,31 +19,20 @@ import           CostModelCreation
 import           Control.Applicative                            (Const, getConst)
 import           Control.Monad.Morph                            (MFunctor, hoist, lift)
 import           Data.Coerce                                    (coerce)
-import           System.IO.Unsafe                               (unsafePerformIO)
 import           Unsafe.Coerce                                  (unsafeCoerce)
 
-import           H.Prelude                                      (MonadR, Region, getExecContext, io,
-                                                                 unsafeRunWithExecContext)
+import           H.Prelude                                      (MonadR)
 import           Language.R                                     as R (R, SomeSEXP, defaultConfig, fromSomeSEXP,
-                                                                      runRegion, unsafeRunRegion, withEmbeddedR)
+                                                                      unsafeRunRegion, withEmbeddedR)
 import           Language.R.QQ                                  (r)
 
-import           Hedgehog                                       (Gen, Group (..), Property, PropertyT, check,
-                                                                 checkSequential, diff, discover, forAll, property,
-                                                                 withTests)
+import           Hedgehog                                       (Gen, Group (..), Property, PropertyT, checkSequential,
+                                                                 diff, forAll, property, withTests)
 import qualified Hedgehog.Gen                                   as Gen
 import qualified Hedgehog.Main                                  as HH (defaultMain)
 import qualified Hedgehog.Range                                 as Range
 
-
-import           Debug.Trace
-
-paramGen :: Gen CostingInteger
-paramGen = Gen.resize 90 $ Gen.integral (Range.linear 0 5000)
-
-type CostingFunctions s = BuiltinCostModelBase (Const (SomeSEXP s))
--- This is just a record in which every field refers to an SEXP (over some state)
-
+-- import           Debug.Trace
 
 {- | This module is supposed to test that the R cost models for built-in functions
    defined in BuiltinCostModel.hs produce the same results as the Haskell
@@ -55,66 +44,84 @@ type CostingFunctions s = BuiltinCostModelBase (Const (SomeSEXP s))
    CostModelCreation to convert R results to picoseconds expressed as
    CostingIntegers.  To deal with (A), we don't check for exact equality of the
    outputs but instead check that the R result and the Haskell result agreee to
-   within a factor of 1/10000 (one hundredth of a percent).
+   within a factor of 1/100 (one percent).
 
 -}
 
+-- | Generate inputs for costing functions, making sure that we test a large
+-- range of inputs, but that we also get small inputs.
+memUsageGen :: Gen CostingInteger
+memUsageGen =
+    Gen.choice [small, largish]
+        where small   = Gen.integral (Range.constant 0 2)
+              largish = Gen.integral (Range.linear 0 5000)
+
+-- Just to make our signatures more concise.  This type is just a record in
+-- which every field refers to an R SEXP (over some state)
+type CostingFunctions s = BuiltinCostModelBase (Const (SomeSEXP s))
+
+{- The region where we want to carry out tests for a two-dimensional model.  The
+   Haskell versions of some of these models are defined in several pieces and we
+   don't yet have corresponding piecewise R models, so we just restrict to the
+   places where the piecewise models are interesting (they're typically constant
+   elsewhere). -}
+data TestDomain =
+    Everywhere
+  | OnDiagonal
+  | BelowDiagonal
 
 -- Approximate equality
 (~=) :: Integral a => a -> a -> Bool
-x ~= y =
-    (x==0 && y==0) ||
-    abs ((x'-y')/y')  < 1/500
-        where x' = fromIntegral x :: Double
-              y' = fromIntegral y :: Double
-
+x ~= y
+  | x==0 && y==0 = True
+  | otherwise = -- Debug.Trace.trace ("err = " ++ show (err * 100)) $
+                err < 1/100
+    where x' = fromIntegral x :: Double
+          y' = fromIntegral y :: Double
+          err = abs ((x'-y')/y')
 prop_addInteger :: CostingFunctions s -> Property
 prop_addInteger =
-    testPredictTwo addInteger . paramAddInteger
+    testPredictTwo Everywhere addInteger . paramAddInteger
 
 prop_subtractInteger :: CostingFunctions s -> Property
 prop_subtractInteger =
-    testPredictTwo subtractInteger . paramSubtractInteger
+    testPredictTwo Everywhere subtractInteger . paramSubtractInteger
 
 prop_multiplyInteger :: CostingFunctions s -> Property
 prop_multiplyInteger =
-    testPredictTwo multiplyInteger . paramMultiplyInteger
-
--- FIXME: We now have piecewise models for division and other functions,
--- and these aren't quite properly integrated with each other yet.
--- For the time being, the relevant tests are disabled.
+    testPredictTwo Everywhere multiplyInteger . paramMultiplyInteger
 
 prop_divideInteger :: CostingFunctions s -> Property
 prop_divideInteger =
-    testPredictTwo divideInteger . paramDivideInteger
+    testPredictTwo BelowDiagonal divideInteger . paramDivideInteger
 
 prop_quotientInteger :: CostingFunctions s -> Property
 prop_quotientInteger =
-    testPredictTwo quotientInteger . paramQuotientInteger
+    testPredictTwo BelowDiagonal quotientInteger . paramQuotientInteger
 
 prop_remainderInteger :: CostingFunctions s -> Property
 prop_remainderInteger =
-    testPredictTwo remainderInteger . paramRemainderInteger
+    testPredictTwo BelowDiagonal remainderInteger . paramRemainderInteger
 
 prop_modInteger :: CostingFunctions s -> Property
 prop_modInteger =
-    testPredictTwo modInteger . paramModInteger
+    testPredictTwo BelowDiagonal modInteger . paramModInteger
 
 prop_lessThanInteger :: CostingFunctions s -> Property
 prop_lessThanInteger =
-    testPredictTwo lessThanInteger . paramLessThanInteger
+    testPredictTwo Everywhere lessThanInteger . paramLessThanInteger
 
 prop_lessThanEqualsInteger :: CostingFunctions s -> Property
 prop_lessThanEqualsInteger =
-    testPredictTwo lessThanEqualsInteger . paramLessThanEqualsInteger
+    testPredictTwo Everywhere lessThanEqualsInteger . paramLessThanEqualsInteger
 
 prop_equalsInteger :: CostingFunctions s -> Property
 prop_equalsInteger =
-    testPredictTwo equalsInteger . paramEqualsInteger
+    testPredictTwo Everywhere equalsInteger . paramEqualsInteger
 
 prop_appendByteString :: CostingFunctions s -> Property
 prop_appendByteString =
-    testPredictTwo appendByteString . paramAppendByteString
+    testPredictTwo Everywhere appendByteString . paramAppendByteString
 
 prop_sha2_256 :: CostingFunctions s -> Property
 prop_sha2_256 =
@@ -129,20 +136,20 @@ prop_blake2b =
     testPredictOne blake2b . paramBlake2b
 
 prop_verifySignature :: CostingFunctions s -> Property
-prop_verifySignature models =
-    testPredictThree verifySignature $ paramVerifySignature models
+prop_verifySignature =
+    testPredictThree verifySignature . paramVerifySignature
 
 prop_equalsByteString :: CostingFunctions s -> Property
 prop_equalsByteString =
-    testPredictTwo equalsByteString . paramEqualsByteString
+    testPredictTwo OnDiagonal equalsByteString . paramEqualsByteString
 
 prop_lessThanByteString :: CostingFunctions s -> Property
 prop_lessThanByteString =
-    testPredictTwo lessThanByteString . paramLessThanByteString
+    testPredictTwo Everywhere lessThanByteString . paramLessThanByteString
 
 prop_lessThanEqualsByteString :: CostingFunctions s -> Property
 prop_lessThanEqualsByteString =
-    testPredictTwo lessThanEqualsByteString . paramLessThanEqualsByteString
+    testPredictTwo Everywhere lessThanEqualsByteString . paramLessThanEqualsByteString
 
 -- prop_ifThenElse :: Property
 -- prop_ifThenElse =
@@ -160,7 +167,7 @@ propertyR :: PropertyT (R s) () -> Property
 -- to hold all the branches for reduction. These branches will contain `(R s)`,
 -- which has a `MonadIO` instance. No `NFData` for `IO`, so no `NFData` for
 -- `TreeT`. For now, this didn't crash yet.
-propertyR prop = withTests 10 $ property $ unsafeHoist unsafeRunRegion prop
+propertyR prop = withTests 100 $ property $ unsafeHoist unsafeRunRegion prop
   where
     unsafeHoist :: (MFunctor t, Monad m) => (m () -> n ()) -> t m () -> t n ()
     unsafeHoist nt = hoist (unsafeCoerce nt)
@@ -184,17 +191,18 @@ testPredictOne haskellModelFun modelR1 = propertyR $ do
         (\t -> msToPs (fromSomeSEXP t :: Double)) <$> [r|predict(modelR_hs, data.frame(x_mem=xD_hs))[[1]]|]
     predictH :: CostingInteger -> CostingInteger
     predictH x = coerce $ exBudgetCPU $ runCostingFunOneArgument modelH (ExMemory x)
-    sizeGen = paramGen
+    sizeGen = memUsageGen
   x <- forAll sizeGen
   byR <- lift $ predictR x
   diff byR (>=) 0  -- Sometimes R gives us models which pass through the origin, so we have to allow zero cost becase of that
   diff byR (~=) (predictH x)
 
 testPredictTwo
-    :: ((SomeSEXP s) -> R s (CostingFun ModelTwoArguments))
+    :: TestDomain
+    -> ((SomeSEXP s) -> R s (CostingFun ModelTwoArguments))
     -> Const (SomeSEXP s) a
     -> Property
-testPredictTwo haskellModelFun modelR1 = propertyR $ do
+testPredictTwo domain haskellModelFun modelR1 = propertyR $ do
   let modelR = getConst modelR1
   modelH <- lift $ haskellModelFun modelR
   let
@@ -207,10 +215,13 @@ testPredictTwo haskellModelFun modelR1 = propertyR $ do
         (\t -> msToPs (fromSomeSEXP t :: Double)) <$> [r|predict(modelR_hs, data.frame(x_mem=xD_hs, y_mem=yD_hs))[[1]]|]
     predictH :: CostingInteger -> CostingInteger -> CostingInteger
     predictH x y = coerce $ exBudgetCPU $ runCostingFunTwoArguments modelH (ExMemory x) (ExMemory y)
-    sizeGen = (,) <$> paramGen <*> paramGen
+    sizeGen = case domain of
+                Everywhere    -> twoArgs
+                OnDiagonal    -> memUsageGen >>= \x -> pure (x,x)
+                BelowDiagonal -> Gen.filter (uncurry (>=)) twoArgs
+        where twoArgs = (,) <$> memUsageGen <*> memUsageGen
   (x, y) <- forAll sizeGen
   byR <- lift $ predictR x y
---  let !() = Debug.Trace.trace ("(" ++ show x ++ "," ++ show y ++ "): byR -> " ++ show byR) $ ()
   diff byR (>=) 0
   diff byR (~=) (predictH x y)
 
@@ -239,58 +250,31 @@ testPredictThree haskellModelFun modelR1 = propertyR $ do
         (\t -> msToPs (fromSomeSEXP t :: Double)) <$> [r|predict(modelR_hs, data.frame(x_mem=xD_hs, y_mem=yD_hs))[[1]]|]
     predictH :: CostingInteger -> CostingInteger -> CostingInteger -> CostingInteger
     predictH x y z = coerce $ exBudgetCPU $ runCostingFunThreeArguments modelH (ExMemory x) (ExMemory y) (ExMemory z)
-    sizeGen = (,,) <$> paramGen <*> paramGen <*> paramGen
+    sizeGen = (,,) <$> memUsageGen <*> memUsageGen <*> memUsageGen
   (x, y, z) <- forAll sizeGen
   byR <- lift $ predictR x y z
   diff byR (>=) 0
   diff byR (~=) (predictH x y z)
 
-
--- main1 :: IO ()
--- main1 =  withEmbeddedR defaultConfig $ defaultMain $ [checkSequential $$(discover)]
-
-{-
-  defaultMain :: [IO Bool] -> IO ()  -- Hedgehog.Main
-  modelsR :: MonadR m => m (BuiltinCostModelBase (Const (SomeSEXP (Region m))))
-
-  withEmbeddedR :: Config -> IO a -> IO a
-
-  Initialize a new instance of R, execute actions that interact with the
-  R instance and then finalize the instance. This is typically called at
-  the very beginning of the main function of the program.
-
-  costModelsR :: MonadR m => m (BuiltinCostModelBase (Const (SomeSEXP (Region m))))
-  - A record (in an R monad) mapping builtin names to R SEXPs
-
-  instance MonadR IO, MonadR (R s)
-
-  data R s a
-
-  The R monad, for sequencing actions interacting with a single instance of the
-  R interpreter, much as the IO monad sequences actions interacting with the
-  real world. The R monad embeds the IO monad, so all IO actions can be lifted
-  to R actions
-
--}
-
+-- TODO: discover the properties automatically.  $$(discover) doesn't work
+-- because it expects to find Properties, but we have to apply each prop_xyz to
+-- 'models' to get a Property
 main :: IO ()
-main = do
-  withEmbeddedR R.defaultConfig $
-    do
+main =
+    withEmbeddedR R.defaultConfig $ do
       models <- unsafeRunRegion costModelsR
-      putStrLn "Eh?"
       let tests =
               [ ("addInteger",               prop_addInteger models)
               , ("subtractInteger",          prop_subtractInteger models)
               , ("multiplyInteger",          prop_multiplyInteger models)
---              , ("divideInteger",            prop_divideInteger models)
---              , ("quotientInteger",          prop_quotientInteger models)
---              , ("remainderInteger",         prop_remainderInteger models)
---              , ("modInteger",               prop_modInteger models)
+              , ("divideInteger",            prop_divideInteger models)
+              , ("quotientInteger",          prop_quotientInteger models)
+              , ("remainderInteger",         prop_remainderInteger models)
+              , ("modInteger",               prop_modInteger models)
               , ("lessThanInteger",          prop_lessThanInteger models)
               , ("lessThanEqualsInteger",    prop_lessThanEqualsInteger models)
               , ("equalsInteger",            prop_equalsInteger models)
---              , ("equalsByteString",         prop_equalsByteString models)
+              , ("equalsByteString",         prop_equalsByteString models)
               , ("appendByteString",         prop_appendByteString models)
               , ("lessThanByteString",       prop_lessThanByteString models)
               , ("lessThanEqualsByteString", prop_lessThanEqualsByteString models)
