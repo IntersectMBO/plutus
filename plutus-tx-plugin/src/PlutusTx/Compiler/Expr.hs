5329b6bbe7d064986b059451197804002d720eda
Marty Stumpf <thealmartyblog@gmail.com>
2021-11-26 11:54:58 -0800

    Fix typos.

--- plutus-tx-plugin/src/PlutusTx/Compiler/Expr.hs
+++ plutus-tx-plugin/src/PlutusTx/Compiler/Expr.hs
@@ -517,8 +517,8 @@ which is of function type but isn't a lambda until you reduce.
 which is a function type but isn't a function type until you reduce.
 
 It's usually okay to stop at this point, since the remaining things usually aren't "proper" arguments.
-In the term case, it's a lambda computed by an application, which won't occur form a "proper" argument.
-In the type case, we only generate type lambdas for newtypes, which will blcok "proper" arguments anyway,
+In the term case, it's a lambda computed by an application, which won't occur from a "proper" argument.
+In the type case, we only generate type lambdas for newtypes, which will block "proper" arguments anyway,
 i.e. it comes from something like this:
 
 f :: Identity (a -> a)
@@ -526,8 +526,8 @@ f = Identity (\x -> x)
 -}
 
 -- | Add entry/exit tracing inside a term's leading arguments, both term and type arguments.
--- @(\a -> /\b -> body)@ into @\a -> /\b -> entryExitTracing body@.
--- @(\a -> /\b -> body)@ into @\a -> /\b -> entryExitTracing body@.
+-- @(\a -> \b -> body)@ into @\a -> \b -> entryExitTracing body@.
+-- @(/\a -> /\b -> body)@ into @/\a -> /\b -> entryExitTracing body@.
 entryExitTracingInside ::
     PIR.Name
     -> T.Text
@@ -542,10 +542,10 @@ entryExitTracingInside lamName displayName = go mempty
             -> PLCType PLC.DefaultUni
             -> PIRTerm PLC.DefaultUni PLC.DefaultFun
         go subst (LamAbs () n t body) (PLC.TyFun () _dom cod) =
-            -- when t = \x -> body, => \x -> traceInside body
+            -- when t = \x -> body, => \x -> entryExitTracingInside body
             LamAbs () n t $ go subst body cod
         go subst (TyAbs () tn1 k body) (PLC.TyForall () tn2 _k ty) =
-            -- when t = /\x -> body, => /\x -> traceInside body
+            -- when t = /\x -> body, => /\x -> entryExitTracingInside body
             -- See Note [Profiling polymorphic functions]
             let subst' = Map.insert tn2 (PLC.TyVar () tn1) subst
             in TyAbs () tn1 k $ go subst' body ty
@@ -889,7 +889,7 @@ coverageCompile :: CompilingDefault uni fun m
                 -> m (PIRTerm uni fun)
 coverageCompile originalExpr exprType src compiledTerm covT =
   case covT of
-    -- Add a location covereage annotation to tell us "we've executed this piece of code"
+    -- Add a location coverage annotation to tell us "we've executed this piece of code"
     LocationCoverage -> do
       ann <- addLocationToCoverageIndex (toCovLoc src)
       ty <- compileTypeNorm exprType
@@ -927,7 +927,7 @@ coverageCompile originalExpr exprType src compiledTerm covT =
           _ -> throwSd CompilationError $ "Lookup of traceBool failed. Expected to get AnId but saw: " GHC.<+> (GHC.ppr traceBoolThing)
     where
       findHeadSymbol :: GHC.CoreExpr -> Maybe GHC.Id
-      findHeadSymbol (GHC.Var n)    = Just $ n
+      findHeadSymbol (GHC.Var n)    = Just n
       findHeadSymbol (GHC.App t _)  = findHeadSymbol t
       findHeadSymbol (GHC.Lam _ t)  = findHeadSymbol t
       findHeadSymbol (GHC.Tick _ t) = findHeadSymbol t
