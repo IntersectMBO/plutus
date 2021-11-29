cbabb1da95a0b024cf06e793d88af7b6bb67740a
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2021-11-29 11:05:40 +0000

    Add some tests for strict let-bindings and case patterns (#4242)
    Strict let-bindings work already, but we had no tests for them; strict
    patterns do nothing.

--- plutus-tx-plugin/test/Plugin/Basic/Spec.hs
+++ plutus-tx-plugin/test/Plugin/Basic/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -19,11 +20,16 @@ import PlutusTx.Plugin
 
 import Data.Proxy
 
+
 basic :: TestNested
 basic = testNested "Basic" [
     goldenPir "monoId" monoId
   , goldenPir "monoK" monoK
   , goldenPir "letFun" letFun
+  , goldenPir "nonstrictLet" nonstrictLet
+  , goldenPir "strictLet" strictLet
+  , goldenPir "strictMultiLet" strictMultiLet
+  , goldenPir "strictLetRec" strictLetRec
   -- must keep the scrutinee as it evaluates to error
   , goldenPir "ifOpt" ifOpt
   -- should fail
@@ -39,6 +45,21 @@ monoK = plc (Proxy @"monoK") (\(i :: Integer) -> \(_ :: Integer) -> i)
 -- GHC actually turns this into a lambda for us, try and make one that stays a let
 letFun :: CompiledCode (Integer -> Integer -> Bool)
 letFun = plc (Proxy @"letFun") (\(x::Integer) (y::Integer) -> let f z = Builtins.equalsInteger x z in f y)
+
+nonstrictLet :: CompiledCode (Integer -> Integer -> Integer)
+nonstrictLet = plc (Proxy @"strictLet") (\(x::Integer) (y::Integer) -> let z = Builtins.addInteger x y in Builtins.addInteger z z)
+
+-- GHC turns strict let-bindings into case expressions, which we correctly turn into strict let-bindings
+strictLet :: CompiledCode (Integer -> Integer -> Integer)
+strictLet = plc (Proxy @"strictLet") (\(x::Integer) (y::Integer) -> let !z = Builtins.addInteger x y in Builtins.addInteger z z)
+
+strictMultiLet :: CompiledCode (Integer -> Integer -> Integer)
+strictMultiLet = plc (Proxy @"strictLet") (\(x::Integer) (y::Integer) -> let !z = Builtins.addInteger x y; !q = Builtins.addInteger z z; in Builtins.addInteger q q)
+
+-- Here we see the wrinkles of GHC's codegen: GHC creates let-bindings for the recursion, with _nested_ case expressions for the strictness.
+-- So we get non-strict external bindings for z and q, and inside that we get strict bindings corresponding to the case expressions.
+strictLetRec :: CompiledCode (Integer -> Integer -> Integer)
+strictLetRec = plc (Proxy @"strictLetRec") (\(x::Integer) (y::Integer) -> let !z = Builtins.addInteger x q; !q = Builtins.addInteger y z in Builtins.addInteger z z)
 
 ifOpt :: CompiledCode Integer
 ifOpt = plc (Proxy @"ifOpt") (if ((1 `Builtins.divideInteger` 0) `Builtins.equalsInteger` 0) then 1 else 1)
