b7e4f2ae5d1e57faec92f94956e689ee113a9cfb
Marty Stumpf <thealmartyblog@gmail.com>
2021-11-29 12:09:31 -0800

    Add LiteralConst and ConArgs.

--- plutus-core/plutus-core/src/PlutusCore/Lexer/Lexer.hs
+++ plutus-core/plutus-core/src/PlutusCore/Lexer/Lexer.hs
@@ -26,45 +26,6 @@ type; having done this, we return to the initial lexer state, denoted by 0,
 where we can use keywords as variable names. A similar strategy is used for
 built in type names.  -}
 
-{- Note [Literal Constants]
-For literal constants, we accept certain types of character sequences that are
-then passed to user-defined parsers which convert them to built-in constants.
-Literal constants have to specify the type of the constant, so we have (con
-integer 9), (con string "Hello"), and so on.  This allows us to use the same
-literal syntax for different types (eg, integer, short, etc) and shift most
-of the responsibility for parsing constants out of the lexer and into the
-parser (and eventually out of the parser to parsers supplied by the types
-themselves).
-
-In the body of a constant we allow:
-  * ()
-  * Single-quoted possibly empty sequences of printable characters
-  * Double-quoted possibly empty sequences of printable characters
-  * Unquoted non-empty sequences of printable characters not including '(' or ')',
-    and not beginning with a single or double quote.  Spaces are allowed in the
-    body of the sequence, but are ignored at the beginning or end.
-
-"Printable" here uses Alex's definition: Unicode code points 32 to 0x10ffff.
-This includes spaces but excludes tabs amongst other things.  One can use the
-usual escape sequences though, as long as the type-specific parser deals with
-them.
-
-These allow us to parse all of the standard types.  We just return all of the
-characters in a TkLiteralConst token, not attempting to do things like stripping
-off quotes or interpreting escape sequences: it's the responsibility of the
-parser for the relevant type to do these things.  Note that 'read' will often do
-the right thing.
-
-The final item above even allows the possibility of parsing complex types such as
-tuples and lists as long as parentheses are not involved.  For example, (con
-tuple <1,2.3,"yes">) and (con intlist [1, 2, -7]) are accepted by the lexer, as
-is the somewhat improbable-looking (con intseq 12 4 55 -4).  Comment characters
-are also allowed, but are not treated specially.  We don't allow (con )) or (con
-tuple (1,2,3)) because it would be difficult for the lexer to decide when it
-had reached the end of the literal: consider a tuple containing a quoted string
-containing ')', for example.
--}
-
 {- Note [Precedence of regular expression matches]
 For reference, Section 3.2.2 of the Alex manual says "When the input stream
 matches more than one rule, the rule which matches the longest prefix of the
