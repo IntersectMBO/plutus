3c69603cea7a9d475ac7f80bd2202538f5eb4bfe
Marty Stumpf <thealmartyblog@gmail.com>
2021-12-17 11:06:17 -0800

    WIP set up core parser.

--- plutus-core/plutus-core/src/PlutusCore/Parser.hs
+++ plutus-core/plutus-core/src/PlutusCore/Parser.hs
@@ -36,6 +36,32 @@ import Data.Proxy
 import Data.Text qualified as T
 import Text.Megaparsec (SourcePos)
 
+tyInst :: a -> Term tyname name uni fun a -> NonEmpty (Type tyname uni a) -> Term tyname name uni fun a
+tyInst loc t (ty :| [])  = TyInst loc t ty
+tyInst loc t (ty :| tys) = TyInst loc (tyInst loc t (ty:|init tys)) (last tys)
+
+tyApps :: a -> Type tyname uni a -> NonEmpty (Type tyname uni a) -> Type tyname uni a
+tyApps loc ty (ty' :| [])  = TyApp loc ty ty'
+tyApps loc ty (ty' :| tys) = TyApp loc (tyApps loc ty (ty':|init tys)) (last tys)
+
+app :: a -> Term tyname name uni fun a -> NonEmpty (Term tyname name uni fun a) -> Term tyname name uni fun a
+app loc t (t' :| []) = Apply loc t t'
+app loc t (t' :| ts) = Apply loc (app loc t (t':|init ts)) (last ts)
+
+--- Running the parser ---
+
+-- parseST :: Parse a -> ByteString -> StateT IdentifierState (Except ParseError) a
+-- parseST p str = runAlexST' str (runExceptT p) >>= liftEither
+
+mapParseRun :: (AsParseError e, MonadError e m, MonadQuote m) => StateT IdentifierState (Except ParseError) b -> m b
+-- we need to run the parser starting from our current next unique, then throw away the rest of the
+-- parser state and get back the new next unique
+mapParseRun run = do
+    nextU <- liftQuote freshUnique
+    (p, (_, u)) <- throwingEither _ParseError $ runExcept $ runStateT run (identifierStateFrom nextU)
+    liftQuote $ markNonFreshBelow u
+    pure p
+
 -- | Parse a PLC program. The resulting program will have fresh names. The underlying monad must be capable
 -- of handling any parse errors.
 parseProgram :: (MonadError e m, MonadQuote m) => ByteString -> m (Program TyName Name DefaultUni DefaultFun SourcePos)
