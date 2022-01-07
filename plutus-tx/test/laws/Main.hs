9ab1b1ba31af6713f42130de9283fd0519d7e4e8
Koz Ross <koz.ross@retro-freedom.nz>
2022-01-07 14:18:08 +1300

    Numerical hierarchy laws

--- plutus-tx/test/laws/Main.hs
+++ plutus-tx/test/laws/Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE KindSignatures     #-}
 {-# LANGUAGE LambdaCase         #-}
 {-# LANGUAGE OverloadedStrings  #-}
-{-# OPTIONS_GHC -Wno-orphans #-} -- We need Arg for hedgehog-fn
+{-# OPTIONS_GHC -Wno-orphans #-} -- We need an Arg instance for hedgehog-fn
 
 module Main (main) where
 
@@ -10,7 +10,7 @@ import Prelude
 
 import Data.Functor.Contravariant (contramap)
 import Data.Kind (Type)
-import Hedgehog (Gen, Property, cover, forAllWith, property, success, (===))
+import Hedgehog (Gen, Property, cover, forAllWith, property, success, (/==), (===))
 import Hedgehog.Function (Arg (build), CoGen, fnWith, forAllFn, vary, via)
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
@@ -27,6 +27,34 @@ main = defaultMain . localOption (HedgehogTestLimit . Just $ 1000) . testGroup "
     testProperty "== is symmetric" propEqSymm,
     testProperty "== is transitive" propEqTrans,
     testProperty "== implies substitution" propEqSub
+    ],
+  testGroup "Ord" [
+    testProperty "<= is reflexive" propOrdRefl,
+    testProperty "<= is anti-symmetric" propOrdAntiSymm,
+    testProperty "<= is transitive" propOrdTrans,
+    testProperty "== implies EQ" propOrdCompare
+    ],
+  testGroup "AdditiveGroup" [
+    testProperty "+ commutes" propPlusComm,
+    testProperty "+ associates" propPlusAssoc,
+    testProperty "zero is an identity" propZeroId,
+    testProperty "x - x = zero" propMinusCancel
+    ],
+  testGroup "MultiplicativeMonoid" [
+    testProperty "* associates" propTimesAssoc,
+    testProperty "one is a left identity" propOneLeftId,
+    testProperty "one is a right identity" propOneRightId
+    ],
+  testGroup "Ring" [
+    testProperty "zero is a left annihilator" propZeroLeftAnnih,
+    testProperty "zero is a right annihilator" propZeroRightAnnih,
+    testProperty "* left-distributes over +" propTimesLeftDistPlus,
+    testProperty "* right-distributes over +" propTimesRightDistPlus
+    ],
+  testGroup "Module" [
+    testProperty "scale one = id" propScaleOne,
+    testProperty "scale distributes over +" propScaleDistPlus,
+    testProperty "scale x (scale y r) = scale (x * y) r" propScaleTimes
     ]
   ]
 
@@ -67,6 +95,124 @@ propEqSub = property $ do
   if x Plutus.== y
   then f x === f y
   else success
+
+propOrdRefl :: Property
+propOrdRefl = property $ do
+  x <- forAllWith ppShow genRational
+  (x Plutus.<= x) === True
+
+propOrdAntiSymm :: Property
+propOrdAntiSymm = property $ do
+  ent <- forAllWith ppShow (entangled genRational)
+  cover 45 "identical" . isDefinitelyIdentical $ ent
+  cover 45 "possibly different" . not . isDefinitelyIdentical $ ent
+  let (x, y) = collapse ent
+  if x Plutus.<= y && y Plutus.<= x
+  then x === y
+  else success
+
+propOrdTrans :: Property
+propOrdTrans = property $ do
+  ent3 <- forAllWith ppShow (entangled3 genRational)
+  cover 45 "identical" . isDefinitelyIdentical3 $ ent3
+  cover 45 "possibly different" . not . isDefinitelyIdentical3 $ ent3
+  let (x, y, z) = collapse3 ent3
+  if x Plutus.<= y && y Plutus.<= z
+  then (x Plutus.<= z) === True
+  else success
+
+propOrdCompare :: Property
+propOrdCompare = property $ do
+  ent <- forAllWith ppShow (entangled genRational)
+  cover 45 "identical" . isDefinitelyIdentical $ ent
+  cover 45 "possibly different" . not . isDefinitelyIdentical $ ent
+  let (x, y) = collapse ent
+  if x Plutus.== y
+  then Plutus.compare x y === Plutus.EQ
+  else Plutus.compare x y /== Plutus.EQ
+
+propPlusComm :: Property
+propPlusComm = property $ do
+  x <- forAllWith ppShow genRational
+  y <- forAllWith ppShow genRational
+  x Plutus.+ y === y Plutus.+ x
+
+propPlusAssoc :: Property
+propPlusAssoc = property $ do
+  x <- forAllWith ppShow genRational
+  y <- forAllWith ppShow genRational
+  z <- forAllWith ppShow genRational
+  (x Plutus.+ y) Plutus.+ z === x Plutus.+ (y Plutus.+ z)
+
+propZeroId :: Property
+propZeroId = property $ do
+  x <- forAllWith ppShow genRational
+  x Plutus.+ Plutus.zero === x
+
+propMinusCancel :: Property
+propMinusCancel = property $ do
+  x <- forAllWith ppShow genRational
+  x Plutus.- x === Plutus.zero
+
+propTimesAssoc :: Property
+propTimesAssoc = property $ do
+  x <- forAllWith ppShow genRational
+  y <- forAllWith ppShow genRational
+  z <- forAllWith ppShow genRational
+  (x Plutus.* y) Plutus.* z === x Plutus.* (y Plutus.* z)
+
+propOneLeftId :: Property
+propOneLeftId = property $ do
+  x <- forAllWith ppShow genRational
+  Plutus.one Plutus.* x === x
+
+propOneRightId :: Property
+propOneRightId = property $ do
+  x <- forAllWith ppShow genRational
+  x Plutus.* Plutus.one === x
+
+propZeroLeftAnnih :: Property
+propZeroLeftAnnih = property $ do
+  x <- forAllWith ppShow genRational
+  Plutus.zero Plutus.* x === Plutus.zero
+
+propZeroRightAnnih :: Property
+propZeroRightAnnih = property $ do
+  x <- forAllWith ppShow genRational
+  x Plutus.* Plutus.zero === Plutus.zero
+
+propTimesLeftDistPlus :: Property
+propTimesLeftDistPlus = property $ do
+  x <- forAllWith ppShow genRational
+  y <- forAllWith ppShow genRational
+  z <- forAllWith ppShow genRational
+  x Plutus.* (y Plutus.+ z) === (x Plutus.* y) Plutus.+ (x Plutus.* z)
+
+propTimesRightDistPlus :: Property
+propTimesRightDistPlus = property $ do
+  x <- forAllWith ppShow genRational
+  y <- forAllWith ppShow genRational
+  z <- forAllWith ppShow genRational
+  (x Plutus.+ y) Plutus.* z === (x Plutus.* z) Plutus.+ (y Plutus.* z)
+
+propScaleOne :: Property
+propScaleOne = property $ do
+  x <- forAllWith ppShow genRational
+  Plutus.scale Plutus.one x === x
+
+propScaleDistPlus :: Property
+propScaleDistPlus = property $ do
+  x <- forAllWith ppShow genInteger
+  y <- forAllWith ppShow genRational
+  z <- forAllWith ppShow genRational
+  Plutus.scale x (y Plutus.+ z) === (Plutus.scale x y) Plutus.+ (Plutus.scale x z)
+
+propScaleTimes :: Property
+propScaleTimes = property $ do
+  x <- forAllWith ppShow genInteger
+  y <- forAllWith ppShow genInteger
+  r <- forAllWith ppShow genRational
+  Plutus.scale x (Plutus.scale y r) === Plutus.scale (x Plutus.* y) r
 
 -- Helpers
 
@@ -120,8 +266,8 @@ collapse3 = \case
 
 isDefinitelyIdentical3 :: Entangled3 a -> Bool
 isDefinitelyIdentical3 = \case
-  Identical3 _   -> True
-  Regular3 _ _ _ -> False
+  Identical3 _ -> True
+  Regular3{}   -> False
 
 entangled3 :: forall (a :: Type) . Gen a -> Gen (Entangled3 a)
 entangled3 gen = Gen.choice [Identical3 <$> gen, Regular3 <$> gen <*> gen <*> gen]
