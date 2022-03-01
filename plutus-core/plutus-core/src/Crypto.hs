2e5202209f7659d4a82bdcde8f5b2ef77ac40e89
kwxm <kenneth.mackenzie@iohk.io>
2022-03-01 05:02:08 +0000

    Add some comments for verifySignature

--- plutus-core/plutus-core/src/Crypto.hs
+++ plutus-core/plutus-core/src/Crypto.hs
@@ -5,11 +5,13 @@ import Crypto.ECC.Ed25519Donna
 import Crypto.Error (maybeCryptoError)
 import Data.ByteString qualified as BS
 
+-- Ed25519-donna signature verification
+-- This will fail if the key or the signature are not of the expected length.
 verifySignature
     :: Alternative f
-    => BS.ByteString  -- ^ Public Key
-    -> BS.ByteString  -- ^ Message
-    -> BS.ByteString  -- ^ Signature
+    => BS.ByteString  -- ^ Public Key (32 bytes)
+    -> BS.ByteString  -- ^ Message    (arbirtary length)
+    -> BS.ByteString  -- ^ Signature  (64 bytes)
     -> f Bool
 verifySignature pubKey msg sig =
     maybe empty pure . maybeCryptoError $
