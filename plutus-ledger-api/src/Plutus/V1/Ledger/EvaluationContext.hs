57bb3bb34b87bd5019097a67c873207f13f2fe55
Nikolaos Bezirgiannis <bezirg@users.noreply.github.com>
2022-05-10 13:29:21 +0200

    Remove EcdsaSecp256k1,SchnorrSecp256k1 from costparamnames

--- plutus-ledger-api/src/Plutus/V1/Ledger/EvaluationContext.hs
+++ plutus-ledger-api/src/Plutus/V1/Ledger/EvaluationContext.hs
@@ -40,9 +40,10 @@ costModelParamNames = Map.keysSet $ fromJust $ extractCostModelParams $
             -- transform all costing-functions to (Just costingFun)
             (bmap (MCostingFun . Just) bcm)
             {
-              -- 'SerialiseData' builtin not available in V1
+              -- 'SerialiseData','EcdsaSecp256k1',SchnorrSecp256k1 builtins not available in V1
               paramSerialiseData = mempty
-              -- TODO: do the same for schnorr and ellipticcurve costingfuns
+            , paramVerifyEcdsaSecp256k1Signature = mempty
+            , paramVerifySchnorrSecp256k1Signature = mempty
             }
 
 -- | The raw cost model params, only to be used for testing purposes.
