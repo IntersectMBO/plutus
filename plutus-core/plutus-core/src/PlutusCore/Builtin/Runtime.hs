fed68b0b13d2b1cbd2e2f6bbd8e2b960a1c55707
effectfully <effectfully@gmail.com>
2022-05-12 16:14:40 +0200

    Docs

--- plutus-core/plutus-core/src/PlutusCore/Builtin/Runtime.hs
+++ plutus-core/plutus-core/src/PlutusCore/Builtin/Runtime.hs
@@ -84,9 +84,7 @@ type family ToCostingType n where
 data BuiltinRuntime val =
     forall n. BuiltinRuntime
         (RuntimeScheme n)
-        (ToRuntimeDenotationType val n)  -- Must be lazy, because we don't want to compute the
-                                          -- denotation when it's fully saturated before figuring
-                                          -- out what it's going to cost.
+        (ToRuntimeDenotationType val n)
         (ToCostingType n)
 
 -- | Determines how to unlift arguments. The difference is that with 'UnliftingImmediate' unlifting
