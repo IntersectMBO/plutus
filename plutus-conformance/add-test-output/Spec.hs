8f4740389ac650800c73f5b91d0472fae9360b29
Marty Stumpf <thealmartyblog@gmail.com>
2022-05-17 12:18:15 -0700

    Address PR comments.

--- plutus-conformance/add-test-output/Spec.hs
+++ plutus-conformance/add-test-output/Spec.hs
@@ -1,15 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | This executable is for easy addition of tests. When run,
 -- output files will be added to all tests that had no output files.
 -- You are advised to manually check that the output is correct.
-
-{-# LANGUAGE OverloadedStrings #-}
+-- In other parts of the codebase we use golden tests with the `accept` test option turned on.
+-- Here we use an executable for easier customization.
 
 module Main
     ( main
     ) where
 
-import Common (UplcProg, evalUplcProg, stripePosProg)
+import Common (UplcProg, evalUplcProg)
 import Control.Exception (SomeException, evaluate, try)
+import Control.Monad (filterM)
+import Data.Foldable (for_)
 import Data.Text qualified as T
 import Options.Applicative
 import PlutusCore.Error (ParserErrorBundle (ParseErrorB))
@@ -21,10 +25,13 @@ import Test.Tasty.Golden (findByExtension)
 import Text.Megaparsec (errorBundlePretty)
 import UntypedPlutusCore.Parser as UPLC (parse, program)
 
--- |  The arguments to the executable. (1) file extension to be searched,
--- (2) directory to be searched,
--- (3) the action to run the input files through; eval (for evaluation tests) or typecheck (for typechecking tests).
-data Args = MkArgs String FilePath Runner
+-- |  The arguments to the executable.
+data Args = MkArgs
+  { _argExt    :: String -- ^ file extension to be searched
+  , _argDir    :: FilePath -- ^ directory to be searched
+  , _argRunner :: Runner
+  -- ^ the action to run the input files through; eval (for evaluation tests) or typecheck (for typechecking tests)
+  }
 
 ext :: Parser String
 ext =
@@ -59,15 +66,17 @@ args = info ((MkArgs <$> ext <*> dir <*> runner) <**> helper)
   (fullDesc <> progDesc helpText)
 
 helpText :: String
-helpText = "This program adds test outputs to specified inputs." <>
-  "To run the program, input the following 3 arguments: " <>
-  "(1) file extension to be searched " <>
-  "(2) directory to be searched " <>
-  "(3) the action to run the input files through; eval (for evaluation tests) or typecheck (for typechecking tests). " <>
-  "E.g. run " <>
-  "`cabal run add-test-output .uplc plutus-conformance/uplc/ eval` " <>
-  "to have the executable search for files with extension `.uplc` in the /uplc directory that are missing output files. " <>
-  " It will evaluate and create output files for them."
+helpText = unlines
+  ["This program adds test outputs to specified inputs."
+  , "To run the program, input the following 3 arguments: "
+  , "(1) file extension to be searched "
+  , "(2) directory to be searched "
+  , "(3) the action to run the input files through; eval (for evaluation tests) or typecheck (for typechecking tests). "
+  , "E.g. run "
+  , "`cabal run add-test-output .uplc plutus-conformance/uplc/ eval` "
+  , "to have the executable search for files with extension `.uplc` in the /uplc directory that are missing output files. "
+  , " It will evaluate and create output files for them."
+  ]
 
 
 main :: IO ()
@@ -75,13 +84,10 @@ main = do
     MkArgs extension directory run <- customExecParser (prefs showHelpOnEmpty) args
     allInputFiles <- findByExtension [extension] directory
     -- only choose the ones without an output file, so as to not edit the ones already with outputs
-    inputFiles <- sequenceA $
-      [do
-              hasOut <- doesFileExist (testIn <> ".expected")
-              if hasOut then pure [] else pure [testIn] | testIn <- allInputFiles]
+    inputFiles <- filterM (fmap (fmap not) (\testIn -> doesFileExist (testIn <> ".expected"))) allInputFiles
     case run of
       Eval -> do
-        mapM_
+        for_ inputFiles
           (\inputFile -> do
             inputStr <- readFile inputFile
             let parsed = runQuoteT $ UPLC.parse UPLC.program inputFile $ T.pack inputStr
@@ -92,7 +98,7 @@ main = do
                 putStrLn $ inputFile <> " failed to parse. Error written to " <> outFilePath
                 writeFile outFilePath (errorBundlePretty peb)
               Right pro -> do
-                res <- try (evalUplcProg (stripePosProg pro) >>= evaluate):: IO (Either SomeException (EvaluationResult UplcProg))
+                res <- try (evaluate $ evalUplcProg (() <$ pro)):: IO (Either SomeException (EvaluationResult UplcProg))
                 case res of
                   Right (EvaluationSuccess prog) -> do
                     putStrLn $ inputFile <> " evaluated; result written to " <> outFilePath
@@ -100,12 +106,11 @@ main = do
                   Right EvaluationFailure      -> do
                     -- warn the user that the file failed to evaluate
                     putStrLn $ inputFile <> " failed to evaluate. Failure written to " <> outFilePath
-                    writeFile outFilePath ((show :: EvaluationResult UplcProg -> String) EvaluationFailure)
-                  Left evalException -> do
+                    writeFile outFilePath (show (EvaluationFailure :: EvaluationResult UplcProg))
+                  Left _ -> do
                     -- warn the user that exception is thrown
-                    putStrLn $ "Exception thrown during evaluation of " <> inputFile <>". Exception written to " <> outFilePath
-                    writeFile outFilePath (show evalException)
+                    putStrLn $ "Exception thrown during evaluation of " <> inputFile <>".Written to " <> outFilePath
+                    writeFile outFilePath "Evaluation failure"
           )
-          (concat inputFiles)
       Typecheck ->
         putStrLn "typechecking has not been implemented yet. Only evaluation tests (eval) are supported."
--- plutus-conformance/src/Common.hs
+++ plutus-conformance/src/Common.hs
@@ -1,68 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Common where
 
-import GHC.IO (unsafePerformIO)
-import PlutusCore.Core (Version (Version), defaultVersion)
+import PlutusCore.Core (defaultVersion)
 import PlutusCore.Default (DefaultFun, DefaultUni)
 import PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCekParameters)
 import PlutusCore.Evaluation.Result (EvaluationResult)
 import PlutusCore.Name (Name)
 import Prelude
-import Test.Tasty
-import Test.Tasty.HUnit
-import Text.Megaparsec (SourcePos)
 import UntypedPlutusCore.Core.Type qualified as UPLC
 import UntypedPlutusCore.Evaluation.Machine.Cek (unsafeEvaluateCekNoEmit)
-
-data TestContent =
-    MkTestContent {
-        testName    :: FilePath
-        , expected  :: EvaluationResult UplcProg
-        , inputProg :: UplcProg
-    }
-
-mkTestContents :: [FilePath] -> [EvaluationResult UplcProg] -> [UplcProg] -> [TestContent]
-mkTestContents lFilepaths lRes lProgs =
-    if length lFilepaths == length lRes && length  lRes == length lProgs then
-        [MkTestContent file res prog | file <- lFilepaths, res <- lRes, prog <- lProgs]
-    else
-        error $ "mkTestContents: Cannot run the tests because the number of input and output programs are not the same. " <>
-            "Number of input files: " <> show (length lProgs) <>
-            " Number of output files: " <> show (length lRes) <>
-            " Make sure all your input programs have an accompanying .expected file."
 
 type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
 
 termToProg :: UPLC.Term Name DefaultUni DefaultFun () -> UplcProg
 termToProg = UPLC.Program () (defaultVersion ())
 
-evalUplcProg :: UplcProg -> IO (EvaluationResult UplcProg)
-evalUplcProg p = pure $
+evalUplcProg :: UplcProg -> EvaluationResult UplcProg
+evalUplcProg p =
     fmap
         termToProg
         (unsafeEvaluateCekNoEmit defaultCekParameters (UPLC._progTerm p))
-
-stripePosProg :: UPLC.Program Name DefaultUni DefaultFun SourcePos -> UplcProg
-stripePosProg (UPLC.Program _ann (Version _ num1 num2 num3) tm) = UPLC.Program () (Version () num1 num2 num3) (stripePosTm tm)
-
-stripePosTm :: UPLC.Term Name DefaultUni DefaultFun SourcePos -> UPLC.Term Name DefaultUni DefaultFun ()
-stripePosTm (UPLC.Var _ name)       = UPLC.Var () name
-stripePosTm (UPLC.LamAbs _ name tm) = UPLC.LamAbs () name (stripePosTm tm)
-stripePosTm (UPLC.Apply _ tm1 tm2)  = UPLC.Apply () (stripePosTm tm1) (stripePosTm tm2)
-stripePosTm (UPLC.Force _ tm)       = UPLC.Force () (stripePosTm tm)
-stripePosTm (UPLC.Delay _ tm)       = UPLC.Delay () (stripePosTm tm)
-stripePosTm (UPLC.Constant _ val)   = UPLC.Constant () val
-stripePosTm (UPLC.Builtin _ f)      = UPLC.Builtin () f
-stripePosTm (UPLC.Error _)          = UPLC.Error ()
-
-mkTestCases :: [TestContent] -> (UplcProg -> IO (EvaluationResult UplcProg)) -> [TestTree]
-mkTestCases tests runner
-  = [ unsafePerformIO $ do
-      result <- runner (inputProg test)
-      pure $ testCase (testName test) (expected test @=? result)
-       | test <- tests]
-
-testUplcEvaluation :: [TestContent] -> (UplcProg -> IO (EvaluationResult UplcProg)) -> TestTree
-testUplcEvaluation lTest runner =
-    testGroup "UPLC evaluation tests" $ mkTestCases lTest runner
