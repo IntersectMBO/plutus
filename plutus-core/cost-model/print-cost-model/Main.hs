bae99123a3c5ff611301a278cdc8ca76180aa73e
kwxm <kenneth.mackenzie@iohk.io>
2022-05-18 15:30:55 +0100

    Spurious comment

--- plutus-core/cost-model/print-cost-model/Main.hs
+++ plutus-core/cost-model/print-cost-model/Main.hs
@@ -68,8 +68,6 @@ data Model
    nice to rationalise this a bit, but it may be too late to do that.
 -}
 
-{- Corece an Aeson Value to an Object (ie, a map) if possible. -}
-
 instance FromJSON Model where
     parseJSON =
         withObject "Model" $
