e2369f062817858ffd2cd8298b0d659cfe7b1012
Marty Stumpf <thealmartyblog@gmail.com>
2022-05-19 11:24:07 -0700

    SCP-3886 Add missing test outputs and an executable that generates the test outputs. (#4609)

--- plutus-conformance/src/Common.hs
+++ plutus-conformance/src/Common.hs
@@ -1,1 +1,20 @@
-module PlutusConformance.Common where
+module Common where
+
+import PlutusCore.Core (defaultVersion)
+import PlutusCore.Default (DefaultFun, DefaultUni)
+import PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCekParameters)
+import PlutusCore.Evaluation.Result (EvaluationResult)
+import PlutusCore.Name (Name)
+import UntypedPlutusCore.Core.Type qualified as UPLC
+import UntypedPlutusCore.Evaluation.Machine.Cek (unsafeEvaluateCekNoEmit)
+
+type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
+
+termToProg :: UPLC.Term Name DefaultUni DefaultFun () -> UplcProg
+termToProg = UPLC.Program () (defaultVersion ())
+
+evalUplcProg :: UplcProg -> EvaluationResult UplcProg
+evalUplcProg p =
+    fmap
+        termToProg
+        (unsafeEvaluateCekNoEmit defaultCekParameters (UPLC._progTerm p))
