51c32c8dd221e956834919f3f142ac4dfb8ed09c
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-05-31 15:35:08 +0100

    Add editorconfig-checker in CI

---
+++ nix/tests/editorconfig-checker.nix
@@ -0,0 +1,23 @@
+{ runCommand, editorconfig-checker, src }:
+
+# Runs `editorconfig-checker` on ${src}. If there are errors,
+# they are written to `$out/nix-support/hydra-build-products`
+runCommand "editorconfig-checker"
+{
+  buildInputs = [ editorconfig-checker ];
+} ''
+  set +e
+  mkdir -p $out/nix-support
+
+  -- changing to the directory and then running it gives better output than
+  -- passing the directory to check, since file names are shorter
+  cd ${src}
+  editorconfig-checker 2>&1 | tee $out/nix-support/editorconfig-checker.log
+
+  if [ $? -ne 0 ]; then
+    echo "*** editorconfig-checker found files that don't match the configuration"
+    exit 1
+  else
+    echo 0 > $out
+  fi
+''
