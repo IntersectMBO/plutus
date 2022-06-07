a960e66df2a381f0216b258e8a76ed5176b4b48e
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-07 15:08:17 -0700

    Add pre-commit to shell.nix

--- shell.nix
+++ shell.nix
@@ -59,6 +59,7 @@ let
     editorconfig-core-c
     ghcid
     jq
+    pre-commit
     nixFlakesAlias
     nixpkgs-fmt
     shellcheck
@@ -91,7 +92,7 @@ haskell.project.shellFor {
     ${pre-commit-check.shellHook}
   '';
 
-  # This is no longer set automatically as of more recent `haskell.nix` revisions, 
+  # This is no longer set automatically as of more recent `haskell.nix` revisions,
   # but is useful for users with LANG settings.
   LOCALE_ARCHIVE = lib.optionalString (stdenv.hostPlatform.libc == "glibc") "${glibcLocales}/lib/locale/locale-archive";
 }
