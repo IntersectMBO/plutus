3c39ca36d004b50319b57ce269ba50dd38ea3658
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-14 06:54:25 -0700

    PLT-108: Script evaluation regression test types (#4691)

---
+++ plutus-ledger-api/testlib/PlutusLedgerApi/Test/EvaluationEvent.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+module PlutusLedgerApi.Test.EvaluationEvent (
+    ScriptEvaluationEvent (..),
+    ScriptEvaluationData (..),
+    ScriptEvaluationResult (..),
+) where
+
+import PlutusCore.Data qualified as PLC
+import PlutusCore.Evaluation.Machine.ExBudget (ExBudget)
+import PlutusLedgerApi.Common
+
+import Codec.Serialise (Serialise (..))
+import GHC.Generics (Generic)
+
+data ScriptEvaluationResult = ScriptEvaluationSuccess | ScriptEvaluationFailure
+    deriving stock (Generic)
+    deriving anyclass (Serialise)
+
+data ScriptEvaluationData = ScriptEvaluationData
+    { dataProtocolVersion :: ProtocolVersion
+    , dataCostParams      :: [Integer]
+    , dataBudget          :: ExBudget
+    , dataScript          :: SerialisedScript
+    , dataInputs          :: [PLC.Data]
+    }
+    deriving stock (Generic)
+    deriving anyclass (Serialise)
+
+data ScriptEvaluationEvent
+    = PlutusV1Event ScriptEvaluationData ScriptEvaluationResult
+    | PlutusV2Event ScriptEvaluationData ScriptEvaluationResult
+    deriving stock (Generic)
+    deriving anyclass (Serialise)
