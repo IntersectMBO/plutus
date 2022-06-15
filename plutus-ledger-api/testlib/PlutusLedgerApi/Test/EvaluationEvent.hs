8dee5f5e034a8e9c6db0fa22d4fa599392d9005e
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-15 05:36:28 -0700

    Add Haddock and checkEvaluationEvent

--- plutus-ledger-api/testlib/PlutusLedgerApi/Test/EvaluationEvent.hs
+++ plutus-ledger-api/testlib/PlutusLedgerApi/Test/EvaluationEvent.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module PlutusLedgerApi.Test.EvaluationEvent (
     ScriptEvaluationEvents (..),
     ScriptEvaluationEvent (..),
     ScriptEvaluationData (..),
     ScriptEvaluationResult (..),
+    UnexpectedEvaluationResult (..),
+    checkEvaluationEvent,
 ) where
 
 import PlutusCore.Data qualified as PLC
@@ -14,11 +17,14 @@ import PlutusLedgerApi.Common
 import Codec.Serialise (Serialise (..))
 import Data.List.NonEmpty (NonEmpty)
 import GHC.Generics (Generic)
+import PlutusLedgerApi.V1 qualified as V1
+import PlutusLedgerApi.V2 qualified as V2
 
 data ScriptEvaluationResult = ScriptEvaluationSuccess | ScriptEvaluationFailure
     deriving stock (Generic)
     deriving anyclass (Serialise)
 
+-- | Data used for an on-chain script evaluation.
 data ScriptEvaluationData = ScriptEvaluationData
     { dataProtocolVersion :: ProtocolVersion
     , dataBudget          :: ExBudget
@@ -28,12 +34,18 @@ data ScriptEvaluationData = ScriptEvaluationData
     deriving stock (Generic)
     deriving anyclass (Serialise)
 
+-- | Information about an on-chain script evaluation event.
 data ScriptEvaluationEvent
     = PlutusV1Event ScriptEvaluationData ScriptEvaluationResult
     | PlutusV2Event ScriptEvaluationData ScriptEvaluationResult
     deriving stock (Generic)
     deriving anyclass (Serialise)
 
+-- | This type contains a list of on-chain script evaluation events. All PlutusV1
+-- evaluations (if any) share the same cost parameters. Same with PlutusV2.
+--
+-- Sharing the cost parameters lets us avoid creating a new `EvaluationContext` for
+-- each `ScriptEvaluationEvent`.
 data ScriptEvaluationEvents = ScriptEvaluationEvents
     { eventsCostParamsV1 :: Maybe [Integer]
     -- ^ Cost parameters shared by all PlutusV1 evaluation events in `eventsEvents`, if any.
@@ -43,3 +55,47 @@ data ScriptEvaluationEvents = ScriptEvaluationEvents
     }
     deriving stock (Generic)
     deriving anyclass (Serialise)
+
+-- | Error type when re-evaluating a `ScriptEvaluationEvent`.
+data UnexpectedEvaluationResult
+    = UnexpectedEvaluationResult
+        ScriptEvaluationEvent
+        [Integer]
+        -- ^ Cost parameters
+        (Either EvaluationError ExBudget)
+        -- ^ Actual evaluation outcome
+
+-- | Re-evaluate an on-chain script evaluation event.
+checkEvaluationEvent ::
+    EvaluationContext ->
+    -- | Cost parameters
+    [Integer] ->
+    ScriptEvaluationEvent ->
+    Maybe UnexpectedEvaluationResult
+checkEvaluationEvent ctx params ev = case ev of
+    PlutusV1Event ScriptEvaluationData{..} expected ->
+        let (_, actual) =
+                V1.evaluateScriptRestricting
+                    dataProtocolVersion
+                    V1.Quiet
+                    ctx
+                    dataBudget
+                    dataScript
+                    dataInputs
+         in verify expected actual
+    PlutusV2Event ScriptEvaluationData{..} expected ->
+        let (_, actual) =
+                V2.evaluateScriptRestricting
+                    dataProtocolVersion
+                    V2.Quiet
+                    ctx
+                    dataBudget
+                    dataScript
+                    dataInputs
+         in verify expected actual
+  where
+    verify ScriptEvaluationSuccess (Left err) =
+        Just $ UnexpectedEvaluationResult ev params (Left err)
+    verify ScriptEvaluationFailure (Right budget) =
+        Just $ UnexpectedEvaluationResult ev params (Right budget)
+    verify _ _ = Nothing
