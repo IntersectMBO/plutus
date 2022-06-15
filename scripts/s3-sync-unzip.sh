48191d7b95c25829885d44cbc9304106268da1dd
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-15 05:49:16 -0700

    Address comments

--- scripts/s3-sync-unzip.sh
+++ scripts/s3-sync-unzip.sh
@@ -13,11 +13,22 @@
 
 set -euo pipefail
 
+# Only download S3 objects whose keys start with this
 S3_PREFIX=$1
+# Only download S3 objects whose keys end with this
 S3_SUFFIX=${2:-"*"}
 
 mkdir -p "$LOCAL_DIR"
 
 set -x
 aws --endpoint-url "$AWS_ENDPOINT_URL" s3 sync "$S3_PREFIX" "$LOCAL_DIR" --exclude "*" --include "$S3_SUFFIX"
-bunzip2 -k "$LOCAL_DIR"/*.bz2 || { true; } 2>/dev/null
+{ set +x; } 2>/dev/null
+
+for zipped in "$LOCAL_DIR"/*.bz2
+do
+  if ! [ -f "${zipped%".bz2"}" ]; then
+    set -x
+    bunzip2 -k "$zipped"
+    { set +x; } 2>/dev/null
+  fi
+done
