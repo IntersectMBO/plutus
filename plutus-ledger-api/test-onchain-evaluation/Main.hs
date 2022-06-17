74e0f7d555c96c2413ad32c3e8f0480c3de40c3f
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-16 19:03:01 -0700

    separate downloader and runner

--- plutus-ledger-api/test-onchain-evaluation/Main.hs
+++ plutus-ledger-api/test-onchain-evaluation/Main.hs
@@ -1,121 +1,115 @@
-{-# LANGUAGE QuasiQuotes      #-}
-{-# LANGUAGE RecordWildCards  #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main (main) where
+
+import Plutus.Script.Evaluation.Test.Options qualified as O
+import PlutusLedgerApi.Common
+import PlutusLedgerApi.Test.EvaluationEvent
+import PlutusLedgerApi.V1 qualified as V1
+import PlutusLedgerApi.V2 qualified as V2
 
 import Codec.Serialise qualified as CBOR
 import Control.Concurrent.ParallelIO.Local qualified as Concurrent
 import Control.Exception (evaluate)
-import Data.ByteString.Base16 qualified as B16
-import Data.ByteString.Lazy qualified as Lazy
-import Data.Foldable (for_)
-import Data.List (intercalate)
-import Data.Maybe (catMaybes)
+import Control.Monad.Extra (whenJust)
+import Data.List.Extra (nubOrd)
+import Data.List.NonEmpty (NonEmpty, nonEmpty, toList)
+import Data.Maybe
+import Data.String (fromString)
 import Data.Text (Text)
-import Data.Text.Encoding qualified as Text
+import Data.Traversable (for)
 import Options.Applicative qualified as OA
-import Plutus.Script.Evaluation.Test.Options qualified as O
-import PlutusCore.Evaluation.Machine.CostModelInterface (CostModelApplyError)
-import PlutusLedgerApi.Test.EvaluationEvent (ScriptEvaluationData (..), ScriptEvaluationEvent (..),
-                                             ScriptEvaluationResult (..))
-import PlutusLedgerApi.V1 qualified as V1
-import PlutusLedgerApi.V2 qualified as V2
+import PlutusCore.Pretty
+import PlutusPrelude (reoption)
+import Prettyprinter
 import PyF (fmt)
 import System.Directory.Extra (listFiles)
 import System.Exit (die)
-import System.FilePath (isExtensionOf, (<.>))
-import System.IO.Extra (withTempDir)
-import System.Process.Extra (system_)
+import System.FilePath (isExtensionOf)
+
+-- | All test failures in a single event dump file
+data TestFailures
+    = InvalidResults FilePath (NonEmpty UnexpectedEvaluationResult)
+    | MissingCostParameters FilePath (NonEmpty LedgerPlutusVersion)
+    deriving stock (Show)
+
+instance Pretty TestFailures where
+    pretty = \case
+        InvalidResults eventsFile errs ->
+            nest 2 $
+                vsep
+                    [ "Test Failures in" <+> pretty eventsFile
+                    , vsep (pretty <$> toList errs)
+                    ]
+        MissingCostParameters eventsFile ver ->
+            "Missing cost parameters for" <+> fromString (show ver) <+> "in" <+> pretty eventsFile
 
 data TestFailure
-    = InvalidEvaluationResult
-        Text
-        -- ^ Serialised `ScriptEvaluationEvent`
-        ScriptEvaluationResult
-    | InvalidCostParams
-        Text
-        -- ^ Serialised `ScriptEvaluationEvent`
-        CostModelApplyError
-    deriving stock (Show)
+    = InvalidResult UnexpectedEvaluationResult
+    | MissingCostParametersFor LedgerPlutusVersion
 
 main :: IO ()
 main = do
     opts <- OA.execParser O.parserInfo
-    let extZipped = O.optsEventFileExt opts <.> "bz2"
-    withTempDir $ \dir -> do
-        putStrLn "Downloading event dump files"
-        let s3DownloadCmd :: String
-            s3DownloadCmd = [fmt|{keyId} {secretKey} {region} aws {endpoint} s3 cp {s3Path} {dir} {options}|]
-              where
-                keyId = [fmt|AWS_ACCESS_KEY_ID={O.optsS3AccessKeyId opts}|]
-                secretKey = [fmt|AWS_SECRET_ACCESS_KEY={O.optsS3SecretAccessKey opts}|]
-                region = [fmt|AWS_DEFAULT_REGION={O.optsS3Region opts}|]
-                endpoint = [fmt|--endpoint-url {O.optsS3Endpoint opts}|]
-                s3Path = [fmt|s3://{O.optsS3Bucket opts}/{O.optsS3Prefix opts}|]
-                options = [fmt|--recursive --exclude "*" --include "*.{extZipped}"|]
-        putStrLn $ "Running: " <> s3DownloadCmd
-        -- It would be better to use amazonka, but the version bounds don't quite line up
-        -- (e.g., amazonka-core has aeson <1.6)
-        system_ s3DownloadCmd
-        eventFilesZipped <- filter (extZipped `isExtensionOf`) <$> listFiles dir
-        for_ eventFilesZipped $ \zipped -> do
-            putStrLn $ "Unzipping " <> zipped
-            system_ $ "bunzip2 " <> zipped
-        eventFiles <- filter (O.optsEventFileExt opts `isExtensionOf`) <$> listFiles dir
-        for_ (zip [1 :: Int ..] eventFiles) $ \(i, eventFile) -> do
-            putStrLn [fmt|Processing event file {i}: {eventFile}|]
-            events <- CBOR.readFileDeserialise @[ScriptEvaluationEvent] eventFile
-            mbErrs <- Concurrent.withPool (O.optsConcurrency opts) $ \pool -> do
-                Concurrent.parallel pool $ fmap (evaluate . checkEvaluationEvent) events
-            case catMaybes mbErrs of
-                [] -> putStrLn "All tests passed"
-                errs ->
-                    die
-                        [fmt|
-Script evaluation regression test failed.
-Number of failed test cases: {length errs}
-{intercalate "\n" (show <$> errs)}
-|]
+    eventFiles <- filter (O.optsEventFileExt opts `isExtensionOf`) <$> listFiles (O.optsDir opts)
+    errs <- fmap concat . for (zip [1 :: Int ..] eventFiles) $ \(i, eventFile) -> do
+        putStrLn [fmt|Processing event file {i}: {eventFile}|]
+        events <- CBOR.readFileDeserialise @ScriptEvaluationEvents eventFile
+        ctxV1 <-
+            either invalidCostParams pure $
+                mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
+        ctxV2 <-
+            either invalidCostParams pure $
+                mkContext V2.mkEvaluationContext (eventsCostParamsV2 events)
+        errs <- fmap catMaybes . Concurrent.withPool (O.optsConcurrency opts) $ \pool -> do
+            Concurrent.parallel pool $ fmap (evaluate . runTest ctxV1 ctxV2) (toList (eventsEvents events))
+        let invalidResults = concatMap (\case InvalidResult r -> [r]; _ -> []) errs
+            missingCostParams = nubOrd $ concatMap (\case MissingCostParametersFor ver -> [ver]; _ -> []) errs
+            failures :: [TestFailures]
+            failures =
+                (InvalidResults eventFile <$> reoption (nonEmpty invalidResults))
+                    <> (MissingCostParameters eventFile <$> reoption (nonEmpty missingCostParams))
 
-checkEvaluationEvent :: ScriptEvaluationEvent -> Maybe TestFailure
-checkEvaluationEvent ev = case ev of
-    PlutusV1Event ScriptEvaluationData{..} expected ->
-        case V1.mkEvaluationContext dataCostParams of
-            Left err -> Just $ InvalidCostParams ev' err
-            Right ctx ->
-                let (_, actual) =
-                        V1.evaluateScriptRestricting
-                            dataProtocolVersion
-                            V1.Quiet
-                            ctx
-                            dataBudget
-                            dataScript
-                            dataInputs
-                 in case (expected, actual) of
-                        (ScriptEvaluationSuccess, Left _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationFailure
-                        (ScriptEvaluationFailure, Right _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationSuccess
-                        _ -> Nothing
-    PlutusV2Event ScriptEvaluationData{..} expected ->
-        case V2.mkEvaluationContext dataCostParams of
-            Left err -> Just $ InvalidCostParams ev' err
-            Right ctx ->
-                let (_, actual) =
-                        V2.evaluateScriptRestricting
-                            dataProtocolVersion
-                            V2.Quiet
-                            ctx
-                            dataBudget
-                            dataScript
-                            dataInputs
-                 in case (expected, actual) of
-                        (ScriptEvaluationSuccess, Left _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationFailure
-                        (ScriptEvaluationFailure, Right _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationSuccess
-                        _ -> Nothing
+        whenJust (nonEmpty invalidResults) $ \xs -> do
+            putStrLn
+                [fmt|
+Some tests failed. Number of failures: {length xs}
+{render . indent 2 . vsep $ pretty <$> toList xs :: Text}
+|]
+        whenJust (nonEmpty missingCostParams) $ \vers -> do
+            putStrLn
+                [fmt|
+Missing cost parameters for: {show (toList vers)}
+|]
+        pure failures
+    case errs of
+        [] -> pure ()
+        _ ->
+            die
+                [fmt|
+Test failures:
+{render . indent 2 . vsep $ pretty <$> errs :: Text}
+|]
   where
-    ev' :: Text
-    ev' = Text.decodeLatin1 . B16.encode . Lazy.toStrict $ CBOR.serialise ev
+    runTest ::
+        Maybe (EvaluationContext, [Integer]) ->
+        Maybe (EvaluationContext, [Integer]) ->
+        ScriptEvaluationEvent ->
+        Maybe TestFailure
+    runTest ctxV1 ctxV2 event = case event of
+        PlutusV1Event{} -> case ctxV1 of
+            Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
+            Nothing            -> Just $ MissingCostParametersFor PlutusV1
+        PlutusV2Event{} -> case ctxV2 of
+            Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
+            Nothing            -> Just $ MissingCostParametersFor PlutusV2
+    mkContext f = \case
+        Nothing         -> Right Nothing
+        Just costParams -> Just . (,costParams) <$> f costParams
+
+    invalidCostParams err = die $ "Invalid cost parameters: " <> show err
--- plutus-ledger-api/test-onchain-evaluation/Plutus/Script/Evaluation/Test/Options.hs
+++ plutus-ledger-api/test-onchain-evaluation/Plutus/Script/Evaluation/Test/Options.hs
@@ -12,66 +12,27 @@ import GHC.Conc (numCapabilities)
 import Options.Applicative qualified as O
 
 data Options = Options
-    { optsS3Endpoint        :: String
-    , optsS3AccessKeyId     :: String
-    , optsS3SecretAccessKey :: String
-    , optsS3Region          :: String
-    , optsS3Bucket          :: String
-    , optsS3Prefix          :: String
-    , optsEventFileExt      :: String
-    , optsConcurrency       :: Int
+    { optsDir          :: String
+    , optsEventFileExt :: String
+    , optsConcurrency  :: Int
     }
 
 parseOptions :: O.Parser Options
 parseOptions = do
-    optsS3Endpoint <-
+    optsDir <-
         O.option O.str $
             mconcat
-                [ O.long "s3-endpoint"
-                , O.value "https://s3.devx.iog.io"
-                , O.metavar "S3_ENDPOINT"
-                ]
-    optsS3AccessKeyId <-
-        O.option O.str $
-            mconcat
-                [ O.long "s3-access-key-id"
-                , O.value "plutus"
-                , O.metavar "S3_ACCESS_KEY_ID"
-                ]
-    optsS3SecretAccessKey <-
-        O.option O.str $
-            mconcat
-                [ O.long "s3-secret-access-key"
-                , O.value "plutuskey"
-                , O.metavar "S3_SECRET_ACCESS_KEY"
-                ]
-    optsS3Region <-
-        O.option O.str $
-            mconcat
-                [ O.long "s3-region"
-                , O.value "us-east-1"
-                , O.metavar "S3_REGION"
-                ]
-    optsS3Bucket <-
-        O.option O.str $
-            mconcat
-                [ O.long "s3-bucket"
-                , O.value "plutus"
-                , O.metavar "S3_BUCKET"
-                ]
-    optsS3Prefix <-
-        O.option O.str $
-            mconcat
-                [ O.long "s3-prefix"
-                , O.value "script-evaluation-dump/"
-                , O.metavar "S3_PREFIX"
+                [ O.short 'd'
+                    , O.long "dir"
+                , O.metavar "DIR"
+                , O.help "dir containing event dump files"
                 ]
     optsEventFileExt <-
         O.option O.str $
             mconcat
-                [ O.long "s3-event-file-extension"
+                [ O.long "event-file-extension"
                 , O.value "event"
-                , O.metavar "S3_EVENT_FILE_EXTENSION"
+                , O.metavar "EVENT_FILE_EXTENSION"
                 ]
     optsConcurrency <-
         O.option O.auto $
