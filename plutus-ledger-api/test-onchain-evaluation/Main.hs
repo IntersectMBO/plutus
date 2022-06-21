806cd5d7e3ee8207022906e00597b4a3ad8c04ec
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-21 06:15:19 -0700

    Address review comments

--- plutus-ledger-api/test-onchain-evaluation/Main.hs
+++ plutus-ledger-api/test-onchain-evaluation/Main.hs
@@ -19,46 +19,53 @@ import System.Environment (getEnv)
 import System.FilePath (isExtensionOf)
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Providers
+
+data SingleTest
+    = SingleTest
+        LedgerPlutusVersion
+        (Maybe (EvaluationContext, [Integer]))
+        ScriptEvaluationEvent
+
+instance IsTest SingleTest where
+    run _opts (SingleTest ver mbCtx ev) _reportProgress = case mbCtx of
+        Just (ctx, params) ->
+            evaluate
+                =<< ( pure . maybe (testPassed mempty) (testFailed . display) $
+                        checkEvaluationEvent ctx params ev
+                    )
+        Nothing -> pure . testFailed $ "missing cost parameters for " <> show ver
+
+    testOptions = mempty
 
 -- | Test cases from a single event dump file
-testOneFile :: FilePath -> IO TestTree
-testOneFile eventFile = do
-    events <- CBOR.readFileDeserialise @ScriptEvaluationEvents eventFile
-    ctxV1 <-
-        either (assertFailure . display) pure $
-            mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
-    ctxV2 <-
-        either (assertFailure . display) pure $
-            mkContext V2.mkEvaluationContext (eventsCostParamsV2 events)
-    pure . testGroup eventFile $
-        fmap
-            ( \(idx, event) ->
-                testCase ("test case " <> show idx) $
-                    evaluate =<< runTest ctxV1 ctxV2 event
-            )
-            (zip [1 :: Int ..] (toList (eventsEvents events)))
+testOneFile :: TestName -> ScriptEvaluationEvents -> TestTree
+testOneFile name events =
+    case ( mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
+         , mkContext V2.mkEvaluationContext (eventsCostParamsV2 events)
+         ) of
+        (Right ctxV1, Right ctxV2) ->
+            testGroup name $
+                fmap
+                    ( \(idx, event) ->
+                        singleTest ("test case " <> show idx) $ case event of
+                            PlutusV1Event{} -> SingleTest PlutusV1 ctxV1 event
+                            PlutusV2Event{} -> SingleTest PlutusV2 ctxV2 event
+                    )
+                    (zip [1 :: Int ..] (toList (eventsEvents events)))
+        (Left err, _) ->
+            testCase name . assertFailure $ display err
+        (_, Left err) ->
+            testCase name . assertFailure $ display err
   where
     mkContext f = \case
         Nothing         -> Right Nothing
         Just costParams -> Just . (,costParams) <$> f costParams
 
-    runTest ::
-        Maybe (EvaluationContext, [Integer]) ->
-        Maybe (EvaluationContext, [Integer]) ->
-        ScriptEvaluationEvent ->
-        Assertion
-    runTest ctxV1 ctxV2 event = case event of
-        PlutusV1Event{} -> case ctxV1 of
-            Just (ctx, params) ->
-                maybe (pure ()) (assertFailure . display) $ checkEvaluationEvent ctx params event
-            Nothing -> assertFailure "missing cost parameters for PlutusV1"
-        PlutusV2Event{} -> case ctxV2 of
-            Just (ctx, params) ->
-                maybe (pure ()) (assertFailure . display) $ checkEvaluationEvent ctx params event
-            Nothing -> assertFailure "missing cost parameters for PlutusV2"
-
 main :: IO ()
 main = do
     dir <- getEnv "EVENT_DUMP_DIR"
     eventFiles <- filter ("event" `isExtensionOf`) <$> listFiles dir
-    defaultMain . testGroup "Mainnet script evaluation test" =<< traverse testOneFile eventFiles
+    eventss <- traverse (CBOR.readFileDeserialise @ScriptEvaluationEvents) eventFiles
+    defaultMain . testGroup "Mainnet script evaluation test" . fmap (uncurry testOneFile) $
+        zip eventFiles eventss
