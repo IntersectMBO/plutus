64ab860d953cd0eb8aa35801e36cce280df920f4
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-06-23 11:50:40 +0100

    Merge pull request #4727 from input-output-hk/mpj/conformance-test-refactor
    Refactor conformance test infrastructure a little

--- plutus-conformance/src/PlutusConformance/Common.hs
+++ plutus-conformance/src/PlutusConformance/Common.hs
@@ -1,61 +1,64 @@
 {-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE NamedFieldPuns       #-}
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 {- | Plutus conformance test suite library. -}
 module PlutusConformance.Common where
 
-import Control.DeepSeq (force)
+import Control.Lens (traverseOf)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
-import PlutusCore.Core (defaultVersion)
 import PlutusCore.Default (DefaultFun, DefaultUni)
-import PlutusCore.Error (ParserErrorBundle (ParseErrorB))
+import PlutusCore.Error (ParserErrorBundle)
 import PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCekParameters)
 import PlutusCore.Name (Name)
 import PlutusCore.Quote (runQuoteT)
 import PlutusPrelude
+import System.Directory
+import System.FilePath (takeBaseName, (</>))
 import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.Golden (findByExtension)
-import Test.Tasty.HUnit (testCase, (@=?))
 import Test.Tasty.Providers
 import Text.Megaparsec (SourcePos)
-import UnliftIO.Exception
-import UntypedPlutusCore.Core.Type qualified as UPLC
+import UntypedPlutusCore qualified as UPLC
 import UntypedPlutusCore.Evaluation.Machine.Cek (evaluateCekNoEmit)
 import UntypedPlutusCore.Parser qualified as UPLC
+import Witherable
 
--- Common functions for all tests
+type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
 
--- | A TestContent contains the input file path, input and output file contents.
-data TestContent =
-   MkTestContent {
-       -- | The path of the input file. This is also used to name the test.
-       testName    :: FilePath
-       -- | The expected output of the test in `Text`.
-       , expected  :: T.Text
-       -- | The input to be tested, in `Text`.
-       , inputProg :: T.Text
-   }
+type UplcEvaluator = UplcProg -> Maybe UplcProg
 
-{- | Create a list of `TestContent` with the given lists.
-The lengths of the input lists have to be the same, otherwise, an error occurs. -}
-mkTestContents ::
-    [FilePath] -- ^ The list of paths of the input files.
-    -> [T.Text] -- ^ The list of expected outputs.
-    -> [T.Text] -- ^ The list of the inputs.
-    -> [TestContent]
-mkTestContents lFilepaths lRes lProgs =
-    case zipWith3Exact (\f r p -> MkTestContent f r p) lFilepaths lRes lProgs of
-        Just tests -> tests
-        Nothing -> error $ unlines
-            ["mkTestContents: Cannot run the tests because the number of input and output programs are not the same. "
-            , "Number of input files: "
-            , show (length lProgs)
-            , " Number of output files: "
-            , show (length lRes)
-            , " Make sure all your input programs have an accompanying .expected file."
-            ]
+-- | A UPLC evaluation test suite.
+data UplcEvaluationTest =
+    MkUplcEvaluationTest {
+       -- | The evaluator function to use for the test.
+       evaluator :: UplcEvaluator
+       -- | The test directory in which the test files are located.
+       , testDir :: FilePath
+    }
+
+-- Tells 'tasty' that 'UplcEvaluationTest' "is" a test that can be run,
+-- by specifying how to run it and what custom options it might expect.
+instance IsTest UplcEvaluationTest where
+    run _ MkUplcEvaluationTest{testDir,evaluator} _ = do
+        let name = takeBaseName testDir
+        input <- T.readFile $ testDir </> name <> ".uplc"
+        let parsed = parseTxt input
+
+        expected <- T.readFile $ testDir </> name <> ".uplc.expected"
+        let checkContents c | c == expected = pure (testPassed "")
+            checkContents c = pure (testFailed (show c))
+
+        case parsed of
+            Left _ -> checkContents shownParseError
+            Right p -> do
+               case evaluator (void p) of
+                   Nothing -> checkContents shownEvaluationFailure
+                   Just p' -> checkContents (display p')
+
+    testOptions = pure []
+
 
 {- | The default shown text when a parse error occurs.
 We don't want to show the detailed parse errors so that
@@ -69,62 +72,47 @@ shownEvaluationFailure = "evaluation failure"
 
 -- For UPLC evaluation tests
 
-type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
-
-termToProg :: UPLC.Term Name DefaultUni DefaultFun () -> UplcProg
-termToProg = UPLC.Program () (defaultVersion ())
-
--- | Our `runner` for the UPLC tests is the CEK machine.
-evalUplcProg :: UplcProg -> Maybe UplcProg
-evalUplcProg p =
-    let eitherExceptionProg =
-            fmap
-                termToProg
-                (evaluateCekNoEmit defaultCekParameters (UPLC._progTerm p))
-    in
-        case eitherExceptionProg of
-            Left _     -> Nothing
-            Right prog -> Just prog
-
-{- | Run an input with the `runner` and return the result, in `Text`.
-When fail, the result is either the default text for parse error or evaluation failure. -}
-mkResult ::
-    (UplcProg -> Maybe UplcProg) -- ^ The `runner` to run the test inputs with.
-    -> Either ParserErrorBundle (UPLC.Program Name DefaultUni DefaultFun SourcePos)
-    -- ^ The result of parsing.
-    -> IO T.Text -- ^ The result of the `runner` in `Text`.
-mkResult _ (Left (ParseErrorB _err)) = pure shownParseError
-mkResult runner (Right prog)        = do
-    -- catch exceptions from `runner` and keep going unless it's an async exception.
-    maybeException <- try (evaluate $ force $ runner (() <$ prog)):: IO (Either SomeException (Maybe UplcProg))
-    case maybeException of
-        Left _         -> pure shownEvaluationFailure
-        -- it doesn't matter how the evaluation fail, they're all "evaluation failure"
-        Right Nothing  -> pure shownEvaluationFailure
-        Right (Just a) -> pure $ display a
-
 -- | The default parser to parse the inputs.
 parseTxt ::
     T.Text
     -> Either ParserErrorBundle (UPLC.Program Name DefaultUni DefaultFun SourcePos)
 parseTxt resTxt = runQuoteT $ UPLC.parseProgram resTxt
 
--- | Build a test given the runner and a `TestContent`.
--- TODO maybe abstract this for other tests too if it takes in `mkResult`.
-mkUplcTestCase :: (UplcProg -> Maybe UplcProg) -> TestContent -> TestTree
-mkUplcTestCase runner test =
-    testCase (testName test) (
-        do r <- mkResult runner (parseTxt (inputProg test))
-           expected test @=? r)
+-- | Our `evaluator` for the UPLC tests is the CEK machine.
+evalUplcProg :: UplcEvaluator
+evalUplcProg = traverseOf UPLC.progTerm eval
+  where
+    eval t = do
+        -- The evaluator throws if the term has free variables
+        case UPLC.deBruijnTerm t of
+            Left (_ :: UPLC.FreeVariableError) -> Nothing
+            Right _                            -> Just ()
+        case evaluateCekNoEmit defaultCekParameters t of
+            Left _     -> Nothing
+            Right prog -> Just prog
 
--- | Run the tests given a `runner` that evaluates UPLC programs.
+-- | Run the tests given a `evaluator` that evaluates UPLC programs.
 runUplcEvalTests ::
-    (UplcProg -> Maybe UplcProg)-- ^ The action to run the input through for the tests.
+    UplcEvaluator -- ^ The action to run the input through for the tests.
     -> IO ()
-runUplcEvalTests runner = do
-    inputFiles <- findByExtension [".uplc"] "uplc/evaluation/"
-    outputFiles <- findByExtension [".expected"] "uplc/evaluation/"
-    lProgTxt <- for inputFiles T.readFile
-    lEvaluatedRes <- for outputFiles T.readFile
-    let testContents = mkTestContents inputFiles lEvaluatedRes lProgTxt
-    defaultMain $ testGroup "UPLC evaluation tests" $ fmap (mkUplcTestCase runner) testContents
+runUplcEvalTests evaluator = do
+    tests <- discoverTests (\dir -> MkUplcEvaluationTest evaluator dir) "uplc/evaluation"
+    defaultMain $ testGroup "UPLC evaluation tests" [tests]
+
+-- Common functions for all tests
+
+-- | Walk a file tree, making test groups for directories with subdirectories,
+-- and test cases for directories without.
+discoverTests :: IsTest t => (FilePath -> t) -> FilePath -> IO TestTree
+discoverTests tester dir = do
+    let name = takeBaseName dir
+    children <- listDirectory dir
+    subdirs <- flip wither children $ \child -> do
+        let fullPath = dir </> child
+        isDir <- doesDirectoryExist fullPath
+        pure $ if isDir then Just fullPath else Nothing
+    if null subdirs
+    -- no children, this is a test case directory
+    then pure $ singleTest name $ tester dir
+    -- has children, so it's a grouping directory
+    else testGroup name <$> traverse (discoverTests tester) subdirs
