8580de7a5ac6f1c07941a7e81b75da9f2364b0c2
Marty Stumpf <thealmartyblog@gmail.com>
2022-06-23 10:22:42 -0700

    Tidy up Common file.

--- plutus-conformance/src/PlutusConformance/Common.hs
+++ plutus-conformance/src/PlutusConformance/Common.hs
@@ -27,19 +27,25 @@ import UntypedPlutusCore.Evaluation.Machine.Cek (evaluateCekNoEmit)
 import UntypedPlutusCore.Parser qualified as UPLC
 import Witherable
 
-type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
+-- Common functions for all tests
 
-type UplcEvaluator = UplcProg -> Maybe UplcProg
+-- | Walk a file tree, making test groups for directories with subdirectories,
+-- and test cases for directories without.
+discoverTests :: IsTest t => (FilePath -> t) -> FilePath -> IO TestTree
+discoverTests tester dir = do
+    let name = takeBaseName dir
+    children <- listDirectory dir
+    subdirs <- flip wither children $ \child -> do
+        let fullPath = dir </> child
+        isDir <- doesDirectoryExist fullPath
+        pure $ if isDir then Just fullPath else Nothing
+    if null subdirs
+    -- no children, this is a test case directory
+    then pure $ singleTest name $ tester dir
+    -- has children, so it's a grouping directory
+    else testGroup name <$> traverse (discoverTests tester) subdirs
 
--- | A UPLC evaluation test suite.
-data UplcEvaluationTest =
-    MkUplcEvaluationTest {
-       -- | The evaluator function to use for the test.
-       evaluator :: UplcEvaluator
-       -- | The test directory in which the test files are located.
-       , testDir :: FilePath
-    }
-
+-- | A test `option` to accept the test results as the expected results.
 -- This is basically the same option as 'tasty-golden' uses, but it's not
 -- worth a dependency just to reuse the tiny datatype.
 -- Set like other tasty options with --test-options, e.g.
@@ -76,6 +82,31 @@ checkExpected (AcceptTests accept) expectedFile actual = do
             pure $ testPassed "Expected file did not exist, created it"
         else pure $ testFailed $ "Expected file did not exist:" ++ show expectedFile
 
+{- | The default shown text when a parse error occurs.
+We don't want to show the detailed parse errors so that
+users of the test suite can produce this expected outputs more easily. -}
+shownParseError :: T.Text
+shownParseError = "parse error"
+
+-- | The default shown text when evaluation fails.
+shownEvaluationFailure :: T.Text
+shownEvaluationFailure = "evaluation failure"
+
+-- UPLC evaluation test functions
+
+type UplcProg = UPLC.Program Name DefaultUni DefaultFun ()
+
+type UplcEvaluator = UplcProg -> Maybe UplcProg
+
+-- | A UPLC evaluation test suite.
+data UplcEvaluationTest =
+    MkUplcEvaluationTest {
+       -- | The evaluator function to use for the test.
+       evaluator :: UplcEvaluator
+       -- | The test directory in which the test files are located.
+       , testDir :: FilePath
+    }
+
 -- Tells 'tasty' that 'UplcEvaluationTest' "is" a test that can be run,
 -- by specifying how to run it and what custom options it might expect.
 instance IsTest UplcEvaluationTest where
@@ -95,19 +126,6 @@ instance IsTest UplcEvaluationTest where
                    Just p' -> check (display p')
 
     testOptions = pure [Option (Proxy :: Proxy AcceptTests)]
-
-
-{- | The default shown text when a parse error occurs.
-We don't want to show the detailed parse errors so that
-users of the test suite can produce this expected outputs more easily. -}
-shownParseError :: T.Text
-shownParseError = "parse error"
-
--- | The default shown text when evaluation fails.
-shownEvaluationFailure :: T.Text
-shownEvaluationFailure = "evaluation failure"
-
--- For UPLC evaluation tests
 
 -- | The default parser to parse the inputs.
 parseTxt ::
@@ -135,21 +153,3 @@ runUplcEvalTests ::
 runUplcEvalTests evaluator = do
     tests <- discoverTests (\dir -> MkUplcEvaluationTest evaluator dir) "uplc/evaluation"
     defaultMain $ testGroup "UPLC evaluation tests" [tests]
-
--- Common functions for all tests
-
--- | Walk a file tree, making test groups for directories with subdirectories,
--- and test cases for directories without.
-discoverTests :: IsTest t => (FilePath -> t) -> FilePath -> IO TestTree
-discoverTests tester dir = do
-    let name = takeBaseName dir
-    children <- listDirectory dir
-    subdirs <- flip wither children $ \child -> do
-        let fullPath = dir </> child
-        isDir <- doesDirectoryExist fullPath
-        pure $ if isDir then Just fullPath else Nothing
-    if null subdirs
-    -- no children, this is a test case directory
-    then pure $ singleTest name $ tester dir
-    -- has children, so it's a grouping directory
-    else testGroup name <$> traverse (discoverTests tester) subdirs
