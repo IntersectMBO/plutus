5dcc86e757cea5da177172f8b433ce552dba07e1
kwxm <kenneth.mackenzie@iohk.io>
2022-06-27 19:57:30 +0100

    Remove Term1 from plutus-ledger

--- plutus-ledger-api/src/PlutusLedgerApi/V1/Scripts.hs
+++ plutus-ledger-api/src/PlutusLedgerApi/V1/Scripts.hs
@@ -197,7 +197,6 @@ mkError evalError (Just t) =
        UPLC.Var _ _       -> Nothing
        UPLC.Constant _ _  -> Nothing
        UPLC.Error _       -> Nothing
-       UPLC.Term1 _ t     -> findBuiltin t
 
 mkValidatorScript :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ()) -> Validator
 mkValidatorScript = Validator . fromCompiledCode
