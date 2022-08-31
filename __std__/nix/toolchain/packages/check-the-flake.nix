d7672686ebe86892679a7c0b88c49bf05c13bfa8
zeme-iohk <lorenzo.calegari@iohk.io>
2022-08-31 12:11:21 +0200

    WIP

--- __std__/nix/toolchain/packages/check-the-flake.nix
+++ __std__/nix/toolchain/packages/check-the-flake.nix
@@ -1,38 +1,72 @@
 { inputs, cell }:
 
-# TODO(std) needs to be fixed once __std__ is brought to the toplevel.
 inputs.nixpkgs.writeShellApplication {
-  name = "check-the-flake";
-  runtimeInputs = [ inputs.nixpkgs.nix ];
+  name = "irrelevant";
   text = ''
-    root="$(repo-root)"
-
-    shell_files=$(
-      find \
-        "$root/__std__/nix" \
-        -name "*.nix" \
-        -and -not -name "*default.nix" \
-        -and -path "*devshells*" \
-        -exec basename {} .nix \;
+    my_list=(
+      item_1
+      item_2
     )
-
-    for file in $shell_files; do 
-      nix develop ".#$file" --build
-    done 
-
-    derivation_files=$(
-      find \
-        "$(repo-root)/__std__/nix" \
-        -name "*.nix" \
-        -and -not -name "*default.nix" \
-        -and -not -path "*library*" \
-        -and -not -path "*devshells*" \
-        -exec basename {} .nix \;
-    ) 
-
-    for file in $derivation_files; do 
-      nix build ".#$file"
-    done 
+    if [[ "${my_list[*]}" =~ "item_3" ]]; then 
+      echo inside_list
+    else 
+      echo outside_list
+    fi 
   '';
 }
 
+
+
+
+# { inputs, cell }:
+
+# # TODO(std) needs to be fixed once __std__ is brought to the toplevel.
+# # TODO(std) remove derivations from the broken list as they get fixed
+# inputs.nixpkgs.writeShellApplication {
+#   name = "check-the-flake";
+#   runtimeInputs = [ inputs.nixpkgs.nix ];
+#   text = ''
+#     root="$(repo-root)"
+
+#     shell_fragments=$(
+#       find \
+#         "$root/__std__/nix" \
+#         -name "*.nix" \
+#         -and -not -name "*default.nix" \
+#         -and -path "*devshells*" \
+#         -exec basename {} .nix \;
+#     )
+
+#     for fragment in $shell_fragments; do 
+#       echo building "$fragment" 
+#       nix develop ".#$fragment" --build
+#     done 
+
+#     derivation_fragments=$(
+#       find \
+#         "$(repo-root)/__std__/nix" \
+#         -name "*.nix" \
+#         -and -not -name "*default.nix" \
+#         -and -not -path "*library*" \
+#         -and -not -path "*devshells*" \
+#         -exec basename {} .nix \;
+#     ) 
+
+#     # shellcheck disable=SC2034
+#     broken_fragments=(
+#       system-f-in-agda-paper
+#       unraveling-recursion-paper
+#     )
+
+#     for fragment in $derivation_fragments; do 
+#       # shellcheck disable=SC2076
+#       # shellcheck disable=SC2034
+#       if [[ "${{broken_fragments[*]}" =~ "$fragment" ]]; then 
+#         echo building "$fragment" 
+#         nix build ".#$fragment"
+#       else 
+#         echo skipping broken "$fragment"
+#       fi 
+#     done 
+#   '';
+# }
