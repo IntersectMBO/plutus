ebdd25403b86ea44c0825d1f569099e92b3656b1
zeme-iohk <lorenzo.calegari@iohk.io>
2022-08-31 11:13:12 +0200

    WIP

--- __std__/nix/toolchain/packages/sphinxcontrib-bibtex.nix
+++ __std__/nix/toolchain/packages/sphinxcontrib-bibtex.nix
@@ -2,7 +2,7 @@
 
 inputs.nixpkgs.python3.sphinxcontrib-bibtex.overrideAttrs (oldAttrs: rec {
   version = "2.2.0";
-  src = python-super.fetchPypi {
+  src = inputs.nixpkgs.python3.fetchPypi {
     inherit (oldAttrs) pname;
     inherit version;
     sha256 = "1cp3dj5bbl122d64i3vbqhjhfplnh1rwm9dw4cy9hxjd2lz8803m";
