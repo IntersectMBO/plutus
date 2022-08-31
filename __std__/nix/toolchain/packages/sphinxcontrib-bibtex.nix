9d68edf2a25590dc2351e881d4f6ff0f55d58d5c
zeme-iohk <lorenzo.calegari@iohk.io>
2022-08-31 11:18:17 +0200

    using better sphinxcontrib-bibtex

--- __std__/nix/toolchain/packages/sphinxcontrib-bibtex.nix
+++ __std__/nix/toolchain/packages/sphinxcontrib-bibtex.nix
@@ -1,8 +1,8 @@
 { inputs, cell }:
 
-inputs.nixpkgs.python3.sphinxcontrib-bibtex.overrideAttrs (oldAttrs: rec {
+inputs.nixpkgs.python3Packages.sphinxcontrib-bibtex.overrideAttrs (oldAttrs: rec {
   version = "2.2.0";
-  src = inputs.nixpkgs.python3.fetchPypi {
+  src = inputs.nixpkgs.python3Packages.fetchPypi {
     inherit (oldAttrs) pname;
     inherit version;
     sha256 = "1cp3dj5bbl122d64i3vbqhjhfplnh1rwm9dw4cy9hxjd2lz8803m";
