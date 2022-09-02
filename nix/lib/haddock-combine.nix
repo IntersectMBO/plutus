eaa4baa4dfa3b65922d954fa5e94b8d0e5811758
zeme-iohk <lorenzo.calegari@iohk.io>
2022-09-02 10:42:28 +0200

    try 2

--- nix/lib/haddock-combine.nix
+++ nix/lib/haddock-combine.nix
@@ -18,6 +18,7 @@ runCommand "haddock-join"
   exportReferencesGraph = lib.concatLists
     (lib.imap0 (i: pkg: [ "graph-${toString i}" pkg ]) hsdocs);
 } ''
+  echo **************************************** 1
   hsdocsRec="$(cat graph* | grep -F /nix/store | sort | uniq)"
   # Merge all the docs from the packages and their doc dependencies.
   # We don't use symlinkJoin because:
@@ -37,6 +38,7 @@ runCommand "haddock-join"
   # We're now going to rewrite all the pre-generated Haddock HTML output
   # so that links point to the appropriate place within our combined output,
   # rather than into the store.
+  echo **************************************** 2
   root=$out/share/doc
   for f in $(find $out -name "*.html"); do
     # Replace all links to the docs we're processing with relative links
@@ -51,6 +53,7 @@ runCommand "haddock-join"
 
   # Move to the docdir. We do this so that we can give relative docpaths to
   # Haddock so it will generate relative (relocatable) links in the index.
+  echo **************************************** 3
   cd $out/share/doc
   # Non-recursively collect all the interface files and their docpaths
   # (in this case we can just use the enclosing directory).
@@ -70,6 +73,7 @@ runCommand "haddock-join"
     popd
   done
 
+  echo **************************************** 4
   # Generate the contents and index
   ${ghc}/bin/haddock \
     --gen-contents \
@@ -78,12 +82,14 @@ runCommand "haddock-join"
     ${lib.optionalString (prologue != null) "--prologue ${prologue}"} \
     "''${interfaceOpts[@]}"
 
+  echo **************************************** 5
   # TODO: remove patch when haddock > 2.24.0
   # patch quick-jump.css to fix scrolling in search for chromium
   for f in $(find $out -name "quick-jump.css"); do
     sed -i -r "s,^\#search-results \{,\#search-results \{ max-height:80%;overflow-y:scroll;," "$f"
   done
 
+  echo **************************************** 6
   # Following: https://github.com/input-output-hk/ouroboros-network/blob/2068d091bc7dcd3f4538fb76f1b598f219d1e0c8/scripts/haddocs.sh#L87 # editorconfig-checker-disable-line
   # Assemble a toplevel `doc-index.json` from package level ones.
   shopt -s globstar
