5097092f599c2679260ff46b490558b5db1ffd39
zeme-iohk <lorenzo.calegari@iohk.io>
2022-09-02 14:27:06 +0200

    WIP

--- __std__/nix/toolchain/scripts/editorconfig-checker.nix
+++ __std__/nix/toolchain/scripts/editorconfig-checker.nix
@@ -1,29 +1,27 @@
 { inputs, cell }:
 
-cell.packages.todo-derivation
+let src = cell.library.gitignore-nix.gitignoreSource inputs.self; in
 
-# let src = inputs.cells.toolchain.packages.gitignore-nix.gitignoreSource inputs.self; in
+inputs.nixpkgs.writeShellApplication {
 
-# inputs.nixpkgs.writeShellApplication {
+  name = "editorconfig-checker";
 
-#   name = "editorconfig-checker";
+  runtimeInputs = [
+    inputs.nixpkgs.editorconfig-checker
+  ];
 
-#   runtimeInputs = [
-#     inputs.nixpkgs.editorconfig-checker
-#   ];
+  text = ''
+    mkdir -p "$out/nix-support"
 
-#   text = ''
-#     mkdir -p "$out/nix-support"
+    set -e
+    # changing to the directory and then running it gives better output than
+    # passing the directory to check, since file names are shorter
+    cd ${src}
+    editorconfig-checker 2>&1 | tee "$out/nix-support/editorconfig-checker.log"
 
-#     set -e
-#     # changing to the directory and then running it gives better output than
-#     # passing the directory to check, since file names are shorter
-#     cd $src
-#     editorconfig-checker 2>&1 | tee "$out/nix-support/editorconfig-checker.log"
-
-#     if [ $? -ne 0 ]; then
-#       echo "*** editorconfig-checker found files that don't match the configuration"
-#       exit 1
-#     fi
-#   '';
-# }
+    if [ $? -ne 0 ]; then
+      echo "*** editorconfig-checker found files that don't match the configuration"
+      exit 1
+    fi
+  '';
+}
