884f73612039c389324a8a1abf4bf01216837c81
zeme-iohk <lorenzo.calegari@iohk.io>
2022-09-11 21:43:03 +0200

    WIP

--- __std__/nix/doc/packages/combined-haddock.nix
+++ __std__/nix/doc/packages/combined-haddock.nix
@@ -1,32 +1,35 @@
 { inputs, cell }:
 
-inputs.cells.toolchain.packages.todo-derivation
-
 # TODO(std) need haskell-nix for this
 
 # # editorconfig-checker-disable-file
-# { haddock-combine, haskell, haskell-nix, writeTextFile }:
-# let
-#   toHaddock = haskell-nix.haskellLib.collectComponents' "library" haskell.projectPackagesAllHaddock;
-# in
-# haddock-combine {
-#   hspkgs = builtins.attrValues toHaddock;
-#   prologue = writeTextFile {
-#     name = "prologue";
-#     text = ''
-#       = Combined documentation for all the public Plutus libraries
+{ haddock-combine, haskell, haskell-nix, writeTextFile }:
+let
+  toHaddock = 
+    inputs.cells.toolchain.library.haskell-nix.haskellLib.collectComponents' 
+      "library" 
+      inputs.cells.plutus.packages.all-packages-with-haddock;
+in
+inputs.cells.toolchain.library.haddock-combine {
 
-#       == Handy module entrypoints
+  hspkgs = builtins.attrValues toHaddock;
+  
+  prologue = inputs.nixpkgs.writeTextFile {
+    name = "prologue";
+    text = ''
+      = Combined documentation for all the public Plutus libraries
 
-#         * "PlutusTx": Compiling Haskell to PLC (Plutus Core; on-chain code).
-#         * "PlutusTx.Prelude": Haskell prelude replacement compatible with PLC.
-#         * "Plutus.Contract": Writing Plutus apps (off-chain code).
-#         * "Ledger.Constraints": Constructing and validating Plutus
-#            transactions. Built on "PlutusTx" and
-#            "Plutus.Contract".
-#         * "Ledger.Typed.Scripts": A type-safe interface for spending and
-#            producing script outputs. Built on "PlutusTx".
-#         * "Plutus.Trace.Emulator": Testing Plutus contracts in the emulator.
-#     '';
-#   };
-# }
+      == Handy module entrypoints
+
+        * "PlutusTx": Compiling Haskell to PLC (Plutus Core; on-chain code).
+        * "PlutusTx.Prelude": Haskell prelude replacement compatible with PLC.
+        * "Plutus.Contract": Writing Plutus apps (off-chain code).
+        * "Ledger.Constraints": Constructing and validating Plutus
+           transactions. Built on "PlutusTx" and
+           "Plutus.Contract".
+        * "Ledger.Typed.Scripts": A type-safe interface for spending and
+           producing script outputs. Built on "PlutusTx".
+        * "Plutus.Trace.Emulator": Testing Plutus contracts in the emulator.
+    '';
+  };
+}
