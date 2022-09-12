fc3a8b9705148f7d6699d88723af3de83fc40c23
zeme-iohk <lorenzo.calegari@iohk.io>
2022-09-12 16:39:23 +0200

    WIP

--- __std__/nix/toolchain/packages/nix-flakes-alias.nix
+++ __std__/nix/toolchain/packages/nix-flakes-alias.nix
@@ -1,6 +1,6 @@
 { inputs, cell }:
 
-# TODO(std) what is this for?
+# TODO(std) remove this when everything else is done 
 inputs.nixpkgs.runCommand "nix-flakes-alias" { } ''
   mkdir -p $out/bin
   ln -sv ${inputs.nixpkgs.nixFlakes}/bin/nix $out/bin/nix-flakes
