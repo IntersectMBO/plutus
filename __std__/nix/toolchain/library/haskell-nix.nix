f73c5b875e0db6d83f54bfe72065a77b457763b0
zeme-iohk <lorenzo.calegari@iohk.io>
2022-09-12 15:16:39 +0200

    WIP

--- __std__/nix/toolchain/library/haskell-nix.nix
+++ __std__/nix/toolchain/library/haskell-nix.nix
@@ -1,16 +1,25 @@
 { inputs, cell }:
 
-# TODO(std)
-{
-  haskellLib = {
-    collectComponents' = _: _: { };
+# TODO(std) fill the stubs 
+
+let 
+  stubs = {
+    Agda = cell.packages.todo-derivation;
+    cabal-install = cell.packages.todo-derivation;
   };
 
-  cabalProject' = _: cell.packages.todo-derivation;
+in 
 
-  hackage-project = _: { 
-    hsPkgs = {
-      Agda = cell.packages.todo-derivation;
+  {
+    haskellLib = {
+      collectComponents' = _: _: { };
     };
-  }; 
-}
+
+    cabalProject' = _: {
+      hsPkgs = stubs;
+    }
+
+    hackage-project = _: { 
+      hsPkgs = stubs;
+    }; 
+  }
