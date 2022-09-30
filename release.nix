f23afb1db38193ddbebc87fa08432ff481a5f76e
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-09-30 09:39:17 +0100

    Revert "Roots only"
    This reverts commit 7c1039cd638dbc50f54cbef8e0093a3b2ec741d2.

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 { supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
   # Passed in by Hydra depending on the configuration, contains the revision and the out path
 , plutus ? null
-, rootsOnly ? true
+, rootsOnly ? false
 }:
 let
   traceNames = prefix: builtins.mapAttrs (n: v:
