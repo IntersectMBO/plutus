3d2b5c71eaba7e13671e4e254bd09c261351409a
effectfully <effectfully@gmail.com>
2022-10-03 19:23:17 +0200

    Drop everything term-related

--- plutus-core/plutus-ir/test/GeneratorSpec.hs
+++ plutus-core/plutus-ir/test/GeneratorSpec.hs
@@ -7,17 +7,12 @@ import Test.Tasty.Extras
 import Test.Tasty.QuickCheck
 
 import GeneratorSpec.Substitution
-import GeneratorSpec.Terms
 import GeneratorSpec.Types
 
 generators :: Int -> TestNested
 generators factor = return $ testGroup "generators"
   [ testProperty "prop_genKindCorrect"  $ withMaxSuccess (factor*100000) (prop_genKindCorrect False)
   , testProperty "prop_shrinkTypeSound" $ withMaxSuccess (factor*100000) prop_shrinkTypeSound
-  , testProperty "prop_genTypeCorrect"  $ withMaxSuccess (factor*10000) (prop_genTypeCorrect False)
-  , testProperty "prop_shrinkTermSound" $ withMaxSuccess (factor*20) prop_shrinkTermSound
   , testProperty "prop_substType"       $ withMaxSuccess (factor*10000) prop_substType
   , testProperty "prop_unify"           $ withMaxSuccess (factor*10000) prop_unify
-  -- TODO: make this work.
-  -- , testProperty "prop_compile" prop_compile
   ]
