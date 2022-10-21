63b0cb2141957d6c6a45846f4145a965c5d4295b
Ziyang Liu <unsafeFixIO@gmail.com>
2022-10-21 06:40:08 -0700

    Add a comment

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -23,4 +23,12 @@ major_version="${components[0]}.${components[1]}"
 find . -regex ".*/$versioned_packages_regex\.cabal" -exec sed -i "s/\(^version:\s*\).*/\1$1/" {} \;
 
 # update version bounds in all cabal files
+# It looks for patterns like the following:
+#
+# - ", plutus-core"
+# - ", plutus-core:uplc"
+# - ", plutus-core ^>=1.0"
+# - ", plutus-core:{plutus-core, plutus-core-testlib}  ^>=1.0"
+#
+# and updates the version bounds to "^>={major version}"
 find . -name "*.cabal" -exec sed -i "s/\(, ${versioned_packages_regex}[^^]*\).*/\1 ^>=$major_version/" {} \;
