7bcf5d1757ab8c516c7fd1cc0b8dd216c3a3facf
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-10-26 09:35:13 +0100

    Make more shell inputs into commands (#4919)
    * Make more shell inputs into commands
    
    * move check-the-flake to nix

--- __std__/cells/plutus/library/make-plutus-shell.nix
+++ __std__/cells/plutus/library/make-plutus-shell.nix
@@ -15,25 +15,67 @@ inputs.std.lib.dev.mkShell {
 
   commands = [
     {
-      package = cell.packages.check-the-flake;
-      category = "general commands";
-      help = "For nix maintainers: build everything in the flake";
-    }
-    {
-      package = cell.packages.fix-cabal-fmt;
-      category = "plutus";
-      help = "Format all cabal files in-place";
-    }
-    {
       package = cell.packages.fix-png-optimization;
-      category = "plutus";
+      category = "general commands";
       help = "Fix all PNG files in-place";
     }
     {
+      package = pkgs.shellcheck;
+      category = "general commands";
+      help = "Shell file checker";
+    }
+    {
+      package = pkgs.editorconfig-checker;
+      category = "general commands";
+      help = "Checker for editorconfig conformance";
+    }
+    {
+      package = pkgs.R;
+      category = "general commands";
+      help = "Statistical modelling tool";
+    }
+
+    {
+      package = cell.packages.fix-cabal-fmt;
+      category = "haskell";
+      help = "Format all cabal files in-place";
+    }
+    {
       package = cell.packages.fix-stylish-haskell;
-      category = "plutus";
+      category = "haskell";
       help = "Run stylish-haskell on all haskell files in-place";
     }
+    {
+      package = cell.packages.cabal-install;
+      name = "cabal";
+      category = "haskell";
+      help = "Haskell build tool";
+    }
+    {
+      package = cell.packages.haskell-language-server;
+      name = "haskell-language-server";
+      category = "haskell";
+      help = "Haskell Language Server";
+    }
+    {
+      package = cell.packages.hlint;
+      name = "hlint";
+      category = "haskell";
+      help = "Haskell linting tool";
+    }
+    {
+      package = cell.packages.stylish-haskell;
+      name = "stylish-haskell";
+      category = "haskell";
+      help = "Haskell code formatter";
+    }
+    {
+      package = cell.packages.cabal-fmt;
+      name = "cabal-fmt";
+      category = "haskell";
+      help = "Cabal file formatter";
+    }
+
     {
       package = cell.packages.sphinx-build-readthedocs-site;
       category = "docs";
@@ -49,37 +91,47 @@ inputs.std.lib.dev.mkShell {
       category = "docs";
       help = "nix build and serve the doc site on port 3000";
     }
+
+    {
+      package = cell.packages.nixpkgs-fmt;
+      category = "nix";
+      help = "Nix code formatter";
+    }
+    {
+      # tullia input isn't de-systemized for some reason
+      package = inputs.tullia.packages.${pkgs.system}.tullia;
+      category = "nix";
+      help = "Tools for working with CI tasks";
+    }
+    {
+      package = cell.packages.check-the-flake;
+      category = "nix";
+      help = "For nix maintainers: build everything in the flake";
+    }
   ];
 
   packages = [
-    cell.packages.sphinx-toolchain
-    cell.packages.cabal-install
-    cell.packages.haskell-language-server
+    # Only occasionally useful, not worth calling out as a command
     cell.packages.hie-bios
-    cell.packages.hlint
-    cell.packages.stylish-haskell
-    cell.packages.cabal-fmt
-    cell.packages.nixpkgs-fmt
+    # Provides sphinx-build and other things, unclear how to represent it as a command
+    cell.packages.sphinx-toolchain
 
-    # tullia input isn't de-systemized for some reason
-    inputs.tullia.packages.${pkgs.system}.tullia
-
+    # Misc  useful stuff, could make these commands but there's a lot already
     pkgs.editorconfig-core-c
-    pkgs.editorconfig-checker
     pkgs.jq
     pkgs.pre-commit
-    pkgs.shellcheck
     pkgs.yq
-    pkgs.zlib
-    pkgs.ghcid
     pkgs.gnused
     pkgs.awscli2
     pkgs.bzip2
+
+    # Needed to make building things work, not for commands
+    pkgs.zlib
     pkgs.cacert
     pkgs.pkg-config # TODO(std) Keep an eye on https://github.com/input-output-hk/plutus/pull/4906
 
+    # More R libraries
     pkgs.rPackages.plotly
-    pkgs.R
   ];
 
   devshell.startup."pre-commit-check".text = cell.packages.pre-commit-check.shellHook;
