df41e7d9b613c71c98026359c4beee27d7b4bcb9
zeme-iohk <lorenzo.calegari@iohk.io>
2022-11-04 16:27:12 +0100

    Move benchmarks to cicero

--- nix/cells/cloud/actions.nix
+++ nix/cells/cloud/actions.nix
@@ -41,4 +41,23 @@
       }
     '';
   };
+
+  "plutus/benchmark" = {
+    task = "benchmark";
+    io = ''
+      // This is a CUE expression that defines what events trigger a new run of this action.
+      // There is no documentation for this yet. Ask SRE if you have trouble changing this.
+
+      let github = {
+        #input: "GitHub event"
+        #repo: "input-output-hk/plutus"
+      }
+
+      #lib.merge
+      #ios: [
+        #lib.io.github_push & github,
+        { #lib.io.github_pr, github, #target_default: false },
+      ]
+    '';
+  };
 }
