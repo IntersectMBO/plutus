910a19f346b04bb8edaf8bc801c20fd883300d5a
Parthiv Seetharaman <pachum99@myrdd.info>
2022-11-16 07:40:17 -0800

    don't use github-ci preset

--- nix/cells/cloud/pipelines.nix
+++ nix/cells/cloud/pipelines.nix
@@ -21,17 +21,6 @@ let
       preset = {
         # needed on top-level task to set runtime options
         nix.enable = true;
-
-        github-ci = {
-          # Tullia tasks can run locally or on Cicero.
-          # When no facts are present we know that we are running locally and vice versa.
-          # When running locally, the current directory is already bind-mounted
-          # into the container, so we don't need to fetch the source from GitHub
-          # and we don't want to report a GitHub status.
-          enable = config.actionRun.facts != { };
-          repo = "input-output-hk/plutus";
-          sha = config.preset.github-ci.lib.getRevision "GitHub event" null;
-        };
       };
     };
 
