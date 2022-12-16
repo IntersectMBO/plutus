41e24f662fc32507bfad2e4acbc4c02fa064a3b7
zeme-iohk <lorenzo.calegari@iohk.io>
2022-12-16 17:33:15 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -158,9 +158,11 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.iog.io"
+      "https://cache.zw3rk.com"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
     allow-import-from-derivation = true;
   };
