1c219e734692066a812ed057c71331a1a3ec945a
kwxm <kenneth.mackenzie@iohk.io>
2022-12-22 09:37:46 +0000

    Add a few Haskell BLS examples

--- plutus-benchmark/bls-benchmarks/Main.hs
+++ plutus-benchmark/bls-benchmarks/Main.hs
@@ -23,6 +23,7 @@ import PlutusCore (DefaultFun, DefaultUni)
 import PlutusCore.Evaluation.Machine.ExBudget (ExBudget (exBudgetCPU, exBudgetMemory))
 import PlutusCore.Evaluation.Machine.ExBudgetingDefaults qualified as PLC
 import PlutusCore.Evaluation.Machine.ExMemory (ExCPU (..), ExMemory (..))
+import PlutusCore.Pretty qualified as PP
 import PlutusTx qualified as Tx
 import UntypedPlutusCore qualified as UPLC
 import UntypedPlutusCore.Evaluation.Machine.Cek qualified as Cek
@@ -44,7 +45,7 @@ import Hedgehog.Internal.Range qualified as R
 import System.IO.Unsafe (unsafePerformIO)
 import Text.Printf (printf)
 
-import Prelude (Double, IO, Integral, String, fromIntegral, (*), (/))
+import Prelude (Double, IO, Integral, String, fromIntegral, show, (*), (/))
 
 -- Protocol parameters (November 2022)
 
@@ -84,23 +85,7 @@ listOfSizedByteStrings n l = unsafePerformIO . G.sample $
                              G.list (R.singleton $ fromIntegral n)
                                   (G.bytes (R.singleton $ fromIntegral l))
 
-{-# INLINABLE hashAndAdd #-}
-hashAndAdd :: BuiltinByteString -> BuiltinByteString -> BuiltinBLS12_381_G1_Element
-hashAndAdd s1 s2 =
-    Tx.bls12_381_G1_add (Tx.bls12_381_G1_hashToCurve s1) (Tx.bls12_381_G1_hashToCurve s2)
-
-{-# INLINABLE hashAndAddG1 #-}
-hashAndAddG1 :: [BuiltinByteString] -> BuiltinBLS12_381_G1_Element
-hashAndAddG1 [] = error ()
-hashAndAddG1 (p:ps) =
-    go ps (Tx.bls12_381_G1_hashToCurve p)
-    where go [] acc     = acc
-          go (q:qs) acc = go qs $ Tx.bls12_381_G1_add (Tx.bls12_381_G1_hashToCurve q) acc
-
-mkScript :: [ByteString] -> UProg
-mkScript l =
-    let points = map toBuiltin l
-    in Tx.getPlc $ $$(Tx.compile [|| hashAndAddG1 ||]) `Tx.applyCode` Tx.liftCode points
+-------------------------------- Printing --------------------------------
 
 -- Printing utilities
 percentage :: (Integral a, Integral b) => a -> b -> Double
@@ -121,13 +106,13 @@ evaluate (UPLC.Program _ _ prog) =
               ExMemory mem = exBudgetMemory budget
           in (fromIntegral cpu, fromIntegral mem)
 
+
 -- | Evaluate a script and print out the serialised size and the CPU and memory
 -- usage, both as absolute values and percentages of the maxima specified in the
 -- protocol parameters.
-printStatistics :: Integer -> IO ()
-printStatistics n = do
-    let script = mkScript (listOfSizedByteStrings n 4)
-        serialised = Flat.flat (toAnonDeBruijnProg script)
+printStatistics :: Integer -> UProg -> IO ()
+printStatistics n script = do
+    let serialised = Flat.flat (toAnonDeBruijnProg script)
         size = BS.length serialised
         (cpu, mem) = evaluate script
     printf "  %3d %7d %8s %15d %8s %15d %8s \n"
@@ -136,12 +121,89 @@ printStatistics n = do
            cpu  (percentTxt cpu  max_tx_ex_steps)
            mem  (percentTxt mem  max_tx_ex_mem)
 
--- | Check that the Haskell version succeeds on a list of inputs.
-testHaskell :: Integer -> IO ()
-testHaskell _n = return ()
+------------------------------- Examples ---------------------------------
+
+-- Adding points in G1
+
+{-# INLINABLE hashAndAddG1 #-}
+hashAndAddG1 :: [BuiltinByteString] -> BuiltinBLS12_381_G1_Element
+hashAndAddG1 [] = error ()
+hashAndAddG1 (p:ps) =
+    go ps (Tx.bls12_381_G1_hashToCurve p)
+    where go [] acc     = acc
+          go (q:qs) acc = go qs $ Tx.bls12_381_G1_add (Tx.bls12_381_G1_hashToCurve q) acc
+
+mkHashAndAddG1Script :: [ByteString] -> UProg
+mkHashAndAddG1Script l =
+    let points = map toBuiltin l
+    in Tx.getPlc $ $$(Tx.compile [|| hashAndAddG1 ||]) `Tx.applyCode` Tx.liftCode points
+
+testHashAndAddG1 :: Integer -> IO ()
+testHashAndAddG1 n =
+    let script = mkHashAndAddG1Script (listOfSizedByteStrings n 4)
+    in printStatistics n script
+
+-- Adding points in G2
+
+{-# INLINABLE hashAndAddG2 #-}
+hashAndAddG2 :: [BuiltinByteString] -> BuiltinBLS12_381_G2_Element
+hashAndAddG2 [] = error ()
+hashAndAddG2 (p:ps) =
+    go ps (Tx.bls12_381_G2_hashToCurve p)
+    where go [] acc     = acc
+          go (q:qs) acc = go qs $ Tx.bls12_381_G2_add (Tx.bls12_381_G2_hashToCurve q) acc
+
+mkHashAndAddG2Script :: [ByteString] -> UProg
+mkHashAndAddG2Script l =
+    let points = map toBuiltin l
+    in Tx.getPlc $ $$(Tx.compile [|| hashAndAddG2 ||]) `Tx.applyCode` Tx.liftCode points
+
+testHashAndAddG2 :: Integer -> IO ()
+testHashAndAddG2 n =
+    let script = mkHashAndAddG2Script (listOfSizedByteStrings n 4)
+    in printStatistics n script
+
+-- Pairing operations
+
+{-# INLINABLE runPairingFunctions #-}
+runPairingFunctions
+    :: Tx.BuiltinBLS12_381_G1_Element
+    -> Tx.BuiltinBLS12_381_G2_Element
+    -> Tx.BuiltinBLS12_381_G1_Element
+    -> Tx.BuiltinBLS12_381_G2_Element
+    -> Bool
+runPairingFunctions p1 p2 q1 q2 =
+    let r1 = Tx.bls12_381_millerLoop p1 p2
+        r2 = Tx.bls12_381_millerLoop q1 q2
+    in Tx.bls12_381_finalVerify r1 r2
+
+makePairingScript
+    :: BuiltinBLS12_381_G1_Element
+    -> BuiltinBLS12_381_G2_Element
+    -> BuiltinBLS12_381_G1_Element
+    -> BuiltinBLS12_381_G2_Element
+    -> UProg
+makePairingScript p1 p2 q1 q2 =
+    Tx.getPlc $ $$(Tx.compile [|| runPairingFunctions ||])
+          `Tx.applyCode` Tx.liftCode p1
+          `Tx.applyCode` Tx.liftCode p2
+          `Tx.applyCode` Tx.liftCode q1
+          `Tx.applyCode` Tx.liftCode q2
+
+testPairing :: IO ()
+testPairing = do
+    let p1 = Tx.bls12_381_G1_hashToCurve $ toBuiltin $ BS.pack [0x23, 0x43, 0x56, 0xf2]
+        p2 = Tx.bls12_381_G2_hashToCurve $ toBuiltin $ BS.pack [0x10, 0x00, 0xff, 0x88]
+        q1 = Tx.bls12_381_G1_hashToCurve $ toBuiltin $ BS.pack [0x11, 0x22, 0x33, 0x44]
+        q2 = Tx.bls12_381_G2_hashToCurve $ toBuiltin $ BS.pack [0xa0, 0xb1, 0xc2, 0xd3]
+        script = makePairingScript p1 p2 q1 q2
+    printStatistics 1 script
+--    printf $ show $ PP.prettyClassicDebug script
 
 main :: IO ()
 main = do
   printf "    n     script size             CPU usage               Memory usage\n"
   printf "  ----------------------------------------------------------------------\n"
-  mapM_ printStatistics [0, 10..150]
+  mapM_ testHashAndAddG2 [0, 10..150]
+--  testPairing
+
