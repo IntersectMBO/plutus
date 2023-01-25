92b90b35290fcd74b96e8ec8f7d85be906d71fb4
zeme-iohk <lorenzo.calegari@iohk.io>
2023-01-25 19:30:33 +0700

    Fix bash typo on ci-plutus-benchmark.sh (#5085)

--- scripts/ci-plutus-benchmark.sh
+++ scripts/ci-plutus-benchmark.sh
@@ -20,7 +20,6 @@
 set -e
 
 PR_NUMBER="$1"
-
 BENCHMARK_NAME="$2"
 
 if [ -z "$PR_NUMBER" ] ; then
@@ -79,9 +78,9 @@ echo -e "</details>"
 
 jq -Rs '.' bench-compare-result.log >bench-compare.json
 
-if [ -v $GITHUB_TOKEN ] ; then
+if [ -z "$GITHUB_TOKEN" ] ; then
+   echo "[ci-plutus-benchmark]: GITHUB_TOKEN not set, not posting results to GitHub"
+else
    echo "[ci-plutus-benchmark]: Posting results to GitHub ..."
    curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": $(<bench-compare.json)}" "https://api.github.com/repos/input-output-hk/plutus/issues/${PR_NUMBER}/comments"
-else
-   echo "[ci-plutus-benchmark]: GITHUB_TOKEN not set, not posting results to GitHub"
 fi
