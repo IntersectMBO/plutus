5b4a6fb45d8b6a3017159899537e105061bdfd97
kwxm <kenneth.mackenzie@iohk.io>
2023-02-16 13:06:01 +0000

    Add Groth16 verification example

--- plutus-benchmark/bls-benchmarks/Main.hs
+++ plutus-benchmark/bls-benchmarks/Main.hs
@@ -9,6 +9,8 @@
 {-# LANGUAGE ViewPatterns        #-}
 
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 {- | Check how many Ed25519 signature verifications we can perform within the
@@ -40,13 +42,15 @@ import Cardano.Crypto.Seed (mkSeedFromBytes)
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
 import Data.ByteString.Hash qualified as Hash
+import Data.List (foldr)
+import Data.Word
 import Flat qualified
 import Hedgehog.Internal.Gen qualified as G
 import Hedgehog.Internal.Range qualified as R
 import System.IO.Unsafe (unsafePerformIO)
 import Text.Printf (printf)
 
-import Prelude (Double, IO, Integral, String, fromIntegral, show, (*), (/))
+import Prelude (Double, IO, Integral, String, fromIntegral, putStrLn, show, (*), (/))
 
 -- Protocol parameters (November 2022)
 
@@ -117,8 +121,8 @@ printStatistics n script = do
     let serialised = Flat.flat (toAnonDeBruijnProg script)
         size = BS.length serialised
         (cpu, mem) = evaluate script
-    printf "  %3d %7d %8s %15d %8s %15d %8s \n"
-           n
+    printf "  %3s %7d %8s %15d %8s %15d %8s \n"
+           (if n > 0 then show n else "-")
            size (percentTxt size max_tx_size)
            cpu  (percentTxt cpu  max_tx_ex_steps)
            mem  (percentTxt mem  max_tx_ex_mem)
@@ -263,13 +267,173 @@ testPairing = do
         q1 = Tx.bls12_381_G1_hashToCurve $ toBuiltin $ BS.pack [0x11, 0x22, 0x33, 0x44]
         q2 = Tx.bls12_381_G2_hashToCurve $ toBuiltin $ BS.pack [0xa0, 0xb1, 0xc2, 0xd3]
         script = makePairingScript p1 p2 q1 q2
-    printStatistics 1 script
---    printf $ show $ PP.prettyClassicDebug script
+    printStatistics (-1) script
+--      printf $ show $ PP.prettyClassicDebug script
+
+
+----------------------------- Groth16 verification -----------------------------------
+
+-- Lots of group elements for input to the computation
+
+alpha_g1_bs :: BuiltinByteString
+alpha_g1_bs = toBuiltin $ BS.pack
+              [ 183, 29, 177, 250, 95, 65, 54, 46, 147, 2, 91, 53, 86, 215, 110, 173, 18, 37, 207, 89,
+                13, 28, 219, 158, 56, 42, 31, 235, 183, 150, 61, 205, 36, 165, 30, 24, 223, 4, 171, 34,
+                27, 236, 175, 41, 22, 159, 175, 37]
+
+alpha_g1 :: BuiltinBLS12_381_G1_Element
+alpha_g1 = Tx.bls12_381_G1_uncompress alpha_g1_bs
+
+beta_g2_bs :: BuiltinByteString
+beta_g2_bs = toBuiltin $ BS.pack [
+  179, 162, 107, 11, 71, 18, 231, 141, 93, 113, 120, 109, 150, 19, 42, 124, 88, 80, 35,
+  163, 102, 50, 202, 218, 68, 23, 26, 195, 244, 93, 181, 36, 195, 246, 87, 12, 138, 63,
+  125, 236, 53, 174, 26, 195, 48, 155, 5, 221, 11, 48, 109, 180, 247, 79, 217, 236, 66,
+  28, 167, 12, 84, 66, 93, 146, 46, 172, 76, 64, 59, 0, 219, 145, 111, 222, 223, 6, 91,
+  220, 224, 14, 206, 23, 185, 122, 78, 151, 23, 62, 77, 89, 137, 129, 142, 223, 170, 76]
+beta_g2 :: BuiltinBLS12_381_G2_Element
+beta_g2 = Tx.bls12_381_G2_uncompress beta_g2_bs
+
+gamma_g2_bs ::BuiltinByteString
+gamma_g2_bs = toBuiltin $ BS.pack [
+  181, 172, 184, 0, 205, 73, 237, 140, 189, 219, 244, 145, 161, 252, 248, 171, 252, 147,
+  240, 157, 56, 187, 178, 236, 182, 176, 142, 35, 164, 100, 44, 229, 156, 155, 3, 134,
+  83, 154, 195, 206, 205, 251, 102, 169, 240, 39, 252, 33, 15, 37, 149, 16, 117, 100, 68,
+  188, 94, 239, 101, 79, 77, 6, 18, 181, 214, 55, 95, 149, 38, 177, 185, 102, 206, 83,
+  184, 241, 37, 148, 225, 179, 153, 208, 130, 49, 207, 230, 194, 105, 164, 74, 168, 213,
+  135, 242, 54, 157]
+
+gamma_g2 :: BuiltinBLS12_381_G2_Element
+gamma_g2 = Tx.bls12_381_G2_uncompress gamma_g2_bs
+
+delta_g2_bs ::BuiltinByteString
+delta_g2_bs = toBuiltin $ BS.pack [
+  179, 170, 121, 123, 175, 163, 154, 72, 246, 248, 124, 36, 131, 200, 148, 194, 129, 200,
+  7, 130, 28, 71, 48, 31, 251, 117, 90, 207, 207, 210, 44, 35, 35, 206, 223, 99, 73, 199,
+  254, 221, 50, 0, 164, 174, 85, 134, 49, 229, 1, 210, 153, 235, 147, 19, 92, 7, 207,
+  105, 76, 161, 24, 209, 179, 134, 73, 5, 41, 198, 15, 87, 147, 92, 239, 168, 159, 202,
+  250, 19, 168, 63, 132, 32, 123, 118, 254, 7, 141, 200, 89, 212, 2, 116, 61, 70, 140,
+  21]
+delta_g2 :: BuiltinBLS12_381_G2_Element
+delta_g2 = Tx.bls12_381_G2_uncompress delta_g2_bs
+
+
+gamma_abc_g1_1_bs :: BuiltinByteString
+gamma_abc_g1_1_bs = toBuiltin $ BS.pack [
+  183, 246, 208, 109, 211, 229, 36, 110, 246, 181, 27, 7, 92, 48, 182, 143, 212, 144,
+  251, 248, 94, 2, 5, 247, 159, 160, 77, 129, 19, 49, 146, 19, 148, 99, 181, 232, 239,
+  178, 44, 57, 239, 61, 209, 197, 9, 32, 21, 184]
+
+gamma_abc_g1_1 :: BuiltinBLS12_381_G1_Element
+gamma_abc_g1_1 = Tx.bls12_381_G1_uncompress gamma_abc_g1_1_bs
+
+gamma_abc_g1_2_bs :: BuiltinByteString
+gamma_abc_g1_2_bs = toBuiltin $ BS.pack [
+  162, 230, 55, 219, 255, 82, 161, 228, 168, 197, 217, 133, 179, 65, 31, 197, 253, 68,
+  175, 96, 126, 66, 146, 62, 171, 180, 122, 216, 118, 225, 240, 43, 91, 224, 52, 173,
+  175, 115, 149, 42, 232, 175, 254, 229, 245, 24, 65, 222]
+
+gamma_abc_g1_2 :: BuiltinBLS12_381_G1_Element
+gamma_abc_g1_2 = Tx.bls12_381_G1_uncompress gamma_abc_g1_2_bs
+
+a_bs :: BuiltinByteString
+a_bs = toBuiltin $ BS.pack [
+  160, 91, 229, 15, 171, 87, 149, 187, 135, 132, 57, 58, 80, 69, 249, 135, 71, 23, 58,
+  210, 135, 245, 94, 33, 52, 113, 189, 85, 151, 69, 85, 20, 82, 69, 60, 76, 58, 57, 231,
+  200, 131, 16, 132, 159, 60, 122, 31, 195]
+
+a :: BuiltinBLS12_381_G1_Element
+a = Tx.bls12_381_G1_uncompress a_bs
+
+
+b_bs :: BuiltinByteString
+b_bs= toBuiltin $ BS.pack [
+  173, 99, 72, 182, 183, 179, 76, 134, 191, 55, 167, 72, 205, 45, 130, 162, 80, 223, 198,
+  72, 70, 117, 102, 136, 37, 161, 111, 125, 166, 160, 77, 52, 36, 17, 62, 50, 92, 231,
+  52, 236, 68, 149, 96, 130, 192, 160, 110, 95, 24, 104, 225, 241, 166, 229, 89, 185,
+  254, 129, 241, 169, 1, 248, 166, 52, 27, 48, 28, 69, 178, 93, 48, 128, 251, 197, 3,
+  147, 83, 216, 247, 27, 85, 11, 39, 78, 196, 192, 124, 112, 205, 17, 83, 86, 44, 49, 76,
+  151]
+
+b :: BuiltinBLS12_381_G2_Element
+b = Tx.bls12_381_G2_uncompress b_bs
+
+
+c_bs :: BuiltinByteString
+c_bs = toBuiltin $ BS.pack [
+  181, 105, 204, 73, 27, 77, 240, 53, 203, 244, 158, 149, 31, 212, 254, 48, 170, 130, 54,
+  176, 226, 175, 104, 244, 193, 89, 44, 212, 13, 235, 235, 113, 138, 243, 54, 57, 219,
+  107, 193, 226, 218, 157, 152, 229, 83, 229, 234, 237]
+
+c :: BuiltinBLS12_381_G1_Element
+c = Tx.bls12_381_G1_uncompress c_bs
+
+scalar :: Integer
+scalar = 11090173236178880413184798967381823895855059959147020707603928894861818263064
+
+{-# INLINABLE groth16Verify #-}
+groth16Verify
+    :: BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> BuiltinByteString
+    -> Integer
+    -> Bool
+groth16Verify ag1 bg2 gg2 d abc1 abc2 a b c s =
+    let ag1'  = Tx.bls12_381_G1_uncompress ag1
+        bg2'  = Tx.bls12_381_G2_uncompress bg2
+        gg2'  = Tx.bls12_381_G2_uncompress gg2
+        d'    = Tx.bls12_381_G2_uncompress d
+        abc1' = Tx.bls12_381_G1_uncompress abc1
+        abc2' = Tx.bls12_381_G1_uncompress abc2
+        a'    = Tx.bls12_381_G1_uncompress a
+        b'    = Tx.bls12_381_G2_uncompress b
+        c'    = Tx.bls12_381_G1_uncompress c
+        l1    = Tx.bls12_381_millerLoop    a' b'
+        l2    = Tx.bls12_381_millerLoop    ag1' bg2'
+        l3    = Tx.bls12_381_millerLoop    c' d'
+        p     = Tx.bls12_381_G1_add        abc1' (Tx.bls12_381_G1_mul s abc2')
+        l4    = Tx.bls12_381_millerLoop    p gg2'
+        y     = Tx.bls12_381_GT_mul        l2 (Tx.bls12_381_GT_mul l3 l4)
+    in Tx.bls12_381_finalVerify l1 y
+
+mkGroth16VerifyScript :: UProg
+mkGroth16VerifyScript =
+    Tx.getPlc $ $$(Tx.compile [|| groth16Verify ||])
+           `Tx.applyCode` Tx.liftCode alpha_g1_bs
+           `Tx.applyCode` Tx.liftCode beta_g2_bs
+           `Tx.applyCode` Tx.liftCode gamma_g2_bs
+           `Tx.applyCode` Tx.liftCode delta_g2_bs
+           `Tx.applyCode` Tx.liftCode gamma_abc_g1_1_bs
+           `Tx.applyCode` Tx.liftCode gamma_abc_g1_2_bs
+           `Tx.applyCode` Tx.liftCode a_bs
+           `Tx.applyCode` Tx.liftCode b_bs
+           `Tx.applyCode` Tx.liftCode c_bs
+           `Tx.applyCode` Tx.liftCode scalar
 
 printHeader :: IO ()
 printHeader = do
   printf "    n     script size             CPU usage               Memory usage\n"
   printf "  ----------------------------------------------------------------------\n"
+
+-- The computation should return True
+checkGroth16Verify :: IO ()
+checkGroth16Verify =
+    let l1     = Tx.bls12_381_millerLoop    a b
+        l2     = Tx.bls12_381_millerLoop    alpha_g1 beta_g2
+        l3     = Tx.bls12_381_millerLoop    c delta_g2
+        p      = Tx.bls12_381_G1_add        gamma_abc_g1_1 (Tx.bls12_381_G1_mul scalar gamma_abc_g1_2)
+        l4     = Tx.bls12_381_millerLoop    p gamma_g2
+        y      = Tx.bls12_381_GT_mul        l2 (Tx.bls12_381_GT_mul l3 l4)
+        result = Tx.bls12_381_finalVerify l1 y
+    in if result
+       then printf "OK\n"
+       else printf "Wrong result\n"
 
 main :: IO ()
 main = do
@@ -298,5 +462,8 @@ main = do
   testPairing
   printf "\n\n"
 
---  testPairing
+  printf "Groth16 verification\n"
+  printHeader
+  printStatistics (-1)  mkGroth16VerifyScript
+
 
