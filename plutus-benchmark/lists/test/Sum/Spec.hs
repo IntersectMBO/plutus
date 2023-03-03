e06e67fe1b227a629e83b1cde7f29fa26467af58
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-03-03 12:42:34 +0000

    PLT-1604: Add some benchmarks for summing using 'data lists' (#5180)
    * Add some benchmarks for summing using 'data lists'
    
    Matthias says that Aiken encodes _all_ structured data as `Data`. I was
    very surprised that this is performant at all, so I thought I would
    write a benchmark to test.
    
    I wrote a variant of our existing list benchmarks that just uses
    explicit operations on `Data` to mimic a list of integers.
    
    Indeed, "data lists" seem to be significantly worse (3-4x) than
    both Scott-encoding and builtin lists. That's not terribly surprising,
    since deconstructing a list encoded using `Data` requires at least four
    builtin operations:
    - `UnConstrData` to get the `Constr` args
    - `Fst` to get the tag
    - `Snd` to get the args
    - `Head` to get the first arg
    - ... additional `Head`/`Tail`s to get further args
    
    I do think my benchmark is slightly worse for data lists than other
    usage, since it is working with a list of integers, rather than `Data`
    objects, so we have to do another builtin operation to extract the final
    integer. But I think the size of the difference is large enough that
    this is unlikely to be decisive.
    
    * Rename files

--- plutus-benchmark/lists/test/Sum/Spec.hs
+++ plutus-benchmark/lists/test/Sum/Spec.hs
@@ -2,12 +2,15 @@
 module Sum.Spec (tests) where
 
 import Test.Tasty
+import Test.Tasty.Extras
 import Test.Tasty.QuickCheck
 
 import PlutusBenchmark.Common (Term, cekResultMatchesHaskellValue)
 
 import PlutusBenchmark.Lists.Sum.Compiled qualified as Compiled
 import PlutusBenchmark.Lists.Sum.HandWritten qualified as HandWritten
+
+import PlutusTx.Test qualified as Tx
 
 -- | Check that the various summation functions all give the same result as 'sum'
 
@@ -17,12 +20,25 @@ prop_sum termMaker l = cekResultMatchesHaskellValue (termMaker l) (===) (sum l)
 tests :: TestTree
 tests =
   testGroup "plutus-benchmark list-sum tests"
-    [ testProperty "Handwritten right fold (Scott lists)"    $ prop_sum HandWritten.mkSumRightScottTerm
-    , testProperty "Handwritten right fold (built-in lists)" $ prop_sum HandWritten.mkSumRightBuiltinTerm
-    , testProperty "Compiled right fold (Scott lists)"       $ prop_sum Compiled.mkSumRightScottTerm
-    , testProperty "Compiled right fold (built-in lists)"    $ prop_sum Compiled.mkSumRightBuiltinTerm
-    , testProperty "Handwritten left fold (Scott lists)"     $ prop_sum HandWritten.mkSumLeftScottTerm
-    , testProperty "Handwritten left fold (built-in lists)"  $ prop_sum HandWritten.mkSumLeftBuiltinTerm
-    , testProperty "Compiled left fold (Scott lists)"        $ prop_sum Compiled.mkSumLeftScottTerm
-    , testProperty "Compiled left fold (built-in lists)"     $ prop_sum Compiled.mkSumLeftBuiltinTerm
+    [ testGroup "correct evaluation"
+      [ testProperty "Handwritten right fold (Scott lists)"    $ prop_sum HandWritten.mkSumRightScottTerm
+      , testProperty "Handwritten right fold (built-in lists)" $ prop_sum HandWritten.mkSumRightBuiltinTerm
+      , testProperty "Compiled right fold (Scott lists)"       $ prop_sum Compiled.mkSumRightScottTerm
+      , testProperty "Compiled right fold (built-in lists)"    $ prop_sum Compiled.mkSumRightBuiltinTerm
+      , testProperty "Compiled right fold (data lists)"        $ prop_sum Compiled.mkSumRightDataTerm
+      , testProperty "Handwritten left fold (Scott lists)"     $ prop_sum HandWritten.mkSumLeftScottTerm
+      , testProperty "Handwritten left fold (built-in lists)"  $ prop_sum HandWritten.mkSumLeftBuiltinTerm
+      , testProperty "Compiled left fold (Scott lists)"        $ prop_sum Compiled.mkSumLeftScottTerm
+      , testProperty "Compiled left fold (built-in lists)"     $ prop_sum Compiled.mkSumLeftBuiltinTerm
+      , testProperty "Compiled left fold (data lists)"         $ prop_sum Compiled.mkSumLeftDataTerm
+      ]
+    , runTestNestedIn ["lists", "test"] $ testNested "Sum"
+      [ Tx.goldenBudget "right-fold-scott"    $ Compiled.mkSumRightScottCode input
+      , Tx.goldenBudget "right-fold-built-in" $ Compiled.mkSumRightBuiltinCode input
+      , Tx.goldenBudget "right-fold-data"     $ Compiled.mkSumRightDataCode input
+      , Tx.goldenBudget "left-fold-scott"     $ Compiled.mkSumLeftScottCode input
+      , Tx.goldenBudget "left-fold-built-in"  $ Compiled.mkSumLeftBuiltinCode input
+      , Tx.goldenBudget "left-fold-data"      $ Compiled.mkSumLeftDataCode input
+      ]
     ]
+  where input = [1..100]
