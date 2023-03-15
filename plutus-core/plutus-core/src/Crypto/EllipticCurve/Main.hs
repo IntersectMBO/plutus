e451055b01d7a0ddc229742ee91391154b26fd4b
kwxm <kwxm@inf.ed.ac.uk>
2023-03-15 10:53:21 +0000

    Fix types in bls-sizes executable

--- plutus-core/plutus-core/src/Crypto/EllipticCurve/Main.hs
+++ plutus-core/plutus-core/src/Crypto/EllipticCurve/Main.hs
@@ -9,19 +9,19 @@ import Crypto.EllipticCurve.BLS12_381.Internal
 import Data.Proxy
 import Text.Printf
 
-sizeInfo :: Integer -> String
+sizeInfo :: Int -> String
 sizeInfo n = printf "%3d bytes  / %3d words" n (n `div` 8)
 
 main :: IO ()
 main = do
-  printf "G1 memory size:     %s\n" $ sizeInfo ((sizeP (Proxy :: Proxy Curve1)) :: Integer)
-  printf "G1 compressed size: %s\n" $ sizeInfo ((compressedSizeP (Proxy :: Proxy Curve1)) :: Integer)
-  printf "G1 serialised size: %s\n" $ sizeInfo ((serializedSizeP (Proxy :: Proxy Curve1)) :: Integer)
-  printf "G1 affine size:     %s\n" $ sizeInfo ((sizeAffine (Proxy :: Proxy Curve1)) :: Integer)
+  printf "G1 memory size:     %s\n" $ sizeInfo ((sizeP (Proxy :: Proxy Curve1)) :: Int)
+  printf "G1 compressed size: %s\n" $ sizeInfo ((compressedSizeP (Proxy :: Proxy Curve1)) :: Int)
+  printf "G1 serialised size: %s\n" $ sizeInfo ((serializedSizeP (Proxy :: Proxy Curve1)) :: Int)
+  printf "G1 affine size:     %s\n" $ sizeInfo ((sizeAffine (Proxy :: Proxy Curve1)) :: Int)
   printf "\n"
-  printf "G2 memory size:     %s\n" $ sizeInfo ((sizeP (Proxy :: Proxy Curve2)) :: Integer)
-  printf "G2 compressed size: %s\n" $ sizeInfo ((compressedSizeP (Proxy :: Proxy Curve2)) :: Integer)
-  printf "G2 serialised size: %s\n" $ sizeInfo ((serializedSizeP (Proxy :: Proxy Curve2)) :: Integer)
-  printf "G2 affine size:     %s\n" $ sizeInfo ((sizeAffine (Proxy :: Proxy Curve2)) :: Integer)
+  printf "G2 memory size:     %s\n" $ sizeInfo ((sizeP (Proxy :: Proxy Curve2)) :: Int)
+  printf "G2 compressed size: %s\n" $ sizeInfo ((compressedSizeP (Proxy :: Proxy Curve2)) :: Int)
+  printf "G2 serialised size: %s\n" $ sizeInfo ((serializedSizeP (Proxy :: Proxy Curve2)) :: Int)
+  printf "G2 affine size:     %s\n" $ sizeInfo ((sizeAffine (Proxy :: Proxy Curve2)) :: Int)
   printf "\n"
-  printf "GT memory size:     %s\n" $ sizeInfo (sizePT:: Integer)
+  printf "GT memory size:     %s\n" $ sizeInfo (sizePT:: Int)
