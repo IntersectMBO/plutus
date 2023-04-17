6630d15e3007697e7e7f9a7d55b9fd1975997b22
kwxm <kwxm@inf.ed.ac.uk>
2023-04-17 15:53:53 +0100

    Update cabal file

--- plutus-core/untyped-plutus-core/test/Evaluation/Builtins/BLS12_381/Utils.hs
+++ plutus-core/untyped-plutus-core/test/Evaluation/Builtins/BLS12_381/Utils.hs
@@ -59,7 +59,9 @@ mkApp1 b x = mkIterApp () (builtin () b) [x]
 mkApp2 :: PLC.DefaultFun -> PlcTerm -> PlcTerm -> PlcTerm
 mkApp2 b x y = mkIterApp () (builtin () b) [x,y]
 
--- ByteString utilities
+{- | ByteString utilities.  These are used in tests to check that the format of
+   compressed points conforms to the specification at
+   https://github.com/supranational/blst#serialization-format . -}
 
 -- The most siginificant bit of a serialised curve point is set if the
 -- serialised point is in compressed form (x-coordinate only)
@@ -84,22 +86,27 @@ unsafeUnconsBS b =
       Nothing -> error "Tried to uncons empty bytestring"
       Just p  -> p
 
--- Apply some function to the most significant byte of a bytestring
+-- | Apply some function to the most significant byte of a bytestring
 modifyMSB :: (Word8 -> Word8) -> ByteString -> ByteString
 modifyMSB f s =
     let (w,rest) = unsafeUnconsBS s
     in BS.cons (f w) rest
 
+-- | Flip a specified set of bits in the most siginficant byte of a bytestring.
 flipBits :: Word8 -> ByteString -> ByteString
 flipBits mask = modifyMSB (mask `xor`)
 
+-- | Clear a specified set of bits in the most siginficant byte of a bytestring.
 clearBits :: Word8 -> ByteString -> ByteString
 clearBits mask = modifyMSB ((complement mask) .&.)
 
+-- | Set a specified set of bits in the most siginficant byte of a bytestring.
 setBits :: Word8 -> ByteString -> ByteString
 setBits mask = modifyMSB (mask .|.)
 
+-- | Check that a specified set of bits is set in the most siginficant byte of a
+-- bytestring.
 isSet :: Word8 -> ByteString -> Bool
 isSet mask s =
     let (w,_) = unsafeUnconsBS s
-    in w .&. mask /= 0
+    in w .&. mask == mask
