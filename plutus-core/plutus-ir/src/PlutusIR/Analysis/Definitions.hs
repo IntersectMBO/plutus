ac63af91ba809529ce8280eeb3c64ac79be28f4b
Marty Stumpf <marty.stumpf@iohk.io>
2023-04-17 12:32:30 -0700

    WIP add let term to analysis.

--- plutus-core/plutus-ir/src/PlutusIR/Analysis/Definitions.hs
+++ plutus-core/plutus-ir/src/PlutusIR/Analysis/Definitions.hs
@@ -12,7 +12,7 @@ import Data.Functor.Foldable
 import PlutusCore.Error
 import PlutusCore.Name
 import PlutusIR.Core
-import PlutusIR.Core.Instance.Recursive (TermF (IWrapF, LamAbsF, TyAbsF, TyInstF, VarF))
+import PlutusIR.Core.Instance.Recursive (TermF (IWrapF, LamAbsF, LetF, TyAbsF, TyInstF, VarF))
 
 import Control.Monad.State
 import Control.Monad.Writer
@@ -27,13 +27,29 @@ termDefs
         MonadWriter [UniqueError ann] m)
     => Term tyname name uni fun ann
     -> m ()
-termDefs = cata $ \case
-    VarF ann n         -> addUsage n ann TermScope
-    LamAbsF ann n ty t -> addDef n ann TermScope >> typeDefs ty >> t
-    IWrapF _ pat arg t -> typeDefs pat >> typeDefs arg >> t
-    TyAbsF ann tn _ t  -> addDef tn ann TypeScope >> t
-    TyInstF _ t ty     -> t >> typeDefs ty
-    x                  -> sequence_ x
+termDefs =
+    let -- Add bindings to definition maps.
+        -- addBindingDef :: Binding tyname name uni fun ann -
+        addBindingDef (TermBind _tmAnn _tmStrict (VarDecl varAnn n varTy) tm) =
+            addDef n varAnn TermScope >> typeDefs varTy >> tm
+        addBindingDef (TypeBind _tyBindingAnn (TyVarDecl tyN tyAnn _) ty) =
+            addDef tyN tyAnn TypeScope >> typeDefs ty
+        addBindingDef
+            (DatatypeBind
+                _a
+                (Datatype dataAnn _ _ dataName _)) =
+            addDef dataName dataAnn TermScope
+    in
+    cata $ \case
+        LetF ann r bindings term -> do
+            mapM_ addBindingDef bindings >> term
+        VarF ann n         -> addUsage n ann TermScope
+        LamAbsF ann n ty t -> addDef n ann TermScope >> typeDefs ty >> t
+        IWrapF _ pat arg t -> typeDefs pat >> typeDefs arg >> t
+        TyAbsF ann tn _ t  -> addDef tn ann TypeScope >> t
+        TyInstF _ t ty     -> t >> typeDefs ty
+        x                  -> sequence_ x
+
 runTermDefs
     :: (Ord ann,
         HasUnique name TermUnique,
