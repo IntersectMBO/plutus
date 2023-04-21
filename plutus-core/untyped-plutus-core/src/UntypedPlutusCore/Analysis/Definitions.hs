ed2e8352f0ff36538d1c2036291c283bc8f47167
Marty Stumpf <marty.stumpf@iohk.io>
2023-04-21 09:31:04 -0700

    Remove Recursive.hs in UPLC and do definition analysis with subterm traversals.

--- plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Analysis/Definitions.hs
+++ plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Analysis/Definitions.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 -- | Definition analysis for untyped Plutus Core.
 -- This just adapts term-related code from PlutusCore.Analysis.Definitions;
 -- we just re-use the typed machinery to do the hard work here.
@@ -10,13 +9,12 @@ module UntypedPlutusCore.Analysis.Definitions
 import UntypedPlutusCore.Core
 
 import PlutusCore.Analysis.Definitions (ScopeType (TermScope), UniqueInfos, addDef, addUsage)
-import PlutusCore.Error
-import PlutusCore.Name
+import PlutusCore.Error (UniqueError)
+import PlutusCore.Name (HasUnique, TermUnique (TermUnique), Unique (Unique))
 
-import Data.Functor.Foldable
-
-import Control.Monad.State
-import Control.Monad.Writer
+import Control.Lens (forMOf)
+import Control.Monad.State (MonadState, execStateT)
+import Control.Monad.Writer (MonadWriter, WriterT (runWriterT))
 
 termDefs
     :: (Ord ann,
@@ -24,11 +22,15 @@ termDefs
         MonadState (UniqueInfos ann) m,
         MonadWriter [UniqueError ann] m)
     => Term name uni fun ann
-    -> m ()
-termDefs = cata $ \case
-    VarF ann n      -> addUsage n ann TermScope
-    LamAbsF ann n t -> addDef n ann TermScope >> t
-    x               -> sequence_ x
+    -> m (Term name uni fun ann)
+termDefs tm = case tm of
+    Var ann n      -> do
+        addUsage n ann TermScope
+        pure tm
+    LamAbs ann n t -> do
+        addDef n ann TermScope
+        termDefs t
+    x               -> forMOf termSubterms x termDefs
 
 runTermDefs
     :: (Ord ann,
