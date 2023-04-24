caf66ac8a25d8259b8b782cb5d4c7a18d29bd816
Ziyang Liu <unsafeFixIO@gmail.com>
2023-04-24 12:15:45 -0700

    Do the simple thing

--- plutus-core/plutus-ir/src/PlutusIR/Transform/KnownCon.hs
+++ plutus-core/plutus-ir/src/PlutusIR/Transform/KnownCon.hs
@@ -15,7 +15,6 @@ import PlutusIR.Core
 import PlutusIR.Transform.Rename ()
 
 import Control.Lens hiding (cons)
-import Data.Bitraversable
 import Data.List.Extra qualified as List
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -50,7 +49,7 @@ knownCon ::
     Term tyname name uni fun a ->
     m (Term tyname name uni fun a)
 knownCon t0 = do
-    -- We are going to record information about variables in a global map, so we 
+    -- We are going to record information about variables in a global map, so we
     -- need global uniqueness
     t <- PLC.rename t0
     let ctxt =
@@ -75,54 +74,37 @@ go ::
     m (Term tyname name uni fun a)
 go ctxt = go'
   where
-    go' = \case
-        t@Apply{} -> do
-            let (fun, args) = collectArgs t
-            case fun of
-                Var _ n
-                    -- it appears in our map, so it's a destructor
-                    | Just cons <- Map.lookup n ctxt
-                    , ((TermExpr scrut, _) : (TypeExpr _resTy, _) : rest) <-
-                        -- The datatype may have some type arguments, we
-                        -- aren't interested in them, so we drop them. We can
-                        -- do this easily because we know that they are all type
-                        -- arguments, and then we have a term argument for the
-                        -- scrutinee
-                        dropWhile (isTyArg . fst) args
-                    -- The scrutinee is itself an application
-                    , (Var _ con, conArgs) <- collectArgs scrut
-                    -- ... of one of the constructors from the same datatype as the destructor
-                    , Just i <- List.findIndex (== con) cons
-                    -- ... and there is a  branch for that constructor in the destructor application
-                    , Just (TermExpr branch, _) <- rest List.!? i
-                    , -- This condition ensures the destructor is fully-applied
-                      -- (which should always be the case in programs that come from Plutus Tx, but not necessarily in arbitrary PIR programs).
-                      -- it's okay to have more arguments: the result of the destructor might itself be
-                      -- a function which is being applied!
-                      length rest >= length cons -> do
-                        pure $
-                            mkTermApps
-                                branch
-                                -- The arguments to the selected branch consists of the arguments
-                                -- to the constructor (without the leading type arguments - e.g.,
-                                -- if the scrutinee is `Just {integer} 1`, we only need the `1`),
-                                -- and the remaining arguments in `rest` (because the destructor
-                                -- may be over-applied).
-                                ((dropWhile (isTyArg . fst) conArgs) <> drop (length cons) rest)
-                _ ->
-                    -- This is more efficient than `forOf termSubterms t go'`.
-                    -- The latter is quadratic in the length of `args`.
+    go' t
+        |   (Var _ n, args) <- collectArgs t
+            , Just cons <- Map.lookup n ctxt
+            , ((TermExpr scrut, _) : (TypeExpr _resTy, _) : rest) <-
+                -- The datatype may have some type arguments, we
+                -- aren't interested in them, so we drop them. We can
+                -- do this easily because we know that they are all type
+                -- arguments, and then we have a term argument for the
+                -- scrutinee
+                dropWhile (isTyArg . fst) args
+            -- The scrutinee is itself an application
+            , (Var _ con, conArgs) <- collectArgs scrut
+            -- ... of one of the constructors from the same datatype as the destructor
+            , Just i <- List.findIndex (== con) cons
+            -- ... and there is a  branch for that constructor in the destructor application
+            , Just (TermExpr branch, _) <- rest List.!? i
+            , -- This condition ensures the destructor is fully-applied
+                -- (which should always be the case in programs that come from Plutus Tx,
+                -- but not necessarily in arbitrary PIR programs).
+                -- it's okay to have more arguments: the result of the destructor might itself be
+                -- a function which is being applied!
+                length rest >= length cons = do
+                pure $
                     mkTermApps
-                        <$> go' fun
-                        <*> traverse
-                            ( bitraverse
-                                ( \case
-                                    TermExpr term -> TermExpr <$> go' term
-                                    expr          -> pure expr
-                                )
-                                pure
-                            )
-                            args
-        t -> forOf termSubterms t go'
+                        branch
+                        -- The arguments to the selected branch consists of the arguments
+                        -- to the constructor (without the leading type arguments - e.g.,
+                        -- if the scrutinee is `Just {integer} 1`, we only need the `1`),
+                        -- and the remaining arguments in `rest` (because the destructor
+                        -- may be over-applied).
+                        ((dropWhile (isTyArg . fst) conArgs) <> drop (length cons) rest)
+        | otherwise = forOf termSubterms t go'
 
     isTyArg = \case TypeExpr{} -> True; _ -> False
