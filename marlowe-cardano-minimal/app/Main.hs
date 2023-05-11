df890b669c8376eecb11c43c883ef4de1d8b4169
Brian W Bush <code@functionally.io>
2023-05-11 12:28:25 -0600

    PLT-5596 Fixed external serialization of Marlowe validators.

--- marlowe-cardano-minimal/app/Main.hs
+++ marlowe-cardano-minimal/app/Main.hs
@@ -11,31 +11,51 @@ import Benchmark.Marlowe (executeBenchmark)
 import Benchmark.Marlowe.Types (Benchmark (bReferenceCost))
 import Language.Marlowe.Scripts (rolePayoutValidatorBytes, rolePayoutValidatorHash)
 import PlutusLedgerApi.V2 (SerialisedScript)
+import Cardano.Binary (serialize')
+import Language.Marlowe.Scripts (marloweValidatorBytes, marloweValidatorHash,
+                                 rolePayoutValidatorBytes, rolePayoutValidatorHash)
+import PlutusLedgerApi.V2 (ScriptHash, SerialisedScript)
 
 import Benchmark.Marlowe.RolePayout qualified as RP
 import Data.ByteString qualified as BS
 import Data.ByteString.Base16 qualified as B16
-import Data.ByteString.Short qualified as SBS
 
 
 main :: IO ()
 main =
   do
 
+    printValidator
+      "Semantics"
+      "marlowe-semantics.plutus"
+      marloweValidatorHash
+      marloweValidatorBytes
+
+    printValidator
+      "Role payout"
+      "marlowe-rolepayout.plutus"
+      rolePayoutValidatorHash
+      rolePayoutValidatorBytes
+
     -- FIXME: Work in progress on benchmarks for semantics validator.
---  putStrLn $ "Semantics validator hash:   " <> show marloweValidatorHash
---  BS.writeFile "marlowe-semantics.plutus"
---    $ "{\"type\": \"PlutusScriptV2\", \"description\": \"\", \"cborHex\": \""
---    <> B16.encode (SBS.fromShort marloweValidatorBytes) <> "\"}"
-
-    putStrLn $ "Role-payout validator hash: " <> show rolePayoutValidatorHash
-    -- FIXME: I suspect that this serialization is incorrect because hashing it
-    --        with `cardano-cli` does not agree with the hash computed above.
-    BS.writeFile "marlowe-rolepayout.plutus"
-      $ "{\"type\": \"PlutusScriptV2\", \"description\": \"\", \"cborHex\": \""
-      <> B16.encode (SBS.fromShort rolePayoutValidatorBytes) <> "\"}"
-
     mapM_ (printResult RP.serialisedValidator) RP.benchmarks
+
+printValidator
+  :: String
+  -> FilePath
+  -> ScriptHash
+  -> SerialisedScript
+  -> IO ()
+printValidator name file hash validator =
+  do
+    putStrLn $ name <> ":"
+    putStrLn $ "  Validator hash: " <> show hash
+    putStrLn $ "  Validator file: " <> file
+    BS.writeFile file
+      $ "{\"type\": \"PlutusScriptV2\", \"description\": \"\", \"cborHex\": \""
+      <> B16.encode (serialize' validator) <> "\"}"
+    putStrLn ""
+
 
 printResult
   :: SerialisedScript
