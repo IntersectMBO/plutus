c5f01d31d23da9cc68de8337ea9b43f49df3eba4
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-05-18 15:21:53 +0100

    Try to fix the longitudinal benchmarking job (#5332)

--- scripts/run-longitudinal-benchmarks.sh
+++ scripts/run-longitudinal-benchmarks.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# We shouldn't need this if we're running in a nix shell, but
+# we currently do because of https://github.com/input-output-hk/haskell.nix/issues/1955
+cabal update
 for bench in $BENCHMARKS; do 
   2>&1 cabal run "$bench" | tee "$bench-output.txt"
 done 
