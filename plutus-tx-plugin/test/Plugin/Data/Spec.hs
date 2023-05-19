acead85ff60b6502872a7c1ac449a56a4a62d1e8
effectfully <effectfully@gmail.com>
2023-05-19 05:54:18 +0200

    [Textual] Improve readable pretty-printing (#5325)

--- plutus-tx-plugin/test/Plugin/Data/Spec.hs
+++ plutus-tx-plugin/test/Plugin/Data/Spec.hs
@@ -19,6 +19,7 @@ module Plugin.Data.Spec where
 
 import Test.Tasty.Extras
 
+import PlutusCore.Pretty qualified as PLC
 import PlutusCore.Test
 import PlutusTx.Builtins qualified as Builtins
 import PlutusTx.Code
@@ -248,7 +249,11 @@ recursiveTypes = testNested "recursive" [
     , goldenUEval "ptreeFirstEval" [ toUPlc ptreeFirst, toUPlc ptreeConstruct ]
     , goldenUEval "sameEmptyRoseEval" [ toUPlc sameEmptyRose, toUPlc emptyRoseConstruct ]
     , goldenUPlc "sameEmptyRose" sameEmptyRose
-    , goldenTPlc "interListConstruct" interListConstruct
+    , goldenTPlcWith
+        ".tplc-read"
+        (ppThrow . fmap PLC.AsReadable)
+        "interListConstruct"
+        interListConstruct
     , goldenUEval "processInterListEval" [ toUPlc processInterList, toUPlc interListConstruct ]
   ]
 
