8ecbfd0c2c1c5e6e38c570b0cc5f104b2bfb83d8
kwxm <kenneth.mackenzie@iohk.io>
2023-05-23 12:38:37 +0100

    Adjust formatting for print-builtin-signatures

--- plutus-core/executables/src/PlutusCore/Executable/Common.hs
+++ plutus-core/executables/src/PlutusCore/Executable/Common.hs
@@ -590,7 +590,7 @@ runPrintBuiltinSignatures :: IO ()
 runPrintBuiltinSignatures = do
     let builtins = enumerate @PLC.DefaultFun
     mapM_
-      (\x -> putStr (printf "%-25s: %s\n" (show $ PP.pretty x) (show $ getSignature x)))
+      (\x -> putStr (printf "%-35s: %s\n" (show $ PP.pretty x) (show $ getSignature x)))
       builtins
   where
     getSignature (PLC.toBuiltinMeaning @_ @_ @(PlcTerm ()) def -> PLC.BuiltinMeaning sch _ _) =
