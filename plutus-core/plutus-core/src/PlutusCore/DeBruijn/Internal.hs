0aec75be223ee48899bfa82dd45bdd2947b62b41
Ziyang Liu <unsafeFixIO@gmail.com>
2023-05-24 14:11:52 -0700

    formatting

--- plutus-core/plutus-core/src/PlutusCore/DeBruijn/Internal.hs
+++ plutus-core/plutus-core/src/PlutusCore/DeBruijn/Internal.hs
@@ -1,48 +1,47 @@
--- editorconfig-checker-disable-file
 {-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TemplateHaskell       #-}
-
 -- This fires on GHC-9.2.4 for some reason, not entirely sure
 -- what's going on
 {-# OPTIONS_GHC -Wno-identities #-}
+
 -- | Support for using de Bruijn indices for term and type names.
-module PlutusCore.DeBruijn.Internal
-    ( Index (..)
-    , HasIndex (..)
-    , DeBruijn (..)
-    , NamedDeBruijn (..)
-    , FakeNamedDeBruijn (..)
-    , TyDeBruijn (..)
-    , NamedTyDeBruijn (..)
-    , FreeVariableError (..)
-    , AsFreeVariableError (..)
-    , Level (..)
-    , Levels (..)
-    , declareUnique
-    , declareBinder
-    , withScope
-    , getIndex
-    , getUnique
-    , unNameDeBruijn
-    , unNameTyDeBruijn
-    , fakeNameDeBruijn
-    , fakeTyNameDeBruijn
-    , nameToDeBruijn
-    , tyNameToDeBruijn
-    , deBruijnToName
-    , deBruijnToTyName
-    , freeIndexThrow
-    , freeIndexAsConsistentLevel
-    , freeUniqueThrow
-    , runDeBruijnT
-    , deBruijnInitIndex
-    , toFake
-    , fromFake
-    ) where
+module PlutusCore.DeBruijn.Internal (
+  Index (..),
+  HasIndex (..),
+  DeBruijn (..),
+  NamedDeBruijn (..),
+  FakeNamedDeBruijn (..),
+  TyDeBruijn (..),
+  NamedTyDeBruijn (..),
+  FreeVariableError (..),
+  AsFreeVariableError (..),
+  Level (..),
+  Levels (..),
+  declareUnique,
+  declareBinder,
+  withScope,
+  getIndex,
+  getUnique,
+  unNameDeBruijn,
+  unNameTyDeBruijn,
+  fakeNameDeBruijn,
+  fakeTyNameDeBruijn,
+  nameToDeBruijn,
+  tyNameToDeBruijn,
+  deBruijnToName,
+  deBruijnToTyName,
+  freeIndexThrow,
+  freeIndexAsConsistentLevel,
+  freeUniqueThrow,
+  runDeBruijnT,
+  deBruijnInitIndex,
+  toFake,
+  fromFake,
+) where
 
 import PlutusCore.Name
 import PlutusCore.Pretty
@@ -67,22 +66,23 @@ import GHC.Generics
 
 -- | A relative index used for de Bruijn identifiers.
 newtype Index = Index Word64
-    deriving stock (Generic)
-    deriving newtype (Show, Num, Enum, Real, Integral, Eq, Ord, Pretty, NFData, Read)
+  deriving stock (Generic)
+  deriving newtype (Show, Num, Enum, Real, Integral, Eq, Ord, Pretty, NFData, Read)
 
 -- | The LamAbs index (for debruijn indices) and the starting level of DeBruijn monad
 deBruijnInitIndex :: Index
 deBruijnInitIndex = 0
 
 -- The bangs gave us a speedup of 6%.
+
 -- | A term name as a de Bruijn index.
-data NamedDeBruijn = NamedDeBruijn { ndbnString :: !T.Text, ndbnIndex :: !Index }
-    deriving stock (Show, Generic, Read)
-    deriving anyclass NFData
+data NamedDeBruijn = NamedDeBruijn {ndbnString :: !T.Text, ndbnIndex :: !Index}
+  deriving stock (Show, Generic, Read)
+  deriving anyclass (NFData)
 
 -- | A wrapper around nameddebruijn that must hold the invariant of name=`fakeName`.
 newtype FakeNamedDeBruijn = FakeNamedDeBruijn NamedDeBruijn
-    deriving newtype (Show, Eq, NFData, PrettyBy config)
+  deriving newtype (Show, Eq, NFData, PrettyBy config)
 
 toFake :: DeBruijn -> FakeNamedDeBruijn
 toFake (DeBruijn ix) = FakeNamedDeBruijn $ NamedDeBruijn fakeName ix
@@ -95,61 +95,66 @@ fakeName :: T.Text
 fakeName = "i"
 
 instance Eq NamedDeBruijn where
-    -- ignoring actual names and only relying solely on debruijn indices
-    (NamedDeBruijn _ ix1) == (NamedDeBruijn _ ix2) = ix1 == ix2
+  -- ignoring actual names and only relying solely on debruijn indices
+  (NamedDeBruijn _ ix1) == (NamedDeBruijn _ ix2) = ix1 == ix2
 
 -- | A term name as a de Bruijn index, without the name string.
-newtype DeBruijn = DeBruijn { dbnIndex :: Index }
-    deriving stock (Show, Generic, Eq)
-    deriving newtype (NFData)
+newtype DeBruijn = DeBruijn {dbnIndex :: Index}
+  deriving stock (Show, Generic, Eq)
+  deriving newtype (NFData)
 
 -- | A type name as a de Bruijn index.
 newtype NamedTyDeBruijn = NamedTyDeBruijn NamedDeBruijn
-    deriving stock (Show, Generic)
-    deriving newtype (PrettyBy config, NFData)
-    -- ignoring actual names and only relying solely on debruijn indices
-    deriving Eq via NamedDeBruijn
+  deriving stock (Show, Generic)
+  deriving newtype (PrettyBy config, NFData)
+  -- ignoring actual names and only relying solely on debruijn indices
+  deriving (Eq) via NamedDeBruijn
+
 instance Wrapped NamedTyDeBruijn
 
 -- | A type name as a de Bruijn index, without the name string.
 newtype TyDeBruijn = TyDeBruijn DeBruijn
-    deriving stock (Show, Generic)
-    deriving newtype (NFData, PrettyBy config)
-    deriving Eq via DeBruijn
+  deriving stock (Show, Generic)
+  deriving newtype (NFData, PrettyBy config)
+  deriving (Eq) via DeBruijn
+
 instance Wrapped TyDeBruijn
 
-instance HasPrettyConfigName config => PrettyBy config NamedDeBruijn where
-    prettyBy config (NamedDeBruijn txt (Index ix))
-        -- See Note [Pretty-printing names with uniques]
-        | showsUnique = pretty . toPrintedName $ txt <> "_i" <> render (pretty ix)
-        | otherwise   = pretty $ toPrintedName txt
-      where
-        PrettyConfigName showsUnique = toPrettyConfigName config
+instance (HasPrettyConfigName config) => PrettyBy config NamedDeBruijn where
+  prettyBy config (NamedDeBruijn txt (Index ix))
+    -- See Note [Pretty-printing names with uniques]
+    | showsUnique = pretty . toPrintedName $ txt <> "_i" <> render (pretty ix)
+    | otherwise = pretty $ toPrintedName txt
+    where
+      PrettyConfigName showsUnique = toPrettyConfigName config
 
-instance HasPrettyConfigName config => PrettyBy config DeBruijn where
-    prettyBy config (DeBruijn (Index ix))
-        | showsUnique = "i" <> pretty ix
-        | otherwise   = ""
-        where PrettyConfigName showsUnique = toPrettyConfigName config
+instance (HasPrettyConfigName config) => PrettyBy config DeBruijn where
+  prettyBy config (DeBruijn (Index ix))
+    | showsUnique = "i" <> pretty ix
+    | otherwise = ""
+    where
+      PrettyConfigName showsUnique = toPrettyConfigName config
 
 class HasIndex a where
-    index :: Lens' a Index
+  index :: Lens' a Index
 
 instance HasIndex NamedDeBruijn where
-    index = lens g s where
-        g = ndbnIndex
-        s n i = n{ndbnIndex=i}
+  index = lens g s
+    where
+      g = ndbnIndex
+      s n i = n{ndbnIndex = i}
 
 instance HasIndex DeBruijn where
-    index = lens g s where
-        g = dbnIndex
-        s n i = n{dbnIndex=i}
+  index = lens g s
+    where
+      g = dbnIndex
+      s n i = n{dbnIndex = i}
 
 instance HasIndex NamedTyDeBruijn where
-    index = _Wrapped' . index
+  index = _Wrapped' . index
 
 instance HasIndex TyDeBruijn where
-    index = _Wrapped' . index
+  index = _Wrapped' . index
 
 -- Converting from normal names to DeBruijn indices, and vice versa
 
@@ -159,11 +164,12 @@ are *relative* - that is, they say how many levels above the *current* level to 
 the binder.
 
 However, when doing conversions it is easier to record the  *absolute* level of a variable,
-in our state, since that way we don't have to adjust our mapping when we go under a binder (whereas
-for relative indices we would need to increment them all by one, as the current level has increased).
+in our state, since that way we don't have to adjust our mapping when we go under a binder
+(whereas for relative indices we would need to increment them all by one, as the current
+level has increased).
 
-However, this means that we *do* need to do an adjustment when we store an index as a level or extract
-a level to use it as an index. The adjustment is fairly straightforward:
+However, this means that we *do* need to do an adjustment when we store an index as a level
+or extract a level to use it as an index. The adjustment is fairly straightforward:
 - An index `i` points to a binder `i` levels above (smaller than) the current level, so the level
   of `i` is `current - i`.
 - A level `l` which is `i` levels above (smaller than) the current level has an index of `i`, so it
@@ -175,85 +181,93 @@ We use a newtype to keep these separate, since getting it wrong will lead to ann
 -- | An absolute level in the program.
 newtype Level = Level Integer deriving newtype (Eq, Ord, Num, Real, Enum, Integral)
 
--- | During visiting the AST we hold a reader "state" of current level and a current scoping (levelMapping).
--- Invariant-A: the current level is positive and greater than all levels in the levelMapping.
--- Invariant-B: only positive levels are stored in the levelMapping.
+{- | During visiting the AST we hold a reader "state" of current level and a current
+scoping (levelMapping).
+Invariant-A: the current level is positive and greater than all levels in the levelMapping.
+Invariant-B: only positive levels are stored in the levelMapping.
+-}
 data Levels = Levels
-            { currentLevel :: Level
-            , levelMapping :: BM.Bimap Unique Level
-            }
+  { currentLevel :: Level
+  , levelMapping :: BM.Bimap Unique Level
+  }
 
 -- | Declare a name with a unique, recording the mapping to a 'Level'.
 declareUnique :: (MonadReader Levels m, HasUnique name unique) => name -> m a -> m a
 declareUnique n =
-    local $ \(Levels current ls) -> Levels current $ BM.insert (n ^. theUnique) current ls
+  local $ \(Levels current ls) -> Levels current $ BM.insert (n ^. theUnique) current ls
 
-{-| Declares a new binder by assigning a fresh unique to the *current level*.
+{- | Declares a new binder by assigning a fresh unique to the *current level*.
 Maintains invariant-B of 'Levels' (that only positive levels are stored),
 since current level is always positive (invariant-A).
 See NOTE: [DeBruijn indices of Binders]
 -}
 declareBinder :: (MonadReader Levels m, MonadQuote m) => m a -> m a
 declareBinder act = do
-    newU <- freshUnique
-    local (\(Levels current ls) -> Levels current $ BM.insert newU current ls) act
+  newU <- freshUnique
+  local (\(Levels current ls) -> Levels current $ BM.insert newU current ls) act
 
--- | Enter a scope, incrementing the current 'Level' by one
--- Maintains invariant-A (that the current level is positive).
-withScope :: MonadReader Levels m => m a -> m a
-withScope = local $ \(Levels current ls) -> Levels (current+1) ls
+{- | Enter a scope, incrementing the current 'Level' by one
+Maintains invariant-A (that the current level is positive).
+-}
+withScope :: (MonadReader Levels m) => m a -> m a
+withScope = local $ \(Levels current ls) -> Levels (current + 1) ls
 
--- | We cannot do a correct translation to or from de Bruijn indices if the program is not well-scoped.
--- So we throw an error in such a case.
+{- | We cannot do a correct translation to or from de Bruijn indices if the program is
+not well-scoped. So we throw an error in such a case.
+-}
 data FreeVariableError
-    = FreeUnique !Unique
-    | FreeIndex !Index
-    deriving stock (Show, Eq, Ord, Generic)
-    deriving anyclass (Exception, NFData)
+  = FreeUnique !Unique
+  | FreeIndex !Index
+  deriving stock (Show, Eq, Ord, Generic)
+  deriving anyclass (Exception, NFData)
 
 instance Pretty FreeVariableError where
-    pretty (FreeUnique u) = "Free unique:" <+> pretty u
-    pretty (FreeIndex i)  = "Free index:" <+> pretty i
+  pretty (FreeUnique u) = "Free unique:" <+> pretty u
+  pretty (FreeIndex i)  = "Free index:" <+> pretty i
 makeClassyPrisms ''FreeVariableError
 
--- | Get the 'Index' corresponding to a given 'Unique'.
--- Uses supplied handler for free names (uniques).
-getIndex :: MonadReader Levels m => Unique -> (Unique -> m Index) -> m Index
+{- | Get the 'Index' corresponding to a given 'Unique'.
+Uses supplied handler for free names (uniques).
+-}
+getIndex :: (MonadReader Levels m) => Unique -> (Unique -> m Index) -> m Index
 getIndex u h = do
-    Levels current ls <- ask
-    case BM.lookup u ls of
-        Just foundlvl -> pure $ levelToIx current foundlvl
-        -- This call should return an index greater than the current level,
-        -- otherwise it will map unbound variables to bound variables.
-        Nothing       ->  h u
+  Levels current ls <- ask
+  case BM.lookup u ls of
+    Just foundlvl -> pure $ levelToIx current foundlvl
+    -- This call should return an index greater than the current level,
+    -- otherwise it will map unbound variables to bound variables.
+    Nothing       -> h u
   where
     -- Compute the relative 'Index' of a absolute 'Level' relative to the current 'Level'.
     levelToIx :: Level -> Level -> Index
     levelToIx (Level current) (Level foundLvl) =
-        -- Thanks to invariant-A, we can be sure that 'level >= foundLvl ', since foundLvl is in the levelMapping
-        -- and thus the computation 'current-foundLvl' is '>=0' and its conversion to Natural will not lead to arithmetic underflow.
-        fromIntegral $ current - foundLvl
+      -- Thanks to invariant-A, we can be sure that 'level >= foundLvl ', since foundLvl
+      -- is in the levelMapping and thus the computation 'current-foundLvl' is '>=0' and
+      -- its conversion to Natural will not lead to arithmetic underflow.
+      fromIntegral $ current - foundLvl
 
--- | Get the 'Unique' corresponding to a given 'Index'.
--- Uses supplied handler for free debruijn indices.
-getUnique :: MonadReader Levels m => Index -> (Index -> m Unique) -> m Unique
+{- | Get the 'Unique' corresponding to a given 'Index'.
+Uses supplied handler for free debruijn indices.
+-}
+getUnique :: (MonadReader Levels m) => Index -> (Index -> m Unique) -> m Unique
 getUnique ix h = do
-    Levels current ls <- ask
-    case BM.lookupR (ixToLevel current ix) ls of
-        -- Because of invariant-B, the levelMapping contains only positive (absolute) levels.
-        Just u  -> pure u
-        -- This call should return a free/unbound unique,
-        -- otherwise it will map unbound variables to bound variables.
-        Nothing ->
-            -- the lookup failed, meaning the index corresponds to a strictly-negative (absolute) level.
-            h ix
+  Levels current ls <- ask
+  case BM.lookupR (ixToLevel current ix) ls of
+    -- Because of invariant-B, the levelMapping contains only positive (absolute) levels.
+    Just u -> pure u
+    -- This call should return a free/unbound unique,
+    -- otherwise it will map unbound variables to bound variables.
+    Nothing ->
+      -- the lookup failed, meaning the index corresponds to a strictly-negative
+      -- (absolute) level.
+      h ix
 
-unNameDeBruijn
-    :: NamedDeBruijn -> DeBruijn
+unNameDeBruijn ::
+  NamedDeBruijn -> DeBruijn
 unNameDeBruijn (NamedDeBruijn _ ix) = DeBruijn ix
 
-unNameTyDeBruijn
-    :: NamedTyDeBruijn -> TyDeBruijn
+unNameTyDeBruijn ::
+  NamedTyDeBruijn -> TyDeBruijn
 unNameTyDeBruijn (NamedTyDeBruijn db) = TyDeBruijn $ unNameDeBruijn db
 
 fakeNameDeBruijn :: DeBruijn -> NamedDeBruijn
@@ -262,66 +276,70 @@ fakeNameDeBruijn = coerce . toFake
 fakeTyNameDeBruijn :: TyDeBruijn -> NamedTyDeBruijn
 fakeTyNameDeBruijn (TyDeBruijn n) = NamedTyDeBruijn $ fakeNameDeBruijn n
 
-nameToDeBruijn
-    :: MonadReader Levels m
-    => (Unique -> m Index)
-    -> Name
-    -> m NamedDeBruijn
+nameToDeBruijn ::
+  (MonadReader Levels m) =>
+  (Unique -> m Index) ->
+  Name ->
+  m NamedDeBruijn
 nameToDeBruijn h (Name str u) = NamedDeBruijn str <$> getIndex u h
 
-tyNameToDeBruijn
-    :: MonadReader Levels m
-    => (Unique -> m Index)
-    -> TyName
-    -> m NamedTyDeBruijn
+tyNameToDeBruijn ::
+  (MonadReader Levels m) =>
+  (Unique -> m Index) ->
+  TyName ->
+  m NamedTyDeBruijn
 tyNameToDeBruijn h (TyName n) = NamedTyDeBruijn <$> nameToDeBruijn h n
 
-deBruijnToName
-    :: MonadReader Levels m
-    => (Index -> m Unique)
-    -> NamedDeBruijn
-    -> m Name
+deBruijnToName ::
+  (MonadReader Levels m) =>
+  (Index -> m Unique) ->
+  NamedDeBruijn ->
+  m Name
 deBruijnToName h (NamedDeBruijn str ix) = Name str <$> getUnique ix h
 
-deBruijnToTyName
-    :: MonadReader Levels m
-    => (Index -> m Unique)
-    -> NamedTyDeBruijn
-    -> m TyName
+deBruijnToTyName ::
+  (MonadReader Levels m) =>
+  (Index -> m Unique) ->
+  NamedTyDeBruijn ->
+  m TyName
 deBruijnToTyName h (NamedTyDeBruijn n) = TyName <$> deBruijnToName h n
 
 -- | The default handler of throwing an error upon encountering a free name (unique).
 freeUniqueThrow :: (AsFreeVariableError e, MonadError e m) => Unique -> m Index
 freeUniqueThrow =
-    throwing _FreeVariableError . FreeUnique
+  throwing _FreeVariableError . FreeUnique
 
 -- | The default handler of throwing an error upon encountering a free debruijn index.
 freeIndexThrow :: (AsFreeVariableError e, MonadError e m) => Index -> m Unique
 freeIndexThrow =
-    throwing _FreeVariableError . FreeIndex
+  throwing _FreeVariableError . FreeIndex
 
-{-| A different implementation of a handler,  where "free" debruijn indices do not throw an error
+{- | A different implementation of a handler,  where "free" debruijn indices do not throw an error
 but are instead gracefully converted to fresh uniques.
-These generated uniques remain free; i.e.  if the original term was open, it will remain open after applying this handler.
+These generated uniques remain free; i.e.  if the original term was open, it will remain open
+after applying this handler.
 These generated free uniques are consistent across the open term (by using a state cache).
 -}
-freeIndexAsConsistentLevel :: (MonadReader Levels m, MonadState (M.Map Level Unique) m, MonadQuote m) => Index -> m Unique
+freeIndexAsConsistentLevel ::
+  (MonadReader Levels m, MonadState (M.Map Level Unique) m, MonadQuote m) =>
+  Index ->
+  m Unique
 freeIndexAsConsistentLevel ix = do
-    cache <- get
-    Levels current _ <- ask
-    -- the absolute level is strictly-negative
-    let absoluteLevel =  ixToLevel current ix
-    case M.lookup absoluteLevel cache of
-        Nothing -> do
-            u <- freshUnique
-            -- the cache contains only strictly-negative levels
-            put (M.insert absoluteLevel u cache)
-            pure u
-        Just u -> pure u
+  cache <- get
+  Levels current _ <- ask
+  -- the absolute level is strictly-negative
+  let absoluteLevel = ixToLevel current ix
+  case M.lookup absoluteLevel cache of
+    Nothing -> do
+      u <- freshUnique
+      -- the cache contains only strictly-negative levels
+      put (M.insert absoluteLevel u cache)
+      pure u
+    Just u -> pure u
 
 -- Compute the absolute 'Level' of a relative 'Index' relative to the current 'Level'.
--- The index `ixAST` may be malformed or point to a free variable because it comes straight from the AST;
--- in such a case, this function may return a negative level.
+-- The index `ixAST` may be malformed or point to a free variable because it comes straight
+-- from the AST; in such a case, this function may return a negative level.
 ixToLevel :: Level -> Index -> Level
 ixToLevel (Level current) ixAST = Level $ current - fromIntegral ixAST
 
--- plutus-core/plutus-core/src/PlutusCore/Parser/ParserCommon.hs
+++ plutus-core/plutus-core/src/PlutusCore/Parser/ParserCommon.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo       #-}
-
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- | Common functions for parsers of UPLC, PLC, and PIR.
-
 module PlutusCore.Parser.ParserCommon where
 
 import Control.Monad (void, when)
@@ -29,31 +27,34 @@ sure to enclose every 'Parser' that doesn't consume trailing whitespce (e.g. 'ta
 'manyTill', 'Lex.decimal' etc) in a call to 'lexeme'.
 -}
 
-newtype ParserState = ParserState { identifiers :: M.Map T.Text Unique }
-    deriving stock (Show)
+newtype ParserState = ParserState {identifiers :: M.Map T.Text Unique}
+  deriving stock (Show)
 
 type Parser =
-    ParsecT ParserError T.Text (StateT ParserState (ReaderT (Maybe Version) Quote))
+  ParsecT ParserError T.Text (StateT ParserState (ReaderT (Maybe Version) Quote))
 
 instance (Stream s, MonadQuote m) => MonadQuote (ParsecT e s m)
 
 initial :: ParserState
 initial = ParserState M.empty
 
--- | Return the unique identifier of a name.
--- If it's not in the current parser state, map the name to a fresh id
--- and add it to the state. Used in the Name parser.
-intern :: (MonadState ParserState m, MonadQuote m)
-    => T.Text -> m Unique
+{- | Return the unique identifier of a name.
+If it's not in the current parser state, map the name to a fresh id
+and add it to the state. Used in the Name parser.
+-}
+intern ::
+  (MonadState ParserState m, MonadQuote m) =>
+  T.Text ->
+  m Unique
 intern n = do
-    st <- get
-    case M.lookup n (identifiers st) of
-        Just u -> return u
-        Nothing -> do
-            fresh <- freshUnique
-            let identifiers' = M.insert n fresh $ identifiers st
-            put $ ParserState identifiers'
-            return fresh
+  st <- get
+  case M.lookup n (identifiers st) of
+    Just u -> return u
+    Nothing -> do
+      fresh <- freshUnique
+      let identifiers' = M.insert n fresh $ identifiers st
+      put $ ParserState identifiers'
+      return fresh
 
 -- | Get the version of the program being parsed, if we know it.
 getVersion :: Parser (Maybe Version)
@@ -63,9 +64,10 @@ getVersion = ask
 withVersion :: Version -> Parser a -> Parser a
 withVersion v = local (const $ Just v)
 
--- | Run an action conditionally based on a predicate on the version.
--- If we don't know the version then the predicate is assumed to be
--- false, i.e. we act if we _know_ the predicate is satisfied.
+{- | Run an action conditionally based on a predicate on the version.
+If we don't know the version then the predicate is assumed to be
+false, i.e. we act if we _know_ the predicate is satisfied.
+-}
 whenVersion :: (Version -> Bool) -> Parser () -> Parser ()
 whenVersion p act = do
   mv <- getVersion
@@ -73,11 +75,15 @@ whenVersion p act = do
     Nothing -> pure ()
     Just v  -> when (p v) act
 
-parse :: (AsParserErrorBundle e, MonadError e m, MonadQuote m) =>
-    Parser a -> String -> T.Text -> m a
+parse ::
+  (AsParserErrorBundle e, MonadError e m, MonadQuote m) =>
+  Parser a ->
+  String ->
+  T.Text ->
+  m a
 parse p file str = do
-    let res = fmap toErrorB (runReaderT (evalStateT (runParserT p file str) initial) Nothing)
-    throwingEither _ParserErrorBundle =<< liftQuote res
+  let res = fmap toErrorB (runReaderT (evalStateT (runParserT p file str) initial) Nothing)
+  throwingEither _ParserErrorBundle =<< liftQuote res
 
 toErrorB :: Either (ParseErrorBundle T.Text ParserError) a -> Either ParserErrorBundle a
 toErrorB (Left err) = Left $ ParseErrorB err
@@ -97,12 +103,13 @@ leadingWhitespace = (whitespace *>)
 trailingWhitespace :: Parser a -> Parser a
 trailingWhitespace = (<* whitespace)
 
--- | Returns a parser for @a@ by calling the supplied function on the starting
--- and ending positions of @a@.
---
--- The supplied function should usually return a parser that does /not/ consume trailing
--- whitespaces. Otherwise, the end position will be the first character after the
--- trailing whitespaces.
+{- | Returns a parser for @a@ by calling the supplied function on the starting
+and ending positions of @a@.
+
+The supplied function should usually return a parser that does /not/ consume trailing
+whitespaces. Otherwise, the end position will be the first character after the
+trailing whitespaces.
+-}
 withSpan' :: (SrcSpan -> Parser a) -> Parser a
 withSpan' f = mdo
   start <- getSourcePos
@@ -111,9 +118,10 @@ withSpan' f = mdo
   let sp = toSrcSpan start end
   pure res
 
--- | Like `withSpan'`, but the result parser consumes whitespaces.
---
--- @withSpan = (<* whitespace) . withSpan'
+{- | Like `withSpan'`, but the result parser consumes whitespaces.
+
+@withSpan = (<* whitespace) . withSpan'
+-}
 withSpan :: (SrcSpan -> Parser a) -> Parser a
 withSpan = (<* whitespace) . withSpan'
 
@@ -134,33 +142,33 @@ inBraces = between (symbol "{") (char '}')
 
 toSrcSpan :: SourcePos -> SourcePos -> SrcSpan
 toSrcSpan start end =
-    SrcSpan
-        { srcSpanFile = sourceName start
-        , srcSpanSLine = unPos (sourceLine start)
-        , srcSpanSCol = unPos (sourceColumn start)
-        , srcSpanELine = unPos (sourceLine end)
-        , srcSpanECol = unPos (sourceColumn end)
-        }
+  SrcSpan
+    { srcSpanFile = sourceName start
+    , srcSpanSLine = unPos (sourceLine start)
+    , srcSpanSCol = unPos (sourceColumn start)
+    , srcSpanELine = unPos (sourceLine end)
+    , srcSpanECol = unPos (sourceColumn end)
+    }
 
 version :: Parser Version
 version = trailingWhitespace $ do
-    x <- Lex.decimal
-    void $ char '.'
-    y <- Lex.decimal
-    void $ char '.'
-    Version x y <$> Lex.decimal
+  x <- Lex.decimal
+  void $ char '.'
+  y <- Lex.decimal
+  void $ char '.'
+  Version x y <$> Lex.decimal
 
 -- | Parses a `Name`. Does not consume leading or trailing whitespaces.
 name :: Parser Name
 name = try $ parseUnquoted <|> parseQuoted
   where
     parseUnquoted = do
-        void $ lookAhead (satisfy isIdentifierStartingChar)
-        str <- takeWhileP (Just "identifier-unquoted") isIdentifierChar
-        Name str <$> intern str
+      void $ lookAhead (satisfy isIdentifierStartingChar)
+      str <- takeWhileP (Just "identifier-unquoted") isIdentifierChar
+      Name str <$> intern str
     parseQuoted = do
-        void $ char '`'
-        void $ lookAhead (satisfy isQuotedIdentifierChar)
-        str <- takeWhileP (Just "identifier-quoted") isQuotedIdentifierChar
-        void $ char '`'
-        Name str <$> intern str
+      void $ char '`'
+      void $ lookAhead (satisfy isQuotedIdentifierChar)
+      str <- takeWhileP (Just "identifier-quoted") isQuotedIdentifierChar
+      void $ char '`'
+      Name str <$> intern str
