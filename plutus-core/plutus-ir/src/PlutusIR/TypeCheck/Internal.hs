6bafb888c7f34dedcc263c0aade65e25e872c928
effectfully <effectfully@gmail.com>
2023-06-12 16:37:53 +0200

    [Refactoring] [TypeCheck] Get rid of 'dummy' nonsense (#5365)

--- plutus-core/plutus-ir/src/PlutusIR/TypeCheck/Internal.hs
+++ plutus-core/plutus-ir/src/PlutusIR/TypeCheck/Internal.hs
@@ -41,6 +41,7 @@ import PlutusCore.TypeCheck.Internal hiding (checkTypeM, inferTypeM, runTypeChec
 
 import Control.Monad (when)
 import Control.Monad.Error.Lens
+import Data.Text qualified as Text
 -- Using @transformers@ rather than @mtl@, because the former doesn't impose the 'Monad' constraint
 -- on 'local'.
 import Control.Lens ((^?))
@@ -125,7 +126,9 @@ checkTypeM
 -- [check| G !- term : vTy]
 checkTypeM ann term vTy = do
     vTermTy <- inferTypeM term
-    when (vTermTy /= vTy) $ throwing _TypeError (TypeMismatch ann (void term) (unNormalized vTy) vTermTy)
+    when (vTermTy /= vTy) $ do
+        let expectedVTy = ExpectedExact $ unNormalized vTy
+        throwing _TypeError $ TypeMismatch ann (void term) expectedVTy vTermTy
 
 -- See the [Global uniqueness] and [Type rules] notes.
 -- | Synthesize the type of a term, returning a normalized type.
@@ -177,7 +180,9 @@ inferTypeM (Apply ann fun arg)      = do
             -- Subparts of a normalized type, so normalized.
             checkTypeM ann arg $ Normalized vDom
             pure $ Normalized vCod
-        _ -> throwing _TypeError (TypeMismatch ann (void fun) (TyFun () dummyType dummyType) vFunTy)
+        _ -> do
+            let expectedTyFun = ExpectedShape "fun k l" ["k", "l"]
+            throwing _TypeError $ TypeMismatch ann (void fun) expectedTyFun vFunTy
 
 -- [infer| G !- body : all (n :: nK) vCod]    [check| G !- ty :: tyK]    ty ~> vTy
 -- -------------------------------------------------------------------------------
@@ -189,7 +194,9 @@ inferTypeM (TyInst ann body ty)     = do
             checkKindM ann ty nK
             vTy <- normalizeTypeM $ void ty
             substNormalizeTypeM vTy n vCod
-        _ -> throwing _TypeError (TypeMismatch ann (void body) (TyForall () dummyTyName dummyKind dummyType) vBodyTy)
+        _ -> do
+            let expectedTyForall = ExpectedShape "all a kind body" ["a", "kind", "body"]
+            throwing _TypeError (TypeMismatch ann (void body) expectedTyForall vBodyTy)
 
 -- [infer| G !- arg :: k]    [check| G !- pat :: (k -> *) -> k -> *]    pat ~> vPat    arg ~> vArg
 -- [check| G !- term : NORM (vPat (\(a :: k) -> ifix vPat a) vArg)]
@@ -213,7 +220,9 @@ inferTypeM (Unwrap ann term)        = do
             k <- inferKindM $ ann <$ vArg
             -- Subparts of a normalized type, so normalized.
             unfoldIFixOf (Normalized vPat) (Normalized vArg) k
-        _                  -> throwing _TypeError (TypeMismatch ann (void term) (TyIFix () dummyType dummyType) vTermTy)
+        _                  -> do
+            let expectedTyIFix = ExpectedShape "ifix pat arg" ["pat", "arg"]
+            throwing _TypeError (TypeMismatch ann (void term) expectedTyIFix vTermTy)
 
 -- [check| G !- ty :: *]    ty ~> vTy
 -- ----------------------------------
@@ -231,7 +240,12 @@ inferTypeM t@(Constr ann resTy i args) = do
 
     -- We don't know exactly what to expect, we only know what the i-th sum should look like, so we
     -- assert that we should have some types in the sum up to there, and then the known product type.
-    let expectedType = TySOP () (replicate ((fromIntegral i) - 1) [dummyType] ++ [replicate (length args) dummyType])
+    let prodPrefix  = map (\j -> "prod_" <> Text.pack (show j)) [0 .. i - 1]
+        fields      = map (\k -> "field_" <> Text.pack (show k)) [0 .. length args - 1]
+        prod_i      = "[" <> Text.intercalate " " fields <> "]"
+        shape       = "sop " <> foldMap (<> " ") prodPrefix <> prod_i <> " ... prod_n"
+        vars        = prodPrefix ++ fields ++ ["prod_n"]
+        expectedSop = ExpectedShape shape vars
     case unNormalized vResTy of
         TySOP _ vSTys -> case vSTys ^? wix i of
             Just pTys -> case zipExact args pTys of
@@ -239,11 +253,11 @@ inferTypeM t@(Constr ann resTy i args) = do
                 Just ps -> for_ ps $ \(arg, pTy) -> checkTypeM ann arg (Normalized pTy)
                 -- the number of args does not match the number of types in the i'th SOP
                 -- alternative
-                Nothing -> throwing _TypeError (TypeMismatch ann (void t) expectedType vResTy)
+                Nothing -> throwing _TypeError (TypeMismatch ann (void t) expectedSop vResTy)
             -- result type does not contain an i'th sum alternative
-            Nothing -> throwing _TypeError (TypeMismatch ann (void t) expectedType vResTy)
+            Nothing -> throwing _TypeError (TypeMismatch ann (void t) expectedSop vResTy)
         -- result type is not a SOP type
-        _ -> throwing _TypeError (TypeMismatch ann (void t) expectedType vResTy)
+        _ -> throwing _TypeError (TypeMismatch ann (void t) expectedSop vResTy)
 
     pure vResTy
 
@@ -259,7 +273,8 @@ inferTypeM (Case ann resTy scrut cases) = do
 
     -- We don't know exactly what to expect, we only know that it should
     -- be a SOP with the right number of sum alternatives
-    let expectedType = TySOP () (replicate (length cases) [dummyType])
+    let prods = map (\j -> "prod_" <> Text.pack (show j)) [0 .. length cases - 1]
+        expectedSop = ExpectedShape (Text.intercalate " " $ "sop" : prods) prods
     case unNormalized vScrutTy of
         TySOP _ sTys -> case zipExact cases sTys of
             Just casesAndArgTypes -> for_ casesAndArgTypes $ \(c, argTypes) ->
@@ -267,9 +282,9 @@ inferTypeM (Case ann resTy scrut cases) = do
                 checkTypeM ann c (Normalized $ mkIterTyFun () argTypes (unNormalized vResTy))
             -- scrutinee does not have a SOP type with the right number of alternatives
             -- for the number of cases
-            Nothing -> throwing _TypeError (TypeMismatch ann (void scrut) expectedType vScrutTy)
+            Nothing -> throwing _TypeError (TypeMismatch ann (void scrut) expectedSop vScrutTy)
         -- scrutinee does not have a SOP type at all
-        _ -> throwing _TypeError (TypeMismatch ann (void scrut) expectedType vScrutTy)
+        _ -> throwing _TypeError (TypeMismatch ann (void scrut) expectedSop vScrutTy)
 
     -- If we got through all that, then every case type is correct, including that
     -- they all result in vResTy, so we can safely conclude that that is the type of the
