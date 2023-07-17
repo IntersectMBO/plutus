ae46aceea52148a2964133e4e9310f730d04a2da
Marty Stumpf <marty.stumpf@iohk.io>
2023-07-17 10:53:31 -0700

    Rename inlineSaturatedApp to inlineApp.

--- plutus-core/plutus-ir/src/PlutusIR/Transform/Inline/CallSiteInline.hs
+++ plutus-core/plutus-ir/src/PlutusIR/Transform/Inline/CallSiteInline.hs
@@ -132,12 +132,12 @@ applyAndBetaReduce rhs args0 = do
   go rhs varArity args0
 
 -- | Consider whether to inline an application.
-inlineSaturatedApp ::
+inlineApp ::
   forall tyname name uni fun ann.
   (InliningConstraints tyname name uni fun) =>
   Term tyname name uni fun ann ->
   InlineM tyname name uni fun ann (Term tyname name uni fun ann)
-inlineSaturatedApp t
+inlineApp t
   | (Var _ann name, args) <- splitApplication t =
       gets (lookupVarInfo name) >>= \case
         Just varInfo -> do
