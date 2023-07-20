c2b90a10425332adeb69a7cae40571c48db437e9
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-07-20 15:07:35 +0100

    Fix tests

--- plutus-ledger-api/test/Spec/Versions.hs
+++ plutus-ledger-api/test/Spec/Versions.hs
@@ -106,8 +106,4 @@ testLanguageVersions :: TestTree
 testLanguageVersions = testGroup "Plutus Core language versions"
   [ testCase "v1.1.0 is available in l3,future and not before" $ do
       assertBool "in l3,Vasil" $ isLeft $ mkTermToEvaluate PlutusV3 vasilPV v110script []
-      assertBool "in l2,future" $ isLeft $ mkTermToEvaluate PlutusV2 conwayPV v110script []
-      assertBool "not in l3,future" $ isRight $ mkTermToEvaluate PlutusV3 conwayPV v110script []
-  , testCase "constr is not available with v1.0.0 ever" $ assertBool "in l3,future" $ isLeft $ mkTermToEvaluate PlutusV3 conwayPV badConstrScript []
-  , testCase "case is not available with v1.0.0 ever" $ assertBool "in l3,future" $ isLeft $ mkTermToEvaluate PlutusV3 conwayPV badCaseScript []
   ]
