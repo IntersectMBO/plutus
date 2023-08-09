5bd9d241ec09a659c0d0c931e6dd15a2b95d3645
effectfully <effectfully@gmail.com>
2023-08-09 11:16:35 +0200

    [Test] Dump types for 'stdlib' and 'examples' (#5450)

---
+++ plutus-core/plutus-core/examples/PlutusCore/Examples/Data/Function.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module PlutusCore.Examples.Data.Function
+    ( unsafeCoerce
+    ) where
+
+import PlutusCore.Core
+import PlutusCore.MkPlc
+import PlutusCore.Name
+import PlutusCore.Quote
+
+import PlutusCore.StdLib.Data.Function
+
+unsafeCoerce :: Term TyName Name uni fun ()
+unsafeCoerce = runQuote $ do
+    a  <- freshTyName "a"
+    b  <- freshTyName "b"
+    return
+        . TyAbs () a (Type ())
+        . TyAbs () b (Type ())
+        . Apply () (mkIterInstNoAnn fix [TyVar () a, TyVar () b])
+        . TyInst () idFun
+        $ TyFun () (TyVar () a) (TyVar () b)
