337e0b7bd32b9f0df61e54e0d3cc49cea7b6bfa0
kwxm <kenneth.mackenzie@iohk.io>
2023-08-25 10:13:09 +0100

    Oops

--- plutus-core/untyped-plutus-core/test/Evaluation/Builtins/BLS12_381/Utils.hs
+++ plutus-core/untyped-plutus-core/test/Evaluation/Builtins/BLS12_381/Utils.hs
@@ -32,7 +32,7 @@ data CekResult =
 
 evalTerm :: PlcTerm -> CekResult
 evalTerm term =
-    case typecheckEvaluateCekNoEmit def defaultBuiltinCostModel term
+    case typecheckEvaluateCekNoEmit PLC.def defaultBuiltinCostModel term
     of Left e -> TypeCheckError e
        Right x  ->
            case x of
