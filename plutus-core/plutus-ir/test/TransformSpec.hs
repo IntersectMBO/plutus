5d1ac2ed274ce43d5be5e4ef614fb29de41ed4ac
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-10-02 10:35:00 +0100

    Tidy LetFloatIn

--- plutus-core/plutus-ir/test/TransformSpec.hs
+++ plutus-core/plutus-ir/test/TransformSpec.hs
@@ -47,9 +47,7 @@ transform :: TestNested
 transform =
     testNested
         "transform"
-        [ letFloatInConservative
-        , letFloatInRelaxed
-        , knownCon
+        [ knownCon
         , recSplit
         , inline
         , nameCapture
@@ -62,52 +60,6 @@ transform =
         , commuteDefaultFun
         , strictifyBindings
         ]
-
-letFloatInConservative :: TestNested
-letFloatInConservative =
-    testNested "letFloatIn/conservative" $
-        map
-            (goldenPirM goldenFloatTC pTerm)
-            [ "avoid-floating-into-lam"
-            , "avoid-floating-into-tyabs"
-            ]
-  where
-    goldenFloatTC pir = rethrow . asIfThrown @(PIR.Error PLC.DefaultUni PLC.DefaultFun ()) $ do
-        let pirFloated = runQuote $ LetFloatIn.floatTerm def False pir
-        -- make sure the floated result typechecks
-        _ <- runQuoteT . flip inferType (() <$ pirFloated) =<< TC.getDefTypeCheckConfig ()
-        -- letmerge is not necessary for floating, but is a nice visual transformation
-        pure $ LetMerge.letMerge pirFloated
-
-letFloatInRelaxed :: TestNested
-letFloatInRelaxed =
-    testNested "letFloatIn/relaxed" $
-        map
-            (goldenPirM goldenFloatTC pTerm)
-            [ "avoid-floating-into-RHS"
-            , "avoid-moving-strict-nonvalue-bindings"
-            , "cannot-float-into-app"
-            , "datatype1"
-            , "datatype2"
-            , "float-into-fun-and-arg-1"
-            , "float-into-fun-and-arg-2"
-            , "float-into-lam1"
-            , "float-into-lam2"
-            , "float-into-RHS"
-            , "float-into-tyabs1"
-            , "float-into-tyabs2"
-            , "float-into-constr"
-            , "float-into-case-arg"
-            , "float-into-case-branch"
-            , "type"
-            ]
-  where
-    goldenFloatTC pir = rethrow . asIfThrown @(PIR.Error PLC.DefaultUni PLC.DefaultFun ()) $ do
-        let pirFloated = runQuote $ LetFloatIn.floatTerm def True pir
-        -- make sure the floated result typechecks
-        _ <- runQuoteT . flip inferType (() <$ pirFloated) =<< TC.getDefTypeCheckConfig ()
-        -- letmerge is not necessary for floating, but is a nice visual transformation
-        pure $ LetMerge.letMerge pirFloated
 
 knownCon :: TestNested
 knownCon =
