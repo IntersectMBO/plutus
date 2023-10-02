075d8f87da65adaf09adcd289e473fe31b818fec
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-10-02 11:59:32 +0100

    Tidy RetainedSize

--- plutus-core/plutus-ir/test/TransformSpec.hs
+++ plutus-core/plutus-ir/test/TransformSpec.hs
@@ -31,7 +31,7 @@ transform :: TestNested
 transform =
     testNested
         "transform"
-        [ retainedSize
+        [
         ]
 
 instance Semigroup PLC.SrcSpan where
@@ -40,37 +40,4 @@ instance Semigroup PLC.SrcSpan where
 instance Monoid PLC.SrcSpan where
     mempty = initialSrcSpan ""
 
-
-retainedSize :: TestNested
-retainedSize =
-    testNested "retainedSize" $
-        map
-            (goldenPir renameAndAnnotate pTerm)
-            [ "typeLet"
-            , "termLet"
-            , "strictLet"
-            , "nonstrictLet"
-            , -- @Maybe@ is referenced, so it retains all of the data type.
-              "datatypeLiveType"
-            , -- @Nothing@ is referenced, so it retains all of the data type.
-              "datatypeLiveConstr"
-            , -- @match_Maybe@ is referenced, so it retains all of the data type.
-              "datatypeLiveDestr"
-            , "datatypeDead"
-            , "singleBinding"
-            , "builtinBinding"
-            , "etaBuiltinBinding"
-            , "etaBuiltinBindingUsed"
-            , "nestedBindings"
-            , "nestedBindingsIndirect"
-            , "recBindingSimple"
-            , "recBindingComplex"
-            ]
-  where
-    displayAnnsConfig = PLC.PrettyConfigClassic PLC.defPrettyConfigName True
-    renameAndAnnotate =
-        PLC.AttachPrettyConfig displayAnnsConfig
-            . RetainedSize.annotateWithRetainedSize def
-            . runQuote
-            . PLC.rename
 
