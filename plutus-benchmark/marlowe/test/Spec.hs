6ab54c56074bbb9d6e301f2506ed9bf369c3f82e
Ziyang Liu <unsafeFixIO@gmail.com>
2023-10-09 01:56:10 -0700

    Add golden size tests for Marlowe validators (#5578)

--- plutus-benchmark/marlowe/test/Spec.hs
+++ plutus-benchmark/marlowe/test/Spec.hs
@@ -12,20 +12,21 @@ import PlutusBenchmark.Marlowe.Scripts.RolePayout (rolePayoutValidator)
 import PlutusBenchmark.Marlowe.Scripts.Semantics (marloweValidator)
 import PlutusBenchmark.Marlowe.Types qualified as M
 import PlutusCore.Default (DefaultFun, DefaultUni)
-import PlutusCore.Test (goldenUEvalBudget)
+import PlutusCore.Test (goldenSize, goldenUEvalBudget)
 import PlutusLedgerApi.V2 (scriptContextTxInfo, txInfoId)
 import PlutusTx.Code (CompiledCode)
+import PlutusTx.Test ()
 import UntypedPlutusCore (NamedDeBruijn)
 import UntypedPlutusCore.Core.Type qualified as UPLC
 
 mkBudgetTest ::
     CompiledCode a
     -> M.Benchmark
-    -> (String, [UPLC.Program NamedDeBruijn DefaultUni DefaultFun ()])
+    -> (String, UPLC.Program NamedDeBruijn DefaultUni DefaultFun ())
 mkBudgetTest validator bm@M.Benchmark{..} =
   let benchName = show $ txInfoId $ scriptContextTxInfo bScriptContext
   in
-    (benchName, [benchmarkToUPLC validator bm])
+    (benchName, benchmarkToUPLC validator bm)
 
 main :: IO ()
 main = do
@@ -40,8 +41,16 @@ main = do
       allTests =
         testGroup "plutus-benchmark Marlowe tests"
             [ runTestNestedIn ["marlowe", "test"] $ testNested "semantics" $
-                map (uncurry goldenUEvalBudget . mkBudgetTest marloweValidator) semanticsMBench
+                goldenSize "semantics" marloweValidator
+                  : [ goldenUEvalBudget name [value]
+                    | bench <- semanticsMBench
+                    , let (name, value) = mkBudgetTest marloweValidator bench
+                    ]
             , runTestNestedIn ["marlowe", "test"] $ testNested "role-payout" $
-                map (uncurry goldenUEvalBudget . mkBudgetTest rolePayoutValidator) rolePayoutMBench
+                goldenSize "role-payout" rolePayoutValidator
+                  : [ goldenUEvalBudget name [value]
+                    | bench <- rolePayoutMBench
+                    , let (name, value) = mkBudgetTest rolePayoutValidator bench
+                    ]
             ]
   defaultMain allTests
