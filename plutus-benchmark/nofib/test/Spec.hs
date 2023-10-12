2cc6d929ca3899f197af2de7055fe3f3b9e93404
kwxm <kenneth.mackenzie@iohk.io>
2023-10-12 14:23:33 +0100

    Versioned golden tests for nofib

--- plutus-benchmark/nofib/test/Spec.hs
+++ plutus-benchmark/nofib/test/Spec.hs
@@ -8,7 +8,7 @@ run to completion. -}
 module Main where
 
 import Test.Tasty
-import Test.Tasty.Extras (TestNested, runTestNestedIn)
+import Test.Tasty.Extras (TestNested, runTestNested, testNestedGhc)
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
@@ -24,9 +24,10 @@ import PlutusCore.Default
 import PlutusTx qualified as Tx
 import PlutusTx.Test qualified as Tx
 
-runTestNested :: TestNested -> TestTree
-runTestNested = runTestNestedIn ["nofib", "test"]
-
+-- Run a collection of golden tests with results stored in folders named
+-- according to the GHC version.
+versionedTestGroup :: [TestNested] -> TestTree
+versionedTestGroup = runTestNested . testNestedGhc "nofib/test"
 
 -- Unit tests comparing PLC and Haskell computations on given inputs
 
@@ -46,9 +47,10 @@ testClausify = testGroup "clausify"
                , testCase "formula3" $ mkClausifyTest Clausify.F3
                , testCase "formula4" $ mkClausifyTest Clausify.F4
                , testCase "formula5" $ mkClausifyTest Clausify.F5
-               , runTestNested $ Tx.goldenPirReadable "clausify-F5" formula5example
-               , runTestNested $ Tx.goldenSize "clausify-F5" formula5example
-               , runTestNested $ Tx.goldenBudget "clausify-F5" formula5example
+               , versionedTestGroup [ Tx.goldenPirReadable "clausify-F5" formula5example
+                                    , Tx.goldenSize "clausify-F5" formula5example
+                                    , Tx.goldenBudget "clausify-F5" formula5example
+                              ]
                ]
     where formula5example = Clausify.mkClausifyCode Clausify.F5
 
@@ -66,9 +68,10 @@ testKnights = testGroup "knights"  -- Odd sizes call "error" because there are n
               , testCase "depth 100, 4x4" $ mkKnightsTest 100 4
               , testCase "depth 100, 6x6" $ mkKnightsTest 100 6
               , testCase "depth 100, 8x8" $ mkKnightsTest 100 8
-              , runTestNested $ Tx.goldenPirReadable "knights10-4x4" knightsExample
-              , runTestNested $ Tx.goldenSize "knights10-4x4" knightsExample
-              , runTestNested $ Tx.goldenBudget "knights10-4x4" knightsExample
+              , versionedTestGroup [ Tx.goldenPirReadable "knights10-4x4" knightsExample
+                                   , Tx.goldenSize "knights10-4x4" knightsExample
+                                   , Tx.goldenBudget "knights10-4x4" knightsExample
+                             ]
               ]
     where knightsExample = Knights.mkKnightsCode 10 4
 
@@ -86,9 +89,10 @@ testQueens = testGroup "queens"
                , testCase "Bjbt1" $ mkQueensTest 4 Queens.Bjbt1
                , testCase "Bjbt2" $ mkQueensTest 4 Queens.Bjbt2
                , testCase "Fc"    $ mkQueensTest 4 Queens.Fc
-               , runTestNested    $ Tx.goldenPirReadable "queens4-bt" queens4btExample
-               , runTestNested    $ Tx.goldenSize "queens4-bt" queens4btExample
-               , runTestNested    $ Tx.goldenBudget "queens4-bt" queens4btExample
+               , versionedTestGroup [ Tx.goldenPirReadable "queens4-bt" queens4btExample
+                                    , Tx.goldenSize "queens4-bt" queens4btExample
+                                    , Tx.goldenBudget "queens4-bt" queens4btExample
+                                    ]
                ]
              , testGroup "5x5"
                [ testCase "Bt"    $ mkQueensTest 5 Queens.Bt
@@ -96,9 +100,10 @@ testQueens = testGroup "queens"
                , testCase "Bjbt1" $ mkQueensTest 5 Queens.Bjbt1
                , testCase "Bjbt2" $ mkQueensTest 5 Queens.Bjbt2
                , testCase "Fc"    $ mkQueensTest 5 Queens.Fc
-               , runTestNested    $ Tx.goldenPirReadable "queens5-fc" queens5fcExample
-               , runTestNested    $ Tx.goldenSize "queens5-fc" queens5fcExample
-               , runTestNested    $ Tx.goldenBudget "queens5-fc" queens5fcExample
+               , versionedTestGroup [ Tx.goldenPirReadable "queens5-fc" queens5fcExample
+                                    , Tx.goldenSize "queens5-fc" queens5fcExample
+                                    , Tx.goldenBudget "queens5-fc" queens5fcExample
+                                    ]
                ]
              ]
     where queens4btExample = Queens.mkQueensCode 4 Queens.Bt
