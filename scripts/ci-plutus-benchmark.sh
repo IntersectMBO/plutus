97b2c107e2fd6d9255dd8f2730f55856efe9f2e2
Lorenzo Calegari <zeme@192.168.1.53>
2023-10-18 14:30:42 +0200

    wip

--- scripts/ci-plutus-benchmark.sh
+++ scripts/ci-plutus-benchmark.sh
@@ -32,48 +32,59 @@ if [ -z "$PR_BRANCH" ] ; then
    echo "[ci-plutus-benchmark]: 'PR_BRANCH' is not set, probably running locally"
 else 
    echo "[ci-plutus-benchmark]: 'PR_BRANCH' set to $PR_BRANCH, fetching origin ..."
+   echo git fetch origin
    git fetch origin 
+   echo git checkout "$PR_BRANCH"
    git checkout "$PR_BRANCH"
 fi
 
 PR_BRANCH_REF="$(git rev-parse --short HEAD)"
 
-echo "[ci-plutus-benchmark]: Processing benchmark comparison for benchmark '$BENCHMARK_NAME' on PR $PR_NUMBER"
+echo "*********************************************"
+echo PR_BRANCH_REF=$PR_BRANCH_REF
+echo git status: 
+git status 
+echo git branch: 
+git branch
+echo git merge-base HEAD origin/master 
+echo ... "$(git merge-base HEAD origin/master)"
 
-echo "[ci-plutus-benchmark]: Running as user:"
-whoami 
+# echo "[ci-plutus-benchmark]: Processing benchmark comparison for benchmark '$BENCHMARK_NAME' on PR $PR_NUMBER"
 
-echo "[ci-plutus-benchmark]: Updating cabal database ..."
-cabal update
+# echo "[ci-plutus-benchmark]: Running as user:"
+# whoami 
 
-echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
-cabal clean
+# echo "[ci-plutus-benchmark]: Updating cabal database ..."
+# cabal update
 
-echo "[ci-plutus-benchmark]: Running benchmark for PR branch at $PR_BRANCH_REF ..."
-2>&1 cabal bench "$BENCHMARK_NAME" | tee bench-PR.log
+# echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
+# cabal clean
 
-echo "[ci-plutus-benchmark]: Switching branches ..."
+# echo "[ci-plutus-benchmark]: Running benchmark for PR branch at $PR_BRANCH_REF ..."
+# 2>&1 cabal bench "$BENCHMARK_NAME" | tee bench-PR.log
+
+# echo "[ci-plutus-benchmark]: Switching branches ..."
 git checkout "$(git merge-base HEAD origin/master)"
 BASE_BRANCH_REF=$(git rev-parse --short HEAD)
 
-echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
-cabal clean
+# echo "[ci-plutus-benchmark]: Clearing caches with cabal clean ..."
+# cabal clean
 
-echo "[ci-plutus-benchmark]: Running benchmark for base branch at $BASE_BRANCH_REF ..."
-2>&1 cabal bench "$BENCHMARK_NAME" | tee bench-base.log 
-git checkout "$PR_BRANCH_REF"  # .. so we use the most recent version of the comparison script
+# echo "[ci-plutus-benchmark]: Running benchmark for base branch at $BASE_BRANCH_REF ..."
+# 2>&1 cabal bench "$BENCHMARK_NAME" | tee bench-base.log 
+# git checkout "$PR_BRANCH_REF"  # .. so we use the most recent version of the comparison script
 
-echo "[ci-plutus-benchmark]: Comparing results ..."
-{
-# The blank line is important, otherwise Github doesn't render markdown in the body of the details element.
-# See https://gist.github.com/ericclemmons/b146fe5da72ca1f706b2ef72a20ac39d for examples
-cat <<EOF 
-Comparing benchmark results of '$BENCHMARK_NAME' on '$BASE_BRANCH_REF' (base) and '$PR_BRANCH_REF' (PR)
+# echo "[ci-plutus-benchmark]: Comparing results ..."
+# {
+# # The blank line is important, otherwise Github doesn't render markdown in the body of the details element.
+# # See https://gist.github.com/ericclemmons/b146fe5da72ca1f706b2ef72a20ac39d for examples
+# cat <<EOF 
+# Comparing benchmark results of '$BENCHMARK_NAME' on '$BASE_BRANCH_REF' (base) and '$PR_BRANCH_REF' (PR)
 
-<details>
-<summary>Results table</summary>
+# <details>
+# <summary>Results table</summary>
 
-EOF
-./plutus-benchmark/bench-compare-markdown bench-base.log bench-PR.log "${BASE_BRANCH_REF:0:7}" "${PR_BRANCH_REF:0:7}"
-echo -e "</details>"
-} > bench-compare-result.log
+# EOF
+# ./plutus-benchmark/bench-compare-markdown bench-base.log bench-PR.log "${BASE_BRANCH_REF:0:7}" "${PR_BRANCH_REF:0:7}"
+# echo -e "</details>"
+# } > bench-compare-result.log
