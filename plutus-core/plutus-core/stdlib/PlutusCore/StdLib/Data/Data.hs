048cf4441ce918ad728901fd91b8ac78783f5019
effectfully <effectfully@gmail.com>
2023-11-10 05:22:23 +0100

    Simplify 'caseData'

--- plutus-core/plutus-core/stdlib/PlutusCore/StdLib/Data/Data.hs
+++ plutus-core/plutus-core/stdlib/PlutusCore/StdLib/Data/Data.hs
@@ -19,64 +19,19 @@ import PlutusCore.MkPlc
 import PlutusCore.Name
 import PlutusCore.Quote
 
-import Data.ByteString (ByteString)
-import PlutusCore.StdLib.Data.Integer
-import PlutusCore.StdLib.Data.Unit
-
 -- | @Data@ as a built-in PLC type.
 dataTy :: uni `HasTypeLevel` Data => Type tyname uni ()
 dataTy = mkTyBuiltin @_ @Data ()
 
 -- | Pattern matching over 'Data' inside PLC.
 --
--- > \(d : data) ->
--- >     /\(r :: *) ->
--- >      \(fConstr : integer -> list data -> r)
--- >       (fMap : list (pair data data) -> r)
--- >       (fList : list data -> r)
--- >       (fI : integer -> r)
--- >       (fB : bytestring -> r) ->
--- >           caseData
--- >               {unit -> r}
--- >               d
--- >               (\(i : integer) (ds : list data) (u : unit) -> fConstr i ds)
--- >               (\(es : list (pair data data)) (u : unit) -> fMap es)
--- >               (\(ds : list data) (u : unit) -> fList ds)
--- >               (\(i : integer) (u : unit) -> fI i)
--- >               (\(b : bytestring) (u : unit) -> fB b)
--- >               unitval
+-- > \(d : data) -> /\(r :: *) -> caseData {r} d
 caseData :: TermLike term TyName Name DefaultUni DefaultFun => term ()
 caseData = runQuote $ do
-    r       <- freshTyName "r"
-    fConstr <- freshName "fConstr"
-    fMap    <- freshName "fMap"
-    fList   <- freshName "fList"
-    fI      <- freshName "fI"
-    fB      <- freshName "fB"
-    d       <- freshName "d"
-    i       <- freshName "i"
-    ds      <- freshName "ds"
-    es      <- freshName "es"
-    b       <- freshName "b"
-    u       <- freshName "u"
-    let bytestring   = mkTyBuiltin @_ @ByteString ()
-        listData     = mkTyBuiltin @_ @[Data] ()
-        listPairData = mkTyBuiltin @_ @[(Data, Data)] ()
+    r <- freshTyName "r"
+    d <- freshName "d"
     return
         . lamAbs () d dataTy
         . tyAbs () r (Type ())
-        . lamAbs () fConstr (TyFun () integer . TyFun () listData $ TyVar () r)
-        . lamAbs () fMap (TyFun () listPairData $ TyVar () r)
-        . lamAbs () fList (TyFun () listData $ TyVar () r)
-        . lamAbs () fI (TyFun () integer $ TyVar () r)
-        . lamAbs () fB (TyFun () bytestring $ TyVar () r)
-        $ mkIterAppNoAnn (tyInst () (builtin () CaseData) . TyFun () unit $ TyVar () r)
-            [ var () d
-            , lamAbs () i integer . lamAbs () ds listData . lamAbs () u unit $
-                mkIterAppNoAnn (var () fConstr) [ var () i, var () ds]
-            , lamAbs () es listPairData . lamAbs () u unit . apply () (var () fMap) $ var () es
-            , lamAbs () ds listData . lamAbs () u unit . apply () (var () fList) $ var () ds
-            , lamAbs () i integer . lamAbs () u unit . apply () (var () fI) $ var () i
-            , lamAbs () b bytestring . lamAbs () u unit . apply () (var () fB) $ var () b
-            , unitval
-            ]
+        . apply () (tyInst () (builtin () CaseData) $ TyVar () r)
+        $ var () d
