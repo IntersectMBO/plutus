0e3c20b536bcd705dd55224b90a79241a23acfee
Ziyang Liu <unsafeFixIO@gmail.com>
2023-11-16 01:34:33 -0800

    Add recordFieldsManual test (#5636)
    * Add recordFieldsManual test
    
    * Use field accessors repeatedly
    
    * 9.6

--- plutus-tx-plugin/test/AsData/Budget/Spec.hs
+++ plutus-tx-plugin/test/AsData/Budget/Spec.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
-
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:context-level=0 #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:defer-errors #-}
 
@@ -14,7 +13,7 @@ import PlutusTx.Builtins qualified as PlutusTx
 import PlutusTx.Code
 import PlutusTx.IsData qualified as PlutusTx
 import PlutusTx.Lift (liftCodeDef)
-import PlutusTx.Test (goldenBudget, goldenEvalCekCatch, goldenPirReadable)
+import PlutusTx.Test (goldenBudget, goldenEvalCekCatch, goldenPirReadable, goldenUPlcReadable)
 import PlutusTx.TH (compile)
 
 import AsData.Budget.Types
@@ -24,14 +23,21 @@ tests =
   testNestedGhc
     ("AsData" </> "Budget")
     [ goldenPirReadable "onlyUseFirstField" onlyUseFirstField
+    , goldenUPlcReadable "onlyUseFirstField" onlyUseFirstField
     , goldenEvalCekCatch "onlyUseFirstField" $ [onlyUseFirstField `unsafeApplyCode` inp]
     , goldenBudget "onlyUseFirstField-budget" (onlyUseFirstField `unsafeApplyCode` inp)
     , goldenPirReadable "patternMatching" patternMatching
+    , goldenUPlcReadable "patternMatching" patternMatching
     , goldenEvalCekCatch "patternMatching" $ [patternMatching `unsafeApplyCode` inp]
     , goldenBudget "patternMatching-budget" (patternMatching `unsafeApplyCode` inp)
     , goldenPirReadable "recordFields" recordFields
+    , goldenUPlcReadable "recordFields" recordFields
     , goldenEvalCekCatch "recordFields" $ [recordFields `unsafeApplyCode` inp]
     , goldenBudget "recordFields-budget" (recordFields `unsafeApplyCode` inp)
+    , goldenPirReadable "recordFields-manual" recordFieldsManual
+    , goldenUPlcReadable "recordFields-manual" recordFieldsManual
+    , goldenEvalCekCatch "recordFields-manual" $ [recordFieldsManual `unsafeApplyCode` inp]
+    , goldenBudget "recordFields-budget-manual" (recordFieldsManual `unsafeApplyCode` inp)
     ]
 
 -- A function that only accesses the first field of `Ints`.
@@ -50,12 +56,25 @@ patternMatching =
         [||
         \d ->
           let (Ints x y z w) = PlutusTx.unsafeFromBuiltinData d
-           in x `PlutusTx.addInteger` y `PlutusTx.addInteger` z `PlutusTx.addInteger` w
+           in x
+                `PlutusTx.addInteger` y
+                `PlutusTx.addInteger` z
+                `PlutusTx.addInteger` w
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (y `PlutusTx.addInteger` z)
+                                          (x `PlutusTx.addInteger` w)
+                                          then x `PlutusTx.addInteger` z
+                                          else y `PlutusTx.addInteger` w
+                                      )
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (z `PlutusTx.addInteger` y)
+                                          (w `PlutusTx.addInteger` x)
+                                          then z `PlutusTx.addInteger` x
+                                          else w `PlutusTx.addInteger` y
+                                      )
         ||]
     )
 
--- TODO: this does the same thing as `patternMatching`, but is much more expensive,
--- since there's no sharing between the code that accesses different fields.
 recordFields :: CompiledCode (PlutusTx.BuiltinData -> Integer)
 recordFields =
   $$( compile
@@ -66,7 +85,61 @@ recordFields =
               y = int2 ints
               z = int3 ints
               w = int4 ints
-           in x `PlutusTx.addInteger` y `PlutusTx.addInteger` z `PlutusTx.addInteger` w
+           in x
+                `PlutusTx.addInteger` y
+                `PlutusTx.addInteger` z
+                `PlutusTx.addInteger` w
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (y `PlutusTx.addInteger` z)
+                                          (x `PlutusTx.addInteger` w)
+                                          then x `PlutusTx.addInteger` z
+                                          else y `PlutusTx.addInteger` w
+                                      )
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (int3 ints `PlutusTx.addInteger` int2 ints)
+                                          (int4 ints `PlutusTx.addInteger` int1 ints)
+                                          then
+                                            int3 ints
+                                              `PlutusTx.addInteger` int1 ints
+                                          else
+                                            int4 ints
+                                              `PlutusTx.addInteger` int2 ints
+                                      )
+        ||]
+    )
+
+-- This is much more efficient than `recordFields` since the manually written
+-- field accessors are more CSE-friendly.
+recordFieldsManual :: CompiledCode (PlutusTx.BuiltinData -> Integer)
+recordFieldsManual =
+  $$( compile
+        [||
+        \d ->
+          let ints = PlutusTx.unsafeFromBuiltinData d
+              x = int1Manual ints
+              y = int2Manual ints
+              z = int3Manual ints
+              w = int4Manual ints
+           in x
+                `PlutusTx.addInteger` y
+                `PlutusTx.addInteger` z
+                `PlutusTx.addInteger` w
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (y `PlutusTx.addInteger` z)
+                                          (x `PlutusTx.addInteger` w)
+                                          then x `PlutusTx.addInteger` z
+                                          else y `PlutusTx.addInteger` w
+                                      )
+                `PlutusTx.addInteger` ( if PlutusTx.lessThanInteger
+                                          (int3Manual ints `PlutusTx.addInteger` int2Manual ints)
+                                          (int4Manual ints `PlutusTx.addInteger` int1Manual ints)
+                                          then
+                                            int3Manual ints
+                                              `PlutusTx.addInteger` int1Manual ints
+                                          else
+                                            int4Manual ints
+                                              `PlutusTx.addInteger` int2Manual ints
+                                      )
         ||]
     )
 
--- plutus-tx-plugin/test/AsData/Budget/Types.hs
+++ plutus-tx-plugin/test/AsData/Budget/Types.hs
@@ -12,6 +12,7 @@
 module AsData.Budget.Types where
 
 import PlutusTx.AsData qualified as AsData
+import PlutusTx.Builtins.Internal qualified as BI
 import PlutusTx.IsData qualified as PlutusTx
 import PlutusTx.Prelude qualified as PlutusTx
 
@@ -25,3 +26,59 @@ AsData.asData
       }
       deriving newtype (PlutusTx.Eq, PlutusTx.FromData, PlutusTx.UnsafeFromData, PlutusTx.ToData)
     |]
+
+-- | This is a manually implemented `AsData`, with field accessors that are
+-- more friendly to CSE. We should generate field accessors like these in TH.
+newtype IntsManual = IntsManual PlutusTx.BuiltinData
+  deriving newtype
+    ( PlutusTx.Eq
+    , PlutusTx.FromData
+    , PlutusTx.UnsafeFromData
+    , PlutusTx.ToData
+    )
+
+int1Manual :: IntsManual -> Integer
+int1Manual (IntsManual d) =
+  let tup = BI.unsafeDataAsConstr d
+      i = BI.fst tup
+      ds0 = BI.snd tup
+      d0 = BI.head ds0
+   in if i PlutusTx.== 0
+        then BI.unsafeDataAsI d0
+        else PlutusTx.error ()
+
+int2Manual :: IntsManual -> Integer
+int2Manual (IntsManual d) =
+  let tup = BI.unsafeDataAsConstr d
+      i = BI.fst tup
+      ds0 = BI.snd tup
+      ds1 = BI.tail ds0
+      d1 = BI.head ds1
+   in if i PlutusTx.== 0
+        then BI.unsafeDataAsI d1
+        else PlutusTx.error ()
+
+int3Manual :: IntsManual -> Integer
+int3Manual (IntsManual d) =
+  let tup = BI.unsafeDataAsConstr d
+      i = BI.fst tup
+      ds0 = BI.snd tup
+      ds1 = BI.tail ds0
+      ds2 = BI.tail ds1
+      d2 = BI.head ds2
+   in if i PlutusTx.== 0
+        then BI.unsafeDataAsI d2
+        else PlutusTx.error ()
+
+int4Manual :: IntsManual -> Integer
+int4Manual (IntsManual d) =
+  let tup = BI.unsafeDataAsConstr d
+      i = BI.fst tup
+      ds0 = BI.snd tup
+      ds1 = BI.tail ds0
+      ds2 = BI.tail ds1
+      ds3 = BI.tail ds2
+      d3 = BI.head ds3
+   in if i PlutusTx.== 0
+        then BI.unsafeDataAsI d3
+        else PlutusTx.error ()
