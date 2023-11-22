e2cbee0d31da1b2dfa42cc76fb112dc69fa06798
Ziyang Liu <unsafeFixIO@gmail.com>
2023-11-22 13:22:26 -0800

    1.15.0.1 (#5651)
    This release contains backport of the change to PlutusLedgerApi.V1.Value (adding `-fexpose-all-unfoldings`) from #5587.

--- plutus-ledger-api/src/PlutusLedgerApi/V1/Value.hs
+++ plutus-ledger-api/src/PlutusLedgerApi/V1/Value.hs
@@ -12,6 +12,10 @@
 {-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
 {-# OPTIONS_GHC -fno-spec-constr #-}
 {-# OPTIONS_GHC -fno-specialise #-}
+{-# OPTIONS_GHC -fexpose-all-unfoldings #-}
+-- We need -fexpose-all-unfoldings to compile the Marlowe validator
+-- with GHC 9.6.2.
+-- TODO. Look into this more closely: see PLT-7976.
 
 -- | Functions for working with 'Value'.
 module PlutusLedgerApi.V1.Value(
