b617c8b634c7fef73c5dc1cfec2c0659ee8e9a60
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2024-01-09 22:21:41 +0100

    Move interface for extracted Coq tests and corresponding stub

--- plutus-core/plutus-ir/cert/tests-stub/PlutusIR/Certifier/Tests.hs
+++ plutus-core/plutus-ir/cert/tests-stub/PlutusIR/Certifier/Tests.hs
@@ -4,7 +4,7 @@
 -- | Interface to automatically extracted code from plutus-cert (Coq):
 -- - Conversion functions for PIR's AST to the representation in Coq.
 -- - Boolean decision procedures for translation relations
-module PlutusIR.Certifier (is_dead_code, is_unique) where
+module PlutusIR.Certifier.Tests (is_dead_code, is_unique) where
 
 import PlutusCore qualified as P
 import PlutusIR qualified as PIR
--- plutus-core/plutus-ir/cert/tests/PlutusIR/Certifier/Tests.hs
+++ plutus-core/plutus-ir/cert/tests/PlutusIR/Certifier/Tests.hs
@@ -7,7 +7,7 @@
 -- | Interface to automatically extracted code from plutus-cert (Coq):
 -- - Conversion functions for PIR's AST to the representation in Coq.
 -- - Boolean decision procedures for translation relations
-module PlutusIR.Certifier (is_dead_code, is_unique) where
+module PlutusIR.Certifier.Tests (is_dead_code, is_unique) where
 
 import PlutusCore qualified as P (DefaultFun (..), DefaultUni (..), Some (..), SomeTypeIn (..),
                                   ValueOf (..))
