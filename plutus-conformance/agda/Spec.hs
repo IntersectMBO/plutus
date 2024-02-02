7be1c9ca4f23b38b4f861cfc611a6b2df25afee2
Mauro Jaskelioff <mjaskelioff@gmail.com>
2024-02-02 12:21:05 -0300

    agdaEvalUplcProg with constant parameters

--- plutus-conformance/agda/Spec.hs
+++ plutus-conformance/agda/Spec.hs
@@ -4,66 +4,67 @@
 module Main (main) where
 
 import Control.Monad.Trans.Except
-import MAlonzo.Code.Evaluator.Term (runUAgda, runUCountingAgda)
+import MAlonzo.Code.Evaluator.Term (runUCountingAgda)
 import PlutusConformance.Common
 import PlutusCore (Error (..))
 import PlutusCore.Default
+import PlutusCore.Evaluation.Machine.CostModelInterface
 import PlutusCore.Evaluation.Machine.ExBudget
 import PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCekMachineCosts)
 import PlutusCore.Evaluation.Machine.ExMemory
 import PlutusCore.Evaluation.Machine.SimpleBuiltinCostModel
+
 import PlutusCore.Quote
 import UntypedPlutusCore qualified as UPLC
 import UntypedPlutusCore.DeBruijn
 
--- | Our `evaluator` for the Agda UPLC tests is the CEK machine without costs.
-agdaEvalUplcProgNoCost :: UplcEvaluator
-agdaEvalUplcProgNoCost = UplcEvaluatorWithoutCosting $ \(UPLC.Program () version tmU) ->
-    let
-        -- turn it into an untyped de Bruijn term
-        tmUDB :: ExceptT FreeVariableError Quote (UPLC.Term NamedDeBruijn DefaultUni DefaultFun ())
-        tmUDB = deBruijnTerm tmU
-    in
-    case runQuote $ runExceptT $ withExceptT FreeVariableErrorE tmUDB of
-        -- if there's an exception, evaluation failed, should return `Nothing`.
-        Left _ -> Nothing
-        -- evaluate the untyped term with CEK
-        Right tmUDBSuccess ->
-            case runUAgda tmUDBSuccess of
-                Left _ -> Nothing
-                Right tmEvaluated ->
-                    let tmNamed = runQuote $ runExceptT $
-                            withExceptT FreeVariableErrorE $ unDeBruijnTerm tmEvaluated
-                    in
-                    -- turn it back into a named term
-                    case tmNamed of
-                        Left _          -> Nothing
-                        Right namedTerm -> Just $ UPLC.Program () version namedTerm
+-- -- | Our `evaluator` for the Agda UPLC tests is the CEK machine without costs.
+-- agdaEvalUplcProgNoCost :: UplcEvaluator
+-- agdaEvalUplcProgNoCost = UplcEvaluatorWithoutCosting $ \(UPLC.Program () version tmU) ->
+--     let
+--         -- turn it into an untyped de Bruijn term
+--         tmUDB :: ExceptT FreeVariableError Quote (UPLC.Term NamedDeBruijn DefaultUni DefaultFun ())
+--         tmUDB = deBruijnTerm tmU
+--     in
+--     case runQuote $ runExceptT $ withExceptT FreeVariableErrorE tmUDB of
+--         -- if there's an exception, evaluation failed, should return `Nothing`.
+--         Left _ -> Nothing
+--         -- evaluate the untyped term with CEK
+--         Right tmUDBSuccess ->
+--             case runUAgda tmUDBSuccess of
+--                 Left _ -> Nothing
+--                 Right tmEvaluated ->
+--                     let tmNamed = runQuote $ runExceptT $
+--                             withExceptT FreeVariableErrorE $ unDeBruijnTerm tmEvaluated
+--                     in
+--                     -- turn it back into a named term
+--                     case tmNamed of
+--                         Left _          -> Nothing
+--                         Right namedTerm -> Just $ UPLC.Program () version namedTerm
 
 -- | Our `evaluator` for the Agda UPLC tests is the CEK machine with costs.
 agdaEvalUplcProg :: UplcEvaluator
-agdaEvalUplcProg = UplcEvaluatorWithCosting $ \(UPLC.Program () version tmU) ->
+agdaEvalUplcProg = UplcEvaluatorWithCosting $ \ modelParams (UPLC.Program () version tmU) ->
     let
         -- turn it into an untyped de Bruijn term
         tmUDB :: ExceptT FreeVariableError Quote (UPLC.Term NamedDeBruijn DefaultUni DefaultFun ())
         tmUDB = deBruijnTerm tmU
+
+        {- TODO: In processModelParams construct the pair in the result from the argument.
+          Here, we ignore the argument and just return a default set.
+        -}
+        processModelParams  :: CostModelParams -> (CekMachineCosts ,  BuiltinCostMap)
+        processModelParams _ = (defaultCekMachineCosts , defaultSimpleBuiltinCostModel)
+
+        defaultModelParams :: (CekMachineCosts ,  BuiltinCostMap)
+        defaultModelParams = processModelParams modelParams
     in
     case runQuote $ runExceptT $ withExceptT FreeVariableErrorE tmUDB of
         -- if there's an exception, evaluation failed, should return `Nothing`.
         Left _ -> Nothing
         -- evaluate the untyped term with CEK
         Right tmUDBSuccess ->
-             {- In the following hole we need to plug in a BuiltinCostMap
-             type BuiltinCostMap = [(Text, CpuAndMemoryModel)]
-
-            see `plutus-metatheory/src/Cost/JSON.hs`
-
-            but currently we get it from a json file in the plutus-metatheory project
-            (which itself is copied from a file in the plutus-core project).
-            We need to wait for a fix in which plutus-core exports this map without IO (using Template Haskell).
-
-             -}
-            case runUCountingAgda (defaultCekMachineCosts , defaultSimpleBuiltinCostModel) tmUDBSuccess of
+            case runUCountingAgda defaultModelParams tmUDBSuccess of
                 Left _ -> Nothing
                 Right (tmEvaluated,(cpuCost,memCost)) ->
                     let tmNamed = runQuote $ runExceptT $
