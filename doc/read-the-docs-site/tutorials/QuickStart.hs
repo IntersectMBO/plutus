3352bb40f998aa8b2f16eb16c4c6900f9cb06e01
Ana Pantilie <ana.pantilie95@gmail.com>
2024-02-05 15:53:49 +0200

    Add required language extension to QuickStart
    Signed-off-by: Ana Pantilie <ana.pantilie95@gmail.com>

--- doc/read-the-docs-site/tutorials/QuickStart.hs
+++ doc/read-the-docs-site/tutorials/QuickStart.hs
@@ -1,4 +1,6 @@
 -- BLOCK1
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Main where
 
 import AuctionValidator
@@ -9,16 +11,17 @@ import PlutusLedgerApi.V2 qualified as V2
 
 main :: IO ()
 main = B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScript
-  where
-    script = auctionValidatorScript params
-    serialisedScript = V2.serialiseCompiledCode script
-    params =
-      AuctionParams
-        { apSeller = error "Replace with seller's wallet address"
-        , -- The asset to be auctioned is 10000 lovelaces
-          apAsset = V2.singleton V2.adaSymbol V2.adaToken 10000
-        , -- The minimum bid is 100 lovelaces
-          apMinBid = 100
-        , apEndTime = error "Replace with your desired end time"
-        }
+ where
+  script = auctionValidatorScript params
+  serialisedScript = V2.serialiseCompiledCode script
+  params =
+    AuctionParams
+      { apSeller = error "Replace with seller's wallet address"
+      , -- The asset to be auctioned is 10000 lovelaces
+        apAsset = V2.singleton V2.adaSymbol V2.adaToken 10000
+      , -- The minimum bid is 100 lovelaces
+        apMinBid = 100
+      , apEndTime = error "Replace with your desired end time"
+      }
+
 -- BLOCK2
