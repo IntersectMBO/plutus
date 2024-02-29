fac90d8da5e4266840b308ed04af890f996b4021
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-02-29 12:38:32 +0100

    refactor: remove unnecessary ghc option pragmas

--- plutus-tx-plugin/test/Blueprint/Tests/Lib.hs
+++ plutus-tx-plugin/test/Blueprint/Tests/Lib.hs
@@ -9,16 +9,6 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE ViewPatterns          #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# OPTIONS_GHC -fno-full-laziness #-}
-{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
-{-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
-{-# OPTIONS_GHC -fno-spec-constr #-}
-{-# OPTIONS_GHC -fno-specialise #-}
-{-# OPTIONS_GHC -fno-strictness #-}
-{-# OPTIONS_GHC -fno-unbox-small-strict-fields #-}
-{-# OPTIONS_GHC -fno-unbox-strict-fields #-}
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.1.0 #-}
 
 module Blueprint.Tests.Lib where
 
