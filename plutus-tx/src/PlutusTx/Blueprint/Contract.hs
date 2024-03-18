5526150420232a3edd4ad0a13f4683308aebe9b5
Yura Lazarev <Unisay@users.noreply.github.com>
2024-03-18 14:05:33 +0100

    CIP-0057: Alternative approach to deriving and referencing schema definitons. (#5839)
    * Add pointer to issue on the ledger's cost model interface (#5828)
    
    * Agda html (#5827)
    
    * Moved all Agda files to Markdown.
    
    * Added some brief text to the TOC
    
    * Fixed the file extension in Setup.hs for the Markdown Agda.
    
    * Added all possible file extensions and the preprocessor seems to run now.
    
    * Cleaned out .lagda from the cabal file, since everything is .lagda.md now.
    
    * chore(deps): bump tj-actions/changed-files from 42 to 43 (#5835)
    
    Bumps [tj-actions/changed-files](https://github.com/tj-actions/changed-files) from 42 to 43.
    - [Release notes](https://github.com/tj-actions/changed-files/releases)
    - [Changelog](https://github.com/tj-actions/changed-files/blob/main/HISTORY.md)
    - [Commits](https://github.com/tj-actions/changed-files/compare/v42...v43)
    
    ---
    updated-dependencies:
    - dependency-name: tj-actions/changed-files
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Kwxm/correct conformance comments (#5829)
    
    * Fix comments in conformance tests
    
    * Fix comments in conformance tests
    
    * Fix comments in conformance tests
    
    * CIP-57: unroll top level types
    
    * Extract `goldenJson` function and move it to the `Tests.Lib`
    
    * Note about unrolling types
    
    * CIP-57: accumulate schema definitions preserving types for safe refs.
    
    * Build schema definitions by un-rolling a list of top-level types.
    
    * validatorParameters :: []
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Kenneth MacKenzie <kenneth.mackenzie@iohk.io>
    Co-authored-by: Ramsay Taylor <ramsay@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- plutus-tx/src/PlutusTx/Blueprint/Contract.hs
+++ plutus-tx/src/PlutusTx/Blueprint/Contract.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GADTs              #-}
 {-# LANGUAGE KindSignatures     #-}
 {-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE RecordWildCards    #-}
@@ -12,78 +13,36 @@ import Data.Aeson (ToJSON (..), (.=))
 import Data.Aeson qualified as Aeson
 import Data.Aeson.Extra (optionalField, requiredField)
 import Data.Aeson.Extra qualified as Aeson
-import Data.Kind (Type)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Set (Set)
 import Data.Text (Text)
 import PlutusPrelude (ensure)
-import PlutusTx.Blueprint.Definition (DefinitionId, UnrollAll)
+import PlutusTx.Blueprint.Definition (DefinitionId, Definitions, definitionsToMap)
 import PlutusTx.Blueprint.Preamble (Preamble)
-import PlutusTx.Blueprint.Schema (Schema)
 import PlutusTx.Blueprint.Validator (ValidatorBlueprint)
 
 {- | A blueprint of a smart contract, as defined by the CIP-0057
 
-The 'referencedTypes' phantom type parameter is used to track the types used in the contract
+The 'referencedTypes' type variable is used to track the types used in the contract
 making sure their schemas are included in the blueprint and that they are referenced
-in a type-safe way.
+in a type-safe way. See the note ["Unrolling" types] for more details.
 -}
-data ContractBlueprint (referencedTypes :: [Type]) = MkContractBlueprint
-  { contractId          :: Maybe Text
-  -- ^ An optional identifier for the contract.
-  , contractPreamble    :: Preamble
-  -- ^ An object with meta-information about the contract.
-  , contractValidators  :: Set (ValidatorBlueprint referencedTypes)
-  -- ^ A set of validator blueprints that are part of the contract.
-  , contractDefinitions :: Map DefinitionId (Schema referencedTypes)
-  -- ^ A registry of schema definitions used across the blueprint.
-  }
-  deriving stock (Show)
+data ContractBlueprint where
+  MkContractBlueprint ::
+    forall referencedTypes.
+    { contractId :: Maybe Text
+    -- ^ An optional identifier for the contract.
+    , contractPreamble :: Preamble
+    -- ^ An object with meta-information about the contract.
+    , contractValidators :: Set (ValidatorBlueprint referencedTypes)
+    -- ^ A set of validator blueprints that are part of the contract.
+    , contractDefinitions :: Definitions referencedTypes
+    -- ^ A registry of schema definitions used across the blueprint.
+    } ->
+    ContractBlueprint
 
-{- Note ["Unrolling" types]
-
-ContractBlueprint needs to be parameterized by a list of types used in
-a contract's type signature (including nested types) in order to:
-  a) produce a JSON-schema definition for every type used.
-  b) ensure that the schema definitions are referenced in a type-safe way.
-
-Given the following contract validator's type signature:
-
-  typedValidator :: Redeemer -> Datum -> ScriptContext -> Bool
-
-and the following data type definitions:
-
-  data Redeemer = MkRedeemer MyStruct
-  data MyStruct = MkMyStruct { field1 :: Integer, field2 :: Bool }
-  type Datum = ()
-
-The ContractBlueprint type should be:
-
-  ContractBlueprint '[Redeemer, MyStruct, Integer, Bool, ()]
-
-However, for contract blurprints authors specifying all the nested types manually is
-cumbersome and error-prone. To make it easier to work with, we provide the Unroll type family
-that can be used to traverse a type accumulating all types nested within it:
-
-  Unroll Redeemer ~ '[Redeemer, MyStruct, Integer, Bool]
-  UnrollAll '[Redeemer, Datum] ~ '[Redeemer, MyStruct, Integer, Bool, ()]
-
-This way blueprint authors can specify the top-level types used in a contract and the UnrollAll
-type family will take care of discovering all the nested types:
-
-  Blueprint '[Redeemer, Datum]
-
-  is equivalent to
-
-  ContractBlueprint '[Redeemer, MyStruct, Integer, Bool, ()]
-
--}
-
--- | A contract blueprint with all (nested) types discovered from a list of top-level types.
-type Blueprint topTypes = ContractBlueprint (UnrollAll topTypes)
-
-instance ToJSON (ContractBlueprint referencedTypes) where
+instance ToJSON ContractBlueprint where
   toJSON MkContractBlueprint{..} =
     Aeson.buildObject $
       requiredField "$schema" schemaUrl
@@ -99,7 +58,10 @@ instance ToJSON (ContractBlueprint referencedTypes) where
         . requiredField "preamble" contractPreamble
         . requiredField "validators" contractValidators
         . optionalField "$id" contractId
-        . optionalField "definitions" (ensure (not . Map.null) contractDefinitions)
+        . optionalField "definitions" definitions
    where
     schemaUrl :: String
     schemaUrl = "https://cips.cardano.org/cips/cip57/schemas/plutus-blueprint.json"
+
+    definitions :: Maybe (Map DefinitionId Aeson.Value)
+    definitions = ensure (not . Map.null) (definitionsToMap contractDefinitions toJSON)
