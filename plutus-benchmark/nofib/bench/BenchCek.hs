a855ec01490132f5929ac0ba6e230dcfd7842be2
effectfully <effectfully@gmail.com>
2024-03-29 15:21:30 +0100

    Try things

--- plutus-benchmark/nofib/bench/BenchCek.hs
+++ plutus-benchmark/nofib/bench/BenchCek.hs
@@ -5,6 +5,8 @@ module Main where
 
 import Shared (benchWith, evaluateCekLikeInProd, mkEvalCtx)
 
+import UntypedPlutusCore.Evaluation.Machine.Cek (unsafeExtractEvaluationResult)
+
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
 import Criterion (whnf)
@@ -14,7 +16,6 @@ main = do
   evalCtx <- evaluate $ force mkEvalCtx
   let mkCekBM term =
           -- `force` to try to ensure that deserialiation is not included in benchmarking time.
-          let !benchTerm = force term
-              eval = either (error . show) (\_ -> ()) . evaluateCekLikeInProd evalCtx
-          in whnf eval benchTerm
+          let eval = unsafeExtractEvaluationResult . evaluateCekLikeInProd evalCtx
+          in whnf eval term
   benchWith mkCekBM
