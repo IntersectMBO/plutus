c2571d75c8e7ffdf852d18546693519740481033
kwxm <kenneth.mackenzie@iohk.io>
2024-04-09 05:09:24 +0100

    Better initial values for some folds

--- plutus-benchmark/bls12-381-costs/src/PlutusBenchmark/BLS12_381/Scripts.hs
+++ plutus-benchmark/bls12-381-costs/src/PlutusBenchmark/BLS12_381/Scripts.hs
@@ -92,9 +92,8 @@ byteStringToIntegerLE = Tx.byteStringToInteger LittleEndian
 
 {-# INLINABLE hashAndAddG1 #-}
 hashAndAddG1 :: [BuiltinByteString] -> BuiltinBLS12_381_G1_Element
-hashAndAddG1 [] = Tx.bls12_381_G1_uncompress Tx.bls12_381_G1_compressed_zero
-hashAndAddG1 (p:ps) =
-    go ps (Tx.bls12_381_G1_hashToGroup p emptyByteString)
+hashAndAddG1 l =
+    go l (Tx.bls12_381_G1_uncompress Tx.bls12_381_G1_compressed_zero)
     where go [] !acc     = acc
           go (q:qs) !acc = go qs $ Tx.bls12_381_G1_add (Tx.bls12_381_G1_hashToGroup q emptyByteString) acc
 
@@ -106,9 +105,8 @@ mkHashAndAddG1Script l =
 -- Hash some bytestrings onto G2 and add them all together
 {-# INLINABLE hashAndAddG2 #-}
 hashAndAddG2 :: [BuiltinByteString] -> BuiltinBLS12_381_G2_Element
-hashAndAddG2 [] = Tx.bls12_381_G2_uncompress Tx.bls12_381_G2_compressed_zero
-hashAndAddG2 (p:ps) =
-    go ps (Tx.bls12_381_G2_hashToGroup p emptyByteString)
+hashAndAddG2 l =
+    go l (Tx.bls12_381_G2_uncompress Tx.bls12_381_G2_compressed_zero)
     where go [] !acc     = acc
           go (q:qs) !acc = go qs $ Tx.bls12_381_G2_add (Tx.bls12_381_G2_hashToGroup q emptyByteString) acc
 
@@ -120,9 +118,8 @@ mkHashAndAddG2Script l =
 -- Uncompress a list of compressed G1 points and add them all together
 {-# INLINABLE uncompressAndAddG1 #-}
 uncompressAndAddG1 :: [BuiltinByteString] -> BuiltinBLS12_381_G1_Element
-uncompressAndAddG1 [] = Tx.bls12_381_G1_uncompress Tx.bls12_381_G1_compressed_zero
-uncompressAndAddG1 (p:ps) =
-    go ps (Tx.bls12_381_G1_uncompress p)
+uncompressAndAddG1 l =
+    go l (Tx.bls12_381_G1_uncompress Tx.bls12_381_G1_compressed_zero)
     where go [] acc     = acc
           go (q:qs) acc = go qs $ Tx.bls12_381_G1_add (Tx.bls12_381_G1_uncompress q) acc
 
@@ -135,9 +132,8 @@ mkUncompressAndAddG1Script l =
 -- Uncompress a list of compressed G1 points and add them all together
 {-# INLINABLE uncompressAndAddG2 #-}
 uncompressAndAddG2 :: [BuiltinByteString] -> BuiltinBLS12_381_G2_Element
-uncompressAndAddG2 [] = Tx.bls12_381_G2_uncompress Tx.bls12_381_G2_compressed_zero
-uncompressAndAddG2 (p:ps) =
-    go ps (Tx.bls12_381_G2_uncompress p)
+uncompressAndAddG2 l =
+    go l (Tx.bls12_381_G2_uncompress Tx.bls12_381_G2_compressed_zero)
     where go [] acc     = acc
           go (q:qs) acc = go qs $ Tx.bls12_381_G2_add (Tx.bls12_381_G2_uncompress q) acc
 
