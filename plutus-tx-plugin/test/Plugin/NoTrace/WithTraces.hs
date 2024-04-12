4f621060fca61a37fb42d5acf18dbaccb9d49a18
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-04-12 15:22:02 +0200

    Add some more complex test cases for the remove-trace plugin option

--- plutus-tx-plugin/test/Plugin/NoTrace/WithTraces.hs
+++ plutus-tx-plugin/test/Plugin/NoTrace/WithTraces.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TypeApplications  #-}
 {-# OPTIONS_GHC -fplugin PlutusTx.Plugin #-}
 -- The plugin uses non-conservative optimizations by default and they remove some traces.
@@ -11,30 +9,27 @@
 
 module Plugin.NoTrace.WithTraces where
 
-import PlutusTx (CompiledCode, compile)
-import PlutusTx.Builtins qualified as B
-import PlutusTx.Prelude qualified as P
+import Data.Proxy (Proxy (..))
+import Plugin.NoTrace.Lib qualified as Lib
+import PlutusTx.Bool (Bool)
+import PlutusTx.Builtins (BuiltinString, Integer)
+import PlutusTx.Code (CompiledCode)
+import PlutusTx.Plugin (plc)
 
-trace :: CompiledCode (B.BuiltinString -> ())
-trace = $$(compile [||\(x :: B.BuiltinString) -> B.trace x ()||])
+traceArgument :: CompiledCode (BuiltinString -> ())
+traceArgument = plc (Proxy @"traceArgument") Lib.traceArgument
+
+traceShow :: CompiledCode ()
+traceShow = plc (Proxy @"traceShow") Lib.traceShow
 
 traceDirect :: CompiledCode ()
-traceDirect = $$(compile [||B.trace "test" ()||])
+traceDirect = plc (Proxy @"traceDirect") Lib.traceDirect
 
-tracePrelude :: CompiledCode Integer
-tracePrelude = $$(compile [||P.trace "test" (1 :: Integer)||])
+traceNonConstant :: CompiledCode (BuiltinString -> BuiltinString)
+traceNonConstant = plc (Proxy @"traceNonConstant") Lib.traceNonConstant
 
 traceComplex :: CompiledCode (Bool -> ())
-traceComplex =
-  $$(compile [||\(b :: Bool) -> if b then P.trace "yes" () else P.traceError "no" ()||])
+traceComplex = plc (Proxy @"traceComplex") Lib.traceComplex
 
-traceRepeatedly :: CompiledCode P.Integer
-traceRepeatedly =
-  $$( compile
-        [||
-        let i1 = P.trace "Making my first int" (1 :: P.Integer)
-            i2 = P.trace "Making my second int" (2 :: P.Integer)
-            i3 = P.trace "Adding them up" (i1 P.+ i2)
-         in i3
-        ||]
-    )
+traceRepeatedly :: CompiledCode Integer
+traceRepeatedly = plc (Proxy @"traceRepeatedly") Lib.traceRepeatedly
