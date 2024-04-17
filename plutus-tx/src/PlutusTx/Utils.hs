07a889e3f250b09df77605678936c33bdae1e614
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2024-04-17 13:13:53 +0100

    Add Delay type and use it
    Fixes #5908.
    
    Unsure where to put this, it could go in its own module I guess.

--- plutus-tx/src/PlutusTx/Utils.hs
+++ plutus-tx/src/PlutusTx/Utils.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE RankNTypes       #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-unused-foralls #-}
 module PlutusTx.Utils where
 
 -- We do not use qualified import because the whole module contains off-chain code
+import PlutusTx.Builtins.Internal qualified as BI
 import Prelude as Haskell
 
 mustBeReplaced :: String -> a
@@ -8,3 +12,10 @@ mustBeReplaced placeholder =
   error $
     "The " <> show placeholder <> " placeholder must have been replaced by the \
       \core-to-plc plugin during compilation."
+
+-- | Delay evalaution of the expression inside the 'Delay' constructor.
+newtype Delay a = Delay (forall b. a)
+
+-- | Force the evaluation of the expression delayed by the 'Delay'.
+force :: Delay a -> a
+force (Delay a) = a @BI.BuiltinUnit
