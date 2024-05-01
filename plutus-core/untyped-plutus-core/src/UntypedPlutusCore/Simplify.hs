61a857e9c5c17279765312bfdfec95ae4dd32e2f
Ana Pantilie <ana.pantilie95@gmail.com>
2024-05-01 23:21:55 +0300

    Experiment: print Agda-readable ASTs from UPLC simplifier
    Signed-off-by: Ana Pantilie <ana.pantilie95@gmail.com>

--- plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Simplify.hs
+++ plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Simplify.hs
@@ -13,7 +13,9 @@ import PlutusCore.Default qualified as PLC
 import PlutusCore.Default.Builtins
 import PlutusCore.Name.Unique
 import UntypedPlutusCore.Core.Type
+import UntypedPlutusCore.DeBruijn (FreeVariableError, deBruijnTerm)
 import UntypedPlutusCore.Simplify.Opts as Opts
+import UntypedPlutusCore.TempUntyped (UTerm, conv)
 import UntypedPlutusCore.Transform.CaseOfCase
 import UntypedPlutusCore.Transform.CaseReduce
 import UntypedPlutusCore.Transform.Cse
@@ -24,6 +26,7 @@ import UntypedPlutusCore.Transform.Inline (InlineHints (..), inline)
 import Control.Monad
 import Data.List
 import Data.Typeable
+import Debug.Trace (trace)
 
 simplifyProgram ::
     forall name uni fun m a.
@@ -64,9 +67,18 @@ simplifyTerm opts builtinSemanticsVariant =
         >=> inline (_soInlineConstants opts) (_soInlineHints opts) builtinSemanticsVariant
 
     caseOfCase' :: Term name uni fun a -> Term name uni fun a
-    caseOfCase' = case eqT @fun @DefaultFun of
-      Just Refl -> caseOfCase
-      Nothing   -> id
+    caseOfCase' t = case eqT @fun @DefaultFun of
+      Just Refl ->
+        case eqT @uni @PLC.DefaultUni of
+          Just Refl ->
+            case eqT @name @Name of
+              Just Refl ->
+                case deBruijnTerm t of
+                  Right res                       -> trace (show . conv $ res) $ caseOfCase t
+                  Left (err :: FreeVariableError) -> error $ show err
+              Nothing -> caseOfCase t
+          Nothing -> caseOfCase t
+      Nothing   -> t
 
     cseStep :: Int -> Term name uni fun a -> m (Term name uni fun a)
     cseStep _ =
