b6e82b6f6ed2d5ad4b250513b8bf119e1bd480d4
Yura Lazarev <1009751+Unisay@users.noreply.github.com>
2024-05-09 00:10:45 +0200

    CaseOfCase UPLC transformation tests. (#5960)

--- plutus-core/untyped-plutus-core/test/Spec.hs
+++ plutus-core/untyped-plutus-core/test/Spec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
@@ -10,10 +11,11 @@ import Evaluation.Builtins (test_builtins)
 import Evaluation.Debug (test_debug)
 import Evaluation.FreeVars (test_freevars)
 import Evaluation.Golden (test_golden)
-import Evaluation.Machines
+import Evaluation.Machines (test_budget, test_machines, test_tallying)
 import Evaluation.Regressions (schnorrVerifyRegressions)
 import Flat.Spec (test_flat)
 import Generators (test_parsing)
+import Transform.CaseOfCase.Test (test_caseOfCase)
 import Transform.Simplify (test_simplify)
 
 import Test.Tasty
@@ -21,18 +23,21 @@ import Test.Tasty
 main :: IO ()
 main = do
   setLocaleEncoding utf8
-  defaultMain $ testGroup "Untyped Plutus Core"
-    [ test_machines
-    , test_builtins
-    , test_budget
-    , test_golden
-    , test_tallying
-    , test_simplify
-    , test_debruijn
-    , test_freevars
-    , test_parsing
-    , test_debug
-    , test_flat
-    , schnorrVerifyRegressions
-    , evalOrder
-    ]
+  defaultMain $
+    testGroup
+      "Untyped Plutus Core"
+      [ test_machines
+      , test_builtins
+      , test_budget
+      , test_caseOfCase
+      , test_golden
+      , test_tallying
+      , test_simplify
+      , test_debruijn
+      , test_freevars
+      , test_parsing
+      , test_debug
+      , test_flat
+      , schnorrVerifyRegressions
+      , evalOrder
+      ]
