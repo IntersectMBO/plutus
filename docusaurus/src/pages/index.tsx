fee97555801f298786a507305d584f6046c4ffa7
ianhanssoniohk <ian.hansson@iohk.io>
2024-05-15 10:22:01 +0100

    Initial Docusaurus Setup

---
+++ docusaurus/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const Home = () => {
+  return <Layout>Landing</Layout>;
+};
+
+export default Home;
---
+++ docusaurus/src/theme/MDXComponents.ts
@@ -0,0 +1,7 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import LiteralInclude from "@site/src/components/LiteralInclude";
+
+export default {
+  ...MDXComponents,
+  LiteralInclude,
+};
