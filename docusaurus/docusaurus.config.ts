c6b26b50e8e54ecd8d101f754a407929081504ee
ianhanssoniohk <ian.hansson@iohk.io>
2024-05-25 15:20:31 +0100

    Update favicon and social card assets

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/docusaurus-social-card.png",
     navbar: {
       title: "Plutus",
       logo: {
