edf3b0a154cdc6cd074c20284442802bcccfa439
Ana Pantilie <ana.pantilie95@gmail.com>
2024-05-30 17:49:03 +0300

    Copy Value tests for Data.Value
    Signed-off-by: Ana Pantilie <ana.pantilie95@gmail.com>

--- plutus-ledger-api/test-plugin/Spec.hs
+++ plutus-ledger-api/test-plugin/Spec.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Spec.Budget qualified
+import Spec.Data.Budget qualified
+import Spec.Data.Value qualified
 import Spec.Value qualified
 
 import Test.Tasty
@@ -12,4 +14,6 @@ tests :: TestTree
 tests = testGroup "plutus-ledger-api-plugin-test"
     [ Spec.Budget.tests
     , Spec.Value.test_EqValue
+    , Spec.Data.Budget.tests
+    , Spec.Data.Value.test_EqValue
     ]
