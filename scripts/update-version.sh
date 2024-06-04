e243c74775f0df3d86c812119b12adda253cc97c
kwxm <kenneth.mackenzie@iohk.io>
2024-06-04 22:28:25 +0100

    Allow whitespace after comma

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -43,8 +43,8 @@ sed -i "s/\(^version:\s*\).*/\1$VERSION/" "./$PACKAGE/$PACKAGE.cabal"
 # The second sed command is to match the case where the package name is at the end of the line:
 # we need this because the first case requires at least one character after the package name.
 #
-# Note that this all requires a comma (maybe preceded and followed by whitespace) at the start
-# of the line: it won't work with commas at the end.
+# Note that this all requires a comma (maybe preceded and/or followed by whitespace) at the
+# start of the line: it won't work with commas at the end.
 
 echo "Updating version bounds on $PACKAGE to '^>=$major_version'"
 find . -name "?*.cabal" -exec sed -i "s/\(^[ \t]*,[ \t]*$PACKAGE[^-A-Za-z0-1][^^]*\).*/\1^>=$major_version/" {} \; \
