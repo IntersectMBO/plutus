3736f3c757ee8afa6ab997b43008808654047714
Ana Pantilie <ana.pantilie95@gmail.com>
2024-06-13 20:03:53 +0300

    Add plutus-ledger-api-test Data tests
    Signed-off-by: Ana Pantilie <ana.pantilie95@gmail.com>

---
+++ plutus-ledger-api/test/Spec/Data/CostModelParams.hs
@@ -0,0 +1,75 @@
+-- editorconfig-checker-disable-file
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE TypeApplications #-}
+module Spec.Data.CostModelParams where
+
+import PlutusCore.Evaluation.Machine.ExBudgetingDefaults (defaultCostModelParamsForTesting)
+
+import PlutusLedgerApi.Common
+
+import PlutusLedgerApi.Data.V1 as V1
+import PlutusLedgerApi.Data.V2 as V2
+import PlutusLedgerApi.Data.V3 as V3
+import PlutusLedgerApi.Test.V3.Data.EvaluationContext qualified as V3
+
+import Control.Monad.Except
+import Control.Monad.Writer.Strict
+import Data.Either
+import Data.Foldable
+import Data.List.Extra
+import Data.Set as Set
+import Test.Tasty
+import Test.Tasty.HUnit
+
+tests :: TestTree
+tests =
+  testGroup
+    "costModelParams"
+    [ testCase "length" $ do
+            166 @=? length v1_ParamNames
+            185 @=? length v2_ParamNames
+            251 @=? length v3_ParamNames
+    , testCase "tripping paramname" $ do
+            for_ v1_ParamNames $ \ p ->
+                assertBool "tripping v1 cm params failed" $ Just p == readParamName (showParamName p)
+            for_ v2_ParamNames $ \ p ->
+                assertBool "tripping v2 cm params failed" $ Just p == readParamName (showParamName p)
+            for_ v3_ParamNames $ \ p ->
+                assertBool "tripping v3 cm params failed" $ Just p == readParamName (showParamName p)
+    , testCase "default values costmodelparamsfortesting" $ do
+            defaultCostModelParamsForTesting @=? Just (toCostModelParams V3.costModelParamsForTesting)
+    , testCase "context length" $ do
+            let costValuesForTesting = fmap snd V3.costModelParamsForTesting
+            -- the `costModelParamsForTesting` reflects only the latest version (V3), so this should succeed because the lengths match
+            assertBool "wrong number of arguments in V3.mkEvaluationContext" $ isRight $ runExcept $ runWriterT $ V3.mkEvaluationContext costValuesForTesting
+            -- this one should succeed because we allow adding new builtins to an existing version, by appending new cost model parameters, for more info:
+            -- See Note [Cost model parameters from the ledger's point of view]
+            assertBool "larger number of params did not warn" $ hasWarnMoreParams (length v3_ParamNames) (1 + length v3_ParamNames) $
+                runExcept $ runWriterT $ V3.mkEvaluationContext $ costValuesForTesting ++ [1] -- dummy param value appended
+    , testCase "cost model parameters" $ do
+         -- v1 is missing some cost model parameters because new builtins are added in v2
+         assertBool "v1 params is not a proper subset of v2 params" $ v1_ParamNames `paramProperSubset` v2_ParamNames
+         -- v1/v2 and v3 cost models are not comparable because we added new builtins in v3 but also
+         -- removed some superseded cost model parameters.
+         assertBool "v1 params and v3 params are comparable" $
+           not (v1_ParamNames `paramSubset` v3_ParamNames)
+           && not (v3_ParamNames `paramSubset` v1_ParamNames)
+         assertBool "v2 params and v3 params are comparable" $
+           not (v2_ParamNames `paramSubset` v3_ParamNames)
+           && not (v3_ParamNames `paramSubset` v2_ParamNames)
+    ]
+  where
+    hasWarnMoreParams :: Int -> Int -> Either a (b, [CostModelApplyWarn]) -> Bool
+    hasWarnMoreParams testExpected testActual (Right (_,[CMTooManyParamsWarn{..}]))
+        | testExpected==cmExpected && testActual==cmActual  = True
+    hasWarnMoreParams _ _ _ = False
+
+    paramProperSubset pA pB =
+        Set.fromList (showParamName <$> pA) `Set.isProperSubsetOf` Set.fromList (showParamName <$> pB)
+
+    paramSubset pA pB =
+        Set.fromList (showParamName <$> pA) `Set.isSubsetOf` Set.fromList (showParamName <$> pB)
+
+    v1_ParamNames = enumerate @V1.ParamName
+    v2_ParamNames = enumerate @V2.ParamName
+    v3_ParamNames = enumerate @V3.ParamName
---
+++ plutus-ledger-api/testlib/PlutusLedgerApi/Test/V2/Data/EvaluationContext.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+module PlutusLedgerApi.Test.V2.Data.EvaluationContext
+    ( costModelParamsForTesting
+    , mCostModel
+    , clearMachineCostModel
+    , clearBuiltinCostModel
+    ) where
+
+import PlutusCore.Evaluation.Machine.BuiltinCostModel
+import PlutusCore.Evaluation.Machine.ExBudgetingDefaults
+import PlutusCore.Evaluation.Machine.MachineParameters
+import PlutusLedgerApi.Data.V2 qualified as V2
+import PlutusLedgerApi.Test.Common.EvaluationContext as Common
+import PlutusLedgerApi.Test.V3.Data.EvaluationContext qualified as V3
+import PlutusPrelude
+
+import Data.Int (Int64)
+import Data.Map qualified as Map
+import Data.Maybe
+
+-- | Example values of costs for @PlutusV2@, in expected ledger order.
+-- Suitable to be used in testing.
+costModelParamsForTesting :: [(V2.ParamName, Int64)]
+costModelParamsForTesting = Map.toList $ fromJust $
+    Common.extractCostModelParamsLedgerOrder mCostModel
+
+-- | The PlutusV2 "cost model" is constructed by the v3 "cost model", by clearing v3 introductions.
+mCostModel :: MCostModel
+mCostModel = toMCostModel defaultCekCostModelForTestingB
+           & machineCostModel
+           %~ V3.clearMachineCostModel
+           & builtinCostModel
+           %~ V3.clearBuiltinCostModel
+
+{- | Assign to `mempty` those CEK constructs that @PlutusV2@ introduces (indirectly by introducing
+a ledger language version with those CEK constructs).
+
+This can be used to generate a (machine) cost model of the previous plutus version,
+by omitting the generation of the costs concerning the missing @PlutusV2@ CEK constructs.
+-}
+clearMachineCostModel :: (m ~ MCekMachineCosts) => m -> m
+clearMachineCostModel = id -- nothing changed, so nothing to clear
+
+{- | Assign to `mempty` those builtins that the @PlutusV2@ introduces.
+
+This can be used to generate a (builtin) cost model of the previous version
+by omitting the generation of the costs concerning the missing @PlutusV2@ builtins.
+-}
+clearBuiltinCostModel :: (m ~ MBuiltinCostModel) => m -> m
+clearBuiltinCostModel r = r
+               { paramSerialiseData = mempty
+               , paramVerifyEcdsaSecp256k1Signature = mempty
+               , paramVerifySchnorrSecp256k1Signature = mempty
+               , paramIntegerToByteString = mempty
+               , paramByteStringToInteger = mempty
+               }
