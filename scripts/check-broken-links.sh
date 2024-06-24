564181583118f820d541ec3a9619506b8648ce3f
zeme <lorenzo.calegari@iohk.io>
2024-06-24 19:44:21 +0200

    wip

--- scripts/check-broken-links.sh
+++ scripts/check-broken-links.sh
@@ -18,8 +18,7 @@ for file in "${TARGETS[@]}"; do
     echo "Checking ${file}"
     grep -oE "\b(https?://|www\.)[^\[\(\)\"]+\b" "${file}" \
         | linkchecker --no-warnings --recursion-level 0 --output failures --check-extern --stdin \
-        # For some reason linkchecker fails to check this URL though it is valid
-        --ignore-url https://img.shields.io/matrix/plutus-core%3Amatrix.org
+        --ignore-url https://img.shields.io/matrix/plutus-core%3Amatrix.org # For some reason linkchecker fails to check this URL though it is valid
     if [ $? -ne 0 ]; then 
         echo "${file} has broken links, see output above"
         FAILED=1
