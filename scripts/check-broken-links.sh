74eb5aab38ea9b92fb1a07651f2ef7fe5b8ad096
zeme <lorenzo.calegari@iohk.io>
2024-06-24 18:20:10 +0200

    Add workflows to check broken links and deploy papers to GH Pages

---
+++ scripts/check-broken-links.sh
@@ -0,0 +1,25 @@
+TARGETS=(
+    .github/ISSUE_TEMPLATE/bug_report.yml
+    .github/ISSUE_TEMPLATE/feature_request.yml
+    .github/PULL_REQUEST_TEMPLATE.md
+    .github/SECURITY.md
+    CODE_OF_CONDUCT.md
+    CONTRIBUTING.adoc
+    LICENSE
+    NOTICE
+    README.adoc
+    RELEASE.adoc
+    STYLEGUIDE.adoc
+)
+
+FAILED=0
+
+for file in "${TARGETS[@]}"; do 
+    grep -oE "\b(https?://|www\.)[^\[\(\)\"]+\b" "${file}" | linkchecker --no-warnings --recursion-level 0 --output failures --check-extern --stdin
+    if [ $? -ne 0 ]; then 
+        echo "${file} has broken links, see output above"
+        FAILED=1
+    fi 
+done 
+
+exit "${FAILED}"
