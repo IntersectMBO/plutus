e72e862b9ec22ae2604aacef6c1a2d2c2d59eb77
zeme <lorenzo.calegari@iohk.io>
2024-07-02 08:23:05 +0200

    Update baseUrl in docusaurus.config.ts

--- doc/docusaurus/docusaurus.config.ts
+++ doc/docusaurus/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://intersectmbo.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/plutus/docs/",
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
