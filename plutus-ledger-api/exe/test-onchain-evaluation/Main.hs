71f4aa2c4aa5d52214a6e3e9eb917f65ee7c0d4d
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-07-10 16:44:32 +0200

    Analyse script events supports PlutusLedgerLanguage V3

--- plutus-ledger-api/exe/test-onchain-evaluation/Main.hs
+++ plutus-ledger-api/exe/test-onchain-evaluation/Main.hs
@@ -17,6 +17,7 @@ import Control.Monad.Extra (whenJust)
 import Control.Monad.Writer.Strict
 import Data.List.NonEmpty (nonEmpty)
 import Data.Maybe (catMaybes)
+import PlutusLedgerApi.V3.EvaluationContext qualified as V3
 import System.Directory.Extra (listFiles)
 import System.Environment (getEnv)
 import System.FilePath (isExtensionOf, takeBaseName)
@@ -26,35 +27,40 @@ import Test.Tasty.HUnit
 -- | Test cases from a single event dump file
 testOneFile :: FilePath -> TestTree
 testOneFile eventFile = testCase (takeBaseName eventFile) $ do
-    events <- loadEvents eventFile
-    case ( mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
-         , mkContext V2.mkEvaluationContext (eventsCostParamsV2 events)
-         ) of
-        (Right ctxV1, Right ctxV2) -> do
-            errs <-
-                fmap catMaybes $
-                    mapConcurrently
-                    (evaluate . runSingleEvent ctxV1 ctxV2)
-                    (eventsOf events)
-            whenJust (nonEmpty errs) $ assertFailure . renderTestFailures
-        (Left err, _) -> assertFailure $ display err
-        (_, Left err) -> assertFailure $ display err
-  where
-    mkContext f = \case
-        Nothing         -> Right Nothing
-        Just costParams -> Just . (,costParams) . fst <$> runWriterT (f costParams)
+  events <- loadEvents eventFile
+  case ( mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
+       , mkContext V2.mkEvaluationContext (eventsCostParamsV2 events)
+       , mkContext V3.mkEvaluationContext (eventsCostParamsV2 events)
+       ) of
+    (Right ctxV1, Right ctxV2, Right ctxV3) -> do
+      errs <-
+        fmap catMaybes $
+          mapConcurrently
+            (evaluate . runSingleEvent ctxV1 ctxV2 ctxV3)
+            (eventsOf events)
+      whenJust (nonEmpty errs) $ assertFailure . renderTestFailures
+    (Left err, _, _) -> assertFailure $ display err
+    (_, Left err, _) -> assertFailure $ display err
+    (_, _, Left err) -> assertFailure $ display err
+ where
+  mkContext f = \case
+    Nothing -> Right Nothing
+    Just costParams -> Just . (,costParams) . fst <$> runWriterT (f costParams)
 
-    runSingleEvent ctxV1 ctxV2 event =
-        case event of
-            PlutusV1Event{} -> case ctxV1 of
-                Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
-                Nothing            -> Just $ MissingCostParametersFor PlutusV1
-            PlutusV2Event{} -> case ctxV2 of
-                Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
-                Nothing            -> Just $ MissingCostParametersFor PlutusV2
+  runSingleEvent ctxV1 ctxV2 ctxV3 event =
+    case event of
+      PlutusEvent PlutusV1 _ _ -> case ctxV1 of
+        Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
+        Nothing            -> Just $ MissingCostParametersFor PlutusV1
+      PlutusEvent PlutusV2 _ _ -> case ctxV2 of
+        Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
+        Nothing            -> Just $ MissingCostParametersFor PlutusV2
+      PlutusEvent PlutusV3 _ _ -> case ctxV3 of
+        Just (ctx, params) -> InvalidResult <$> checkEvaluationEvent ctx params event
+        Nothing            -> Just $ MissingCostParametersFor PlutusV3
 
 main :: IO ()
 main = do
-    dir <- getEnv "EVENT_DUMP_DIR"
-    eventFiles <- filter ("event" `isExtensionOf`) <$> listFiles dir
-    defaultMain . testGroup "Mainnet script evaluation test" $ fmap testOneFile eventFiles
+  dir <- getEnv "EVENT_DUMP_DIR"
+  eventFiles <- filter ("event" `isExtensionOf`) <$> listFiles dir
+  defaultMain . testGroup "Mainnet script evaluation test" $ fmap testOneFile eventFiles
