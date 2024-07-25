a7e7adbc66a89f2c1b1f4c27813f2dcb2699b7f2
kwxm <kenneth.mackenzie@iohk.io>
2024-07-25 22:05:07 +0100

    Enable nqueens benchmark now that writeBits works

--- plutus-benchmark/bitwise/bench/Main.hs
+++ plutus-benchmark/bitwise/bench/Main.hs
@@ -1,17 +1,14 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
-{-
 import Criterion.Main (bench, defaultMain)
 import PlutusBenchmark.Common (benchProgramCek, mkMostRecentEvalCtx)
 import PlutusBenchmark.NQueens (nqueens)
 import PlutusTx.Code (CompiledCode, getPlcNoAnn)
+import PlutusTx.Plugin ()
 import PlutusTx.TH (compile)
--}
-
-main :: IO ()
-main = print "Pending"
-
-{- Currently not able to run, due to problems with writeBits compiling under PlutusTx
 
 main :: IO ()
 main = defaultMain [
@@ -21,6 +18,5 @@ main = defaultMain [
 -- Helpers
 
 nqueensCompiled :: CompiledCode [(Integer, Integer)]
-nqueensCompiled = $$(compile [||nqueens 8||])
+nqueensCompiled = $$(compile [|| nqueens 8 ||])
 
--}
