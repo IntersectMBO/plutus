2ec20c8b9e6ab6e29326fb0464ba169d1285fd0c
Koz Ross <koz.ross@retro-freedom.nz>
2024-07-25 14:47:51 +1200

    Tests

--- plutus-benchmark/bitwise/test/Main.hs
+++ plutus-benchmark/bitwise/test/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import PlutusBenchmark.NQueens (nqueens)
+import Crypto.Hash.SHA512 qualified as SHA512Ref
+import Crypto.Sign.Ed25519 (PublicKey (PublicKey), Signature (Signature))
+import Crypto.Sign.Ed25519 qualified as Ed25519Ref
+import PlutusBenchmark.Ed25519 (checkValid)
+import PlutusBenchmark.SHA512 (sha512)
+import PlutusTx.Builtins (fromBuiltin, toBuiltin)
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
@@ -8,5 +15,27 @@ main :: IO ()
 main = defaultMain . testGroup "nqueens" $ [
   testCase "solves for 8 queens" $ assertEqual ""
     [(0,0), (1,4), (2,7), (3,5), (4,2), (5,6), (6,1), (7,3)]
-    (nqueens 8)
+    (nqueens 8),
+  testGroup "Ed25519" [
+    testCase "SHA512 works" sha512Case,
+    testCase "Ed25519 works" ed25519Case
+    ]
   ]
+
+-- Cases
+
+sha512Case :: IO ()
+sha512Case = do
+  let testBS = "hello world"
+  let expected = SHA512Ref.hash testBS
+  let actual = fromBuiltin . sha512 . toBuiltin $ testBS
+  assertEqual "" expected actual
+
+ed25519Case :: IO ()
+ed25519Case = do
+  let msg = "hello world"
+  (refPK@(PublicKey pk), refSK) <- Ed25519Ref.createKeypair
+  let signed@(Signature sigRaw) = Ed25519Ref.dsign refSK msg
+  let expected = Ed25519Ref.dverify refPK msg signed
+  let actual = checkValid (toBuiltin sigRaw) (toBuiltin msg) (toBuiltin pk)
+  assertEqual "" expected actual
