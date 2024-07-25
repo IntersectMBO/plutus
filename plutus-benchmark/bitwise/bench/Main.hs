c157b45d49ba3013e3d37a38873eb53c2d1aacd8
Koz Ross <koz.ross@retro-freedom.nz>
2024-07-26 09:57:23 +1200

    Benchmark

--- plutus-benchmark/bitwise/bench/Main.hs
+++ plutus-benchmark/bitwise/bench/Main.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
+-- editorconfig-checker-disable-file
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Main (main) where
 
@@ -8,11 +10,14 @@ import PlutusBenchmark.Common (benchProgramCek, mkMostRecentEvalCtx)
 import PlutusBenchmark.NQueens (nqueens)
 import PlutusTx.Code (CompiledCode, getPlcNoAnn)
 import PlutusTx.Plugin ()
+import PlutusBenchmark.Ed25519 (checkValid)
+import PlutusTx.Code (CompiledCode, getPlcNoAnn)
 import PlutusTx.TH (compile)
 
 main :: IO ()
 main = defaultMain [
-  bench "8-queens" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $ nqueensCompiled
+  bench "8-queens" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $ nqueensCompiled,
+  bench "Ed25519" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $ ed25519Compiled
   ]
 
 -- Helpers
@@ -20,3 +25,7 @@ main = defaultMain [
 nqueensCompiled :: CompiledCode [(Integer, Integer)]
 nqueensCompiled = $$(compile [|| nqueens 8 ||])
 
+ed25519Compiled :: CompiledCode Bool
+ed25519Compiled = $$(compile [|| checkValid "x\\SUB`\\EOT\\209\\145$h\\209\\207\\193\\b\\149\\253\\249%\\231\\b\\DC1s\\212\\191l/\\v)\\235\\208\\200tW\\155\\202\\163\\&8Z\\n\\rzojc\\\\\\189\\&3\\230\\139\\171\\GS\\162\\129\\\\|=\\253\\ACK.\\137&^\\SOH\\ETX\\216\\v"
+                                            "hello world"
+                                            "V\\244\\f8\\223\\229\\&2\\189Z@\\137F\\168\\221\\245\\&3\\137W\\EOT\\229\\223T)O\\219\\SI\\242\\153\\CAN\\210Nw" ||])
