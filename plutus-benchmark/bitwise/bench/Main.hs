9d8371924655dfca650015d7885c2c577ed2540a
Koz Ross <koz.ross@retro-freedom.nz>
2024-07-30 09:35:35 +1200

    Fix benchmarks to avoid CTE

--- plutus-benchmark/bitwise/bench/Main.hs
+++ plutus-benchmark/bitwise/bench/Main.hs
@@ -8,30 +8,39 @@ module Main (main) where
 import Criterion.Main (bench, defaultMain)
 import PlutusBenchmark.Common (benchProgramCek, mkMostRecentEvalCtx)
 import PlutusBenchmark.NQueens (nqueens)
-import PlutusTx.Code (CompiledCode, getPlcNoAnn)
-import PlutusTx.Plugin ()
-import PlutusBenchmark.Ed25519 (checkValid)
-import PlutusTx.Code (CompiledCode, getPlcNoAnn)
+import PlutusTx.Builtins (BuiltinByteString, BuiltinData)
+import PlutusTx.Code (CompiledCode, getPlcNoAnn, unsafeApplyCode)
+import PlutusTx.IsData (toBuiltinData, unsafeFromBuiltinData)
 import PlutusTx.TH (compile)
 
 main :: IO ()
 main = defaultMain [
-  bench "8-queens" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $ nqueensCompiled,
-  bench "Ed25519" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $ ed25519Compiled
+  bench "Ed25519" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $
+    checkValidCompiled `unsafeApplyCode` signatureAsData `unsafeApplyCode` msgAsData `unsafeApplyCode` pkAsData,
+  bench "8-queens" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $
+    nqueensCompiled `unsafeApplyCode` dimAsData
   ]
 
 -- Helpers
 
-nqueensCompiled :: CompiledCode [(Integer, Integer)]
-nqueensCompiled = $$(compile [|| nqueens 8 ||])
+checkValidCompiled :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> Bool)
+checkValidCompiled = $$(compile [|| \signature msg pk -> checkValid (unsafeFromBuiltinData signature)
+                                                                    (unsafeFromBuiltinData msg)
+                                                                    (unsafeFromBuiltinData pk) ||])
 
-ed25519Compiled :: CompiledCode Bool
-ed25519Compiled = $$(compile [||
-  let msg = "hello world"
-      signature = "\NUL\147!x\173\167\209z`\t\243|\195$X$\233\166\234\NUL\134\152l\DC4\243\&4\217\NAK\152\180{$M\227R\214\218%\241\157\ENQ\SO\ENQ\t\152\140\171\240\200f\184\133\203\227z\163\NUL\185\155Y\139\178\249\STX"
-      pk = "(:\255\251\129\&7-^w\253\145\vh\ESC\171r\189\223/\213Qzb\249\175$z\211q\195\DC1\198"
-    in checkValid signature msg pk
- ||])
+msgAsData :: CompiledCode BuiltinData
+msgAsData = $$(compile [|| toBuiltinData ("hello world" :: BuiltinByteString) ||])
 
-nqueensCompiled :: CompiledCode [(Integer, Integer)]
-nqueensCompiled = $$(compile [||nqueens 8||])
+signatureAsData :: CompiledCode BuiltinData
+signatureAsData =
+  $$(compile [|| toBuiltinData ("\NUL\147!x\173\167\209z`\t\243|\195$X$\233\166\234\NUL\134\152l\DC4\243\&4\217\NAK\152\180{$M\227R\214\218%\241\157\ENQ\SO\ENQ\t\152\140\171\240\200f\184\133\203\227z\163\NUL\185\155Y\139\178\249\STX" :: BuiltinByteString) ||])
+
+pkAsData :: CompiledCode BuiltinData
+pkAsData =
+  $$(compile [|| toBuiltinData ("(:\255\251\129\&7-^w\253\145\vh\ESC\171r\189\223/\213Qzb\249\175$z\211q\195\DC1\198" :: BuiltinByteString) ||])
+
+nqueensCompiled :: CompiledCode (BuiltinData -> [(Integer, Integer)])
+nqueensCompiled = $$(compile [|| \dim -> nqueens (unsafeFromBuiltinData dim) ||])
+
+dimAsData :: CompiledCode BuiltinData
+dimAsData = $$(compile [|| toBuiltinData (8 :: Integer) ||])
