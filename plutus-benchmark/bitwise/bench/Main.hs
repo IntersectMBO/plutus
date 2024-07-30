8c9926d1f8952264e30e1caf490abeaaff3dade3
Koz Ross <koz.ross@retro-freedom.nz>
2024-07-30 12:52:03 +1200

    Goldens for Ed25519

--- plutus-benchmark/bitwise/bench/Main.hs
+++ plutus-benchmark/bitwise/bench/Main.hs
@@ -1,18 +1,14 @@
 -- editorconfig-checker-disable-file
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
 
 module Main (main) where
 
 import Criterion.Main (bench, defaultMain)
 import PlutusBenchmark.Common (benchProgramCek, mkMostRecentEvalCtx)
-import PlutusBenchmark.NQueens (nqueens)
+import PlutusBenchmark.Ed25519.Compiled (checkValidCompiled, msgAsData, pkAsData, signatureAsData)
 import PlutusBenchmark.NQueens.Compiled (dimAsData, nqueensCompiled)
-import PlutusTx.Builtins (BuiltinByteString, BuiltinData)
-import PlutusTx.Code (CompiledCode, getPlcNoAnn, unsafeApplyCode)
-import PlutusTx.IsData (toBuiltinData, unsafeFromBuiltinData)
-import PlutusTx.TH (compile)
+import PlutusTx.Code (getPlcNoAnn, unsafeApplyCode)
 
 main :: IO ()
 main = defaultMain [
@@ -21,21 +17,3 @@ main = defaultMain [
   bench "8-queens" . benchProgramCek mkMostRecentEvalCtx . getPlcNoAnn $
     nqueensCompiled `unsafeApplyCode` dimAsData
   ]
-
--- Helpers
-
-checkValidCompiled :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> Bool)
-checkValidCompiled = $$(compile [|| \signature msg pk -> checkValid (unsafeFromBuiltinData signature)
-                                                                    (unsafeFromBuiltinData msg)
-                                                                    (unsafeFromBuiltinData pk) ||])
-
-msgAsData :: CompiledCode BuiltinData
-msgAsData = $$(compile [|| toBuiltinData ("hello world" :: BuiltinByteString) ||])
-
-signatureAsData :: CompiledCode BuiltinData
-signatureAsData =
-  $$(compile [|| toBuiltinData ("\NUL\147!x\173\167\209z`\t\243|\195$X$\233\166\234\NUL\134\152l\DC4\243\&4\217\NAK\152\180{$M\227R\214\218%\241\157\ENQ\SO\ENQ\t\152\140\171\240\200f\184\133\203\227z\163\NUL\185\155Y\139\178\249\STX" :: BuiltinByteString) ||])
-
-pkAsData :: CompiledCode BuiltinData
-pkAsData =
-  $$(compile [|| toBuiltinData ("(:\255\251\129\&7-^w\253\145\vh\ESC\171r\189\223/\213Qzb\249\175$z\211q\195\DC1\198" :: BuiltinByteString) ||])
