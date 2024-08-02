501e3f94ee85bed5d5e50bf62a0986bc79faa697
Ziyang Liu <unsafeFixIO@gmail.com>
2024-08-01 17:38:22 -0700

    Update plutus-tx-plugin/app/GeneratePluginOptionsDoc.hs
    Co-authored-by: Kenneth MacKenzie <kwxm@inf.ed.ac.uk>

--- plutus-tx-plugin/app/GeneratePluginOptionsDoc.hs
+++ plutus-tx-plugin/app/GeneratePluginOptionsDoc.hs
@@ -57,7 +57,7 @@ These options can be passed to the compiler via the `OPTIONS_GHC` pragma, for in
 
 ``` haskell
 {{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:dump-uplc #-}}
-{{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:max-simplifier-iterations=3 #-}}
+{{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:max-simplifier-iterations-uplc=3 #-}}
 ```
 
 For each boolean option, you can add a `no-` prefix to switch it off, such as `no-typecheck`, `no-simplifier-beta`.
