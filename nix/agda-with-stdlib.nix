d1bb967a75d1d9c77e3085d26d4e6292b51e8e30
Ana Pantilie <ana.pantilie95@gmail.com>
2024-08-20 17:13:54 +0300

    Use standard-library 2.0; fix build

--- nix/agda-with-stdlib.nix
+++ nix/agda-with-stdlib.nix
@@ -4,13 +4,13 @@ let
   # Need a newer version for 2.6.2 compatibility
   stdlib = repoRoot.nix.agda-packages.standard-library.overrideAttrs (oldAtts: rec {
 
-    version = "2.1";
+    version = "2.0";
 
     src = pkgs.fetchFromGitHub {
       repo = "agda-stdlib";
       owner = "agda";
       rev = "v${version}";
-      sha256 = "sha256-tv/Fj8ZJgSvieNLlXBjyIR7MSmDp0e2QbN1d/0xBpFg=";
+      sha256 = "sha256-TjGvY3eqpF+DDwatT7A78flyPcTkcLHQ1xcg+MKgCoE=";
     };
     # This is preConfigure is copied from more recent nixpkgs that also
     # uses version 1.7 of standard-library. Old nixpkgs (that used 1.4)
@@ -18,7 +18,7 @@ let
     # (that used 1.6) had a preConfigure step that attempts to `rm`
     # files that are now in the .gitignore list for 1.
     preConfigure = ''
-      runhaskell GenerateEverything.hs
+      runhaskell GenerateEverything.hs --include-deprecated
       # We will only build/consider Everything.agda, in particular we don't want Everything*.agda
       # do be copied to the store.
       rm EverythingSafe.agda
