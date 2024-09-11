a9b163e56b6d4ac9c409ce4641dd984690142b96
Kenneth MacKenzie <kwxm@inf.ed.ac.uk>
2024-09-11 22:55:13 +0100

    Kwxm/ci/visible total (#6470)
    * Put the TOTAL line for a benchmakr comparison in a table of its own
    
    * Try again
    
    * Complete merge

--- scripts/ci-plutus-benchmark.sh
+++ scripts/ci-plutus-benchmark.sh
@@ -81,8 +81,9 @@ Comparing benchmark results of '$BENCHMARK_NAME' on '$BASE_BRANCH_REF' (base) an
 
 EOF
 ./plutus-benchmark/bench-compare-markdown bench-base.log bench-PR.log "${BASE_BRANCH_REF:0:7}" "${PR_BRANCH_REF:0:7}" |
-awk '/^[| \t]*$/ {print "</details>"; next}
-     /TOTAL/ {print "\n| :------| :------: | :------: | :------: |"}
+    awk -v hdr1="${BASE_BRANCH_REF:0:7}" -v hdr2="${PR_BRANCH_REF:0:7}" '
+     /^[| \t]*$/ {print "</details>"; next}
+     /TOTAL/ {printf ("\n| %s | %s | Change |\n| :------| :------: | :------: | :------: |\n", hdr1, hdr2)}
      {print}' 
 # ^ This puts </details> after the individual results and puts the TOTAL line in a small table on its own.
 } > bench-compare-result.log
