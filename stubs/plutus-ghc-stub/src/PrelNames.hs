5fa0207b93a7706e58b2d5c57be883996361bb55
Ziyang Liu <unsafeFixIO@gmail.com>
2024-09-25 04:09:08 -0700

    Remove plutus-ghc-stub (#6514)

--- stubs/plutus-ghc-stub/src/PrelNames.hs
+++
@@ -1,9 +0,0 @@
-module PrelNames where
-
-import StubTypes
-
-eqIntegerPrimName, eqName :: Name
-isStringClassName :: Name
-eqIntegerPrimName = error "eqInteger#"
-eqName = error "eqName"
-isStringClassName = error "isStringClassName"
