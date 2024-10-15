b0a28e72749a7f232434fa4c3df090f7c498c70c
zeme <lorenzo.calegari@iohk.io>
2024-10-15 10:46:25 +0200

    Update all flake inputs and add texliveFull to the devShell

--- nix/shell.nix
+++ nix/shell.nix
@@ -35,6 +35,9 @@ in
     repoRoot.nix.r-with-packages
     pkgs.R
 
+    # LaTeX environment
+    pkgs.texliveFull
+
     # Misc useful stuff, could make these commands but there's a lot already
     pkgs.jekyll
     pkgs.plantuml
