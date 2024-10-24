3a0ca719a39b300a8d82b622f1a11ea57f664900
zeme <lorenzo.calegari@iohk.io>
2024-10-24 15:36:53 +0200

    Add agda-extra-env.nix

---
+++ nix/agda-extra-env.nix
@@ -0,0 +1,15 @@
+# Additional configuration for Agda environment. The `uplc` executable requires
+# knowledge of the Agda standard library location and the metatheory source directory.
+# This information is needed both inside the nix shell and by the upls exec at runtime.
+{ repoRoot, inputs, pkgs, system, lib }:
+{
+  shellHookExports = ''
+    export AGDA_STDLIB_SRC="${repoRoot.nix.agda-with-stdlib}/src"
+    export PLUTUS_METHATHEORY_SRC="${repoRoot.nix.plutus-metatheory}/src"
+  '';
+
+  wrapProgramArgs = ''
+    --set AGDA_STDLIB_SRC "${repoRoot.nix.agda-with-stdlib}/src" \
+    --set PLUTUS_METHATHEORY_SRC "${repoRoot.nix.plutus-metatheory}/src"
+  '';
+}
