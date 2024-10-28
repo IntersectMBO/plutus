7a849fab3ee4f2f748f042fd6271ed78078da005
zeme <lorenzo.calegari@iohk.io>
2024-10-28 15:25:15 +0100

    wip

--- nix/agda.nix
+++ nix/agda.nix
@@ -119,14 +119,16 @@ rec {
   };
 
 
+  # TODO this is a bit of a hack, but it's the only way to get the uplc
+  # executable to find the metatheory and the stdandard library.
   shell-hook-exports = ''
     export AGDA_STDLIB_SRC="${agda-stdlib}/src"
-    export PLUTUS_METHATHEORY_SRC="${../plutus-metatheory/src}"
+    export PLUTUS_METHATHEORY_SRC="./plutus-metatheory/src"
   '';
 
 
   wrap-program-args = ''
     --set AGDA_STDLIB_SRC "${agda-stdlib}/src" \
-    --set PLUTUS_METHATHEORY_SRC "${../plutus-metatheory/src}"
+    --set PLUTUS_METHATHEORY_SRC "./plutus-metatheory/src"
   '';
 }
