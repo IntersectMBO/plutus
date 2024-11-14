5e94541fef26b4dae975d911921ed339d77dbfe2
effectfully <effectfully@gmail.com>
2024-11-14 02:47:18 +0100

    [Evaluation] [Performance] Remove scope checking

--- plutus-ledger-api/src/PlutusLedgerApi/Common/Eval.hs
+++ plutus-ledger-api/src/PlutusLedgerApi/Common/Eval.hs
@@ -107,8 +107,7 @@ mkTermToEvaluate ll pv script args = do
     unless (v `Set.member` plcVersionsAvailableIn ll pv) $
       throwing _ScriptDecodeError $ PlutusCoreLanguageNotAvailableError v ll pv
 
-    -- make sure that term is closed, i.e. well-scoped
-    through (liftEither . first DeBruijnError . UPLC.checkScope) appliedT
+    pure appliedT
 
 toMachineParameters :: MajorProtocolVersion -> EvaluationContext -> DefaultMachineParameters
 toMachineParameters pv (EvaluationContext ll toSemVar machParsList) =
