c780fb223d585e420d7944cf0fce17696ad11301
Ana Pantilie <ana.pantilie95@gmail.com>
2024-11-15 20:24:10 +0200

    Fix nix preconfigure step

--- nix/agda.nix
+++ nix/agda.nix
@@ -14,12 +14,12 @@ rec {
     };
 
     # This is preConfigure is copied from more recent nixpkgs that also
-    # uses version 1.7 of standard-library. Old nixpkgs (that used 1.4)
+    # uses version 2.1.1 of standard-library. Old nixpkgs (that used 1.4)
     # had a preConfigure step that worked with 1.7. Less old nixpkgs
     # (that used 1.6) had a preConfigure step that attempts to `rm`
     # files that are now in the .gitignore list for 1.
     preConfigure = ''
-      runhaskell GenerateEverything.hs
+      runhaskell GenerateEverything.hs --include-deprecated
       # We will only build/consider Everything.agda, in particular we don't want Everything*.agda
       # do be copied to the store.
       rm EverythingSafe.agda
