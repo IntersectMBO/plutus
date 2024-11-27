60e9f582e4d9561ff56c004d753a23787edbf289
zeme <lorenzo.calegari@iohk.io>
2024-11-27 18:22:24 +0100

    wip

--- __benchmarks__/bench.sh
+++ __benchmarks__/bench.sh
@@ -1,7 +1,7 @@
 # RESP:PRED
 
-for i in {1..30}; do
-    cabal bench plutus-benchmark:validation \
+for i in {1..10}; do
+    taskset -c 1 cabal bench plutus-benchmark:validation \
       --benchmark-options="--csv bench-validation-auction_1-1.csv --regress cpuTime:cycles --regress numGcs:iters --regress cycles:time --regress allocated:iters --regress cycles:iters +RTS -T -RTS auction_1-1" | tee bench-validation-auction_1-1.txt; 
 done
 
@@ -34,6 +34,26 @@ So we removed "--regress numGcs:iters --regress allocated:iters"
 
 With more memory -A we get worse results
 
-On Monday run the benchmark with -rtsopts and -S, outside cabal and criterion 
+Then we run a 10 batch of auction_1-1 pinned to core 0,1,7 (see results)
 
-jeff has no meeting until thristda
+
+
+run_on_core() {
+  core="$1"
+  taskset -c $core cabal bench plutus-benchmark:validation \
+    --benchmark-options="--csv bench-validation-auction_1-1.csv --regress cpuTime:cycles --regress numGcs:iters --regress cycles:time --regress allocated:iters --regress cycles:iters +RTS -T -RTS auction_1-1" | tee bench-validation-auction_1-1.txt; 
+}
+
+sometimes the cpu cores do not get up to speed.
+either set every core to performance or pin the core
+or slow down a core and see if the results are consistent
+
+https://stackoverflow.com/questions/4713088/how-do-i-use-git-bisect
+https://www.kernel.org/doc/Documentation/cpu-freq/governors.txt
+https://wiki.archlinux.org/title/CPU_frequency_scaling#Scaling_governors
+
+non pessimised code: 
+
+https://haskell.foundation/hs-opt-handbook.github.io/src/Measurement_Observation/Core_Profiling/data_type_memory_footprint.html#memory-footprint-of-data-types-chapter
+
+https://youtu.be/IroPQ150F6c?si=jJL3cDMLw55OPf84
---
+++ plutus-benchmark/validation/bench/BenchCekNew.hs
@@ -0,0 +1,26 @@
+{- | Validation benchmarks for the CEK machine. -}
+module Main where
+
+import Common (benchTermCek, benchWith, mkEvalCtx, unsafeUnflat)
+import Control.Exception (evaluate)
+import PlutusBenchmark.Common (toNamedDeBruijnTerm)
+import PlutusCore.Default (BuiltinSemanticsVariant (DefaultFunSemanticsVariantA))
+import PlutusLedgerApi.Common (PlutusLedgerLanguage (PlutusV1))
+import UntypedPlutusCore as UPLC
+
+{-|
+ Benchmarks only for the CEK execution time of the data/*.flat validation scripts
+
+ Run the benchmarks.  You can run groups of benchmarks by typing things like
+     `stack bench -- plutus-benchmark:validation --ba crowdfunding`
+   or
+     `cabal bench -- plutus-benchmark:validation --benchmark-options crowdfunding`.
+-}
+main :: IO ()
+main = do
+  -- The validation benchmarks were all created with PlutusV1, so let's make
+  -- sure that the evaluation context matches.
+  evalCtx <- evaluate $ mkEvalCtx PlutusV1 DefaultFunSemanticsVariantA
+  let mkCekBM file program =
+          benchTermCek evalCtx . toNamedDeBruijnTerm . UPLC._progTerm $ unsafeUnflat file program
+  benchWith mkCekBM
