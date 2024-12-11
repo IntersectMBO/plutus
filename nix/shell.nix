9782e84f7665b7d1c9236a28e2f0a291949e57e6
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2024-12-11 10:34:23 +0100

    Add pkgs.sudo to the shell for use in ci-plutus-benchmarks.sh (#6740)

--- nix/shell.nix
+++ nix/shell.nix
@@ -22,9 +22,10 @@ let
     '';
 
 
-  papi-pkgs = lib.optionals pkgs.hostPlatform.isLinux [
+  linux-pkgs = lib.optionals pkgs.hostPlatform.isLinux [
     # Needed to fix the frequency and governor of the CPU running the benchmarks
     pkgs.cpufrequtils
+    pkgs.sudo
     # Underlying benchmarking library used by plutus-benchmark and tasty-papi
     pkgs.papi
   ];
@@ -83,7 +84,7 @@ in
   welcomeMessage = "🤟 \\033[1;34mWelcome to Plutus\\033[0m 🤟";
 
 
-  packages = lib.concatLists [ all-pkgs papi-pkgs ];
+  packages = lib.concatLists [ all-pkgs linux-pkgs ];
 
 
   scripts.assemble-changelog = {
