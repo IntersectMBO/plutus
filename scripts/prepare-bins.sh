c26f587c87e42d593c7474e2e607292faf1f0ca9
zeme <lorenzo.calegari@iohk.io>
2024-12-11 08:46:42 +0100

    WIP

--- scripts/prepare-bins.sh
+++ scripts/prepare-bins.sh
@@ -2,28 +2,11 @@
 
 set -euo pipefail
 
-banner='\n
-Lets prepare binaries for a release:\n
- 1. Build `pir`\n
- 2. Compress `pir` with `upx`\n
- 3. Build `uplc`\n
- 4. Compress `uplc` with `upx`\n
-'
+echo -e "Preparing static binaries for release..."
 
-echo -e $banner
-
-echo "Building pir..."
-
-nix build ".#hydraJobs.x86_64-linux.musl64.ghc96.pir"
-
-echo "Compressing pir..."
-
-upx -9 ./result/bin/pir -o pir-x86_64-linux-ghc96 --force-overwrite
-
-echo "Building uplc..."
-
-nix build ".#hydraJobs.x86_64-linux.musl64.ghc96.uplc"
-
-echo "Compressing uplc..."
-
-upx -9 ./result/bin/uplc -o uplc-x86_64-linux-ghc96 --force-overwrite
+for exec in "uplc pir plc"; do 
+  echo "Building $exec..."
+  nix build ".#hydraJobs.x86_64-linux.musl64.ghc96.$exec"
+  echo "Compressing $exec..."
+  upx -9 ./result/bin/$exec -o "$exec-x86_64-linux-ghc96" --force-overwrite
+done 
