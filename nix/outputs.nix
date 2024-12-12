a4cf669fd0361179c442d0c1f5bc3bde02987d67
zeme <lorenzo.calegari@iohk.io>
2024-12-12 14:34:05 +0100

    Formatted nix

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -10,9 +10,8 @@ let
   ghc98 = project.variants.ghc98;
   ghc810 = project.variants.ghc810;
   ghc910 = project.variants.ghc910;
-in
 
-[
+in [
   {
     inherit (project) cabalProject;
 
@@ -37,48 +36,49 @@ in
     packages.pre-commit-check = ghc96.pre-commit-check;
   }
 
-  (lib.optionalAttrs (system == "x86_64-linux" || system == "x86_64-darwin")
-    {
-      hydraJobs.plutus-metatheory-site = repoRoot.nix.plutus-metatheory-site;
+  (lib.optionalAttrs (system == "x86_64-linux" || system == "x86_64-darwin") {
+    hydraJobs.plutus-metatheory-site = repoRoot.nix.plutus-metatheory-site;
 
-      hydraJobs.ghc96 = ghc96.hydraJobs;
-      hydraJobs.ghc810 = ghc810.hydraJobs;
-      hydraJobs.ghc98 = ghc98.hydraJobs;
-      hydraJobs.ghc910 = ghc910.hydraJobs;
-    })
+    hydraJobs.ghc96 = ghc96.hydraJobs;
+    hydraJobs.ghc810 = ghc810.hydraJobs;
+    hydraJobs.ghc98 = ghc98.hydraJobs;
+    hydraJobs.ghc910 = ghc910.hydraJobs;
+  })
 
-  (lib.optionalAttrs (system == "x86_64-linux")
-    {
-      hydraJobs.latex-documents = repoRoot.nix.latex-documents;
-      hydraJobs.pre-commit-check = ghc96.pre-commit-check;
+  (lib.optionalAttrs (system == "x86_64-linux") {
+    hydraJobs.latex-documents = repoRoot.nix.latex-documents;
+    hydraJobs.pre-commit-check = ghc96.pre-commit-check;
 
-      hydraJobs.mingwW64.ghc96 = ghc96-mingwW64.hydraJobs;
+    hydraJobs.mingwW64.ghc96 = ghc96-mingwW64.hydraJobs;
 
-      hydraJobs.musl64.ghc96.pir = ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.pir; # editorconfig-checker-disable-line
-      hydraJobs.musl64.ghc96.plc = ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.plc; # editorconfig-checker-disable-line
-      hydraJobs.musl64.ghc96.uplc = ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.uplc; # editorconfig-checker-disable-line
-      hydraJobs.musl64.ghc96.plutus = ghc96-musl64.cabalProject.hsPkgs.plutus-core.components.exes.plutus; # editorconfig-checker-disable-line
-    })
+    hydraJobs.musl64.ghc96.pir =
+      ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.pir; # editorconfig-checker-disable-line
+    hydraJobs.musl64.ghc96.plc =
+      ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.plc; # editorconfig-checker-disable-line
+    hydraJobs.musl64.ghc96.uplc =
+      ghc96-musl64.cabalProject.hsPkgs.plutus-executables.components.exes.uplc; # editorconfig-checker-disable-line
+    hydraJobs.musl64.ghc96.plutus =
+      ghc96-musl64.cabalProject.hsPkgs.plutus-core.components.exes.plutus; # editorconfig-checker-disable-line
+  })
 
-  (lib.optionalAttrs (system == "aarch64-darwin")
-    {
-      # Plausibly if things build on x86 darwin then they'll build on aarch darwin.
-      # Se we only build roots and dev sshells on aarch to avoid overloading the builders.
-      hydraJobs.ghc810.devShell = ghc810.devShell;
-      hydraJobs.ghc96.devShell = ghc96.devShell;
-      hydraJobs.ghc98.devShell = ghc98.devShell;
-      hydraJobs.ghc910.devShell = ghc910.devShell;
+  (lib.optionalAttrs (system == "aarch64-darwin") {
+    # Plausibly if things build on x86 darwin then they'll build on aarch darwin.
+    # Se we only build roots and dev sshells on aarch to avoid overloading the builders.
+    hydraJobs.ghc810.devShell = ghc810.devShell;
+    hydraJobs.ghc96.devShell = ghc96.devShell;
+    hydraJobs.ghc98.devShell = ghc98.devShell;
+    hydraJobs.ghc910.devShell = ghc910.devShell;
 
-      hydraJobs.ghc810.roots = ghc810.hydraJobs.roots;
-      hydraJobs.ghc810.plan-nix = ghc810.hydraJobs.plan-nix;
+    hydraJobs.ghc810.roots = ghc810.hydraJobs.roots;
+    hydraJobs.ghc810.plan-nix = ghc810.hydraJobs.plan-nix;
 
-      hydraJobs.ghc96.roots = ghc96.hydraJobs.roots;
-      hydraJobs.ghc96.plan-nix = ghc96.hydraJobs.plan-nix;
+    hydraJobs.ghc96.roots = ghc96.hydraJobs.roots;
+    hydraJobs.ghc96.plan-nix = ghc96.hydraJobs.plan-nix;
 
-      hydraJobs.ghc98.roots = ghc98.hydraJobs.roots;
-      hydraJobs.ghc98.plan-nix = ghc98.hydraJobs.plan-nix;
+    hydraJobs.ghc98.roots = ghc98.hydraJobs.roots;
+    hydraJobs.ghc98.plan-nix = ghc98.hydraJobs.plan-nix;
 
-      hydraJobs.ghc910.roots = ghc910.hydraJobs.roots;
-      hydraJobs.ghc910.plan-nix = ghc910.hydraJobs.plan-nix;
-    })
+    hydraJobs.ghc910.roots = ghc910.hydraJobs.roots;
+    hydraJobs.ghc910.plan-nix = ghc910.hydraJobs.plan-nix;
+  })
 ]
--- nix/project.nix
+++ nix/project.nix
@@ -5,10 +5,11 @@
 let
   cabalProject = pkgs.haskell-nix.cabalProject' ({ config, pkgs, ... }:
     let
-      isCompilingMingwW64 = pkgs.stdenv.hostPlatform.system == "x86_64-windows" && pkgs.stdenv.hostPlatform.libc == "msvcrt";
-      isCompilingMusl64 = pkgs.stdenv.hostPlatform.system == "x86_64-linux" && pkgs.stdenv.hostPlatform.libc == "musl";
-    in
-    {
+      isCompilingMingwW64 = pkgs.stdenv.hostPlatform.system == "x86_64-windows"
+        && pkgs.stdenv.hostPlatform.libc == "msvcrt";
+      isCompilingMusl64 = pkgs.stdenv.hostPlatform.system == "x86_64-linux"
+        && pkgs.stdenv.hostPlatform.libc == "musl";
+    in {
       name = "plutus";
 
       # We need the mkDefault here since compiler-nix-name will be overridden
@@ -37,21 +38,22 @@ let
         ghc910.compiler-nix-name = "ghc910";
       };
 
-      inputMap = { "https://chap.intersectmbo.org/" = inputs.iogx.inputs.CHaP; };
+      inputMap = {
+        "https://chap.intersectmbo.org/" = inputs.iogx.inputs.CHaP;
+      };
 
       sha256map = {
-        "https://github.com/jaccokrijnen/plutus-cert"."e814b9171398cbdfecdc6823067156a7e9fc76a3" = "0srqvx0b819b5crrbsa9hz2fnr50ahqizvvm0wdmyq2bbpk2rka7";
+        "https://github.com/jaccokrijnen/plutus-cert"."e814b9171398cbdfecdc6823067156a7e9fc76a3" =
+          "0srqvx0b819b5crrbsa9hz2fnr50ahqizvvm0wdmyq2bbpk2rka7";
       };
 
       modules = [
 
-        (
-          lib.mkIf (!isCompilingMingwW64 && !isCompilingMusl64) repoRoot.nix.agda.agda-project-module-patch-default
-        )
+        (lib.mkIf (!isCompilingMingwW64 && !isCompilingMusl64)
+          repoRoot.nix.agda.agda-project-module-patch-default)
 
-        (
-          lib.mkIf isCompilingMusl64 repoRoot.nix.agda.agda-project-module-patch-musl64
-        )
+        (lib.mkIf isCompilingMusl64
+          repoRoot.nix.agda.agda-project-module-patch-musl64)
 
         # Common
         {
@@ -59,17 +61,23 @@ let
             # plutus-metatheory needs agda with the stdlib around for the custom setup
             # I can't figure out a way to apply this as a blanket change for all the
             # components in the package, oh well
-            plutus-metatheory.components.library.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
-            plutus-metatheory.components.exes.plc-agda.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
-            plutus-metatheory.components.tests.test-NEAT.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-metatheory.components.library.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-metatheory.components.exes.plc-agda.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-metatheory.components.tests.test-NEAT.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
 
-            plutus-executables.components.exes.uplc.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-executables.components.exes.uplc.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
             # plutus-executables.components.exes.uplc.postInstall = ''
             #   wrapProgram $out/bin/uplc ${repoRoot.nix.agda.wrap-program-args}
             # '';
 
-            plutus-executables.components.tests.test-simple.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
-            plutus-executables.components.tests.test-detailed.build-tools = [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-executables.components.tests.test-simple.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
+            plutus-executables.components.tests.test-detailed.build-tools =
+              [ repoRoot.nix.agda.agda-with-stdlib ];
 
             plutus-core.components.benchmarks.update-cost-model = {
               build-tools = [ repoRoot.nix.r-with-packages ];
@@ -87,8 +95,8 @@ let
               let
                 ocamlPkgs = pkgs.ocaml-ng.ocamlPackages_4_10;
                 coqPkgs = pkgs.coqPackages_8_13;
-              in
-              with ocamlPkgs; with coqPkgs; [
+              in with ocamlPkgs;
+              with coqPkgs; [
                 pkgs.perl
                 ocaml
                 ocamlbuild
@@ -101,11 +109,15 @@ let
               ];
 
             plutus-core.components.tests.plutus-core-test.postInstall = ''
-              wrapProgram $out/bin/plutus-core-test --set PATH ${lib.makeBinPath [ pkgs.diffutils ]}
+              wrapProgram $out/bin/plutus-core-test --set PATH ${
+                lib.makeBinPath [ pkgs.diffutils ]
+              }
             '';
 
             plutus-core.components.tests.plutus-ir-test.postInstall = ''
-              wrapProgram $out/bin/plutus-ir-test --set PATH ${lib.makeBinPath [ pkgs.diffutils ]}
+              wrapProgram $out/bin/plutus-ir-test --set PATH ${
+                lib.makeBinPath [ pkgs.diffutils ]
+              }
             '';
 
             # We want to build it but not run the tests in CI.
@@ -137,12 +149,9 @@ let
       ];
     });
 
-
   project = lib.iogx.mkHaskellProject {
     inherit cabalProject;
     shellArgs = repoRoot.nix.shell;
   };
 
-in
-
-project
+in project
