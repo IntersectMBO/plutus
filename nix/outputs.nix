4555bca7259bfbc10e1eab2d1a5a9b760e8ce234
zeme <lorenzo.calegari@iohk.io>
2025-01-15 13:07:11 +0100

    Remove ghc98 and ghc910 from nix builds

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -7,9 +7,7 @@ let
   ghc96-mingwW64 = project.variants.ghc96.cross.mingwW64;
   ghc96-musl64 = project.variants.ghc96.cross.musl64;
   ghc96-profiled = project.variants.ghc96-profiled;
-  ghc98 = project.variants.ghc98;
   ghc810 = project.variants.ghc810;
-  ghc910 = project.variants.ghc910;
 
 in [
   {
@@ -20,12 +18,9 @@ in [
     devShells.profiled = ghc96-profiled.devShell;
     devShells.ghc96 = ghc96.devShell;
     devShells.ghc810 = ghc810.devShell;
-    devShells.ghc98 = ghc98.devShell;
-    devShells.ghc910 = ghc910.devShell;
 
     packages = ghc96.packages;
     apps = ghc96.apps;
-    # checks = ghc96.checks;
 
     latex-documents = repoRoot.nix.latex-documents;
 
@@ -42,8 +37,6 @@ in [
 
     hydraJobs.ghc96 = ghc96.hydraJobs;
     hydraJobs.ghc810 = ghc810.hydraJobs;
-    hydraJobs.ghc98 = ghc98.hydraJobs;
-    hydraJobs.ghc910 = ghc910.hydraJobs;
   })
 
   (lib.optionalAttrs (system == "x86_64-linux") {
@@ -67,20 +60,12 @@ in [
     # Se we only build roots and dev sshells on aarch to avoid overloading the builders.
     hydraJobs.ghc810.devShell = ghc810.devShell;
     hydraJobs.ghc96.devShell = ghc96.devShell;
-    hydraJobs.ghc98.devShell = ghc98.devShell;
-    hydraJobs.ghc910.devShell = ghc910.devShell;
 
     hydraJobs.ghc810.roots = ghc810.hydraJobs.roots;
     hydraJobs.ghc810.plan-nix = ghc810.hydraJobs.plan-nix;
 
     hydraJobs.ghc96.roots = ghc96.hydraJobs.roots;
     hydraJobs.ghc96.plan-nix = ghc96.hydraJobs.plan-nix;
-
-    hydraJobs.ghc98.roots = ghc98.hydraJobs.roots;
-    hydraJobs.ghc98.plan-nix = ghc98.hydraJobs.plan-nix;
-
-    hydraJobs.ghc910.roots = ghc910.hydraJobs.roots;
-    hydraJobs.ghc910.plan-nix = ghc910.hydraJobs.plan-nix;
   })
   {
     checks = repoRoot.nix.utils.flattenDerivationTree "ci" "-"
