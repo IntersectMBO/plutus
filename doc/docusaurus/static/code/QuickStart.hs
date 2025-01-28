fc00f5dc37661373a27f01e99d8c983f70454c3e
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2025-01-28 11:06:21 +0100

    Update AuctionValidator to use PlutusLedgerApi.V3

--- doc/docusaurus/static/code/QuickStart.hs
+++ doc/docusaurus/static/code/QuickStart.hs
@@ -10,17 +10,33 @@ import Data.ByteString.Short qualified as B
 import PlutusLedgerApi.V2 qualified as V2
 
 main :: IO ()
-main = B.writeFile "validator.uplc" . Base16.encode $ B.fromShort serialisedScript
-  where
-    script = auctionValidatorScript params
-    serialisedScript = V2.serialiseCompiledCode script
-    params =
-      AuctionParams
-        { apSeller = error "Replace with seller's wallet address"
-        , -- The asset to be auctioned is 10000 lovelaces
-          apAsset = V2.singleton V2.adaSymbol V2.adaToken 10000
-        , -- The minimum bid is 100 lovelaces
-          apMinBid = 100
-        , apEndTime = error "Replace with your desired end time"
-        }
+main =
+  B.writeFile "validator.uplc.hex"
+    $ Base16.encode
+    $ BS.fromShort
+    $ serialiseCompiledCode
+    $ auctionValidatorScript AuctionParams
+      { apSeller =
+          -- Replace with the hex-encoded seller's public key hash:
+          Crypto.PubKeyHash (
+            stringToBuiltinByteStringHex
+              "0000000000000000000000000000000000000000\
+              \0000000000000000000000000000000000000000"
+            )
+      , apCurrencySymbol =
+          -- Replace with your desired currency symbol (minting policy hash):
+          Value.CurrencySymbol (
+            stringToBuiltinByteStringHex
+              "00000000000000000000000000000000000000000000000000000000"
+            )
+      , apTokenName =
+          -- Replace with your desired token name:
+          Value.tokenName "MY_TOKEN"
+      , apMinBid =
+          -- Minimal bid in lovelace:
+          100
+      , apEndTime =
+          -- Replace with your desired end time in milliseconds:
+          Time.fromMilliSeconds 1_725_227_091_000
+      }
 -- BLOCK2
