1cece6b21e531ddf050642e2028eee537b8609de
Ziyang Liu <unsafeFixIO@gmail.com>
2025-02-28 18:58:15 -0800

    Allow `PlutusTx.Optimize.Inline.inline` to inline local bindings (#6882)

--- plutus-tx-plugin/test/Inline/Spec.hs
+++ plutus-tx-plugin/test/Inline/Spec.hs
@@ -1,9 +1,9 @@
+{-# LANGUAGE BangPatterns    #-}
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:defer-errors #-}
 
 module Inline.Spec where
-
 
 import System.FilePath
 
@@ -86,6 +86,11 @@ tests =
           )
       , goldenPirReadable "recursive" recursive
       , goldenUPlcReadable "recursive" recursive
+      , goldenPirReadable "inlineLocalOnce" compiledInlineLocalOnce
+      , goldenUPlcReadable "inlineLocalOnce" compiledInlineLocalOnce
+      , goldenEvalCekCatch
+          "inlineLocalOnce"
+          [compiledInlineLocalOnce `unsafeApplyCode` liftCodeDef 2]
       ]
 
 double :: Integer -> Integer
@@ -97,6 +102,18 @@ factorial x
   | x `PlutusTx.equalsInteger` 0 = 1
   | otherwise = x `PlutusTx.multiplyInteger` factorial (x `PlutusTx.subtractInteger` 1)
 {-# INLINEABLE factorial #-}
+
+{-| This test case verifies that `inline` can inline local bindings
+(like `square`).
+
+The third usage of `square` is inlined in PIR, but not in UPLC, since
+in UPLC the inlining is reversed by CSE.
+-}
+inlineLocalOnce :: Integer -> Integer
+inlineLocalOnce x = square `PlutusTx.addInteger` square `PlutusTx.addInteger` inline square
+  where
+    !square = x `PlutusTx.multiplyInteger` x
+{-# INLINEABLE inlineLocalOnce #-}
 
 notInlined :: CompiledCode (Integer -> Integer -> Integer -> Integer)
 notInlined =
@@ -155,3 +172,6 @@ recursive =
             `PlutusTx.multiplyInteger` inline factorial z
         ||]
     )
+
+compiledInlineLocalOnce :: CompiledCode (Integer -> Integer)
+compiledInlineLocalOnce = $$(compile [||inlineLocalOnce||])
