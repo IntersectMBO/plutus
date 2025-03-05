ba03bd744cadc9147703fedf97e84dbe9a583b7b
kwxm <kenneth.mackenzie@iohk.io>
2025-03-05 00:34:56 +0000

    Bigger y

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
@@ -54,7 +54,7 @@ benchExpModInteger _gen =
   in createThreeTermBuiltinBenchWithWrappers
      (IntegerCostedByLog, IntegerCostedByLog, IntegerCostedByLog)
      builtinName []
-     (fmap (\n -> n) inputs) (fmap (\n -> 20*p+411*n+332341241) inputs) moduli
+     (fmap (\n -> n) inputs) (fmap (\n -> 20*p*p+411*n+332341241*p) inputs) moduli
 
 makeBenchmarks :: StdGen -> [Benchmark]
 makeBenchmarks gen =
