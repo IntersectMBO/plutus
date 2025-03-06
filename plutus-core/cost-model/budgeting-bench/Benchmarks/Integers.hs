2d8c026346e3f5f3fe05e8a9f9460a4ddee88aa7
kwxm <kenneth.mackenzie@iohk.io>
2025-03-06 23:13:09 +0000

    Modulus = 2^255

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
@@ -47,8 +47,8 @@ benchExpModInteger :: StdGen -> Benchmark
 benchExpModInteger _gen =
   let builtinName = ExpModInteger
       pow (a::Integer) (b::Integer) = a^b
-      p = (pow 2 255) + 400
-      -- = 2^4 × 3 × 9907 × 644977 × 97 674011
+      p = (pow 2 255)
+      -- 2^255 + 400 = 2^4 × 3 × 9907 × 644977 × 97 674011
       --   × 1932 601194 339139 344835 240473 879578 700967 872768 315843 651779
 --      d = p `div` 20
       inputs = fmap (\n -> 2^n-1) [1,10..255::Integer]
@@ -57,7 +57,8 @@ benchExpModInteger _gen =
      (IntegerCostedByLog, IntegerCostedByLog, IntegerCostedByLog)
      builtinName []
      (fmap (\n -> n) inputs)
-     (fmap (\n -> (pow 2 1000)*n) inputs)
+     (fmap (\n -> n) inputs)
+--     (fmap (\n -> (pow 2 1000)*n) inputs)
      moduli
 
 makeBenchmarks :: StdGen -> [Benchmark]
