73850fdc575b855c9a9f57112645011189446c0e
kwxm <kenneth.mackenzie@iohk.io>
2025-03-07 00:55:08 +0000

    Modulus = 2^255, big Y

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
@@ -57,8 +57,8 @@ benchExpModInteger _gen =
      (IntegerCostedByLog, IntegerCostedByLog, IntegerCostedByLog)
      builtinName []
      (fmap (\n -> n) inputs)
-     (fmap (\n -> n) inputs)
---     (fmap (\n -> (pow 2 1000)*n) inputs)
+--     (fmap (\n -> n) inputs)
+     (fmap (\n -> (pow 2 1000)*n) inputs)
      moduli
 
 makeBenchmarks :: StdGen -> [Benchmark]
