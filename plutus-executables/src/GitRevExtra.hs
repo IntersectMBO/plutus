467cfd704fc1e8e28875efd10c44b124995de42a
zeme <lorenzo.calegari@iohk.io>
2025-03-12 18:41:09 +0000

    done

---
+++ plutus-executables/src/GitRevExtra.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module GitRevExtra
+  ( gitBranch
+  , gitHash
+  , gitCommitDate
+  , gitCommitCount
+  ) where
+
+import Control.Monad.Fail qualified as Control.Monad.Fail
+import Development.GitRev qualified as GitRev
+import Language.Haskell.TH qualified as TH
+import Paths_plutus_executables qualified as Paths_plutus_executables
+import System.Environment qualified as System.Environment
+
+
+data VersionVariable
+  = GitBranch
+  | GitHash
+  | GitCommitDate
+  | GitCommitCount
+  deriving stock (Show, Eq)
+
+
+gitBranch :: TH.ExpQ
+gitBranch = TH.stringE =<< getVersionVariable GitBranch
+
+
+gitHash :: TH.ExpQ
+gitHash = TH.stringE =<< getVersionVariable GitHash
+
+
+gitCommitDate :: TH.ExpQ
+gitCommitDate = TH.stringE =<< getVersionVariable GitCommitDate
+
+
+gitCommitCount :: TH.ExpQ
+gitCommitCount = TH.stringE =<< getVersionVariable GitCommitCount
+
+
+getVersionVariable :: VersionVariable -> TH.Q String
+getVersionVariable verVar = do
+  valueFromEnv <- getValueFromEnv
+  case (valueFromGit, valueFromEnv) of
+    ("UNKNOWN", Just v) ->
+      return v
+    ("UNKNOWN", Nothing) ->
+      noValueFound
+    (v, Just v') | v /= v' ->
+      inconsistentValues v v'
+    (v, _) ->
+      return v
+  where
+    valueFromGit :: String
+    valueFromGit = case verVar of
+      GitHash       -> $(GitRev.gitHash)
+      GitCommitDate -> $(GitRev.gitCommitDate)
+
+    getValueFromEnv :: TH.Q (Maybe String)
+    getValueFromEnv = lookupEnvQ envVarName
+
+    envVarName :: String
+    envVarName = case verVar of
+      GitBranch      -> "GIT_BRANCH"
+      GitHash        -> "GIT_HASH"
+      GitCommitDate  -> "GIT_COMMIT_DATE"
+      GitCommitCount -> "GIT_COMMIT_COUNT"
+
+    displayName :: String
+    displayName = case verVar of
+      GitBranch      -> "branch name"
+      GitHash        -> "commit hash"
+      GitCommitDate  -> "commit date"
+      GitCommitCount -> "commit count"
+
+    lookupEnvQ :: String -> TH.Q (Maybe String)
+    lookupEnvQ = TH.runIO . System.Environment.lookupEnv
+
+    inconsistentValues :: String -> String -> TH.Q String
+    inconsistentValues v v' =
+      Control.Monad.Fail.fail $
+        "Inconsistent values for the "
+          <> displayName
+          <> ": git reports "
+          <> v
+          <> " while the env var "
+          <> envVarName
+          <> " is set to "
+          <> v'
+          <> "."
+
+    noValueFound :: TH.Q String
+    noValueFound =
+      Control.Monad.Fail.fail $
+        "No value found for the "
+          <> displayName
+          <> " neither from git nor from the env var "
+          <> envVarName
+          <> "."
--- plutus-executables/src/VersionInfo.hs
+++ plutus-executables/src/VersionInfo.hs
@@ -1,93 +1,25 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module VersionInfo where
+module VersionInfo
+  ( makeVersionInfo
+  ) where
 
-import Control.Monad.Fail qualified as Control.Monad.Fail
-import Development.GitRev qualified as GitRev
+
+import Data.Version qualified as Data.Version
+import GitRevExtra qualified as GitRevExtra
 import Language.Haskell.TH qualified as TH
-import System.Environment qualified as System.Environment
+import Paths_plutus_executables qualified as Paths_plutus_executables
 
 
-data VersionVariable
-  = GitBranch
-  | GitHash
-  | GitCommitDate
-  deriving stock (Show, Eq)
-
-
-defaultVersionInfo :: TH.ExpQ
-defaultVersionInfo = do
-  branch <- getVersionVariable GitBranch
-  hash <- getVersionVariable GitHash
-  commitDate <- getVersionVariable GitCommitDate
-  TH.stringE (branch <> "@" <> hash <> " (" <> commitDate <> ")")
-
-
-gitBranch :: TH.ExpQ
-gitBranch = TH.stringE =<< getVersionVariable GitBranch
-
-
-gitHash :: TH.ExpQ
-gitHash = TH.stringE =<< getVersionVariable GitHash
-
-
-gitCommitDate :: TH.ExpQ
-gitCommitDate = TH.stringE =<< getVersionVariable GitCommitDate
-
-
-getVersionVariable :: VersionVariable -> TH.Q String
-getVersionVariable verVar = do
-  valueFromNix <- getValueFromNix
-  case (valueFromGit, valueFromNix) of
-    ("UNKNOWN", Just v) ->
-      return v
-    ("UNKNOWN", Nothing) ->
-      Control.Monad.Fail.fail $
-        "Neither git nor nix knows the " <> displayName <> "."
-    (v, Just v') | v == v' ->
-      Control.Monad.Fail.fail $
-        "git (" <> v <> ") and nix (" <> v' <> ") disagree on the " <> displayName <> "."
-    (v, _) ->
-      return v
-  where
-    valueFromGit :: String
-    valueFromGit = case verVar of
-      GitBranch     -> $(GitRev.gitBranch)
-      GitHash       -> $(GitRev.gitHash)
-      GitCommitDate -> $(GitRev.gitCommitDate)
-
-    getValueFromNix :: TH.Q (Maybe String)
-    getValueFromNix = case verVar of
-      GitBranch     -> lookupEnvVarQ "GIT_BRANCH"
-      GitHash       -> lookupEnvVarQ "GIT_HASH"
-      GitCommitDate -> lookupEnvVarQ "GIT_COMMIT_DATE"
-
-    displayName :: String
-    displayName = case verVar of
-      GitBranch     -> "branch name"
-      GitHash       -> "commit hash"
-      GitCommitDate -> "commit date"
-
-    lookupEnvVarQ :: String -> TH.Q (Maybe String)
-    lookupEnvVarQ = TH.runIO . System.Environment.lookupEnv
-
-
-
-    -- failWith :: String -> a
-    -- failWith msg = do
-    --   System.IO.hPutStrLn System.IO.stderr msg
-    --   System.Exit.exitFailure
-
-    -- unsafeReadEnvVar :: String -> String
-    -- unsafeReadEnvVar
-    --   = System.Unsafe.IO.unsafeInterleaveIO
-    --   . System.Environment.getEnv
-
-
-
--- getEnvQ :: String -> TH.ExpQ
--- getEnvQ varName = do
---   varVal <- TH.runIO (System.Environment.lookupEnv varName)
---   [|Just varVal|]
-
-
+makeVersionInfo :: String -> TH.ExpQ
+makeVersionInfo execName = do
+  let version = Data.Version.showVersion Paths_plutus_executables.version
+  [| execName
+      <> " version "
+      <> version
+      <> " - git rev "
+      <> $(GitRevExtra.gitHash)
+      <> " ("
+      <> $(GitRevExtra.gitCommitDate)
+      <> ")"
+    |]
