1037c65763744c43665ff7ed7586c11c95e69cc6
Ana Pantilie <ana.pantilie95@gmail.com>
2025-03-14 14:30:01 +0200

    Fix the warnings in the metatheory

--- plutus-metatheory/src/Raw.hs
+++ plutus-metatheory/src/Raw.hs
@@ -98,7 +98,9 @@ convTyCon (SomeTypeIn DefaultUniBLS12_381_G2_Element) = RTyCon (RTyConAtom ATyCo
 convTyCon (SomeTypeIn DefaultUniBLS12_381_MlResult)   = RTyCon (RTyConAtom ATyConBLS12_381_MlResult)
 convTyCon (SomeTypeIn DefaultUniProtoList)            = RTyCon RTyConList
 convTyCon (SomeTypeIn DefaultUniProtoPair)            = RTyCon RTyConPair
-convTyCon _                                           = error "unsupported builtin type application"
+convTyCon (SomeTypeIn (DefaultUniApply _ _))          = error "unsupported builtin type application"
+-- TODO: this should be fixed once the metatheory supports builtin arrays
+convTyCon (SomeTypeIn DefaultUniProtoArray)           = error "unsupported builtin array"
 
 conv :: Term NamedTyDeBruijn NamedDeBruijn DefaultUni DefaultFun a -> RTerm
 conv (Var _ x)           = RVar (unIndex (ndbnIndex x))
