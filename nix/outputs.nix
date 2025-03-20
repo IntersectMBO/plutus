67554d13162c8c7c5335cf4d7497d4ae1b80ddde
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2025-03-20 10:48:53 +0000

    Make the Git revision available to pir/uplc/plc programs (#6949)

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -36,7 +36,7 @@ let
     { inherit self pkgs; };
 
   project = import ./project.nix
-    { inherit inputs pkgs lib agda-with-stdlib r-with-packages; };
+    { inherit inputs pkgs lib agda-with-stdlib r-with-packages utils; };
 
   mkShell = project: import ./shell.nix
     { inherit inputs pkgs lib project agda-with-stdlib r-with-packages; };
@@ -122,6 +122,7 @@ let
   hydraJobs = ciJobs;
 
   __internal = {
+    inherit self;
     inherit pkgs;
     inherit project;
     inherit agda-with-stdlib;
