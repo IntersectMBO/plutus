4d0cfe3b12f94a61108408d91d49fa4909013b47
Yura <yuriy.lazaryev@iohk.io>
2025-03-21 18:17:50 +0100

    refactor: extract test lib from the Purity spec

--- plutus-core/untyped-plutus-core/test/Analysis/Spec.hs
+++ plutus-core/untyped-plutus-core/test/Analysis/Spec.hs
@@ -2,58 +2,12 @@
 
 module Analysis.Spec where
 
-import Test.Tasty.Extras
+import Analysis.Spec.Lib
 
-import PlutusCore qualified as PLC
-import PlutusCore.MkPlc
-import PlutusCore.Pretty (prettyPlcReadable)
-import PlutusCore.Quote
-import PlutusPrelude (def)
-import Test.Tasty
-import Test.Tasty.HUnit
-import UntypedPlutusCore
-import UntypedPlutusCore.Purity
-
-goldenEvalOrder
-  :: String
-  -> Term Name PLC.DefaultUni PLC.DefaultFun ()
-  -> TestNested
-goldenEvalOrder name tm =
-  nestedGoldenVsDoc name "" (prettyPlcReadable $ termEvaluationOrder def tm)
-
--- Should hit Unknown before trying to process the undefined. Shows
--- that the computation is lazy
--- [ [ n m ] (constr 1 [undefined]) ]
-dangerTerm :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-dangerTerm = runQuote $ do
-  n <- freshName "n"
-  m <- freshName "m"
-  -- The UPLC term type is strict, so it's hard to hide an undefined in there
-  -- Take advantage of the fact that it's still using lazy lists for constr
-  -- arguments for now.
-  pure $ Apply () (Apply () (Var () n) (Var () m)) (Constr () 1 [undefined])
-
-letFun :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-letFun = runQuote $ do
-  n <- freshName "n"
-  let intConst = mkConstant () (1 :: Integer)
-  pure $
-    Apply
-      ()
-      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
-      (Builtin () PLC.AddInteger)
-
-letImpure :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-letImpure = runQuote $ do
-  n <- freshName "n"
-  m <- freshName "m"
-  let intConst = mkConstant () (1 :: Integer)
-  pure $
-    Apply
-      ()
-      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
-      -- don't need this to be well-scoped
-      (Apply () (Var () m) intConst)
+import Test.Tasty (TestTree)
+import Test.Tasty.Extras (embed, runTestNested)
+import Test.Tasty.HUnit (testCase, (@=?))
+import UntypedPlutusCore.Purity (termEvaluationOrder, unEvalOrder)
 
 evalOrder :: TestTree
 evalOrder =
@@ -61,6 +15,12 @@ evalOrder =
     ["untyped-plutus-core", "test", "Analysis", "evalOrder"]
     [ goldenEvalOrder "letFun" letFun
     , goldenEvalOrder "letImpure" letImpure
-    , embed . testCase "evalOrderLazy" $
-        4 @=? length (unEvalOrder $ termEvaluationOrder def dangerTerm)
+    , embed testEvalOrderIsLazy
     ]
+
+testEvalOrderIsLazy :: TestTree
+testEvalOrderIsLazy =
+  testCase "evalOrderLazy" $
+    let order = termEvaluationOrder builtinSemantics dangerTerm
+        subterms = unEvalOrder order
+     in 4 @=? length subterms
