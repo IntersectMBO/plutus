f810009b2320e419d15e1f622d008dca79839b3e
Yura <1009751+Unisay@users.noreply.github.com>
2025-04-08 16:00:48 +0200

    Proper purity check for unsaturated builtins (#6974)
    * refactor: extract test lib from the Purity spec
    
    * UPLC term purity tests and fixes
    
    * refactor (formatting): wrap a long line
    
    * refactor: replace [Arg] with AppCtx to make builtin purity similar for IR and UPLC
    
    * Purity IR: ifThenElse test
    
    * refactor: move AppCtx  and related functions to `Contexts` module.
    
    * feat: use pretty-simple for prettier equality assertions
    
    * tests: specify expected eval order
    
    * fix: evaluation order in purity evaluation
    
    * test: update golden files for budget evaluations and UPLC programs
    
    * s/eval/evalThis/
    
    * Rename Analysis.Spec.Lib to Analysis.Lib
    
    * test: add golden file for if-then-else evaluation and remove redundant test case
    
    * testApplyNoTermParam has 2 cases in it
    
    * Changelog entry.

---
+++ plutus-core/untyped-plutus-core/test/Analysis/Lib.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Analysis.Lib where
+
+import Data.Text qualified as Text
+import Numeric.Natural (Natural)
+import PlutusCore.Builtin (BuiltinSemanticsVariant)
+import PlutusCore.Default (DefaultFun, DefaultUni)
+import PlutusCore.Default.Builtins (DefaultFun (..))
+import PlutusCore.MkPlc (mkConstant, mkIterAppNoAnn)
+import PlutusCore.Name.Unique (Name (..), Unique (..))
+import PlutusCore.Pretty (prettyPlcReadable)
+import PlutusCore.Quote (freshName, runQuote)
+import PlutusPrelude (def)
+import Test.Tasty.Extras (TestNested, nestedGoldenVsDoc)
+import UntypedPlutusCore.Core.Type (Term (..))
+import UntypedPlutusCore.Purity (termEvaluationOrder)
+
+builtinSemantics :: BuiltinSemanticsVariant DefaultFun
+builtinSemantics = def
+
+goldenEvalOrder :: String -> Term Name DefaultUni DefaultFun () -> TestNested
+goldenEvalOrder name tm =
+  let order = termEvaluationOrder builtinSemantics tm
+   in nestedGoldenVsDoc name "" (prettyPlcReadable order)
+
+--------------------------------------------------------------------------------
+-- Terms for testing -----------------------------------------------------------
+
+termIfThenElse :: Term Name DefaultUni DefaultFun ()
+termIfThenElse =
+  Apply
+    ()
+    ( Apply
+        ()
+        (Apply () (Force () (Builtin () IfThenElse)) (termVar 1))
+        (termVar 2)
+    )
+    (termVar 3)
+
+termVar :: Natural -> Term Name DefaultUni DefaultFun ()
+termVar n =
+  Var () (Name ("var" <> Text.pack (show n)) (Unique (fromIntegral n)))
+
+-- that the computation is lazy
+-- [ [ n m ] (constr 1 [undefined]) ]
+dangerTerm :: Term Name DefaultUni DefaultFun ()
+dangerTerm = runQuote $ do
+  n <- freshName "n"
+  m <- freshName "m"
+  -- The UPLC term type is strict, so it's hard to hide an undefined in there
+  -- Take advantage of the fact that it's still using lazy lists for constr
+  -- arguments for now.
+  pure $ Apply () (Apply () (Var () n) (Var () m)) (Constr () 1 [undefined])
+
+letFun :: Term Name DefaultUni DefaultFun ()
+letFun = runQuote $ do
+  n <- freshName "n"
+  let intConst = mkConstant () (1 :: Integer)
+  pure $
+    Apply
+      ()
+      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
+      (Builtin () AddInteger)
+
+letImpure :: Term Name DefaultUni DefaultFun ()
+letImpure = runQuote $ do
+  n <- freshName "n"
+  m <- freshName "m"
+  let intConst = mkConstant () (1 :: Integer)
+  pure $
+    Apply
+      ()
+      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
+      -- don't need this to be well-scoped
+      (Apply () (Var () m) intConst)
--- plutus-core/untyped-plutus-core/test/Analysis/Spec.hs
+++ plutus-core/untyped-plutus-core/test/Analysis/Spec.hs
@@ -1,59 +1,17 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Analysis.Spec where
 
-import Test.Tasty.Extras
-
-import PlutusCore qualified as PLC
-import PlutusCore.MkPlc
-import PlutusCore.Pretty (prettyPlcReadable)
-import PlutusCore.Quote
-import PlutusPrelude (def)
-import Test.Tasty
-import Test.Tasty.HUnit
-import UntypedPlutusCore
-import UntypedPlutusCore.Purity
-
-goldenEvalOrder
-  :: String
-  -> Term Name PLC.DefaultUni PLC.DefaultFun ()
-  -> TestNested
-goldenEvalOrder name tm =
-  nestedGoldenVsDoc name "" (prettyPlcReadable $ termEvaluationOrder def tm)
-
--- Should hit Unknown before trying to process the undefined. Shows
--- that the computation is lazy
--- [ [ n m ] (constr 1 [undefined]) ]
-dangerTerm :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-dangerTerm = runQuote $ do
-  n <- freshName "n"
-  m <- freshName "m"
-  -- The UPLC term type is strict, so it's hard to hide an undefined in there
-  -- Take advantage of the fact that it's still using lazy lists for constr
-  -- arguments for now.
-  pure $ Apply () (Apply () (Var () n) (Var () m)) (Constr () 1 [undefined])
-
-letFun :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-letFun = runQuote $ do
-  n <- freshName "n"
-  let intConst = mkConstant () (1 :: Integer)
-  pure $
-    Apply
-      ()
-      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
-      (Builtin () PLC.AddInteger)
-
-letImpure :: Term Name PLC.DefaultUni PLC.DefaultFun ()
-letImpure = runQuote $ do
-  n <- freshName "n"
-  m <- freshName "m"
-  let intConst = mkConstant () (1 :: Integer)
-  pure $
-    Apply
-      ()
-      (LamAbs () n (mkIterAppNoAnn (Var () n) [intConst, intConst]))
-      -- don't need this to be well-scoped
-      (Apply () (Var () m) intConst)
+import Analysis.Lib
+import PlutusCore.Default (DefaultFun (..), DefaultUni)
+import PlutusCore.Name.Unique (Name (..))
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.Extras (embed, runTestNested)
+import Test.Tasty.HUnit (testCase, (@=?), (@?=))
+import UntypedPlutusCore (Term (Apply, Builtin, Force))
+import UntypedPlutusCore.Purity (isPure, termEvaluationOrder, unEvalOrder)
 
 evalOrder :: TestTree
 evalOrder =
@@ -61,6 +19,117 @@ evalOrder =
     ["untyped-plutus-core", "test", "Analysis", "evalOrder"]
     [ goldenEvalOrder "letFun" letFun
     , goldenEvalOrder "letImpure" letImpure
-    , embed . testCase "evalOrderLazy" $
-        4 @=? length (unEvalOrder $ termEvaluationOrder def dangerTerm)
+    , goldenEvalOrder "ifThenElse" termIfThenElse
+    , embed testEvalOrderIsLazy
+    , embed $
+        testGroup
+          "Purity"
+          [ testSomeTypeSomeTermArgsLeft
+          , testNoTypeSomeTermArgsLeft
+          , testNoTypeNoTermArgsLeft
+          , testForceNoTypeParam
+          , testApplyNoTermParam
+          ]
     ]
+
+testEvalOrderIsLazy :: TestTree
+testEvalOrderIsLazy =
+  testCase "evalOrderLazy" $
+    let order = termEvaluationOrder builtinSemantics dangerTerm
+        subterms = unEvalOrder order
+     in 4 @=? length subterms
+
+{-
+
+  Builtin functions can in theory have their type- and term- arguments
+  interleaved arbitrarily, but in the default set of builtins ('DefaultFun')
+  we have the following patterns:
+
+  - Builtin functions with 0 type arguments followed by N>0 term arguments:
+      - EncodeUtf8 : [ string ] -> bytestring
+      - AddInteger : [ integer, integer ] -> integer
+      - WriteBits  : [ bytestring, list(integer), bool ] -> bytestring
+
+  - Builtin functions with 1 type argument followed by N>0 term arguments:
+      - HeadList   : [ forall a, list(a) ] -> a
+      - Trace      : [ forall a, string, a ] -> a
+      - IfThenElse : [ forall a, bool, a, a ] -> a
+
+  - Builtin functions with 2 type arguments followed by N>0 term arguments:
+      - FstPair    : [ forall a, forall b, pair(a, b) ] -> a
+      - ChooseList : [ forall a, forall b, list(a), b, b ] -> b
+      - CaseList   : [ forall a, forall b
+                      , b
+                      , (fun a (fun [ (con list) a ] b))
+                      , list(a)
+                      ] -> b
+
+  When it comes to purity of builtin terms,
+  we want to test the following cases:
+  +---------------+---------------+
+  | Type args     | Term args     |
+  | left to apply | left to apply |
+  +---------------+---------------+
+  |     some      |     some      |
+  |     none      |     some      |
+  |     none      |     none      |
+  +---------------+---------------+
+-}
+
+testSomeTypeSomeTermArgsLeft :: TestTree
+testSomeTypeSomeTermArgsLeft =
+  testCase "some type args and some term args are unapplied" $
+    map (isPure builtinSemantics) terms @?= [True, True]
+ where
+  terms :: [Term Name DefaultUni DefaultFun ()] =
+    [ Builtin () Trace -- 1 type arg and 2 term args are unapplied
+    , Force () (Builtin () FstPair) -- 2 type args, 1 applied and 1 left
+    ]
+
+testNoTypeSomeTermArgsLeft :: TestTree
+testNoTypeSomeTermArgsLeft =
+  testCase "no type args and some term args are unapplied" $
+    map (isPure builtinSemantics) terms @?= [True, True]
+ where
+  terms :: [Term Name DefaultUni DefaultFun ()] =
+    [ Builtin () EncodeUtf8 -- no type args, 1 term arg left to apply
+    , Force () (Builtin () Trace) -- 1 type arg applied, 2 term args left
+    ]
+
+testNoTypeNoTermArgsLeft :: TestTree
+testNoTypeNoTermArgsLeft =
+  testGroup
+    "no type args and no term args are unapplied"
+    [ testAddInteger
+    , testIfThenElse
+    ]
+ where
+  testAddInteger :: TestTree =
+    testCase "AddInteger" $ isPure builtinSemantics term @?= False
+   where
+    term :: Term Name DefaultUni DefaultFun () =
+      Apply () (Apply () (Builtin () AddInteger) (termVar 1)) (termVar 2)
+
+  testIfThenElse :: TestTree =
+    testCase "IfThenElseApplied" $
+      isPure builtinSemantics termIfThenElse @?= False
+
+testForceNoTypeParam :: TestTree
+testForceNoTypeParam =
+  testCase "forcing a non-existing type param is impure" $
+    isPure builtinSemantics (Force () (Builtin () EncodeUtf8)) @?= False
+
+testApplyNoTermParam :: TestTree
+testApplyNoTermParam =
+  testGroup
+    "invalid application of a term param is impure"
+    [ testCase "when a type param is expected" $
+        isPure builtinSemantics termExpectingType @?= False
+    , testCase "when a builtin is saturated" $
+        isPure builtinSemantics termSaturated @?= False
+    ]
+ where
+  termExpectingType :: Term Name DefaultUni DefaultFun () =
+    Apply () (Builtin () Trace) (termVar 1)
+  termSaturated :: Term Name DefaultUni DefaultFun () =
+    Apply () (Builtin () EncodeUtf8) (termVar 1)
