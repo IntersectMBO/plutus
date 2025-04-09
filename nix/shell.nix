76e791825c05c99c069a6dfff1676646537a06dc
zeme <lorenzo.calegari@iohk.io>
2025-04-09 17:49:14 +0200

    Bump ghc966 To ghc967

--- nix/shell.nix
+++ nix/shell.nix
@@ -135,7 +135,7 @@ let
 
   shell = {
     ghc8107 = quick-shell;
-    ghc966 = full-shell;
+    ghc967 = full-shell;
     ghc984 = quick-shell;
     ghc9101 = quick-shell;
   }.${project.args.compiler-nix-name};
