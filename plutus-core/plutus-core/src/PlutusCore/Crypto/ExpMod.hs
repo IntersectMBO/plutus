122453e55da5fe3ed33574b7e6a5e7170fa43460
Kenneth MacKenzie <kenneth.mackenzie@iohk.io>
2025-04-30 17:00:26 +0100

    Fix `expModInteger` bug (#7064)
    * Fix bug in expModInteger
    
    * Update comment
    
    * Update golden files

--- plutus-core/plutus-core/src/PlutusCore/Crypto/ExpMod.hs
+++ plutus-core/plutus-core/src/PlutusCore/Crypto/ExpMod.hs
@@ -10,16 +10,23 @@ import PlutusCore.Builtin
 import GHC.Natural
 import GHC.Num.Integer
 
--- similar to `Default.Builtins.nonZeroSecondArg`
--- We don't really need it because integerPowMod# returns `()` on zero mod, but we put
--- in case of future implementation changes.
--- TODO https://github.com/IntersectMBO/plutus-private/issues/1474
-expMod _ _ 0 = fail "Cannot divide by zero"
-expMod b e m =
-    case integerPowMod# b e m of
-        (# n | #)  -> pure n
-        (# | () #) -> fail "expMod: failure"
-
-
+-- | Modular exponentiation.  This uses GHC.Num.integerPowMod#, which gives the
+-- wrong answer in some cases.  TODO: we'll be able to remove some of the guards
+-- when/if integerPowMod# gets fixed.
 expMod :: Integer -> Integer -> Natural -> BuiltinResult Natural
+expMod b e m
+  | m <= 0 = fail "expMod: invalid modulus"
+  -- ^ We can't have m<0 when m is a Natural, but we may as well be paranoid.
+  | m == 1 = pure 0
+  -- ^ Just in case: GHC.Num.Integer.integerRecip# gets this wrong.  Note that 0
+  -- is invertible modulo 1, with inverse 0.
+  | b == 0 && e < 0 = failNonInvertible 0 m
+  -- ^ integerPowMod# incorrectly returns 0 in this case.
+  | otherwise =
+      case integerPowMod# b e m of
+        (# n | #)  -> pure n
+        (# | () #) -> failNonInvertible b m
+  where failNonInvertible :: Integer -> Natural -> BuiltinResult Natural
+        failNonInvertible b1 m1 =
+          fail ("expMod: " ++ (show b1) ++ " is not invertible modulo " ++ (show m1))
 {-# INLINE expMod #-}
