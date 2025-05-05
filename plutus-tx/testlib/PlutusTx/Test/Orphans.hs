acf55af3304b9bb1083b795d4404f8e7a4e7da2d
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2025-05-05 17:18:25 +0200

    chore: extract PlutusTx.Test.Orphans

---
+++ plutus-tx/testlib/PlutusTx/Test/Orphans.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module PlutusTx.Test.Orphans where
+
+import Prelude
+
+import Flat (Flat)
+import PlutusCore qualified as PLC
+import PlutusCore.Builtin qualified as PLC
+import PlutusCore.Pretty (Pretty, PrettyConst)
+import PlutusCore.Pretty qualified as PLC
+import PlutusCore.Test (ToTPlc (..), ToUPlc (..), catchAll)
+import PlutusIR.Analysis.Builtins qualified as PIR
+import PlutusIR.Test ()
+import PlutusIR.Transform.RewriteRules qualified as PIR
+import PlutusPrelude (Default)
+import PlutusTx.Code (CompiledCodeIn, getPir, getPlcNoAnn)
+import Test.Tasty.Extras ()
+
+instance
+  (PLC.Closed uni, uni `PLC.Everywhere` Flat, Flat fun)
+  => ToUPlc (CompiledCodeIn uni fun a) uni fun
+  where
+  toUPlc compiledCode = toUPlc =<< catchAll (getPlcNoAnn compiledCode)
+
+instance
+  ( PLC.PrettyParens (PLC.SomeTypeIn uni)
+  , PLC.GEq uni
+  , PLC.Typecheckable uni fun
+  , PLC.Closed uni
+  , uni `PLC.Everywhere` PrettyConst
+  , Pretty fun
+  , uni `PLC.Everywhere` Flat
+  , Flat fun
+  , Default (PLC.CostingPart uni fun)
+  , Default (PIR.BuiltinsInfo uni fun)
+  , Default (PIR.RewriteRules uni fun)
+  )
+  => ToTPlc (CompiledCodeIn uni fun a) uni fun
+  where
+  toTPlc compiledCode =
+    catchAll (getPir compiledCode) >>= \case
+      Nothing -> fail "No PIR available"
+      Just program -> toTPlc program
