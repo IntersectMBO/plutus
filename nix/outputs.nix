282ace3a28af377ea7c900ea3bc85956ca0157c8
zeme <lorenzo.calegari@iohk.io>
2025-05-07 15:50:55 +0200

    wip

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -11,9 +11,6 @@ let
   utils = import ./utils.nix
     { inherit lib; };
 
-  build-latex = import ./build-latex.nix
-    { inherit pkgs; };
-
   r-with-packages = import ./r-with-packages.nix
     { inherit pkgs; };
 
@@ -24,10 +21,7 @@ let
     { inherit self pkgs lib metatheory-agda-library; };
 
   build-latex-doc = import ./build-latex-doc.nix
-    { inherit pkgs lib agda-tools build-latex; };
-
-  unraveling-recursion-paper = import ./unraveling-recursion-paper.nix
-    { inherit self pkgs lib build-latex agda-tools; };
+    { inherit pkgs lib agda-tools; };
 
   latex-documents = import ./latex-documents.nix
     { inherit self build-latex-doc; };
@@ -64,25 +58,18 @@ let
       [ "devShells" ]; # Won't build on Windows
   };
 
-  project-coverage-report =
-    project.projectVariants.ghc96-coverage.projectCoverageReport;
-
   extra-artifacts =
-    { inherit unraveling-recursion-paper; } //
     { inherit metatheory-site; } //
     { inherit metatheory-agda-library; } //
     (latex-documents);
 
   project-variants-hydra-jobs = {
-    ghc810 = (project.flake { }).hydraJobs.ghc810;
     ghc96 = (project.flake { }).hydraJobs.ghc96;
     ghc98 = (project.flake { }).hydraJobs.ghc98;
     ghc910 = (project.flake { }).hydraJobs.ghc910;
   };
 
   project-variants-roots-and-plan-nix = {
-    ghc810.roots = project-variants-hydra-jobs.ghc810.roots;
-    ghc810.plan-nix = project-variants-hydra-jobs.ghc810.plan-nix;
     ghc96.roots = project-variants-hydra-jobs.ghc96.roots;
     ghc96.plan-nix = project-variants-hydra-jobs.ghc96.plan-nix;
     ghc98.roots = project-variants-hydra-jobs.ghc98.roots;
@@ -91,6 +78,9 @@ let
     ghc910.plan-nix = project-variants-hydra-jobs.ghc910.plan-nix;
   };
 
+  project-coverage-report =
+    project.projectVariants.ghc96-coverage.projectCoverageReport;
+
   packages =
     exposed-haskell-packages //
     static-haskell-packages //
@@ -98,7 +88,6 @@ let
 
   non-profiled-shells = rec {
     default = ghc96;
-    ghc810 = mkShell project.projectVariants.ghc810;
     ghc96 = mkShell project.projectVariants.ghc96;
     ghc98 = mkShell project.projectVariants.ghc98;
     ghc910 = mkShell project.projectVariants.ghc910;
@@ -143,7 +132,6 @@ let
     inherit agda-tools;
     inherit r-with-packages;
     inherit build-latex-doc;
-    inherit build-latex;
     inherit extra-artifacts;
     inherit windows-packages;
     inherit static-haskell-packages;
