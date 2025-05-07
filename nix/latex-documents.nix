593488e7ae0f9d1be8ad72f1beed3526dc216011
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2025-05-07 11:50:49 +0200

    Simplify LaTeX Builds (#7058)

--- nix/latex-documents.nix
+++ nix/latex-documents.nix
@@ -2,42 +2,38 @@
 {
   cost-model-notes = build-latex-doc {
     name = "cost-model-notes";
-    src = self + /doc/notes/cost-model-notes;
     description = "Notes on cost models";
-    texFiles = [ "cost-model-notes.tex" ];
+    src = self + /doc/notes/cost-model-notes;
   };
 
   eutxo-paper = build-latex-doc {
     name = "eutxo-paper";
     description = "eutxo";
     src = self + /doc/papers/eutxo;
-    texFiles = [ "eutxo.tex" ];
   };
 
   eutxoma-paper = build-latex-doc {
     name = "eutxoma-paper";
     description = "eutxoma";
     src = self + /doc/papers/eutxoma;
-    texFiles = [ "eutxoma.tex" ];
   };
 
   extended-utxo-spec = build-latex-doc {
     name = "extended-utxo-spec";
-    src = self + /doc/extended-utxo-spec;
     description = "Extended UTXO specification";
+    src = self + /doc/extended-utxo-spec;
   };
 
   lazy-machine-notes = build-latex-doc {
     name = "lazy-machine-notes";
-    src = self + /doc/notes/fomega/lazy-machine;
-    texFiles = [ "lazy-plutus-core.tex" ];
     description = "lazy machine discussion";
+    src = self + /doc/notes/fomega/lazy-machine;
   };
 
   multi-currency-notes = build-latex-doc {
     name = "multi-currency-notes";
-    src = self + /doc/notes/multi-currency;
     description = "Multi-currency paper";
+    src = self + /doc/notes/multi-currency;
   };
 
   plutus-core-spec-old = build-latex-doc {
@@ -50,29 +46,30 @@
     name = "plutus-core-spec";
     description = "Plutus core specification";
     src = self + /doc/plutus-core-spec;
-    texFiles = [ "plutus-core-specification.tex" ];
   };
 
   plutus-report = build-latex-doc {
     name = "plutus-report";
     description = "plutus report";
     src = self + /doc/plutus-report;
-    texFiles = [ "plutus.tex" ];
-  };
-
-  system-f-in-agda-paper = build-latex-doc {
-    name = "system-f-in-agda-paper";
-    src = self + /doc/papers/system-f-in-agda;
-    description = "system-f in agda";
-    texFiles = [ "paper.tex" ];
-    withAgda = true;
-    agdaFile = "paper.lagda";
   };
 
   utxoma-paper = build-latex-doc {
     name = "utxoma-paper";
     description = "utxoma";
     src = self + /doc/papers/utxoma;
-    texFiles = [ "utxoma.tex" ];
+  };
+
+  system-f-in-agda-paper = build-latex-doc {
+    name = "system-f-in-agda-paper";
+    description = "system-f in agda";
+    src = self + /doc/papers/system-f-in-agda;
+  };
+
+  unraveling-recursion-paper = build-latex-doc {
+    name = "unraveling-recursion-paper";
+    description = "unraveling recursion-paper";
+    src = self + /doc/papers/unraveling-recursion;
   };
 }
+
