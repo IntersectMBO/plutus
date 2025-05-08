fa12373c1571798a7defa906de47bf28502c492e
kwxm <kenneth.mackenzie@iohk.io>
2025-05-08 05:49:46 +0100

    Big data

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
@@ -71,7 +71,8 @@ benchExpModInteger :: StdGen -> Benchmark
 benchExpModInteger _gen =
   let fun = ExpModInteger
       pow (a::Integer) (b::Integer) = a^b
-      moduli = fmap (\n -> pow 2 (32*n) - 11) [1, 3..31]
+--      moduli = fmap (\n -> pow 2 (32*n) - 11) [1, 3..31]
+      moduli = fmap (\n -> pow 2 (256*n) - 11) [1, 3..31]
       -- ^ 16 entries, sizes = 4, 12, ..., 124 bytes (memoryUsage = 1,2,...,16)
       es = fmap (\n -> pow 2 (fromIntegral $ integerLog2 n) - 1) moduli
       -- ^ Largest number less than modulus with binary expansion 1111...1.
