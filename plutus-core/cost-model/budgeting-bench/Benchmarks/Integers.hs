0470ae493cd7ce7a61c6c16fa4f8082fcd94e225
kwxm <kenneth.mackenzie@iohk.io>
2025-05-08 05:22:39 +0100

    Update comment

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Integers.hs
@@ -72,7 +72,7 @@ benchExpModInteger _gen =
   let fun = ExpModInteger
       pow (a::Integer) (b::Integer) = a^b
       moduli = fmap (\n -> pow 2 (32*n) - 11) [1, 3..31]
-      -- ^ 16 entries, sizes = 4, 12, ..., 124 bytes (memoryUsage 1,2,...,16)
+      -- ^ 16 entries, sizes = 4, 12, ..., 124 bytes (memoryUsage = 1,2,...,16)
       es = fmap (\n -> pow 2 (fromIntegral $ integerLog2 n) - 1) moduli
       -- ^ Largest number less than modulus with binary expansion 1111...1.
       -- This is the worst case.
