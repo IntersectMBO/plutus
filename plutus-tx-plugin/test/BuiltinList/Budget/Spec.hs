115f5d07c9838b41d155bc992e6d96616d99aaa4
zeme <lorenzo.calegari@iohk.io>
2025-06-03 14:55:24 +0200

    wip

--- plutus-tx-plugin/test/BuiltinList/Budget/Spec.hs
+++ plutus-tx-plugin/test/BuiltinList/Budget/Spec.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell   #-}
-{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:max-cse-iterations=0 #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:max-simplifier-iterations-pir=0 #-}
 {-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:max-simplifier-iterations-uplc=0 #-}
 
