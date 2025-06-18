f02a8d77cb4f1167dff7f86279f641127873cc0a
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2025-06-18 09:25:27 +0200

    Set minimum deployed version of haddock to 1.40.0.0 (#7153)

--- scripts/combined-haddock.sh
+++ scripts/combined-haddock.sh
@@ -241,7 +241,7 @@ echo "Injecting additional prologue html"
 list-valid-plutus-versions() {
   git fetch --tags --force
   local versions="$(git tag | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort -rV | uniq | tr ' ' '\n')"
-  local min_ver="1.30.0.0"
+  local min_ver="1.40.0.0"
   echo master 
   echo latest
   for ver in $versions; do
