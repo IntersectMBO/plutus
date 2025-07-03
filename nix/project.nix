a60d73d2cb59887d1358ecb48686146d70f577a8
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2025-07-03 14:26:24 +0200

    fix: comment out coq-ext-lib due to missing attribute error

--- nix/project.nix
+++ nix/project.nix
@@ -73,7 +73,9 @@ let
               pkgs.ocaml-ng.ocamlPackages_4_10.findlib
               pkgs.coqPackages_8_13.coq
               pkgs.coqPackages_8_13.mathcomp
-              pkgs.coqPackages_8_13.coq-ext-lib
+              # Commented out because it started causing error:
+              #   attribute 'coq-ext-lib' missing
+              # pkgs.coqPackages_8_13.coq-ext-lib
               pkgs.coqPackages_8_13.ssreflect
               pkgs.coqPackages_8_13.equations
             ];
