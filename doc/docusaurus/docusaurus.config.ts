133c10724ee4c9dc292248e63f0d17160d3d58a6
zeme <lorenzo.calegari@iohk.io>
2025-07-08 09:47:41 +0200

    wp

--- doc/docusaurus/docusaurus.config.ts
+++ doc/docusaurus/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs/",
+  baseUrl: process.env.DOCUSAURUS_BASE_URL || "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
