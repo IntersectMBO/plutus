658de5935a97943fcdf345b40d6827ba059a2dca
kwxm <kenneth.mackenzie@iohk.io>
2025-07-15 07:10:50 +0100

    Experiments with evaluateBuiltins

--- plutus-benchmark/bls12-381-costs/src/PlutusBenchmark/BLS12_381/RunTests.hs
+++ plutus-benchmark/bls12-381-costs/src/PlutusBenchmark/BLS12_381/RunTests.hs
@@ -19,26 +19,6 @@ import Prelude (IO, mapM_)
 
 -- Run various tests and print information about their costs
 
-printCosts_HashAndAddG1 :: Handle -> Integer -> IO ()
-printCosts_HashAndAddG1 h n =
-    let script = mkHashAndAddG1Script (listOfByteStringsOfLength n 4)
-    in printSizeStatistics h (TestSize n) script
-
-printCosts_HashAndAddG2 :: Handle -> Integer -> IO ()
-printCosts_HashAndAddG2 h n =
-    let script = mkHashAndAddG2Script (listOfByteStringsOfLength n 4)
-    in printSizeStatistics h (TestSize n) script
-
-printCosts_UncompressAndAddG1 :: Handle -> Integer -> IO ()
-printCosts_UncompressAndAddG1 h n =
-    let script = mkUncompressAndAddG1Script (listOfByteStringsOfLength n 4)
-    in printSizeStatistics h (TestSize n) script
-
-printCosts_UncompressAndAddG2 :: Handle -> Integer -> IO ()
-printCosts_UncompressAndAddG2 h n =
-    let script = mkUncompressAndAddG2Script (listOfByteStringsOfLength n 4)
-    in printSizeStatistics h (TestSize n) script
-
 printCosts_Pairing :: Handle -> IO ()
 printCosts_Pairing h = do
     let emptyDST = toBuiltin BS.empty
@@ -52,103 +32,8 @@ printCosts_Pairing h = do
 runTests :: Handle -> IO ()
 runTests h = do
 
-  hPrintf h "Hash n bytestrings onto G1 and add points\n\n"
-  printHeader h
-  mapM_ (printCosts_HashAndAddG1 h) [0, 10..150]
-  hPrintf h "\n\n"
-
-  hPrintf h "Hash n bytestrings onto G2 and add points\n\n"
-  printHeader h
-  mapM_ (printCosts_HashAndAddG2 h) [0, 10..150]
-  hPrintf h "\n\n"
-
-  hPrintf h "Uncompress n G1 points and add the results\n\n"
-  printHeader h
-  mapM_ (printCosts_UncompressAndAddG1 h) [0, 10..150]
-  hPrintf h "\n\n"
-
-  hPrintf h "Uncompress n G2 points and add the results\n\n"
-  printHeader h
-  mapM_ (printCosts_UncompressAndAddG2 h) [0, 10..150]
-  hPrintf h "\n\n"
-
   hPrintf h "Apply pairing to two pairs of points in G1 x G2 and run finalVerify on the results\n\n"
   printHeader h
   printCosts_Pairing h
   hPrintf h "\n\n"
 
-  hPrintf h "Groth16 verification example\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkGroth16VerifyScript
-  hPrintf h "\n"
-
-  hPrintf h "VRF example\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkVrfBlsPolicy
-  hPrintf h "\n"
-
-  hPrintf h "G1 Verify\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkG1VerifyPolicy
-  hPrintf h "\n"
-
-  hPrintf h "G2 Verify\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkG2VerifyPolicy
-  hPrintf h "\n"
-
-  hPrintf h "Aggregate Single Key\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkAggregateSingleKeyG1Policy
-  hPrintf h "\n"
-
-  hPrintf h "Aggregate Multi Key\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkAggregateMultiKeyG2Policy
-  hPrintf h "\n"
-
-  hPrintf h "Schnorr Signature G1\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkSchnorrG1VerifyPolicy
-  hPrintf h "\n"
-
-  hPrintf h "Schnorr Signature G2\n\n"
-  printHeader h
-  printSizeStatistics h NoSize mkSchnorrG2VerifyPolicy
-  hPrintf h "\n"
-
-  if checkGroth16Verify_Haskell
-  then hPrintf h "Groth16Verify succeeded\n"
-  else hPrintf h "Groth16Verify failed\n"
-
-  if checkVerifyBlsSimpleScript
-  then hPrintf h "Simple Verify succeeded\n"
-  else hPrintf h "Simple Verify failed\n"
-
-  if checkVrfBlsScript
-  then hPrintf h "VRF succeeded\n"
-  else hPrintf h "VRF failed\n"
-
-  if checkG1VerifyScript
-  then hPrintf h "G1 Verify succeeded\n"
-  else hPrintf h "G1 Verify failed\n"
-
-  if checkG2VerifyScript
-  then hPrintf h "G2 Verify succeeded\n"
-  else hPrintf h "G2 Verify failed\n"
-
-  if checkAggregateSingleKeyG1Script
-  then hPrintf h "Aggregate Signature Single Key G1 Verify succeeded\n"
-  else hPrintf h "Aggregate Signature Single Key G1 Verify failed\n"
-
-  if checkAggregateMultiKeyG2Script
-  then hPrintf h "Aggregate Signature Multi Key G2 Verify succeeded\n"
-  else hPrintf h "Aggregate Signature Multi Key G2 Verify failed\n"
-
-  if checkSchnorrG1VerifyScript
-  then hPrintf h "Schnorr G1 Verify succeeded\n"
-  else hPrintf h "Schnorr G1 Verify failed\n"
-
-  if checkSchnorrG2VerifyScript
-  then hPrintf h "Schnorr G2 Verify succeeded\n"
-  else hPrintf h "Schnorr G2 Verify failed\n"
