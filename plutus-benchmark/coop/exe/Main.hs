da01a89052205f09f391703c28686e54be137300
Seungheon Oh <me@sh5.org>
2025-07-16 13:13:33 -0500

    Add golden for script evaluation budgets

--- plutus-benchmark/coop/exe/Main.hs
+++ plutus-benchmark/coop/exe/Main.hs
@@ -16,56 +16,17 @@ import Data.Foldable (traverse_)
 import Flat (flat, unflat)
 import System.Directory (doesFileExist)
 import System.FilePath ((<.>), (</>))
-import Test.QuickCheck.Gen (Gen (unGen))
-import Test.QuickCheck.Random (mkQCGen)
 
 import PlutusCore.Annotation (SrcSpans)
-import PlutusLedgerApi.V1.Address (scriptHashAddress)
-import PlutusLedgerApi.V1.Value (AssetClass, assetClass)
-import PlutusLedgerApi.V2 (Address, CurrencySymbol (CurrencySymbol), Datum (Datum),
-                           Redeemer (Redeemer), ScriptHash (ScriptHash), TokenName (TokenName))
+import PlutusLedgerApi.V2 (Datum (Datum), Redeemer (Redeemer))
 import PlutusTx.Code (CompiledCodeIn (DeserializedCode, SerializedCode))
 import PlutusTx.Test.Util.Apply (unsafeApplyCodeN)
 import UntypedPlutusCore qualified as UPLC
 
 import PlutusBenchmark.Common (getDataDir)
-import PlutusBenchmark.Coop.Gen qualified as CG
 import PlutusBenchmark.Coop.Scripts qualified as Scripts
+import PlutusBenchmark.Coop.TestContext
 import PlutusBenchmark.Coop.Types
-
--- Some Constants Used for Script Context --
-coopAc :: AssetClass
-coopAc = assetClass (CurrencySymbol "$COOP CurrencySymbol") (TokenName "$COOP TokenName")
-
-aaAc :: AssetClass
-aaAc = assetClass (CurrencySymbol "$AA CurrencySymbol") (TokenName "$AA TokenName")
-
-certCs :: CurrencySymbol
-certCs = CurrencySymbol "cert-policy- hash"
-
-authCs :: CurrencySymbol
-authCs = CurrencySymbol "auth-policy- hash"
-
-fsCs :: CurrencySymbol
-fsCs = CurrencySymbol "fs-policy- hash"
-
-certVAddr :: Address
-certVAddr = scriptHashAddress . ScriptHash $ "@CertV hash"
-
-fsVAddr :: Address
-fsVAddr = scriptHashAddress . ScriptHash $ "@FsV hash"
-
-aaQ :: Integer
-aaQ = 42
-
-certMpParams :: CertMpParams
-certMpParams = CertMpParams aaAc aaQ certVAddr
-
-fsMpParams :: FsMpParams
-fsMpParams = FsMpParams coopAc fsVAddr (AuthParams authCs certCs)
-
-authMpParams :: AuthMpParams
-authMpParams = AuthMpParams aaAc aaQ
 
 getScriptDirectory :: IO FilePath
 getScriptDirectory = do
@@ -114,50 +75,45 @@ liftCodeDefAsData = liftCodeDef . toBuiltinData
 main :: IO ()
 main = do
   let
-    seed = 1
-    size = 1
-    runGen :: Gen a -> a
-    runGen g = unGen g (mkQCGen seed) size
     scripts =
       [ unsafeApplyCodeN
           Scripts.fsV
           (liftCodeDef $ Datum $ toBuiltinData ())
           (liftCodeDefAsData ())
-          (liftCodeDefAsData $ runGen CG.genCorrectMustBurnOwnSingletonValueCtx)
+          (liftCodeDefAsData correctMustBurnOwnSingletonValueContext)
 
       , unsafeApplyCodeN
           Scripts.certMp
           (liftCodeDef certMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData CertMpMint)
-          (liftCodeDefAsData $ runGen $ CG.genCorrectCertMpMintingCtx certMpParams certCs)
+          (liftCodeDefAsData correctCertMpMintingContext)
       , unsafeApplyCodeN
           Scripts.certMp
           (liftCodeDef certMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData CertMpBurn)
-          (liftCodeDefAsData $
-             runGen $ CG.genCertRdmrAc >>= CG.genCorrectCertMpBurningCtx certMpParams certCs)
+          (liftCodeDefAsData correctCertMpBurningContext)
 
       , unsafeApplyCodeN
           Scripts.fsMp
           (liftCodeDef fsMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData FsMpMint)
-          (liftCodeDefAsData $ runGen $ CG.genCorrectFsMpMintingCtx fsMpParams fsCs)
+          (liftCodeDefAsData correctFsMpMintingContext)
       , unsafeApplyCodeN
           Scripts.fsMp
           (liftCodeDef fsMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData FsMpBurn)
-          (liftCodeDefAsData $ runGen $ CG.genCorrectFsMpBurningCtx fsMpParams fsCs)
+          (liftCodeDefAsData correctFsMpBurningContext)
 
       , unsafeApplyCodeN
           Scripts.authMp
           (liftCodeDef authMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData AuthMpMint)
-          (liftCodeDefAsData $ runGen $ CG.genCorrectAuthMpMintingCtx authMpParams authCs)
+          (liftCodeDefAsData correctAuthMpMintingContext)
       , unsafeApplyCodeN
           Scripts.authMp
           (liftCodeDef authMpParams)
           (liftCodeDefAsData $ Redeemer $ toBuiltinData AuthMpBurn)
-          (liftCodeDefAsData $ runGen $ CG.genCorrectAuthMpBurningCtx authCs)
+          (liftCodeDefAsData correctAuthMpBurningContext)
       ]
 
   traverse_ (uncurry createIfNotExists) (zip ((\i -> "coop-" <> show @Integer i) <$> [1..]) scripts)
