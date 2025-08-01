6b030cc60ec90f922fd6176d1376ee924b345524
Pablo Lamela <pablo.lamela@iohk.io>
2025-08-01 18:49:10 +0200

    Ad-hoc changes for 32bit probably unsafe

--- plutus-core/plutus-core/src/PlutusCore/Default/Universe.hs
+++ plutus-core/plutus-core/src/PlutusCore/Default/Universe.hs
@@ -505,7 +505,6 @@ readKnownAsInteger term =
                     ]
 {-# INLINE readKnownAsInteger #-}
 
-#if WORD_SIZE_IN_BITS == 64
 -- See Note [Integral types as Integer].
 deriving via AsInteger Int instance
         KnownTypeAst tyname DefaultUni Int
@@ -524,7 +523,6 @@ instance KnownBuiltinTypeIn DefaultUni term Integer => MakeKnownIn DefaultUni te
 instance KnownBuiltinTypeIn DefaultUni term Integer => ReadKnownIn DefaultUni term Word where
     readKnown term = fromIntegral @Word64 @Word <$> readKnown term
     {-# INLINE readKnown #-}
-#endif
 
 deriving via AsInteger Int8 instance
         KnownTypeAst tyname DefaultUni Int8
