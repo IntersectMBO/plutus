4b9b60705c3f7169d57ebafa2e2a051fc8e05a57
Ana Pantilie <ana.pantilie95@gmail.com>
2025-08-02 15:47:08 +0300

    Criterion working

--- plutus-benchmark/values/bench/Bench.hs
+++ plutus-benchmark/values/bench/Bench.hs
@@ -13,29 +13,28 @@ import PlutusBenchmark.Common (getConfig)
 import PlutusBenchmark.Values.FlattenedValue qualified as FlattenedValue
 import PlutusBenchmark.Values.NestedValue qualified as NestedValue
 
-import Criterion.Main (bench, bgroup, defaultMainWith, env, whnf)
+import Criterion.Main (bench, bgroup, defaultMainWith, env, nf)
 
 mkMockValues :: Int -> Int -> [Integer] -> (NestedValue.Value, FlattenedValue.Value)
 mkMockValues numPolicies numToksPerPolicy amounts =
-    (NestedValue.empty, FlattenedValue.empty) -- Placeholder for actual implementation
-    -- let policies = (\i -> encodeUtf8 $ "policy" <> T.pack (show i)) <$> [1..numPolicies]
-    --     numTokens = numPolicies * numToksPerPolicy
-    --     tokens = (\i -> encodeUtf8 $ "token" <> T.pack (show i)) <$> [1..numTokens]
-    --     dupPolicies = concatMap (\p -> replicate numToksPerPolicy p) policies
-    --     rawFlatValue :: [((ByteString, ByteString), Integer)]
-    --     rawFlatValue = zip (zip dupPolicies tokens) amounts
-    --     flattenedValue = FlattenedValue.Value $ Map.fromList rawFlatValue
-    --     rawNestValue =
-    --         let x = groupBy (\((p1, _), _) ((p2, _), _) -> p1 == p2) rawFlatValue
-    --          in [ (p, tkMap) | l <- x, let p = (fst . fst) (head l), let tkMap = Map.fromList $ map (\((_, tk), am) -> (tk, am)) l ]
-    --     nestedValue = NestedValue.Value $ Map.fromList rawNestValue
-    --  in (nestedValue, flattenedValue)
+    let policies = (\i -> encodeUtf8 $ "policy" <> T.pack (show i)) <$> [1..numPolicies]
+        numTokens = numPolicies * numToksPerPolicy
+        tokens = (\i -> encodeUtf8 $ "token" <> T.pack (show i)) <$> [1..numTokens]
+        dupPolicies = concatMap (\p -> replicate numToksPerPolicy p) policies
+        rawFlatValue :: [((ByteString, ByteString), Integer)]
+        rawFlatValue = zip (zip dupPolicies tokens) amounts
+        flattenedValue = FlattenedValue.Value $ Map.fromList rawFlatValue
+        rawNestValue =
+            let x = groupBy (\((p1, _), _) ((p2, _), _) -> p1 == p2) rawFlatValue
+             in [ (p, tkMap) | l <- x, let p = (fst . fst) (head l), let tkMap = Map.fromList $ map (\((_, tk), am) -> (tk, am)) l ]
+        nestedValue = NestedValue.Value $ Map.fromList rawNestValue
+     in (nestedValue, flattenedValue)
 
 setupInsertEnvNested :: IO NestedValue.Value -- (ByteString, ByteString, NestedValue.Value)
 setupInsertEnvNested = do
     let polId = BS.empty -- encodeUtf8 "policyId"
         tokName = BS.empty -- encodeUtf8 "tokenName"
-        (nValue, _) = mkMockValues 100 20 (replicate 200 200)
+        (nValue, _) = mkMockValues 10000 2000 (replicate 2000000 200)
     -- pure (polId, tokName, nValue)
     pure nValue
 
@@ -43,21 +42,27 @@ setupInsertEnvFlattened :: IO FlattenedValue.Value -- (ByteString, ByteString, F
 setupInsertEnvFlattened = do
     let polId = BS.empty -- encodeUtf8 "policyId"
         tokName = BS.empty -- encodeUtf8 "tokenName"
-        (_, fValue) = mkMockValues 100 20 (replicate 200 200)
+        (_, fValue) = mkMockValues 10000 2000 (replicate 2000000 200)
     -- pure (polId, tokName, fValue)
     pure fValue
 
 
 main :: IO ()
 main = do
-    config <- getConfig 15.0
+    config <- getConfig 5
     defaultMainWith config $
         [ env setupInsertEnvNested $ \ ~nValue ->
-            bgroup "insertCoin"
-                [ bench "NestedValue" $ whnf (NestedValue.insertCoin BS.empty BS.empty 200) nValue
+            bgroup "NestedValue"
+                [ bench "insertCoin"
+                    $ nf (NestedValue.insertCoin (encodeUtf8 "policy500") (encodeUtf8 "token10000") 200) nValue
+                , bench "lookupCoin"
+                    $ nf (NestedValue.lookupCoin (encodeUtf8 "policy500") (encodeUtf8 "token10000")) nValue
                 ]
         , env setupInsertEnvFlattened $ \ ~fValue ->
-            bgroup "insertCoin"
-                [ bench "FlattenedValue" $ whnf (FlattenedValue.insertCoin BS.empty BS.empty 200) fValue
+            bgroup "FlattenedValue"
+                [ bench "insertCoin"
+                    $ nf (FlattenedValue.insertCoin (encodeUtf8 "policy500") (encodeUtf8 "token10000") 200) fValue
+                , bench "lookupCoin"
+                    $ nf (FlattenedValue.lookupCoin (encodeUtf8 "policy500") (encodeUtf8 "token10000")) fValue
                 ]
         ]
