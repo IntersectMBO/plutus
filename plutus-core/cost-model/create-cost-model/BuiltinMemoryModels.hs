55cba542a64bea26578a0522a8c4def48533fe1c
Ana Pantilie <ana.pantilie95@gmail.com>
2025-09-16 18:40:30 +0300

    Add insertCoin and unionValue costing skeleton

--- plutus-core/cost-model/create-cost-model/BuiltinMemoryModels.hs
+++ plutus-core/cost-model/create-cost-model/BuiltinMemoryModels.hs
@@ -181,5 +181,7 @@ builtinMemoryModels = BuiltinCostModelBase
   , paramValueContains                   = Id $ ModelTwoArgumentsConstantCost 32
   , paramValueData                       = Id $ ModelOneArgumentConstantCost 32
   , paramUnValueData                     = Id $ ModelOneArgumentConstantCost 32
+  , paramInsertCoin                      = Id $ ModelFourArgumentsConstantCost 1
+  , paramUnionValue                      = Id $ ModelTwoArgumentsConstantCost 1
   }
   where identityFunction = OneVariableLinearFunction 0 1
