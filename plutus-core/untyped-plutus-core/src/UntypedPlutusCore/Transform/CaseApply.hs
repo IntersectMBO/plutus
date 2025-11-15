5d158b7053969c240e10ee99522de09fe5dfc72c
Seungheon Oh <me@sh5.org>
2025-11-15 01:33:51 -0600

    Add `CaseApply` optimization

---
+++ plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Transform/CaseApply.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE LambdaCase #-}
+module UntypedPlutusCore.Transform.CaseApply
+    ( caseApply
+    ) where
+
+import Data.Vector qualified as V
+import UntypedPlutusCore.Core
+import UntypedPlutusCore.Transform.Simplifier (SimplifierStage (CaseApply), SimplifierT,
+                                               recordSimplification)
+
+caseApply
+    :: Monad m
+    => Term name uni fun a
+    -> SimplifierT name uni fun a m (Term name uni fun a)
+caseApply term = do
+  let result = processNestedApp term
+  recordSimplification term CaseApply result
+  return result
+
+{- |
+Transforms
+@@
+[[[[f a] b] c] ...]
+@@
+Into
+@@
+(case (constr 0 a b c ...) f)
+@@
+-}
+processNestedApp
+  :: Term name uni fun a
+  -> Term name uni fun a
+processNestedApp (Apply ann body arg) =
+  if length args >= 3
+  then
+    Case ann
+      (Constr ann 0 (processNestedApp <$> args))
+      (V.singleton (processNestedApp innerBody))
+  else Apply ann (processNestedApp body) (processNestedApp arg)
+  where
+    (args, innerBody) = consumeApp [arg] body
+    consumeApp as (Apply _ body' arg') = consumeApp (processNestedApp arg':as) body'
+    consumeApp as t                    = (as, t)
+processNestedApp (LamAbs ann name t) =
+  LamAbs ann name (processNestedApp  t)
+processNestedApp (Force ann t) =
+  Force ann (processNestedApp t)
+processNestedApp (Delay ann t) =
+  Delay ann (processNestedApp t)
+processNestedApp (Constr ann idx ts) =
+  Constr ann idx (processNestedApp <$> ts)
+processNestedApp (Case ann t bs) =
+  Case ann (processNestedApp t) (processNestedApp <$> bs)
+processNestedApp t = t
--- plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Transform/Simplifier.hs
+++ plutus-core/untyped-plutus-core/src/UntypedPlutusCore/Transform/Simplifier.hs
@@ -63,6 +63,7 @@ data SimplifierStage
   | CaseReduce
   | Inline
   | CSE
+  | CaseApply
 
 data Simplification name uni fun a =
   Simplification
