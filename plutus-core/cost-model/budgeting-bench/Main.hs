b7c479ea82adb9e090f5158b0c0af31b0693a60c
Ana Pantilie <ana.pantilie95@gmail.com>
2025-11-15 22:55:20 +0200

    Add benchmarking code and costing model for insertCoin

--- plutus-core/cost-model/budgeting-bench/Main.hs
+++ plutus-core/cost-model/budgeting-bench/Main.hs
@@ -48,20 +48,20 @@ main = do
   criterionMainWith
        Start
        defaultConfig $
-           Benchmarks.Bitwise.makeBenchmarks
-        <> Benchmarks.Bool.makeBenchmarks        gen
-        <> Benchmarks.ByteStrings.makeBenchmarks gen
-        <> Benchmarks.Crypto.makeBenchmarks      gen
-        <> Benchmarks.Data.makeBenchmarks        gen
-        <> Benchmarks.Integers.makeBenchmarks    gen
-        <> Benchmarks.Lists.makeBenchmarks       gen
-        <> Benchmarks.Arrays.makeBenchmarks      gen
-        <> Benchmarks.Misc.makeBenchmarks        gen
-        <> Benchmarks.Pairs.makeBenchmarks       gen
-        <> Benchmarks.Strings.makeBenchmarks     gen
-        <> Benchmarks.Tracing.makeBenchmarks     gen
-        <> Benchmarks.Unit.makeBenchmarks        gen
-        <> Benchmarks.Values.makeBenchmarks      gen
+            Benchmarks.Bitwise.makeBenchmarks
+         <> Benchmarks.Bool.makeBenchmarks        gen
+         <> Benchmarks.ByteStrings.makeBenchmarks gen
+         <> Benchmarks.Crypto.makeBenchmarks      gen
+         <> Benchmarks.Data.makeBenchmarks        gen
+         <> Benchmarks.Integers.makeBenchmarks    gen
+         <> Benchmarks.Lists.makeBenchmarks       gen
+         <> Benchmarks.Arrays.makeBenchmarks      gen
+         <> Benchmarks.Misc.makeBenchmarks        gen
+         <> Benchmarks.Pairs.makeBenchmarks       gen
+         <> Benchmarks.Strings.makeBenchmarks     gen
+         <> Benchmarks.Tracing.makeBenchmarks     gen
+         <> Benchmarks.Unit.makeBenchmarks        gen
+         <> Benchmarks.Values.makeBenchmarks      gen
 
   {- Run the nop benchmarks with a large time limit (30 seconds) in an attempt to
      get accurate results. -}
