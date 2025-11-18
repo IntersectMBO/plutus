c009c1025757dde824b07e74705ff62ac7ccb7f2
zeme <lorenzo.calegari@iohk.io>
2025-11-18 17:14:36 +0100

    Before applying

--- nix/fourmolu.nix
+++ nix/fourmolu.nix
@@ -5,15 +5,24 @@
 ghc:
 
 let
+
+  attrs = {
+    "ghc96".rev = "0.17.0.0";
+    "ghc96".sha256 = "";
+
+    "ghc912".rev = "0.19.0.1";
+    "ghc912".sha256 = "";
+  }.${ghc};
+
   fourmolu-project = pkgs.haskell-nix.cabalProject' {
     src = pkgs.fetchFromGitHub {
       owner = "zeme-wana";
       repo = "fourmolu";
-      rev = "3644b248a6ce19a85cce46edb20f5de08cb7546a";
-      sha256 = "sha256-zyEkx4oKHQepPb+52dNVkg93SZHWeVf6JqM5WFOmqJE=";
+      inherit (attrs) rev sha256;
     };
     compiler-nix-name = ghc;
   };
+
 in
 
 fourmolu-project.hsPkgs.fourmolu.components.exes.fourmolu
