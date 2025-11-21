c60f30d5351e74dedf6ea501efdb59fc4d1075cb
zeme <lorenzo.calegari@iohk.io>
2025-11-21 09:34:49 +0100

    Support ghc912

--- nix/fourmolu.nix
+++ nix/fourmolu.nix
@@ -8,10 +8,10 @@ let
 
   attrs = {
     "ghc96".rev = "0.17.0.0";
-    "ghc96".sha256 = "";
+    "ghc96".sha256 = "sha256-SzPmmpLOkRF6eLSSFzw/ZV1ERPvQOuIPfhZ/gpNpfZQ=";
 
     "ghc912".rev = "0.19.0.1";
-    "ghc912".sha256 = "";
+    "ghc912".sha256 = "sha256-8A+LkCoXJj0edVe6lYEk5o0Nra+MC2Qm6i5Bribp1g4=";
   }.${ghc};
 
   fourmolu-project = pkgs.haskell-nix.cabalProject' {
