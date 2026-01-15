390c363c428b577ca2485690d2145aca471f7b7c
kwxm <kenneth.mackenzie@iohk.io>
2026-01-15 06:49:31 +0000

    Temporary above-diagonal inputs for valueContains

--- plutus-core/cost-model/budgeting-bench/Benchmarks/Values.hs
+++ plutus-core/cost-model/budgeting-bench/Benchmarks/Values.hs
@@ -123,7 +123,7 @@ valueContainsBenchmark gen =
     -- when contained size > container size, containment is impossible
     ValueContains -- the builtin fun
     [] -- no type arguments needed (monomorphic builtin)
-    (valueContainsArgs gen) -- the argument combos to generate benchmarks for
+    (take 50 $ fmap (\(x, y) -> (y, x)) $ (valueContainsArgs gen)) -- the argument combos to generate benchmarks for
 
 valueContainsArgs :: StdGen -> [(Value, Value)]
 valueContainsArgs gen = runStateGen_ gen \g -> do
