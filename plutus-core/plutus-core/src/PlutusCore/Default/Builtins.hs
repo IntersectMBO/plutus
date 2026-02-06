54e88b505d28acafa1cce576b7bbd2e3371f48b5
Pablo Lamela <pablo.lamela@iohk.io>
2026-02-06 03:57:32 +0100

    Fix issue with `KnownTypeAst` and wasm

--- plutus-core/plutus-core/src/PlutusCore/Default/Builtins.hs
+++ plutus-core/plutus-core/src/PlutusCore/Default/Builtins.hs
@@ -57,6 +57,7 @@ import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as BSL
 import Data.ByteString.Short qualified as SBS
+import Data.Int (Int64)
 import Data.Ix (Ix)
 import Data.Text.Array (pattern ByteArray)
 import Data.Text.Encoding (encodeUtf8)
@@ -1201,28 +1202,38 @@ instance uni ~ DefaultUni => ToBuiltinMeaning uni DefaultFun where
           DefaultFunSemanticsVariantD -> consByteStringMeaning_V1
           DefaultFunSemanticsVariantE -> consByteStringMeaning_V2
   toBuiltinMeaning _semvar SliceByteString =
+<<<<<<< HEAD
     let sliceByteStringDenotation :: Int -> Int -> BS.ByteString -> BuiltinResult BS.ByteString
         sliceByteStringDenotation start n xs = pure $ BS.take n (BS.drop start xs)
+=======
+    let sliceByteStringDenotation :: Int64 -> Int64 -> BS.ByteString -> BS.ByteString
+        sliceByteStringDenotation start n xs = BS.take (fromIntegral n) (BS.drop (fromIntegral start) xs)
+>>>>>>> 32d3b8b919 (Fix issue with `KnownTypeAst` and wasm)
         {-# INLINE sliceByteStringDenotation #-}
      in makeBuiltinMeaning
           sliceByteStringDenotation
           (runCostingFunThreeArguments . paramSliceByteString)
   toBuiltinMeaning _semvar LengthOfByteString =
+<<<<<<< HEAD
     let lengthOfByteStringDenotation :: BS.ByteString -> BuiltinResult Int
         lengthOfByteStringDenotation = pure . BS.length
+=======
+    let lengthOfByteStringDenotation :: BS.ByteString -> Int64
+        lengthOfByteStringDenotation = fromIntegral . BS.length
+>>>>>>> 32d3b8b919 (Fix issue with `KnownTypeAst` and wasm)
         {-# INLINE lengthOfByteStringDenotation #-}
      in makeBuiltinMeaning
           lengthOfByteStringDenotation
           (runCostingFunOneArgument . paramLengthOfByteString)
   toBuiltinMeaning _semvar IndexByteString =
-    let indexByteStringDenotation :: BS.ByteString -> Int -> BuiltinResult Word8
+    let indexByteStringDenotation :: BS.ByteString -> Int64 -> BuiltinResult Word8
         indexByteStringDenotation xs n = do
-          unless (n >= 0 && n < BS.length xs) $
+          unless (n >= 0 && n < fromIntegral (BS.length xs)) $
             -- See Note [Structural vs operational errors within builtins].
             -- The arguments are going to be printed in the "cause" part of the error
             -- message, so we don't need to repeat them here.
             fail "Index out of bounds"
-          pure $ BS.index xs n
+          pure $ BS.index xs (fromIntegral n)
         {-# INLINE indexByteStringDenotation #-}
      in makeBuiltinMeaning
           indexByteStringDenotation
@@ -1811,8 +1822,8 @@ instance uni ~ DefaultUni => ToBuiltinMeaning uni DefaultFun where
   -- Bitwise operations
 
   toBuiltinMeaning _semvar ReadBit =
-    let readBitDenotation :: BS.ByteString -> Int -> BuiltinResult Bool
-        readBitDenotation = Bitwise.readBit
+    let readBitDenotation :: BS.ByteString -> Int64 -> BuiltinResult Bool
+        readBitDenotation bs = Bitwise.readBit bs . fromIntegral
         {-# INLINE readBitDenotation #-}
      in makeBuiltinMeaning
           readBitDenotation
@@ -1850,15 +1861,15 @@ instance uni ~ DefaultUni => ToBuiltinMeaning uni DefaultFun where
           rotateByteStringDenotation
           (runCostingFunTwoArguments . paramRotateByteString)
   toBuiltinMeaning _semvar CountSetBits =
-    let countSetBitsDenotation :: BS.ByteString -> Int
-        countSetBitsDenotation = Bitwise.countSetBits
+    let countSetBitsDenotation :: BS.ByteString -> Int64
+        countSetBitsDenotation = fromIntegral . Bitwise.countSetBits
         {-# INLINE countSetBitsDenotation #-}
      in makeBuiltinMeaning
           countSetBitsDenotation
           (runCostingFunOneArgument . paramCountSetBits)
   toBuiltinMeaning _semvar FindFirstSetBit =
-    let findFirstSetBitDenotation :: BS.ByteString -> Int
-        findFirstSetBitDenotation = Bitwise.findFirstSetBit
+    let findFirstSetBitDenotation :: BS.ByteString -> Int64
+        findFirstSetBitDenotation = fromIntegral . Bitwise.findFirstSetBit
         {-# INLINE findFirstSetBitDenotation #-}
      in makeBuiltinMeaning
           findFirstSetBitDenotation
@@ -1937,10 +1948,10 @@ instance uni ~ DefaultUni => ToBuiltinMeaning uni DefaultFun where
           dropListDenotation
           (runCostingFunTwoArguments . paramDropList)
   toBuiltinMeaning _semvar LengthOfArray =
-    let lengthOfArrayDenotation :: SomeConstant uni (Vector a) -> BuiltinResult Int
+    let lengthOfArrayDenotation :: SomeConstant uni (Vector a) -> BuiltinResult Int64
         lengthOfArrayDenotation (SomeConstant (Some (ValueOf uni vec))) =
           case uni of
-            DefaultUniArray _uniA -> pure $ Vector.length vec
+            DefaultUniArray _uniA -> pure $ fromIntegral (Vector.length vec)
             _ -> throwError $ structuralUnliftingError "Expected an array but got something else"
         {-# INLINE lengthOfArrayDenotation #-}
      in makeBuiltinMeaning lengthOfArrayDenotation (runCostingFunOneArgument . paramLengthOfArray)
@@ -1954,11 +1965,11 @@ instance uni ~ DefaultUni => ToBuiltinMeaning uni DefaultFun where
         {-# INLINE listToArrayDenotation #-}
      in makeBuiltinMeaning listToArrayDenotation (runCostingFunOneArgument . paramListToArray)
   toBuiltinMeaning _semvar IndexArray =
-    let indexArrayDenotation :: SomeConstant uni (Vector a) -> Int -> BuiltinResult (Opaque val a)
+    let indexArrayDenotation :: SomeConstant uni (Vector a) -> Int64 -> BuiltinResult (Opaque val a)
         indexArrayDenotation (SomeConstant (Some (ValueOf uni vec))) n =
           case uni of
             DefaultUniArray arg -> do
-              case vec Vector.!? n of
+              case vec Vector.!? fromIntegral n of
                 Nothing -> fail "Array index out of bounds"
                 Just el -> pure $ fromValueOf arg el
             _ ->
