cb4df31e466638589714b399c8e6e2551d2bd57d
Yuriy Lazaryev <yuriy.lazaryev@iohk.io>
2026-02-09 17:49:32 +0100

    test(plutus-tx): add golden tests for deriveOrd
    Add MyNewtype, SomeLargeADT, Tree, and These golden tests to match
    deriveEq coverage. Add unit tests for new types including newtype,
    large ADT, and recursive tree comparisons.

--- plutus-tx/test/Ord/Spec.hs
+++ plutus-tx/test/Ord/Spec.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE EmptyDataDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE EmptyDataDeriving #-}
 
 module Ord.Spec (ordTests) where
 
@@ -11,6 +11,7 @@ import PlutusTx.Enum as Tx
 import PlutusTx.Eq
 import PlutusTx.Ord as Tx
 import PlutusTx.Test.Golden
+import PlutusTx.These (These)
 import Test.Tasty
 import Test.Tasty.Extras
 import Test.Tasty.HUnit
@@ -41,6 +42,26 @@ newtype PhantomADT e = PhantomADT ()
 deriveEq ''PhantomADT
 deriveOrd ''PhantomADT
 
+newtype MyNewtype = MyNewtype Integer
+  deriving stock (HS.Eq, HS.Ord)
+deriveEq ''MyNewtype
+deriveOrd ''MyNewtype
+
+data SomeLargeADT a b c d e
+  = SomeLargeADT1 Integer a Bool b c d
+  | SomeLargeADT2
+  | SomeLargeADT3 {f1 :: e, f2 :: e, _f3 :: e, _f4 :: e, _f5 :: e}
+  deriving stock (HS.Eq, HS.Ord)
+deriveEq ''SomeLargeADT
+deriveOrd ''SomeLargeADT
+
+data Tree a
+  = Leaf a
+  | Node (Tree a) (Tree a)
+  deriving stock (HS.Eq, HS.Ord)
+deriveEq ''Tree
+deriveOrd ''Tree
+
 data SomeVoid
   deriving stock (HS.Eq, HS.Ord)
 deriveEq ''SomeVoid
@@ -51,11 +72,24 @@ unitTests =
   testGroup "PlutusTx.Ord unit tests" $
     let l = Tx.enumFromTo @SomeVeryLargeEnum HS.minBound HS.maxBound
         l' = HS.tail l
+        v1 :: SomeLargeADT () BuiltinByteString () () Integer
+        v1 = SomeLargeADT3 1 2 3 4 5
+        v2 :: SomeLargeADT () BuiltinByteString () () Integer
+        v2 = SomeLargeADT3 1 2 3 4 6
+        v3 :: SomeLargeADT () BuiltinByteString () () Integer
+        v3 = SomeLargeADT2
      in [ testCase "enum series is lt" $ zipWith Tx.compare l l' @?= (take (length l') [LT, LT ..])
         , testCase "product1" $ SomeProduct 1 (encodeUtf8 "a") True (Right ()) Tx.> SomeProduct 0 (encodeUtf8 "a") True (Right ()) @? "product1 failed"
         , testCase "product2" $ SomeProduct 1 (encodeUtf8 "a") True (Right ()) Tx.< SomeProduct 1 (encodeUtf8 "b") True (Left ()) @? "product2 failed"
         , testCase "product3" $ SomeProduct 1 (encodeUtf8 "a") True (Right ()) Tx.> SomeProduct 1 (encodeUtf8 "a") False (Left ()) @? "product3 failed"
-        , testCase "product3" $ SomeProduct 1 (encodeUtf8 "a") True (Left ()) Tx.< SomeProduct 1 (encodeUtf8 "a") True (Right ()) @? "product4 failed"
+        , testCase "product4" $ SomeProduct 1 (encodeUtf8 "a") True (Left ()) Tx.< SomeProduct 1 (encodeUtf8 "a") True (Right ()) @? "product4 failed"
+        , testCase "newtype-lt" $ MyNewtype 1 Tx.< MyNewtype 2 @? "newtype-lt failed"
+        , testCase "newtype-eq" $ Tx.compare (MyNewtype 42) (MyNewtype 42) @?= EQ
+        , testCase "large-adt-same-con" $ Tx.compare v1 v2 @?= LT
+        , testCase "large-adt-diff-con" $ Tx.compare v3 v1 @?= LT
+        , testCase "tree-leaf-lt" $ Tx.compare (Leaf (1 :: Integer)) (Leaf 2) @?= LT
+        , testCase "tree-node" $ Tx.compare (Node (Leaf (1 :: Integer)) (Leaf 2)) (Node (Leaf 1) (Leaf 3)) @?= LT
+        , testCase "tree-leaf-vs-node" $ Tx.compare (Leaf (1 :: Integer)) (Node (Leaf 0) (Leaf 0)) @?= LT
         , testCase "void" $ (undefined :: SomeVoid) `Tx.compare` undefined @=? (undefined :: SomeVoid) `HS.compare` undefined
         ]
 
@@ -66,6 +100,11 @@ goldenTests =
     [ $(goldenCodeGen "SomeVeryLargeEnum" (deriveOrd ''SomeVeryLargeEnum))
     , $(goldenCodeGen "SomeProduct" (deriveOrd ''SomeProduct))
     , $(goldenCodeGen "PhantomADT" (deriveOrd ''PhantomADT))
+    , $(goldenCodeGen "MyNewtype" (deriveOrd ''MyNewtype))
+    , $(goldenCodeGen "SomeLargeADT" (deriveOrd ''SomeLargeADT))
+    , $(goldenCodeGen "Tree" (deriveOrd ''Tree))
+    , -- Types from PlutusTx modules that use deriveOrd
+      $(goldenCodeGen "These" (deriveOrd ''These))
     ]
 
 ordTests :: TestTree
